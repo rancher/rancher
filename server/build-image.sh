a816f20b7b4233faca9709e96654b958d4aceff7
Darren Shepherd <darren@rancher.com>
2015-07-15 22:22:20 -0700

    v0.28.0

--- server/build-image.sh
+++ server/build-image.sh
@@ -3,8 +3,11 @@ set -e -x
 
 cd $(dirname $0)
 
-mkdir -p target
-docker run -it -v $(pwd)/target:/output rancher/s6-builder:v0.1.0 /opt/build.sh
+if [ ! -e target/.done ]; then
+    mkdir -p target
+    docker run -it -v $(pwd)/target:/output rancher/s6-builder:v0.1.0 /opt/build.sh
+    touch target/.done
+fi
 
 TAG=${TAG:-$(grep CATTLE_RANCHER_SERVER_IMAGE Dockerfile | awk '{print $3}')}
 IMAGE=rancher/server:${TAG}
