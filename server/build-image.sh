7cb12b18ae3a49de32bd021ce8d34250f996ff5e
Adam White <adam.white@buyerquest.com>
2016-04-15 13:11:56 -0400

    Adding quotes for globbing

--- server/build-image.sh
+++ server/build-image.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 set -e -x
 
-cd $(dirname $0)
+cd "$(dirname "$0")"
 
 if [ ! -e target/.done ]; then
     mkdir -p target
-    docker run -it -v $(pwd)/target:/output rancher/s6-builder:v0.1.0 /opt/build.sh
+    docker run -it -v "$(pwd)/target:/output" rancher/s6-builder:v0.1.0 /opt/build.sh
     touch target/.done
 fi
 
 TAG=${TAG:-$(awk '/CATTLE_RANCHER_SERVER_IMAGE/{print $3}' Dockerfile)}
 IMAGE=rancher/server:${TAG}
 
-docker build -t ${IMAGE} .
+docker build -t "${IMAGE}" .
 
-echo Done building ${IMAGE}
+echo Done building "${IMAGE}"
