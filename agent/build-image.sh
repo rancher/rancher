d03543b0cd669fc4fd8db91ad9df6526b428e984
Ivan Mikushin <i.mikushin@gmail.com>
2016-06-09 16:38:07 -0700

    Build agent image for amd64 and arm

--- agent/build-image.sh
+++ agent/build-image.sh
@@ -1,12 +1,44 @@
 #!/bin/bash
+set -e -x
+
+ARCH=${ARCH:-"$(docker version -f {{.Server.Arch}})"}
+IMAGE=${IMAGE:-"$(grep 'ENV RANCHER_AGENT_IMAGE ' Dockerfile | awk '{print $3}')"}
 
 cd $(dirname $0)
+cat > image-manifest.yml << EOF
+image: ${IMAGE}
+manifests:
+- image: ${IMAGE}_amd64
+  platform:
+    architecture: amd64
+    os: linux
+EOF
 
-if [ -z "$TAG" ]; then
-    TAG=$(grep RANCHER_AGENT_IMAGE Dockerfile | cut -f2 -d:)
+FROM_IMAGE=$(grep -e '^FROM ' Dockerfile | awk '{print $2}')
+ARCH_IMAGES=($(grep -e '^# FROM ' Dockerfile | head -n 1 | sed 's/# FROM //'))
+for s in "${ARCH_IMAGES[@]}"; do
+    a="$(echo ${s} | cut -f1 -d=)"
+    if [ "${a}" = "${ARCH}" ]; then
+        FROM_ARCH_IMAGE="$(echo ${s} | cut -f2 -d=)"
+    fi
+    cat >> image-manifest.yml << EOF
+- image: ${IMAGE}_${a}
+  platform:
+    architecture: ${a}
+    os: linux
+EOF
+done
+if [ "${FROM_ARCH_IMAGE}" != "" ]; then
+    docker inspect ${FROM_ARCH_IMAGE} >/dev/null 2>&1 || docker pull ${FROM_ARCH_IMAGE}
+    docker tag ${FROM_ARCH_IMAGE} ${FROM_IMAGE}
 fi
 
-IMAGE=rancher/agent:${TAG}
+cat Dockerfile | sed "s/^# ARCH=${ARCH}: //" > Dockerfile.tmp
+trap 'rm Dockerfile.tmp' EXIT
 
-echo Building $IMAGE
-docker build -t ${IMAGE} .
+docker build -t ${IMAGE}_${ARCH} -f Dockerfile.tmp .
+docker tag ${IMAGE}_${ARCH} ${IMAGE}
+
+if [ -n "$IMAGE_REGISTRY" ]; then
+    echo $IMAGE >> ${IMAGE_REGISTRY}
+fi
