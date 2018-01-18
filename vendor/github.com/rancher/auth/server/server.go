e193c4f0169fed342eb18c75c87794e9e0341667
Craig Jellick <craig@rancher.com>
2018-01-18 16:33:04 -0700

    vendor update for auth tokens

---
+++ vendor/github.com/rancher/auth/server/server.go
@@ -0,0 +1,46 @@
+package server
+
+import (
+	"context"
+	"net/http"
+
+	normanapi "github.com/rancher/norman/api"
+	"github.com/rancher/norman/types"
+	managementSchema "github.com/rancher/types/apis/management.cattle.io/v3/schema"
+	"github.com/rancher/types/config"
+
+	"github.com/rancher/auth/api/setup"
+	"github.com/rancher/auth/tokens"
+)
+
+var crdVersions = []*types.APIVersion{
+	&managementSchema.Version,
+}
+
+func NewTokenAPIHandler(ctx context.Context, mgmtCtx *config.ManagementContext) (http.Handler, error) {
+	err := tokens.NewTokenAPIServer(ctx, mgmtCtx)
+	if err != nil {
+		return nil, err
+	}
+
+	handler, err := new(ctx, mgmtCtx)
+	if err != nil {
+		return nil, err
+	}
+	return handler, nil
+}
+
+func new(ctx context.Context, mgmtCtx *config.ManagementContext) (http.Handler, error) {
+	schemas := types.NewSchemas().AddSchemas(managementSchema.Schemas)
+
+	if err := setup.Schemas(ctx, mgmtCtx, schemas); err != nil {
+		return nil, err
+	}
+
+	server := normanapi.NewAPIServer()
+
+	if err := server.AddSchemas(schemas); err != nil {
+		return nil, err
+	}
+	return server, nil
+}
