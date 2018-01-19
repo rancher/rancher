ba14f697cbfb85cdd3098e86aae06424ed550196
Darren Shepherd <darren@rancher.com>
2018-01-18 22:22:59 -0700

    golint fixes

--- server/server.go
+++ server/server.go
@@ -8,9 +8,9 @@ import (
 	"github.com/rancher/auth/filter"
 	"github.com/rancher/auth/server"
 	managementapi "github.com/rancher/management-api/server"
+	"github.com/rancher/rancher/server/capabilities"
 	"github.com/rancher/rancher/server/proxy"
 	"github.com/rancher/rancher/server/ui"
-	"github.com/rancher/rancher/server/capabilities"
 	managementSchema "github.com/rancher/types/apis/management.cattle.io/v3/schema"
 	"github.com/rancher/types/config"
 )
