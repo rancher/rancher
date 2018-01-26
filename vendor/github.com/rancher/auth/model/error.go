6759a1cfa9c6497f4a28ee257b8a703f09d4aaa4
Darren Shepherd <darren@rancher.com>
2018-01-26 08:57:52 -0700

    Update vendor

--- vendor/github.com/rancher/auth/model/error.go
+++
@@ -1,8 +0,0 @@
-package model
-
-//AuthError structure contains the error resource definition
-type AuthError struct {
-	Type    string `json:"type"`
-	Status  string `json:"status"`
-	Message string `json:"message"`
-}
