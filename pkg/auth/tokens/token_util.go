606eb57a2ccd2491d675285453b296b64fda46d4
Darren Shepherd <darren@rancher.com>
2018-01-29 13:06:17 -0700

    Move token generation to reusable function

--- pkg/auth/tokens/token_util.go
+++ pkg/auth/tokens/token_util.go
@@ -1,36 +1,15 @@
 package tokens
 
 import (
-	"crypto/rand"
 	"encoding/base64"
-	"github.com/sirupsen/logrus"
-	"math/big"
 	"net/http"
 	"strconv"
 	"strings"
 	"time"
 
 	"github.com/rancher/types/apis/management.cattle.io/v3"
+	"github.com/sirupsen/logrus"
 )
-
-const (
-	characters  = "bcdfghjklmnpqrstvwxz2456789"
-	tokenLength = 54
-)
-
-var charsLength = big.NewInt(int64(len(characters)))
-
-func generateKey() (string, error) {
-	token := make([]byte, tokenLength)
-	for i := range token {
-		r, err := rand.Int(rand.Reader, charsLength)
-		if err != nil {
-			return "", err
-		}
-		token[i] = characters[r.Int64()]
-	}
-	return string(token), nil
-}
 
 func getAuthProviderName(principalID string) string {
 	parts := strings.Split(principalID, "://")
