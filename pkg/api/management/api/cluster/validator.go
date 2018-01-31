1b9a32875ae7bd508569e100c12e77efa9f26fd8
Alena Prokharchyk <alena@rancher.com>
2018-01-31 14:57:13 -0800

    Cluster config: validate as top level field

--- pkg/api/management/api/cluster/validator.go
+++ pkg/api/management/api/cluster/validator.go
@@ -8,20 +8,9 @@ import (
 	"github.com/rancher/norman/types/convert"
 )
 
-const (
-	specField = "spec"
-)
-
 func Validator(request *types.APIContext, schema *types.Schema, data map[string]interface{}) error {
-	spec, ok := data[specField]
-	if !ok {
-		return nil
-	}
-
-	specData, _ := convert.EncodeToMap(spec)
-
 	found := false
-	for k, v := range specData {
+	for k, v := range data {
 		if strings.HasSuffix(k, "Config") && !convert.IsEmpty(v) {
 			found = true
 			break
