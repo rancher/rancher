c563885692307620e1d4c258f12b2f81cc58b952
Darren Shepherd <darren@rancher.com>
2018-02-07 13:34:39 -0700

    Rename cluster to user and put all controllers in controllers package

---
+++ pkg/api/user/store/ingress/store.go
@@ -0,0 +1,60 @@
+package ingress
+
+import (
+	"github.com/rancher/norman/store/proxy"
+	"github.com/rancher/norman/types"
+	"github.com/rancher/rancher/pkg/controllers/user/ingress"
+	"github.com/rancher/types/config"
+	"github.com/satori/uuid"
+)
+
+type Store struct {
+	types.Store
+	proxyStore *proxy.Store
+	controller *ingress.Controller
+}
+
+func NewStore(workload *config.UserOnlyContext) *Store {
+	return &Store{
+		Store: proxy.NewProxyStore(workload.UnversionedClient,
+			[]string{"apis"},
+			"extensions",
+			"v1beta1",
+			"Ingress",
+			"ingresses"),
+		proxyStore: proxy.NewRawProxyStore(workload.UnversionedClient,
+			[]string{"apis"},
+			"extensions",
+			"v1beta1",
+			"Ingress",
+			"ingresses"),
+		controller: ingress.NewIngressWorkloadController(workload),
+	}
+}
+
+func (s *Store) Create(apiContext *types.APIContext, schema *types.Schema, data map[string]interface{}) (map[string]interface{}, error) {
+	data["uuid"] = uuid.NewV4().String()
+	_, err := s.controller.Reconcile(data, true)
+	if err != nil {
+		return nil, err
+	}
+	return s.Store.Create(apiContext, schema, data)
+}
+
+func (s *Store) Update(apiContext *types.APIContext, schema *types.Schema, data map[string]interface{}, id string) (map[string]interface{}, error) {
+	existing, err := s.Store.ByID(apiContext, schema, id)
+	if err != nil {
+		return existing, err
+	}
+
+	for k, v := range data {
+		existing[k] = v
+	}
+
+	_, err = s.controller.Reconcile(existing, true)
+	if err != nil {
+		return nil, err
+	}
+
+	return s.Store.Update(apiContext, schema, data, id)
+}
---
+++ pkg/controllers/management/agent/manager.go
@@ -0,0 +1,142 @@
+package agent
+
+import (
+	"context"
+	"encoding/base64"
+	"net/url"
+	"sync"
+
+	clusterController "github.com/rancher/rancher/pkg/controllers/user"
+	"github.com/rancher/types/apis/management.cattle.io/v3"
+	"github.com/rancher/types/config"
+	"github.com/sirupsen/logrus"
+	"k8s.io/client-go/rest"
+)
+
+type Manager struct {
+	ManagementConfig rest.Config
+	LocalConfig      *rest.Config
+	controllers      sync.Map
+}
+
+type record struct {
+	clusterRec *v3.Cluster
+	cluster    *config.UserContext
+	ctx        context.Context
+	cancel     context.CancelFunc
+}
+
+func NewManager(management *config.ManagementContext) *Manager {
+	return &Manager{
+		ManagementConfig: management.RESTConfig,
+		LocalConfig:      management.LocalConfig,
+	}
+}
+
+func (m *Manager) Stop(cluster *v3.Cluster) {
+	obj, ok := m.controllers.Load(cluster.UID)
+	if !ok {
+		return
+	}
+	logrus.Info("Stopping cluster agent for", obj.(*record).cluster.ClusterName)
+	obj.(*record).cancel()
+	m.controllers.Delete(cluster.UID)
+}
+
+func (m *Manager) Start(ctx context.Context, cluster *v3.Cluster) error {
+	obj, ok := m.controllers.Load(cluster.UID)
+	if ok {
+		if !m.changed(obj.(*record), cluster) {
+			return nil
+		}
+		m.Stop(obj.(*record).clusterRec)
+	}
+
+	controller, err := m.toRecord(ctx, cluster)
+	if controller == nil || err != nil {
+		return err
+	}
+
+	obj, loaded := m.controllers.LoadOrStore(cluster.UID, controller)
+	if !loaded {
+		go func() {
+			if err := m.doStart(obj.(*record)); err != nil {
+				m.Stop(cluster)
+			}
+		}()
+	}
+
+	return nil
+}
+
+func (m *Manager) changed(r *record, cluster *v3.Cluster) bool {
+	existing := r.clusterRec
+	if existing.Status.APIEndpoint != cluster.Status.APIEndpoint ||
+		existing.Status.ServiceAccountToken != cluster.Status.ServiceAccountToken ||
+		existing.Status.CACert != cluster.Status.CACert {
+		return true
+	}
+
+	return false
+}
+
+func (m *Manager) doStart(rec *record) error {
+	logrus.Info("Starting cluster agent for", rec.cluster.ClusterName)
+	if err := clusterController.Register(rec.ctx, rec.cluster); err != nil {
+		return err
+	}
+	return rec.cluster.Start(rec.ctx)
+}
+
+func (m *Manager) toRESTConfig(cluster *v3.Cluster) (*rest.Config, error) {
+	if cluster == nil {
+		return nil, nil
+	}
+
+	if cluster.Spec.Internal {
+		return m.LocalConfig, nil
+	}
+
+	if cluster.Status.APIEndpoint == "" || cluster.Status.CACert == "" || cluster.Status.ServiceAccountToken == "" {
+		return nil, nil
+	}
+
+	u, err := url.Parse(cluster.Status.APIEndpoint)
+	if err != nil {
+		return nil, err
+	}
+
+	caBytes, err := base64.StdEncoding.DecodeString(cluster.Status.CACert)
+	if err != nil {
+		return nil, err
+	}
+
+	return &rest.Config{
+		Host:        u.Host,
+		Prefix:      u.Path,
+		BearerToken: cluster.Status.ServiceAccountToken,
+		TLSClientConfig: rest.TLSClientConfig{
+			CAData: caBytes,
+		},
+	}, nil
+}
+
+func (m *Manager) toRecord(ctx context.Context, cluster *v3.Cluster) (*record, error) {
+	kubeConfig, err := m.toRESTConfig(cluster)
+	if kubeConfig == nil || err != nil {
+		return nil, err
+	}
+
+	clusterContext, err := config.NewUserContext(m.ManagementConfig, *kubeConfig, cluster.Name)
+	if err != nil {
+		return nil, err
+	}
+
+	s := &record{
+		cluster:    clusterContext,
+		clusterRec: cluster,
+	}
+	s.ctx, s.cancel = context.WithCancel(ctx)
+
+	return s, nil
+}
