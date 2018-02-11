2555a7af88e67554b1caa61070a34307d1fedcdf
Yuxing <yuxing@rancher.com>
2018-02-11 16:51:21 +0800

    Refactor the Windows bootstrap agent logic
    1. Move most of the common logic into rancher-tools.psm1
    2. Fix the add host issue https://github.com/rancher/rancher/issues/11303
    3. No more download binaries in bootstrap agent. Download them when running the startup.ps1 script.

--- agent-windows/tools/startup.ps1
+++ agent-windows/tools/startup.ps1
@@ -2,97 +2,50 @@ param(
     [Parameter(ValueFromPipeline=$true)]
     [string]$inputStr
 )
-
 $downloadFolder="download"
 $rancherBaseDir="rancher"
 $BasicLocation="c:\program files\$rancherBaseDir"
-$AgentBinary="agent"
-$PerHostSubnetBinary="per-host-subnet"
-$rancherCompomentList="$PerHostSubnetBinary","$AgentBinary"
+Import-Module -Name "c:\program files\$rancherBaseDir\rancher-tools.psm1" -Verbose
+
+$rancherCompomentList=Get-RancherCompomentList
 $strs=$inputStr.Split(",")
+$RegisterUrl=$strs[0].Trim("`"")
 $HostLabels=$strs[1].Trim("`"")
 $AgentIp=$strs[2].Trim("`"")
-if($HostLabels -ne $null){
+
+if(($HostLabels -ne $null) -and ($HostLabels -ne "")){
     [System.Environment]::SetEnvironmentVariable("CATTLE_HOST_LABELS",$HostLabels,"Machine")
 }
-if ($AgentIp -ne $null){
+if (($AgentIp -ne $null) -and ($AgentIp -ne "")){
     [System.Environment]::SetEnvironmentVariable("CATTLE_AGENT_IP",$AgentIp,"Machine")
 }
-function Test-Updates {
-    param(
-        [string]$target=""
-    )
-    $rtn=$true
-    for($i=0;$i -lt $rancherCompomentList.Count;$i++){ 
-        $listTar=$rancherCompomentList[$i]
-        if(("$target" -eq "$listTar") -or ("$target" -eq "")){
-            $downloadHash=(Get-FileHash -Algorithm MD5 -Path "$pwd/$downloadFolder/$rancherBaseDir/$listTar.exe").Hash
-            $downloadScriptHash=(Get-FileHash -Algorithm MD5 -Path "$pwd/$downloadFolder/$rancherBaseDir/startup_$listTar.ps1").Hash
-            $currentHash=(Get-FileHash -Algorithm MD5 -Path "$pwd/$listTar.exe").Hash
-            $currentScriptHash=(Get-FileHash -Algorithm MD5 -Path "$pwd/startup_$listTar.ps1").Hash
-            if(("$downloadHash" -ne "$currentHash") -or ("$downloadScriptHash" -ne "$currentScriptHash")){
-                $rtn = $rtn -and $true
-            } else {
-                $rtn = $false
-            }
-        }
-    }
-    return $rtn
-}
-
-function Update-Binaries  {
-    param(
-        [string]$target=""
-    )
-    for($i=0;$i -lt $rancherCompomentList.Count;$i++){ 
-        $listTar=$rancherCompomentList[$i]
-        if(("$target" -eq "$listTar") -or ("$target" -eq "")){
-            Copy-Item -Path "$pwd/$downloadFolder/$rancherBaseDir/$listTar.exe" -Destination "$pwd/$listTar.exe"
-            Copy-Item -Path "$pwd/$downloadFolder/$rancherBaseDir/startup_$listTar.ps1" -Destination "$pwd/startup_$listTar.ps1"
-        }
-    }
-}
-
-function Test-RancherComponent {
-    param(
-        [string]$target=""
-    )
-    $rtn=$true
-    for($i=0;$i -lt $rancherCompomentList.Count;$i++){ 
-        $listTar=$rancherCompomentList[$i]
-        if(("$target" -eq "$listTar") -or ("$target" -eq "")){
-            if($(Test-Path "$pwd/startup_$listTar.ps1") -and $(Test-Path "$pwd/$listTar.exe")){
-                $rtn = $rtn -and $true
-                
-            } else {
-                $rtn = $false
-            }
-        }
-    }
-    return $rtn
-}
-
-function Stop-RancherServices  {
-    Stop-Service rancher-agent
-    Stop-Service rancher-per-host-subnet
-    Write-Host "Stop Services Success"
-}
-
-function Start-RancherServices  {
-    Start-Service rancher-agent
-    sleep 5
-    Start-Service rancher-per-host-subnet
-    Write-Host "Start Services Success"
+if ($RegisterUrl -ne ""){
+    [System.Environment]::SetEnvironmentVariable("CATTLE_REGISTER_URL",$RegisterUrl,"Machine")
 }
 
 try {
     Set-Location -Path $BasicLocation
+    $key,$secret,$url,$this_ip=$(Register-Node $RegisterUrl)
+    if ($AgentIp -ne $null) { $this_ip=$AgentIp}
+    if (Test-Path "$downloadFolder"){Remove-Item -Path "$downloadFolder" -Confirm:$false -Recurse -Force }
+    New-Item -ItemType Directory -Path "$downloadFolder" > $null
+    
+    foreach($compoment in $rancherCompomentList){
+        $downloadList=Get-DownloadListByTarget $compoment
+        foreach($target in $downloadList.Keys){
+            $zipFile=$downloadList[$target]
+            Get-RancherBinary $key $secret $url $target $zipFile
+            Expand-Archive "$zipFile" -DestinationPath "./$downloadFolder"
+            Remove-Item -Path "$zipFile"
+        }
+    }
+
     for($i=0;$i -lt $rancherCompomentList.Count;$i++){
         $listTar=$rancherCompomentList[$i]
         Write-Host "checking $listTar update"
-        if(-not $(Test-RancherComponent "$listTar") -or $(Test-Updates "$listTar")){
+        if(-not $(Test-RancherComponent "$listTar")){
             Update-Binaries "$listTar"
-            Invoke-Expression "'$inputStr' | ./startup_$listTar.ps1"
+            Start-RancherCompoment $listTar $inputStr
             Write-Host "create or update $listTar success"
         } else{
             Write-Host "version of $listTar is the same, do nothing"
