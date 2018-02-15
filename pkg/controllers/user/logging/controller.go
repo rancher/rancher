3bea3c39924af42748eee9e6b8a8b0ee246d1304
Aiwantaozi <michelia.feng@gmail.com>
2018-02-15 15:23:15 +0800

    remove init namespace and configmap data, only create on demend

--- pkg/controllers/user/logging/controller.go
+++ pkg/controllers/user/logging/controller.go
@@ -1,26 +1,10 @@
 package logging
 
 import (
-	rv1 "github.com/rancher/types/apis/core/v1"
 	"github.com/rancher/types/config"
-	"github.com/sirupsen/logrus"
-
-	"github.com/rancher/rancher/pkg/controllers/user/logging/utils"
 )
 
 func Register(cluster *config.UserContext) {
 	registerClusterLogging(cluster)
 	registerProjectLogging(cluster)
-
-	if err := initData(cluster.Core.Namespaces(""), cluster.Core.ConfigMaps("")); err != nil {
-		logrus.Errorf("init logging data error, %v", err)
-	}
-}
-
-func initData(ns rv1.NamespaceInterface, cm rv1.ConfigMapInterface) error {
-	if err := utils.IniteNamespace(ns); err != nil {
-		return err
-	}
-
-	return utils.InitConfigMap(cm)
 }
