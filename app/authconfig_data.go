64771e9de599024d2913a53a78ecf8dfeaac7d39
Craig Jellick <craig@rancher.com>
2018-02-16 16:08:46 -0700

    organize app data better

---
+++ app/authconfig_data.go
@@ -0,0 +1,39 @@
+package app
+
+import (
+	"github.com/rancher/rancher/pkg/auth/providers/activedirectory"
+	"github.com/rancher/rancher/pkg/auth/providers/github"
+	localprovider "github.com/rancher/rancher/pkg/auth/providers/local"
+	"github.com/rancher/types/apis/management.cattle.io/v3"
+	"github.com/rancher/types/client/management/v3"
+	"github.com/rancher/types/config"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func addAuthConfigs(management *config.ManagementContext) error {
+	if err := addAuthConfig(github.Name, client.GithubConfigType, false, management); err != nil {
+		return err
+	}
+
+	if err := addAuthConfig(activedirectory.Name, client.ActiveDirectoryConfigType, false, management); err != nil {
+		return err
+	}
+
+	return addAuthConfig(localprovider.Name, client.LocalConfigType, true, management)
+}
+
+func addAuthConfig(name, aType string, enabled bool, management *config.ManagementContext) error {
+	_, err := management.Management.AuthConfigs("").ObjectClient().Create(&v3.AuthConfig{
+		ObjectMeta: v1.ObjectMeta{
+			Name: name,
+		},
+		Type:    aType,
+		Enabled: enabled,
+	})
+	if err != nil && !apierrors.IsAlreadyExists(err) {
+		return err
+	}
+
+	return nil
+}
