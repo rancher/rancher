aa05eb14cdd236794eaf872ea58d80b4f08c2268
Alena Prokharchyk <alena@rancher.com>
2018-03-02 11:43:15 -0800

    PublicEndpoints API: set nodeId to machine id

--- pkg/api/customization/workload/transform_store.go
+++ pkg/api/customization/workload/transform_store.go
@@ -3,9 +3,14 @@ package workload
 import (
 	"strings"
 
+	"github.com/rancher/norman/api/access"
 	"github.com/rancher/norman/store/transform"
 	"github.com/rancher/norman/types"
+	"github.com/rancher/norman/types/convert"
 	"github.com/rancher/norman/types/definition"
+	managementschema "github.com/rancher/types/apis/management.cattle.io/v3/schema"
+	managementv3 "github.com/rancher/types/client/management/v3"
+	"github.com/sirupsen/logrus"
 )
 
 func New(store types.Store) types.Store {
@@ -24,7 +29,33 @@ func New(store types.Store) types.Store {
 			if typeName != "" && id != "" {
 				data["id"] = strings.ToLower(typeName) + ":" + id
 			}
+			setNodeName(apiContext, data)
 			return data, nil
 		},
+	}
+}
+
+func setNodeName(apiContext *types.APIContext, data map[string]interface{}) {
+	if val, ok := data["publicEndpoints"]; ok {
+		var nodes []managementv3.Node
+		nodeNameToID := map[string]string{}
+		if err := access.List(apiContext, &managementschema.Version, managementv3.NodeType, &types.QueryOptions{}, &nodes); err == nil {
+			for _, node := range nodes {
+				nodeNameToID[node.NodeName] = node.ID
+			}
+		}
+		eps := convert.ToInterfaceSlice(val)
+		for _, ep := range eps {
+			epMap, err := convert.EncodeToMap(ep)
+			if err != nil {
+				logrus.Errorf("Failed to convert public endpoint: %v", err)
+				continue
+			}
+			epMap["serviceId"] = epMap["serviceName"]
+			if convert.IsEmpty(epMap["nodeName"]) {
+				continue
+			}
+			epMap["nodeId"] = nodeNameToID[convert.ToString(epMap["nodeName"])]
+		}
 	}
 }
