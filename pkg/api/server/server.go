973ca884d6915857d02e295a7c1e2a9947213cb8
Craig Jellick <craig@rancher.com>
2018-03-02 14:57:25 -0700

    Use proper access control manager
    This lets us user dynamic per-cluster access control clients
    when and where necessary

--- pkg/api/server/server.go
+++ pkg/api/server/server.go
@@ -11,36 +11,35 @@ import (
 	"github.com/rancher/rancher/pkg/api/controllers/settings"
 	"github.com/rancher/rancher/pkg/api/server/managementstored"
 	"github.com/rancher/rancher/pkg/api/server/userstored"
-	"github.com/rancher/rancher/pkg/rbac"
 	clusterSchema "github.com/rancher/types/apis/cluster.cattle.io/v3/schema"
 	managementSchema "github.com/rancher/types/apis/management.cattle.io/v3/schema"
 	projectSchema "github.com/rancher/types/apis/project.cattle.io/v3/schema"
 	"github.com/rancher/types/config"
 )
 
-func New(ctx context.Context, apiContext *config.ScaledContext) (http.Handler, error) {
-	subscribe.Register(&builtin.Version, apiContext.Schemas)
-	subscribe.Register(&managementSchema.Version, apiContext.Schemas)
-	subscribe.Register(&clusterSchema.Version, apiContext.Schemas)
-	subscribe.Register(&projectSchema.Version, apiContext.Schemas)
+func New(ctx context.Context, scaledContext *config.ScaledContext) (http.Handler, error) {
+	subscribe.Register(&builtin.Version, scaledContext.Schemas)
+	subscribe.Register(&managementSchema.Version, scaledContext.Schemas)
+	subscribe.Register(&clusterSchema.Version, scaledContext.Schemas)
+	subscribe.Register(&projectSchema.Version, scaledContext.Schemas)
 
-	if err := managementstored.Setup(ctx, apiContext); err != nil {
+	if err := managementstored.Setup(ctx, scaledContext); err != nil {
 		return nil, err
 	}
 
-	if err := userstored.Setup(ctx, apiContext); err != nil {
+	if err := userstored.Setup(ctx, scaledContext); err != nil {
 		return nil, err
 	}
 
 	server := normanapi.NewAPIServer()
-	server.AccessControl = rbac.NewAccessControl(apiContext.RBAC)
+	server.AccessControl = scaledContext.AccessControl
 
-	if err := server.AddSchemas(apiContext.Schemas); err != nil {
+	if err := server.AddSchemas(scaledContext.Schemas); err != nil {
 		return nil, err
 	}
 
-	dynamicschema.Register(apiContext, server.Schemas)
-	err := settings.Register(apiContext)
+	dynamicschema.Register(scaledContext, server.Schemas)
+	err := settings.Register(scaledContext)
 
 	return server, err
 }
