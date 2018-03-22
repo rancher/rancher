277641db271f6c492dc404c1378fe38bd3eb4342
Aiwantaozi <michelia.feng@gmail.com>
2018-03-22 16:18:50 +0800

    support rke log

--- pkg/controllers/user/logging/generator/cluster_template.go
+++ pkg/controllers/user/logging/generator/cluster_template.go
@@ -1,6 +1,28 @@
 package generator
 
 var ClusterTemplate = `{{ if ne .clusterTarget.CurrentTarget "none" }}
+
+<source>
+  @type  tail
+  path  /var/lib/rancher/rke/log/*.log
+  pos_file  /fluentd/etc/log/fluentd-rke-logging.pos
+  time_format  %Y-%m-%dT%H:%M:%S
+  tag  rke.*
+  format  json
+  read_from_head  true
+</source>
+
+<filter rke.**>
+  @type record_transformer
+  enable_ruby true  
+  <record>
+    tag ${tag}
+    driver rke
+    component ${tag_suffix[6].split("_")[0]}
+    container_id ${tag_suffix[6].split(".")[0]}
+  </record>
+</filter>
+
 <source>
    @type  tail
    path  /var/log/containers/*.log
@@ -101,6 +123,81 @@ var ClusterTemplate = `{{ if ne .clusterTarget.CurrentTarget "none" }}
     num_threads 8
     slow_flush_log_threshold 40.0
 </match>
+
+<match  rke.**> 
+    {{ if eq .clusterTarget.CurrentTarget "embedded"}}
+    @type elasticsearch
+    include_tag_key  true
+    hosts "elasticsearch.cattle-logging:9200"
+    reload_connections "true"
+    logstash_prefix rke
+    logstash_format true
+    logstash_dateformat  {{.clusterTarget.WrapEmbedded.DateFormat}}
+    type_name  "rke_log"
+    reload_connections false
+    {{end -}}
+
+    {{ if eq .clusterTarget.CurrentTarget "elasticsearch"}}
+    @type elasticsearch
+    include_tag_key  true
+    {{ if and .clusterTarget.ElasticsearchConfig.AuthUserName .clusterTarget.ElasticsearchConfig.AuthPassword}}
+    hosts {{.clusterTarget.WrapElasticsearch.Scheme}}://{{.clusterTarget.ElasticsearchConfig.AuthUserName}}:{{.clusterTarget.ElasticsearchConfig.AuthPassword}}@{{.clusterTarget.WrapElasticsearch.Host}}
+    {{else -}}
+    hosts {{.clusterTarget.ElasticsearchConfig.Endpoint}}    
+    {{end -}}
+
+    reload_connections "true"
+    logstash_prefix rke
+    logstash_format true
+    logstash_dateformat  {{.clusterTarget.WrapElasticsearch.DateFormat}}
+    type_name  "rke_log"
+    reload_connections false
+    {{end -}}
+
+    {{ if eq .clusterTarget.CurrentTarget "splunk"}}
+    @type splunk-http-eventcollector
+    server  {{.clusterTarget.WrapSplunk.Server}}
+    all_items true
+    protocol {{.clusterTarget.WrapSplunk.Scheme}}
+    sourcetype {{.clusterTarget.SplunkConfig.Source}}
+    token {{.clusterTarget.SplunkConfig.Token}}
+    format json
+    reload_connections "true"
+    {{end -}}
+
+    {{ if eq .clusterTarget.CurrentTarget "kafka"}}
+    @type kafka_buffered
+    {{ if .clusterTarget.KafkaConfig.ZookeeperEndpoint }}
+    zookeeper {{.clusterTarget.WrapKafka.Zookeeper}}
+    {{else}}
+    brokers {{.clusterTarget.WrapKafka.Brokers}}
+    {{end}}
+    default_topic {{.clusterTarget.KafkaConfig.Topic}}
+    output_data_type  "json"
+    output_include_tag true
+    output_include_time true
+    # get_kafka_client_log  true
+    max_send_retries  3
+    {{end -}}
+
+    {{ if eq .clusterTarget.CurrentTarget "syslog"}}
+    @type remote_syslog
+    host {{.clusterTarget.WrapSyslog.Host}}
+    port {{.clusterTarget.WrapSyslog.Port}}
+    severity {{.clusterTarget.SyslogConfig.Severity}}
+    program {{.clusterTarget.SyslogConfig.Program}}
+    {{end -}}
+
+    flush_interval 2s
+    buffer_type file
+    buffer_path /fluentd/etc/buffer/cluster.buffer
+    buffer_queue_limit 128
+    buffer_chunk_limit 256m
+    max_retry_wait 30
+    disable_retry_limit
+    num_threads 8
+    slow_flush_log_threshold 40.0
+    </match>
 
 {{end -}}
 `
