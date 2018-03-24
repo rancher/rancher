ebc0fdc4a9099d7a41c081f676604a69283830a4
Darren Shepherd <darren@rancher.com>
2018-03-23 17:55:05 -0700

    Update vendor

--- vendor/github.com/rancher/types/client/management/v3/zz_generated_target_workload.go
+++ vendor/github.com/rancher/types/client/management/v3/zz_generated_target_workload.go
@@ -4,13 +4,11 @@ const (
 	TargetWorkloadType                     = "targetWorkload"
 	TargetWorkloadFieldAvailablePercentage = "availablePercentage"
 	TargetWorkloadFieldSelector            = "selector"
-	TargetWorkloadFieldType                = "type"
 	TargetWorkloadFieldWorkloadID          = "workloadId"
 )
 
 type TargetWorkload struct {
 	AvailablePercentage *int64            `json:"availablePercentage,omitempty" yaml:"availablePercentage,omitempty"`
 	Selector            map[string]string `json:"selector,omitempty" yaml:"selector,omitempty"`
-	Type                string            `json:"type,omitempty" yaml:"type,omitempty"`
 	WorkloadID          string            `json:"workloadId,omitempty" yaml:"workloadId,omitempty"`
 }
