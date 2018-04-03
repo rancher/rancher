0e47055e488cf813d34602001ab9b07dde5a7363
Aiwantaozi <michelia.feng@gmail.com>
2018-04-03 11:19:15 +0800

    update vendor

--- vendor/github.com/rancher/types/client/management/v3/zz_generated_embedded_config.go
+++ vendor/github.com/rancher/types/client/management/v3/zz_generated_embedded_config.go
@@ -6,6 +6,10 @@ const (
 	EmbeddedConfigFieldElasticsearchEndpoint = "elasticsearchEndpoint"
 	EmbeddedConfigFieldIndexPrefix           = "indexPrefix"
 	EmbeddedConfigFieldKibanaEndpoint        = "kibanaEndpoint"
+	EmbeddedConfigFieldLimitsCPU             = "limitsCpu"
+	EmbeddedConfigFieldLimitsMemery          = "limitsMemory"
+	EmbeddedConfigFieldRequestsCPU           = "requestsCpu"
+	EmbeddedConfigFieldRequestsMemery        = "requestsMemory"
 )
 
 type EmbeddedConfig struct {
@@ -13,4 +17,8 @@ type EmbeddedConfig struct {
 	ElasticsearchEndpoint string `json:"elasticsearchEndpoint,omitempty" yaml:"elasticsearchEndpoint,omitempty"`
 	IndexPrefix           string `json:"indexPrefix,omitempty" yaml:"indexPrefix,omitempty"`
 	KibanaEndpoint        string `json:"kibanaEndpoint,omitempty" yaml:"kibanaEndpoint,omitempty"`
+	LimitsCPU             int64  `json:"limitsCpu,omitempty" yaml:"limitsCpu,omitempty"`
+	LimitsMemery          int64  `json:"limitsMemory,omitempty" yaml:"limitsMemory,omitempty"`
+	RequestsCPU           int64  `json:"requestsCpu,omitempty" yaml:"requestsCpu,omitempty"`
+	RequestsMemery        int64  `json:"requestsMemory,omitempty" yaml:"requestsMemory,omitempty"`
 }
