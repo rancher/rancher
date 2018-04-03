83a92ae511436414e9ba1a5cc43707992986c92e
Darren Shepherd <darren@rancher.com>
2018-04-02 20:51:42 -0700

    Fix yaml link for workloads

--- pkg/api/customization/yaml/formatter.go
+++ pkg/api/customization/yaml/formatter.go
@@ -6,9 +6,9 @@ import (
 
 func NewFormatter(next types.Formatter) types.Formatter {
 	return func(request *types.APIContext, resource *types.RawResource) {
-		resource.Links["yaml"] = request.URLBuilder.Link("yaml", resource)
 		if next != nil {
 			next(request, resource)
 		}
+		resource.Links["yaml"] = request.URLBuilder.Link("yaml", resource)
 	}
 }
