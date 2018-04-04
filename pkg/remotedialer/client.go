c1e412b722d70834895d781fc59d397d8b8b671d
Darren Shepherd <darren@rancher.com>
2018-04-04 03:13:03 -0700

    Check plan every 2 minutes in agent

--- pkg/remotedialer/client.go
+++ pkg/remotedialer/client.go
@@ -1,6 +1,7 @@
 package remotedialer
 
 import (
+	"context"
 	"net/http"
 	"time"
 
@@ -10,14 +11,14 @@ import (
 
 type ConnectAuthorizer func(proto, address string) bool
 
-func ClientConnect(wsURL string, headers http.Header, dialer *websocket.Dialer, auth ConnectAuthorizer, onConnect func() error) {
+func ClientConnect(wsURL string, headers http.Header, dialer *websocket.Dialer, auth ConnectAuthorizer, onConnect func(context.Context) error) {
 	if err := connectToProxy(wsURL, headers, auth, dialer, onConnect); err != nil {
 		logrus.WithError(err).Error("Failed to connect to proxy")
 		time.Sleep(time.Duration(5) * time.Second)
 	}
 }
 
-func connectToProxy(proxyURL string, headers http.Header, auth ConnectAuthorizer, dialer *websocket.Dialer, onConnect func() error) error {
+func connectToProxy(proxyURL string, headers http.Header, auth ConnectAuthorizer, dialer *websocket.Dialer, onConnect func(context.Context) error) error {
 	logrus.WithField("url", proxyURL).Info("Connecting to proxy")
 
 	if dialer == nil {
@@ -29,7 +30,10 @@ func connectToProxy(proxyURL string, headers http.Header, auth ConnectAuthorizer
 		return err
 	}
 
-	if err := onConnect(); err != nil {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	if err := onConnect(ctx); err != nil {
 		return err
 	}
 
