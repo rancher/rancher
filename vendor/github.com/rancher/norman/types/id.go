f7956bdb4dbe3f20925a0feb13efbd4501c31e0e
Darren Shepherd <darren@rancher.com>
2018-04-16 22:17:49 -0700

    Update vendor

--- vendor/github.com/rancher/norman/types/id.go
+++ vendor/github.com/rancher/norman/types/id.go
@@ -15,5 +15,5 @@ var (
 func GenerateName(typeName string) string {
 	base := typeName[0:1] + lowerChars.ReplaceAllString(typeName[1:], "")
 	last := utilrand.String(5)
-	return fmt.Sprintf("1%s-%s", strings.ToLower(base), last)
+	return fmt.Sprintf("%s-%s", strings.ToLower(base), last)
 }
