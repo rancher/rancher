697f819f6f4fd7781dfc7c075ecbb3ecdd7da9c7
Alena Prokharchyk <alena@rancher.com>
2018-04-23 20:43:07 -0700

    Ingress state annotation name change

--- pkg/controllers/user/ingress/ingress_common.go
+++ pkg/controllers/user/ingress/ingress_common.go
@@ -9,6 +9,10 @@ import (
 	"k8s.io/api/extensions/v1beta1"
 )
 
+const (
+	ingressStateAnnotation = "field.cattle.io.ingress/state"
+)
+
 func GetStateKey(host string, path string, port string) string {
 	key := fmt.Sprintf("%s/%s/%s", host, path, port)
 	return base64.URLEncoding.EncodeToString([]byte(key))
@@ -19,7 +23,7 @@ func GetIngressState(obj *v1beta1.Ingress) map[string]string {
 	if annotations == nil {
 		return nil
 	}
-	if v, ok := annotations["ingress.cattle.io/state"]; ok {
+	if v, ok := annotations[ingressStateAnnotation]; ok {
 		state := make(map[string]string)
 		json.Unmarshal([]byte(convert.ToString(v)), &state)
 		return state
