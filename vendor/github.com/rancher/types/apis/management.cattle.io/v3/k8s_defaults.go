910e530fa4841c47aede19997ea1e0ab651fdf7a
galal-hussein <hussein.galal.ahmed.11@gmail.com>
2018-05-02 19:12:50 +0200

    Update types

--- vendor/github.com/rancher/types/apis/management.cattle.io/v3/k8s_defaults.go
+++ vendor/github.com/rancher/types/apis/management.cattle.io/v3/k8s_defaults.go
@@ -8,7 +8,7 @@ const (
 
 var (
 	m          = image.Mirror
-	ToolsImage = m("rancher/rke-tools:v0.1.4")
+	ToolsImage = m("rancher/rke-tools:v0.1.5")
 
 	// K8sVersionToRKESystemImages - images map for 2.0
 	K8sVersionToRKESystemImages = map[string]RKESystemImages{
