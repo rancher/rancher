c7d24e288b3d8da32045c552f5fadf46ea379eb8
Nathan Jenan <nathanj@rancher.com>
2018-05-11 09:33:06 -0700

    Preventing disabled roles from being assigned
    This change prevents any roles from being assigned via a CRTB or PRTB
    when their parent roletemplate has the disabled field set to true.
    
    Issue:
    https://github.com/rancher/rancher/issues/13418

---
+++ pkg/api/customization/roletemplatebinding/validator.go
@@ -0,0 +1,54 @@
+package roletemplatebinding
+
+import (
+	"fmt"
+	"github.com/rancher/norman/httperror"
+	"github.com/rancher/norman/types"
+	"github.com/rancher/types/apis/management.cattle.io/v3"
+	"github.com/rancher/types/client/management/v3"
+	"github.com/rancher/types/config"
+)
+
+func NewPRTBValidator(management *config.ScaledContext) types.Validator {
+	return newValidator(management, client.ProjectRoleTemplateBindingFieldRoleTemplateId)
+}
+
+func NewCRTBValidator(management *config.ScaledContext) types.Validator {
+	return newValidator(management, client.ClusterRoleTemplateBindingFieldRoleTemplateId)
+}
+
+func newValidator(management *config.ScaledContext, field string) types.Validator {
+	validator := &Validator{
+		roleTemplateLister: management.Management.RoleTemplates("").Controller().Lister(),
+		field:              field,
+	}
+
+	return validator.Validator
+}
+
+type Validator struct {
+	roleTemplateLister v3.RoleTemplateLister
+	field              string
+}
+
+func (v *Validator) Validator(request *types.APIContext, schema *types.Schema, data map[string]interface{}) error {
+	return v.ValidateRoleTemplateBinding(data[v.field])
+}
+
+func (v *Validator) ValidateRoleTemplateBinding(obj interface{}) error {
+	roleTemplateID, ok := obj.(string)
+	if !ok {
+		return httperror.NewAPIError(httperror.MissingRequired, "Request does not have a valid roleTemplateId")
+	}
+
+	roleTemplate, err := v.roleTemplateLister.Get("", roleTemplateID)
+	if err != nil {
+		return httperror.NewAPIError(httperror.ServerError, fmt.Sprintf("Error getting role template: %v", err))
+	}
+
+	if roleTemplate.Enabled != nil && !*roleTemplate.Enabled {
+		return httperror.NewAPIError(httperror.InvalidState, "Role is disabled and cannot be assigned")
+	}
+
+	return nil
+}
