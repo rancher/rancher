81fac93c261b5f4c6bf223434dbd82a7e7eb64e8
kinarashah <kinarahs@usc.edu>
2018-05-22 12:27:38 -0700

    convert ingress rules correctly
    for ingress rules with same host, path and port, only one rule should be created, others will be deleted
    
    tests

--- tests/core/test_ingress.py
+++ tests/core/test_ingress.py
@@ -31,30 +31,96 @@ def test_ingress(pc):
 
     ns = pc.cluster.client.create_namespace(name=random_str(),
                                             projectId=pc.project.id)
-    # wl = client.create_workload(namespaceId=ns.id,
-    #                             scale=1,
-    #                             containers=[{
-    #                                 'name': 'one',
-    #                                 'image': 'nginx',
-    #                             }])
 
-    # name = random_str()
-    # client.create_ingress(name=name,
-    #                       namespaceId=ns.id,
-    #                       rules=[
-    #                           {
-    #                               'paths': {
-    #                                   '/': {
-    #                                       'targetPort': 80,
-    #                                       'workloadIds': [wl.id],
-    #                                   }
-    #                               }
-    #                           },
-    #                       ])
+    name = random_str()
+    workload = client.create_workload(
+                                    name=name,
+                                    namespaceId=ns.id,
+                                    scale=1,
+                                    containers=[{
+                                        'name': 'one',
+                                        'image': 'nginx',
+                                    }])
 
-    # assert ingress.rules[0]['paths']['/'] == {
-    #     'targetPort': 80,
-    #     'workloadIds': [wl.id]
-    # }
+    name = random_str()
+    ingress = client.create_ingress(name=name,
+                                    namespaceId=ns.id,
+                                    rules=[{
+                                          'host': "foo.com",
+                                          'paths': {
+                                              '/': {
+                                                  'targetPort': 80,
+                                                  'workloadIds':
+                                                  [workload.id],
+                                              }
+                                          }},
+                                      ])
+
+    assert len(ingress.rules) == 1
+    assert ingress.rules[0]['host'] == "foo.com"
+    path = ingress.rules[0]['paths']['/']
+    assert path['targetPort'] == 80
+    assert path['workloadIds'] == [workload.id]
+    assert path['serviceId'] is None
+
+    client.delete(ns)
+
+
+def test_ingress_rules_same_hostPortPath(pc):
+    client = pc.client
+
+    ns = pc.cluster.client.create_namespace(name=random_str(),
+                                            projectId=pc.project.id)
+
+    name = random_str()
+    workload1 = client.create_workload(
+                                    name=name,
+                                    namespaceId=ns.id,
+                                    scale=1,
+                                    containers=[{
+                                        'name': 'one',
+                                        'image': 'nginx',
+                                    }])
+
+    name = random_str()
+    workload2 = client.create_workload(
+                                    name=name,
+                                    namespaceId=ns.id,
+                                    scale=1,
+                                    containers=[{
+                                        'name': 'one',
+                                        'image': 'nginx',
+                                    }])
+
+    name = random_str()
+    ingress = client.create_ingress(name=name,
+                                    namespaceId=ns.id,
+                                    rules=[{
+                                          'host': "foo.com",
+                                          'paths': {
+                                              '/': {
+                                                  'targetPort': 80,
+                                                  'workloadIds':
+                                                  [workload1.id],
+                                              }
+                                          }},
+                                          {
+                                          'host': "foo.com",
+                                          'paths': {
+                                              '/': {
+                                                  'targetPort': 80,
+                                                  'workloadIds':
+                                                  [workload2.id],
+                                              }
+                                          }},
+                                      ])
+
+    assert len(ingress.rules) == 1
+    assert ingress.rules[0]['host'] == "foo.com"
+    path = ingress.rules[0]['paths']['/']
+    assert path['targetPort'] == 80
+    assert len(path['workloadIds']) == 2
+    assert set(path['workloadIds']) == set([workload1.id, workload2.id])
+    assert path['serviceId'] is None
 
     client.delete(ns)
