efddc83d06d9f2dcbf31012fb50d4135074173eb
Nathan Jenan <nathanj@rancher.com>
2018-06-19 16:41:39 -0700

    Update vendor

--- vendor/github.com/rancher/types/client/management/v3/zz_generated_amazon_elastic_container_service_config.go
+++ vendor/github.com/rancher/types/client/management/v3/zz_generated_amazon_elastic_container_service_config.go
@@ -1,12 +1,20 @@
 package client
 
 const (
-	AmazonElasticContainerServiceConfigType           = "amazonElasticContainerServiceConfig"
-	AmazonElasticContainerServiceConfigFieldAccessKey = "accessKey"
-	AmazonElasticContainerServiceConfigFieldSecretKey = "secretKey"
+	AmazonElasticContainerServiceConfigType              = "amazonElasticContainerServiceConfig"
+	AmazonElasticContainerServiceConfigFieldAccessKey    = "accessKey"
+	AmazonElasticContainerServiceConfigFieldInstanceType = "instanceType"
+	AmazonElasticContainerServiceConfigFieldMaximumNodes = "maximumNodes"
+	AmazonElasticContainerServiceConfigFieldMinimumNodes = "minimumNodes"
+	AmazonElasticContainerServiceConfigFieldRegion       = "region"
+	AmazonElasticContainerServiceConfigFieldSecretKey    = "secretKey"
 )
 
 type AmazonElasticContainerServiceConfig struct {
-	AccessKey string `json:"accessKey,omitempty" yaml:"accessKey,omitempty"`
-	SecretKey string `json:"secretKey,omitempty" yaml:"secretKey,omitempty"`
+	AccessKey    string `json:"accessKey,omitempty" yaml:"accessKey,omitempty"`
+	InstanceType string `json:"instanceType,omitempty" yaml:"instanceType,omitempty"`
+	MaximumNodes int64  `json:"maximumNodes,omitempty" yaml:"maximumNodes,omitempty"`
+	MinimumNodes int64  `json:"minimumNodes,omitempty" yaml:"minimumNodes,omitempty"`
+	Region       string `json:"region,omitempty" yaml:"region,omitempty"`
+	SecretKey    string `json:"secretKey,omitempty" yaml:"secretKey,omitempty"`
 }
