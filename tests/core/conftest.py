e41536c107618bedff4c277e0f0ce3cd46de4cbe
Craig Jellick <craig@rancher.com>
2018-07-12 09:57:43 -0700

    Test improvements
    Introduce the kubernetes python client to allow tests to query
    kubernetes directly
    
    Add some more helper functions to conftest
    
    Introduce a project-owner rbac test

--- tests/core/conftest.py
+++ tests/core/conftest.py
@@ -4,6 +4,10 @@ import requests
 import time
 import urllib3
 from .common import random_str
+from kubernetes.client import ApiClient, Configuration
+from kubernetes.config.kube_config import KubeConfigLoader
+from rancher import ApiError
+import yaml
 
 # This stops ssl warnings for unsecure certs
 urllib3.disable_warnings()
@@ -11,14 +15,17 @@ urllib3.disable_warnings()
 BASE_URL = 'https://localhost:8443/v3'
 AUTH_URL = BASE_URL + '-public/localproviders/local?action=login'
 CHNG_PWD_URL = BASE_URL + '/users/admin?action=changepassword'
+DEFAULT_TIMEOUT = 45
 
 
 class ManagementContext:
     """Contains a client that is scoped to the managment plane APIs. That is,
     APIs that are not specific to a cluster or project."""
 
-    def __init__(self, client):
+    def __init__(self, client, k8s_client=None, user=None):
         self.client = client
+        self.k8s_client = k8s_client
+        self.user = user
 
 
 class ClusterContext:
@@ -45,7 +52,7 @@ class ProjectContext:
         self.client = client
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def admin_mc():
     """Returns a ManagementContext for the default global admin user."""
     requests.post(CHNG_PWD_URL, json={
@@ -58,19 +65,25 @@ def admin_mc():
     }, verify=False)
     client = rancher.Client(url=BASE_URL, token=r.json()['token'],
                             verify=False)
-    return ManagementContext(client)
+    k8s_client = kubernetes_api_client(client, 'local')
+    return ManagementContext(client, k8s_client)
 
 
 @pytest.fixture
 def admin_cc(admin_mc):
     """Returns a ClusterContext for the local cluster for the default global
     admin user."""
-    cluster = admin_mc.client.by_id_cluster('local')
+    cluster, client = cluster_and_client('local', admin_mc.client)
+    return ClusterContext(admin_mc, cluster, client)
+
+
+def cluster_and_client(cluster_id, mgmt_client):
+    cluster = mgmt_client.by_id_cluster(cluster_id)
     url = cluster.links.self + '/schemas'
     client = rancher.Client(url=url,
                             verify=False,
-                            token=admin_mc.client.token)
-    return ClusterContext(admin_mc, cluster, client)
+                            token=mgmt_client.token)
+    return cluster, client
 
 
 @pytest.fixture
@@ -107,7 +120,66 @@ def user_mc(admin_mc):
     }, verify=False)
     client = rancher.Client(url=BASE_URL, token=response.json()['token'],
                             verify=False)
-    return ManagementContext(client)
+    return ManagementContext(client, user=user)
+
+
+@pytest.fixture
+def admin_cc_client(admin_cc):
+    """Returns the client from the default admin's ClusterContext"""
+    return admin_cc.client
+
+
+@pytest.fixture
+def admin_pc_client(admin_pc):
+    """Returns the client from the default admin's ProjectContext """
+    return admin_pc.client
+
+
+def wait_for(callback, timeout=DEFAULT_TIMEOUT, fail_handler=None):
+    sleep_time = _sleep_time()
+    start = time.time()
+    ret = callback()
+    while ret is None or ret is False:
+        time.sleep(next(sleep_time))
+        if time.time() - start > timeout:
+            exception_msg = 'Timeout waiting for condition.'
+            if fail_handler:
+                exception_msg = exception_msg + ' Fail handler message: ' + \
+                                fail_handler()
+            raise Exception(exception_msg)
+        ret = callback()
+    return ret
+
+
+def _sleep_time():
+    sleep = 0.01
+    while True:
+        yield sleep
+        sleep *= 2
+        if sleep > 1:
+            sleep = 1
+
+
+def wait_until_available(client, obj, timeout=DEFAULT_TIMEOUT):
+    start = time.time()
+    sleep = 0.01
+    while True:
+        time.sleep(sleep)
+        sleep *= 2
+        if sleep > 2:
+            sleep = 2
+        try:
+            obj = client.reload(obj)
+        except ApiError as e:
+            if e.error.status != 403:
+                raise e
+        else:
+            return obj
+        delta = time.time() - start
+        if delta > timeout:
+            msg = 'Timeout waiting for [{}:{}] for condition after {}' \
+                  ' seconds'.format(obj.type, obj.id, delta)
+            raise Exception(msg)
 
 
 def wait_for_condition(condition_type, status, client, obj):
@@ -141,13 +213,11 @@ def find_condition(condition_type, status, obj):
     return False
 
 
-@pytest.fixture
-def admin_cc_client(admin_cc):
-    """Returns the client from the default admin's ClusterContext"""
-    return admin_cc.client
-
-
-@pytest.fixture
-def admin_pc_client(admin_pc):
-    """Returns the client from the default admin's ProjectContext """
-    return admin_pc.client
+def kubernetes_api_client(rancher_client, cluster_name):
+    c = rancher_client.by_id_cluster(cluster_name)
+    kc = c.generateKubeconfig()
+    loader = KubeConfigLoader(config_dict=yaml.load(kc.config))
+    client_configuration = type.__call__(Configuration)
+    loader.load_and_set(client_configuration)
+    k8s_client = ApiClient(configuration=client_configuration)
+    return k8s_client
--- tests/core/test_rbac.py
+++ tests/core/test_rbac.py
@@ -1,3 +1,9 @@
+import kubernetes
+from .common import random_str
+from .conftest import wait_until_available,\
+    cluster_and_client, kubernetes_api_client, wait_for
+
+
 def test_multi_user(admin_mc, user_mc):
     """Tests a bug in the python client where multiple clients would not
     work properly. All clients would get the auth header of the last  client"""
@@ -9,3 +15,100 @@ def test_multi_user(admin_mc, user_mc):
     # more correct. Instead, list gets filtered to zero
     ac = user_mc.client.list_auth_config()
     assert len(ac) == 0
+
+
+def test_project_owner(admin_cc, admin_mc, user_mc, request):
+    """Tests that a non-admin member can create a project, create and
+    add a namespace to it, and can do workload related things in the namespace.
+
+    This is the first test written incorporating a non-admin user and the
+    kubernetes python client. It does a lot partially as an experiment and
+    partially as an example for other yet-to-be-written tests
+    """
+    admin_client = admin_mc.client
+    admin_client.create_cluster_role_template_binding(
+        userId=user_mc.user.id,
+        roleTemplateId="cluster-member",
+        clusterId=admin_cc.cluster.id,
+    )
+
+    user_client = user_mc.client
+
+    # When this returns, the user can successfully access the cluster and thus
+    # can create a project in it. We generally need this wait_until_available
+    # call when we are creating cluster, project, and namespaces as non-admins
+    # because until the rbac controllers have had a chance to run and the
+    # creator is bootstrapped into the resource, they will not be able to
+    # access it
+    wait_until_available(user_client, admin_cc.cluster)
+
+    p = user_client.create_project(name='test-' + random_str(),
+                                   clusterId=admin_cc.cluster.id)
+
+    # In case something goes badly as the user, add a finalizer to
+    # delete the project as the admin
+    request.addfinalizer(lambda: admin_client.delete(p))
+
+    # When this returns, the user can successfully access the project and thus
+    # can create a namespace in it
+    p = wait_until_available(user_client, p)
+    p = user_client.wait_success(p)
+    assert p.state == 'active'
+
+    k8s_client = kubernetes_api_client(user_client, 'local')
+    auth = kubernetes.client.AuthorizationV1Api(k8s_client)
+
+    # Rancher API doesn't have a surefire way of knowing exactly when the user
+    # has the ability to create namespaces yet. So we have to rely on an actual
+    # kubernetes auth check.
+    def can_create_ns():
+        access_review = kubernetes.client.V1SelfSubjectAccessReview(spec={
+            "resourceAttributes": {
+                'verb': 'create',
+                'resource': 'namespaces',
+                'group': '',
+            },
+        })
+        response = auth.create_self_subject_access_review(access_review)
+        return response.status.allowed
+
+    wait_for(can_create_ns)
+
+    cluster, c_client = cluster_and_client('local', user_mc.client)
+    ns = c_client.create_namespace(name='test-' + random_str(),
+                                   projectId=p.id)
+    ns = wait_until_available(c_client, ns)
+    ns = c_client.wait_success(ns)
+    assert ns.state == 'active'
+
+    # Simple proof that user can get pods in the created namespace.
+    # We just care that the list call does not error out
+    core = kubernetes.client.CoreV1Api(api_client=k8s_client)
+    core.list_namespaced_pod(ns.name)
+
+    # As the user, assert that the two expected role bindings exist in the
+    # namespace for the user. There should be one for the rancher role
+    # 'project-owner' and one for the k8s built-in role 'admin'
+    rbac = kubernetes.client.RbacAuthorizationV1Api(api_client=k8s_client)
+    rbs = rbac.list_namespaced_role_binding(ns.name)
+    rb_dict = {}
+    assert len(rbs.items) == 2
+    for rb in rbs.items:
+        if rb.subjects[0].name == user_mc.user.id:
+            rb_dict[rb.role_ref.name] = rb
+    assert 'project-owner' in rb_dict
+    assert 'admin' in rb_dict
+
+    # As an additional measure of proof and partially just as an exercise in
+    # using this particular k8s api, check that the user can create
+    # deployments using the subject access review api
+    access_review = kubernetes.client.V1LocalSubjectAccessReview(spec={
+        "resourceAttributes": {
+            'namespace': ns.name,
+            'verb': 'create',
+            'resource': 'deployments',
+            'group': 'extensions',
+        },
+    })
+    response = auth.create_self_subject_access_review(access_review)
+    assert response.status.allowed is True
