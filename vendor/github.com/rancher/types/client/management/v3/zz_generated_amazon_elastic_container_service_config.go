0a8861a86ef85a0d4501b49ff69fe3b663f47401
Alena Prokharchyk <alena@rancher.com>
2018-07-16 16:22:00 -0700

    Vendor update

--- vendor/github.com/rancher/types/client/management/v3/zz_generated_amazon_elastic_container_service_config.go
+++ vendor/github.com/rancher/types/client/management/v3/zz_generated_amazon_elastic_container_service_config.go
@@ -1,20 +1,28 @@
 package client
 
 const (
-	AmazonElasticContainerServiceConfigType              = "amazonElasticContainerServiceConfig"
-	AmazonElasticContainerServiceConfigFieldAccessKey    = "accessKey"
-	AmazonElasticContainerServiceConfigFieldInstanceType = "instanceType"
-	AmazonElasticContainerServiceConfigFieldMaximumNodes = "maximumNodes"
-	AmazonElasticContainerServiceConfigFieldMinimumNodes = "minimumNodes"
-	AmazonElasticContainerServiceConfigFieldRegion       = "region"
-	AmazonElasticContainerServiceConfigFieldSecretKey    = "secretKey"
+	AmazonElasticContainerServiceConfigType                = "amazonElasticContainerServiceConfig"
+	AmazonElasticContainerServiceConfigFieldAccessKey      = "accessKey"
+	AmazonElasticContainerServiceConfigFieldInstanceType   = "instanceType"
+	AmazonElasticContainerServiceConfigFieldMaximumNodes   = "maximumNodes"
+	AmazonElasticContainerServiceConfigFieldMinimumNodes   = "minimumNodes"
+	AmazonElasticContainerServiceConfigFieldRegion         = "region"
+	AmazonElasticContainerServiceConfigFieldSecretKey      = "secretKey"
+	AmazonElasticContainerServiceConfigFieldSecurityGroups = "securityGroups"
+	AmazonElasticContainerServiceConfigFieldServiceRole    = "serviceRole"
+	AmazonElasticContainerServiceConfigFieldSubnets        = "subnets"
+	AmazonElasticContainerServiceConfigFieldVirtualNetwork = "virtualNetwork"
 )
 
 type AmazonElasticContainerServiceConfig struct {
-	AccessKey    string `json:"accessKey,omitempty" yaml:"accessKey,omitempty"`
-	InstanceType string `json:"instanceType,omitempty" yaml:"instanceType,omitempty"`
-	MaximumNodes int64  `json:"maximumNodes,omitempty" yaml:"maximumNodes,omitempty"`
-	MinimumNodes int64  `json:"minimumNodes,omitempty" yaml:"minimumNodes,omitempty"`
-	Region       string `json:"region,omitempty" yaml:"region,omitempty"`
-	SecretKey    string `json:"secretKey,omitempty" yaml:"secretKey,omitempty"`
+	AccessKey      string   `json:"accessKey,omitempty" yaml:"accessKey,omitempty"`
+	InstanceType   string   `json:"instanceType,omitempty" yaml:"instanceType,omitempty"`
+	MaximumNodes   int64    `json:"maximumNodes,omitempty" yaml:"maximumNodes,omitempty"`
+	MinimumNodes   int64    `json:"minimumNodes,omitempty" yaml:"minimumNodes,omitempty"`
+	Region         string   `json:"region,omitempty" yaml:"region,omitempty"`
+	SecretKey      string   `json:"secretKey,omitempty" yaml:"secretKey,omitempty"`
+	SecurityGroups []string `json:"securityGroups,omitempty" yaml:"securityGroups,omitempty"`
+	ServiceRole    string   `json:"serviceRole,omitempty" yaml:"serviceRole,omitempty"`
+	Subnets        []string `json:"subnets,omitempty" yaml:"subnets,omitempty"`
+	VirtualNetwork string   `json:"virtualNetwork,omitempty" yaml:"virtualNetwork,omitempty"`
 }
