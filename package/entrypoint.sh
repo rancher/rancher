3f151b91e88176e2ac8160a0425ab592de561d1b
Bill Maxwell <bill@rancher.com>
2018-07-17 10:29:08 -0700

    pass cmd args to entrypoint

--- package/entrypoint.sh
+++ package/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-exec tini -- rancher --http-listen-port=80 --https-listen-port=443 --audit-log-path=${AUDIT_LOG_PATH} --audit-level=${AUDIT_LEVEL} --audit-log-maxage=${AUDIT_LOG_MAXAGE} --audit-log-maxbackup=${AUDIT_LOG_MAXBACKUP} --audit-log-maxsize=${AUDIT_LOG_MAXSIZE}
+exec tini -- rancher --http-listen-port=80 --https-listen-port=443 --audit-log-path=${AUDIT_LOG_PATH} --audit-level=${AUDIT_LEVEL} --audit-log-maxage=${AUDIT_LOG_MAXAGE} --audit-log-maxbackup=${AUDIT_LOG_MAXBACKUP} --audit-log-maxsize=${AUDIT_LOG_MAXSIZE} "${@}"
