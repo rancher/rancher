e4d2426f0fa588657a824d451452a330d9fe76b1
kinarashah <kinara@rancher.com>
2018-08-10 02:22:28 -0700

    Fix moving of netpolicies on move ns
    -Network policies should be synced when ns is moved to none
    -No policy should be programmed when projectID is ""

--- pkg/controllers/user/networkpolicy/nssyncer.go
+++ pkg/controllers/user/networkpolicy/nssyncer.go
@@ -37,37 +37,52 @@ func (nss *nsSyncer) Sync(key string, ns *corev1.Namespace) error {
 
 	logrus.Debugf("nsSyncer: Sync: %v, %+v", ns.Name, *ns)
 
-	projectID, ok := ns.Labels[nslabels.ProjectIDFieldLabel]
-	if ok {
+	projectID := ns.Labels[nslabels.ProjectIDFieldLabel]
+	movedToNone := projectID == ""
+	if !movedToNone {
 		logrus.Debugf("nsSyncer: Sync: ns=%v projectID=%v", ns.Name, projectID)
 		// program project isolation network policy
 		if err := nss.npmgr.programNetworkPolicy(projectID, nss.clusterNamespace); err != nil {
 			return fmt.Errorf("nsSyncer: Sync: error programming network policy: %v (ns=%v, projectID=%v), ", err, ns.Name, projectID)
 		}
-		// handle moving of namespace between projects
-		systemNamespaces, _, err := nss.npmgr.getSystemNSInfo(nss.clusterNamespace)
-		if err != nil {
-			return fmt.Errorf("nsSyncer: error getting systemNamespaces %v", err)
-		}
-		if err = nss.syncNodePortServices(systemNamespaces, ns.Name); err != nil {
-			return fmt.Errorf("nsSyncer: error syncing services %v", err)
-		}
-		if err = nss.syncHostPortPods(systemNamespaces, ns.Name); err != nil {
-			return fmt.Errorf("nsSyncer: error syncing pods %v", err)
-		}
+	}
+	// handle moving of namespace between projects
+	if err := nss.syncOnMove(ns.Name, projectID, movedToNone); err != nil {
+		return fmt.Errorf("nsSyncer: Sync: error moving network policy: %v (ns=%v, projectID=%v), ", err, ns.Name, projectID)
+	}
+	if movedToNone {
+		return nil
 	}
 
 	// handle if hostNetwork policy is needed
 	return nss.npmgr.handleHostNetwork(nss.clusterNamespace)
 }
 
-func (nss *nsSyncer) syncNodePortServices(systemNamespaces map[string]bool, nsName string) error {
+func (nss *nsSyncer) syncOnMove(nsName string, projectID string, movedToNone bool) error {
+	systemNamespaces, _, err := nss.npmgr.getSystemNSInfo(nss.clusterNamespace)
+	if err != nil {
+		return fmt.Errorf("nsSyncer: error getting systemNamespaces %v", err)
+	}
+	if movedToNone {
+		nss.npmgr.delete(nsName, "np-default")
+		nss.npmgr.delete(nsName, "hn-nodes")
+	}
+	if err = nss.syncNodePortServices(systemNamespaces, nsName, movedToNone); err != nil {
+		return fmt.Errorf("nsSyncer: error syncing services %v", err)
+	}
+	if err = nss.syncHostPortPods(systemNamespaces, nsName, movedToNone); err != nil {
+		return fmt.Errorf("nsSyncer: error syncing pods %v", err)
+	}
+	return nil
+}
+
+func (nss *nsSyncer) syncNodePortServices(systemNamespaces map[string]bool, nsName string, moved bool) error {
 	svcs, err := nss.serviceLister.List(nsName, labels.NewSelector())
 	if err != nil {
 		return err
 	}
 	for _, svc := range svcs {
-		if systemNamespaces[svc.Namespace] {
+		if systemNamespaces[svc.Namespace] || moved {
 			policyName := getNodePortsPolicyName(svc)
 			nss.npmgr.delete(svc.Namespace, policyName)
 			continue
@@ -79,13 +94,13 @@ func (nss *nsSyncer) syncNodePortServices(systemNamespaces map[string]bool, nsNa
 	return nil
 }
 
-func (nss *nsSyncer) syncHostPortPods(systemNamespaces map[string]bool, nsName string) error {
+func (nss *nsSyncer) syncHostPortPods(systemNamespaces map[string]bool, nsName string, moved bool) error {
 	pods, err := nss.podLister.List(nsName, labels.NewSelector())
 	if err != nil {
 		return err
 	}
 	for _, pod := range pods {
-		if systemNamespaces[pod.Namespace] {
+		if systemNamespaces[pod.Namespace] || moved {
 			policyName := getHostPortsPolicyName(pod)
 			nss.npmgr.delete(pod.Namespace, policyName)
 			continue
