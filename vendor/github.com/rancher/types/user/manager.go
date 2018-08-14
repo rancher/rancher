08c4941aaf5a86f45873276204261dd443b5761f
Dan Ramich <danold215@gmail.com>
2018-08-14 09:58:59 -0700

    Vendor update

--- vendor/github.com/rancher/types/user/manager.go
+++ vendor/github.com/rancher/types/user/manager.go
@@ -3,6 +3,7 @@ package user
 import (
 	"github.com/rancher/norman/types"
 	"github.com/rancher/types/apis/management.cattle.io/v3"
+	apitypes "k8s.io/apimachinery/pkg/types"
 )
 
 type Manager interface {
@@ -12,4 +13,5 @@ type Manager interface {
 	EnsureUser(principalName, displayName string) (*v3.User, error)
 	CheckAccess(accessMode string, allowedPrincipalIDs []string, user v3.Principal, groups []v3.Principal) (bool, error)
 	SetPrincipalOnCurrentUserByUserID(userID string, principal v3.Principal) (*v3.User, error)
+	CreateNewUserClusterRoleBinding(userName string, userUID apitypes.UID) error
 }
