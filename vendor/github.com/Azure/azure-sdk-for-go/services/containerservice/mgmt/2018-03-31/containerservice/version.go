17fbbaf14303a6a1b8953feb0966014e3f7cdc7a
Nathan Jenan <nathanj@rancher.com>
2018-08-30 14:54:58 -0700

    Update Vendor

--- vendor/github.com/Azure/azure-sdk-for-go/services/containerservice/mgmt/2018-03-31/containerservice/version.go
+++ vendor/github.com/Azure/azure-sdk-for-go/services/containerservice/mgmt/2018-03-31/containerservice/version.go
@@ -21,7 +21,7 @@ import "github.com/Azure/azure-sdk-for-go/version"
 
 // UserAgent returns the UserAgent string to use when sending http.Requests.
 func UserAgent() string {
-	return "Azure-SDK-For-Go/" + version.Number + " containerservice/2017-08-31"
+	return "Azure-SDK-For-Go/" + version.Number + " containerservice/2018-03-31"
 }
 
 // Version returns the semantic version (see http://semver.org) of the client.
