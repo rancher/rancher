f557455566df3d5413dc9306e90e6f8b539a87e3
carolyn <carolyn@rancher.com>
2018-09-10 17:23:59 -0700

    update vendor

--- vendor/github.com/rancher/types/client/management/v3/zz_generated_active_directory_config.go
+++ vendor/github.com/rancher/types/client/management/v3/zz_generated_active_directory_config.go
@@ -18,6 +18,7 @@ const (
 	ActiveDirectoryConfigFieldGroupObjectClass             = "groupObjectClass"
 	ActiveDirectoryConfigFieldGroupSearchAttribute         = "groupSearchAttribute"
 	ActiveDirectoryConfigFieldGroupSearchBase              = "groupSearchBase"
+	ActiveDirectoryConfigFieldGroupSearchFilter            = "groupSearchFilter"
 	ActiveDirectoryConfigFieldLabels                       = "labels"
 	ActiveDirectoryConfigFieldName                         = "name"
 	ActiveDirectoryConfigFieldNestedGroupMembershipEnabled = "nestedGroupMembershipEnabled"
@@ -37,6 +38,7 @@ const (
 	ActiveDirectoryConfigFieldUserObjectClass              = "userObjectClass"
 	ActiveDirectoryConfigFieldUserSearchAttribute          = "userSearchAttribute"
 	ActiveDirectoryConfigFieldUserSearchBase               = "userSearchBase"
+	ActiveDirectoryConfigFieldUserSearchFilter             = "userSearchFilter"
 )
 
 type ActiveDirectoryConfig struct {
@@ -56,6 +58,7 @@ type ActiveDirectoryConfig struct {
 	GroupObjectClass             string            `json:"groupObjectClass,omitempty" yaml:"groupObjectClass,omitempty"`
 	GroupSearchAttribute         string            `json:"groupSearchAttribute,omitempty" yaml:"groupSearchAttribute,omitempty"`
 	GroupSearchBase              string            `json:"groupSearchBase,omitempty" yaml:"groupSearchBase,omitempty"`
+	GroupSearchFilter            string            `json:"groupSearchFilter,omitempty" yaml:"groupSearchFilter,omitempty"`
 	Labels                       map[string]string `json:"labels,omitempty" yaml:"labels,omitempty"`
 	Name                         string            `json:"name,omitempty" yaml:"name,omitempty"`
 	NestedGroupMembershipEnabled *bool             `json:"nestedGroupMembershipEnabled,omitempty" yaml:"nestedGroupMembershipEnabled,omitempty"`
@@ -75,4 +78,5 @@ type ActiveDirectoryConfig struct {
 	UserObjectClass              string            `json:"userObjectClass,omitempty" yaml:"userObjectClass,omitempty"`
 	UserSearchAttribute          string            `json:"userSearchAttribute,omitempty" yaml:"userSearchAttribute,omitempty"`
 	UserSearchBase               string            `json:"userSearchBase,omitempty" yaml:"userSearchBase,omitempty"`
+	UserSearchFilter             string            `json:"userSearchFilter,omitempty" yaml:"userSearchFilter,omitempty"`
 }
