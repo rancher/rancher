d9c16bbe34a165eb14cb854d9240b49fe1ce7e31
rajashree <rajashree.28m@gmail.com>
2018-09-20 15:40:05 -0700

    Add current user principal to allowedIDs during auth setup

--- pkg/auth/providers/saml/saml_provider.go
+++ pkg/auth/providers/saml/saml_provider.go
@@ -14,13 +14,13 @@ import (
 	"github.com/rancher/rancher/pkg/auth/providers/common"
 	"github.com/rancher/rancher/pkg/auth/tokens"
 	"github.com/rancher/types/apis/management.cattle.io/v3"
+	"github.com/rancher/types/apis/management.cattle.io/v3public"
 	"github.com/rancher/types/client/management/v3"
 	publicclient "github.com/rancher/types/client/management/v3public"
 	"github.com/rancher/types/config"
 	"github.com/rancher/types/user"
 	"github.com/sirupsen/logrus"
 
-	"github.com/rancher/types/apis/management.cattle.io/v3public"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 )
