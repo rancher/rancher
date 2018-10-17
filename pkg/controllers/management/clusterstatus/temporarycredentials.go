5b24df407e04ebc12efd6386ec98fcabbaa05950
Nathan Jenan <nathanj@rancher.com>
2018-10-17 16:38:37 -0700

    Adding controller to add a temporary security token annotation
    This change adds a controller that looks for the `sessionToken` to be present
    on the EKS config, and if it is there, add an annotation to the cluster.  The
    reason this is necessary is becuase credentials that have a `sessionToken`
    will expire and the UI must take additional steps to refresh the credentials
    before attempting an operation on the cluster, but because `sessionToken` is a
    password field, it will be hidden from the UI also.
    
    Issue:
    https://github.com/rancher/rancher/issues/16005

---
+++ pkg/controllers/management/clusterstatus/temporarycredentials.go
@@ -0,0 +1,50 @@
+package clusterstatus
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/rancher/types/apis/management.cattle.io/v3"
+	"github.com/rancher/types/config"
+)
+
+const temporaryCredentialsAnnotationKey = "clusterstatus.management.cattle.io/temporary-security-credentials"
+
+func Register(management *config.ManagementContext) {
+	c := &clusterAnnotations{
+		clusters: management.Management.Clusters(""),
+	}
+
+	management.Management.Clusters("").AddHandler("temporary-credentials", c.sync)
+}
+
+type clusterAnnotations struct {
+	clusters v3.ClusterInterface
+}
+
+func (cd *clusterAnnotations) sync(key string, cluster *v3.Cluster) error {
+	if key == "" || cluster == nil || cluster.DeletionTimestamp != nil {
+		return nil
+	}
+
+	if eksConfig := cluster.Spec.AmazonElasticContainerServiceConfig; eksConfig != nil {
+		newValue := strconv.FormatBool(eksConfig.SessionToken != "")
+
+		if newValue != cluster.Annotations[temporaryCredentialsAnnotationKey] {
+			original := cluster
+			cluster = original.DeepCopy()
+
+			if cluster.Annotations == nil {
+				cluster.Annotations = make(map[string]string)
+			}
+
+			cluster.Annotations[temporaryCredentialsAnnotationKey] = newValue
+			_, err := cd.clusters.Update(cluster)
+			if err != nil {
+				return fmt.Errorf("error updating temporary credentials annotation: %v", err)
+			}
+		}
+	}
+
+	return nil
+}
