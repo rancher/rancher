091a01c3c21c0ffeb21a03d668585a44fc983c03
Nathan Jenan <nathanj@rancher.com>
2018-10-22 09:06:08 -0700

    Display Temp Credentials status Immediately
    This change makes is to that the temporary security credentials annotation
    displays immediately on the cluster instead of only after the cluster has
    finished provisioning.
    
    Issue:
    https://github.com/rancher/rancher/issues/16005

---
+++ tests/core/test_kontainer_engine_annotations.py
@@ -0,0 +1,37 @@
+from .common import random_str
+
+
+def get_cluster_annotation(admin_mc, remove_resource, config):
+    cluster = admin_mc.client.create_cluster(
+        name=random_str(), amazonElasticContainerServiceConfig=config)
+    remove_resource(cluster)
+
+    cluster = admin_mc.client.reload(cluster)
+
+    return cluster.annotations[
+        "clusterstatus.management.cattle.io/temporary-security-credentials"]
+
+
+def test_eks_cluster_gets_temp_security_credentials_annotation(
+        admin_mc, remove_resource):
+    eks = {
+        "accessKey": "not a real access key",
+        "secretKey": "not a real secret key",
+        "sessionToken": "not a real session token",
+        "region": "us-west-2",
+    }
+
+    annotation = get_cluster_annotation(admin_mc, remove_resource, eks)
+    assert annotation == "true"
+
+
+def test_eks_cluster_does_not_get_temp_security_credentials_annotation(
+        admin_mc, remove_resource):
+    eks = {
+        "accessKey": "not a real access key",
+        "secretKey": "not a real secret key",
+        "region": "us-west-2",
+    }
+
+    annotation = get_cluster_annotation(admin_mc, remove_resource, eks)
+    assert annotation == "false"
