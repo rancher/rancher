f2280240fb51a98d3d4fbc8c2a6e112a2fbd097c
rajashree <rajashree.28m@gmail.com>
2018-10-25 16:46:20 -0700

    Modify catalogs-use and manage roles for rbac

--- pkg/controllers/management/template/template_global_catalog.go
+++ pkg/controllers/management/template/template_global_catalog.go
@@ -12,6 +12,12 @@ import (
 	"k8s.io/apimachinery/pkg/selection"
 )
 
+var rolesToUpdateForGlobalTemplates = map[string][]string{
+	"user":            {"get", "list", "watch"},
+	"catalogs-use":    {"get", "list", "watch"},
+	"catalogs-manage": {"*"},
+}
+
 func (tm *RBACTemplateManager) syncForGlobalCatalog(key string, obj *v3.Catalog) error {
 	var templateRuleExists, templateVersionsRuleExists bool
 	// Not going to check here whether obj is nil/deleted or not. Whether global catalog is created, updated or deleted, this method reconciles
@@ -23,57 +29,59 @@ func (tm *RBACTemplateManager) syncForGlobalCatalog(key string, obj *v3.Catalog)
 		return err
 	}
 
-	userRole, err := tm.globalRoleClient.Get("user", metav1.GetOptions{})
-	userRole = userRole.DeepCopy()
+	for roleName, verbs := range rolesToUpdateForGlobalTemplates {
+		role, err := tm.globalRoleClient.Get(roleName, metav1.GetOptions{})
+		role = role.DeepCopy()
 
-	templates, templateVersions, err := tm.getTemplateAndTemplateVersions(r)
-	sort.Strings(templates)
-	sort.Strings(templateVersions)
-	updatedRules := userRole.Rules
-	newTemplateRule := []v1.PolicyRule{
-		{
-			APIGroups:     []string{"management.cattle.io"},
-			Resources:     []string{templateRule},
-			ResourceNames: templates,
-			Verbs:         []string{"get", "list", "watch"},
-		},
-	}
-
-	newTemplateVersionRule := []v1.PolicyRule{
-		{
-			APIGroups:     []string{"management.cattle.io"},
-			Resources:     []string{templateVersionRule},
-			ResourceNames: templateVersions,
-			Verbs:         []string{"get", "list", "watch"},
-		},
-	}
-
-	for ind, r := range updatedRules {
-		switch r.Resources[0] {
-		case templateRule:
-			r.ResourceNames = templates
-			updatedRules[ind] = r
-			templateRuleExists = true
-		case templateVersionRule:
-			r.ResourceNames = templateVersions
-			updatedRules[ind] = r
-			templateVersionsRuleExists = true
+		templates, templateVersions, err := tm.getTemplateAndTemplateVersions(r)
+		sort.Strings(templates)
+		sort.Strings(templateVersions)
+		updatedRules := role.Rules
+		newTemplateRule := []v1.PolicyRule{
+			{
+				APIGroups:     []string{"management.cattle.io"},
+				Resources:     []string{templateRule},
+				ResourceNames: templates,
+				Verbs:         verbs,
+			},
 		}
-	}
 
-	if !templateRuleExists {
-		updatedRules = append(updatedRules, newTemplateRule...)
-	}
-	if !templateVersionsRuleExists {
-		updatedRules = append(updatedRules, newTemplateVersionRule...)
-	}
+		newTemplateVersionRule := []v1.PolicyRule{
+			{
+				APIGroups:     []string{"management.cattle.io"},
+				Resources:     []string{templateVersionRule},
+				ResourceNames: templateVersions,
+				Verbs:         verbs,
+			},
+		}
 
-	if !reflect.DeepEqual(userRole.Rules, updatedRules) {
-		newRole := userRole.DeepCopy()
-		newRole.Rules = updatedRules
-		_, err = tm.globalRoleClient.Update(newRole)
-		if err != nil {
-			return err
+		for ind, r := range updatedRules {
+			switch r.Resources[0] {
+			case templateRule:
+				r.ResourceNames = templates
+				updatedRules[ind] = r
+				templateRuleExists = true
+			case templateVersionRule:
+				r.ResourceNames = templateVersions
+				updatedRules[ind] = r
+				templateVersionsRuleExists = true
+			}
+		}
+
+		if !templateRuleExists {
+			updatedRules = append(updatedRules, newTemplateRule...)
+		}
+		if !templateVersionsRuleExists {
+			updatedRules = append(updatedRules, newTemplateVersionRule...)
+		}
+
+		if !reflect.DeepEqual(role.Rules, updatedRules) {
+			newRole := role.DeepCopy()
+			newRole.Rules = updatedRules
+			_, err = tm.globalRoleClient.Update(newRole)
+			if err != nil {
+				return err
+			}
 		}
 	}
 
