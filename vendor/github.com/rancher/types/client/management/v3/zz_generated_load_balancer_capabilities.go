e0538a88dd3f0fe46009f5db4ad344fe9d1cb1dc
rajashree <rajashree.28m@gmail.com>
2018-10-25 18:01:01 -0700

    Vendor folder changes

--- vendor/github.com/rancher/types/client/management/v3/zz_generated_load_balancer_capabilities.go
+++ vendor/github.com/rancher/types/client/management/v3/zz_generated_load_balancer_capabilities.go
@@ -1,14 +1,14 @@
 package client
 
 const (
-	L4LoadBalancerType                      = "l4LoadBalancer"
-	L4LoadBalancerFieldEnabled              = "enabled"
-	L4LoadBalancerFieldHealthCheckSupported = "healthCheckSupported"
-	L4LoadBalancerFieldProtocolsSupported   = "protocolsSupported"
-	L4LoadBalancerFieldProvider             = "provider"
+	LoadBalancerCapabilitiesType                      = "loadBalancerCapabilities"
+	LoadBalancerCapabilitiesFieldEnabled              = "enabled"
+	LoadBalancerCapabilitiesFieldHealthCheckSupported = "healthCheckSupported"
+	LoadBalancerCapabilitiesFieldProtocolsSupported   = "protocolsSupported"
+	LoadBalancerCapabilitiesFieldProvider             = "provider"
 )
 
-type L4LoadBalancer struct {
+type LoadBalancerCapabilities struct {
 	Enabled              bool     `json:"enabled,omitempty" yaml:"enabled,omitempty"`
 	HealthCheckSupported bool     `json:"healthCheckSupported,omitempty" yaml:"healthCheckSupported,omitempty"`
 	ProtocolsSupported   []string `json:"protocolsSupported,omitempty" yaml:"protocolsSupported,omitempty"`
