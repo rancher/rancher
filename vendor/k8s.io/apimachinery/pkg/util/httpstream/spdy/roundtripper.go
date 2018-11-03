531a4cf19d166cebb73cf4e29bedc85178a76a14
Darren Shepherd <darren@rancher.com>
2018-11-02 17:06:36 -0700

    hack

--- vendor/k8s.io/apimachinery/pkg/util/httpstream/spdy/roundtripper.go
+++ vendor/k8s.io/apimachinery/pkg/util/httpstream/spdy/roundtripper.go
@@ -255,7 +255,7 @@ func (s *SpdyRoundTripper) RoundTrip(req *http.Request) (*http.Response, error) 
 	)
 
 	if s.followRedirects {
-		conn, rawResponse, err = utilnet.ConnectWithRedirects(req.Method, req.URL, header, req.Body, s)
+		conn, rawResponse, _, err = utilnet.ConnectWithRedirects(true, req.Method, req.URL, header, req.Body, s)
 	} else {
 		clone := utilnet.CloneRequest(req)
 		clone.Header = header
