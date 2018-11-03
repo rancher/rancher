f296b3eea26f3c2ad2383cf2d3933cb42c10db8a
Darren Shepherd <darren@rancher.com>
2018-11-02 23:07:01 -0700

    Update vendor

---
+++ vendor/github.com/rancher/norman/pkg/kwrapper/kubectl/main.go
@@ -0,0 +1,39 @@
+package kubectl
+
+import (
+	goflag "flag"
+	"fmt"
+	"math/rand"
+	"os"
+	"time"
+
+	"github.com/docker/docker/pkg/reexec"
+	"github.com/spf13/pflag"
+	utilflag "k8s.io/apiserver/pkg/util/flag"
+	"k8s.io/apiserver/pkg/util/logs"
+	"k8s.io/kubernetes/pkg/kubectl/cmd"
+)
+
+func init() {
+	reexec.Register("kubectl", Main)
+}
+
+func Main() {
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	command := cmd.NewDefaultKubectlCommand()
+
+	// TODO: once we switch everything over to Cobra commands, we can go back to calling
+	// utilflag.InitFlags() (by removing its pflag.Parse() call). For now, we have to set the
+	// normalize func and add the go flag set by hand.
+	pflag.CommandLine.SetNormalizeFunc(utilflag.WordSepNormalizeFunc)
+	pflag.CommandLine.AddGoFlagSet(goflag.CommandLine)
+	// utilflag.InitFlags()
+	logs.InitLogs()
+	defer logs.FlushLogs()
+
+	if err := command.Execute(); err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		os.Exit(1)
+	}
+}
