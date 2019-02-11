9e42361f757a4f800832aa14f0515717ade6daac
orangedeng <jxfa0043379@hotmail.com>
2019-02-11 14:31:54 +0800

    Set `ui-index` setting to local if rancher version is formal
    **Problem:**
    For now, we don't respect the `ui-index` setting in formal
    rancher version. There is no way for users to customize their UI in
    those version.
    
    **Solution:**
    - Use `local` value in `ui-index` setting when it is a formal version.
    - When `ui-index` setting is `local`, fetch UI from file
    - When in development version, always fetch UI from remote address
    in `ui-index`.

--- server/ui/ui.go
+++ server/ui/ui.go
@@ -1,13 +1,11 @@
 package ui
 
 import (
+	"crypto/tls"
 	"io"
 	"net/http"
 	"os"
 	"path/filepath"
-	"strings"
-
-	"crypto/tls"
 
 	"github.com/rancher/norman/parse"
 	"github.com/rancher/rancher/pkg/settings"
@@ -29,19 +27,11 @@ func Content() http.Handler {
 }
 
 func UI(next http.Handler) http.Handler {
-	local := false
 	_, err := os.Stat(indexHTML())
-	if err == nil {
-		local = true
-	}
-
-	if local && !strings.HasPrefix(settings.ServerVersion.Get(), "v") {
-		local = false
-	}
-
+	local := err == nil
 	return http.HandlerFunc(func(resp http.ResponseWriter, req *http.Request) {
 		if parse.IsBrowser(req, true) {
-			if local {
+			if local && settings.UIIndex.Get() == "local" {
 				http.ServeFile(resp, req, indexHTML())
 			} else {
 				ui(resp, req)
