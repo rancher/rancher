03dd1256a789ccb11299c3edea23549466a0cb04
rmweir <ricardo.weir1@gmail.com>
2019-02-12 10:20:27 -0700

    Fix cluster provisioning states
    Adding cluster initial conditions:
    This change adds a set of initial conditions to newly created clusters. These
    conditions are so that a cluster shows the proper status on the UI before any
    controllers have had a chance to run (this can be quite a few moments) on the
    cluster.
    
    Removed pending for non-imported drivers:
    Removed pending state for non-imported drivers.
    Pending logic executes once, instead of persisting
    until pending state is true.
    
    Add reason to forget error:
    Added "Reasoning" field to forget error. Prior,
    the pending state would display red and transitioning
    field would be false. Now, the pending state will
    display blue and transition will be set to true.
    
    Add tests for different engines:
    Added and revised tests for different engines. Non-imported engines
    should start with default conditions, while imported engines should
    start with no conditions to ensure they enter the pending
    state.

--- tests/core/test_cluster_defaults.py
+++ tests/core/test_cluster_defaults.py
@@ -1,9 +1,11 @@
-import pytest
 import json
+import pytest
+
+from .common import random_str
 
 
 @pytest.mark.skip(reason="cluster-defaults disabled")
-def test_initial_defaults(admin_mc):
+def test_generic_initial_defaults(admin_mc):
     cclient = admin_mc.client
     schema_defaults = {}
     setting_defaults = {}
@@ -15,12 +17,12 @@ def test_initial_defaults(admin_mc):
         if name == "enableNetworkPolicy":
             schema_defaults["enableNetworkPolicy"] = default
 
-    for name in cclient.schema.types['rancherKubernetesEngineConfig']\
+    for name in cclient.schema.types['rancherKubernetesEngineConfig'] \
             .resourceFields.keys():
         if name == "ignoreDockerVersion":
-            schema_defaults["ignoreDockerVersion"] = cclient.schema.\
-                types["rancherKubernetesEngineConfig"].\
-                resourceFields["ignoreDockerVersion"].\
+            schema_defaults["ignoreDockerVersion"] = cclient.schema. \
+                types["rancherKubernetesEngineConfig"]. \
+                resourceFields["ignoreDockerVersion"]. \
                 data_dict()["default"]
 
     setting = cclient.list_setting(name="cluster-defaults")
@@ -31,3 +33,44 @@ def test_initial_defaults(admin_mc):
         data["rancherKubernetesEngineConfig"]["ignoreDockerVersion"]
 
     assert schema_defaults == setting_defaults
+
+
+def test_generic_initial_conditions(admin_mc, remove_resource):
+    cluster = admin_mc.client.create_cluster(
+            name=random_str(), amazonElasticContainerServiceConfig={
+                "accessKey": "asdfsd"})
+    remove_resource(cluster)
+
+    assert len(cluster.conditions) == 3
+    assert cluster.conditions[0].type == 'Pending'
+    assert cluster.conditions[0].status == 'True'
+
+    assert cluster.conditions[1].type == 'Provisioned'
+    assert cluster.conditions[1].status == 'Unknown'
+
+    assert cluster.conditions[2].type == 'Waiting'
+    assert cluster.conditions[2].status == 'Unknown'
+
+
+def test_rke_initial_conditions(admin_mc, remove_resource):
+    cluster = admin_mc.client.create_cluster(
+            name=random_str(), rancherKubernetesEngineConfig={
+                "accessKey": "asdfsd"})
+    remove_resource(cluster)
+
+    assert len(cluster.conditions) == 3
+    assert cluster.conditions[0].type == 'Pending'
+    assert cluster.conditions[0].status == 'True'
+
+    assert cluster.conditions[1].type == 'Provisioned'
+    assert cluster.conditions[1].status == 'Unknown'
+
+    assert cluster.conditions[2].type == 'Waiting'
+    assert cluster.conditions[2].status == 'Unknown'
+
+
+def test_import_initial_conditions(admin_mc, remove_resource):
+    cluster = admin_mc.client.create_cluster(name=random_str())
+    remove_resource(cluster)
+
+    assert cluster.conditions is None
