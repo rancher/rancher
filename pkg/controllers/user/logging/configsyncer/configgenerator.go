fcbd6dacbdb60cddf29e44bee26eb7e0f679a22b
Aiwantaozi <michelia.feng@gmail.com>
2019-02-12 12:58:07 +0800

    Use positive option instead of negative
    Problem:
    excludeSystemComponent is a negative field
    
    Solution:
    change to includeSystemComponent and use true as default
    
    Issue:
    https://github.com/rancher/rancher/issues/15555
    
    Types:
    https://github.com/rancher/types/pull/715

--- pkg/controllers/user/logging/configsyncer/configgenerator.go
+++ pkg/controllers/user/logging/configsyncer/configgenerator.go
@@ -37,7 +37,7 @@ func (s *ConfigGenerator) GenerateClusterLoggingConfig(clusterLogging *mgmtv3.Cl
 	}
 
 	var excludeNamespaces string
-	if clusterLogging.Spec.ExcludeSystemComponent {
+	if !*clusterLogging.Spec.IncludeSystemComponent {
 		var err error
 		if excludeNamespaces, err = s.addExcludeNamespaces(systemProjectID); err != nil {
 			return nil, err
