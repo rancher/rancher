b0a1fb977352290816c4c2206780ace5d24581b7
Fyery <chenfeier115121@163.com>
2019-02-12 16:01:40 +0800

    Update to expose some extra parameters for options
    GKE enhancement to add more complex options
    
    ISSUE:
    https://github.com/rancher/rancher/issues/14634
    https://github.com/rancher/rancher/issues/14298
    https://github.com/rancher/rancher/issues/13980
    https://github.com/rancher/rancher/issues/14299

---
+++ server/capabilities/gke_networks_endpoint.go
@@ -0,0 +1,89 @@
+package capabilities
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"google.golang.org/api/compute/v1"
+	"net/http"
+)
+
+// NewGKENetworksHandler creates a new GKENetworksHandler
+func NewGKENetworksHandler() *GKENetworksHandler {
+	return &GKENetworksHandler{}
+}
+
+// GKENetworksHandler lists available networks in GKE
+type GKENetworksHandler struct {
+}
+
+func (g *GKENetworksHandler) ServeHTTP(writer http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodPost {
+		writer.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	writer.Header().Set("Content-Type", "application/json")
+
+	var body zoneCapabilitiesRequestBody
+	err := extractRequestBody(writer, req, &body)
+
+	if err != nil {
+		handleErr(writer, err)
+		return
+	}
+
+	err = validateCapabilitiesRequestBody(writer, &body.capabilitiesRequestBody)
+
+	if err != nil {
+		handleErr(writer, err)
+		return
+	}
+
+	if body.Credentials == "" {
+		writer.WriteHeader(http.StatusBadRequest)
+		handleErr(writer, fmt.Errorf("invalid credentials"))
+		return
+	}
+
+	client, err := g.getServiceClient(context.Background(), body.Credentials)
+
+	if err != nil {
+		writer.WriteHeader(http.StatusInternalServerError)
+		handleErr(writer, err)
+		return
+	}
+
+	result, err := client.Networks.List(body.ProjectID).Do()
+
+	if err != nil {
+		writer.WriteHeader(http.StatusInternalServerError)
+		handleErr(writer, err)
+		return
+	}
+
+	serialized, err := json.Marshal(result)
+
+	if err != nil {
+		writer.WriteHeader(http.StatusInternalServerError)
+		handleErr(writer, err)
+		return
+	}
+
+	writer.Write(serialized)
+}
+
+func (g *GKENetworksHandler) getServiceClient(ctx context.Context, credentialContent string) (*compute.Service, error) {
+	client, err := getOAuthClient(ctx, credentialContent)
+
+	if err != nil {
+		return nil, err
+	}
+
+	service, err := compute.New(client)
+
+	if err != nil {
+		return nil, err
+	}
+	return service, nil
+}
