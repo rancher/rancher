dc6fe56c9771edcf7a0a88c7cfa0e1c7cb2a86bd
Dan Ramich <danold215@gmail.com>
2019-02-15 16:15:30 -0700

    Add Frame-Options header
    Problem:
    Frame-Options header is not being set, this is a potential secutiry
    issue
    
    Solution:
    Set the header "X-Frame-Options:deny"

--- server/responsewriter/cache.go
+++ server/responsewriter/cache.go
@@ -33,3 +33,10 @@ func NoCache(handler http.Handler) http.Handler {
 		handler.ServeHTTP(w, r)
 	})
 }
+
+func DenyFrameOptions(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("X-Frame-Options", "deny")
+		handler.ServeHTTP(w, r)
+	})
+}
