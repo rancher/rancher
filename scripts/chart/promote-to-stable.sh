d90c1f23b1bd9734e510a77e9d830ce68d459fbe
Bill Maxwell <bill@rancher.com>
2019-02-19 13:12:21 -0700

    updated to match drone1 yaml format

--- scripts/chart/promote-to-stable.sh
+++ scripts/chart/promote-to-stable.sh
@@ -1,32 +1,37 @@
 #!/bin/bash
 
+drone_cli_help()
+{
+    echo "  Requires Drone CLI and personal access token"
+    echo "  Download CLI following instructions at https://docs.drone.io/cli/install/"
+    echo "  Then configure access token: https://docs.drone.io/cli/setup/"
+    echo ""
+    echo "  To test if Drone CLI is properly configured type:"
+    echo ""
+    echo "     drone build ls rancher/rancher"
+    echo ""
+    echo "  This will show the last 25 builds"
+}
+
 if [[ -z "${1}" ]]; then
     echo "Promote a chart in latest to stable."
     echo "  $0 <tag>"
     echo ""
-    echo "  Requires a GitHub Personal Access Token exported as GH_TOKEN"
-    echo "    Create a token at https://github.com/settings/tokens"
-    echo "    Permissions: repo_deployment"
+    drone_cli_help
     exit 1
 fi
 
-if [[ -z "${GH_TOKEN}" ]]; then
-    echo 'ERROR: Missing $GH_TOKEN'
-    echo 'Create a GitHub Personal Access Token and export it as GH_TOKEN'
-    echo 'Required permissions: repo_deployment'
-    echo 'https://github.com/settings/tokens'
+if ! drone -v; then
+    drone_cli_help
     exit 1
 fi
 
-data=$(cat <<EOF
-{
-    "ref": "refs/tags/${1}",
-    "environment": "promote-stable",
-    "auto_merge": false,
-    "description": "promote-to-stable.sh"
-}
-EOF
-)
+build_number=$(drone build ls rancher/rancher --event tag --format "{{.Number}},{{.Ref}}"| grep ${1} |cut -d',' -f1)
 
-echo $data | curl -H "Authorization: token ${GH_TOKEN}" -H "Content-Type: application/json" \
--X POST https://api.github.com/repos/rancher/rancher/deployments -d @-
+if [[ -n ${build_number} ]];then 
+  drone build promote rancher/rancher ${build_number} promote-stable
+  exit 0
+fi
+
+echo "No Build Found for TAG: ${1}"
+exit 1
