817c093f09f47de5838ca7c375899f3dc778e5d5
rmweir <ricardo.weir1@gmail.com>
2019-02-21 10:43:01 -0700

    Add and fix integration tests
    Added integration test for creating kontainer driver
    with a URL that is in use. Added integration test for
    updating driver with URL that is in use. Added driver
    removal for preceeding test.

--- tests/core/test_kontainer_drivers.py
+++ tests/core/test_kontainer_drivers.py
@@ -2,6 +2,7 @@ import sys
 
 import pytest
 
+from rancher import ApiError
 from .conftest import wait_for_condition, wait_until
 
 DRIVER_URL = "https://github.com/rancher/kontainer-engine-driver-example/" \
@@ -63,7 +64,7 @@ def test_kontainer_driver_lifecycle(admin_mc, remove_resource):
 
 
 @pytest.mark.nonparallel
-def test_enabling_driver_exposes_schema(admin_mc, remove_resource):
+def test_enabling_driver_exposes_schema(admin_mc, wait_remove_resource):
     """ Test if enabling driver exposes its dynamic schema, drivers are
      downloaded / installed once they are active """
     kd = admin_mc.client.create_kontainerDriver(
@@ -71,7 +72,7 @@ def test_enabling_driver_exposes_schema(admin_mc, remove_resource):
         active=False,
         url=DRIVER_URL
     )
-    remove_resource(kd)
+    wait_remove_resource(kd)
 
     kd = wait_for_condition('Inactive', 'True', admin_mc.client, kd,
                             timeout=90)
@@ -95,6 +96,60 @@ def test_enabling_driver_exposes_schema(admin_mc, remove_resource):
     admin_mc.client.update_by_id_kontainerDriver(kd.id, kd)
 
     verify_driver_not_in_types(admin_mc.client, kd)
+
+
+@pytest.mark.nonparallel
+def test_create_duplicate_driver_conflict(admin_mc, wait_remove_resource):
+    """ Test if adding a driver with a pre-existing driver's URL
+    returns a conflict error"""
+    kd = admin_mc.client.create_kontainerDriver(
+        createDynamicSchema=True,
+        active=True,
+        url=DRIVER_URL
+    )
+    wait_remove_resource(kd)
+    kd = wait_for_condition('Active', 'True', admin_mc.client, kd, timeout=90)
+
+    try:
+        kd2 = admin_mc.client.create_kontainerDriver(
+            createDynamicSchema=True,
+            active=True,
+            url=DRIVER_URL
+        )
+        wait_remove_resource(kd2)
+        pytest.fail("Failed to catch duplicate driver URL on create")
+    except ApiError as e:
+        assert e.error.status == 409
+        assert "Driver URL already in use:" in e.error.message
+
+
+@pytest.mark.nonparallel
+def test_update_duplicate_driver_conflict(admin_mc, wait_remove_resource):
+    """ Test if updating a driver's URL to a pre-existing driver's URL
+    returns a conflict error"""
+    kd1 = admin_mc.client.create_kontainerDriver(
+        createDynamicSchema=True,
+        active=True,
+        url=DRIVER_URL
+    )
+    wait_remove_resource(kd1)
+    kd1 = wait_for_condition('Active', 'True', admin_mc.client, kd1,
+                             timeout=90)
+
+    kd2 = admin_mc.client.create_kontainerDriver(
+        createDynamicSchema=True,
+        active=True,
+        url=DRIVER_URL + "2"
+    )
+    wait_remove_resource(kd2)
+    kd2.url = DRIVER_URL
+
+    try:
+        admin_mc.client.update_by_id_kontainerDriver(kd2.id, kd2)
+        pytest.fail("Failed to catch duplicate driver URL on update")
+    except ApiError as e:
+        assert e.error.status == 409
+        assert "Driver URL already in use:" in e.error.message
 
 
 def verify_driver_in_types(client, kd):
