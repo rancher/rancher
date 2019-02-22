c9398e0c7423ccfb9b0a6ee0954b944e10674065
kinarashah <kinara@rancher.com>
2019-02-21 16:07:37 -0800

    propagate app state to multi cluster app target

--- pkg/controllers/management/multiclusterapp/state.go
+++ pkg/controllers/management/multiclusterapp/state.go
@@ -3,7 +3,6 @@ package multiclusterapp
 import (
 	"context"
 	"fmt"
-	"github.com/rancher/norman/condition"
 	"github.com/rancher/types/apis/management.cattle.io/v3"
 	pv3 "github.com/rancher/types/apis/project.cattle.io/v3"
 	"github.com/rancher/types/config"
@@ -11,6 +10,12 @@ import (
 	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	"strings"
+)
+
+const (
+	installing = "installing"
+	deploying  = "deploying"
+	active     = "active"
 )
 
 //StartMCAppStateController gets all corresponding apps and update condition on multi cluster app sync
@@ -32,92 +37,98 @@ func (m *MCAppStateController) sync(key string, mcapp *v3.MultiClusterApp) (runt
 	if mcapp == nil || mcapp.DeletionTimestamp != nil {
 		return mcapp, nil
 	}
+	mcappState := active
 	if v3.MultiClusterAppConditionInstalled.IsUnknown(mcapp) && v3.MultiClusterAppConditionInstalled.GetMessage(mcapp) == "upgrading" {
-		return mcapp, nil
+		mcappState = ""
 	}
-	var apps []*pv3.App
-	for _, t := range mcapp.Spec.Targets {
+	var toUpdate *v3.MultiClusterApp
+	for ind, t := range mcapp.Spec.Targets {
+		if t.AppName == "" {
+			mcappState = installing
+			continue
+		}
 		split := strings.SplitN(t.ProjectName, ":", 2)
 		if len(split) != 2 {
 			return mcapp, fmt.Errorf("error in splitting project ID %v", t.ProjectName)
 		}
 		projectNS := split[1]
-		if t.AppName != "" {
-			app, err := m.Apps.Get(projectNS, t.AppName)
-			if err != nil {
-				if errors.IsNotFound(err) {
-					logrus.Infof("app %s not found for mcapp %s in projectNS %s", t.AppName, mcapp.Name, projectNS)
-					continue
+		app, err := m.Apps.Get(projectNS, t.AppName)
+		if err != nil {
+			if errors.IsNotFound(err) {
+				logrus.Debugf("app %s not found for mcapp %s in projectNS %s", t.AppName, mcapp.Name, projectNS)
+				if mcappState != "" {
+					mcappState = installing
 				}
-				return mcapp, err
+				continue
 			}
-			if value, ok := app.Labels[MultiClusterAppIDSelector]; !ok || value != mcapp.Name {
-				return mcapp, fmt.Errorf("app %s missing label selector for %s", t.AppName, mcapp.Name)
-			}
-			apps = append(apps, app)
+			return mcapp, err
 		}
-	}
-	toUpdate := mcapp.DeepCopy()
-	if len(apps) != len(mcapp.Spec.Targets) {
-		if !v3.MultiClusterAppConditionInstalled.IsUnknown(toUpdate) {
-			v3.MultiClusterAppConditionInstalled.Unknown(toUpdate)
-			return m.MultiClusterApps.Update(toUpdate)
+		if value, ok := app.Labels[MultiClusterAppIDSelector]; !ok || value != mcapp.Name {
+			return mcapp, fmt.Errorf("app %s missing label selector for %s", t.AppName, mcapp.Name)
 		}
-		return mcapp, nil
-	}
-	updating := map[string]bool{}
-	installing := map[string]bool{}
-	for _, app := range apps {
 		if !pv3.AppConditionInstalled.IsTrue(app) {
-			installing[app.Name] = true
+			toUpdate = setAppState(toUpdate, ind, installing, mcapp)
+			if mcappState != "" {
+				mcappState = installing
+			}
 		} else if !pv3.AppConditionDeployed.IsTrue(app) {
-			updating[app.Name] = true
+			toUpdate = setAppState(toUpdate, ind, deploying, mcapp)
+			if mcappState == active {
+				mcappState = deploying
+			}
+		} else {
+			toUpdate = setAppState(toUpdate, ind, active, mcapp)
 		}
 	}
-	if checkForUpdate(installing, toUpdate, v3.MultiClusterAppConditionInstalled) {
+	if mcappState == installing || mcappState == deploying {
+		toUpdate = setMcAppStateUnknown(toUpdate, mcapp, mcappState)
+	} else if mcappState == active {
+		toUpdate = setMcappStateActive(toUpdate, mcapp)
+	}
+	if toUpdate != nil {
 		return m.MultiClusterApps.Update(toUpdate)
 	}
-
-	if checkForUpdate(updating, toUpdate, v3.MultiClusterAppConditionDeployed) {
-		return m.MultiClusterApps.Update(toUpdate)
-	}
-
 	return mcapp, nil
 }
 
-func checkForUpdate(transitioning map[string]bool, mcapp *v3.MultiClusterApp, cond condition.Cond) bool {
-	if len(transitioning) > 0 {
-		existing := strings.Split(cond.GetMessage(mcapp), ",")
-		if !Equal(existing, transitioning) {
-			cond.Unknown(mcapp)
-			cond.Message(mcapp, GetMsg(transitioning))
-			return true
-		}
-	} else if !cond.IsTrue(mcapp) {
-		cond.True(mcapp)
-		cond.Reason(mcapp, "")
-		cond.Message(mcapp, "")
-		return true
+func setAppState(toUpdate *v3.MultiClusterApp, ind int, state string, mcapp *v3.MultiClusterApp) *v3.MultiClusterApp {
+	if mcapp.Spec.Targets[ind].State == state {
+		return toUpdate
 	}
-	return false
+	if toUpdate == nil {
+		toUpdate = mcapp.DeepCopy()
+	}
+	toUpdate.Spec.Targets[ind].State = state
+	return toUpdate
 }
 
-func GetMsg(data map[string]bool) string {
-	var keys []string
-	for key := range data {
-		keys = append(keys, key)
+func setMcappStateActive(toUpdate *v3.MultiClusterApp, mcapp *v3.MultiClusterApp) *v3.MultiClusterApp {
+	if v3.MultiClusterAppConditionInstalled.IsTrue(mcapp) && v3.MultiClusterAppConditionDeployed.IsTrue(mcapp) {
+		return toUpdate
 	}
-	return fmt.Sprintf("%s", strings.Join(keys, ","))
+	if toUpdate == nil {
+		toUpdate = mcapp.DeepCopy()
+	}
+	v3.MultiClusterAppConditionDeployed.True(toUpdate)
+	v3.MultiClusterAppConditionInstalled.True(toUpdate)
+	return toUpdate
 }
 
-func Equal(existing []string, toUpdate map[string]bool) bool {
-	if len(existing) != len(toUpdate) {
-		return false
+func setMcAppStateUnknown(toUpdate *v3.MultiClusterApp, mcapp *v3.MultiClusterApp, state string) *v3.MultiClusterApp {
+	cond := v3.MultiClusterAppConditionInstalled
+	cleanCond := v3.MultiClusterAppConditionDeployed
+	if state == deploying {
+		cond = v3.MultiClusterAppConditionDeployed
+		cleanCond = v3.MultiClusterAppConditionInstalled
 	}
-	for _, name := range existing {
-		if _, ok := toUpdate[name]; !ok {
-			return false
+	if !cond.IsUnknown(mcapp) {
+		if toUpdate == nil {
+			toUpdate = mcapp.DeepCopy()
 		}
+		if cleanCond.IsUnknown(toUpdate) {
+			cleanCond.True(toUpdate)
+		}
+		cond.Unknown(toUpdate)
 	}
-	return true
+	return toUpdate
 }
