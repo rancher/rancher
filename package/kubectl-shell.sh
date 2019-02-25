30d0811f9f1801201222b7f337b73cec33e4b5f9
moelsayed <m.elsayed@gmail.com>
2019-02-26 01:45:30 +0200

    Remove arg check

--- package/kubectl-shell.sh
+++ package/kubectl-shell.sh
@@ -15,11 +15,6 @@ if [ -z "${cluster}" ]; then
     exit 1
 fi
 
-if [ -z "${cacerts}" ]; then
-    echo No cacerts provided
-    exit 1
-fi
-
 echo "# Run kubectl commands inside here"
 echo "# e.g. kubectl get all"
 export TERM=screen-256color
