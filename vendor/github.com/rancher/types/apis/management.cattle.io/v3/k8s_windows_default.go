abdd9a9dc661f953d22f009465b0b768a90c3265
Alena Prokharchyk <alena@rancher.com>
2019-03-06 14:39:10 -0800

    Types vendor update

--- vendor/github.com/rancher/types/apis/management.cattle.io/v3/k8s_windows_default.go
+++ vendor/github.com/rancher/types/apis/management.cattle.io/v3/k8s_windows_default.go
@@ -247,6 +247,14 @@ var (
 			CanalCNIBinaries:   m("rancher/canal-cni:v0.0.1-nanoserver-1803"),
 			KubeletPause:       m("rancher/kubelet-pause:v0.0.1-nanoserver-1803"),
 		},
+		"v1.13.4-rancher1-2": {
+			NginxProxy:         m("rancher/nginx-proxy:v0.0.1-nanoserver-1803"),
+			KubernetesBinaries: m("rancher/hyperkube:v1.13.4-nanoserver-1803"),
+			FlannelCNIBinaries: m("rancher/flannel-cni:v0.0.1-nanoserver-1803"),
+			CalicoCNIBinaries:  m("rancher/calico-cni:v0.0.1-nanoserver-1803"),
+			CanalCNIBinaries:   m("rancher/canal-cni:v0.0.1-nanoserver-1803"),
+			KubeletPause:       m("rancher/kubelet-pause:v0.0.1-nanoserver-1803"),
+		},
 	}
 )
 
