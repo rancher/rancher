f03aee24a64d78327fb4ad1cc25162d8ddd5c76a
Erik Wilson <Erik.E.Wilson@gmail.com>
2019-03-12 14:24:45 -0700

    Use k8s_defaults for AuthImage setting
    Set the kube-api-auth image setting from types k8s_default data so it
    is only defined in a single location.
    
    For rancher/rancher/issues/18755

--- pkg/settings/setting.go
+++ pkg/settings/setting.go
@@ -14,7 +14,7 @@ var (
 	InjectDefaults string
 
 	AgentImage                      = NewSetting("agent-image", "rancher/rancher-agent:master")
-	AuthImage                       = NewSetting("auth-image", "rancher/kube-api-auth:v0.1.3")
+	AuthImage                       = NewSetting("auth-image", v3.ToolsSystemImages.AuthSystemImages.KubeAPIAuth)
 	WindowsAgentImage               = NewSetting("windows-agent-image", "rancher/rancher-agent:master-nanoserver-1803")
 	CACerts                         = NewSetting("cacerts", "")
 	CLIURLDarwin                    = NewSetting("cli-url-darwin", "https://releases.rancher.com/cli/v1.0.0-alpha8/rancher-darwin-amd64-v1.0.0-alpha8.tar.gz")
