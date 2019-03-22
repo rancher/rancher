49266059ac17a904535fa36d758c084344b7fc55
dax <dax@rancher.com>
2019-03-21 17:05:39 -0700

    Fix helm creating lots of configmaps
    Problem: Helm/Tiller creates configmaps to track revision changes.
    Rancher should use AppRevisions to do this but was not tracking AppRevisions
    on failed helm upgrades. This caused rancher to run helm/tiller multiple
    times on failing helm charts which created a large number of configmaps
    quickly which would slow down the UI.
    
    Solution: Rancher now correctly makes AppRevisions for failed helm
    upgrades. Tiller has a max revision history of 1 (the minimum for
    tiller to remain functional). Tiller will delete previous configmaps
    the each time it is run.

--- pkg/api/customization/app/app.go
+++ pkg/api/customization/app/app.go
@@ -151,7 +151,8 @@ func (w Wrapper) ActionHandler(actionName string, action *types.Action, apiConte
 		if valuesYaml != nil {
 			obj.Spec.ValuesYaml = convert.ToString(valuesYaml)
 		}
-
+		// indicate this a user driven action
+		pv3.AppConditionUserTriggeredAction.True(obj)
 		if _, err := w.AppGetter.Apps(namespace).Update(obj); err != nil {
 			return err
 		}
@@ -183,7 +184,8 @@ func (w Wrapper) ActionHandler(actionName string, action *types.Action, apiConte
 			obj.Annotations[creatorIDAnno] = w.UserManager.GetUser(apiContext)
 		}
 		obj.Spec.Files = appRevision.Status.Files
-
+		// indicate this a user driven action
+		pv3.AppConditionUserTriggeredAction.True(obj)
 		if _, err := w.AppGetter.Apps(namespace).Update(obj); err != nil {
 			return err
 		}
