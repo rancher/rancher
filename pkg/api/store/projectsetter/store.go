90af40efa27bf81a0abfb9d70ccb1d5d33a55f27
rmweir <ricardo.weir1@gmail.com>
2019-03-26 11:41:45 -0700

    Find project namespaces before listing and filter
    Now, identifies and supplies namespaces to namespace filter for requests
    associated with given project. Prior, the backend listed items belonging
    to all namespaces in all projects no matter what, and then filtered. This
    led to longer request times for projects that contained a fraction of
    total resources. Now, request times are improved and largely dependent on
    resource amount for the given project.

--- pkg/api/store/projectsetter/store.go
+++ pkg/api/store/projectsetter/store.go
@@ -5,8 +5,11 @@ import (
 	"github.com/rancher/norman/types"
 	"github.com/rancher/norman/types/convert"
 	"github.com/rancher/rancher/pkg/clustermanager"
+	"github.com/rancher/rancher/pkg/project"
 	"github.com/rancher/types/apis/core/v1"
 	"github.com/rancher/types/client/cluster/v3"
+	k8sv1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/labels"
 )
 
 func New(store types.Store, manager *clustermanager.Manager) types.Store {
@@ -14,15 +17,33 @@ func New(store types.Store, manager *clustermanager.Manager) types.Store {
 		ClusterManager: manager,
 	}
 	return &transform.Store{
-		Store:             store,
+		Store: projectSetter{
+			store,
+			manager,
+		},
 		Transformer:       t.object,
 		ListTransformer:   t.list,
 		StreamTransformer: t.stream,
 	}
 }
 
+type projectSetter struct {
+	types.Store
+
+	ClusterManager *clustermanager.Manager
+}
+
 type transformer struct {
 	ClusterManager *clustermanager.Manager
+}
+
+func (p projectSetter) List(apiContext *types.APIContext, schema *types.Schema, opt *types.QueryOptions) ([]map[string]interface{}, error) {
+	options := *opt
+	if err := p.setOptionsNamespaces(apiContext, &options); err != nil {
+		return nil, err
+	}
+
+	return p.Store.List(apiContext, schema, &options)
 }
 
 func (t *transformer) object(apiContext *types.APIContext, schema *types.Schema, data map[string]interface{}, opt *types.QueryOptions) (map[string]interface{}, error) {
@@ -41,6 +62,56 @@ func (t *transformer) list(apiContext *types.APIContext, schema *types.Schema, d
 	}
 
 	return data, nil
+}
+
+// Retrieves project from api sub-context, then finds associated namespaces. If found, assigns to options.
+func (p projectSetter) setOptionsNamespaces(apiContext *types.APIContext, opt *types.QueryOptions) error {
+	clusterName := p.ClusterManager.ClusterName(apiContext)
+	if clusterName == "" {
+		return nil
+	}
+
+	clusterContext, err := p.ClusterManager.UserContext(clusterName)
+	if err != nil {
+		return err
+	}
+
+	namespaces, err := clusterContext.Core.Namespaces("").Controller().Lister().List("", labels.NewSelector())
+	if err != nil {
+		return err
+	}
+
+	matchingNamespaces := getMatchingNamespaces(*apiContext, namespaces)
+
+	if opt == nil {
+		opt = &types.QueryOptions{}
+	}
+
+	// It is important this field is set to not nil even if there are no namespaces, so that no namespaces are queried instead of all namespaces
+	if opt.Namespaces == nil {
+		opt.Namespaces = make([]string, 0)
+	}
+
+	// Not using namespaces in opt.Conditions to avoid conflicts
+	opt.Namespaces = append(opt.Namespaces, matchingNamespaces...)
+
+	return nil
+}
+
+func getMatchingNamespaces(apiContext types.APIContext, namespaces []*k8sv1.Namespace) []string {
+	var matchingNamespaces []string
+
+	projectID := apiContext.SubContext["/v3/schemas/project"]
+	if projectID == "" {
+		return matchingNamespaces
+	}
+
+	for _, ns := range namespaces {
+		if ns.Annotations[project.ProjectIDAnn] == projectID {
+			matchingNamespaces = append(matchingNamespaces, ns.Name)
+		}
+	}
+	return matchingNamespaces
 }
 
 func (t *transformer) stream(apiContext *types.APIContext, schema *types.Schema, data chan map[string]interface{}, opt *types.QueryOptions) (chan map[string]interface{}, error) {
