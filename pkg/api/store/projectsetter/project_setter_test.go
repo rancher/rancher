fae96b72e31deb1dbccf7d5fa352e1f23a3c2109
rmweir <ricardo.weir1@gmail.com>
2019-03-29 14:35:32 -0700

    Add tests for project setter
    Added tests for logic in project setter store.

---
+++ pkg/api/store/projectsetter/project_setter_test.go
@@ -0,0 +1,73 @@
+package projectsetter
+
+import (
+	"github.com/rancher/norman/types"
+	"github.com/rancher/rancher/pkg/project"
+	"k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"net/http"
+	"testing"
+)
+
+// Tests that getMatchingNamespaces returns the proper amount namespaces given duplicates, empty slices, and multiple
+// matching namespaces.
+func TestOptionsCorrectNamespaces(t *testing.T) {
+	dummyAPIContext := &types.APIContext{
+		Method:     http.MethodGet,
+		SubContext: map[string]string{"/v3/schemas/project": "p-test123"},
+	}
+
+	namespaceList := []*v1.Namespace{
+		{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:        "ns1",
+				Annotations: map[string]string{project.ProjectIDAnn: "p-test123"},
+			},
+		},
+		{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:        "ns2",
+				Annotations: map[string]string{project.ProjectIDAnn: "p-test1234"},
+			},
+		},
+	}
+
+	namespaces := getMatchingNamespaces(*dummyAPIContext, namespaceList)
+
+	if len(namespaces) == 0 {
+		t.Error("Matching namespace was not returned")
+	}
+
+	namespaceList = []*v1.Namespace{
+		{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:        "ns1",
+				Annotations: map[string]string{project.ProjectIDAnn: "p-test123"},
+			},
+		},
+		{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:        "ns2",
+				Annotations: map[string]string{project.ProjectIDAnn: "p-test1234"},
+			},
+		},
+		{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:        "ns3",
+				Annotations: map[string]string{project.ProjectIDAnn: "p-test123"},
+			},
+		},
+	}
+
+	namespaces = getMatchingNamespaces(*dummyAPIContext, namespaceList)
+	if len(namespaces) != 2 {
+		t.Error("Should be able to find multiple namespaces")
+	}
+
+	namespaceList = []*v1.Namespace{}
+	namespaces = getMatchingNamespaces(*dummyAPIContext, namespaceList)
+
+	if len(namespaces) > 0 {
+		t.Error("Namespaces should not be returned from empty namespace list")
+	}
+}
