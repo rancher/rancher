f8ca44d6e090cf55d2e9b314994d56e355ed0f7c
kinarashah <kinara@rancher.com>
2019-03-29 10:19:02 -0700

    remove validation for cloudcredential on nodetemplate create

--- pkg/api/customization/nodetemplate/validation.go
+++ pkg/api/customization/nodetemplate/validation.go
@@ -3,8 +3,6 @@ package nodetemplate
 import (
 	"github.com/rancher/norman/httperror"
 	"github.com/rancher/norman/types"
-	"github.com/rancher/norman/types/convert"
-	"github.com/rancher/norman/types/values"
 	"github.com/rancher/rancher/pkg/configfield"
 )
 
@@ -15,23 +13,10 @@ const (
 	DigitalOceandriver = "digitalocean"
 )
 
-var drivers = map[string]bool{
-	Amazonec2driver:    true,
-	Azuredriver:        true,
-	DigitalOceandriver: true,
-	Vmwaredriver:       true,
-}
-
 func Validator(request *types.APIContext, schema *types.Schema, data map[string]interface{}) error {
 	driver := configfield.GetDriver(data)
 	if driver == "" {
 		return httperror.NewAPIError(httperror.MissingRequired, "a Config field must be set")
-	}
-	credID := convert.ToString(values.GetValueN(data, "cloudCredentialId"))
-	if credID == "" {
-		if _, ok := drivers[driver]; ok {
-			return httperror.NewAPIError(httperror.MissingRequired, "Cloud Credential must be set")
-		}
 	}
 	if data != nil {
 		data["driver"] = driver
