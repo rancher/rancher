c919e0263a4c596d20100b22c15290ebcaca89bf
galal-hussein <hussein.galal.ahmed.11@gmail.com>
2019-04-17 02:01:23 +0200

    Add RBAC check for rotating cluster certificates

--- pkg/api/customization/cluster/actionhandler.go
+++ pkg/api/customization/cluster/actionhandler.go
@@ -47,6 +47,13 @@ type ActionHandler struct {
 }
 
 func (a ActionHandler) ClusterActionHandler(actionName string, action *types.Action, apiContext *types.APIContext) error {
+	canUpdateCluster := func() bool {
+		cluster := map[string]interface{}{
+			"id": apiContext.ID,
+		}
+
+		return apiContext.AccessControl.CanDo(v3.ClusterGroupVersionKind.Group, v3.ClusterResource.Name, "update", apiContext, cluster, apiContext.Schema) == nil
+	}
 	switch actionName {
 	case "generateKubeconfig":
 		return a.GenerateKubeconfigActionHandler(actionName, action, apiContext)
@@ -55,6 +62,9 @@ func (a ActionHandler) ClusterActionHandler(actionName string, action *types.Act
 	case "exportYaml":
 		return a.ExportYamlHandler(actionName, action, apiContext)
 	case "rotateCertificates":
+		if !canUpdateCluster() {
+			return httperror.NewAPIError(httperror.Unauthorized, "can not rotate certificates")
+		}
 		return a.RotateCertificates(actionName, action, apiContext)
 	}
 	return httperror.NewAPIError(httperror.NotFound, "not found")
