7df123b887d7e5ba90fdd5b4bf7039b350adcb1e
rmweir <ricardo.weir1@gmail.com>
2019-04-23 17:57:53 -0700

    Remove content-length from gzip responses (#19728)
    * Remove content-length from gzip responses
    
    Removed content-length header from all gzipped responses.
    Also, added gzip encoding check for node keys logic. Prior,
    gzipped responses could have content-length set prematurely.
    Nodekeys would overwrite content-length header. This caused
    an error and would prevent nodekeys response from being
    properly interpreted when using http2.

--- server/responsewriter/gzip.go
+++ server/responsewriter/gzip.go
@@ -26,6 +26,13 @@ func (g gzipResponseWriter) Write(b []byte) (int, error) {
 	return g.Writer.Write(b)
 }
 
+// should always be used when using gzip to overwrite outdated header info
+func (g gzipResponseWriter) WriteHeader(statusCode int) {
+	g.Header().Set("Content-Encoding", "gzip")
+	g.Header().Del("Content-Length")
+	g.ResponseWriter.WriteHeader(statusCode)
+}
+
 func Gzip(handler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if !strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
@@ -36,6 +43,7 @@ func Gzip(handler http.Handler) http.Handler {
 		defer gz.Close()
 
 		gzw := &wrapWriter{gzipResponseWriter{Writer: gz, ResponseWriter: w}, http.StatusOK}
+		// setting content-encoding is kept here in case the user does not use WriteHeader
 		gzw.Header().Set("Content-Encoding", "gzip")
 		handler.ServeHTTP(gzw, r)
 	})
