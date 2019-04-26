631456083ef2063d61474b02945e0f7b06f4b73c
rmweir <ricardo.weir1@gmail.com>
2019-04-26 16:47:30 -0700

    refmt, only run rbac tests

--- tests/integration/test_rbac.py
+++ tests/integration/test_rbac.py
@@ -20,7 +20,7 @@ def test_multi_user(admin_mc, user_mc):
     assert len(ac) == 0
 
 
-@pytest.mark.parametrize('params',range(100))
+@pytest.mark.parametrize('params', range(100))
 def test_project_owner(admin_cc, admin_mc, user_mc, params, remove_resource):
     """Tests that a non-admin member can create a project, create and
     add a namespace to it, and can do workload related things in the namespace.
@@ -234,8 +234,8 @@ def test_user_role_permissions(admin_mc, user_factory, remove_resource):
 
 
 @pytest.mark.parametrize('params', range(100))
-def test_impersonation_passthrough(admin_mc, admin_cc, params, user_mc, user_factory,
-                                   remove_resource, request):
+def test_impersonation_passthrough(admin_mc, admin_cc, params, user_mc,
+                                   user_factory, remove_resource, request):
     """Test users abalility to impersonate other users"""
     config = kubernetes.client.Configuration()
     config.verify_ssl = False
