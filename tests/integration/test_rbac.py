b7474a8514de574e4c08e03c052d1890e3311c82
rmweir <ricardo.weir1@gmail.com>
2019-04-26 17:30:12 -0700

    make tests non parallel

--- tests/integration/test_rbac.py
+++ tests/integration/test_rbac.py
@@ -20,7 +20,8 @@ def test_multi_user(admin_mc, user_mc):
     assert len(ac) == 0
 
 
-@pytest.mark.parametrize('params', range(100))
+# @pytest.mark.parametrize('params', range(100))
+@pytest.mark.nonparallel
 def test_project_owner(admin_cc, admin_mc, user_mc, params, remove_resource):
     """Tests that a non-admin member can create a project, create and
     add a namespace to it, and can do workload related things in the namespace.
@@ -235,7 +236,8 @@ def test_user_role_permissions(admin_mc, user_factory, remove_resource):
                                            "to view roleTemplates")
 
 
-@pytest.mark.parametrize('params', range(100))
+# @pytest.mark.parametrize('params', range(100))
+@pytest.mark.nonparallel
 def test_impersonation_passthrough(admin_mc, admin_cc, params, user_mc,
                                    user_factory, remove_resource, request):
     """Test users abalility to impersonate other users"""
