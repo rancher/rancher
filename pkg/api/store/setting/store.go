f162567a96727bfbfd58022ec3453ea0ee04d0ff
rmweir <ricardo.weir1@gmail.com>
2019-04-29 13:00:26 -0700

    Disable ability to delete readonly settings
    Users are no longer able to delete reaadonly settings through
    the settings api. Settings tests no longer attempt remove cacert
    setting, aside from test for deletion. Reenabled rbac tests.
    Prior, settings tests were deleting cacert setting causing some
    rbac tests to fail if they were executed after. Now, it is not
    possible to delete cacert or any other readonly setting, and
    settings test will not needlessly attempt to delete them.

--- pkg/api/store/setting/store.go
+++ pkg/api/store/setting/store.go
@@ -1,36 +1,54 @@
 package setting
 
 import (
+	"fmt"
 	"os"
 
+	"github.com/rancher/norman/httperror"
 	"github.com/rancher/norman/store/transform"
 	"github.com/rancher/norman/types"
 	"github.com/rancher/norman/types/convert"
+	"github.com/rancher/norman/types/slice"
+	"github.com/rancher/rancher/pkg/api/customization/setting"
 	"github.com/rancher/rancher/pkg/settings"
 )
 
-func New(store types.Store) types.Store {
-	return &transform.Store{
-		Store: store,
-		Transformer: func(apiContext *types.APIContext, schema *types.Schema, data map[string]interface{}, opt *types.QueryOptions) (map[string]interface{}, error) {
-			v, ok := data["value"]
-			id := convert.ToString(data["id"])
-			value, ok2 := os.LookupEnv(settings.GetEnvKey(id))
-			switch {
-			case ok2:
-				data["value"] = value
-				data["customized"] = false
-				data["source"] = "env"
+type Store struct {
+	types.Store
+}
 
-			case !ok || v == "":
-				data["value"] = data["default"]
-				data["customized"] = false
-				data["source"] = "default"
-			default:
-				data["customized"] = true
-				data["source"] = "db"
-			}
-			return data, nil
+func New(store types.Store) types.Store {
+	return &Store{
+		&transform.Store{
+			Store: store,
+			Transformer: func(apiContext *types.APIContext, schema *types.Schema, data map[string]interface{}, opt *types.QueryOptions) (map[string]interface{}, error) {
+				v, ok := data["value"]
+				id := convert.ToString(data["id"])
+				value, ok2 := os.LookupEnv(settings.GetEnvKey(id))
+				switch {
+				case ok2:
+					data["value"] = value
+					data["customized"] = false
+					data["source"] = "env"
+
+				case !ok || v == "":
+					data["value"] = data["default"]
+					data["customized"] = false
+					data["source"] = "default"
+				default:
+					data["customized"] = true
+					data["source"] = "db"
+				}
+				return data, nil
+			},
 		},
 	}
+}
+
+func (s *Store) Delete(apiContext *types.APIContext, schema *types.Schema, id string) (map[string]interface{}, error) {
+	if slice.ContainsString(setting.ReadOnlySettings, id) {
+		return nil, httperror.NewAPIError(httperror.MethodNotAllowed, fmt.Sprintf("Cannot delete readOnly setting %s.", id))
+	}
+
+	return s.Store.Delete(apiContext, schema, id)
 }
