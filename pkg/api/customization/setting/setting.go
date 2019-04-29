f697d668727603c70d61b768f3ca50e34110c024
rmweir <ricardo.weir1@gmail.com>
2019-04-29 13:00:26 -0700

    Disable ability to delete readonly settings
    Users are no longer able to delete reaadonly settings through
    the settings api. Settings tests no longer attempt remove cacert
    setting, aside from test for deletion. Reenabled rbac tests.
    Prior, settings tests were deleting cacert setting causing some
    rbac tests to fail if they were executed after. Now, it is not
    possible to delete cacert or any other readonly setting, and
    settings test will not needlessly attempt to delete them.

--- pkg/api/customization/setting/setting.go
+++ pkg/api/customization/setting/setting.go
@@ -12,7 +12,7 @@ import (
 	v3client "github.com/rancher/types/client/management/v3"
 )
 
-var readOnlySettings = []string{
+var ReadOnlySettings = []string{
 	"cacerts",
 }
 
@@ -29,7 +29,7 @@ func PipelineFormatter(apiContext *types.APIContext, resource *types.RawResource
 func Formatter(apiContext *types.APIContext, resource *types.RawResource) {
 	if convert.ToString(resource.Values["source"]) == "env" {
 		delete(resource.Links, "update")
-	} else if slice.ContainsString(readOnlySettings, resource.ID) {
+	} else if slice.ContainsString(ReadOnlySettings, resource.ID) {
 		delete(resource.Links, "update")
 	}
 }
@@ -50,7 +50,7 @@ func Validator(request *types.APIContext, schema *types.Schema, data map[string]
 	}
 	if setting.Source == "env" {
 		return httperror.NewAPIError(httperror.MethodNotAllowed, fmt.Sprintf("%s is readOnly because its value is from environment variable", id))
-	} else if slice.ContainsString(readOnlySettings, id) {
+	} else if slice.ContainsString(ReadOnlySettings, id) {
 		return httperror.NewAPIError(httperror.MethodNotAllowed, fmt.Sprintf("%s is readOnly", id))
 	}
 
