f77fb8a91816e2b5aada6dace58b9f0ba1baf8a7
orangedeng <jxfa0043379@hotmail.com>
2019-05-10 10:23:47 +0800

    Clean cattle-prometheus ns

--- pkg/agent/clean/clean.go
+++ pkg/agent/clean/clean.go
@@ -93,7 +93,7 @@ func Cluster() error {
 	}
 
 	var errors []error
-	var toRemove []string
+	var toRemove = make([]string, len(nsToRemove))
 	copy(toRemove, nsToRemove)
 
 	for _, f := range getNSFuncs {
