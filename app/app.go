3129f0403be3e8c8b4859e734e0dbece2c5baff1
rmweir <ricardo.weir1@gmail.com>
2019-05-10 11:48:56 -0700

    Add catalog url and branch upgrade logic
    Added logic for maintaining default url/branch for system-library
    catalog, and its annotation. Prior, older versions of rancher could
    potentially use an incompatible version of system-charts by
    default. Now, system-charts default info will be dependent on the
    CATTLE_SYSTEM_CHART_DEFAULT_URL and CATTLE_SYSTEM_CHART_DEFAULT
    _BRANCH environment variables.

--- app/app.go
+++ app/app.go
@@ -175,7 +175,7 @@ func addData(management *config.ManagementContext, cfg Config) error {
 		return err
 	}
 
-	if err := addCatalogs(management); err != nil {
+	if err := syncCatalogs(management); err != nil {
 		return err
 	}
 
