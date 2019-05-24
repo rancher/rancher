53b4452e4f67499a2a5413edbd8c29bdc558e80c
Alena Prokharchyk <alena@rancher.com>
2019-05-24 11:05:33 -0700

    types and rke vendor update

--- vendor/github.com/rancher/rke/cluster/certificates.go
+++ vendor/github.com/rancher/rke/cluster/certificates.go
@@ -17,7 +17,7 @@ import (
 
 func SetUpAuthentication(ctx context.Context, kubeCluster, currentCluster *Cluster, fullState *FullState) error {
 	if kubeCluster.AuthnStrategies[AuthnX509Provider] {
-		compareKubeAPICerts(ctx, kubeCluster, currentCluster)
+		compareCerts(ctx, kubeCluster, currentCluster)
 		kubeCluster.Certificates = fullState.DesiredState.CertificatesBundle
 		return nil
 	}
@@ -225,15 +225,21 @@ func GetClusterCertsFromNodes(ctx context.Context, kubeCluster *Cluster) (map[st
 	return nil, err
 }
 
-func compareKubeAPICerts(ctx context.Context, kubeCluster, currentCluster *Cluster) {
-	// checking if kubeapi cert got changed then we set force deploy to true
-	// to force deploying the kubeapi cert with new SANs
+func compareCerts(ctx context.Context, kubeCluster, currentCluster *Cluster) {
+	// check if relevant certs were changed and if so set force deploy to true
+	// to deploy certs with new SANs
 	if currentCluster != nil {
-		currentKubeAPICert := currentCluster.Certificates[pki.KubeAPICertName]
-		desiredKubeAPICert := kubeCluster.Certificates[pki.KubeAPICertName]
-		if desiredKubeAPICert.CertificatePEM != currentKubeAPICert.CertificatePEM {
-			log.Infof(ctx, "[certificates] KubeAPI certificate changed, force deploying certs")
-			kubeCluster.ForceDeployCerts = true
+		for _, certName := range []string{
+			pki.KubeAPICertName,
+			pki.EtcdCertName,
+		} {
+			currentCert := currentCluster.Certificates[certName]
+			desiredCert := kubeCluster.Certificates[certName]
+			if desiredCert.CertificatePEM != currentCert.CertificatePEM {
+				log.Infof(ctx, "[certificates] %s certificate changed, force deploying certs", certName)
+				kubeCluster.ForceDeployCerts = true
+				return
+			}
 		}
 	}
 }
