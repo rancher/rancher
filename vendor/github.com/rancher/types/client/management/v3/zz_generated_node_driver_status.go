2a04a5d25d9e47b71731008285140347e29b77d3
Dan Ramich <danold215@gmail.com>
2019-05-30 16:25:28 -0700

    Vendor changes

--- vendor/github.com/rancher/types/client/management/v3/zz_generated_node_driver_status.go
+++ vendor/github.com/rancher/types/client/management/v3/zz_generated_node_driver_status.go
@@ -1,10 +1,16 @@
 package client
 
 const (
-	NodeDriverStatusType            = "nodeDriverStatus"
-	NodeDriverStatusFieldConditions = "conditions"
+	NodeDriverStatusType                             = "nodeDriverStatus"
+	NodeDriverStatusFieldAppliedChecksum             = "appliedChecksum"
+	NodeDriverStatusFieldAppliedDockerMachineVersion = "appliedDockerMachineVersion"
+	NodeDriverStatusFieldAppliedURL                  = "appliedURL"
+	NodeDriverStatusFieldConditions                  = "conditions"
 )
 
 type NodeDriverStatus struct {
-	Conditions []Condition `json:"conditions,omitempty" yaml:"conditions,omitempty"`
+	AppliedChecksum             string      `json:"appliedChecksum,omitempty" yaml:"appliedChecksum,omitempty"`
+	AppliedDockerMachineVersion string      `json:"appliedDockerMachineVersion,omitempty" yaml:"appliedDockerMachineVersion,omitempty"`
+	AppliedURL                  string      `json:"appliedURL,omitempty" yaml:"appliedURL,omitempty"`
+	Conditions                  []Condition `json:"conditions,omitempty" yaml:"conditions,omitempty"`
 }
