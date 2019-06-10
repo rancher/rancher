14b5922c793cb825f45e8d09f25d899fc8641135
Dan Ramich <danold215@gmail.com>
2019-06-10 10:19:07 -0700

    Add userDataFile for openstack nodedriver

--- pkg/controllers/management/drivers/nodedriver/machine_driver.go
+++ pkg/controllers/management/drivers/nodedriver/machine_driver.go
@@ -34,7 +34,7 @@ var (
 		"azure":        map[string]string{"customData": "customData"},
 		"digitalocean": map[string]string{"sshKeyPath": "sshKeyContents", "userdata": "userdata"},
 		"exoscale":     map[string]string{"sshKey": "sshKey", "userdata": "userdata"},
-		"openstack":    map[string]string{"privateKeyFile": "privateKeyFile"},
+		"openstack":    map[string]string{"privateKeyFile": "privateKeyFile", "userDataFile": "userDataFile"},
 		"otc":          map[string]string{"privateKeyFile": "privateKeyFile"},
 		"packet":       map[string]string{"userdata": "userdata"},
 	}
