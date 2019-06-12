b653f371370947ff9908870b402d73d35e4d59e0
JacieChao <kathyyy@163.com>
2019-06-12 17:06:14 +0800

    Add Baidu CCE driver

--- tests/integration/suite/test_rbac.py
+++ tests/integration/suite/test_rbac.py
@@ -381,14 +381,14 @@ def ns_count(client, count):
 
 def test_appropriate_users_can_see_kontainer_drivers(user_factory):
     kds = user_factory().client.list_kontainer_driver()
-    assert len(kds) == 7
+    assert len(kds) == 8
 
     kds = user_factory('clusters-create').client.list_kontainer_driver()
-    assert len(kds) == 7
+    assert len(kds) == 8
 
     kds = user_factory('kontainerdrivers-manage').client. \
         list_kontainer_driver()
-    assert len(kds) == 7
+    assert len(kds) == 8
 
     kds = user_factory('settings-manage').client.list_kontainer_driver()
     assert len(kds) == 0
