78af6d6b078020bbbe0468f01c5d45c4e27f66c5
Dan Ramich <danold215@gmail.com>
2019-06-19 11:18:15 -0700

    Update jailer script

--- package/jailer.sh
+++ package/jailer.sh
@@ -21,9 +21,12 @@ mkdir -p /opt/jail/$NAME/tmp
 # Copy over required files to the jail
 cp -r /lib /opt/jail/$NAME
 cp -r /lib64 /opt/jail/$NAME
+cp -r /usr/lib /opt/jail/$NAME/usr
 cp /etc/ssl/certs/ca-certificates.crt /opt/jail/$NAME/etc/ssl/certs
 cp /etc/resolv.conf /opt/jail/$NAME/etc/
+cp /etc/passwd /opt/jail/$NAME/etc/
 cp /etc/hosts /opt/jail/$NAME/etc/
+cp /etc/nsswitch.conf /opt/jail/$NAME/etc/
 
 if [ -d /var/lib/rancher/management-state/bin ] && [ "$(ls -A /var/lib/rancher/management-state/bin)" ]; then
   ( cd /var/lib/rancher/management-state/bin
@@ -44,8 +47,11 @@ cp -l /usr/bin/docker-machine /opt/jail/$NAME/usr/bin
 # Hard link helm into the jail 
 cp -l /usr/bin/helm /opt/jail/$NAME/usr/bin
 
+# Hard link ssh into the jail
+cp -l /usr/bin/ssh /opt/jail/$NAME/usr/bin
+
 cd /dev
 # tar copy /dev excluding mqueue and shm
-tar cf - --exclude=mqueue --exclude=shm . | (cd /opt/jail/${NAME}/dev; tar xfp -)
+tar cf - --exclude=mqueue --exclude=shm --exclude=pts . | (cd /opt/jail/${NAME}/dev; tar xfp -)
 
 touch /opt/jail/$NAME/done
