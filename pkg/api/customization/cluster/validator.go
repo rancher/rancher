e6755155b10337c7e278f577169fb4a7095ba98e
Prachi Damle <prachi@rancher.com>
2019-06-19 12:19:20 -0700

    Cluster Template Enforcement

--- pkg/api/customization/cluster/validator.go
+++ pkg/api/customization/cluster/validator.go
@@ -1,21 +1,40 @@
 package cluster
 
 import (
+	"strings"
+
+	"fmt"
+
 	"github.com/rancher/norman/httperror"
 	"github.com/rancher/norman/types"
 	"github.com/rancher/norman/types/convert"
+	gaccess "github.com/rancher/rancher/pkg/api/customization/globalnamespaceaccess"
+	"github.com/rancher/rancher/pkg/namespace"
+	"github.com/rancher/rancher/pkg/settings"
 	v3 "github.com/rancher/types/apis/management.cattle.io/v3"
+	mgmtclient "github.com/rancher/types/client/management/v3"
 )
 
 type Validator struct {
-	ClusterLister v3.ClusterLister
+	ClusterLister                 v3.ClusterLister
+	ClusterTemplateLister         v3.ClusterTemplateLister
+	ClusterTemplateRevisionLister v3.ClusterTemplateRevisionLister
+	Users                         v3.UserInterface
+	GrbLister                     v3.GlobalRoleBindingLister
+	GrLister                      v3.GlobalRoleLister
 }
 
 func (v *Validator) Validator(request *types.APIContext, schema *types.Schema, data map[string]interface{}) error {
 	var spec v3.ClusterSpec
+
 	if err := convert.ToObj(data, &spec); err != nil {
 		return httperror.WrapAPIError(err, httperror.InvalidBodyContent, "Cluster spec conversion error")
 	}
+
+	if err := v.validateEnforcement(request, data); err != nil {
+		return err
+	}
+
 	if err := v.validateLocalClusterAuthEndpoint(request, &spec); err != nil {
 		return err
 	}
@@ -48,4 +67,95 @@ func (v *Validator) validateLocalClusterAuthEndpoint(request *types.APIContext, 
 	}
 
 	return nil
+}
+
+func (v *Validator) validateEnforcement(request *types.APIContext, data map[string]interface{}) error {
+
+	if !strings.EqualFold(settings.ClusterTemplateEnforcement.Get(), "true") {
+		return nil
+	}
+
+	var spec mgmtclient.Cluster
+	if err := convert.ToObj(data, &spec); err != nil {
+		return httperror.WrapAPIError(err, httperror.InvalidBodyContent, "Cluster spec conversion error")
+	}
+
+	if !v.checkClusterForEnforcement(&spec) {
+		return nil
+	}
+
+	ma := gaccess.MemberAccess{
+		Users:     v.Users,
+		GrLister:  v.GrLister,
+		GrbLister: v.GrbLister,
+	}
+
+	//if user is admin, no checks needed
+	callerID := request.Request.Header.Get(gaccess.ImpersonateUserHeader)
+
+	isAdmin, err := ma.IsAdmin(callerID)
+	if err != nil {
+		return err
+	}
+	if isAdmin {
+		return nil
+	}
+
+	//enforcement is true, template is a must
+	if spec.ClusterTemplateID == "" || spec.ClusterTemplateRevisionID == "" {
+		return httperror.NewFieldAPIError(httperror.MissingRequired, "", "A clusterTemplate is required to create a cluster")
+	}
+
+	enforced, err := v.isTemplateEnforced(&spec)
+	if err != nil {
+		return err
+	}
+
+	if !enforced {
+		return httperror.NewFieldAPIError(httperror.PermissionDenied, "", fmt.Sprintf("%v is not a valid clusterTemplate to create a cluster", spec.ClusterTemplateID))
+	}
+
+	return nil
+}
+
+func (v *Validator) checkClusterForEnforcement(spec *mgmtclient.Cluster) bool {
+	if spec.RancherKubernetesEngineConfig != nil {
+		return true
+	}
+
+	if spec.ClusterTemplateID != "" || spec.ClusterTemplateRevisionID != "" {
+		return true
+	}
+	return false
+}
+
+func (v *Validator) isTemplateEnforced(spec *mgmtclient.Cluster) (bool, error) {
+	//template must be marked as enforced=true  + must be public
+
+	split := strings.SplitN(spec.ClusterTemplateID, ":", 2)
+	if len(split) != 2 {
+		return false, fmt.Errorf("error in splitting clusterTemplate name %v", spec.ClusterTemplateID)
+	}
+	templateName := split[1]
+
+	clusterTemp, err := v.ClusterTemplateLister.Get(namespace.GlobalNamespace, templateName)
+	if err != nil {
+		return false, err
+	}
+	if !clusterTemp.Spec.Enforced {
+		return false, nil
+	}
+
+	//make sure it is public
+	var isPublic bool
+	for _, member := range clusterTemp.Spec.Members {
+		if strings.EqualFold(member.GroupPrincipalName, "*") {
+			isPublic = true
+			break
+		}
+	}
+	if !isPublic {
+		return false, nil
+	}
+	return true, nil
 }
