2322702b01c1a8e564a3664105c4fc9b8535c281
Aiwantaozi <michelia.feng@gmail.com>
2019-07-09 18:17:24 +0800

    fix upgrade system image failed when manage multiple clusters
    Problem:
    only one cluster system server upgrade success when there are multiple
    cluster
    
    Solution:
    this cause by only one alert/logging/pipeline upgradeService was init,
    when there are multiple cluster, the former clusterName will be
    overwrite by the laster one. Now change handle multiple cluster when
    init upgradeService
    
    Issue:
    https://github.com/rancher/rancher/issues/21364

--- pkg/controllers/user/logging/deployer/upgradeimpl.go
+++ pkg/controllers/user/logging/deployer/upgradeimpl.go
@@ -1,14 +1,13 @@
 package deployer
 
 import (
-	"context"
 	"fmt"
 	"reflect"
 	"strings"
+	"time"
 
 	"github.com/rancher/rancher/pkg/controllers/user/helm/common"
 	loggingconfig "github.com/rancher/rancher/pkg/controllers/user/logging/config"
-	"github.com/rancher/rancher/pkg/controllers/user/systemimage"
 	"github.com/rancher/rancher/pkg/project"
 	"github.com/rancher/rancher/pkg/settings"
 	appsv1beta2 "github.com/rancher/types/apis/apps/v1beta2"
@@ -24,11 +23,14 @@ import (
 )
 
 var (
-	serviceName = "logging"
+	ServiceName             = "logging"
+	waitCatalogSyncInterval = 60 * time.Second
 )
 
-type loggingService struct {
+type LoggingService struct {
 	clusterName    string
+	clusterLister  v3.ClusterLister
+	catalogLister  v3.CatalogLister
 	projectLister  v3.ProjectLister
 	templateLister v3.CatalogTemplateLister
 	daemonsets     appsv1beta2.DaemonSetInterface
@@ -36,25 +38,27 @@ type loggingService struct {
 	appDeployer    *AppDeployer
 }
 
-func init() {
-	systemimage.RegisterSystemService(serviceName, &loggingService{})
+func NewService() *LoggingService {
+	return &LoggingService{}
 }
 
-func (l *loggingService) Init(ctx context.Context, cluster *config.UserContext) {
+func (l *LoggingService) Init(cluster *config.UserContext) {
 	ad := &AppDeployer{
 		AppsGetter: cluster.Management.Project,
 		Namespaces: cluster.Core.Namespaces(metav1.NamespaceAll),
 	}
 
 	l.clusterName = cluster.ClusterName
-	l.projectLister = cluster.Management.Management.Projects(metav1.NamespaceAll).Controller().Lister()
+	l.clusterLister = cluster.Management.Management.Clusters("").Controller().Lister()
+	l.catalogLister = cluster.Management.Management.Catalogs(metav1.NamespaceAll).Controller().Lister()
+	l.projectLister = cluster.Management.Management.Projects(cluster.ClusterName).Controller().Lister()
 	l.templateLister = cluster.Management.Management.CatalogTemplates(metav1.NamespaceAll).Controller().Lister()
 	l.daemonsets = cluster.Apps.DaemonSets(loggingconfig.LoggingNamespace)
 	l.secrets = cluster.Core.Secrets(loggingconfig.LoggingNamespace)
 	l.appDeployer = ad
 }
 
-func (l *loggingService) Version() (string, error) {
+func (l *LoggingService) Version() (string, error) {
 	catalogID := settings.SystemLoggingCatalogID.Get()
 	templateVersionID, _, err := common.ParseExternalID(catalogID)
 	if err != nil {
@@ -63,7 +67,7 @@ func (l *loggingService) Version() (string, error) {
 	return templateVersionID, nil
 }
 
-func (l *loggingService) Upgrade(currentVersion string) (string, error) {
+func (l *LoggingService) Upgrade(currentVersion string) (string, error) {
 	appName := loggingconfig.AppName
 	templateID := loggingconfig.RancherLoggingTemplateID()
 	template, err := l.templateLister.Get(namespace.GlobalNamespace, templateID)
@@ -74,6 +78,15 @@ func (l *loggingService) Upgrade(currentVersion string) (string, error) {
 	newFullVersion := fmt.Sprintf("%s-%s", templateID, template.Spec.DefaultVersion)
 	if currentVersion == newFullVersion {
 		return currentVersion, nil
+	}
+
+	// check cluster ready before upgrade, because helm will not retry if got cluster not ready error
+	cluster, err := l.clusterLister.Get(metav1.NamespaceAll, l.clusterName)
+	if err != nil {
+		return "", fmt.Errorf("get cluster %s failed, %v", l.clusterName, err)
+	}
+	if !v3.ClusterConditionReady.IsTrue(cluster) {
+		return "", fmt.Errorf("cluster %v not ready", l.clusterName)
 	}
 
 	//clean old version
@@ -107,6 +120,21 @@ func (l *loggingService) Upgrade(currentVersion string) (string, error) {
 		}
 		return "", errors.Wrapf(err, "get app %s:%s failed", systemProject.Name, appName)
 	}
+
+	_, systemCatalogName, _, _, _, err := common.SplitExternalID(newCatalogID)
+	if err != nil {
+		return "", err
+	}
+
+	systemCatalog, err := l.catalogLister.Get(metav1.NamespaceAll, systemCatalogName)
+	if err != nil {
+		return "", fmt.Errorf("get catalog %s failed, %v", systemCatalogName, err)
+	}
+
+	if !v3.CatalogConditionUpgraded.IsTrue(systemCatalog) || !v3.CatalogConditionRefreshed.IsTrue(systemCatalog) || !v3.CatalogConditionDiskCached.IsTrue(systemCatalog) {
+		return "", fmt.Errorf("catalog %v not ready", systemCatalogName)
+	}
+
 	newApp := app.DeepCopy()
 	newApp.Spec.ExternalID = newCatalogID
 
@@ -118,7 +146,7 @@ func (l *loggingService) Upgrade(currentVersion string) (string, error) {
 	return newFullVersion, nil
 }
 
-func (l *loggingService) removeLegacy() error {
+func (l *LoggingService) removeLegacy() error {
 	op := metav1.DeletePropagationBackground
 	errMsg := "failed to remove legacy logging %s %s:%s when upgrade"
 
