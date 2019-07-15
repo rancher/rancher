b3eb358119a4666f0810fa0eb012c4952d143288
Prachi Damle <prachi@rancher.com>
2019-07-15 16:23:28 -0700

    DeriveKubernetesVersion from the generic major.minor.patch

--- pkg/api/customization/clustertemplate/utils.go
+++ pkg/api/customization/clustertemplate/utils.go
@@ -9,23 +9,31 @@ import (
 	"github.com/rancher/norman/httperror"
 )
 
-func CheckKubernetesVersionFormat(k8sVersion string) error {
+const (
+	RKEConfigK8sVersion = "rancherKubernetesEngineConfig.kubernetesVersion"
+)
+
+func CheckKubernetesVersionFormat(k8sVersion string) (bool, error) {
 	if !strings.Contains(k8sVersion, "-rancher") {
 		errMsg := fmt.Sprintf("Requested kubernetesVersion %v is not of valid semver [major.minor.patch] format", k8sVersion)
 		vparts := strings.Split(k8sVersion, ".")
 		if len(vparts) != 3 {
-			return httperror.NewAPIError(httperror.InvalidFormat, errMsg)
+			return false, httperror.NewAPIError(httperror.InvalidFormat, errMsg)
 		}
 		for _, part := range vparts[:2] {
 			//part should be a numeric value
 			if _, err := strconv.Atoi(part); err != nil {
-				return httperror.NewAPIError(httperror.InvalidFormat, errMsg)
+				return false, httperror.NewAPIError(httperror.InvalidFormat, errMsg)
 			}
 		}
 		_, err := semver.ParseRange("=" + k8sVersion)
 		if err != nil {
-			return httperror.NewAPIError(httperror.InvalidFormat, errMsg)
+			return false, httperror.NewAPIError(httperror.InvalidFormat, errMsg)
+		}
+
+		if vparts[2] == "x" {
+			return true, nil
 		}
 	}
-	return nil
+	return false, nil
 }
