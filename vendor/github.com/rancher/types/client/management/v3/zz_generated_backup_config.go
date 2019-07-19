4aa4e1ab39fa9364bc467d563b8af54eab97fd40
moelsayed <m.elsayed@gmail.com>
2019-07-19 03:17:30 +0200

    vendor update

--- vendor/github.com/rancher/types/client/management/v3/zz_generated_backup_config.go
+++ vendor/github.com/rancher/types/client/management/v3/zz_generated_backup_config.go
@@ -6,7 +6,6 @@ const (
 	BackupConfigFieldIntervalHours  = "intervalHours"
 	BackupConfigFieldRetention      = "retention"
 	BackupConfigFieldS3BackupConfig = "s3BackupConfig"
-	BackupConfigFieldTimestamp      = "timestamp"
 )
 
 type BackupConfig struct {
@@ -14,5 +13,4 @@ type BackupConfig struct {
 	IntervalHours  int64           `json:"intervalHours,omitempty" yaml:"intervalHours,omitempty"`
 	Retention      int64           `json:"retention,omitempty" yaml:"retention,omitempty"`
 	S3BackupConfig *S3BackupConfig `json:"s3BackupConfig,omitempty" yaml:"s3BackupConfig,omitempty"`
-	Timestamp      bool            `json:"timestamp,omitempty" yaml:"timestamp,omitempty"`
 }
