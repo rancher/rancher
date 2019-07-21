83aeabe112a1d16e86e8a69f7fd202f8c68d8125
Sebastiaan van Steenis <mail@superseb.nl>
2019-07-21 22:12:39 +0200

    Add certificate check and detail when agent starts

--- pkg/agent/main.go
+++ pkg/agent/main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"context"
+	"crypto/tls"
+	"crypto/x509"
 	"encoding/base64"
 	"encoding/json"
+	"encoding/pem"
 	"fmt"
-	"log"
+	"io/ioutil"
 	"net/http"
 	"net/url"
 	"os"
@@ -50,7 +53,7 @@ func main() {
 	}
 
 	if err != nil {
-		log.Fatal(err)
+		logrus.Fatal(err)
 	}
 }
 
@@ -162,6 +165,100 @@ func run() error {
 		return err
 	}
 
+	// Check if secure connection can be made successfully
+	var httpClient = &http.Client{
+		Timeout: time.Second * 5,
+	}
+
+	_, err = httpClient.Get(server)
+	if err != nil {
+		if strings.Contains(err.Error(), "x509:") {
+			certErr := err
+			if strings.Contains(err.Error(), "certificate signed by unknown authority") {
+				certErr = fmt.Errorf("Certificate chain is not complete, please check if all needed intermediate certificates are included in the server certificate (in the correct order) and if the cacerts setting in Rancher either contains the correct CA certificate (in the case of using self signed certificates) or is empty (in the case of using a certificate signed by a recognized CA). Certificate information is displayed above. error: %s", err)
+			}
+			if strings.Contains(err.Error(), "certificate has expired or is not yet valid") {
+				certErr = fmt.Errorf("Server certificate is not valid, please check if the host has the correct time configured and if the server certificate has a notAfter date and time in the future. Certificate information is displayed above. error: %s", err)
+			}
+			if strings.Contains(err.Error(), "because it doesn't contain any IP SANs") || strings.Contains(err.Error(), "certificate is not valid for any names, but wanted to match") || strings.Contains(err.Error(), "cannot validate certificate for") {
+				certErr = fmt.Errorf("Server certificate does not contain correct DNS and/or IP address entries in the Subject Alternative Names (SAN). Certificate information is displayed above. error: %s", err)
+			}
+			insecureClient := &http.Client{
+				Timeout: time.Second * 5,
+				Transport: &http.Transport{
+					TLSClientConfig: &tls.Config{
+						InsecureSkipVerify: true,
+					},
+				},
+			}
+			res, err := insecureClient.Get(server)
+			if err != nil {
+				logrus.Errorf("Could not connect to %s: %v", server, err)
+				os.Exit(1)
+			}
+			var lastFoundIssuer string
+			if res.TLS != nil && len(res.TLS.PeerCertificates) > 0 {
+				logrus.Infof("Certificate details from %s", serverURL)
+				var previouscert *x509.Certificate
+				for i := range res.TLS.PeerCertificates {
+					cert := res.TLS.PeerCertificates[i]
+					logrus.Infof("Certificate #%d (%s)", i, serverURL)
+					certinfo(cert)
+					if i > 0 {
+						if previouscert.Issuer.String() != cert.Subject.String() {
+							logrus.Errorf("Certficate's Subject (%s) does not match with previous certificate Issuer (%s). Please check if the configured server certificate contains all needed intermediate certificates and make sure they are in the correct order (server certificate first, intermediates after)", cert.Subject.String(), previouscert.Issuer.String())
+						}
+					}
+					previouscert = cert
+					lastFoundIssuer = cert.Issuer.String()
+				}
+			}
+			caFileLocation := "/etc/kubernetes/ssl/certs/serverca"
+			if _, err := os.Stat(caFileLocation); err == nil {
+				caFile, err := ioutil.ReadFile(caFileLocation)
+				if err != nil {
+					return err
+				}
+				var blocks [][]byte
+				for {
+					var certDERBlock *pem.Block
+					certDERBlock, caFile = pem.Decode(caFile)
+					if certDERBlock == nil {
+						break
+					}
+
+					if certDERBlock.Type == "CERTIFICATE" {
+						blocks = append(blocks, certDERBlock.Bytes)
+					}
+				}
+				if len(blocks) > 1 {
+					logrus.Warnf("Found %d certificates at %s, should be 1", len(blocks), caFileLocation)
+				}
+				logrus.Infof("Certificate details for %s", caFileLocation)
+
+				blockcount := 0
+				var lastCACert *x509.Certificate
+				for _, block := range blocks {
+					cert, err := x509.ParseCertificate(block)
+					if err != nil {
+						logrus.Println(err)
+						continue
+					}
+
+					logrus.Infof("Certificate #%d (%s)", blockcount, caFileLocation)
+					certinfo(cert)
+
+					blockcount = blockcount + 1
+					lastCACert = cert
+				}
+				if lastFoundIssuer != lastCACert.Issuer.String() {
+					logrus.Errorf("Issuer of last certificate found in chain (%s) does not match with CA certificate Issuer (%s). Please check if the configured server certificate contains all needed intermediate certificates and make sure they are in the correct order (server certificate first, intermediates after)", lastFoundIssuer, lastCACert.Issuer.String())
+				}
+			}
+			return certErr
+		}
+	}
+
 	onConnect := func(ctx context.Context) error {
 		connected()
 		connectConfig := fmt.Sprintf("https://%s/v3/connect/config", serverURL.Host)
@@ -218,4 +315,24 @@ func run() error {
 		}, onConnect)
 		time.Sleep(5 * time.Second)
 	}
+}
+
+func certinfo(cert *x509.Certificate) {
+	logrus.Infof("Subject: %+v", cert.Subject)
+	logrus.Infof("Issuer: %+v", cert.Issuer)
+	logrus.Infof("IsCA: %+v", cert.IsCA)
+	if len(cert.DNSNames) > 0 {
+		logrus.Infof("DNS Names: %+v", cert.DNSNames)
+	} else {
+		logrus.Infof("DNS Names: <none>")
+	}
+	if len(cert.IPAddresses) > 0 {
+		logrus.Infof("IPAddresses: %+v", cert.IPAddresses)
+	} else {
+		logrus.Info("IPAddresses: <none>")
+	}
+	logrus.Infof("NotBefore: %+v", cert.NotBefore)
+	logrus.Infof("NotAfter: %+v", cert.NotAfter)
+	logrus.Infof("SignatureAlgorithm: %+v", cert.SignatureAlgorithm)
+	logrus.Infof("PublicKeyAlgorithm: %+v", cert.PublicKeyAlgorithm)
 }
