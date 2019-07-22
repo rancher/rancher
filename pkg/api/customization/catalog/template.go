a943e44b24f62c80b475bbdbae82cb8973bf8e06
Dax McDonald <dax@rancher.com>
2019-07-22 11:57:48 -0700

    Modify template formatter to not use proxy
    Rancher should not proxy out requests to get
    icons if they are publicly availible. Rancher will
    still host icons that are provided with the chart
    though.

--- pkg/api/customization/catalog/template.go
+++ pkg/api/customization/catalog/template.go
@@ -21,8 +21,20 @@ func TemplateFormatter(apiContext *types.APIContext, resource *types.RawResource
 	resource.Values["versionLinks"] = extractVersionLinks(apiContext, resource)
 
 	//icon
-	delete(resource.Values, "icon")
-	resource.Links["icon"] = apiContext.URLBuilder.Link("icon", resource)
+	ic, ok := resource.Values["icon"]
+	if ok {
+		if strings.HasPrefix(ic.(string), "file:") {
+			delete(resource.Values, "icon")
+			resource.Links["icon"] = apiContext.URLBuilder.Link("icon", resource)
+
+		} else {
+			delete(resource.Values, "icon")
+			resource.Links["icon"] = ic.(string)
+		}
+	} else {
+		delete(resource.Values, "icon")
+		resource.Links["icon"] = apiContext.URLBuilder.Link("icon", resource)
+	}
 
 	val := resource.Values
 	if val[client.CatalogTemplateFieldCatalogID] != nil {
@@ -118,6 +130,9 @@ func (t TemplateWrapper) TemplateIconHandler(apiContext *types.APIContext, next 
 
 		iconReader := bytes.NewReader(iconBytes)
 		apiContext.Response.Header().Set("Cache-Control", "private, max-age=604800")
+		// add security headers (similar to raw.githubusercontent)
+		apiContext.Response.Header().Set("Content-Security-Policy", "default-src 'none'; style-src 'unsafe-inline'; sandbox")
+		apiContext.Response.Header().Set("X-Content-Type-Options", "nosniff")
 		http.ServeContent(apiContext.Response, apiContext.Request, template.IconFilename, t, iconReader)
 		return nil
 	default:
