75fc7ee373b4e1b6e666dee02119e6c9172be213
Guangbo Chen <steven.guangbo.chen@gmail.com>
2019-07-27 07:21:51 +0800

    Update vendor

--- vendor/github.com/rancher/types/client/management/v3/zz_generated_monitoring_output.go
+++ vendor/github.com/rancher/types/client/management/v3/zz_generated_monitoring_output.go
@@ -3,8 +3,10 @@ package client
 const (
 	MonitoringOutputType         = "monitoringOutput"
 	MonitoringOutputFieldAnswers = "answers"
+	MonitoringOutputFieldVersion = "version"
 )
 
 type MonitoringOutput struct {
 	Answers map[string]string `json:"answers,omitempty" yaml:"answers,omitempty"`
+	Version string            `json:"version,omitempty" yaml:"version,omitempty"`
 }
