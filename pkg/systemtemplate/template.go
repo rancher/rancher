f217c09ee119a7521437f2381b9139c782022413
galal-hussein <hussein.galal.ahmed.11@gmail.com>
2019-08-02 01:35:35 +0200

    Add kubeapiserver proxy template

--- pkg/systemtemplate/template.go
+++ pkg/systemtemplate/template.go
@@ -2,7 +2,30 @@ package systemtemplate
 
 var templateSource = `
 ---
-
+apiVersion: rbac.authorization.k8s.io/v1
+kind: ClusterRole
+metadata:
+  name: proxy-clusterrole-kubeapiserver
+rules:
+- apiGroups: [""]
+  resources:
+  - nodes/metrics
+  - nodes/proxy
+  verbs: ["get", "list", "watch", "create"]
+---
+apiVersion: rbac.authorization.k8s.io/v1
+kind: ClusterRoleBinding
+metadata:
+  name: proxy-role-binding-kubernetes-master
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: ClusterRole
+  name: proxy-clusterrole-kubeapiserver
+subjects:
+- apiGroup: rbac.authorization.k8s.io
+  kind: User
+  name: kube-apiserver
+---
 apiVersion: v1
 kind: Namespace
 metadata:
