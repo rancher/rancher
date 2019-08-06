4032788dc1e4ff8518c64e6a4e6bf51d79491757
Caleb Bron <caleb@rancher.com>
2019-08-05 22:53:19 -0700

    dont limit protocol

--- pkg/api/customization/catalog/validator.go
+++ pkg/api/customization/catalog/validator.go
@@ -1,13 +1,9 @@
 package catalog
 
 import (
-	"fmt"
 	"net/http"
-	"net/url"
 	"regexp"
-	"strings"
 
-	"github.com/pkg/errors"
 	"github.com/rancher/norman/httperror"
 	"github.com/rancher/norman/types"
 )
@@ -20,11 +16,7 @@ var (
 func Validator(request *types.APIContext, schema *types.Schema, data map[string]interface{}) error {
 	url, ok := data["url"].(string)
 	if ok && len(url) > 0 {
-		sanitizedURL, err := validateURL(url)
-		if err != nil {
-			return httperror.NewAPIError(httperror.InvalidBodyContent, fmt.Sprintf("%v", err))
-		}
-		data["url"] = sanitizedURL
+		data["url"] = sanitizeURL(url)
 		return nil
 	} else if request.Method == http.MethodPost {
 		return httperror.NewAPIError(httperror.MissingRequired, "Catalog URL not specified")
@@ -32,18 +24,10 @@ func Validator(request *types.APIContext, schema *types.Schema, data map[string]
 	return nil
 }
 
-func validateURL(pathURL string) (string, error) {
+func sanitizeURL(pathURL string) string {
 	// Remove inline control character
 	pathURL = controlChars.ReplaceAllString(pathURL, "")
 	// Remove control characters that have been urlencoded such as %0d, %1B
 	pathURL = controlEncoded.ReplaceAllString(pathURL, "")
-	// Validate scheme
-	parsedURL, err := url.Parse(pathURL)
-	if err != nil {
-		return "", err
-	}
-	if !strings.HasPrefix(parsedURL.Scheme, "http") {
-		return "", errors.Errorf("unsupported protocol scheme '%s'", parsedURL.Scheme)
-	}
-	return parsedURL.String(), nil
+	return pathURL
 }
