b1b865852f8c2320d0580903705593120415a140
Caleb Bron <caleb@rancher.com>
2019-08-05 22:53:19 -0700

    dont limit protocol

--- tests/integration/suite/test_catalog.py
+++ tests/integration/suite/test_catalog.py
@@ -1,6 +1,6 @@
 import pytest
 import time
-from rancher import ApiError
+
 from .common import wait_for_template_to_be_created, \
     wait_for_template_to_be_deleted, random_str
 from .conftest import set_server_version
@@ -25,40 +25,6 @@ def test_catalog(admin_mc):
     client.delete(catalog2)
     wait_for_template_to_be_deleted(client, name1)
     wait_for_template_to_be_deleted(client, name2)
-
-
-def test_invalid_catalog(admin_mc, remove_resource):
-    client = admin_mc.client
-    name = random_str()
-    bad_url = "git://github.com/StrongMonkey/charts-1.git"
-    # POST: Bad URL
-    with pytest.raises(ApiError) as e:
-        catalog = client.create_catalog(name=name,
-                                        branch="test",
-                                        url=bad_url,
-                                        )
-        remove_resource(catalog)
-    assert e.value.error.status == 422
-    # POST: No URL
-    with pytest.raises(ApiError) as e:
-        catalog = client.create_catalog(name=name,
-                                        branch="test",
-                                        url="",
-                                        )
-        remove_resource(catalog)
-    assert e.value.error.status == 422
-    # PUT: Bad URL
-    good_url = "https://github.com/StrongMonkey/charts-1.git"
-    catalog = client.create_catalog(name=name,
-                                    branch="test",
-                                    url=good_url,
-                                    )
-    remove_resource(catalog)
-    wait_for_template_to_be_created(client, name)
-    with pytest.raises(ApiError) as e:
-        catalog.url = bad_url
-        client.update_by_id_catalog(catalog.id, catalog)
-    assert e.value.error.status == 422
 
 
 def test_invalid_catalog_chars(admin_mc, remove_resource):
