76ba36623b0f559a340cf4672d156c2da99bdb40
Craig Jellick <craig@rancher.com>
2019-08-05 20:31:33 -0700

    Revert "catalog-regex v2.2"

--- pkg/api/server/managementstored/setup.go
+++ pkg/api/server/managementstored/setup.go
@@ -295,7 +295,6 @@ func Catalog(schemas *types.Schemas, managementContext *config.ScaledContext) {
 	schema.ActionHandler = handler.RefreshActionHandler
 	schema.CollectionFormatter = catalog.CollectionFormatter
 	schema.LinkHandler = handler.ExportYamlHandler
-	schema.Validator = catalog.Validator
 }
 
 func ProjectCatalog(schemas *types.Schemas, managementContext *config.ScaledContext) {
@@ -306,7 +305,6 @@ func ProjectCatalog(schemas *types.Schemas, managementContext *config.ScaledCont
 	}
 	schema.ActionHandler = handler.RefreshProjectCatalogActionHandler
 	schema.CollectionFormatter = catalog.CollectionFormatter
-	schema.Validator = catalog.Validator
 }
 
 func ClusterCatalog(schemas *types.Schemas, managementContext *config.ScaledContext) {
@@ -317,7 +315,6 @@ func ClusterCatalog(schemas *types.Schemas, managementContext *config.ScaledCont
 	}
 	schema.ActionHandler = handler.RefreshClusterCatalogActionHandler
 	schema.CollectionFormatter = catalog.CollectionFormatter
-	schema.Validator = catalog.Validator
 }
 
 func ClusterRegistrationTokens(schemas *types.Schemas) {
