8c567a62fb07aafde8650e2f5072dcf9c655dc1b
Caleb Bron <caleb@rancher.com>
2019-08-05 22:53:19 -0700

    dont limit protocol

--- pkg/api/customization/catalog/validator_test.go
+++ pkg/api/customization/catalog/validator_test.go
@@ -14,42 +14,32 @@ func TestValidateURL(t *testing.T) {
 		name    string
 		pathURL string
 		want    string
-		wantErr bool
 	}{
 		{
 			name:    "Remove control characters",
 			pathURL: "http://example.com/1\r2\n345\b67\t",
 			want:    "http://example.com/1234567",
-			wantErr: false,
 		},
 		{
 			name:    "Remove urlEncoded control characters",
 			pathURL: "https://example.com/12%003%1F45%0A%0a6",
 			want:    "https://example.com/123456",
-			wantErr: false,
 		},
 		{
 			name:    "Remove all control characters, allow uppercase scheme",
-			pathURL: "HTTPS://www.example%0D.com/Hello\r\nWorld",
+			pathURL: "https://www.example%0D.com/Hello\r\nWorld",
 			want:    "https://www.example.com/HelloWorld",
-			wantErr: false,
 		},
 		{
-			name:    "Error on wrong protocol",
+			name:    "Allow git protocol",
 			pathURL: "git://www.example.com",
-			want:    "",
-			wantErr: true,
+			want:    "git://www.example.com",
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := validateURL(tt.pathURL)
-			if tt.wantErr {
-				assert.NotNil(t, err)
-			} else {
-				assert.Equal(t, got, tt.want)
-
-			}
+			got := sanitizeURL(tt.pathURL)
+			assert.Equal(t, got, tt.want)
 		})
 	}
 }
