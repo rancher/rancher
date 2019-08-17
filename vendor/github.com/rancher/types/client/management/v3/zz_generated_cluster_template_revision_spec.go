b089b5f5d1c1d838d3f20cd9a57ef860732538ce
Prachi Damle <prachi@rancher.com>
2019-08-16 17:43:34 -0700

    Vendor updates

--- vendor/github.com/rancher/types/client/management/v3/zz_generated_cluster_template_revision_spec.go
+++ vendor/github.com/rancher/types/client/management/v3/zz_generated_cluster_template_revision_spec.go
@@ -4,6 +4,7 @@ const (
 	ClusterTemplateRevisionSpecType                   = "clusterTemplateRevisionSpec"
 	ClusterTemplateRevisionSpecFieldClusterConfig     = "clusterConfig"
 	ClusterTemplateRevisionSpecFieldClusterTemplateID = "clusterTemplateId"
+	ClusterTemplateRevisionSpecFieldDisplayName       = "displayName"
 	ClusterTemplateRevisionSpecFieldEnabled           = "enabled"
 	ClusterTemplateRevisionSpecFieldQuestions         = "questions"
 )
@@ -11,6 +12,7 @@ const (
 type ClusterTemplateRevisionSpec struct {
 	ClusterConfig     *ClusterSpecBase `json:"clusterConfig,omitempty" yaml:"clusterConfig,omitempty"`
 	ClusterTemplateID string           `json:"clusterTemplateId,omitempty" yaml:"clusterTemplateId,omitempty"`
+	DisplayName       string           `json:"displayName,omitempty" yaml:"displayName,omitempty"`
 	Enabled           *bool            `json:"enabled,omitempty" yaml:"enabled,omitempty"`
 	Questions         []Question       `json:"questions,omitempty" yaml:"questions,omitempty"`
 }
