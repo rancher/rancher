3872ba136608908046ff2b29b5e78591ec5bfad4
Dax McDonald <dax@rancher.com>
2019-08-19 17:01:43 -0700

    Update vendor

--- vendor/github.com/rancher/norman/pkg/kwrapper/k8s/k3s.go
+++ vendor/github.com/rancher/norman/pkg/kwrapper/k8s/k3s.go
@@ -4,11 +4,12 @@ package k8s
 
 import (
 	"context"
+	"fmt"
 
 	"k8s.io/client-go/rest"
 )
 
 func getEmbedded(ctx context.Context) (bool, context.Context, *rest.Config, error) {
-	return false, ctx, nil, fmt.Error("embedded only supported on linux")
+	return false, ctx, nil, fmt.Errorf("embedded only supported on linux")
 
 }
