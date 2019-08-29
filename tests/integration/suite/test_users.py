c68d88eee458f0cb882bfab1aab669fd57838517
Dax McDonald <dax@rancher.com>
2019-08-29 13:32:44 -0700

    Cleanup legacy globalrolebindings finalizers to deleted clusters
    Previously, this only removed finalizers from deleted grbs with a 1 hour delay.
    This meant that when a user had their admin access removed it would take one hour for
    the change to propogate if they had a finalizer to a non-existent cluster in their list of finalizers.
    
    Now, we premptively remove "bad" finalziers from grbs that point to non-existent clusters so that on
    change they will not need to be cleaned. This only occurs once and all grbs created after this fix will
    have the "field.cattle.io/grbUpgrade" annotation to avoid running through this sync handler.

--- tests/integration/suite/test_users.py
+++ tests/integration/suite/test_users.py
@@ -1,5 +1,7 @@
 import pytest
+from kubernetes.client import CustomObjectsApi
 from rancher import ApiError
+from .conftest import random_str, wait_for
 
 
 def test_user_cant_delete_self(admin_mc):
@@ -16,3 +18,59 @@ def test_user_cant_deactivate_self(admin_mc):
         client.update(admin_mc.user, enabled=False)
 
     assert e.value.error.status == 422
+
+
+def test_globalrolebinding_finalizer_cleanup(admin_mc, remove_resource):
+    """This ensures that globalrolebinding cleanup of clusters < v2.2.8
+        is performed correctly"""
+    client = admin_mc.client
+    grb = client.create_globalRoleBinding(
+        globalRoleId="admin", userId="u-" + random_str()
+    )
+    remove_resource(grb)
+    assert grb.annotations["field.cattle.io/grbUpgrade"] == "true"
+
+    # create a grb without the rancher api with a bad finalizer
+    api = CustomObjectsApi(admin_mc.k8s_client)
+    json = {
+        "apiVersion": "management.cattle.io/v3",
+        "globalRoleName": "admin",
+        "kind": "GlobalRoleBinding",
+        "metadata": {
+            "finalizers": ["clusterscoped.controller.cattle.io/grb-sync_fake"],
+            "generation": 1,
+            "name": "grb-" + random_str(),
+        },
+        "userName": "u-" + random_str(),
+    }
+    grb_k8s = api.create_cluster_custom_object(
+        group="management.cattle.io",
+        version="v3",
+        plural="globalrolebindings",
+        body=json,
+    )
+    remove_resource(grb_k8s["metadata"]["name"])
+    grb_name = grb_k8s["metadata"]["name"]
+    grb_k8s = client.by_id_globalRoleBinding(id=grb_name)
+
+    def check_annotation():
+        grb1 = client.by_id_globalRoleBinding(grb_k8s.id)
+        try:
+            if grb1.annotations["field.cattle.io/grbUpgrade"] == "true":
+                return True
+            else:
+                return False
+        except (AttributeError, KeyError):
+            return False
+
+    wait_for(check_annotation, fail_handler=lambda: "annotation was not added")
+    grb1 = api.get_cluster_custom_object(
+        group="management.cattle.io",
+        version="v3",
+        plural="globalrolebindings",
+        name=grb_k8s.id,
+    )
+    assert (
+        "clusterscoped.controller.cattle.io/grb-sync_fake"
+        not in grb1["metadata"]["finalizers"]
+    )
