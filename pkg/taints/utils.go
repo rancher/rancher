af64496eb8ffb8ea9d48fc32407403fcaec945e4
orangedeng <jxfa0043379@hotmail.com>
2019-08-30 15:09:34 +0800

    Fix worker taints not applied
    **Problem:**
    Setting node pool taints and node template taints doesn't work.
    
    **Solution:**
    Add kubelet args `--register-with-taints` parameters to kubelet worker
    process. So those taints will be added after kubelet is up and running.
    If `--register-with-taints` args is set by kubelet service extra_args,
    taints from node template, node pool and kubelet will be merged.

--- pkg/taints/utils.go
+++ pkg/taints/utils.go
@@ -13,6 +13,10 @@ func GetTaintsString(taint v1.Taint) string {
 	return fmt.Sprintf("%s=%s:%s", taint.Key, taint.Value, taint.Effect)
 }
 
+func GetRKETaintsString(taint v3.RKETaint) string {
+	return fmt.Sprintf("%s=%s:%s", taint.Key, taint.Value, taint.Effect)
+}
+
 func GetKeyEffectString(taint v1.Taint) string {
 	return fmt.Sprintf("%s:%s", taint.Key, taint.Effect)
 }
@@ -85,6 +89,14 @@ func GetRKETaintsFromStrings(sources []string) []v3.RKETaint {
 	return rtn
 }
 
+func GetStringsFromRKETaint(taints []v3.RKETaint) []string {
+	var rtn []string
+	for _, taint := range taints {
+		rtn = append(rtn, GetRKETaintsString(taint))
+	}
+	return rtn
+}
+
 func GetRKETaintsFromTaints(sources []v1.Taint) []v3.RKETaint {
 	rtn := make([]v3.RKETaint, len(sources))
 	for i, source := range sources {
@@ -94,6 +106,41 @@ func GetRKETaintsFromTaints(sources []v1.Taint) []v3.RKETaint {
 			Value:     source.Value,
 			TimeAdded: source.TimeAdded,
 		}
+	}
+	return rtn
+}
+
+func GetStringsFromTaint(taints []v1.Taint) []string {
+	var rtn []string
+	for _, taint := range taints {
+		rtn = append(rtn, GetTaintsString(taint))
+	}
+	return rtn
+}
+
+func GetTaintsFromStrings(sources []string) []v1.Taint {
+	var rtn []v1.Taint
+	for _, source := range sources {
+		taint := GetTaintFromString(source)
+		if taint == nil {
+			continue
+		}
+		rtn = append(rtn, *taint)
+	}
+	return rtn
+}
+
+//MergeTaints will override t1 taint by t2 with same key and effect
+func MergeTaints(t1 []v1.Taint, t2 []v1.Taint) []v1.Taint {
+	set1 := GetKeyEffectTaintSet(t1)
+	set2 := GetKeyEffectTaintSet(t2)
+	rtn := t2
+	for key, i := range set1 {
+		if j, ok := set2[key]; ok {
+			logrus.Infof("overriding taint %s with %s", GetTaintsString(t1[i]), GetTaintsString(t2[j]))
+			continue
+		}
+		rtn = append(rtn, t1[i])
 	}
 	return rtn
 }
