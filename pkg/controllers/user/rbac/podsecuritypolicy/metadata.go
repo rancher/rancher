d49dbb5d86cb63959c28c905a21a77291df57092
Dan Ramich <danold215@gmail.com>
2019-08-30 10:47:05 -0700

    Update psp apiversion

--- pkg/controllers/user/rbac/podsecuritypolicy/metadata.go
+++ pkg/controllers/user/rbac/podsecuritypolicy/metadata.go
@@ -2,7 +2,7 @@ package podsecuritypolicy
 
 const (
 	apiGroup                                   = "rbac.authorization.k8s.io"
-	apiVersion                                 = "extensions/v1beta1"
+	apiVersion                                 = "policy/v1beta1"
 	podSecurityPolicyTemplateParentAnnotation  = "serviceaccount.cluster.cattle.io/pod-security"
 	podSecurityPolicyTemplateVersionAnnotation = "serviceaccount.cluster.cattle.io/pod-security-version"
 	projectIDAnnotation                        = "field.cattle.io/projectId"
