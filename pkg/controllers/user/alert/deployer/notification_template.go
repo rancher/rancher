41ce3ac49866aef3918d81e3a8b4758fd29bd101
gitlawr <lawrleegle@gmail.com>
2019-09-04 11:06:13 +0800

    Add "[Resolved]" to notification title on resolved
    So as to make resolved notifications recognizable.

--- pkg/controllers/user/alert/deployer/notification_template.go
+++ pkg/controllers/user/alert/deployer/notification_template.go
@@ -3,34 +3,37 @@ package deployer
 const (
 	NotificationTmpl = `
 {{- define "rancher.title" -}}
-{{- if eq .CommonLabels.alert_type "event" }}
+{{- if eq .Status "resolved" -}}
+[Resolved]
+{{- end -}}
+{{- if eq .CommonLabels.alert_type "event" -}}
 {{ .CommonLabels.event_type}} event of {{.GroupLabels.resource_kind}} occurred
 
-{{- else if eq .CommonLabels.alert_type "systemService" }}
+{{- else if eq .CommonLabels.alert_type "systemService" -}}
 The system component {{ .GroupLabels.component_name}} is not running
 
-{{- else if eq .CommonLabels.alert_type "nodeHealthy" }}
+{{- else if eq .CommonLabels.alert_type "nodeHealthy" -}}
 The kubelet on the node {{ .GroupLabels.node_name}} is not healthy
 
-{{- else if eq .CommonLabels.alert_type "nodeCPU" }}
+{{- else if eq .CommonLabels.alert_type "nodeCPU" -}}
 The CPU usage on the node {{ .GroupLabels.node_name}} is over {{ .CommonLabels.cpu_threshold}}%
 
-{{- else if eq .CommonLabels.alert_type "nodeMemory" }}
+{{- else if eq .CommonLabels.alert_type "nodeMemory" -}}
 The memory usage on the node {{ .GroupLabels.node_name}} is over {{ .CommonLabels.mem_threshold}}%
 
-{{- else if eq .CommonLabels.alert_type "podNotScheduled" }}
+{{- else if eq .CommonLabels.alert_type "podNotScheduled" -}}
 The Pod {{ if .GroupLabels.namespace}}{{.GroupLabels.namespace}}:{{end}}{{.GroupLabels.pod_name}} is not scheduled
 
-{{- else if eq .CommonLabels.alert_type "podNotRunning" }}
+{{- else if eq .CommonLabels.alert_type "podNotRunning" -}}
 The Pod {{ if .GroupLabels.namespace}}{{.GroupLabels.namespace}}:{{end}}{{.GroupLabels.pod_name}} is not running
 
-{{- else if eq .CommonLabels.alert_type "podRestarts" }}
+{{- else if eq .CommonLabels.alert_type "podRestarts" -}}
 The Pod {{ if .GroupLabels.namespace}}{{.GroupLabels.namespace}}:{{end}}{{.GroupLabels.pod_name}} restarts {{ .CommonLabels.restart_times}} times in {{ .CommonLabels.restart_interval}} sec
 
-{{- else if eq .CommonLabels.alert_type "workload" }}
+{{- else if eq .CommonLabels.alert_type "workload" -}}
 The workload {{ if .GroupLabels.workload_namespace}}{{.GroupLabels.workload_namespace}}:{{end}}{{.GroupLabels.workload_name}} has available replicas less than {{ .CommonLabels.available_percentage}}%
 
-{{- else if eq .CommonLabels.alert_type "metric" }}
+{{- else if eq .CommonLabels.alert_type "metric" -}}
 The metric {{ .CommonLabels.alert_name}} crossed the threshold 
 {{ end -}}
 {{ end -}}
