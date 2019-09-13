592a8992272fcd9b1e594ff7f41e3bb01dea401b
Alena Prokharchyk <alena@rancher.com>
2019-09-13 09:56:41 -0700

    Vendor update for types

--- vendor/github.com/rancher/types/apis/management.cattle.io/v3/tools_system_images.go
+++ vendor/github.com/rancher/types/apis/management.cattle.io/v3/tools_system_images.go
@@ -9,15 +9,9 @@ var (
 	m = image.Mirror
 
 	ToolsSystemImages = struct {
-		AlertSystemImages    AlertSystemImages
 		PipelineSystemImages projectv3.PipelineSystemImages
-		LoggingSystemImages  LoggingSystemImages
 		AuthSystemImages     AuthSystemImages
 	}{
-		AlertSystemImages: AlertSystemImages{
-			AlertManager:       m("prom/alertmanager:v0.15.2"),
-			AlertManagerHelper: m("rancher/alertmanager-helper:v0.0.2"),
-		},
 		PipelineSystemImages: projectv3.PipelineSystemImages{
 			Jenkins:       m("rancher/pipeline-jenkins-server:v0.1.4"),
 			JenkinsJnlp:   m("jenkins/jnlp-slave:3.10-1-alpine"),
@@ -27,11 +21,6 @@ var (
 			Registry:      m("registry:2"),
 			RegistryProxy: m("rancher/pipeline-tools:v0.1.12"),
 			KubeApply:     m("rancher/pipeline-tools:v0.1.12"),
-		},
-		LoggingSystemImages: LoggingSystemImages{
-			Fluentd:                       m("rancher/fluentd:v0.1.11"),
-			FluentdHelper:                 m("rancher/fluentd-helper:v0.1.2"),
-			LogAggregatorFlexVolumeDriver: m("rancher/log-aggregator:v0.1.4"),
 		},
 		AuthSystemImages: AuthSystemImages{
 			KubeAPIAuth: m("rancher/kube-api-auth:v0.1.3"),
