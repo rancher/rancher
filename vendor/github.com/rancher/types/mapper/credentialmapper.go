7fdca3ee4f3900bf3cae95acf23c82c87ea6e51a
kinarashah <kinara@rancher.com>
2019-09-17 10:05:40 -0700

    vendor update types

--- vendor/github.com/rancher/types/mapper/credentialmapper.go
+++ vendor/github.com/rancher/types/mapper/credentialmapper.go
@@ -14,11 +14,11 @@ type CredentialMapper struct {
 
 func (s CredentialMapper) FromInternal(data map[string]interface{}) {
 	formatData(data)
-	name := convert.ToString(values.GetValueN(data, "field.cattle.io/name"))
+	name := convert.ToString(values.GetValueN(data, "annotations", "field.cattle.io/name"))
 	if name == "" {
 		id := convert.ToString(values.GetValueN(data, "id"))
 		if id != "" {
-			data["field.cattle.io/name"] = id
+			values.PutValue(data, id, "annotations", "field.cattle.io/name")
 		}
 	}
 	delete(data, "data")
