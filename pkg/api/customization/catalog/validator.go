4154125d6fc49d5fa32c8103d4abd2087a064000
Craig Jellick <craig@rancher.com>
2019-09-19 17:06:25 -0700

    Revert "Error on invalid catalog url"

--- pkg/api/customization/catalog/validator.go
+++ pkg/api/customization/catalog/validator.go
@@ -2,9 +2,7 @@ package catalog
 
 import (
 	"net/http"
-	"net/url"
 	"regexp"
-	"strings"
 
 	"github.com/rancher/norman/httperror"
 	"github.com/rancher/norman/types"
@@ -16,14 +14,9 @@ var (
 )
 
 func Validator(request *types.APIContext, schema *types.Schema, data map[string]interface{}) error {
-	if pathURL, _ := data["url"].(string); pathURL != "" {
-		if err := validateURL(pathURL); err != nil {
-			return err
-		}
-		if u, err := url.Parse(pathURL); err == nil {
-			u.Scheme = strings.ToLower(u.Scheme) // git commands are case-sensitive
-			data["url"] = u.String()
-		}
+	url, ok := data["url"].(string)
+	if ok && len(url) > 0 {
+		data["url"] = sanitizeURL(url)
 		return nil
 	} else if request.Method == http.MethodPost {
 		return httperror.NewAPIError(httperror.MissingRequired, "Catalog URL not specified")
@@ -31,9 +24,10 @@ func Validator(request *types.APIContext, schema *types.Schema, data map[string]
 	return nil
 }
 
-func validateURL(pathURL string) error {
-	if controlChars.FindStringIndex(pathURL) != nil || controlEncoded.FindStringIndex(pathURL) != nil {
-		return httperror.NewAPIError(httperror.InvalidBodyContent, "Invalid characters in catalog URL")
-	}
-	return nil
+func sanitizeURL(pathURL string) string {
+	// Remove inline control character
+	pathURL = controlChars.ReplaceAllString(pathURL, "")
+	// Remove control characters that have been urlencoded such as %0d, %1B
+	pathURL = controlEncoded.ReplaceAllString(pathURL, "")
+	return pathURL
 }
--- tests/integration/suite/test_catalog.py
+++ tests/integration/suite/test_catalog.py
@@ -1,27 +1,21 @@
-import pytest
 import time
-
-from rancher import ApiError
 from .common import wait_for_template_to_be_created, \
     wait_for_template_to_be_deleted, random_str
 
 
-def test_catalog(admin_mc, remove_resource):
+def test_catalog(admin_mc):
     client = admin_mc.client
     name1 = random_str()
     name2 = random_str()
-    url1 = "https://github.com/StrongMonkey/charts-1.git"
-    url2 = "HTTP://github.com/StrongMonkey/charts-1.git"
+    url = "https://github.com/StrongMonkey/charts-1.git"
     catalog1 = client.create_catalog(name=name1,
                                      branch="test",
-                                     url=url1,
+                                     url=url,
                                      )
-    remove_resource(catalog1)
     catalog2 = client.create_catalog(name=name2,
                                      branch="test",
-                                     url=url2,
+                                     url=url,
                                      )
-    remove_resource(catalog2)
     wait_for_template_to_be_created(client, name1)
     wait_for_template_to_be_created(client, name2)
     client.delete(catalog1)
@@ -34,23 +28,15 @@ def test_invalid_catalog_chars(admin_mc, remove_resource):
     client = admin_mc.client
     name = random_str()
     url = "https://github.com/%0dStrongMonkey%0A/charts-1.git"
-    with pytest.raises(ApiError) as e:
-        catalog = client.create_catalog(name=name,
-                                        branch="test",
-                                        url=url,
-                                        )
-        remove_resource(catalog)
-    assert e.value.error.status == 422
-    assert e.value.error.message == "Invalid characters in catalog URL"
-    url = "https://github.com/StrongMonkey\t/charts-1.git"
-    with pytest.raises(ApiError) as e:
-        catalog = client.create_catalog(name=name,
-                                        branch="test",
-                                        url=url,
-                                        )
-        remove_resource(catalog)
-    assert e.value.error.status == 422
-    assert e.value.error.message == "Invalid characters in catalog URL"
+    catalog = client.create_catalog(name=name,
+                                    branch="test",
+                                    url=url,
+                                    )
+    remove_resource(catalog)
+    wait_for_template_to_be_created(client, name)
+    catalog = client.reload(catalog)
+    correct_url = "https://github.com/StrongMonkey/charts-1.git"
+    assert catalog['url'] == correct_url
 
 
 def test_global_catalog_template_access(admin_mc, user_factory,
