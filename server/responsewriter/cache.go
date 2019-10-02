bcdac553d361676f2552e6c3fb499d87b5038960
Dan Ramich <danold215@gmail.com>
2019-10-02 11:48:14 -0700

    Add X-Content-Type-Options header everywhere

--- server/responsewriter/cache.go
+++ server/responsewriter/cache.go
@@ -40,3 +40,10 @@ func DenyFrameOptions(handler http.Handler) http.Handler {
 		handler.ServeHTTP(w, r)
 	})
 }
+
+func ContentTypeOptions(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("X-Content-Type-Options", "nosniff")
+		handler.ServeHTTP(w, r)
+	})
+}
