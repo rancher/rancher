71313e6fa1d974b49a439b85fe97c372aeab0f5b
dstockton <david.stockton@confluent.io>
2019-11-19 14:11:24 +0000

    Improvement - Update github.com/Azure/azure-sdk-for-go library

--- server/capabilities/aks_virtual_networks_endpoint.go
+++ server/capabilities/aks_virtual_networks_endpoint.go
@@ -8,7 +8,7 @@ import (
 	"net/http"
 	"regexp"
 
-	"github.com/Azure/azure-sdk-for-go/services/network/mgmt/2018-07-01/network"
+	"github.com/Azure/azure-sdk-for-go/services/network/mgmt/2019-06-01/network"
 	"github.com/Azure/go-autorest/autorest"
 	"github.com/Azure/go-autorest/autorest/adal"
 	"github.com/Azure/go-autorest/autorest/azure"
