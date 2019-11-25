7778b98e1a0d0ac7b9c14e4536e62de4db7debc9
rmweir <ricardo.weir1@gmail.com>
2019-11-25 14:31:52 -0700

    Add test for GRB validator

--- tests/integration/suite/test_global_role_bindings.py
+++ tests/integration/suite/test_global_role_bindings.py
@@ -1,3 +1,6 @@
+import pytest
+
+from rancher import ApiError
 from .common import random_str
 
 
@@ -15,3 +18,19 @@ def test_cannot_update_global_role(admin_mc, remove_resource):
         id=grb.id,
         globalRoleId="settings-manage")
     assert grb.globalRoleId == "nodedrivers-manage"
+
+
+def test_globalrole_must_exist(admin_mc, remove_resource):
+    """Asserts that globalRoleId must reference an existing role"""
+    admin_client = admin_mc.client
+
+    with pytest.raises(ApiError) as e:
+        grb = admin_client.create_global_role_binding(
+            name="gr-" + random_str(),
+            globalRoleId="somefakerole",
+            userId=admin_mc.user.id
+        )
+        remove_resource(grb)
+    assert e.value.error.status == 404
+    assert "globalRole.management.cattle.io \"somefakerole\" not found" in \
+        e.value.error.message
