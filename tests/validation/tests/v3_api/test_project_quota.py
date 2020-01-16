ff4dfd7538f97c5aa5201027679968f775e72bb6
GGGitBoy <1812041288@qq.com>
2020-01-16 12:43:14 +0800

    Add validation test for validating namespace's quota
    https://github.com/rancher/rancher/issues/24479

--- tests/validation/tests/v3_api/test_project_quota.py
+++ tests/validation/tests/v3_api/test_project_quota.py
@@ -121,6 +121,48 @@ def test_resource_quota_create_namespace_with_ns_quota():
         print(str(e))
     assert "MaxLimitExceeded" in errorstring
 
+def test_namespace_quota_edit(remove_resource):
+    client, cluster = get_global_admin_client_and_cluster()
+
+    pj1 = client.create_project(name=random_str(),
+                                clusterId=cluster.id,
+                                resourceQuota={"limit": {"limitsCpu": "20m"}},
+                                namespaceDefaultResourceQuota={
+                                    "limit": {"limitsCpu": "10m"}})
+
+    pj2 = client.create_project(name=random_str(),
+                                clusterId=cluster.id,
+                                resourceQuota={"limit": {"limitsCpu": "15m"}},
+                                namespaceDefaultResourceQuota={
+                                    "limit": {"limitsCpu": "15m"}})
+
+    
+    p_client = get_cluster_client_for_token(cluster, ADMIN_TOKEN)
+
+    ns1 = p_client.create_namespace(name=random_str(),
+                                 clusterId=cluster.id,
+                                 projectId=pj1.id)
+    
+    ns2 = p_client.create_namespace(name=random_str(),
+                                  clusterId=cluster.id,
+                                  projectId=pj2.id)
+
+    ns1 = p_client.wait_success(ns1)
+    ns2 = p_client.wait_success(ns2)
+
+    p_client.action(obj=ns2,
+                  action_name="move",
+                  projectId=None)
+
+    ns1 = p_client.update(ns1,
+                resourceQuota={"limit": {"limitsCpu": "11m"}})
+
+    ns1_limitsCpu = ns1.resourceQuota.limit.limitsCpu
+    assert ns1_limitsCpu == "11m"
+
+    remove_resource(pj1)
+    remove_resource(pj2)
+    remove_resource(ns2)
 
 def validate_resoucequota_thru_kubectl(namespace):
 
