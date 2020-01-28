0fa440350b002df6c7c17f5af0658f34d90652a7
soumyalj <soumyalj>
2020-01-28 11:54:10 -0800

    Added RBAC tests for secrets Renamed rbac tests

--- tests/validation/tests/v3_api/test_secrets.py
+++ tests/validation/tests/v3_api/test_secrets.py
@@ -1,5 +1,5 @@
 import base64
-
+from rancher import ApiError
 import pytest
 
 from .common import *  # NOQA
@@ -10,6 +10,10 @@ namespace = {"p_client": None, "ns": None, "cluster": None, "project": None}
 
 
 def test_secret_create_all_ns():
+
+    """
+    Verify creation of secrets is functional
+    """
 
     p_client = namespace["p_client"]
     ns = namespace["ns"]
@@ -51,6 +55,10 @@ def test_secret_create_all_ns():
 
 def test_secret_create_single_ns():
 
+    """
+    Verify editing secrets is functional
+    """
+
     p_client = namespace["p_client"]
     ns = namespace["ns"]
     # Value is base64 encoded
@@ -66,8 +74,11 @@ def test_secret_create_single_ns():
                                                              ns, keyvaluepair)
 
 
-def test_delete_secret_all_ns():
+def test_secret_delete_all_ns():
 
+    """
+    Verify Deletion of secrets is functional
+    """
     p_client = namespace["p_client"]
     ns = namespace["ns"]
     # Value is base64 encoded
@@ -77,7 +88,7 @@ def test_delete_secret_all_ns():
     delete_secret(p_client, secret, ns, keyvaluepair)
 
 
-def test_delete_secret_single_ns():
+def test_secret_delete_single_ns():
 
     p_client = namespace["p_client"]
     ns = namespace["ns"]
@@ -89,7 +100,7 @@ def test_delete_secret_single_ns():
     delete_secret(p_client, secret, ns, keyvaluepair)
 
 
-def test_edit_secret_all_ns():
+def test_secret_edit_all_ns():
 
     p_client = namespace["p_client"]
     name = random_test_name("default")
@@ -138,7 +149,8 @@ def test_edit_secret_all_ns():
         p_client, secret, new_ns1, updatedsecretdata)
 
 
-def test_edit_secret_single_ns():
+def test_secret_edit_single_ns():
+
     p_client = namespace["p_client"]
     ns = namespace["ns"]
     name = random_test_name("default")
@@ -169,6 +181,335 @@ def test_edit_secret_single_ns():
                                                        updatedsecretdata)
     create_and_validate_workload_with_secret_as_env_variable(
         p_client, secret, ns, updatedsecretdata)
+
+
+rbac_role_list = [
+    (CLUSTER_OWNER),
+    (PROJECT_OWNER),
+    (PROJECT_MEMBER),
+]
+
+
+@if_test_rbac
+@pytest.mark.parametrize("role", rbac_role_list)
+def test_rbac_secret_create(role):
+    """
+    Verify creation of secrets for Cluster owner, project owner and project
+    member
+    """
+    token = rbac_get_user_token_by_role(role)
+    project = rbac_get_project()
+    ns = rbac_get_namespace()
+    p_client = get_project_client_for_token(project, token)
+    rbac_secret_create(p_client, ns)
+
+
+@if_test_rbac
+@pytest.mark.parametrize("role", rbac_role_list)
+def test_rbac_secret_edit(role):
+    """
+    Verify editing of secrets for Cluster owner, project owner and project
+    member
+    """
+    token = rbac_get_user_token_by_role(role)
+    project = rbac_get_project()
+    ns = rbac_get_namespace()
+    p_client = get_project_client_for_token(project, token)
+    rbac_secret_edit(p_client, ns, project=project)
+
+
+@if_test_rbac
+@pytest.mark.parametrize("role", rbac_role_list)
+def test_rbac_secret_delete(role):
+    """
+    Verify deletion of secrets for Cluster owner, project owner and project
+    member
+    """
+    user_token = rbac_get_user_token_by_role(role)
+    project = rbac_get_project()
+    ns = rbac_get_namespace()
+    p_client = get_project_client_for_token(project, user_token)
+    rbac_secret_delete(p_client, ns)
+
+
+@if_test_rbac
+def test_rbac_secret_create_cluster_member(remove_resource):
+
+    """
+    Verify cluster member can create secret and deploy workload using secret
+    in the project he created
+    """
+
+    user_token = rbac_get_user_token_by_role(CLUSTER_MEMBER)
+    project, ns = create_project_and_ns(user_token, namespace["cluster"],
+                                        random_test_name("rbac-cluster-mem"))
+    p_client = get_project_client_for_token(project, user_token)
+    rbac_secret_create(p_client, ns)
+
+    # Create a project as cluster owner and verify the cluster member cannot
+    # create secret in this project
+
+    keyvaluepair = {"testall": "valueall"}
+    cluster_owner_token = rbac_get_user_token_by_role(CLUSTER_OWNER)
+    ownerproject, ns = create_project_and_ns(
+                        cluster_owner_token,
+                        namespace["cluster"],
+                        random_test_name("rbac-cluster-owner"))
+    cluster_member_client = get_project_client_for_token(ownerproject,
+                                                         user_token)
+    with pytest.raises(ApiError) as e:
+        create_secret(keyvaluepair, singlenamespace=False,
+                      p_client=cluster_member_client)
+    assert e.value.error.status == 403
+    assert e.value.error.code == 'PermissionDenied'
+
+    remove_resource(project)
+
+
+@if_test_rbac
+def test_rbac_secret_edit_cluster_member(remove_resource):
+
+    """
+    Verify cluster member can create secret and edit secret in the project he
+    created
+    """
+
+    user_token = rbac_get_user_token_by_role(CLUSTER_MEMBER)
+    project, ns = create_project_and_ns(user_token, namespace["cluster"],
+                                        random_test_name("rbac-cluster-mem"))
+    p_client = get_project_client_for_token(project, user_token)
+    rbac_secret_edit(p_client, ns, project=project)
+
+    # Create a project as cluster owner and verify the cluster member cannot
+    # edit secret in this project
+
+    keyvaluepair = {"testall": "valueall"}
+
+    value1 = ("valueall")
+    value2 = ("valueallnew")
+    updated_dict = {"testall": value1, "testallnew": value2}
+
+    cluster_owner_token = rbac_get_user_token_by_role(CLUSTER_OWNER)
+    ownerproject, ns = create_project_and_ns(
+        cluster_owner_token,
+        namespace["cluster"],
+        random_test_name("rbac-cluster-owner"))
+    cluster_owner_client = get_project_client_for_token(ownerproject,
+                                                        cluster_owner_token)
+    cluster_member_client = get_project_client_for_token(ownerproject,
+                                                         user_token)
+    ownersecret = create_secret(keyvaluepair, singlenamespace=False,
+                                p_client=cluster_owner_client)
+
+    with pytest.raises(ApiError) as e:
+        cluster_member_client.update(ownersecret, namespaceId='NULL',
+                                     data=updated_dict)
+    assert e.value.error.status == 404
+    assert e.value.error.code == 'NotFound'
+
+    remove_resource(project)
+
+
+@if_test_rbac
+def test_rbac_secret_delete_cluster_member(remove_resource):
+
+    """
+    Verify cluster member can create secret and delete secret in the project he
+    created
+    """
+
+    keyvaluepair = {"testall": "valueall"}
+    user_token = rbac_get_user_token_by_role(CLUSTER_MEMBER)
+    project, ns = create_project_and_ns(user_token, namespace["cluster"],
+                                        random_test_name("rbac-cluster-mem"))
+    p_client = get_project_client_for_token(project, user_token)
+    rbac_secret_delete(p_client, ns)
+
+    # Create a project as cluster owner and verify the cluster member cannot
+    # delete secret in this project
+
+    cluster_owner_token = rbac_get_user_token_by_role(CLUSTER_OWNER)
+    ownerproject, ns = create_project_and_ns(
+        cluster_owner_token,
+        namespace["cluster"],
+        random_test_name("rbac-cluster-owner"))
+    cluster_owner_client = get_project_client_for_token(ownerproject,
+                                                        cluster_owner_token)
+    cluster_member_client = get_project_client_for_token(ownerproject,
+                                                         user_token)
+    ownersecret = create_secret(keyvaluepair, singlenamespace=False,
+                                p_client=cluster_owner_client)
+
+    with pytest.raises(ApiError) as e:
+        delete_secret(cluster_member_client, ownersecret, ns, keyvaluepair)
+    assert e.value.error.status == 403
+    assert e.value.error.code == 'Forbidden'
+
+    remove_resource(project)
+
+
+@if_test_rbac
+def test_rbac_secret_create_project_readonly():
+
+    """
+    Verify read-only user cannot create secret
+    """
+
+    project = rbac_get_project()
+    user_token1 = rbac_get_user_token_by_role(PROJECT_READ_ONLY)
+    readonly_user_client = get_project_client_for_token(project, user_token1)
+
+    keyvaluepair = {"testall": "valueall"}
+
+    # Read Only member cannot create secrets
+    with pytest.raises(ApiError) as e:
+        create_secret(keyvaluepair, singlenamespace=False,
+                      p_client=readonly_user_client)
+    assert e.value.error.status == 403
+    assert e.value.error.code == 'PermissionDenied'
+
+
+@if_test_rbac
+def test_rbac_secret_edit_project_readonly_member(remove_resource):
+
+    """
+    Verify read-only user cannot edit secret
+    """
+
+    cluster_owner_token = rbac_get_user_token_by_role(CLUSTER_OWNER)
+    project = rbac_get_project()
+    ns = rbac_get_namespace()
+    user_token1 = rbac_get_user_token_by_role(PROJECT_READ_ONLY)
+    readonly_user_client = get_project_client_for_token(project, user_token1)
+
+    keyvaluepair = {"testall": "valueall"}
+
+    cluster_owner_p_client = get_project_client_for_token(project,
+                                                          cluster_owner_token)
+    # As a cluster owner, create a secret
+    secret = create_secret(keyvaluepair, p_client=cluster_owner_p_client,
+                           ns=ns)
+
+    # Readonly member cannot edit secret
+    value1 = ("valueall")
+    value2 = ("valueallnew")
+    updated_dict = {"testall": value1, "testallnew": value2}
+
+    with pytest.raises(ApiError) as e:
+        readonly_user_client.update(secret,
+                                    namespaceId=ns['name'],
+                                    data=updated_dict)
+    assert e.value.error.status == 404
+    assert e.value.error.code == 'NotFound'
+
+    remove_resource(secret)
+
+
+@if_test_rbac
+def test_rbac_secret_delete_project_readonly(remove_resource):
+
+    """
+    Verify read-only user cannot delete secret
+    """
+
+    cluster_owner_token = rbac_get_user_token_by_role(CLUSTER_OWNER)
+    project = rbac_get_project()
+    ns = rbac_get_namespace()
+    user_token1 = rbac_get_user_token_by_role(PROJECT_READ_ONLY)
+    readonly_user_client = get_project_client_for_token(project, user_token1)
+
+    keyvaluepair = {"testall": "valueall"}
+
+    cluster_owner_p_client = get_project_client_for_token(project,
+                                                          cluster_owner_token)
+    # As a cluster owner, create a secret
+    secret = create_secret(keyvaluepair, p_client=cluster_owner_p_client,
+                           ns=ns)
+    # Assert read-only user cannot delete the secret
+    with pytest.raises(ApiError) as e:
+        delete_secret(readonly_user_client, secret, ns, keyvaluepair)
+    assert e.value.error.status == 403
+    assert e.value.error.code == 'Forbidden'
+
+    remove_resource(secret)
+
+
+def rbac_secret_create(p_client, ns):
+
+    """
+    Verify creating secret is functional.
+    The p_client passed as the parameter would be as per the role assigned
+    """
+
+    keyvaluepair = {"testall": "valueall"}
+    secret = create_secret(keyvaluepair, singlenamespace=False,
+                           p_client=p_client)
+
+    # Create workloads with secret in existing namespace
+    create_and_validate_workload_with_secret_as_volume(p_client, secret,
+                                                       ns, keyvaluepair)
+
+
+def rbac_secret_edit(p_client, ns, project=None):
+
+    """
+    Verify creating, editing secret is functional.
+    The p_client passed as the parameter would be as per the role assigned
+    """
+
+    value = base64.b64encode(b"valueall")
+    keyvaluepair = {"testall": value.decode('utf-8')}
+    cluster = namespace["cluster"]
+    c_client = namespace["c_client"]
+
+    # Create a namespace
+    secret = create_secret(keyvaluepair, singlenamespace=False,
+                           p_client=p_client)
+    # Value is base64 encoded
+    value1 = base64.b64encode(b"valueall")
+    value2 = base64.b64encode(b"valueallnew")
+    updated_dict = {"testall": value1.decode(
+        'utf-8'), "testallnew": value2.decode('utf-8')}
+    updated_secret = p_client.update(secret, namespaceId='NULL',
+                                     data=updated_dict)
+
+    assert updated_secret['baseType'] == "secret"
+    updatedsecretdata = updated_secret['data']
+
+    print("UPDATED SECRET DATA")
+    print(updatedsecretdata)
+
+    assert updatedsecretdata.data_dict() == updated_dict
+
+    # Create workloads using updated secret in the existing namespace
+    create_and_validate_workload_with_secret_as_volume(p_client, secret,
+                                                       ns,
+                                                       updatedsecretdata)
+
+    create_and_validate_workload_with_secret_as_env_variable(
+        p_client, secret, ns, updatedsecretdata)
+
+    # Create a new namespace and workloads in the new namespace using secret
+    new_ns1 = create_ns(c_client, cluster, project)
+
+    create_and_validate_workload_with_secret_as_volume(p_client, secret,
+                                                       new_ns1,
+                                                       updatedsecretdata)
+    create_and_validate_workload_with_secret_as_env_variable(
+        p_client, secret, new_ns1, updatedsecretdata)
+
+
+def rbac_secret_delete(p_client, ns):
+
+    """
+    Verify creating, deleting secret is functional.
+    The p_client passed as the parameter would be as per the role assigned
+    """
+    keyvaluepair = {"testall": "valueall"}
+    secret = create_secret(keyvaluepair, singlenamespace=False,
+                           p_client=p_client)
+    # Verify deletion of secret
+    delete_secret(p_client, secret, ns, keyvaluepair)
 
 
 @pytest.fixture(scope='module', autouse="True")
@@ -223,24 +564,29 @@ def validate_workload_with_secret(p_client, workload,
 def delete_secret(client, secret, ns, keyvaluepair):
 
     key = list(keyvaluepair.keys())[0]
-
+    secretname = secret.name
     print("Delete Secret")
     client.delete(secret)
 
     # Sleep to allow for the secret to be deleted
     time.sleep(5)
+    timeout = 30
     print("Secret list after deleting secret")
-    secretdict = client.list_secret()
+    secretdict = client.list_secret(name=secretname)
     print(secretdict)
     print(secretdict.get('data'))
+    start = time.time()
     if len(secretdict.get('data')) > 0:
         testdata = secretdict.get('data')
         print("TESTDATA")
         print(testdata[0]['data'])
-        if key in testdata[0]['data']:
-            assert False
-        else:
-            assert True
+        while key in testdata[0]['data']:
+            if time.time() - start > timeout:
+                raise AssertionError("Timed out waiting for deletion")
+            time.sleep(.5)
+            secretdict = client.list_secret(name=secretname)
+            testdata = secretdict.get('data')
+        assert True
     if len(secretdict.get('data')) == 0:
         assert True
 
@@ -262,6 +608,7 @@ def create_and_validate_workload_with_secret_as_volume(p_client, secret, ns,
                                                        name=None):
     if name is None:
         name = random_test_name("test")
+
     # Create Workload with secret as volume
     mountpath = "/test"
     volumeMounts = [{"readOnly": False, "type": "volumeMount",
@@ -283,8 +630,6 @@ def create_and_validate_workload_with_secret_as_volume(p_client, secret, ns,
     validate_workload_with_secret(p_client, workload, "deployment",
                                   ns.name, keyvaluepair,
                                   workloadwithsecretasVolume=True)
-
-    return workload
 
 
 def create_and_validate_workload_with_secret_as_env_variable(p_client, secret,
@@ -311,7 +656,6 @@ def create_and_validate_workload_with_secret_as_env_variable(p_client, secret,
     validate_workload_with_secret(p_client, workload, "deployment",
                                   ns.name, keyvaluepair,
                                   workloadwithsecretasenvvar=True)
-    return workload
 
 
 def create_secret(keyvaluepair, singlenamespace=False,
