3baae9a61c74d887cf020d961554c0892b5f8824
Darren Shepherd <darren@rancher.com>
2020-02-20 16:56:03 -0700

    Update dashboard URL to latest

--- pkg/settings/setting.go
+++ pkg/settings/setting.go
@@ -23,7 +23,7 @@ var (
 	CLIURLDarwin                      = NewSetting("cli-url-darwin", "https://releases.rancher.com/cli/v1.0.0-alpha8/rancher-darwin-amd64-v1.0.0-alpha8.tar.gz")
 	CLIURLLinux                       = NewSetting("cli-url-linux", "https://releases.rancher.com/cli/v1.0.0-alpha8/rancher-linux-amd64-v1.0.0-alpha8.tar.gz")
 	CLIURLWindows                     = NewSetting("cli-url-windows", "https://releases.rancher.com/cli/v1.0.0-alpha8/rancher-windows-386-v1.0.0-alpha8.zip")
-	DashboardIndex                    = NewSetting("dashboard-index", "https://releases.rancher.com/dashboard/master/index.html")
+	DashboardIndex                    = NewSetting("dashboard-index", "https://releases.rancher.com/dashboard/latest/index.html")
 	EngineInstallURL                  = NewSetting("engine-install-url", "https://releases.rancher.com/install-docker/19.03.sh")
 	EngineISOURL                      = NewSetting("engine-iso-url", "https://releases.rancher.com/os/latest/rancheros-vmware.iso")
 	EngineNewestVersion               = NewSetting("engine-newest-version", "v17.12.0")
