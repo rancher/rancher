0665c655b1cccd336e5cd38b41feb9172a96b6b8
Dax McDonald <dax@rancher.com>
2020-02-24 13:44:39 -0700

    Add rancher k3s upgrader to imported k3s cluster
    K3s upgrader will now be installed on imported k3s clusters wwhen
    their version is set to a newer version for the first time. On
    subsequent occasions, the upgrader will be updated if a new
    version of the app is available. This will also use the cattle-system
    namespace by default instead of the system-upgrade namespace to prevent
    Rancher installations from interfering with user installed upgrader.
    
    Co-authored-by: rmweir <ricardo@rancher.com>

--- pkg/controllers/management/wrangler.go
+++ pkg/controllers/management/wrangler.go
@@ -4,10 +4,14 @@ import (
 	"context"
 
 	"github.com/rancher/rancher/pkg/clustermanager"
+	"github.com/rancher/rancher/pkg/controllers/management/k3supgrade"
 	"github.com/rancher/rancher/pkg/wrangler"
 	"github.com/rancher/types/config"
 )
 
 func RegisterWrangler(ctx context.Context, wranglerContext *wrangler.Context, management *config.ManagementContext, manager *clustermanager.Manager) {
 	// Add controllers to register here
+
+	k3supgrade.Register(ctx, wranglerContext, management, manager)
+
 }
