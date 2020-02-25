8a557050943b8a7ec8b3206cb0ff2bade9ed2fe3
Darren Shepherd <darren@rancher.com>
2020-02-25 10:26:47 -0700

    Move dashboard and responsewriters to steve

--- pkg/auth/providers/saml/saml_client.go
+++ pkg/auth/providers/saml/saml_client.go
@@ -16,7 +16,7 @@ import (
 	"github.com/crewjam/saml/samlsp"
 	"github.com/gorilla/mux"
 	"github.com/rancher/rancher/pkg/auth/tokens"
-	"github.com/rancher/rancher/server/responsewriter"
+	"github.com/rancher/steve/pkg/responsewriter"
 	v3 "github.com/rancher/types/apis/management.cattle.io/v3"
 	log "github.com/sirupsen/logrus"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
