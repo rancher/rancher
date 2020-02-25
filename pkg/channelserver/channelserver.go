683632dabb5cea1790e16f2a147dbf62c8438246
Darren Shepherd <darren@rancher.com>
2020-02-25 10:51:34 -0700

    Bundle channel server

---
+++ pkg/channelserver/channelserver.go
@@ -0,0 +1,78 @@
+package channelserver
+
+import (
+	"context"
+	"encoding/json"
+	"os"
+	"os/exec"
+	"strconv"
+	"time"
+
+	"github.com/rancher/rancher/pkg/settings"
+	"github.com/rancher/wrangler/pkg/data"
+	"github.com/sirupsen/logrus"
+	"k8s.io/client-go/util/flowcontrol"
+)
+
+var (
+	prog = "channelserver"
+)
+
+func getURLAndInterval() (string, string) {
+	val := map[string]interface{}{}
+	if err := json.Unmarshal([]byte(settings.RkeMetadataConfig.Get()), &val); err != nil {
+		logrus.Errorf("failed to parse %s value: %v", settings.RkeMetadataConfig.Name, err)
+		return "", ""
+	}
+	url := data.Object(val).String("url")
+	minutes, _ := strconv.Atoi(data.Object(val).String("refresh-interval-minutes"))
+	if minutes <= 0 {
+		minutes = 1440
+	}
+
+	return url, (time.Duration(minutes) * time.Minute).String()
+
+}
+
+func run() chan error {
+	done := make(chan error, 1)
+	go func() {
+		defer close(done)
+
+		url, interval := getURLAndInterval()
+		cmd := exec.Command(
+			prog,
+			"--config-key=k3s",
+			"--url", url,
+			"--url=/var/lib/rancher-data/driver-metadata/data.json",
+			"--refresh-interval", interval,
+			"--listen-address=0.0.0.0:8115")
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		done <- cmd.Run()
+	}()
+	return done
+}
+
+func Start(ctx context.Context) error {
+	if _, err := exec.LookPath(prog); err != nil {
+		logrus.Errorf("Failed to find %s, will not run /v1-release API: %v", prog, err)
+		return nil
+	}
+
+	backoff := flowcontrol.NewBackOff(5*time.Second, 15*time.Minute)
+	for {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case err := <-run():
+			logrus.Infof("failed to run channelserver: %v", err)
+		}
+		backoff.Next("next", time.Now())
+		select {
+		case <-time.After(backoff.Get("next")):
+		case <-ctx.Done():
+			return ctx.Err()
+		}
+	}
+}
