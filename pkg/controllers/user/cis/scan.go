c0289d2a0ebcdd819b10c2407d3e6455120d164f
Murali Paluru <leodotcloud@gmail.com>
2020-03-06 00:16:44 +0530

    propagate the scan result to the state

--- pkg/controllers/user/cis/scan.go
+++ pkg/controllers/user/cis/scan.go
@@ -56,8 +56,6 @@ func LaunchScan(
 	cluster *v3.Cluster,
 	clusterClient v3.ClusterInterface,
 	clusterScanClient v3.ClusterScanInterface,
-	retryIntervalMilliseconds time.Duration,
-	retryCount int,
 ) (*v3.ClusterScan, error) {
 	var err error
 	var cisScan *v3.ClusterScan
@@ -76,7 +74,7 @@ func LaunchScan(
 	updatedCluster.Annotations[v3.RunCisScanAnnotation] = cisScan.Name
 
 	// Can't add either too many retries or longer interval as this an API handler
-	for i := 0; i < retryCount; i++ {
+	for i := 0; i < NumberOfRetriesForClusterUpdate; i++ {
 		_, err = clusterClient.Update(updatedCluster)
 		if err == nil {
 			break
@@ -84,7 +82,7 @@ func LaunchScan(
 		if !errors.IsConflict(err) {
 			return nil, err
 		}
-		time.Sleep(retryIntervalMilliseconds * time.Millisecond)
+		time.Sleep(RetryIntervalInMilliseconds * time.Millisecond)
 		cluster, err = clusterClient.Get(cluster.Name, v1.GetOptions{})
 		if err != nil {
 			logrus.Errorf("error fetching cluster with id %v: %v", cluster.Name, err)
