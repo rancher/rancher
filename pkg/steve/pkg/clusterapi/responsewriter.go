cced23a5bd7fc972597dc460a191bde10819a3f3
Darren Shepherd <darren@rancher.com>
2020-03-10 13:31:25 -0700

    Fix backend dashbaord issues

--- pkg/steve/pkg/clusterapi/responsewriter.go
+++ pkg/steve/pkg/clusterapi/responsewriter.go
@@ -3,6 +3,7 @@ package clusterapi
 import (
 	"strings"
 
+	"github.com/gorilla/mux"
 	"github.com/rancher/steve/pkg/schemaserver/types"
 	"github.com/rancher/wrangler/pkg/kv"
 )
@@ -12,18 +13,22 @@ type stripNS struct {
 }
 
 func (s stripNS) Write(apiOp *types.APIRequest, code int, obj types.APIObject) {
-	prefix := apiOp.Namespace + "/"
-	if strings.HasPrefix(obj.ID, prefix) {
-		_, obj.ID = kv.RSplit(obj.ID, "/")
+	if mux.Vars(apiOp.Request)["clusterID"] == "" {
+		prefix := apiOp.Namespace + "/"
+		if strings.HasPrefix(obj.ID, prefix) {
+			_, obj.ID = kv.RSplit(obj.ID, "/")
+		}
 	}
 	s.writer.Write(apiOp, code, obj)
 }
 
 func (s stripNS) WriteList(apiOp *types.APIRequest, code int, obj types.APIObjectList) {
-	prefix := apiOp.Namespace + "/"
-	for i := range obj.Objects {
-		if strings.HasPrefix(obj.Objects[i].ID, prefix) {
-			_, obj.Objects[i].ID = kv.RSplit(obj.Objects[i].ID, "/")
+	if mux.Vars(apiOp.Request)["clusterID"] == "" {
+		prefix := apiOp.Namespace + "/"
+		for i := range obj.Objects {
+			if strings.HasPrefix(obj.Objects[i].ID, prefix) {
+				_, obj.Objects[i].ID = kv.RSplit(obj.Objects[i].ID, "/")
+			}
 		}
 	}
 	s.writer.WriteList(apiOp, code, obj)
