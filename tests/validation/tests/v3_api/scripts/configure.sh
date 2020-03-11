0ae79fd1d09ae92b7609de85d90292c2aa55ba31
rancher-max <rancher-max@users.noreply.github.com>
2020-03-11 15:16:24 -0700

    Initial test architecture and tests for cli validation

--- tests/validation/tests/v3_api/scripts/configure.sh
+++ tests/validation/tests/v3_api/scripts/configure.sh
@@ -5,7 +5,7 @@ set -eu
 
 DEBUG="${DEBUG:-false}"
 
-env | egrep '^(CATTLE_|ADMIN|USER|DO|RANCHER_|AWS_|DEBUG|DEFAULT_|OS_|DOCKER_|CLOUD_|KUBE|BUILD_NUMBER|AZURE).*\=.+' | sort > .env
+env | egrep '^(CATTLE_|ADMIN|USER|DO|RANCHER_|AWS_|DEBUG|LOGLEVEL|DEFAULT_|OS_|DOCKER_|CLOUD_|KUBE|BUILD_NUMBER|AZURE).*\=.+' | sort > .env
 
 if [ "false" != "${DEBUG}" ]; then
     cat .env
---
+++ tests/validation/tests/v3_api/test_cli.py
@@ -0,0 +1,348 @@
+import ast
+import os
+import pytest
+
+from .test_rke_cluster_provisioning import (create_and_validate_custom_host,
+                                            cluster_cleanup)
+from .cli_objects import RancherCli
+from .common import (ADMIN_TOKEN, USER_TOKEN, CATTLE_TEST_URL, CLUSTER_NAME,
+                     AWS_ACCESS_KEY_ID, get_admin_client, get_user_client,
+                     get_user_client_and_cluster)
+
+if_test_multicluster = pytest.mark.skipif(ast.literal_eval(
+    os.environ.get('RANCHER_SKIP_MULTICLUSTER', "False")),
+    reason='Multi-Cluster tests are skipped in the interest of time/cost.')
+
+# Supplying default answers due to issue with multi-cluster app install:
+# https://github.com/rancher/rancher/issues/25514
+MULTICLUSTER_APP_ANSWERS = {
+    "analytics.enabled": "true",
+    "defaultImage": "true",
+    "defaultPorts": "true",
+    "ndm.filters.excludePaths": "loop,fd0,sr0,/dev/ram,/dev/dm-,/dev/md",
+    "ndm.filters.excludeVendors": "CLOUDBYT,OpenEBS",
+    "ndm.sparse.count": "0",
+    "ndm.sparse.enabled": "true",
+    "ndm.sparse.path": "/var/openebs/sparse",
+    "ndm.sparse.size": "10737418240", "policies.monitoring.enabled": "true"
+}
+
+
+def test_cli_context_switch(rancher_cli: RancherCli):
+    rancher_cli.log.info("Testing Context Switching")
+    clusters = rancher_cli.get_clusters()
+    client = get_user_client()
+    projects = client.list_project()
+    assert len(projects) > 0
+    for project in projects:
+        rancher_cli.switch_context(project['id'])
+        cluster_name, project_name = rancher_cli.get_context()
+        assert any(cluster["id"] == project['clusterId']
+                   and cluster["name"] == cluster_name for cluster in clusters)
+        assert project_name == project['name']
+
+
+def test_cli_project_create(remove_cli_resource, rancher_cli: RancherCli):
+    rancher_cli.log.info("Testing Creating Projects")
+    initial_projects = rancher_cli.projects.get_current_projects()
+    project = rancher_cli.projects.create_project(use_context=False)
+    remove_cli_resource("project", project["id"])
+    assert project is not None
+    assert len(initial_projects) == len(
+        rancher_cli.projects.get_current_projects()) - 1
+
+
+def test_cli_project_delete(remove_cli_resource, rancher_cli: RancherCli):
+    rancher_cli.log.info("Testing Deleting Projects")
+    initial_projects = rancher_cli.projects.get_current_projects()
+    project = rancher_cli.projects.create_project(use_context=False)
+    remove_cli_resource("project", project["id"])
+    assert project is not None
+    rancher_cli.projects.delete_project(project["name"])
+    assert len(initial_projects) == len(
+        rancher_cli.projects.get_current_projects())
+
+
+def test_cli_namespace_create(remove_cli_resource, rancher_cli: RancherCli):
+    rancher_cli.log.info("Testing Creating Namespaces")
+    p1 = rancher_cli.projects.create_project()
+    remove_cli_resource("project", p1["id"])
+    namespace = rancher_cli.projects.create_namespace()
+    remove_cli_resource("namespace", namespace)
+    assert len(rancher_cli.projects.get_namespaces()) == 1
+    assert "{}|active".format(
+        namespace) in rancher_cli.projects.get_namespaces()
+
+
+def test_cli_namespace_move(remove_cli_resource, rancher_cli: RancherCli):
+    rancher_cli.log.info("Testing Moving Namespaces")
+    p1 = rancher_cli.projects.create_project()
+    remove_cli_resource("project", p1["id"])
+    namespace = rancher_cli.projects.create_namespace()
+    remove_cli_resource("namespace", namespace)
+    assert len(rancher_cli.projects.get_namespaces()) == 1
+
+    p2 = rancher_cli.projects.create_project(use_context=False)
+    remove_cli_resource("project", p2["id"])
+    rancher_cli.projects.move_namespace(namespace, p2["id"])
+    assert len(rancher_cli.projects.get_namespaces()) == 0
+    rancher_cli.projects.switch_context(p2["id"])
+    assert len(rancher_cli.projects.get_namespaces()) == 1
+    assert "{}|active".format(
+        namespace) in rancher_cli.projects.get_namespaces()
+
+
+def test_cli_namespace_delete(remove_cli_resource, rancher_cli: RancherCli):
+    rancher_cli.log.info("Testing Deleting Namespaces")
+    p1 = rancher_cli.projects.create_project()
+    remove_cli_resource("project", p1["id"])
+    namespace = rancher_cli.projects.create_namespace()
+    remove_cli_resource("namespace", namespace)
+    assert len(rancher_cli.projects.get_namespaces()) == 1
+    assert "{}|active".format(
+        namespace) in rancher_cli.projects.get_namespaces()
+    deleted = rancher_cli.projects.delete_namespace(namespace)
+    assert deleted
+
+
+def test_cli_app_install(remove_cli_resource, rancher_cli: RancherCli):
+    rancher_cli.log.info("Testing Upgrading Apps")
+    initial_app = rancher_cli.apps.install("openebs", "openebs",
+                                           version="1.5.0", timeout=120)
+    remove_cli_resource("apps", initial_app["id"])
+    assert initial_app["state"] == "active"
+    assert initial_app["version"] == "1.5.0"
+
+
+def test_cli_app_upgrade(remove_cli_resource, rancher_cli: RancherCli):
+    rancher_cli.log.info("Testing Rolling Back Apps")
+    initial_app = rancher_cli.apps.install("openebs", "openebs",
+                                           version="1.5.0", timeout=120)
+    remove_cli_resource("apps", initial_app["id"])
+    assert initial_app["version"] == "1.5.0"
+    upgraded_app = rancher_cli.apps.upgrade(initial_app, version="1.6.0")
+    assert upgraded_app["state"] == "active"
+    assert upgraded_app["version"] == "1.6.0"
+
+
+def test_cli_app_rollback(remove_cli_resource, rancher_cli: RancherCli):
+    rancher_cli.log.info("Testing Deleting Apps")
+    initial_app = rancher_cli.apps.install("openebs", "openebs",
+                                           version="1.5.0", timeout=120)
+    remove_cli_resource("apps", initial_app["id"])
+    assert initial_app["version"] == "1.5.0"
+    upgraded_app = rancher_cli.apps.upgrade(initial_app, version="1.6.0")
+    assert upgraded_app["version"] == "1.6.0"
+    rolled_back_app = rancher_cli.apps.rollback(upgraded_app, "1.5.0")
+    assert rolled_back_app["state"] == "active"
+    assert rolled_back_app["version"] == "1.5.0"
+
+
+def test_cli_app_delete(rancher_cli: RancherCli):
+    rancher_cli.log.info("Testing Deleting Apps")
+    initial_app = rancher_cli.apps.install("openebs", "openebs",
+                                           version="1.5.0", timeout=120)
+    deleted = rancher_cli.apps.delete(initial_app)
+    assert deleted
+
+
+@if_test_multicluster
+def test_cli_multiclusterapp_install(custom_cluster, remove_cli_resource,
+                                     rancher_cli: RancherCli):
+    rancher_cli.log.info("Testing Installing Multi-Cluster Apps")
+    # Get list of projects to use and ensure that it is 2 or greater
+    client = get_admin_client()
+    projects = client.list_project()
+    targets = []
+    for project in projects:
+        if project["name"] == "Default":
+            rancher_cli.switch_context(project['id'])
+            cluster_name, project_name = rancher_cli.get_context()
+            if cluster_name in [custom_cluster.name, CLUSTER_NAME]:
+                rancher_cli.log.debug("Using cluster: %s", cluster_name)
+                targets.append(project["id"])
+    assert len(targets) > 1
+
+    initial_app = rancher_cli.mcapps.install("openebs", targets=targets,
+                                             role="cluster-owner",
+                                             values=MULTICLUSTER_APP_ANSWERS,
+                                             version="1.5.0", timeout=120)
+    remove_cli_resource("mcapps", initial_app["name"])
+    assert initial_app["state"] == "active"
+    assert initial_app["version"] == "1.5.0"
+    assert len(initial_app["targets"]) == len(targets)
+    for target in initial_app["targets"]:
+        assert target["state"] == "active"
+        assert target["version"] == "1.5.0"
+
+
+@if_test_multicluster
+def test_cli_multiclusterapp_upgrade(custom_cluster, remove_cli_resource,
+                                     rancher_cli: RancherCli):
+    rancher_cli.log.info("Testing Upgrading Multi-Cluster Apps")
+    # Get list of projects to use and ensure that it is 2 or greater
+    client = get_admin_client()
+    projects = client.list_project()
+    targets = []
+    for project in projects:
+        if project["name"] == "Default":
+            rancher_cli.switch_context(project['id'])
+            cluster_name, project_name = rancher_cli.get_context()
+            if cluster_name in [custom_cluster.name, CLUSTER_NAME]:
+                rancher_cli.log.debug("Using cluster: %s", cluster_name)
+                targets.append(project["id"])
+    assert len(targets) > 1
+
+    initial_app = rancher_cli.mcapps.install("openebs", targets=targets,
+                                             role="cluster-owner",
+                                             values=MULTICLUSTER_APP_ANSWERS,
+                                             version="1.5.0", timeout=120)
+    remove_cli_resource("mcapps", initial_app["name"])
+    assert initial_app["version"] == "1.5.0"
+
+    upgraded_app = rancher_cli.mcapps.upgrade(initial_app, version="1.6.0",
+                                              timeout=120)
+    assert upgraded_app["state"] == "active"
+    assert upgraded_app["version"] == "1.6.0"
+    assert upgraded_app["id"] == initial_app["id"]
+    assert len(upgraded_app["targets"]) == len(targets)
+    for target in upgraded_app["targets"]:
+        assert target["state"] == "active"
+        assert target["version"] == "1.6.0"
+
+
+@if_test_multicluster
+def test_cli_multiclusterapp_rollback(custom_cluster, remove_cli_resource,
+                                      rancher_cli: RancherCli):
+    rancher_cli.log.info("Testing Rolling Back Multi-Cluster Apps")
+    # Get list of projects to use and ensure that it is 2 or greater
+    client = get_admin_client()
+    projects = client.list_project()
+    targets = []
+    for project in projects:
+        if project["name"] == "Default":
+            rancher_cli.switch_context(project['id'])
+            cluster_name, project_name = rancher_cli.get_context()
+            if cluster_name in [custom_cluster.name, CLUSTER_NAME]:
+                rancher_cli.log.debug("Using cluster: %s", cluster_name)
+                targets.append(project["id"])
+    assert len(targets) > 1
+
+    initial_app = rancher_cli.mcapps.install("openebs", targets=targets,
+                                             role="cluster-owner",
+                                             values=MULTICLUSTER_APP_ANSWERS,
+                                             version="1.5.0", timeout=120)
+    remove_cli_resource("mcapps", initial_app["name"])
+    assert initial_app["version"] == "1.5.0"
+    upgraded_app = rancher_cli.mcapps.upgrade(initial_app, version="1.6.0",
+                                              timeout=120)
+    assert upgraded_app["version"] == "1.6.0"
+
+    rolled_back_app = rancher_cli.mcapps.rollback(
+        upgraded_app["name"], initial_app["revision"], timeout=120)
+    assert rolled_back_app["state"] == "active"
+    assert rolled_back_app["version"] == "1.5.0"
+    assert rolled_back_app["id"] == upgraded_app["id"]
+    assert len(rolled_back_app["targets"]) == len(targets)
+    for target in rolled_back_app["targets"]:
+        assert target["state"] == "active"
+        assert target["version"] == "1.5.0"
+
+
+@if_test_multicluster
+def test_cli_multiclusterapp_delete(custom_cluster, remove_cli_resource,
+                                    rancher_cli: RancherCli):
+    rancher_cli.log.info("Testing Deleting Multi-Cluster Apps")
+    # Get list of projects to use and ensure that it is 2 or greater
+    client = get_admin_client()
+    projects = client.list_project()
+    targets = []
+    for project in projects:
+        if project["name"] == "Default":
+            rancher_cli.switch_context(project['id'])
+            cluster_name, project_name = rancher_cli.get_context()
+            if cluster_name in [custom_cluster.name, CLUSTER_NAME]:
+                rancher_cli.log.debug("Using cluster: %s", cluster_name)
+                targets.append(project["id"])
+    assert len(targets) > 1
+
+    initial_app = rancher_cli.mcapps.install("openebs", targets=targets,
+                                             role="cluster-owner",
+                                             values=MULTICLUSTER_APP_ANSWERS,
+                                             version="1.5.0", timeout=120)
+    assert initial_app["version"] == "1.5.0"
+    deleted, apps_deleted = rancher_cli.mcapps.delete(initial_app)
+    assert deleted
+    assert apps_deleted
+
+
+def test_cli_catalog(admin_cli: RancherCli):
+    admin_cli.log.info("Testing Creating and Deleting Catalogs")
+    admin_cli.login(CATTLE_TEST_URL, ADMIN_TOKEN)
+    catalog = admin_cli.catalogs.add("https://git.rancher.io/system-charts",
+                                     branch="dev")
+    assert catalog is not None
+    deleted = admin_cli.catalogs.delete(catalog["name"])
+    assert deleted
+
+
+@pytest.fixture(scope='module')
+def custom_cluster(request, rancher_cli):
+    rancher_cli.log.info("Creating cluster in AWS to test CLI actions that "
+                         "require more than one cluster. Please be patient, "
+                         "as this takes some time...")
+    node_roles = [["controlplane"], ["etcd"],
+                  ["worker"], ["worker"], ["worker"]]
+    cluster, aws_nodes = create_and_validate_custom_host(
+        node_roles, random_cluster_name=True)
+
+    def fin():
+        cluster_cleanup(get_user_client(), cluster, aws_nodes)
+    request.addfinalizer(fin)
+    return cluster
+
+
+@pytest.fixture
+def admin_cli(request, rancher_cli) -> RancherCli:
+    """
+       Login occurs at a global scope, so need to ensure we log back in as the
+       user in a finalizer so that future tests have no issues.
+    """
+    rancher_cli.login(CATTLE_TEST_URL, ADMIN_TOKEN)
+
+    def fin():
+        rancher_cli.login(CATTLE_TEST_URL, USER_TOKEN)
+    request.addfinalizer(fin)
+    return rancher_cli
+
+
+@pytest.fixture(scope='module', autouse="True")
+def rancher_cli(request) -> RancherCli:
+    client, cluster = get_user_client_and_cluster()
+    project_id = client.list_project(name='Default',
+                                     clusterId=cluster.id).data[0]["id"]
+    cli = RancherCli(CATTLE_TEST_URL, USER_TOKEN, project_id)
+
+    def fin():
+        cli.cleanup()
+    request.addfinalizer(fin)
+    return cli
+
+
+@pytest.fixture
+def remove_cli_resource(request, rancher_cli):
+    """Remove a resource after a test finishes even if the test fails.
+
+    How to use:
+      pass this function as an argument of your testing function,
+      then call this function with the resource type and its id
+      as arguments after creating any new resource
+    """
+    def _cleanup(resource, r_id):
+        def clean():
+            rancher_cli.switch_context(rancher_cli.DEFAULT_CONTEXT)
+            rancher_cli.log.info("Cleaning up {}: {}".format(resource, r_id))
+            rancher_cli.run_command("{} delete {}".format(resource, r_id),
+                                    expect_error=True)
+        request.addfinalizer(clean)
+    return _cleanup
