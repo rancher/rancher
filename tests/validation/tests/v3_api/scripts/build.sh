2532ff80101857938028395a695705c48a8c78f7
rancher-max <rancher-max@users.noreply.github.com>
2020-03-11 15:16:24 -0700

    Initial test architecture and tests for cli validation

--- tests/validation/tests/v3_api/scripts/build.sh
+++ tests/validation/tests/v3_api/scripts/build.sh
@@ -6,6 +6,7 @@ set -eu
 DEBUG="${DEBUG:-false}"
 RKE_VERSION="${RKE_VERSION:-v1.0.2}"
 KUBECTL_VERSION="${KUBECTL_VERSION:-v1.16.6}"
+CLI_VERSION="${CLI_VERSION:-v2.3.2}"
 
 if [ "false" != "${DEBUG}" ]; then
     echo "Environment:"
@@ -16,7 +17,7 @@ cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )/../../../" && pwd )"
 
 count=0
 while [[ 3 -gt $count ]]; do    
-    docker build -q -f Dockerfile.v3api --build-arg RKE_VERSION=$RKE_VERSION --build-arg KUBECTL_VERSION=$KUBECTL_VERSION -t rancher-validation-${JOB_NAME}${BUILD_NUMBER} .
+    docker build -q -f Dockerfile.v3api --build-arg CLI_VERSION=$CLI_VERSION --build-arg RKE_VERSION=$RKE_VERSION --build-arg KUBECTL_VERSION=$KUBECTL_VERSION -t rancher-validation-${JOB_NAME}${BUILD_NUMBER} .
 
     if [[ $? -eq 0 ]]; then break; fi
     count=$(($count + 1))
