cd08530b965a8656366b3d192bf39d26ca901b3e
Jeremy Singletary <jeremys@rickyninja.net>
2020-03-22 16:27:55 -0700

    don't pollute returned data with nCalls

--- pkg/auth/providers/ldap/cached_config_test.go
+++ pkg/auth/providers/ldap/cached_config_test.go
@@ -10,22 +10,20 @@ import (
 
 func TestConfigCache_GetConfigMap(t *testing.T) {
 	t.Parallel()
+	remoteConf := &fakeRemoteConfig{}
 	p := &ldapProvider{
 		providerName:    "fake-provider",
-		configMapGetter: NewCachedConfig(&fakeRemoteConfig{}, time.Minute*5),
+		configMapGetter: NewCachedConfig(remoteConf, time.Minute*5),
 	}
 	// 1st call will be a cache miss due to first run
 	got, err := p.configMapGetter.GetConfigMap(p.providerName, metav1.GetOptions{})
 	if err != nil {
 		t.Fatal(err)
 	}
-	ncalls := got["nCalls"].(int)
-	if ncalls != 0 {
-		t.Fatalf("ncalls should be zero, got %d", ncalls)
+	if remoteConf.nCalls != 1 {
+		t.Fatalf("ncalls should be one, got %d", remoteConf.nCalls)
 	}
 	want := map[string]interface{}{
-		// copy nCalls, which is generated in the fake
-		"nCalls":              got["nCalls"],
 		"type":                "fake type",
 		"enabled":             true,
 		"accessMode":          "fake access mode",
@@ -42,9 +40,8 @@ func TestConfigCache_GetConfigMap(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	ncalls = got["nCalls"].(int)
-	if ncalls != 0 {
-		t.Fatalf("ncalls should be zero due to cache hit, got %d", ncalls)
+	if remoteConf.nCalls != 1 {
+		t.Fatalf("ncalls should be one due to cache hit, got %d", remoteConf.nCalls)
 	}
 	configMapEqual(t, got, want)
 	ex := p.configMapGetter.(expirer)
@@ -57,10 +54,8 @@ func TestConfigCache_GetConfigMap(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	want["nCalls"] = got["nCalls"]
-	ncalls = got["nCalls"].(int)
-	if ncalls != 1 {
-		t.Fatalf("ncalls should be 1 due to cache miss related to Expire(), got %d", ncalls)
+	if remoteConf.nCalls != 2 {
+		t.Fatalf("ncalls should be 2 due to cache miss related to Expire(), got %d", remoteConf.nCalls)
 	}
 	configMapEqual(t, got, want)
 }
@@ -87,7 +82,6 @@ type fakeRemoteConfig struct {
 func (f *fakeRemoteConfig) GetConfigMap(name string, opts metav1.GetOptions) (map[string]interface{}, error) {
 	defer func() { f.nCalls++ }()
 	return map[string]interface{}{
-		"nCalls":              f.nCalls,
 		"type":                "fake type",
 		"enabled":             true,
 		"accessMode":          "fake access mode",
