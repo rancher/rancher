aad67d480c04c03ab63d5e14a3dbaf8ac1c9088a
Arvind Iyengar <arvind.iyengar@rancher.com>
2020-04-19 13:34:11 -0700

    Add ui-banners setting
    This commit adds the ui-banners setting to Rancher, which will be used to facilitate customizable header / footer banners on the Rancher UI.
    
    Note: GET requests to the setting are unauthenticated so that they can be queried from the login page.

--- pkg/settings/setting.go
+++ pkg/settings/setting.go
@@ -56,6 +56,7 @@ var (
 	TelemetryOpt                      = NewSetting("telemetry-opt", "prompt")
 	TLSMinVersion                     = NewSetting("tls-min-version", "1.2")
 	TLSCiphers                        = NewSetting("tls-ciphers", "TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305")
+	UIBanners                         = NewSetting("ui-banners", "{}")
 	UIFeedBackForm                    = NewSetting("ui-feedback-form", "")
 	UIIndex                           = NewSetting("ui-index", "https://releases.rancher.com/ui/latest-2.4/index.html")
 	UIPath                            = NewSetting("ui-path", "")
