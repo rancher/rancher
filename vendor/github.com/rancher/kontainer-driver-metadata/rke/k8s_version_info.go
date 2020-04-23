5c3c99a71603d7d39adbd477277409f7a2d9cb6b
rajashree <rajashree.28m@gmail.com>
2020-04-23 08:34:53 -0700

    Update rke vendor

--- vendor/github.com/rancher/kontainer-driver-metadata/rke/k8s_version_info.go
+++ vendor/github.com/rancher/kontainer-driver-metadata/rke/k8s_version_info.go
@@ -80,7 +80,7 @@ func loadK8sVersionInfo() map[string]v3.K8sVersionInfo {
 		// The Calico/Canal template in this version use functions that are only available in RKE v1.0.0 and up
 		// This version includes nodelocal dns only available in RKE v1.0.7 and up
 		"v1.15.11-rancher1-3": {
-			MinRancherVersion: "2.3.7",
+			MinRancherVersion: "2.3.7-rc0",
 			MinRKEVersion:     "1.0.7",
 		},
 		// The Calico/Canal template in this version use functions that are only available in RKE v1.0.0 and up
