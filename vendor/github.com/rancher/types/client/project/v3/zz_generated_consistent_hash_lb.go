c3a193bab5242455fee29eec60849b07f1a4c85a
Caleb Bron <caleb@rancher.com>
2020-05-19 16:24:17 -0700

    Allow istios dest rule LB to be nullable

--- vendor/github.com/rancher/types/client/project/v3/zz_generated_consistent_hash_lb.go
+++ vendor/github.com/rancher/types/client/project/v3/zz_generated_consistent_hash_lb.go
@@ -12,5 +12,5 @@ type ConsistentHashLB struct {
 	HTTPCookie      *HTTPCookie `json:"httpCookie,omitempty" yaml:"httpCookie,omitempty"`
 	HTTPHeaderName  string      `json:"httpHeaderName,omitempty" yaml:"httpHeaderName,omitempty"`
 	MinimumRingSize int64       `json:"minimumRingSize,omitempty" yaml:"minimumRingSize,omitempty"`
-	UseSourceIP     bool        `json:"useSourceIp,omitempty" yaml:"useSourceIp,omitempty"`
+	UseSourceIP     *bool       `json:"useSourceIp,omitempty" yaml:"useSourceIp,omitempty"`
 }
