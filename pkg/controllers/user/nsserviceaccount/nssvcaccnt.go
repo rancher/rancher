2022b2609fd8d26ec9cf4e8a21f059cef9f58108
Prachi Damle <prachi@rancher.com>
2020-05-26 17:33:49 -0700

    Change controller to sync on serviceaccounts instead of namespaces

--- pkg/controllers/user/nsserviceaccount/nssvcaccnt.go
+++ pkg/controllers/user/nsserviceaccount/nssvcaccnt.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"strings"
 
+	"github.com/rancher/norman/types/slice"
 	"github.com/rancher/rancher/pkg/settings"
 	rv1 "github.com/rancher/types/apis/core/v1"
 	"github.com/rancher/types/config"
@@ -12,71 +13,59 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 )
 
-type namespaceSvcAccountHandler struct {
-	serviceAccountLister rv1.ServiceAccountLister
-	serviceAccounts      rv1.ServiceAccountInterface
+type defaultSvcAccountHandler struct {
+	serviceAccounts rv1.ServiceAccountInterface
 }
 
 func Register(ctx context.Context, cluster *config.UserContext) {
-	logrus.Debugf("Registering namespaceSvcAccountHandler for checking default serviceaccount")
-	nsh := &namespaceSvcAccountHandler{
-		serviceAccountLister: cluster.Core.ServiceAccounts("").Controller().Lister(),
-		serviceAccounts:      cluster.Core.ServiceAccounts(""),
+	logrus.Debugf("Registering defaultSvcAccountHandler for checking default service account of system namespaces")
+	nsh := &defaultSvcAccountHandler{
+		serviceAccounts: cluster.Core.ServiceAccounts(""),
 	}
-	cluster.Core.Namespaces("").AddHandler(ctx, "namespaceSvcAccountHandler", nsh.Sync)
+	cluster.Core.ServiceAccounts("").AddHandler(ctx, "defaultSvcAccountHandler", nsh.Sync)
 }
 
-func (nsh *namespaceSvcAccountHandler) Sync(key string, ns *corev1.Namespace) (runtime.Object, error) {
-	if ns == nil || ns.DeletionTimestamp != nil {
+func (nsh *defaultSvcAccountHandler) Sync(key string, sa *corev1.ServiceAccount) (runtime.Object, error) {
+	if sa == nil || sa.DeletionTimestamp != nil {
 		return nil, nil
 	}
-	logrus.Debugf("namespaceSvcAccountHandler: Sync namespace: key=%v", key)
-
-	//handle default svcAccount of system namespaces
-	if err := nsh.handleSystemNS(key); err != nil {
-		logrus.Errorf("namespaceSvcAccountHandler: Sync: error handling default ServiceAccount of namespace key=%v, err=%v", key, err)
+	logrus.Debugf("defaultSvcAccountHandler: Sync service account: key=%v", key)
+	//handle default svcAccount of system namespaces only
+	if err := nsh.handleIfSystemNSDefaultSA(sa); err != nil {
+		logrus.Errorf("defaultSvcAccountHandler: Sync: error handling default ServiceAccount of namespace key=%v, err=%v", key, err)
 	}
 	return nil, nil
 }
 
-func (nsh *namespaceSvcAccountHandler) handleSystemNS(namespace string) error {
+func (nsh *defaultSvcAccountHandler) handleIfSystemNSDefaultSA(defSvcAccnt *corev1.ServiceAccount) error {
+	//check if sa is "default"
+	if defSvcAccnt.Name != "default" {
+		return nil
+	}
+	//check if ns is a system-ns
+	namespace := defSvcAccnt.Namespace
 	if namespace == "kube-system" || namespace == "default" || !nsh.isSystemNS(namespace) {
 		return nil
 	}
-	defSvcAccnt, err := nsh.serviceAccountLister.Get(namespace, "default")
-	if err != nil {
-		logrus.Errorf("namespaceSvcAccountHandler: error listing serviceaccount flag: Sync: key=%v, err=%+v", namespace, err)
-		return err
-	}
-
 	if defSvcAccnt.AutomountServiceAccountToken != nil && *defSvcAccnt.AutomountServiceAccountToken == false {
 		return nil
 	}
-
-	defSvcAccntCopy := defSvcAccnt.DeepCopy()
 	automountServiceAccountToken := false
-	defSvcAccntCopy.AutomountServiceAccountToken = &automountServiceAccountToken
-	logrus.Debugf("namespaceSvcAccountHandler: updating default serviceaccount key=%v", defSvcAccntCopy)
-	_, err = nsh.serviceAccounts.Update(defSvcAccntCopy)
+	defSvcAccnt.AutomountServiceAccountToken = &automountServiceAccountToken
+	logrus.Debugf("defaultSvcAccountHandler: updating default service account key=%v", defSvcAccnt)
+	_, err := nsh.serviceAccounts.Update(defSvcAccnt)
 	if err != nil {
-		logrus.Errorf("namespaceSvcAccountHandler: error updating serviceaccnt flag: Sync: key=%v, err=%+v", namespace, err)
+		logrus.Errorf("defaultSvcAccountHandler: error updating default service account flag for namespace: %v, err=%+v", namespace, err)
 		return err
 	}
 	return nil
 }
 
-func (nsh *namespaceSvcAccountHandler) isSystemNS(namespace string) bool {
+func (nsh *defaultSvcAccountHandler) isSystemNS(namespace string) bool {
 	systemNamespacesStr := settings.SystemNamespaces.Get()
 	if systemNamespacesStr == "" {
 		return false
 	}
-
-	systemNamespaces := make(map[string]bool)
-	splitted := strings.Split(systemNamespacesStr, ",")
-	for _, s := range splitted {
-		ns := strings.TrimSpace(s)
-		systemNamespaces[ns] = true
-	}
-
-	return systemNamespaces[namespace]
+	systemNamespaces := strings.Split(systemNamespacesStr, ",")
+	return slice.ContainsString(systemNamespaces, namespace)
 }
