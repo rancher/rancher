b7084f1373a5aeb81d82677e8d3bb257d2d3aa75
Michael Gianatassio <michael.gianatassio@oracle.com>
2020-07-29 10:43:44 -0400

    remove /rancher when resolving cluster

--- pkg/image/resolve.go
+++ pkg/image/resolve.go
@@ -32,10 +32,6 @@ func Resolve(image string) string {
 func ResolveWithCluster(image string, cluster *v3.Cluster) string {
 	reg := util.GetPrivateRepoURL(cluster)
 	if reg != "" && !strings.HasPrefix(image, reg) {
-		//Images from Dockerhub Library repo, we add rancher prefix when using private registry
-		if !strings.Contains(image, "/") {
-			image = "rancher/" + image
-		}
 		return path.Join(reg, image)
 	}
 
