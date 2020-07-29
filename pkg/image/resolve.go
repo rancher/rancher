4cd80717c390f7dab5bcd73e07757ba68c3d51c4
Michael Gianatassio <michael.gianatassio@oracle.com>
2020-07-29 11:31:54 -0400

    add copyright

--- pkg/image/resolve.go
+++ pkg/image/resolve.go
@@ -1,3 +1,6 @@
+// Copyright (C) 2020, Oracle and/or its affiliates.
+// Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
 package image
 
 import (
