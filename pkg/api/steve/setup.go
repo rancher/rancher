039ddb59fd377a855110c41532782cc77b1bfd07
Darren Shepherd <darren@rancher.com>
2020-07-29 22:17:33 -0700

    Move shell link to management.cattle.io.clusters.

--- pkg/api/steve/setup.go
+++ pkg/api/steve/setup.go
@@ -4,11 +4,15 @@ import (
 	"context"
 
 	"github.com/rancher/rancher/pkg/api/steve/catalog"
+	"github.com/rancher/rancher/pkg/api/steve/clusters"
 	"github.com/rancher/rancher/pkg/wrangler"
 	steve "github.com/rancher/steve/pkg/server"
 )
 
 func Setup(ctx context.Context, server *steve.Server, config *wrangler.Context) error {
+	if err := clusters.Register(ctx, server); err != nil {
+		return err
+	}
 	return catalog.Register(ctx,
 		server,
 		config.Core.Secret(),
