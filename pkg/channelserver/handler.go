4f3cb5f594fadb4b992885d9d17f51f8aa348624
Menna <menna.elmasry@rancher.com>
2020-08-05 21:55:16 +0200

    make rawURL constant

--- pkg/channelserver/handler.go
+++ pkg/channelserver/handler.go
@@ -6,9 +6,8 @@ import (
 	rurl "net/url"
 )
 
-var rawURL = "http://localhost:8115"
-
 func NewProxy(ctx context.Context) *httputil.ReverseProxy {
+	const rawURL = "http://localhost:8115"
 	go Start(ctx)
 	url, _ := rurl.Parse(rawURL)
 	return httputil.NewSingleHostReverseProxy(url)
