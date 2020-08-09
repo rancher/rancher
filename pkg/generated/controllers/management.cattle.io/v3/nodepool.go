94ae32b11bd7ab4eb7d553509ae0d4ecc9dca4be
Darren Shepherd <darren@rancher.com>
2020-08-08 18:02:08 -0700

    Update generated code

--- pkg/generated/controllers/management.cattle.io/v3/nodepool.go
+++ pkg/generated/controllers/management.cattle.io/v3/nodepool.go
@@ -318,6 +318,11 @@ func (a *nodePoolStatusHandler) sync(key string, obj *v3.NodePool) (*v3.NodePool
 		}
 	}
 	if !equality.Semantic.DeepEqual(origStatus, &newStatus) {
+		if a.condition != "" {
+			// Since status has changed, update the lastUpdatedTime
+			a.condition.LastUpdated(&newStatus, time.Now().UTC().Format(time.RFC3339))
+		}
+
 		var newErr error
 		obj.Status = newStatus
 		obj, newErr = a.client.UpdateStatus(obj)
--- pkg/generated/controllers/management.cattle.io/v3/notifier.go
+++ pkg/generated/controllers/management.cattle.io/v3/notifier.go
@@ -318,6 +318,11 @@ func (a *notifierStatusHandler) sync(key string, obj *v3.Notifier) (*v3.Notifier
 		}
 	}
 	if !equality.Semantic.DeepEqual(origStatus, &newStatus) {
+		if a.condition != "" {
+			// Since status has changed, update the lastUpdatedTime
+			a.condition.LastUpdated(&newStatus, time.Now().UTC().Format(time.RFC3339))
+		}
+
 		var newErr error
 		obj.Status = newStatus
 		obj, newErr = a.client.UpdateStatus(obj)
