8e7f5dad2146ad08a8f530494b4641527a4a99f0
Luther Monson <luther.monson@gmail.com>
2020-08-20 15:07:47 -0700

    generate

--- pkg/client/generated/management/v3/zz_generated_kube_controller_service.go
+++ pkg/client/generated/management/v3/zz_generated_kube_controller_service.go
@@ -8,6 +8,9 @@ const (
 	KubeControllerServiceFieldExtraEnv              = "extraEnv"
 	KubeControllerServiceFieldImage                 = "image"
 	KubeControllerServiceFieldServiceClusterIPRange = "serviceClusterIpRange"
+	KubeControllerServiceFieldWindowsExtraArgs      = "winExtraArgs"
+	KubeControllerServiceFieldWindowsExtraBinds     = "winExtraBinds"
+	KubeControllerServiceFieldWindowsExtraEnv       = "winExtraEnv"
 )
 
 type KubeControllerService struct {
@@ -17,4 +20,7 @@ type KubeControllerService struct {
 	ExtraEnv              []string          `json:"extraEnv,omitempty" yaml:"extraEnv,omitempty"`
 	Image                 string            `json:"image,omitempty" yaml:"image,omitempty"`
 	ServiceClusterIPRange string            `json:"serviceClusterIpRange,omitempty" yaml:"serviceClusterIpRange,omitempty"`
+	WindowsExtraArgs      map[string]string `json:"winExtraArgs,omitempty" yaml:"winExtraArgs,omitempty"`
+	WindowsExtraBinds     []string          `json:"winExtraBinds,omitempty" yaml:"winExtraBinds,omitempty"`
+	WindowsExtraEnv       []string          `json:"winExtraEnv,omitempty" yaml:"winExtraEnv,omitempty"`
 }
