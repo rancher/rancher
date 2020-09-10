e172f71e0d743ed55e21992e67c7c0c9b8156e3c
Caleb Bron <caleb@rancher.com>
2020-09-10 13:20:24 -0700

    Move all charts branches to their asset equivalent

--- pkg/settings/setting.go
+++ pkg/settings/setting.go
@@ -83,8 +83,8 @@ var (
 	ClusterTemplateEnforcement        = NewSetting("cluster-template-enforcement", "false")
 	InitialDockerRootDir              = NewSetting("initial-docker-root-dir", "/var/lib/docker")
 	SystemCatalog                     = NewSetting("system-catalog", "external") // Options are 'external' or 'bundled'
-	ChartDefaultBranch                = NewSetting("chart-default-branch", "dev-v2.5")
-	PartnerChartDefaultBranch         = NewSetting("partner-chart-default-branch", "main")
+	ChartDefaultBranch                = NewSetting("chart-default-branch", "dev-v2.5-assets")
+	PartnerChartDefaultBranch         = NewSetting("partner-chart-default-branch", "main-assets")
 	IgnoreNodeName                    = NewSetting("ignore-node-name", "")             // nodes to ignore when syncing v1.node to v3.node
 	FleetDefaultWorkspaceName         = NewSetting("fleet-default-workspace-name", "") // fleetWorkspaceName to assign to clusters with none
 )
