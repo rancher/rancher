b2cfe6767514ee2ac0db8473ce9b978fc7ec915a
Darren Shepherd <darren@rancher.com>
2020-09-10 23:07:18 -0700

    Move userpreference from steve to rancher

--- pkg/api/steve/setup.go
+++ pkg/api/steve/setup.go
@@ -5,11 +5,13 @@ import (
 
 	"github.com/rancher/rancher/pkg/api/steve/catalog"
 	"github.com/rancher/rancher/pkg/api/steve/clusters"
+	"github.com/rancher/rancher/pkg/api/steve/userpreferences"
 	"github.com/rancher/rancher/pkg/wrangler"
 	steve "github.com/rancher/steve/pkg/server"
 )
 
 func Setup(ctx context.Context, server *steve.Server, config *wrangler.Context) error {
+	userpreferences.Register(server.BaseSchemas, server.ClientFactory)
 	if err := clusters.Register(ctx, server); err != nil {
 		return err
 	}
