7c756a40d47f276a1d79ed056d84f8d52ee1a58f
Dan Ramich <daniel@rancher.com>
2020-09-16 17:20:13 -0600

    Change airgap tar dir

--- scripts/k3s-images.sh
+++ scripts/k3s-images.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e -x
 
+cd $(dirname $0)/..
+
+mkdir -p bin
+
 images=$(grep -e 'docker.io/rancher/pause' -e 'docker.io/rancher/coredns-coredns' /usr/tmp/k3s-images.txt)
 xargs -n1 docker pull <<< "${images}"
-docker save -o /go/src/github.com/rancher/rancher/package/k3s-airgap-images.tar ${images}
+docker save -o ./bin/k3s-airgap-images.tar ${images}
