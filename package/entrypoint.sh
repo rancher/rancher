b0919dd45d1833bcf38db342178a6a3f2718a24f
Darren Shepherd <darren@rancher.com>
2020-09-17 22:15:14 -0700

    Detect and require --privileged on container start

--- package/entrypoint.sh
+++ package/entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 set -e
 
+if [ ! -e /run/secrets/kubernetes.io/serviceaccount ] && [ ! -e /dev/kmsg ]; then
+    echo "ERROR: Rancher must be ran with the --privileged flag when running outside of Kubernetes"
+    exit 1
+fi
 exec tini -- rancher --http-listen-port=80 --https-listen-port=443 --audit-log-path=${AUDIT_LOG_PATH} --audit-level=${AUDIT_LEVEL} --audit-log-maxage=${AUDIT_LOG_MAXAGE} --audit-log-maxbackup=${AUDIT_LOG_MAXBACKUP} --audit-log-maxsize=${AUDIT_LOG_MAXSIZE} "${@}"
