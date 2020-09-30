6ba22a30c7108ddb7d1aae5f4ffb3bc37a20714b
Darren Shepherd <darren@rancher.com>
2020-09-29 17:05:13 -0700

    Set node-name to local-node in the Docker container

--- package/entrypoint.sh
+++ package/entrypoint.sh
@@ -5,4 +5,5 @@ if [ ! -e /run/secrets/kubernetes.io/serviceaccount ] && [ ! -e /dev/kmsg ]; the
     echo "ERROR: Rancher must be ran with the --privileged flag when running outside of Kubernetes"
     exit 1
 fi
+rm -f /var/lib/rancher/k3s/server/cred/node-passwd
 exec tini -- rancher --http-listen-port=80 --https-listen-port=443 --audit-log-path=${AUDIT_LOG_PATH} --audit-level=${AUDIT_LEVEL} --audit-log-maxage=${AUDIT_LOG_MAXAGE} --audit-log-maxbackup=${AUDIT_LOG_MAXBACKUP} --audit-log-maxsize=${AUDIT_LOG_MAXSIZE} "${@}"
