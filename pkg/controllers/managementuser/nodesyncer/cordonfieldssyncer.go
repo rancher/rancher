a9ae1288a41990c7d10548c6cff379170ef0468a
kinarashah <kinara@rancher.com>
2020-10-05 17:08:42 -0700

    set default value for ignoreDaemonSets

--- pkg/controllers/managementuser/nodesyncer/cordonfieldssyncer.go
+++ pkg/controllers/managementuser/nodesyncer/cordonfieldssyncer.go
@@ -236,11 +236,17 @@ func (d *nodeDrain) getKubeConfig() (*clientcmdapi.Config, error) {
 }
 
 func getFlags(input *v32.NodeDrainInput) []string {
+	var ignoreDaemonSets bool
+	if input.IgnoreDaemonSets == nil {
+		ignoreDaemonSets = true
+	} else {
+		ignoreDaemonSets = *input.IgnoreDaemonSets
+	}
 	return []string{
 		fmt.Sprintf("--delete-local-data=%v", input.DeleteLocalData),
 		fmt.Sprintf("--force=%v", input.Force),
 		fmt.Sprintf("--grace-period=%v", input.GracePeriod),
-		fmt.Sprintf("--ignore-daemonsets=%v", *input.IgnoreDaemonSets),
+		fmt.Sprintf("--ignore-daemonsets=%v", ignoreDaemonSets),
 		fmt.Sprintf("--timeout=%s", convert.ToString(input.Timeout)+"s")}
 }
 
