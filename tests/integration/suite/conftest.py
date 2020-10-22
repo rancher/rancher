62105c04a78277652a0cbf1b395aba235ae69d8b
mrajashree <rajashree.28m@gmail.com>
2020-10-21 18:18:01 -0700

    Increase default timeout to avoid test failures

--- tests/integration/suite/conftest.py
+++ tests/integration/suite/conftest.py
@@ -42,7 +42,7 @@ IP = get_ip()
 SERVER_URL = 'https://' + IP + ':8443'
 BASE_URL = SERVER_URL + '/v3'
 AUTH_URL = BASE_URL + '-public/localproviders/local?action=login'
-DEFAULT_TIMEOUT = 45
+DEFAULT_TIMEOUT = 120
 DEFAULT_CATALOG = "https://github.com/rancher/integration-test-charts"
 WAIT_HTTP_ERROR_CODES = [404, 405]
 
