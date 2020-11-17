ab1b8b6ae71332e897c175bca07e8a6d3a73098b
Nick Gerace <nick.gerace@rancher.com>
2020-11-17 17:38:31 -0500

    Add rollback actions test note for 2.4 and below

--- tests/integration/suite/test_workloads.py
+++ tests/integration/suite/test_workloads.py
@@ -343,7 +343,10 @@ def test_workload_redeploy(admin_pc, remove_resource):
 
 def test_perform_workload_action_read_only(admin_mc, admin_pc, remove_resource,
                                            user_mc, user_factory):
-    """Tests workload actions with a read-only user and a member user."""
+    """Tests workload actions with a read-only user and a member user.
+       Note: this test exists only in 2.5+ due to pod scheduling on the
+       local cluster. Manually ensure that any changes in 2.4 and below
+       work."""
     client = admin_pc.client
     project = admin_pc.project
     user = user_mc
