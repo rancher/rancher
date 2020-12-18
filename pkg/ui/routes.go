f74773068aa10069f7a69ab5dcb5fbadd33d63b4
Daishan <daishan@rancher.com>
2020-12-18 16:10:13 -0700

    Add favicon under vue asset resource

--- pkg/ui/routes.go
+++ pkg/ui/routes.go
@@ -25,6 +25,8 @@ func New(prefs v3.PreferenceCache) http.Handler {
 	router.Handle("/index.txt", ember.ServeAsset())
 	router.Handle("/robots.txt", ember.ServeAsset())
 	router.Handle("/VERSION.txt", ember.ServeAsset())
+	router.Handle("/favicon.png", vue.ServeFaviconDashboard())
+	router.Handle("/favicon.ico", vue.ServeFaviconDashboard())
 	router.PathPrefix("/api-ui").Handler(ember.ServeAsset())
 	router.PathPrefix("/assets/rancher-ui-driver-linode").Handler(emberAlwaysOffline.ServeAsset())
 	router.PathPrefix("/assets").Handler(ember.ServeAsset())
