7303c801c06f03a12b91df26d08dcb044727d0f9
kinarashah <kinara@rancher.com>
2021-01-19 12:21:59 -0800

    go generate

--- pkg/client/generated/project/v3/zz_generated_load_balancer_ingress.go
+++ pkg/client/generated/project/v3/zz_generated_load_balancer_ingress.go
@@ -4,9 +4,11 @@ const (
 	LoadBalancerIngressType          = "loadBalancerIngress"
 	LoadBalancerIngressFieldHostname = "hostname"
 	LoadBalancerIngressFieldIP       = "ip"
+	LoadBalancerIngressFieldPorts    = "ports"
 )
 
 type LoadBalancerIngress struct {
-	Hostname string `json:"hostname,omitempty" yaml:"hostname,omitempty"`
-	IP       string `json:"ip,omitempty" yaml:"ip,omitempty"`
+	Hostname string       `json:"hostname,omitempty" yaml:"hostname,omitempty"`
+	IP       string       `json:"ip,omitempty" yaml:"ip,omitempty"`
+	Ports    []PortStatus `json:"ports,omitempty" yaml:"ports,omitempty"`
 }
