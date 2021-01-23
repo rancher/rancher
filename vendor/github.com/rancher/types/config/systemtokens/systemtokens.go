e070d6058596e9b9bb0af67f7cb601d5138d689e
rmweir <ricardo.weir1@gmail.com>
2021-01-22 17:15:04 -0700

    Update types

--- vendor/github.com/rancher/types/config/systemtokens/systemtokens.go
+++
@@ -1,6 +0,0 @@
-package systemtokens
-
-type Interface interface {
-	EnsureSystemToken(name, description, kind, username string, overrideTTL *int64, randomize bool) (string, error)
-	DeleteToken(tokenName string) error
-}
