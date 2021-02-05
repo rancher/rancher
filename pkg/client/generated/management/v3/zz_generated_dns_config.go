1b35c01b5867b27eddbf6cb6f53327ec04941b16
Steven Crespo <steven.crespo@rancher.com>
2021-02-05 15:39:34 -0800

    Go generate

--- pkg/client/generated/management/v3/zz_generated_dns_config.go
+++ pkg/client/generated/management/v3/zz_generated_dns_config.go
@@ -5,6 +5,7 @@ const (
 	DNSConfigFieldLinearAutoscalerParams = "linearAutoscalerParams"
 	DNSConfigFieldNodeSelector           = "nodeSelector"
 	DNSConfigFieldNodelocal              = "nodelocal"
+	DNSConfigFieldOptions                = "options"
 	DNSConfigFieldProvider               = "provider"
 	DNSConfigFieldReverseCIDRs           = "reversecidrs"
 	DNSConfigFieldStubDomains            = "stubdomains"
@@ -17,6 +18,7 @@ type DNSConfig struct {
 	LinearAutoscalerParams *LinearAutoscalerParams `json:"linearAutoscalerParams,omitempty" yaml:"linearAutoscalerParams,omitempty"`
 	NodeSelector           map[string]string       `json:"nodeSelector,omitempty" yaml:"nodeSelector,omitempty"`
 	Nodelocal              *Nodelocal              `json:"nodelocal,omitempty" yaml:"nodelocal,omitempty"`
+	Options                map[string]string       `json:"options,omitempty" yaml:"options,omitempty"`
 	Provider               string                  `json:"provider,omitempty" yaml:"provider,omitempty"`
 	ReverseCIDRs           []string                `json:"reversecidrs,omitempty" yaml:"reversecidrs,omitempty"`
 	StubDomains            map[string][]string     `json:"stubdomains,omitempty" yaml:"stubdomains,omitempty"`
