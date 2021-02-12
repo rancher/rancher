f52b8660a86406e7b1371637503ae2d679e66e53
Daishan <daishan@rancher.com>
2021-02-12 09:47:35 -0700

    Bind all fleetworkspace except fleet-loal to restricted admin

--- pkg/controllers/management/restrictedadminrbac/register.go
+++ pkg/controllers/management/restrictedadminrbac/register.go
@@ -6,12 +6,15 @@ import (
 	v3 "github.com/rancher/rancher/pkg/generated/norman/management.cattle.io/v3"
 	v1 "github.com/rancher/rancher/pkg/generated/norman/rbac.authorization.k8s.io/v1"
 	"github.com/rancher/rancher/pkg/types/config"
+	"github.com/rancher/rancher/pkg/wrangler"
+	"github.com/rancher/wrangler/pkg/relatedresource"
 	"k8s.io/client-go/tools/cache"
 )
 
 type rbaccontroller struct {
 	grbLister           v3.GlobalRoleBindingLister
 	grbIndexer          cache.Indexer
+	globalRoleBindings  v3.GlobalRoleBindingInterface
 	roleBindings        v1.RoleBindingInterface
 	rbLister            v1.RoleBindingLister
 	clusters            v3.ClusterInterface
@@ -20,19 +23,21 @@ type rbaccontroller struct {
 	crLister            v1.ClusterRoleLister
 	crbLister           v1.ClusterRoleBindingLister
 	clusterRoleBindings v1.ClusterRoleBindingInterface
+	fleetworkspaces     v3.FleetWorkspaceInterface
 }
 
 const (
 	grbByRoleIndex = "management.cattle.io/grb-by-role"
 )
 
-func Register(ctx context.Context, management *config.ManagementContext) {
+func Register(ctx context.Context, management *config.ManagementContext, wrangler *wrangler.Context) {
 
 	informer := management.Management.GlobalRoleBindings("").Controller().Informer()
 	r := rbaccontroller{
 		clusters:            management.Management.Clusters(""),
 		projects:            management.Management.Projects(""),
 		grbLister:           management.Management.GlobalRoleBindings("").Controller().Lister(),
+		globalRoleBindings:  management.Management.GlobalRoleBindings(""),
 		grbIndexer:          informer.GetIndexer(),
 		roleBindings:        management.RBAC.RoleBindings(""),
 		rbLister:            management.RBAC.RoleBindings("").Controller().Lister(),
@@ -40,8 +45,13 @@ func Register(ctx context.Context, management *config.ManagementContext) {
 		clusterRoles:        management.RBAC.ClusterRoles(""),
 		crbLister:           management.RBAC.ClusterRoleBindings("").Controller().Lister(),
 		clusterRoleBindings: management.RBAC.ClusterRoleBindings(""),
+		fleetworkspaces:     management.Management.FleetWorkspaces(""),
 	}
 
 	r.clusters.AddHandler(ctx, "restrictedAdminsRBACCluster", r.clusterRBACSync)
 	r.projects.AddHandler(ctx, "restrictedAdminsRBACProject", r.projectRBACSync)
+
+	r.globalRoleBindings.AddHandler(ctx, "restrictedAdminGlobalBindingsFleet", r.ensureRestricedAdminForFleet)
+	relatedresource.Watch(ctx, "restricted-admin-fleet", r.enqueueGrb, r.globalRoleBindings.Controller(), wrangler.Mgmt.FleetWorkspace())
+
 }
