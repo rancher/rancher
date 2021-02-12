f5f9fdce95b2d2502ea430298851bf5c7185ebbb
Luther Monson <luther.monson@gmail.com>
2021-02-12 14:23:44 -0700

    generate

--- pkg/client/generated/management/v3/zz_generated_node_pool_spec.go
+++ pkg/client/generated/management/v3/zz_generated_node_pool_spec.go
@@ -6,6 +6,7 @@ const (
 	NodePoolSpecFieldControlPlane            = "controlPlane"
 	NodePoolSpecFieldDeleteNotReadyAfterSecs = "deleteNotReadyAfterSecs"
 	NodePoolSpecFieldDisplayName             = "displayName"
+	NodePoolSpecFieldDrainBeforeDelete       = "drainBeforeDelete"
 	NodePoolSpecFieldEtcd                    = "etcd"
 	NodePoolSpecFieldHostnamePrefix          = "hostnamePrefix"
 	NodePoolSpecFieldNodeAnnotations         = "nodeAnnotations"
@@ -21,6 +22,7 @@ type NodePoolSpec struct {
 	ControlPlane            bool              `json:"controlPlane,omitempty" yaml:"controlPlane,omitempty"`
 	DeleteNotReadyAfterSecs int64             `json:"deleteNotReadyAfterSecs,omitempty" yaml:"deleteNotReadyAfterSecs,omitempty"`
 	DisplayName             string            `json:"displayName,omitempty" yaml:"displayName,omitempty"`
+	DrainBeforeDelete       bool              `json:"drainBeforeDelete,omitempty" yaml:"drainBeforeDelete,omitempty"`
 	Etcd                    bool              `json:"etcd,omitempty" yaml:"etcd,omitempty"`
 	HostnamePrefix          string            `json:"hostnamePrefix,omitempty" yaml:"hostnamePrefix,omitempty"`
 	NodeAnnotations         map[string]string `json:"nodeAnnotations,omitempty" yaml:"nodeAnnotations,omitempty"`
