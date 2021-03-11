156b60e4074f8800a528a2473089409d1266e46c
Arvind Iyengar <arvind.iyengar@rancher.com>
2021-03-10 17:38:23 -0800

    Whitelist wins-upgrade.exe to support upgrades

--- package/windows/bootstrap.ps1
+++ package/windows/bootstrap.ps1
@@ -285,6 +285,7 @@ catch
             "$($CATTLE_PREFIX_PATH)etc\kubernetes\bin\kubelet.exe"
             "$($CATTLE_PREFIX_PATH)etc\nginx\nginx.exe"
             "$($CATTLE_PREFIX_PATH)opt\bin\flanneld.exe"
+            "$($CATTLE_PREFIX_PATH)etc\rancher\wins\wins-upgrade.exe"
         )
     }
 } | ConvertTo-Json -Compress -Depth 32 | Out-File -NoNewline -Encoding utf8 -Force -FilePath "$($CATTLE_PREFIX_PATH)etc\rancher\wins\config"
