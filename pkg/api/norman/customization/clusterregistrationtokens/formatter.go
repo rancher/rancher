2ab813a40f29193c4d2511abc8906a8bcf276fdf
Darren Shepherd <darren@rancher.com>
2021-05-04 15:01:43 -0700

    drop the silent flag from the curl command

--- pkg/api/norman/customization/clusterregistrationtokens/formatter.go
+++ pkg/api/norman/customization/clusterregistrationtokens/formatter.go
@@ -20,8 +20,8 @@ const (
 	commandFormat                 = "kubectl apply -f %s"
 	insecureCommandFormat         = "curl --insecure -sfL %s | kubectl apply -f -"
 	nodeCommandFormat             = "sudo docker run -d --privileged --restart=unless-stopped --net=host -v /etc/kubernetes:/etc/kubernetes -v /var/run:/var/run %s %s --server %s --token %s%s"
-	rke2NodeCommandFormat         = "curl -sfL %s | %s sh -s - --server %s --token %s%s"
-	rke2InsecureNodeCommandFormat = "curl --insecure -sfL %s | %s sh -s - --server %s --token %s%s"
+	rke2NodeCommandFormat         = "curl -fL %s | %s sh -s - --server %s --token %s%s"
+	rke2InsecureNodeCommandFormat = "curl --insecure -fL %s | %s sh -s - --server %s --token %s%s"
 	loginCommandFormat            = "echo \"%s\" | sudo docker login --username %s --password-stdin %s"
 	windowsNodeCommandFormat      = `PowerShell -NoLogo -NonInteractive -Command "& {docker run -v c:\:c:\host %s%s bootstrap --server %s --token %s%s%s | iex}"`
 )
@@ -77,7 +77,7 @@ func (f *Formatter) Formatter(request *types.APIContext, resource *types.RawReso
 				rootURL,
 				token,
 				ca)
-			resource.Values["inseucreNodeCommand"] = fmt.Sprintf(rke2NodeCommandFormat,
+			resource.Values["insecureNodeCommand"] = fmt.Sprintf(rke2InsecureNodeCommandFormat,
 				rootURL+"/system-agent-install.sh",
 				AgentEnvVars(cluster),
 				rootURL,
