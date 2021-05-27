55b28651b3ed54f0152ccb1e2f59218359421e55
Izaac Zavaleta <izaac@rancher.com>
2021-05-27 10:23:18 -0700

    adding v2 chart install test

---
+++ tests/validation/tests/v3_api/resource/terraform/charts_v2/variables.tf
@@ -0,0 +1,43 @@
+variable "cluster_id" {
+  default = ""
+}
+
+variable "api_url" {
+  default = ""
+}
+
+variable "token_key" {
+  default = ""
+}
+
+variable "values_path" {
+  default = ""
+}
+
+variable "monitoring_version" {
+  default = ""
+}
+
+variable "kiali_version" {
+  default = ""
+}
+
+variable "istio_version" {
+  default = ""
+}
+
+variable "logging_version" {
+  default = ""
+}
+
+variable "cis_version" {
+  default = ""
+}
+
+variable "project_id" {
+  default = ""
+}
+
+variable "cluster_provider" {
+  default = ""
+}
