8ed7d79f756f6578bd7beedb8aa093665ec16efc
Prachi Damle <prachi@rancher.com>
2021-06-14 11:26:49 -0700

    Revert "Revert "Restricted Admin permission fixes""
    This reverts commit 98d49dbaf80b83cd6d1b2cc6c01b8cce1c424aa0.

--- pkg/api/norman/store/catalog/store.go
+++ pkg/api/norman/store/catalog/store.go
@@ -7,16 +7,24 @@ import (
 	"github.com/rancher/norman/httperror"
 	"github.com/rancher/norman/types"
 	c "github.com/rancher/rancher/pkg/api/norman/customization/catalog"
+	gaccess "github.com/rancher/rancher/pkg/api/norman/customization/globalnamespaceaccess"
+	v3 "github.com/rancher/rancher/pkg/generated/norman/management.cattle.io/v3"
 	"github.com/rancher/rancher/pkg/settings"
 )
 
 type Store struct {
 	types.Store
+	Users     v3.UserInterface
+	GrbLister v3.GlobalRoleBindingLister
+	GrLister  v3.GlobalRoleLister
 }
 
-func Wrap(store types.Store) types.Store {
+func Wrap(store types.Store, users v3.UserInterface, grbLister v3.GlobalRoleBindingLister, grLister v3.GlobalRoleLister) types.Store {
 	return &Store{
-		store,
+		Store:     store,
+		Users:     users,
+		GrbLister: grbLister,
+		GrLister:  grLister,
 	}
 }
 
@@ -37,11 +45,26 @@ func (s *Store) Update(apiContext *types.APIContext, schema *types.Schema, data 
 		return nil, err
 	}
 	if isSystemCatalog {
-		if strings.ToLower(settings.SystemCatalog.Get()) == "bundled" {
+		isRestrictedAdmin, err := s.isRestrictedAdmin(apiContext)
+		if err != nil {
+			return nil, err
+		}
+		if strings.ToLower(settings.SystemCatalog.Get()) == "bundled" || isRestrictedAdmin {
 			return nil, httperror.NewAPIError(httperror.InvalidBodyContent, fmt.Sprint("not allowed to edit system-library catalog"))
 		}
 	}
 	return s.Store.Update(apiContext, schema, data, id)
+}
+
+func (s *Store) isRestrictedAdmin(apiContext *types.APIContext) (bool, error) {
+	ma := gaccess.MemberAccess{
+		Users:     s.Users,
+		GrLister:  s.GrLister,
+		GrbLister: s.GrbLister,
+	}
+	callerID := apiContext.Request.Header.Get(gaccess.ImpersonateUserHeader)
+
+	return ma.IsRestrictedAdmin(callerID)
 }
 
 // isSystemCatalog checks whether the catalog is the the system catalog maintained by rancher
