58c870be3946e0335a7291ca9d1641b2ef824360
Darren Shepherd <darren@rancher.com>
2021-06-13 21:00:45 -0700

    Fix golint issue

--- pkg/data/management/add.go
+++ pkg/data/management/add.go
@@ -40,9 +40,5 @@ func Add(ctx context.Context, wrangler *wrangler.Context, management *config.Man
 		return err
 	}
 
-	if err := addMachineDrivers(management); err != nil {
-		return err
-	}
-
-	return nil
+	return addMachineDrivers(management)
 }
