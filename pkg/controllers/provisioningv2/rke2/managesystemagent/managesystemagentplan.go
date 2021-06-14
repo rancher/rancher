47622bb754c68576f46e9de52772c346e78e310e
Darren Shepherd <darren@rancher.com>
2021-06-13 20:28:31 -0700

    Update to new system-upgrade-controller chart version

--- pkg/controllers/provisioningv2/rke2/managesystemagent/managesystemagentplan.go
+++ pkg/controllers/provisioningv2/rke2/managesystemagent/managesystemagentplan.go
@@ -25,7 +25,7 @@ func (h *handler) OnChangeInstallSUC(cluster *rancherv1.Cluster, status rancherv
 			DefaultNamespace: namespaces.System,
 			RepoName:         "rancher-charts",
 			Chart:            "system-upgrade-controller",
-			Version:          "0.7.001",
+			Version:          "100.0.0",
 			Values: &v1alpha1.GenericMap{
 				Data: map[string]interface{}{
 					"global": map[string]interface{}{
