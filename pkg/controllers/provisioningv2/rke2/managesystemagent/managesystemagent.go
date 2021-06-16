afed603f94c3eee5e1184d00fb26b1d5a1626b61
Darren Shepherd <darren@rancher.com>
2021-06-15 23:27:40 -0700

    Dynamically determine node roles for system-agent suc plan

--- pkg/controllers/provisioningv2/rke2/managesystemagent/managesystemagent.go
+++ pkg/controllers/provisioningv2/rke2/managesystemagent/managesystemagent.go
@@ -2,96 +2,50 @@ package managesystemagent
 
 import (
 	"context"
+	"crypto/sha256"
+	"encoding/hex"
 	"encoding/json"
+	"fmt"
 	"strings"
 
 	"github.com/rancher/fleet/pkg/apis/fleet.cattle.io/v1alpha1"
 	rancherv1 "github.com/rancher/rancher/pkg/apis/provisioning.cattle.io/v1"
+	v3 "github.com/rancher/rancher/pkg/generated/controllers/management.cattle.io/v3"
 	rocontrollers "github.com/rancher/rancher/pkg/generated/controllers/provisioning.cattle.io/v1"
 	namespaces "github.com/rancher/rancher/pkg/namespace"
 	"github.com/rancher/rancher/pkg/settings"
+	"github.com/rancher/rancher/pkg/systemtemplate"
 	"github.com/rancher/rancher/pkg/wrangler"
 	upgradev1 "github.com/rancher/system-upgrade-controller/pkg/apis/upgrade.cattle.io/v1"
+	"github.com/rancher/wrangler/pkg/generic"
+	"github.com/rancher/wrangler/pkg/gvk"
 	"github.com/rancher/wrangler/pkg/name"
 	corev1 "k8s.io/api/core/v1"
+	rbacv1 "k8s.io/api/rbac/v1"
+	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 )
 
-var (
-	workerSelector = metav1.LabelSelector{
-		MatchExpressions: []metav1.LabelSelectorRequirement{
-			{
-				Key:      "node-role.kubernetes.io/etcd",
-				Operator: metav1.LabelSelectorOpNotIn,
-				Values:   []string{"true"},
-			},
-			{
-				Key:      "node-role.kubernetes.io/control-plane",
-				Operator: metav1.LabelSelectorOpNotIn,
-				Values:   []string{"true"},
-			},
-			{
-				Key:      "beta.kubernetes.io/os",
-				Operator: metav1.LabelSelectorOpNotIn,
-				Values:   []string{"windows"},
-			},
-		},
-	}
-	controlPlaneSelector = metav1.LabelSelector{
-		MatchExpressions: []metav1.LabelSelectorRequirement{
-			{
-				Key:      "node-role.kubernetes.io/etcd",
-				Operator: metav1.LabelSelectorOpNotIn,
-				Values:   []string{"true"},
-			},
-			{
-				Key:      "node-role.kubernetes.io/control-plane",
-				Operator: metav1.LabelSelectorOpIn,
-				Values:   []string{"true"},
-			},
-		},
-	}
-	etcdSelector = metav1.LabelSelector{
-		MatchExpressions: []metav1.LabelSelectorRequirement{
-			{
-				Key:      "node-role.kubernetes.io/etcd",
-				Operator: metav1.LabelSelectorOpIn,
-				Values:   []string{"true"},
-			},
-			{
-				Key:      "node-role.kubernetes.io/control-plane",
-				Operator: metav1.LabelSelectorOpNotIn,
-				Values:   []string{"true"},
-			},
-		},
-	}
-	controlPlaneAndEtcdSelector = metav1.LabelSelector{
-		MatchExpressions: []metav1.LabelSelectorRequirement{
-			{
-				Key:      "node-role.kubernetes.io/etcd",
-				Operator: metav1.LabelSelectorOpIn,
-				Values:   []string{"true"},
-			},
-			{
-				Key:      "node-role.kubernetes.io/control-plane",
-				Operator: metav1.LabelSelectorOpIn,
-				Values:   []string{"true"},
-			},
-		},
-	}
-)
-
-type handler struct{}
+type handler struct {
+	clusterRegistrationTokens v3.ClusterRegistrationTokenCache
+}
 
 func Register(ctx context.Context, clients *wrangler.Context) {
-	h := &handler{}
+	h := &handler{
+		clusterRegistrationTokens: clients.Mgmt.ClusterRegistrationToken().Cache(),
+	}
 	rocontrollers.RegisterClusterGeneratingHandler(ctx, clients.Provisioning.Cluster(),
 		clients.Apply.
 			WithSetOwnerReference(false, false).
 			WithCacheTypes(clients.Fleet.Bundle(),
-				clients.Provisioning.Cluster()),
-		"", "manage-system-agent", h.OnChange, nil)
+				clients.Provisioning.Cluster(),
+				clients.Core.Secret(),
+				clients.RBAC.RoleBinding(),
+				clients.RBAC.Role()),
+		"", "manage-system-agent", h.OnChange, &generic.GeneratingHandlerOptions{
+			AllowCrossNamespace: true,
+		})
 	rocontrollers.RegisterClusterGeneratingHandler(ctx, clients.Provisioning.Cluster(),
 		clients.Apply.
 			WithSetOwnerReference(false, false).
@@ -105,7 +59,48 @@ func (h *handler) OnChange(cluster *rancherv1.Cluster, status rancherv1.ClusterS
 		return nil, status, nil
 	}
 
-	bundle := &v1alpha1.Bundle{
+	var (
+		secretName = "steve-aggregation"
+		result     []runtime.Object
+	)
+
+	if cluster.Status.ClusterName == "local" && cluster.Namespace == "fleet-local" {
+		secretName += "-local-"
+
+		token, err := h.clusterRegistrationTokens.Get(cluster.Status.ClusterName, "default-token")
+		if err != nil {
+			return nil, status, err
+		}
+		if token.Status.Token == "" {
+			return nil, status, fmt.Errorf("token not yet generated for %s/%s", token.Namespace, token.Name)
+		}
+
+		digest := sha256.New()
+		digest.Write([]byte(settings.InternalServerURL.Get()))
+		digest.Write([]byte(token.Status.Token))
+		digest.Write([]byte(systemtemplate.InternalCAChecksum()))
+		d := digest.Sum(nil)
+		secretName += hex.EncodeToString(d[:])[:12]
+
+		result = append(result, &corev1.Secret{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      secretName,
+				Namespace: namespaces.System,
+			},
+			Data: map[string][]byte{
+				"CATTLE_SERVER":      []byte(settings.InternalServerURL.Get()),
+				"CATTLE_TOKEN":       []byte(token.Status.Token),
+				"CATTLE_CA_CHECKSUM": []byte(systemtemplate.InternalCAChecksum()),
+			},
+		})
+	}
+
+	resources, err := ToResources(installer(len(cluster.Spec.RKEConfig.NodeConfig) == 0, secretName))
+	if err != nil {
+		return nil, status, err
+	}
+
+	result = append(result, &v1alpha1.Bundle{
 		ObjectMeta: metav1.ObjectMeta{
 			Namespace: cluster.Namespace,
 			Name:      name.SafeConcatName(cluster.Name, "managed", "system", "agent"),
@@ -114,24 +109,7 @@ func (h *handler) OnChange(cluster *rancherv1.Cluster, status rancherv1.ClusterS
 			BundleDeploymentOptions: v1alpha1.BundleDeploymentOptions{
 				DefaultNamespace: namespaces.System,
 			},
-			Resources: []v1alpha1.BundleResource{
-				{
-					Name:    "cp.yaml",
-					Content: installer("cp", "false", "false", "true", cluster.Spec.AgentEnvVars, &controlPlaneSelector),
-				},
-				{
-					Name:    "etcd.yaml",
-					Content: installer("etcd", "false", "true", "false", cluster.Spec.AgentEnvVars, &etcdSelector),
-				},
-				{
-					Name:    "cp-and-etcd.yaml",
-					Content: installer("cp-and-etcd", "false", "true", "true", cluster.Spec.AgentEnvVars, &controlPlaneAndEtcdSelector),
-				},
-				{
-					Name:    "worker.yaml",
-					Content: installer("worker", "true", "false", "false", cluster.Spec.AgentEnvVars, &workerSelector),
-				},
-			},
+			Resources: resources,
 			Targets: []v1alpha1.BundleTarget{
 				{
 					ClusterName: cluster.Name,
@@ -146,68 +124,123 @@ func (h *handler) OnChange(cluster *rancherv1.Cluster, status rancherv1.ClusterS
 				},
 			},
 		},
-	}
+	})
 
-	return []runtime.Object{
-		bundle,
-	}, status, nil
+	return result, status, nil
 }
 
-func installer(name, worker, etcd, controlPlane string, envs []corev1.EnvVar, selector *metav1.LabelSelector) string {
+func installer(allWorkers bool, secretName string) []runtime.Object {
 	image := strings.SplitN(settings.SystemAgentUpgradeImage.Get(), ":", 2)
 	version := "latest"
 	if len(image) == 2 {
 		version = image[1]
 	}
 
-	plan := &upgradev1.Plan{
-		TypeMeta: metav1.TypeMeta{
-			Kind:       "Plan",
-			APIVersion: "upgrade.cattle.io/v1",
-		},
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "system-agent-upgrader-" + name,
-			Namespace: namespaces.System,
-		},
-		Spec: upgradev1.PlanSpec{
-			Concurrency: 10,
-			Version:     version,
-			Tolerations: []corev1.Toleration{{
-				Operator: corev1.TolerationOpExists,
-			}},
-			NodeSelector: selector,
-			Upgrade: &upgradev1.ContainerSpec{
-				Image:   settings.PrefixPrivateRegistry(image[0]),
-				Command: nil,
-				Args:    nil,
-				Env: append(envs, []corev1.EnvVar{
-					{
-						Name:  "CATTLE_ROLE_WORKER",
-						Value: worker,
-					},
-					{
-						Name:  "CATTLE_ROLE_ETCD",
-						Value: etcd,
-					},
-					{
-						Name:  "CATTLE_ROLE_CONTROL_PLANE",
-						Value: controlPlane,
-					},
-				}...),
-				EnvFrom: []corev1.EnvFromSource{{
-					SecretRef: &corev1.SecretEnvSource{
-						LocalObjectReference: corev1.LocalObjectReference{
-							Name: "steve-aggregation",
-						},
-					},
+	env := []corev1.EnvVar{{
+		Name:  "CATTLE_SERVER_QUERY",
+		Value: "?internal=true",
+	}}
+
+	if allWorkers {
+		env = append(env, corev1.EnvVar{
+			Name:  "CATTLE_ROLE_WORKER",
+			Value: "true",
+		})
+	}
+
+	return []runtime.Object{
+		&upgradev1.Plan{
+			TypeMeta: metav1.TypeMeta{
+				Kind:       "Plan",
+				APIVersion: "upgrade.cattle.io/v1",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "system-agent-upgrader",
+				Namespace: namespaces.System,
+			},
+			Spec: upgradev1.PlanSpec{
+				Concurrency: 10,
+				Version:     version,
+				Tolerations: []corev1.Toleration{{
+					Operator: corev1.TolerationOpExists,
 				}},
+				NodeSelector:       &metav1.LabelSelector{},
+				ServiceAccountName: "system-agent-upgrader",
+				Upgrade: &upgradev1.ContainerSpec{
+					Image: settings.PrefixPrivateRegistry(image[0]),
+					Env:   env,
+					EnvFrom: []corev1.EnvFromSource{{
+						SecretRef: &corev1.SecretEnvSource{
+							LocalObjectReference: corev1.LocalObjectReference{
+								Name: secretName,
+							},
+						},
+					}},
+				},
+			},
+		},
+		&corev1.ServiceAccount{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "system-agent-upgrader",
+				Namespace: namespaces.System,
+			},
+		},
+		&rbacv1.ClusterRole{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "system-agent-upgrader",
+			},
+			Rules: []rbacv1.PolicyRule{{
+				Verbs:     []string{"get"},
+				APIGroups: []string{""},
+				Resources: []string{"nodes"},
+			}},
+		},
+		&rbacv1.ClusterRoleBinding{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "system-agent-upgrader",
+			},
+			Subjects: []rbacv1.Subject{{
+				Kind:      "ServiceAccount",
+				Name:      "system-agent-upgrader",
+				Namespace: namespaces.System,
+			}},
+			RoleRef: rbacv1.RoleRef{
+				APIGroup: "rbac.authorization.k8s.io",
+				Kind:     "ClusterRole",
+				Name:     "system-agent-upgrader",
 			},
 		},
 	}
+}
 
-	file, err := json.Marshal(plan)
-	if err != nil {
-		panic(err)
+func ToResources(objs []runtime.Object) (result []v1alpha1.BundleResource, err error) {
+	for _, obj := range objs {
+		obj = obj.DeepCopyObject()
+		if err := gvk.Set(obj); err != nil {
+			return nil, fmt.Errorf("failed to set gvk: %w", err)
+		}
+
+		typeMeta, err := meta.TypeAccessor(obj)
+		if err != nil {
+			return nil, err
+		}
+
+		meta, err := meta.Accessor(obj)
+		if err != nil {
+			return nil, err
+		}
+
+		data, err := json.Marshal(obj)
+		if err != nil {
+			return nil, err
+		}
+
+		digest := sha256.Sum256(data)
+		filename := name.SafeConcatName(typeMeta.GetKind(), meta.GetNamespace(), meta.GetName(), hex.EncodeToString(digest[:])[:12]) + ".yaml"
+		result = append(result, v1alpha1.BundleResource{
+			Name:    filename,
+			Content: string(data),
+		})
 	}
-	return string(file)
+	return
 }
