eb71da05d5ae1bb9fbda778e7e2aa307cb42e4f3
Ricardo Weir <ricardo.weir1@gmail.com>
2021-06-25 14:52:37 -0700

    Update pipeline images

--- pkg/apis/management.cattle.io/v3/tools_system_images.go
+++ pkg/apis/management.cattle.io/v3/tools_system_images.go
@@ -11,13 +11,13 @@ var (
 	}{
 		PipelineSystemImages: projectv3.PipelineSystemImages{
 			Jenkins:       "rancher/pipeline-jenkins-server:v0.1.4",
-			JenkinsJnlp:   "rancher/mirrored-jenkins-jnlp-slave:3.35-4",
-			AlpineGit:     "rancher/pipeline-tools:v0.1.15",
+			JenkinsJnlp:   "rancher/mirrored-jenkins-jnlp-slave:4.7-1",
+			AlpineGit:     "rancher/pipeline-tools:v0.1.16",
 			PluginsDocker: "rancher/mirrored-plugins-docker:19.03.8",
 			Minio:         "rancher/mirrored-minio-minio:RELEASE.2020-07-13T18-09-56Z",
 			Registry:      "registry:2",
-			RegistryProxy: "rancher/pipeline-tools:v0.1.15",
-			KubeApply:     "rancher/pipeline-tools:v0.1.15",
+			RegistryProxy: "rancher/pipeline-tools:v0.1.16",
+			KubeApply:     "rancher/pipeline-tools:v0.1.16",
 		},
 		AuthSystemImages: AuthSystemImages{
 			KubeAPIAuth: "rancher/kube-api-auth:v0.1.4",
