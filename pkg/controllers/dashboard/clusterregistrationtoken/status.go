75d3fe76bfa0d13d64257c24196b3aa52c41f9de
Luther Monson <luther.monson@gmail.com>
2021-08-05 09:05:20 -0700

    Merge pull request #33978 from phillipsj/bugfix/rke2-insecure-comman-missing-labeld
    Adding missing label to the insecure command.

--- pkg/controllers/dashboard/clusterregistrationtoken/status.go
+++ pkg/controllers/dashboard/clusterregistrationtoken/status.go
@@ -22,7 +22,7 @@ const (
 	nodeCommandFormat                    = "sudo docker run -d --privileged --restart=unless-stopped --net=host -v /etc/kubernetes:/etc/kubernetes -v /var/run:/var/run %s %s --server %s --token %s%s"
 	rke2NodeCommandFormat                = "curl -fL %s | sudo %s sh -s - --server %s --label 'cattle.io/os=linux' --token %s%s"
 	rke2WindowsNodeCommandFormat         = `curl.exe -fL %s -o install.ps1 && powershell.exe -ExecutionPolicy Bypass -Command "./install.ps1 -Server %s -Label 'cattle.io/os=windows' -Token %s -Worker%s"`
-	rke2InsecureNodeCommandFormat        = "curl --insecure -fL %s | sudo %s sh -s - --server %s --token %s%s"
+	rke2InsecureNodeCommandFormat        = "curl --insecure -fL %s | sudo %s sh -s - --server %s --label 'cattle.io/os=linux' --token %s%s"
 	rke2InsecureWindowsNodeCommandFormat = `curl.exe --insecure -fL %s -o install.ps1 && powershell.exe -ExecutionPolicy Bypass -Command "./install.ps1 -Server %s -Label 'cattle.io/os=windows' -Token %s -Worker%s"`
 	loginCommandFormat                   = "echo \"%s\" | sudo docker login --username %s --password-stdin %s"
 	windowsNodeCommandFormat             = `PowerShell -NoLogo -NonInteractive -Command "& {docker run -v c:\:c:\host %s%s bootstrap --server %s --token %s%s%s | iex}"`
