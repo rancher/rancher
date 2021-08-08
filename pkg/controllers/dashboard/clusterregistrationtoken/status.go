634dcecfb9b3c476e9d943a7e499fefbbb368c41
Kinara Shah <kinara@rancher.com>
2021-08-08 12:58:24 -0700

    Merge pull request #34050 from phillipsj/bugfix/windows-cmd-to-ps
    Improving windows registration command.

--- pkg/controllers/dashboard/clusterregistrationtoken/status.go
+++ pkg/controllers/dashboard/clusterregistrationtoken/status.go
@@ -21,9 +21,9 @@ const (
 	insecureCommandFormat                = "curl --insecure -sfL %s | kubectl apply -f -"
 	nodeCommandFormat                    = "sudo docker run -d --privileged --restart=unless-stopped --net=host -v /etc/kubernetes:/etc/kubernetes -v /var/run:/var/run %s %s --server %s --token %s%s"
 	rke2NodeCommandFormat                = "curl -fL %s | sudo %s sh -s - --server %s --label 'cattle.io/os=linux' --token %s%s"
-	rke2WindowsNodeCommandFormat         = `curl.exe -fL %s -o install.ps1 && powershell.exe -ExecutionPolicy Bypass -Command "./install.ps1 -Server %s -Label 'cattle.io/os=windows' -Token %s -Worker%s"`
+	rke2WindowsNodeCommandFormat         = `curl.exe -fL %s -o install.ps1; Set-ExecutionPolicy Bypass -Scope Process -Force; ./install.ps1 -Server %s -Label 'cattle.io/os=windows' -Token %s -Worker%s`
 	rke2InsecureNodeCommandFormat        = "curl --insecure -fL %s | sudo %s sh -s - --server %s --label 'cattle.io/os=linux' --token %s%s"
-	rke2InsecureWindowsNodeCommandFormat = `curl.exe --insecure -fL %s -o install.ps1 && powershell.exe -ExecutionPolicy Bypass -Command "./install.ps1 -Server %s -Label 'cattle.io/os=windows' -Token %s -Worker%s"`
+	rke2InsecureWindowsNodeCommandFormat = `curl.exe --insecure -fL %s -o install.ps1; Set-ExecutionPolicy Bypass -Scope Process -Force; ./install.ps1 -Server %s -Label 'cattle.io/os=windows' -Token %s -Worker%s`
 	loginCommandFormat                   = "echo \"%s\" | sudo docker login --username %s --password-stdin %s"
 	windowsNodeCommandFormat             = `PowerShell -NoLogo -NonInteractive -Command "& {docker run -v c:\:c:\host %s%s bootstrap --server %s --token %s%s%s | iex}"`
 )
