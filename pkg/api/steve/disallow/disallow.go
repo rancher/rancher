7947d13f291928d831571bda1e11095ec6afa02a
Ricardo Weir <ricardo.weir@suse.com>
2021-08-19 12:37:53 -0700

    Disallow certain norman types

---
+++ pkg/api/steve/disallow/disallow.go
@@ -0,0 +1,55 @@
+package disallow
+
+import (
+	"net/http"
+
+	"github.com/rancher/apiserver/pkg/types"
+	"github.com/rancher/steve/pkg/attributes"
+	schema2 "github.com/rancher/steve/pkg/schema"
+	steve "github.com/rancher/steve/pkg/server"
+)
+
+var (
+	allowPost = map[string]bool{
+		"settings": true,
+	}
+	allowPut = map[string]bool{
+		"features": true,
+		"settings": true,
+	}
+	disallowGet = map[string]bool{
+		"pipelines":                 true,
+		"pipelineexecutions":        true,
+		"pipelinesettings":          true,
+		"preferences":               true,
+		"sourcecodecredentials":     true,
+		"sourcecodeproviderconfigs": true,
+		"sourcecoderepositories":    true,
+		"templatecontents":          true,
+		"templates":                 true,
+		"templateversions":          true,
+		"tokens":                    true,
+	}
+)
+
+func Register(server *steve.Server) {
+	server.SchemaFactory.AddTemplate(schema2.Template{
+		Customize: func(schema *types.APISchema) {
+			gr := attributes.GR(schema)
+			if gr.Group == "management.cattle.io" || gr.Group == "project.cattle.io" {
+				attributes.AddDisallowMethods(schema,
+					http.MethodPatch,
+					http.MethodDelete)
+				if !allowPut[gr.Resource] {
+					attributes.AddDisallowMethods(schema, http.MethodPut)
+				}
+				if !allowPut[gr.Resource] {
+					attributes.AddDisallowMethods(schema, http.MethodPost)
+				}
+				if disallowGet[gr.Resource] {
+					attributes.AddDisallowMethods(schema, http.MethodGet)
+				}
+			}
+		},
+	})
+}
