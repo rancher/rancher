ee0c091440daa120031341dbe3ed52433b6067f5
Nick Gerace <nick.gerace@suse.com>
2021-08-23 12:41:39 -0400

    Go generate

--- pkg/client/generated/management/v3/zz_generated_private_registry.go
+++ pkg/client/generated/management/v3/zz_generated_private_registry.go
@@ -1,18 +1,18 @@
 package client
 
 const (
-	PrivateRegistryType                  = "privateRegistry"
-	PrivateRegistryFieldCredentialPlugin = "credentialPlugin"
-	PrivateRegistryFieldIsDefault        = "isDefault"
-	PrivateRegistryFieldPassword         = "password"
-	PrivateRegistryFieldURL              = "url"
-	PrivateRegistryFieldUser             = "user"
+	PrivateRegistryType                     = "privateRegistry"
+	PrivateRegistryFieldECRCredentialPlugin = "ecrCredentialPlugin"
+	PrivateRegistryFieldIsDefault           = "isDefault"
+	PrivateRegistryFieldPassword            = "password"
+	PrivateRegistryFieldURL                 = "url"
+	PrivateRegistryFieldUser                = "user"
 )
 
 type PrivateRegistry struct {
-	CredentialPlugin map[string]string `json:"credentialPlugin,omitempty" yaml:"credentialPlugin,omitempty"`
-	IsDefault        bool              `json:"isDefault,omitempty" yaml:"isDefault,omitempty"`
-	Password         string            `json:"password,omitempty" yaml:"password,omitempty"`
-	URL              string            `json:"url,omitempty" yaml:"url,omitempty"`
-	User             string            `json:"user,omitempty" yaml:"user,omitempty"`
+	ECRCredentialPlugin *ECRCredentialPlugin `json:"ecrCredentialPlugin,omitempty" yaml:"ecrCredentialPlugin,omitempty"`
+	IsDefault           bool                 `json:"isDefault,omitempty" yaml:"isDefault,omitempty"`
+	Password            string               `json:"password,omitempty" yaml:"password,omitempty"`
+	URL                 string               `json:"url,omitempty" yaml:"url,omitempty"`
+	User                string               `json:"user,omitempty" yaml:"user,omitempty"`
 }
