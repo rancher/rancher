84a8ceb6beaa78c3283042f4ec28f675ddd7e097
igomez06 <israel.gomez@suse.com>
2021-12-09 09:04:20 -0700

    Jenkins file and other configurations for a jenkins run.

---
+++ tests/v2/validation/configure.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -x
+set -eu
+
+DEBUG="${DEBUG:-false}"
+
+env | egrep '^(ARM_|CATTLE_|ADMIN|USER|DO|RANCHER_|AWS_|DEBUG|LOGLEVEL|DEFAULT_|OS_|DOCKER_|CLOUD_|KUBE|BUILD_NUMBER|AZURE|TEST_).*\=.+' | sort > .env
+
+if [ "false" != "${DEBUG}" ]; then
+    cat .env
+fi
