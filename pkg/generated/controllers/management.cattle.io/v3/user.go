88553fb3d72cd0c7180dc5b75e1bdb5a098415cb
Chris Kim <oats87g@gmail.com>
2022-01-05 09:39:41 -0800

    Bump copyright year to 2022
    Signed-off-by: Chris Kim <oats87g@gmail.com>

--- pkg/generated/controllers/management.cattle.io/v3/user.go
+++ pkg/generated/controllers/management.cattle.io/v3/user.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2021 Rancher Labs, Inc.
+Copyright 2022 Rancher Labs, Inc.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
