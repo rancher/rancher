753b48c63c676b43d9a7dbb2eb072be6269b7d32
Colleen Murphy <colleen.murphy@suse.com>
2022-02-03 13:43:22 -0800

    go generate

--- pkg/client/generated/project/v3/zz_generated_source_code_provider_config.go
+++ pkg/client/generated/project/v3/zz_generated_source_code_provider_config.go
@@ -5,35 +5,37 @@ import (
 )
 
 const (
-	SourceCodeProviderConfigType                 = "sourceCodeProviderConfig"
-	SourceCodeProviderConfigFieldAnnotations     = "annotations"
-	SourceCodeProviderConfigFieldCreated         = "created"
-	SourceCodeProviderConfigFieldCreatorID       = "creatorId"
-	SourceCodeProviderConfigFieldEnabled         = "enabled"
-	SourceCodeProviderConfigFieldLabels          = "labels"
-	SourceCodeProviderConfigFieldName            = "name"
-	SourceCodeProviderConfigFieldNamespaceId     = "namespaceId"
-	SourceCodeProviderConfigFieldOwnerReferences = "ownerReferences"
-	SourceCodeProviderConfigFieldProjectID       = "projectId"
-	SourceCodeProviderConfigFieldRemoved         = "removed"
-	SourceCodeProviderConfigFieldType            = "type"
-	SourceCodeProviderConfigFieldUUID            = "uuid"
+	SourceCodeProviderConfigType                  = "sourceCodeProviderConfig"
+	SourceCodeProviderConfigFieldAnnotations      = "annotations"
+	SourceCodeProviderConfigFieldCreated          = "created"
+	SourceCodeProviderConfigFieldCreatorID        = "creatorId"
+	SourceCodeProviderConfigFieldCredentialSecret = "credentialSecret"
+	SourceCodeProviderConfigFieldEnabled          = "enabled"
+	SourceCodeProviderConfigFieldLabels           = "labels"
+	SourceCodeProviderConfigFieldName             = "name"
+	SourceCodeProviderConfigFieldNamespaceId      = "namespaceId"
+	SourceCodeProviderConfigFieldOwnerReferences  = "ownerReferences"
+	SourceCodeProviderConfigFieldProjectID        = "projectId"
+	SourceCodeProviderConfigFieldRemoved          = "removed"
+	SourceCodeProviderConfigFieldType             = "type"
+	SourceCodeProviderConfigFieldUUID             = "uuid"
 )
 
 type SourceCodeProviderConfig struct {
 	types.Resource
-	Annotations     map[string]string `json:"annotations,omitempty" yaml:"annotations,omitempty"`
-	Created         string            `json:"created,omitempty" yaml:"created,omitempty"`
-	CreatorID       string            `json:"creatorId,omitempty" yaml:"creatorId,omitempty"`
-	Enabled         bool              `json:"enabled,omitempty" yaml:"enabled,omitempty"`
-	Labels          map[string]string `json:"labels,omitempty" yaml:"labels,omitempty"`
-	Name            string            `json:"name,omitempty" yaml:"name,omitempty"`
-	NamespaceId     string            `json:"namespaceId,omitempty" yaml:"namespaceId,omitempty"`
-	OwnerReferences []OwnerReference  `json:"ownerReferences,omitempty" yaml:"ownerReferences,omitempty"`
-	ProjectID       string            `json:"projectId,omitempty" yaml:"projectId,omitempty"`
-	Removed         string            `json:"removed,omitempty" yaml:"removed,omitempty"`
-	Type            string            `json:"type,omitempty" yaml:"type,omitempty"`
-	UUID            string            `json:"uuid,omitempty" yaml:"uuid,omitempty"`
+	Annotations      map[string]string `json:"annotations,omitempty" yaml:"annotations,omitempty"`
+	Created          string            `json:"created,omitempty" yaml:"created,omitempty"`
+	CreatorID        string            `json:"creatorId,omitempty" yaml:"creatorId,omitempty"`
+	CredentialSecret string            `json:"credentialSecret,omitempty" yaml:"credentialSecret,omitempty"`
+	Enabled          bool              `json:"enabled,omitempty" yaml:"enabled,omitempty"`
+	Labels           map[string]string `json:"labels,omitempty" yaml:"labels,omitempty"`
+	Name             string            `json:"name,omitempty" yaml:"name,omitempty"`
+	NamespaceId      string            `json:"namespaceId,omitempty" yaml:"namespaceId,omitempty"`
+	OwnerReferences  []OwnerReference  `json:"ownerReferences,omitempty" yaml:"ownerReferences,omitempty"`
+	ProjectID        string            `json:"projectId,omitempty" yaml:"projectId,omitempty"`
+	Removed          string            `json:"removed,omitempty" yaml:"removed,omitempty"`
+	Type             string            `json:"type,omitempty" yaml:"type,omitempty"`
+	UUID             string            `json:"uuid,omitempty" yaml:"uuid,omitempty"`
 }
 
 type SourceCodeProviderConfigCollection struct {
