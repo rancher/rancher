03726d59f4f312e97cf892b35af2410f1f7ff8df
igomez06 <israel.gomez@suse.com>
2022-02-11 10:28:35 -0700

    Implemented custom cluster tests, that are not bound to just ec2.

--- tests/v2/validation/provisioning/config.go
+++ tests/v2/validation/provisioning/config.go
@@ -4,17 +4,30 @@ import (
 	"strings"
 
 	"github.com/rancher/rancher/tests/framework/pkg/config"
+	"github.com/rancher/rancher/tests/framework/pkg/namegenerator"
 )
 
-const ConfigurationFileKey = "provisioningInput"
+const (
+	namespace               = "fleet-default"
+	defaultRandStringLength = 5
+	ConfigurationFileKey    = "provisioningInput"
+)
 
 type Config struct {
 	NodesAndRoles      string   `json:"nodesAndRoles" yaml:"nodesAndRoles" default:""`
 	KubernetesVersions []string `json:"kubernetesVersion" yaml:"kubernetesVersion"`
 	CNIs               []string `json:"cni" yaml:"cni"`
 	Providers          []string `json:"providers" yaml:"providers"`
+	NodeProviders      []string `json:"nodeProviders" yaml:"nodeProviders"`
 }
 
+func AppendRandomString(baseClusterName string) string {
+	clusterName := "auto-" + baseClusterName + "-" + namegenerator.RandStringLower(defaultRandStringLength)
+	return clusterName
+}
+
+// NodesAndRolesInput is a helper function that reads the nodesAndRoles entry in the config file and returns it in the form
+// of a list of map[string]bool
 func NodesAndRolesInput() []map[string]bool {
 	clustersConfig := new(Config)
 
