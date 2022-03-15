1084b267b280245eb39dde2895605fdbc3e80b81
igomez06 <israel.gomez@suse.com>
2022-03-15 16:30:38 -0600

    Added support for provisioning AKS hosted clusters in the framework. Including config read for the specifics of an AKS cluster.

---
+++ tests/framework/extensions/clusters/aks/create.go
@@ -0,0 +1,26 @@
+package aks
+
+import (
+	"github.com/rancher/rancher/tests/framework/clients/rancher"
+	management "github.com/rancher/rancher/tests/framework/clients/rancher/generated/management/v3"
+)
+
+func CreateAKSHostedCluster(client *rancher.Client, displayName, cloudCredentialID string, enableClusterAlerting, enableClusterMonitoring, enableNetworkPolicy, windowsPreferedCluster bool, labels map[string]string) (*management.Cluster, error) {
+	aksHostCluster := aksHostClusterConfig(displayName, cloudCredentialID)
+	cluster := &management.Cluster{
+		DockerRootDir:           "/var/lib/docker",
+		AKSConfig:               aksHostCluster,
+		Name:                    displayName,
+		EnableClusterAlerting:   enableClusterAlerting,
+		EnableClusterMonitoring: enableClusterMonitoring,
+		EnableNetworkPolicy:     &enableNetworkPolicy,
+		Labels:                  labels,
+		WindowsPreferedCluster:  windowsPreferedCluster,
+	}
+
+	clusterResp, err := client.Management.Cluster.Create(cluster)
+	if err != nil {
+		return nil, err
+	}
+	return clusterResp, err
+}
