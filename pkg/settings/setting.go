8d67b47c54827aec99c3ba29c1209e7785a2fbe4
Jiaqi Luo <6218999+jiaqiluo@users.noreply.github.com>
2022-04-19 14:21:34 -0700

    bump rancher-machine to v0.15.0-rancher84 (#37295)

--- pkg/settings/setting.go
+++ pkg/settings/setting.go
@@ -121,7 +121,7 @@ var (
 	EKSUpstreamRefresh                  = NewSetting("eks-refresh", "300")
 	GKEUpstreamRefresh                  = NewSetting("gke-refresh", "300")
 	HideLocalCluster                    = NewSetting("hide-local-cluster", "false")
-	MachineProvisionImage               = NewSetting("machine-provision-image", "rancher/machine:v0.15.0-rancher83")
+	MachineProvisionImage               = NewSetting("machine-provision-image", "rancher/machine:v0.15.0-rancher84")
 	SystemFeatureChartRefreshSeconds    = NewSetting("system-feature-chart-refresh-seconds", "900")
 
 	FleetMinVersion          = NewSetting("fleet-min-version", "")
