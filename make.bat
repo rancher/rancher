7d4bfd887f927396ccc882490153ef7c9223b3a9
Luther Monson <luther.monson@gmail.com>
2022-05-13 07:28:14 -0700

    removing trash from the repo (#37470)

--- make.bat
+++ make.bat
@@ -1,12 +1,4 @@
 @echo off
-if "%1%"=="trash" (
-    call :trash
-    goto :eof
-)
-if "%1%"=="trash-keep" (
-    call :trash-keep
-    goto :eof
-)
 if "%1%"=="deps" (
     call :deps
     goto :eof
@@ -25,18 +17,4 @@ if not exist .dapper.exe (
     bitsadmin /rawreturn /transfer dappwer-download https://releases.rancher.com/dapper/latest/dapper-Windows-x86_64.exe %~dp0\.dapper.exe
     .dapper.exe -v
 )
-goto :eof
-
-:trash
-call :.dapper
-.dapper.exe -f Dockerfile-windows.dapper -m bind trash.exe
-goto :eof
-
-:trash-keep
-call :.dapper
-.dapper.exe -f Dockerfile-windows.dapper -m bind trash.exe -k
-goto :eof
-
-:deps
-call :trash
 goto :eof
