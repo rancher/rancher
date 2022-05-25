b8c00d9834bbd8d2cd13d0ccd861663dc6d4f6da
jesse.millan <jesse.millan@oracle.com>
2022-05-25 16:20:21 -0700

    Update OKE cluster driver to v1.8.1.

--- pkg/data/management/kontainerdriver_data.go
+++ pkg/data/management/kontainerdriver_data.go
@@ -92,8 +92,8 @@ func addKontainerDrivers(management *config.ManagementContext) error {
 	}
 	if err := creator.addCustomDriver(
 		"oraclecontainerengine",
-		"https://github.com/rancher-plugins/kontainer-engine-driver-oke/releases/download/v1.7.1/kontainer-engine-driver-oke-linux",
-		"5a708bfc01c67558adc887258615900082263ca7d6f4160efb1a58501b0cc608",
+		"https://github.com/rancher-plugins/kontainer-engine-driver-oke/releases/download/v1.8.1/kontainer-engine-driver-oke-linux",
+		"57db1d61f75660adacab4c03bc7909eed8eab8755796bf19cca05adc9852271f",
 		"",
 		false,
 		"*.oraclecloud.com",
