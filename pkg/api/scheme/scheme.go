c0c27abd7283c0e6e5feae88837b467e7fb4fb86
Eliyam Levy <eliyam.levy@suse.com>
2022-05-31 14:31:55 -0400

    Forward porting changes from issue #37181

--- pkg/api/scheme/scheme.go
+++ pkg/api/scheme/scheme.go
@@ -104,10 +104,17 @@ func init() {
 	// unstructured object and allow the missing fields to be included.
 	Scheme.ExcludeGVK(management.SchemeGroupVersion.WithKind("ClusterList"))
 	Scheme.ExcludeGVK(management.SchemeGroupVersion.WithKind("NodeTemplateList"))
+	Scheme.ExcludeGVK(management.SchemeGroupVersion.WithKind("ClusterAlertRuleList"))
+	Scheme.ExcludeGVK(management.SchemeGroupVersion.WithKind("ClusterAlertGroupList"))
+	Scheme.ExcludeGVK(management.SchemeGroupVersion.WithKind("ProjectAlertRuleList"))
+	Scheme.ExcludeGVK(management.SchemeGroupVersion.WithKind("ProjectAlertGroupList"))
 	Scheme.ExcludeGVK(management.SchemeGroupVersion.WithKind("ClusterMonitorGraphList"))
 	Scheme.ExcludeGVK(management.SchemeGroupVersion.WithKind("ProjectMonitorGraphList"))
 	Scheme.ExcludeGVK(management.SchemeGroupVersion.WithKind("ClusterLogging"))
+	Scheme.ExcludeGVK(management.SchemeGroupVersion.WithKind("ClusterLoggingList"))
 	Scheme.ExcludeGVK(management.SchemeGroupVersion.WithKind("ProjectLogging"))
+	Scheme.ExcludeGVK(management.SchemeGroupVersion.WithKind("ProjectLoggingList"))
+
 }
 
 func addKnownTypes(scheme *runtime.Scheme) error {
