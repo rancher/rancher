2d17a17bcc8bca45f4ee8b5b272ad96471c3c6f9
Venkata Krishna Rohit Sakala <rohitsakala@gmail.com>
2022-06-21 14:28:17 +0200

    Add helm v2 to s390x arch

--- package/jailer.sh
+++ package/jailer.sh
@@ -58,17 +58,13 @@ cp -r -l /opt/drivers/management-state/bin /opt/jail/$NAME/var/lib/rancher/manag
 cp -l /usr/bin/rancher-machine /opt/jail/$NAME/usr/bin
 
 # Hard link helm_2 into the jail
-if [[ -f /usr/bin/rancher-helm ]]; then
-  cp -l /usr/bin/rancher-helm /opt/jail/$NAME/usr/bin
-fi
+cp -l /usr/bin/rancher-helm /opt/jail/$NAME/usr/bin
 
 # Hard link helm_3 into the jail
 cp -l /usr/bin/helm_v3 /opt/jail/$NAME/usr/bin
 
 # Hard link tiller into the jail
-if [[ -f /usr/bin/rancher-tiller ]]; then
-  cp -l /usr/bin/rancher-tiller /opt/jail/$NAME/usr/bin
-fi
+cp -l /usr/bin/rancher-tiller /opt/jail/$NAME/usr/bin
 
 # Hard link kustomize into the jail
 cp -l /usr/bin/kustomize /opt/jail/$NAME/usr/bin
