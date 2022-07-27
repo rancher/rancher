f31c846196ced9a9c4c3075f2ba1823e1366e4fd
Colleen Murphy <colleen.murphy@suse.com>
2022-07-27 12:04:08 -0700

    Expose rancher version endpoint
    Expose the rancher version as an unauthenticated endpoint on
    /rancherversion. The version and git commit are set at build time, but
    the "prime" flag is set at runtime by an environment variable in the
    Dockerfile. The endpoint is /rancherversion instead of /version because
    the /version endpoint is just passed through to kubernetes and gives the
    local cluster kubernetes version. The endpoint is unauthenticated rather
    than authenticated because the k8s /version is already exposed[1] as
    well as the /v1-{k3s,rke2}-release/* endpoints, so security through
    obscurity doesn't buy us much here.
    
    [1] https://github.com/kubernetes/kubernetes/issues/84040

--- pkg/multiclustermanager/routes.go
+++ pkg/multiclustermanager/routes.go
@@ -33,6 +33,7 @@ import (
 	"github.com/rancher/rancher/pkg/telemetry"
 	"github.com/rancher/rancher/pkg/tunnelserver/mcmauthorizer"
 	"github.com/rancher/rancher/pkg/types/config"
+	"github.com/rancher/rancher/pkg/version"
 	"github.com/rancher/steve/pkg/auth"
 )
 
@@ -85,6 +86,7 @@ func router(ctx context.Context, localClusterEnabled bool, tunnelAuthorizer *mcm
 	unauthed.Handle("/v3/settings/ui-pl", managementAPI).MatcherFunc(onlyGet)
 	unauthed.Handle("/v3/settings/ui-brand", managementAPI).MatcherFunc(onlyGet)
 	unauthed.Handle("/v3/settings/ui-default-landing", managementAPI).MatcherFunc(onlyGet)
+	unauthed.Handle("/rancherversion", version.NewVersionHandler())
 	unauthed.PathPrefix("/v1-{prefix}-release/channel").Handler(channelserver)
 	unauthed.PathPrefix("/v1-{prefix}-release/release").Handler(channelserver)
 	unauthed.PathPrefix("/v1-saml").Handler(saml.AuthHandler())
