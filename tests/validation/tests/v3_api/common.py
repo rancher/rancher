afc28f4985d9f7c9ba51e197136551d73901d42f
vivek-infracloud <vivek.shilimkar@infracloud.io>
2022-08-10 16:31:00 +0530

    add timeout for wl

--- tests/validation/tests/v3_api/common.py
+++ tests/validation/tests/v3_api/common.py
@@ -597,6 +597,7 @@ def run_command_with_stderr(command, log_out=True):
 
 def wait_for_wl_to_active(client, workload, timeout=DEFAULT_TIMEOUT):
     start = time.time()
+    timeout = start + timeout
     workloads = client.list_workload(uuid=workload.uuid).data
     assert len(workloads) == 1
     wl = workloads[0]
