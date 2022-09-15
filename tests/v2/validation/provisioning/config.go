3ad7601e433afd07acf74a7fd38cfe8662c9bf98
Michael Spencer <MSpencer87@users.noreply.github.com>
2022-09-14 17:29:37 -0700

    Update branch

--- tests/v2/validation/provisioning/config.go
+++ tests/v2/validation/provisioning/config.go
@@ -1,16 +1,9 @@
 package provisioning
 
 import (
-<<<<<<< HEAD
-	"strings"
-
-	rkev1 "github.com/rancher/rancher/pkg/apis/rke.cattle.io/v1"
-	"github.com/rancher/rancher/tests/framework/pkg/config"
-=======
 	"github.com/rancher/rancher/tests/framework/extensions/machinepools"
 	nodepools "github.com/rancher/rancher/tests/framework/extensions/rke1/nodepools"
 	"github.com/rancher/rancher/tests/framework/pkg/namegenerator"
->>>>>>> refs/remotes/origin/add-rke2-ace-test
 )
 
 const (
@@ -19,20 +12,12 @@ const (
 )
 
 type Config struct {
-<<<<<<< HEAD
-	NodesAndRoles            string                         `json:"nodesAndRoles" yaml:"nodesAndRoles" default:""`
-	KubernetesVersions       []string                       `json:"kubernetesVersion" yaml:"kubernetesVersion"`
-	CNIs                     []string                       `json:"cni" yaml:"cni"`
-	Providers                []string                       `json:"providers" yaml:"providers"`
-	LocalClusterAuthEndpoint rkev1.LocalClusterAuthEndpoint `json:"localClusterAuthEndpoint" yaml:"localClusterAuthEndpoint"`
-=======
 	NodesAndRoles      []machinepools.NodeRoles `json:"nodesAndRoles" yaml:"nodesAndRoles" default:"[]"`
 	NodesAndRolesRKE1  []nodepools.NodeRoles    `json:"nodesAndRolesRKE1" yaml:"nodesAndRolesRKE1" default:"[]"`
 	KubernetesVersions []string                 `json:"kubernetesVersion" yaml:"kubernetesVersion"`
 	CNIs               []string                 `json:"cni" yaml:"cni"`
 	Providers          []string                 `json:"providers" yaml:"providers"`
 	NodeProviders      []string                 `json:"nodeProviders" yaml:"nodeProviders"`
->>>>>>> refs/remotes/origin/add-rke2-ace-test
 }
 
 func AppendRandomString(baseClusterName string) string {
