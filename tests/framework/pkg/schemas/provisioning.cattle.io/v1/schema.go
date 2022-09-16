001c6f86486243a875e9c7c2982976d7d0512d3a
igomez06 <israel.gomez@suse.com>
2022-09-16 13:28:15 -0600

    Updated fields that previously used Duration to use string, to fix an unmarshaling error.

--- tests/framework/pkg/schemas/provisioning.cattle.io/v1/schema.go
+++ tests/framework/pkg/schemas/provisioning.cattle.io/v1/schema.go
@@ -4,7 +4,6 @@ import (
 	"github.com/rancher/norman/types"
 	m "github.com/rancher/norman/types/mapper"
 	v1 "github.com/rancher/rancher/pkg/apis/provisioning.cattle.io/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 var (
@@ -23,8 +22,11 @@ func clusterTypes(schemas *types.Schemas) *types.Schemas {
 		m.Drop{Field: "chartValues"},
 		&m.ChangeType{Field: "machineGlobalConfig", Type: "MachineGlobalConfig"}).
 		MustImport(&Version, MachineGlobalConfig{}).
-		MustImportAndCustomize(&Version, metav1.Duration{}, func(schema *types.Schema) {}, Duration{}).
-		MustImportAndCustomize(&Version, v1.RKEConfig{}, func(schema *types.Schema) {}).
+		AddMapperForType(&Version, v1.RKEMachinePool{},
+			&m.ChangeType{Field: "drainBeforeDeleteTimeout", Type: "string"},
+			&m.ChangeType{Field: "nodeStartupTimeout", Type: "string"},
+			&m.ChangeType{Field: "unhealthyNodeTimeout", Type: "string"}).
+		MustImportAndCustomize(&Version, v1.RKEMachinePool{}, func(schema *types.Schema) {}).
 		MustImportAndCustomize(&Version, v1.Cluster{}, func(schema *types.Schema) {
 			schema.ID = "provisioning.cattle.io.cluster"
 		})
