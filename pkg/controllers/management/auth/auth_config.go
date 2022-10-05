567d6e6ee3caa465c2dd208cd70b819411bdfc66
Michael Bolot <michael.bolot@suse.com>
2022-10-05 10:55:25 -0500

    Fixing bugs with auth providers

--- pkg/controllers/management/auth/auth_config.go
+++ pkg/controllers/management/auth/auth_config.go
@@ -8,6 +8,7 @@ import (
 	v3 "github.com/rancher/rancher/pkg/generated/norman/management.cattle.io/v3"
 	"github.com/rancher/rancher/pkg/types/config"
 	"github.com/sirupsen/logrus"
+	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/runtime"
 )
 
@@ -34,6 +35,10 @@ func newAuthConfigController(context context.Context, mgmt *config.ManagementCon
 }
 
 func (ac *authConfigController) sync(key string, obj *v3.AuthConfig) (runtime.Object, error) {
+	err := ac.refreshUsers(obj)
+	if err != nil {
+		return obj, err
+	}
 	if !obj.Enabled {
 		err := ac.cleanup.TriggerCleanup(obj)
 		if err != nil {
@@ -43,4 +48,23 @@ func (ac *authConfigController) sync(key string, obj *v3.AuthConfig) (runtime.Ob
 	}
 
 	return obj, nil
+}
+
+func (ac *authConfigController) refreshUsers(obj *v3.AuthConfig) error {
+	// if we have changed an auth config, refresh all users belonging to the auth config. This addresses:
+	// Disabling an auth provider - now we disable user access
+	// Removing a user from auth provider access - now we will immediately revoke access
+	users, err := ac.users.List("", labels.Everything())
+	if err != nil {
+		return err
+	}
+	for _, user := range users {
+		principalID := providerrefresh.GetPrincipalIDForProvider(obj.Name, user)
+		if principalID != "" {
+			// if we have a principal on this provider, then we need to be refreshed to potentially invalidate
+			// access derived from this provider
+			ac.authRefresher.TriggerUserRefresh(user.Name, true)
+		}
+	}
+	return nil
 }
