02edb8503f162c6b7738a17d5e0a718bce6147fd
rishabh <rishabh@infracloud.io>
2022-11-02 19:21:06 +0530

    Add validation test for etcd backup and restore with k8s upgrade

--- tests/framework/extensions/workloads/template.go
+++ tests/framework/extensions/workloads/template.go
@@ -1,7 +1,9 @@
 package workloads
 
 import (
+	appv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 // NewContainer is a contructor that creates a container for a pod template i.e. corev1.PodTemplateSpec
@@ -16,12 +18,31 @@ func NewContainer(containerName, image string, imagePullPolicy corev1.PullPolicy
 }
 
 // NewPodTemplate is a constructor that creates the pod template for all types of workloads e.g. cronjobs, daemonsets, deployments, and batch jobs
-func NewPodTemplate(containers []corev1.Container, volumes []corev1.Volume, imagePullSecrets []corev1.LocalObjectReference) corev1.PodTemplateSpec {
+func NewPodTemplate(containers []corev1.Container, volumes []corev1.Volume, imagePullSecrets []corev1.LocalObjectReference, labels map[string]string) corev1.PodTemplateSpec {
 	return corev1.PodTemplateSpec{
+		ObjectMeta: metav1.ObjectMeta{
+			Labels: labels,
+		},
 		Spec: corev1.PodSpec{
 			Containers:       containers,
 			Volumes:          volumes,
 			ImagePullSecrets: imagePullSecrets,
 		},
 	}
+}
+
+func NewDeploymentTemplate(deploymentName string, namespace string, podSpec corev1.PodTemplateSpec, matchLabels map[string]string) *appv1.Deployment {
+	return &appv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      deploymentName,
+			Namespace: namespace,
+		},
+		Spec: appv1.DeploymentSpec{
+			Selector: &metav1.LabelSelector{
+				MatchLabels: matchLabels,
+			},
+			Template: podSpec,
+		},
+	}
+
 }
