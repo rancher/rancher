6358db4ce8dde341f928f2d9152ab73509b5be87
Ricardo Weir <ricardo.weir@suse.com>
2022-11-10 15:44:51 -0700

    Move trigger refresh logic to token handler
    Before, a userattribute handler would check if the ExtraByProvider
    field was null and trigger a refresh. ExtraByProvider contains
    data that is available only on tokens. When there was no token
    associated with the user corresponding to the userattribute, the
    handler would fire endlessly. This happened because the field
    continued to be null but the refresh process would update the
    LastRefreshed field which would cause the userattribute handler
    to execute again. Now, the ExtraByProvider check is done on a
    token handler. The token handler is aware that the tokens
    existence may make populating the field possible and the refresh
    process, makes no changes to the token object. This avoids nonstop
    firing of handlers.

--- pkg/controllers/management/auth/token.go
+++ pkg/controllers/management/auth/token.go
@@ -5,6 +5,7 @@ import (
 	"github.com/rancher/rancher/pkg/features"
 	v3 "github.com/rancher/rancher/pkg/generated/norman/management.cattle.io/v3"
 	"github.com/rancher/rancher/pkg/types/config"
+	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 )
 
@@ -13,18 +14,22 @@ const (
 )
 
 type TokenController struct {
-	tokens v3.TokenInterface
+	tokens               v3.TokenInterface
+	userAttributes       v3.UserAttributeInterface
+	userAttributesLister v3.UserAttributeLister
 }
 
 func newTokenController(mgmt *config.ManagementContext) *TokenController {
 	n := &TokenController{
-		tokens: mgmt.Management.Tokens(""),
+		tokens:               mgmt.Management.Tokens(""),
+		userAttributes:       mgmt.Management.UserAttributes(""),
+		userAttributesLister: mgmt.Management.UserAttributes("").Controller().Lister(),
 	}
 	return n
 }
 
 // sync is called periodically and on real updates
-func (n *TokenController) sync(key string, obj *v3.Token) (runtime.Object, error) {
+func (t *TokenController) sync(key string, obj *v3.Token) (runtime.Object, error) {
 	if obj == nil {
 		return nil, nil
 	}
@@ -37,7 +42,7 @@ func (n *TokenController) sync(key string, obj *v3.Token) (runtime.Object, error
 				newObj := obj.DeepCopy()
 				newObj.SetFinalizers(finalizers)
 				var err error
-				obj, err = n.tokens.Update(newObj)
+				obj, err = t.tokens.Update(newObj)
 				if err != nil {
 					return nil, err
 				}
@@ -50,7 +55,20 @@ func (n *TokenController) sync(key string, obj *v3.Token) (runtime.Object, error
 		//compute and save expiresAt
 		newObj := obj.DeepCopy()
 		tokenUtil.SetTokenExpiresAt(newObj)
-		if _, err := n.tokens.Update(newObj); err != nil {
+		if _, err := t.tokens.Update(newObj); err != nil {
+			return nil, err
+		}
+	}
+
+	// trigger corresponding UserAttribute resource to refresh if token potentially
+	// provides new information that is missing from the UserAttribute resource
+	refreshUserAttributes, err := t.userAttributesNeedsRefresh(obj.UserID)
+	if err != nil {
+		return nil, err
+	}
+
+	if refreshUserAttributes {
+		if err = t.triggerUserAttributesRefresh(obj.UserID); err != nil {
 			return nil, err
 		}
 	}
@@ -68,10 +86,36 @@ func (n *TokenController) sync(key string, obj *v3.Token) (runtime.Object, error
 		if err != nil {
 			return nil, err
 		}
-		if _, err := n.tokens.Update(newObj); err != nil {
+		if _, err := t.tokens.Update(newObj); err != nil {
 			return nil, err
 		}
 	}
 
 	return nil, nil
+}
+
+func (t *TokenController) userAttributesNeedsRefresh(user string) (bool, error) {
+	if user == "" {
+		return false, nil
+	}
+
+	userAttribute, err := t.userAttributesLister.Get("", user)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			return false, nil
+		}
+		return false, err
+	}
+	return userAttribute.ExtraByProvider == nil, nil
+}
+
+func (t *TokenController) triggerUserAttributesRefresh(user string) error {
+	userAttribute, err := t.userAttributesLister.Get("", user)
+	if err != nil {
+		return err
+	}
+
+	userAttribute.NeedsRefresh = true
+	_, err = t.userAttributes.Update(userAttribute)
+	return err
 }
