f1eb97c565860b42c3fbb5e07e20a546516d965c
Chris Kim <oats87g@gmail.com>
2023-05-03 23:04:30 -0700

    Add provisioning-operations-tests that test CAPR etcd snapshot creation/restore, certificate rotation, and encryption key rotation
    Signed-off-by: Chris Kim <oats87g@gmail.com>

--- tests/integration/pkg/systemdnode/systemnode.go
+++ tests/integration/pkg/systemdnode/systemnode.go
@@ -113,6 +113,11 @@ INVOCATION_ID=
 						MountPath: "/usr/local/lib/systemd/system/k3s-agent.service.d/10-delegate.conf",
 						SubPath:   "dropin",
 					},
+					{ // We have to set invocation disabling on the rancher-system-agent because it runs rke2/k3s server and this also has cgroup issues.
+						Name:      "systemd",
+						MountPath: "/etc/default/rancher-system-agent",
+						SubPath:   "disable",
+					},
 					{
 						Name:      "systemd",
 						MountPath: "/etc/default/rke2-server",
