014673fd418ba714fd7706ef027fb60b18c89d88
Markus Walker <markus.walker.25@gmail.com>
2023-05-09 09:08:18 -0700

    Stop hosted clusters from deleting before tests run

--- tests/validation/tests/v3_api/common.py
+++ tests/validation/tests/v3_api/common.py
@@ -1503,13 +1503,14 @@ def cluster_cleanup(client, cluster, aws_nodes=None):
         create_config_file(env_details)
         
         
-def hosted_cluster_cleanup(client, cluster):
+def hosted_cluster_cleanup(client, cluster, cluster_name):
     if RANCHER_CLEANUP_CLUSTER:
         client.delete(cluster)
     else:
         env_details = "env.CATTLE_TEST_URL='" + CATTLE_TEST_URL + "'\n"
         env_details += "env.ADMIN_TOKEN='" + ADMIN_TOKEN + "'\n"
         env_details += "env.USER_TOKEN='" + USER_TOKEN + "'\n"
+        env_details += "env.CLUSTER_NAME='" + cluster_name + "'\n"
         create_config_file(env_details)
 
 
