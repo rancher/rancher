cc47ce960a91669e092d06c344d9e36f3e11f36f
Jake Hyde <jakefhyde@gmail.com>
2023-05-12 18:37:46 -0400

    Fix bootstrap password logic to return Data instead of StringData

--- pkg/data/management/bootstrap_password.go
+++ pkg/data/management/bootstrap_password.go
@@ -5,7 +5,6 @@ import (
 	"os"
 
 	"github.com/rancher/wrangler/pkg/randomtoken"
-	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	corev1 "k8s.io/client-go/kubernetes/typed/core/v1"
@@ -16,42 +15,42 @@ const (
 	bootstrapPasswordSecretKey  = "bootstrapPassword"
 )
 
-// GetBootstrapPassword reads the bootstrap password from it's secret.  If the secret is not found
-// it will be set from `CATTLE_BOOTSTRAP_PASSWORD` or generated if this is empty as well.
+// GetBootstrapPassword queries the corresponding bootstrap-secret in the cattle-system namespace. If this secret
+// exists, and has the corresponding bootstrapPassword key, then the value of that key will be returned. Otherwise, the
+// `CATTLE_BOOTSTRAP_PASSWORD` environment variable will be checked. If neither are set, a bootstrap password is
+// generated.
 func GetBootstrapPassword(ctx context.Context, secrets corev1.SecretInterface) (string, bool, error) {
-	var s *v1.Secret
-	var err error
 	var generated bool
 
 	// load the existing secret
-	s, err = secrets.Get(ctx, bootstrapPasswordSecretName, metav1.GetOptions{})
+	secret, err := secrets.Get(ctx, bootstrapPasswordSecretName, metav1.GetOptions{})
 	if err != nil && !errors.IsNotFound(err) {
 		return "", false, err
 	}
 
 	// if the bootstrap password is set return it
-	if s.StringData[bootstrapPasswordSecretKey] != "" {
-		return s.StringData[bootstrapPasswordSecretKey], generated, nil
+	if password, ok := secret.Data[bootstrapPasswordSecretKey]; ok {
+		return string(password), generated, nil
 	}
 
 	// if the password is not set check the env and fall back to generating one
-	s.StringData = make(map[string]string)
-	s.StringData[bootstrapPasswordSecretKey] = os.Getenv("CATTLE_BOOTSTRAP_PASSWORD")
-	if s.StringData[bootstrapPasswordSecretKey] == "" {
+	secret.StringData = make(map[string]string)
+	secret.StringData[bootstrapPasswordSecretKey] = os.Getenv("CATTLE_BOOTSTRAP_PASSWORD")
+	if secret.StringData[bootstrapPasswordSecretKey] == "" {
 		generated = true
-		s.StringData[bootstrapPasswordSecretKey], _ = randomtoken.Generate()
+		secret.StringData[bootstrapPasswordSecretKey], _ = randomtoken.Generate()
 	}
 
 	// persist the password
-	if s.GetResourceVersion() != "" {
-		_, err = secrets.Update(ctx, s, metav1.UpdateOptions{})
+	if secret.GetResourceVersion() != "" {
+		_, err = secrets.Update(ctx, secret, metav1.UpdateOptions{})
 	} else {
-		s.Name = bootstrapPasswordSecretName
-		_, err = secrets.Create(ctx, s, metav1.CreateOptions{})
+		secret.Name = bootstrapPasswordSecretName
+		_, err = secrets.Create(ctx, secret, metav1.CreateOptions{})
 	}
 	if err != nil {
 		return "", false, err
 	}
 
-	return s.StringData[bootstrapPasswordSecretKey], generated, nil
+	return secret.StringData[bootstrapPasswordSecretKey], generated, nil
 }
