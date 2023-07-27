f57ccaf3558b8d05bb995f0c05db7f7ed86f1df3
Corentin Néau <tan.neau@suse.com>
2023-07-27 09:57:20 +0200

    Pin Fleet version
    This deprecates environment variable `CATTLE_FLEET_MIN_VERSION` in favor
    of `CATTLE_FLEET_VERSION`, to enable an exact Fleet version to be
    specified at Rancher installation time.
    
    If `CATTLE_FLEET_MIN_VERSION` is set, its value will be used over
    `CATTLE_FLEET_VERSION`. Regardless of the setting from which it comes,
    the Fleet version chosen for installation will be installed as a min
    version, to be updated only manually.
    
    To this end, the system charts manager is updated to consider a "min
    version" install as targeting that specific min version, instead of the
    latest available version of the chart. However, a min version install is
    still different behaviour from exact version-based install, which
    enforces that version even if a more recent version is already
    installed. Min version-based installs consider more recent versions
    valid, and Rancher will not override/downgrade them.
    
    This allows Fleet to be updated via the Rancher UI, and for that update
    to persist across catalog updates and Rancher restarts provided that the
    newly installed version is at least as recent as the configured min version.

--- pkg/catalogv2/system/manager_test.go
+++ pkg/catalogv2/system/manager_test.go
@@ -1,9 +1,23 @@
 package system
 
+// mockgen --build_flags=--mod=mod -package system -destination=./mock_system_test.go github.com/rancher/rancher/pkg/catalogv2/system ContentClient,OperationClient,HelmClient
+
 import (
+	context "context"
+	"errors"
 	"testing"
 
+	"github.com/golang/mock/gomock"
+	catalog "github.com/rancher/rancher/pkg/apis/catalog.cattle.io/v1"
+	v3 "github.com/rancher/rancher/pkg/apis/management.cattle.io/v3"
+	"github.com/rancher/wrangler/pkg/generic/fake"
 	"github.com/stretchr/testify/assert"
+	"helm.sh/helm/v3/pkg/action"
+	"helm.sh/helm/v3/pkg/chart"
+	"helm.sh/helm/v3/pkg/release"
+	"helm.sh/helm/v3/pkg/repo"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func TestManagerRemovesRelease(t *testing.T) {
@@ -35,4 +49,372 @@ func TestManagerRemovesRelease(t *testing.T) {
 	assert.Equal(t, charts, manager.desiredCharts)
 	manager.Remove("cattle-system", "rancher-webhook")
 	assert.Equal(t, map[desiredKey]map[string]any{}, manager.desiredCharts)
+}
+
+func TestStart(t *testing.T) {
+	t.Parallel()
+
+	ctrl := gomock.NewController(t)
+	ctx := context.Background()
+
+	mockSettingsController := fake.NewMockNonNamespacedControllerInterface[*v3.Setting, *v3.SettingList](ctrl)
+	mockSettingsController.EXPECT().OnChange(ctx, "system-feature-chart-refresh", gomock.Any())
+
+	mockClusterRepos := fake.NewMockNonNamespacedControllerInterface[*catalog.ClusterRepo, *catalog.ClusterRepoList](ctrl)
+	mockClusterRepos.EXPECT().OnChange(ctx, "catalog-refresh-trigger", gomock.Any())
+
+	manager := Manager{
+		settings:     mockSettingsController,
+		clusterRepos: mockClusterRepos,
+	}
+
+	manager.Start(ctx)
+}
+
+func TestInstallCharts(t *testing.T) {
+	var (
+		fleetChartV1 = release.Release{
+			Namespace: "cattle-fleet-system",
+			Name:      "fleet",
+			Chart: &chart.Chart{
+				Metadata: &chart.Metadata{
+					Version: "1.0.0",
+				},
+			},
+			Info: &release.Info{
+				Status: release.StatusDeployed,
+			},
+		}
+		fleetChartV2 = release.Release{
+			Namespace: "cattle-fleet-system",
+			Name:      "fleet",
+			Chart: &chart.Chart{
+				Metadata: &chart.Metadata{
+					Version: "2.0.0",
+				},
+			},
+			Info: &release.Info{
+				Status: release.StatusDeployed,
+			},
+		}
+		fleetChartV3 = release.Release{
+			Namespace: "cattle-fleet-system",
+			Name:      "fleet",
+			Chart: &chart.Chart{
+				Metadata: &chart.Metadata{
+					Version: "3.0.0",
+				},
+			},
+			Info: &release.Info{
+				Status: release.StatusDeployed,
+			},
+		}
+		rancherChartV1 = release.Release{
+			Namespace: "cattle-system",
+			Name:      "rancher-webhook",
+			Chart: &chart.Chart{
+				Metadata: &chart.Metadata{
+					Version: "1.0.0",
+				},
+			},
+			Info: &release.Info{
+				Status: release.StatusDeployed,
+			},
+		}
+		fleetRepoV1 = repo.ChartVersion{
+			Metadata: &chart.Metadata{
+				Version: "1.0.0",
+			},
+			URLs: []string{"foo"},
+		}
+		fleetRepoV2 = repo.ChartVersion{
+			Metadata: &chart.Metadata{
+				Version: "2.0.0",
+			},
+			URLs: []string{"foo"},
+		}
+		fleetRepoV3 = repo.ChartVersion{
+			Metadata: &chart.Metadata{
+				Version: "3.0.0",
+			},
+			URLs: []string{"foo"},
+		}
+		rancherRepoV1 = repo.ChartVersion{
+			Metadata: &chart.Metadata{
+				Version: "1.0.0",
+			},
+			URLs: []string{"foo"},
+		}
+		rancherRepoV2 = repo.ChartVersion{
+			Metadata: &chart.Metadata{
+				Version: "2.0.0",
+			},
+			URLs: []string{"foo"},
+		}
+	)
+
+	tests := []struct {
+		name            string
+		releases        []*release.Release
+		indexedReleases map[string]repo.ChartVersions
+		desiredCharts   map[desiredKey]map[string]any
+		forceAdopt      bool
+		expectInstalls  map[string]bool
+		expectedErr     error
+	}{
+		{
+			name:     "Updates charts to desired version",
+			releases: []*release.Release{&rancherChartV1, &fleetChartV1},
+			indexedReleases: map[string]repo.ChartVersions{
+				"fleet":           {&fleetRepoV1, &fleetRepoV2},
+				"rancher-webhook": {&rancherRepoV1, &rancherRepoV2},
+			},
+			desiredCharts: map[desiredKey]map[string]any{
+				{
+					namespace:    "cattle-fleet-system",
+					name:         "fleet",
+					minVersion:   "1.0.0",
+					exactVersion: "2.0.0",
+				}: {},
+				{
+					namespace:    "cattle-system",
+					name:         "rancher-webhook",
+					minVersion:   "1.0.0",
+					exactVersion: "2.0.0",
+				}: {},
+			},
+			forceAdopt: false,
+			expectInstalls: map[string]bool{
+				"rancher-webhook": true,
+				"fleet":           true,
+			},
+		},
+		{
+			name:     "Keeps installed release matching desired version",
+			releases: []*release.Release{&fleetChartV2, &rancherChartV1},
+			indexedReleases: map[string]repo.ChartVersions{
+				"fleet":           {&fleetRepoV1, &fleetRepoV2},
+				"rancher-webhook": {&rancherRepoV1, &rancherRepoV2},
+			},
+			desiredCharts: map[desiredKey]map[string]any{
+				{
+					namespace:    "cattle-fleet-system",
+					name:         "fleet",
+					minVersion:   "1.0.0",
+					exactVersion: "2.0.0",
+				}: {},
+				{
+					namespace:    "cattle-system",
+					name:         "rancher-webhook",
+					minVersion:   "1.0.0",
+					exactVersion: "2.0.0",
+				}: {},
+			},
+			forceAdopt: false,
+			expectInstalls: map[string]bool{
+				"fleet":           false,
+				"rancher-webhook": true,
+			},
+		},
+		{
+			name:     "Keeps installed release, more recent than desired version",
+			releases: []*release.Release{&rancherChartV1, &fleetChartV3},
+			indexedReleases: map[string]repo.ChartVersions{
+				"fleet":           {&fleetRepoV1, &fleetRepoV2, &fleetRepoV3},
+				"rancher-webhook": {&rancherRepoV1, &rancherRepoV2},
+			},
+			desiredCharts: map[desiredKey]map[string]any{
+				{
+					namespace:    "cattle-fleet-system",
+					name:         "fleet",
+					minVersion:   "1.0.0",
+					exactVersion: "",
+				}: {},
+				{
+					namespace:    "cattle-system",
+					name:         "rancher-webhook",
+					minVersion:   "1.0.0",
+					exactVersion: "2.0.0",
+				}: {},
+			},
+			forceAdopt: false,
+			expectInstalls: map[string]bool{
+				"fleet":           false,
+				"rancher-webhook": true,
+			},
+		},
+		{
+			// This use case can occur when restoring to an older Rancher version.
+			name:     "Downgrades installed release, more recent than desired version",
+			releases: []*release.Release{&rancherChartV1, &fleetChartV3},
+			indexedReleases: map[string]repo.ChartVersions{
+				"fleet":           {&fleetRepoV1, &fleetRepoV2, &fleetRepoV3},
+				"rancher-webhook": {&rancherRepoV1, &rancherRepoV2},
+			},
+			desiredCharts: map[desiredKey]map[string]any{
+				{
+					namespace:    "cattle-fleet-system",
+					name:         "fleet",
+					minVersion:   "1.0.0",
+					exactVersion: "2.0.0",
+				}: {},
+				{
+					namespace:    "cattle-system",
+					name:         "rancher-webhook",
+					minVersion:   "1.0.0",
+					exactVersion: "2.0.0",
+				}: {},
+			},
+			forceAdopt: false,
+			expectInstalls: map[string]bool{
+				"fleet":           true,
+				"rancher-webhook": true,
+			},
+		},
+		{
+			name:     "Installs release if none installed",
+			releases: []*release.Release{&rancherChartV1},
+			indexedReleases: map[string]repo.ChartVersions{
+				"fleet":           {&fleetRepoV1, &fleetRepoV2},
+				"rancher-webhook": {&rancherRepoV1, &rancherRepoV2},
+			},
+			desiredCharts: map[desiredKey]map[string]any{
+				{
+					namespace:    "cattle-fleet-system",
+					name:         "fleet",
+					minVersion:   "1.0.0",
+					exactVersion: "2.0.0",
+				}: {},
+				{
+					namespace:    "cattle-system",
+					name:         "rancher-webhook",
+					minVersion:   "1.0.0",
+					exactVersion: "2.0.0",
+				}: {},
+			},
+			forceAdopt: false,
+			expectInstalls: map[string]bool{
+				"fleet":           true,
+				"rancher-webhook": true,
+			},
+		},
+		{
+			name:     "Fails to install specified release if not found in catalog",
+			releases: []*release.Release{&rancherChartV1},
+			indexedReleases: map[string]repo.ChartVersions{
+				"fleet":           {&fleetRepoV1, &fleetRepoV3},
+				"rancher-webhook": {&rancherRepoV1, &rancherRepoV2},
+			},
+			desiredCharts: map[desiredKey]map[string]any{
+				{
+					namespace:    "cattle-fleet-system",
+					name:         "fleet",
+					minVersion:   "2.0.0",
+					exactVersion: "2.0.0",
+				}: {},
+				{
+					namespace:    "cattle-system",
+					name:         "rancher-webhook",
+					minVersion:   "1.0.0",
+					exactVersion: "2.0.0",
+				}: {},
+			},
+			forceAdopt: false,
+			expectInstalls: map[string]bool{
+				"fleet":           false,
+				"rancher-webhook": true,
+			},
+			expectedErr: errors.New("no chart version found"),
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+
+			ctx := context.Background()
+			mockContentClient := NewMockContentClient(ctrl)
+			mockOperationClient := NewMockOperationClient(ctrl)
+			mockPodClient := fake.NewMockClientInterface[*v1.Pod, *v1.PodList](ctrl)
+			mockHelmClient := NewMockHelmClient(ctrl)
+
+			for dc := range test.desiredCharts {
+				var foundRelease *release.Release
+				for _, r := range test.releases {
+					if r.Name == dc.name && r.Namespace == dc.namespace {
+						foundRelease = r
+					}
+				}
+
+				var foundReleases []*release.Release
+				if foundRelease != nil {
+					foundReleases = []*release.Release{foundRelease}
+				}
+
+				// Call from installCharts and isInstalled
+				mockHelmClient.EXPECT().ListReleases(dc.namespace, dc.name, action.ListDeployed).
+					Return(foundReleases, nil).
+					MaxTimes(2)
+
+				if test.expectInstalls[dc.name] {
+					// Call from install -> hasStatus
+					mockHelmClient.EXPECT().ListReleases(
+						dc.namespace,
+						dc.name,
+						action.ListPendingInstall|action.ListPendingUpgrade|action.ListPendingRollback,
+					).
+						Return(nil, nil)
+
+					upgradeOp := catalog.Operation{
+						Status: catalog.OperationStatus{
+							PodNamespace: dc.namespace,
+						},
+					}
+
+					mockOperationClient.EXPECT().Upgrade(ctx, installUser, "", "rancher-charts", gomock.Any(), gomock.Any()).
+						Return(&upgradeOp, nil)
+
+					pod := v1.Pod{
+						ObjectMeta: metav1.ObjectMeta{
+							Name: "foo",
+						},
+						Status: v1.PodStatus{
+							ContainerStatuses: []v1.ContainerStatus{
+								{
+									Name: "helm",
+									State: v1.ContainerState{
+										Terminated: &v1.ContainerStateTerminated{
+											ExitCode: 0,
+										},
+									},
+								},
+							},
+						},
+					}
+
+					mockPodClient.EXPECT().Get(dc.namespace, gomock.Any(), metav1.GetOptions{}).
+						Return(&pod, nil).Times(1)
+				}
+
+				mockContentClient.EXPECT().Index("", "rancher-charts", "", true).
+					Return(&repo.IndexFile{Entries: test.indexedReleases}, nil)
+			}
+
+			manager := Manager{
+				ctx:           context.Background(),
+				content:       mockContentClient,
+				pods:          mockPodClient,
+				desiredCharts: test.desiredCharts,
+				helmClient:    mockHelmClient,
+				operation:     mockOperationClient,
+			}
+
+			err := manager.installCharts(test.desiredCharts, test.forceAdopt)
+			if test.expectedErr == nil {
+				assert.NoError(t, err)
+			} else {
+				assert.Contains(t, err.Error(), test.expectedErr.Error())
+			}
+		})
+	}
 }
