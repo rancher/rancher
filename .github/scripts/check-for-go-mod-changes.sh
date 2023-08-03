4e9b4d10e226b05e81a5abb6da0623f23aac9595
Tim Hardeck <thardeck@suse.com>
2023-08-03 09:31:52 +0200

    Add Github Action check for bad modules
    by comparing `go.mod` and `pkg/apis/go.mod`.
    
    The test was extracted from `scripts/validate`.

--- .github/scripts/check-for-go-mod-changes.sh
+++ .github/scripts/check-for-go-mod-changes.sh
@@ -15,3 +15,25 @@ if [ -n "$(git status --porcelain)" ]; then
     git status --porcelain
     exit 1
 fi
+
+# Check diff between ./go.mod and ./pkg/apis/go.mod
+badmodule="false"
+while read -r module tag; do
+  # Get tag from module in ./go.mod
+  roottag=$(sed '1,/^require/d' go.mod | grep "${module} " | awk '{ print $2 }')
+  echo "${module}:"
+  echo "${tag} (./pkg/apis/go.mod)"
+  echo "${roottag} (./go.mod)"
+  # Compare with tag from module in ./pkg/apis/go.mod
+  if [ "${tag}" != "${roottag}" ]; then
+    echo "${module} is different ('${tag}' vs '${roottag}')"
+    badmodule="true"
+  fi
+done << EOF
+$(sed '1,/require/d' pkg/apis/go.mod | head -n -1 | grep -v indirect | grep rancher |  awk '{ print $1,$2 }')
+EOF
+
+if [ "${badmodule}" = "true" ]; then
+  echo "Diff found between ./go.mod and ./pkg/apis/go.mod"
+  exit 1
+fi
