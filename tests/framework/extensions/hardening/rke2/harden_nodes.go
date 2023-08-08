e6ab89301ec489ea040a79e2a31479e1918a46ba
Markus Walker <markus.walker.25@gmail.com>
2023-08-08 10:32:27 -0700

    Add support for hardening RKE2/K3S v1.25+ custom clusters

--- tests/framework/extensions/hardening/rke2/harden_nodes.go
+++ tests/framework/extensions/hardening/rke2/harden_nodes.go
@@ -11,40 +11,41 @@ import (
 )
 
 func HardeningNodes(client *rancher.Client, hardened bool, nodes []*nodes.Node, nodeRoles []string) error {
+	logrus.Infof("Starting to harden nodes")
 	for key, node := range nodes {
 		logrus.Infof("Setting kernel parameters on node %s", node.NodeID)
 		_, err := node.ExecuteCommand("sudo bash -c 'echo vm.panic_on_oom=0 >> /etc/sysctl.d/90-kubelet.conf'")
 		if err != nil {
 			return err
 		}
+
 		_, err = node.ExecuteCommand("sudo bash -c 'echo vm.overcommit_memory=1 >> /etc/sysctl.d/90-kubelet.conf'")
 		if err != nil {
 			return err
 		}
+
 		_, err = node.ExecuteCommand("sudo bash -c 'echo kernel.panic=10 >> /etc/sysctl.d/90-kubelet.conf'")
 		if err != nil {
 			return err
 		}
+
 		_, err = node.ExecuteCommand("sudo bash -c 'echo kernel.panic_on_oops=1 >> /etc/sysctl.d/90-kubelet.conf'")
 		if err != nil {
 			return err
 		}
+
 		_, err = node.ExecuteCommand("sudo bash -c 'sysctl -p /etc/sysctl.d/90-kubelet.conf'")
 		if err != nil {
 			return err
 		}
+
 		if strings.Contains(nodeRoles[key], "--etcd") {
 			_, err = node.ExecuteCommand("sudo useradd -r -c \"etcd user\" -s /sbin/nologin -M etcd -U")
 			if err != nil {
 				return err
 			}
 		}
-	}
-	return nil
-}
 
-func PostHardeningConfig(client *rancher.Client, hardened bool, nodes []*nodes.Node, nodeRoles []string) error {
-	for key, node := range nodes {
 		if strings.Contains(nodeRoles[key], "--controlplane") {
 			logrus.Infof("Copying over files to node %s", node.NodeID)
 			user, err := user.Current()
@@ -57,10 +58,12 @@ func PostHardeningConfig(client *rancher.Client, hardened bool, nodes []*nodes.N
 			if err != nil {
 				return err
 			}
+
 			err = node.SCPFileToNode(dirPath+"/account-update.sh", "/home/"+node.SSHUser+"/account-update.sh")
 			if err != nil {
 				return err
 			}
+
 			_, err = node.ExecuteCommand("sudo bash -c 'mv /home/" + node.SSHUser + "/account-update.yaml /var/lib/rancher/rke2/server/account-update.yaml'")
 			if err != nil {
 				return err
@@ -69,16 +72,18 @@ func PostHardeningConfig(client *rancher.Client, hardened bool, nodes []*nodes.N
 			if err != nil {
 				return err
 			}
+
 			_, err = node.ExecuteCommand("sudo bash -c 'chmod +x /var/lib/rancher/rke2/server/account-update.sh'")
 			if err != nil {
 				return err
 			}
+
 			_, err = node.ExecuteCommand("sudo bash -c 'export KUBECONFIG=/etc/rancher/rke2/rke2.yaml && /var/lib/rancher/rke2/server/account-update.sh'")
 			if err != nil {
 				return err
 			}
-			break
 		}
 	}
+
 	return nil
 }
