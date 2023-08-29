ec64d28ea88a5e5a1ed3d15e773f46ded3b937c9
nicholasSUSE <nicholas.paolillo@suse.com>
2023-08-29 17:48:27 -0300

    exchanged unit-tests files
    removed mocked tests at git_test.go
    replaced the previously mocked tests with a real git repository at
    download_test.go
    replaced preivous tests from downloads_test.go at utils_test.go

--- pkg/catalogv2/git/download.go
+++ pkg/catalogv2/git/download.go
@@ -6,43 +6,50 @@ import (
 
 // Ensure will check if repo is cloned, if not the method will clone and reset to the latest commit.
 // If reseting to the latest commit is not possible it will fetch and try to reset again
-func (r *repository) Ensure(branch string) error {
+func (r *Repository) Ensure(branch string) error {
 	err := r.cloneOrOpen("")
 	if err != nil {
-		return err
+		return fmt.Errorf("Ensure failure: %w", err)
 	}
 
-	// Try to reset to the given commit, if success exit
+	// Try to reset to the given branch, if success exit
 	localBranchFullName := fmt.Sprintf("refs/heads/%s", branch)
 	err = r.hardReset(localBranchFullName)
 	if err == nil {
 		return nil
 	}
 
-	// If we do not have the commit locally, fetch and reset
-	return r.fetchAndReset(branch)
+	// If we do not have the branch locally, fetch and reset
+	err = r.fetchAndReset(branch)
+	if err != nil {
+		return fmt.Errorf("Ensure failure: %w", err)
+	}
+	return nil
 }
 
 // Head clones or sets a local repository to the HEAD of a git branch and return its commit hash.
-func (r *repository) Head(branch string) (string, error) {
+func (r *Repository) Head(branch string) (string, error) {
 	err := r.cloneOrOpen(branch)
 	if err != nil {
-		return "", err
+		return "", fmt.Errorf("Head failure: %w", err)
 	}
 
 	err = r.hardReset("HEAD")
 	if err != nil {
-		return "", err
+		return "", fmt.Errorf("Head failure: %w", err)
 	}
 
 	commit, err := r.getCurrentCommit()
-	return commit.String(), err
+	if err != nil {
+		return commit.String(), fmt.Errorf("Head failure: %w", err)
+	}
+	return commit.String(), nil
 }
 
 // CheckUpdate will check if rancher is in bundled mode,
 // if it is not in bundled mode, will make an update.
 // if it is in bundled mode, will just call Head method.
-func (r *repository) CheckUpdate(branch, systemCatalogMode string) (string, error) {
+func (r *Repository) CheckUpdate(branch, systemCatalogMode string) (string, error) {
 	if isBundled(r.Directory) && systemCatalogMode == "bundled" {
 		return r.Head(branch)
 	}
@@ -57,33 +64,38 @@ func (r *repository) CheckUpdate(branch, systemCatalogMode string) (string, erro
 
 // Update will check if repository exists, if exists will check for latest commit and update to it.
 // If the repository does not exist will try cloning again.
-func (r *repository) Update(branch string) (string, error) {
+func (r *Repository) Update(branch string) (string, error) {
 	err := r.cloneOrOpen(branch)
 	if err != nil {
-		return "", err
+		return "", fmt.Errorf("Update failure: %w", err)
 	}
 
 	err = r.hardReset("HEAD")
 	if err != nil {
-		return "", err
+		return "", fmt.Errorf("Update failure: %w", err)
 	}
 
 	commit, err := r.getCurrentCommit()
 	if err != nil {
-		return commit.String(), err
+		return commit.String(), fmt.Errorf("Update failure: %w", err)
 	}
 
 	lastCommit, err := r.getLastCommitHash(branch, commit)
-	if err != nil || lastCommit == commit {
-		return commit.String(), err
+	if err != nil {
+		return commit.String(), fmt.Errorf("Update failure: %w", err)
+	}
+	if lastCommit == commit {
+		return commit.String(), nil
 	}
 
 	err = r.fetchAndReset(branch)
 	if err != nil {
-		return commit.String(), err
+		return "", fmt.Errorf("Update failure: %w", err)
 	}
 
 	lastCommitRef, err := r.getCurrentCommit()
-
-	return lastCommitRef.String(), err
+	if err != nil {
+		return lastCommitRef.String(), fmt.Errorf("Update failure: %w", err)
+	}
+	return lastCommitRef.String(), nil
 }
