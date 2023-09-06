634de0af59624056e8c6ae95d1ce4287ff50821d
felipe-colussi <felipe.colussioliva@suse.com>
2023-09-06 14:10:34 -0300

    add test for ResolveWithCluster

--- pkg/image/resolve_test.go
+++ pkg/image/resolve_test.go
@@ -1,9 +1,16 @@
 package image
 
 import (
+	"fmt"
+	"os"
 	"testing"
 
+	apimgmtv3 "github.com/rancher/rancher/pkg/apis/management.cattle.io/v3"
+	v3 "github.com/rancher/rancher/pkg/generated/norman/management.cattle.io/v3"
+	"github.com/rancher/rancher/pkg/settings"
+	rketypes "github.com/rancher/rke/types"
 	assertlib "github.com/stretchr/testify/assert"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func TestConvertMirroredImages(t *testing.T) {
@@ -39,5 +46,128 @@ func TestConvertMirroredImages(t *testing.T) {
 		imagesSet := cs.inputRawImages
 		convertMirroredImages(imagesSet)
 		assert.Equal(cs.outputImagesShouldEqual, imagesSet)
+	}
+}
+
+func TestResolveWithCluster(t *testing.T) {
+	if os.Getenv("CATTLE_BASE_REGISTRY") != "" {
+		fmt.Println("Skiping TestResolveWithCluster. Can't run the tests with CATTLE_BASE_REGISTRY set")
+	}
+
+	clusterWithPrivateRegistry := func(s string) *v3.Cluster {
+		return &v3.Cluster{
+			TypeMeta:   metav1.TypeMeta{},
+			ObjectMeta: metav1.ObjectMeta{},
+			Spec: apimgmtv3.ClusterSpec{
+				ClusterSpecBase: apimgmtv3.ClusterSpecBase{
+					RancherKubernetesEngineConfig: &rketypes.RancherKubernetesEngineConfig{
+						PrivateRegistries: []rketypes.PrivateRegistry{{
+							URL: s,
+						},
+						},
+					},
+				},
+			},
+		}
+	}
+	type input struct {
+		image              string
+		CattleBaseRegistry string
+		cluster            *v3.Cluster
+	}
+	tests := []struct {
+		name     string
+		input    input
+		expected string
+	}{
+		{
+			name: "No cluster no default registry",
+			input: input{
+				image:              "imagename",
+				CattleBaseRegistry: "",
+				cluster:            nil,
+			},
+			expected: "imagename",
+		},
+		{
+			name: "No cluster with default registry, image without rancher/",
+			input: input{
+				image:              "imagename",
+				CattleBaseRegistry: "custom-registry",
+				cluster:            nil,
+			},
+			expected: "custom-registry/rancher/imagename",
+		},
+		{
+			name: "No cluster with default registry, image with rancher/",
+			input: input{
+				image:              "rancher/imagename",
+				CattleBaseRegistry: "custom-registry",
+				cluster:            nil,
+			},
+			expected: "custom-registry/rancher/imagename",
+		},
+		{
+			name: "Cluster empty URL, no default registry",
+			input: input{
+				image:              "imagename",
+				CattleBaseRegistry: "",
+				cluster:            &v3.Cluster{},
+			},
+			expected: "imagename",
+		},
+		{
+			name: "Cluster empty URL, with default registry",
+			input: input{
+				image:              "imagename",
+				CattleBaseRegistry: "default-registry.com",
+				cluster:            &v3.Cluster{},
+			},
+			expected: "default-registry.com/rancher/imagename",
+		},
+		{
+			name: "Cluster empty URL, with default registry and rancher on image name",
+			input: input{
+				image:              "rancher/imagename",
+				CattleBaseRegistry: "default-registry.com",
+				cluster:            &v3.Cluster{},
+			},
+			expected: "default-registry.com/rancher/imagename",
+		},
+		{
+			name: "Cluster with URL, no default registry, no rancher/ on image",
+			input: input{
+				image:              "imagename",
+				CattleBaseRegistry: "",
+				cluster:            clusterWithPrivateRegistry("cluster-url.com"),
+			},
+			expected: "cluster-url.com/rancher/imagename",
+		},
+		{
+			name: "Cluster with URL, no default registry, with rancher/ on image",
+			input: input{
+				image:              "imagename",
+				CattleBaseRegistry: "",
+				cluster:            clusterWithPrivateRegistry("cluster-url.com"),
+			},
+			expected: "cluster-url.com/rancher/imagename",
+		},
+		{
+			name: "Cluster with URL, and default registry, no rancher/ on image",
+			input: input{
+				image:              "imagename",
+				CattleBaseRegistry: "registry-url",
+				cluster:            clusterWithPrivateRegistry("cluster-url.com"),
+			},
+			expected: "cluster-url.com/rancher/imagename",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := settings.SystemDefaultRegistry.Set(tt.input.CattleBaseRegistry); err != nil {
+				t.Errorf("Failed to test TestResolveWithCluster(), unable to set SystemDefaultRegistry with the value: %v", err)
+			}
+			assertlib.Equalf(t, tt.expected, ResolveWithCluster(tt.input.image, tt.input.cluster), "ResolveWithCluster(%v, %v)", tt.input.image, tt.input.cluster)
+		})
 	}
 }
