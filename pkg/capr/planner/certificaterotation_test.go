b6d2b2a1dfbf815a995e9fbeba0e58f3379dc658
Rayan Das <rayandas91@gmail.com>
2023-09-13 19:35:45 +0530

    remove psp related tests

--- pkg/capr/planner/certificaterotation_test.go
+++ pkg/capr/planner/certificaterotation_test.go
@@ -221,7 +221,6 @@ func Test_rotateCertificatesPlan(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			//t.Parallel()
 			mockPlanner := newMockPlanner(t, InfoFunctions{
 				SystemAgentImage: func() string { return "system-agent" },
 				ImageResolver:    image.ResolveWithControlPlane,
