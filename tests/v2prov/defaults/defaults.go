5f68cee034232be3222d83be0b4beefd9353250d
felipe-colussi <felipe.colussioliva@suse.com>
2023-09-25 10:45:12 -0300

    increase time

--- tests/v2prov/defaults/defaults.go
+++ tests/v2prov/defaults/defaults.go
@@ -12,7 +12,7 @@ var (
 	ObjectStoreServerImage = "rancher/mirrored-minio-minio:RELEASE.2022-12-12T19-27-27Z"
 	ObjectStoreUtilImage   = "rancher/mirrored-minio-mc:RELEASE.2022-12-13T00-23-28Z"
 	SomeK8sVersion         = os.Getenv("SOME_K8S_VERSION")
-	WatchTimeoutSeconds    = int64(240) // 15 minutes.
+	WatchTimeoutSeconds    = int64(900) // 15 minutes.
 	CommonClusterConfig    = map[string]interface{}{
 		"service-cidr": "10.45.0.0/16",
 		"cluster-cidr": "10.44.0.0/16",
