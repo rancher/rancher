cd0066aaac888ea3a6f8b481d9585c230bbcd47f
Johnatas <johnatasr@hotmail.com>
2023-09-27 20:17:13 -0300

    add changes for pr 42000, using envs from build.yml
    Signed-off-by: Johnatas <johnatasr@hotmail.com>

--- .github/scripts/check-final-rc-deps.sh
+++ .github/scripts/check-final-rc-deps.sh
@@ -1,22 +1,17 @@
 #!/bin/sh
 set -ue
 
-FILE_PATHS="
-    Dockerfile.dapper
-    go.mod
-    ./package/Dockerfile
-    ./pkg/apis/go.mod
-    ./pkg/settings/setting.go
-    ./scripts/package-env
-"
+FINAL_RC_PARTIAL_COMMIT_MESSAGE=$(yq eval '.FINAL_RC_PARTIAL_COMMIT_MESSAGE' build.yml)
+FINAL_RC_PRERELEASE_TITLE=$(yq eval '.FINAL_RC_PRERELEASE_TITLE' build.yml)
+FINAL_RC_PRERELEASE_REGEX=$(yq eval '.FINAL_RC_PRERELEASE_REGEX' build.yml)
+FINAL_RC_FILES=$(yq eval '.FINAL_RC_FILES[]' build.yml)
 HEAD_COMMIT_MESSAGE=$(git log -2 --pretty=format:"%s")
-PARTIAL_FINAL_RC_COMMIT_MESSAGE="last commit for final rc"
 RELEASE_TITLE=$(echo "$RELEASE_TITLE")
 BAD_FILES=false
 
-if echo "$HEAD_COMMIT_MESSAGE" | grep -q "$PARTIAL_FINAL_RC_COMMIT_MESSAGE" || echo "$RELEASE_TITLE" | grep -Eq '^Pre-release v2\.8\.[0-9]{1,100}-rc[1-9][0-9]{0,1}$'; then
+if echo "$HEAD_COMMIT_MESSAGE" | grep -q "$FINAL_RC_PARTIAL_COMMIT_MESSAGE" || echo "$RELEASE_TITLE" | grep -Eq "^${FINAL_RC_PRERELEASE_TITLE}${FINAL_RC_PRERELEASE_REGEX}"; then
     echo "Starting check..."
-    for FILE in $FILE_PATHS; do
+    for FILE in $FINAL_RC_FILES; do
         if grep -q -E '\-rc[0-9]+' "$FILE"; then
             BAD_FILES=true
             echo "error: ${FILE} contains rc tags."
