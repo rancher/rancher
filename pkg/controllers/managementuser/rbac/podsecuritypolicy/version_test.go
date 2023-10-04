65918f685cfe44bd884c0bd9f9d42dd88abb840f
Greg Sidelinger <sidelig@nationwide.com>
2023-10-04 12:16:46 -0400

    Adding a version test suite for podsecuritypolicy Fxing podsecuritypolicy version checks for RKE2
    Signed-off-by: Greg Sidelinger <sidelig@nationwide.com>
    (cherry picked from commit 52e6d5e75b66b73466e34f7dac1ac6f67bd1bc8c)

--- pkg/controllers/managementuser/rbac/podsecuritypolicy/version_test.go
+++ pkg/controllers/managementuser/rbac/podsecuritypolicy/version_test.go
@@ -1,63 +1,58 @@
-package podsecuritypolicy_test
+package podsecuritypolicy
 
 import (
 	"fmt"
 	"testing"
 
-	v3 "github.com/rancher/rancher/pkg/apis/management.cattle.io/v3"
-	"github.com/rancher/rancher/pkg/controllers/managementuser/rbac/podsecuritypolicy"
+	apimgmtv3 "github.com/rancher/rancher/pkg/apis/management.cattle.io/v3"
+	mgmtv3 "github.com/rancher/rancher/pkg/generated/norman/management.cattle.io/v3"
 	"github.com/rancher/rancher/pkg/generated/norman/management.cattle.io/v3/fakes"
-	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/assert"
 	"k8s.io/apimachinery/pkg/version"
 )
 
-func newClusterLister(kubernetesVersion string) *fakes.ClusterListerMock {
+func newClusterListerWithVersion(kubernetesVersion string) *fakes.ClusterListerMock {
 	return &fakes.ClusterListerMock{
-		GetFunc: func(namespace, name string) (*v3.Cluster, error) {
-			if name == "invalid" {
-				return nil, fmt.Errorf("invalid cluster: %s", name)
-			} else if name == "not ready" {
-				return &v3.Cluster{Status: v3.ClusterStatus{}}, nil
-			} else {
-				return &v3.Cluster{Status: v3.ClusterStatus{Version: &version.Info{GitVersion: kubernetesVersion}}}, nil
+		GetFunc: func(namespace, name string) (*mgmtv3.Cluster, error) {
+			if name == "test" {
+				cluster := mgmtv3.Cluster{
+					Status: apimgmtv3.ClusterStatus{
+						Version: &version.Info{
+							GitVersion: kubernetesVersion,
+						},
+					},
+				}
+				return &cluster, nil
 			}
+			return nil, fmt.Errorf("invalid cluster: %s", name)
 		},
 	}
 }
 
-func TestCheckClusterVersionFailsForVersionsThatCannotBeParsed(t *testing.T) {
-	t.Parallel()
-	tests := []string{"", "⌘⌘⌘", "v1.2", "v1.24", "v1.24.a", "1.24", "1.24.a", "v1.24+rke2r1"}
-	for _, v := range tests {
-		v := v
-		t.Run(v, func(t *testing.T) {
-			t.Parallel()
-			clusterLister := newClusterLister(v)
-			err := podsecuritypolicy.CheckClusterVersion("test", clusterLister)
-			require.Error(t, err)
-			require.NotErrorIs(t, err, podsecuritypolicy.ErrClusterVersionIncompatible)
-		})
-	}
-}
+func TestCheckClusterVersion(t *testing.T) {
 
-func TestCheckClusterVersionInspectsValidVersionsForCompatibilityWithPSP(t *testing.T) {
-	t.Parallel()
-	tests := []struct {
+	tests := []*struct {
 		version string
 		wantErr bool
+		setup   func()
 	}{
-		// regular version strings
+		// tests for version string size
 		{
-			version: "1.24.9",
+			version: "",
+			wantErr: true,
+		},
+		{
+			version: "v1.24",
 			wantErr: false,
 		},
+		{
+			version: "v1.2",
+			wantErr: true,
+		},
+		// rke1 version strings
 		{
 			version: "v1.24.9",
 			wantErr: false,
-		},
-		{
-			version: "1.25.9",
-			wantErr: true,
 		},
 		{
 			version: "v1.25.9",
@@ -80,19 +75,6 @@ func TestCheckClusterVersionInspectsValidVersionsForCompatibilityWithPSP(t *test
 			version: "v1.26.9+k3s1",
 			wantErr: true,
 		},
-		// rke1 version strings
-		{
-			version: "v1.24.9-rancher1-1",
-			wantErr: false,
-		},
-		{
-			version: "v1.25.9-rancher1-1",
-			wantErr: true,
-		},
-		{
-			version: "v1.26.9-rancher1-1",
-			wantErr: true,
-		},
 		// rke2 version strings
 		{
 			version: "v1.24.9+rke2r1",
@@ -106,51 +88,18 @@ func TestCheckClusterVersionInspectsValidVersionsForCompatibilityWithPSP(t *test
 			version: "v1.26.9+rke2r1",
 			wantErr: true,
 		},
-		// cloud provider version strings
-		{
-			version: "v1.27.3-gke.100",
-			wantErr: true,
-		},
-		{
-			version: "v1.26.9-eks-f8587cb",
-			wantErr: true,
-		},
-		{
-			version: "v1.24.9-eks-f8587cb",
-			wantErr: false,
-		},
 	}
 	for _, tt := range tests {
-		tt := tt
 		t.Run(tt.version, func(t *testing.T) {
-			t.Parallel()
-			clusterLister := newClusterLister(tt.version)
-			err := podsecuritypolicy.CheckClusterVersion("test", clusterLister)
+			clusterLister := newClusterListerWithVersion(tt.version)
+			println(tt.version)
+			err := checkClusterVersion("test", clusterLister)
 			if tt.wantErr {
-				require.Error(t, err)
-				require.ErrorIs(t, err, podsecuritypolicy.ErrClusterVersionIncompatible)
-			} else {
-				require.NoError(t, err)
+				println(err.Error())
+				assert.Error(t, err, "Expected checkClusterVersion to raise error.")
+				return
 			}
 		})
 	}
-}
 
-func TestCheckClusterVersionFailsWhenItCannotFetchVersion(t *testing.T) {
-	t.Parallel()
-	t.Run("version check fails when it can't get cluster", func(t *testing.T) {
-		t.Parallel()
-		clusterLister := newClusterLister("")
-		err := podsecuritypolicy.CheckClusterVersion("invalid", clusterLister)
-		require.Error(t, err)
-		require.NotErrorIs(t, err, podsecuritypolicy.ErrClusterVersionIncompatible)
-	})
-
-	t.Run("version check fails when the version is not yet known", func(t *testing.T) {
-		t.Parallel()
-		clusterLister := newClusterLister("")
-		err := podsecuritypolicy.CheckClusterVersion("not ready", clusterLister)
-		require.Error(t, err)
-		require.NotErrorIs(t, err, podsecuritypolicy.ErrClusterVersionIncompatible)
-	})
 }
