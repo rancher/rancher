8499ed8b6f036cebb7be48f1c563aace28c15602
Venkata Krishna Rohit Sakala <rohitsakala@gmail.com>
2023-10-11 17:25:20 -0700

    Revert "exchanged unit-tests files"
    This reverts commit ec64d28ea88a5e5a1ed3d15e773f46ded3b937c9.

--- pkg/catalogv2/git/download_test.go
+++ pkg/catalogv2/git/download_test.go
@@ -1,188 +1,57 @@
 package git
 
 import (
-	"os"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
-	corev1 "k8s.io/api/core/v1"
+	assertlib "github.com/stretchr/testify/assert"
 )
 
-const chartsSmallForkURL = "https://github.com/rancher/charts-small-fork"
-const mainBranch = "main"
-const lastBranch = "test-1"
-
-func TestMain(m *testing.M) {
-	// Run all the tests
-	exitCode := m.Run()
-
-	// Cleanup after tests
-	cleanup()
-
-	// Exit with the proper code
-	os.Exit(exitCode)
-}
-
-func cleanup() {
-	// Delete the management-state directory
-	os.RemoveAll("management-state")
-}
-
-func Test_Ensure(t *testing.T) {
+func Test_isGitSSH(t *testing.T) {
 	testCases := []struct {
-		test            string
-		secret          *corev1.Secret
-		namespace       string
-		name            string
-		gitURL          string
-		insecureSkipTLS bool
-		caBundle        []byte
-		branch          string
-		expectedError   error
+		gitURL   string
+		expected bool
+	}{
+		// True cases
+		{"customusername@github.com:user/repo.git", true},
+		{"customusername@gitlab.com:user/repo.git", true},
+		{"customusername@gitlab.com:user/repo", true},
+		{"customusername@gitlab.com:user/repo-with-dashes.git", true},
+		{"git@github.com:user/repo.git", true},
+		{"git@gitlab.com:user/repo-with-dashes.git", true},
+		{"git@gitlab.com:user/repo", true},
+		// False cases
+		{"https://github.com/user/repo.git", false},
+		{"http://gitlab.com/user/repo.git", false},
+		{"http://gitlab.com/user/repo", false},
+		{"http://gitlab.com", false},
+		{"git@gitlab.com", false},
+	}
+	assert := assertlib.New(t)
+	for _, tc := range testCases {
+		actual, err := isGitSSH(tc.gitURL)
+		if err != nil {
+			t.Errorf("unexpected error: %s", err)
+		}
+		assert.Equalf(tc.expected, actual, "testcase: %v", tc)
+	}
+}
+
+func Test_gitDir(t *testing.T) {
+	assert := assertlib.New(t)
+	testCases := []struct {
+		namespace string
+		name      string
+		gitURL    string
+		expected  string
 	}{
 		{
-			test:            "#1 TestCase: Success - Clone, Reset And Exit",
-			secret:          nil,
-			namespace:       "cattle-test",
-			name:            "small-fork-test",
-			gitURL:          chartsSmallForkURL,
-			insecureSkipTLS: false,
-			caBundle:        []byte{},
-			branch:          mainBranch,
-			expectedError:   nil,
+			"namespace", "name", "https://git.rancher.io/charts",
+			"management-state/git-repo/namespace/name/4b40cac650031b74776e87c1a726b0484d0877c3ec137da0872547ff9b73a721",
 		},
-		{
-			test:            "#2 TestCase: Success - Clone, Reset And Fetch Last Branch",
-			secret:          nil,
-			namespace:       "cattle-test",
-			name:            "small-fork-test",
-			gitURL:          chartsSmallForkURL,
-			insecureSkipTLS: false,
-			caBundle:        []byte{},
-			branch:          lastBranch,
-			expectedError:   nil,
-		},
+		// NOTE(manno): cannot test the other cases without poluting the filesystem
 	}
-
 	for _, tc := range testCases {
-		t.Run(tc.name, func(t *testing.T) {
-			repo, err := BuildRepoConfig(tc.secret, tc.namespace, tc.name, tc.gitURL, tc.insecureSkipTLS, tc.caBundle)
-			// Check the error
-			if tc.expectedError == nil && tc.expectedError != err {
-				t.Errorf("Expected error: %v |But got: %v", tc.expectedError, err)
-			}
-
-			err = repo.Ensure(tc.branch)
-			// Check the error
-			if tc.expectedError == nil && tc.expectedError != err {
-				t.Errorf("Expected error: %v |But got: %v", tc.expectedError, err)
-			}
-			// Only testing error in some cases
-			if err != nil {
-				assert.EqualError(t, tc.expectedError, err.Error())
-			}
-		})
-	}
-}
-
-func Test_Head(t *testing.T) {
-	testCases := []struct {
-		test            string
-		secret          *corev1.Secret
-		namespace       string
-		name            string
-		gitURL          string
-		insecureSkipTLS bool
-		caBundle        []byte
-		branch          string
-		expectedCommit  string
-		expectedError   error
-	}{
-		{
-			test:            "#1 TestCase: Success - Clone, Reset And Return Commit",
-			secret:          nil,
-			namespace:       "cattle-test",
-			name:            "small-fork-test",
-			gitURL:          chartsSmallForkURL,
-			insecureSkipTLS: false,
-			caBundle:        []byte{},
-			branch:          lastBranch,
-			expectedCommit:  "226d544def39de56db210e96d2b0b535badf9bdd",
-			expectedError:   nil,
-		},
-	}
-
-	for _, tc := range testCases {
-		t.Run(tc.name, func(t *testing.T) {
-			repo, err := BuildRepoConfig(tc.secret, tc.namespace, tc.name, tc.gitURL, tc.insecureSkipTLS, tc.caBundle)
-			// Check the error
-			if tc.expectedError == nil && tc.expectedError != err {
-				t.Errorf("Expected error: %v |But got: %v", tc.expectedError, err)
-			}
-
-			commit, err := repo.Head(tc.branch)
-			// Check the error
-			if tc.expectedError == nil && tc.expectedError != err {
-				t.Errorf("Expected error: %v |But got: %v", tc.expectedError, err)
-			}
-			// Only testing error in some cases
-			if err != nil {
-				assert.EqualError(t, tc.expectedError, err.Error())
-			}
-
-			assert.Equal(t, len(commit), len(tc.expectedCommit))
-		})
-	}
-}
-
-func Test_CheckUpdate(t *testing.T) {
-	testCases := []struct {
-		test              string
-		secret            *corev1.Secret
-		namespace         string
-		name              string
-		gitURL            string
-		insecureSkipTLS   bool
-		caBundle          []byte
-		branch            string
-		systemCatalogMode string
-		expectedCommit    string
-		expectedError     error
-	}{
-		{
-			test:              "#1 TestCase: Success ",
-			secret:            nil,
-			namespace:         "cattle-test",
-			name:              "small-fork-test",
-			gitURL:            chartsSmallForkURL,
-			insecureSkipTLS:   false,
-			caBundle:          []byte{},
-			branch:            lastBranch,
-			systemCatalogMode: "",
-			expectedCommit:    "226d544def39de56db210e96d2b0b535badf9bdd",
-			expectedError:     nil,
-		},
-	}
-
-	for _, tc := range testCases {
-		t.Run(tc.name, func(t *testing.T) {
-			repo, err := BuildRepoConfig(tc.secret, tc.namespace, tc.name, tc.gitURL, tc.insecureSkipTLS, tc.caBundle)
-			// Check the error
-			if tc.expectedError == nil && tc.expectedError != err {
-				t.Errorf("Expected error: %v |But got: %v", tc.expectedError, err)
-			}
-
-			commit, err := repo.CheckUpdate(tc.branch, tc.systemCatalogMode)
-			// Check the error
-			if tc.expectedError == nil && tc.expectedError != err {
-				t.Errorf("Expected error: %v |But got: %v", tc.expectedError, err)
-			}
-			// Only testing error in some cases
-			if err != nil {
-				assert.EqualError(t, tc.expectedError, err.Error())
-			}
-
-			assert.Equal(t, len(commit), len(tc.expectedCommit))
-		})
+		actual := gitDir(tc.namespace, tc.name, tc.gitURL)
+		assert.Equalf(tc.expected, actual, "testcase: %v", tc)
 	}
 }
