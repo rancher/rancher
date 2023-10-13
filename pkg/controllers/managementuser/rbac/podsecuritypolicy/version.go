cf0fddd76b5b05721bc4dc9c902bacd8da0e849b
Max Sokolovsky <genexpr@protonmail.com>
2023-10-13 10:42:10 -0400

    Rewrite cluster version check for PSPs by using a k8s.io package
    (cherry picked from commit f2bdf8595740d6f42d60db5b643c6f8b92e918c0)

--- pkg/controllers/managementuser/rbac/podsecuritypolicy/version.go
+++ pkg/controllers/managementuser/rbac/podsecuritypolicy/version.go
@@ -4,27 +4,33 @@ import (
 	"errors"
 	"fmt"
 
-	mVersion "github.com/mcuadros/go-version"
 	v3 "github.com/rancher/rancher/pkg/generated/norman/management.cattle.io/v3"
+	"k8s.io/apimachinery/pkg/util/version"
 )
 
 var errVersionIncompatible = errors.New("podsecuritypolicies are not available in Kubernetes v1.25 and above")
-var clusterVersionCheckErrorString = "failed to check cluster version compatibility for podsecuritypolicy controllers: %v"
+
+const clusterVersionCheckErrorString = "failed to check cluster version compatibility for podsecuritypolicy controllers: %v"
 
 // checkClusterVersion tries to fetch a cluster by name, extract its Kubernetes version,
 // and check if the version is less than v1.25.
 func checkClusterVersion(clusterName string, clusterLister v3.ClusterLister) error {
 	cluster, err := clusterLister.Get("", clusterName)
 	if err != nil {
-		return fmt.Errorf("failed to get cluster [%s]: %w", clusterName, err)
+		return fmt.Errorf("failed to get cluster %s: %w", clusterName, err)
 	}
 	if cluster.Status.Version == nil {
-		return fmt.Errorf("cannot validate Kubernetes version for podsecuritypolicy capability: cluster [%s] status version is not available yet", clusterName)
+		return fmt.Errorf("cluster %s version is not available yet", clusterName)
 	}
-	if len(cluster.Status.Version.String()) < 5 {
-		return fmt.Errorf("cannot validate Kubernetes version for podsecuritypolicy capability: cluster [%s] status version [%s] is too small", clusterName, cluster.Status.Version.String())
+	v, err := version.ParseSemantic(cluster.Status.Version.String())
+	if err != nil {
+		return err
 	}
-	if mVersion.Compare(cluster.Status.Version.String()[0:5], "v1.25", ">=") {
+	v125, err := version.ParseSemantic("v1.25.0")
+	if err != nil {
+		return err
+	}
+	if v.AtLeast(v125) {
 		return errVersionIncompatible
 	}
 	return nil
