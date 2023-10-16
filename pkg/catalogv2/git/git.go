5db6cff86d2801466ad70a9a06047e44fc652734
nicholasSUSE <nicholas.paolillo@suse.com>
2023-10-16 14:23:29 -0300

    removing deprecated io/ioutil methods

--- pkg/catalogv2/git/git.go
+++ pkg/catalogv2/git/git.go
@@ -6,7 +6,6 @@ import (
 	"crypto/x509"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"net"
 	"net/http"
 	"net/url"
@@ -103,7 +102,7 @@ func (g *git) injectAgent(cmd *exec.Cmd) (io.Closer, error) {
 		return nil, err
 	}
 
-	tmpDir, err := ioutil.TempDir("", "ssh-agent")
+	tmpDir, err := os.MkdirTemp("", "ssh-agent")
 	if err != nil {
 		return nil, err
 	}
@@ -303,7 +302,7 @@ func (g *git) gitCmd(output io.Writer, args ...string) error {
 	}
 
 	if len(g.knownHosts) != 0 {
-		f, err := ioutil.TempFile("", "known_hosts")
+		f, err := os.CreateTemp("", "known_hosts")
 		if err != nil {
 			return err
 		}
@@ -328,7 +327,7 @@ func (g *git) gitCmd(output io.Writer, args ...string) error {
 	}
 
 	if len(g.caBundle) > 0 {
-		f, err := ioutil.TempFile("", "ca-pem-")
+		f, err := os.CreateTemp("", "ca-pem-")
 		if err != nil {
 			return err
 		}
