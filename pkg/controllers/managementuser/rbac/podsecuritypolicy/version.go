b1acd2817bf026e483c7bbd888479f66956e7521
Max Sokolovsky <genexpr@protonmail.com>
2023-10-17 19:13:57 -0400

    Do not register pspdelete controller if the cluster version is >= 1.25
    (cherry picked from commit df6f162e25aa5fc56d2caf9ebe8e666f6e70bd46)

--- pkg/controllers/managementuser/rbac/podsecuritypolicy/version.go
+++ pkg/controllers/managementuser/rbac/podsecuritypolicy/version.go
@@ -8,13 +8,12 @@ import (
 	"k8s.io/apimachinery/pkg/util/version"
 )
 
-var errVersionIncompatible = errors.New("podsecuritypolicies are not available in Kubernetes v1.25 and above")
+var ErrClusterVersionIncompatible = errors.New("podsecuritypolicies are not available in Kubernetes v1.25 and above")
 
-const clusterVersionCheckErrorString = "failed to check cluster version compatibility for podsecuritypolicy controllers: %v"
-
-// checkClusterVersion tries to fetch a cluster by name, extract its Kubernetes version,
-// and check if the version is less than v1.25.
-func checkClusterVersion(clusterName string, clusterLister v3.ClusterLister) error {
+// CheckClusterVersion tries to fetch a management.cattle.io Cluster by name, extract its Kubernetes version,
+// and check if the cluster supports PodSecurityPolicies. If the version can be parsed, the function checks if it's
+// at least 1.25.x. If yes, it returns a special ErrClusterVersionIncompatible error.
+func CheckClusterVersion(clusterName string, clusterLister v3.ClusterLister) error {
 	cluster, err := clusterLister.Get("", clusterName)
 	if err != nil {
 		return fmt.Errorf("failed to get cluster %s: %w", clusterName, err)
@@ -31,7 +30,7 @@ func checkClusterVersion(clusterName string, clusterLister v3.ClusterLister) err
 		return err
 	}
 	if v.AtLeast(v125) {
-		return errVersionIncompatible
+		return ErrClusterVersionIncompatible
 	}
 	return nil
 }
