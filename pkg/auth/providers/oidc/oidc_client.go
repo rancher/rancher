402994dd5d32bc3ad3520c971ccc8a841b542e67
Ethan Howell <ethan.c.howell@hotmail.com>
2023-10-23 17:54:32 -0400

    Update tls handling for oidc client to use system cert pool.

--- pkg/auth/providers/oidc/oidc_client.go
+++ pkg/auth/providers/oidc/oidc_client.go
@@ -4,37 +4,50 @@ import (
 	"context"
 	"crypto/tls"
 	"crypto/x509"
+	"github.com/pkg/errors"
 	"net/http"
 
 	"github.com/coreos/go-oidc/v3/oidc"
 )
 
 func AddCertKeyToContext(ctx context.Context, certificate, key string) (context.Context, error) {
-	if certificate != "" && key != "" {
-		certKeyClient := &http.Client{}
-		err := GetClientWithCertKey(certKeyClient, certificate, key)
-		if err != nil {
-			return nil, err
-		}
-		return oidc.ClientContext(ctx, certKeyClient), nil
+	certKeyClient, err := GetClientWithCertKey(certificate, key)
+	if err != nil {
+		return nil, err
 	}
-	return ctx, nil
+	return oidc.ClientContext(ctx, certKeyClient), nil
 }
 
-func GetClientWithCertKey(httpClient *http.Client, certificate, key string) error {
-	if certificate != "" && key != "" {
-		keyPair, err := tls.X509KeyPair([]byte(certificate), []byte(key))
-		if err != nil {
-			return err
-		}
-		caCertPool := x509.NewCertPool()
-		caCertPool.AppendCertsFromPEM([]byte(certificate))
-		httpClient.Transport = &http.Transport{
-			TLSClientConfig: &tls.Config{
-				RootCAs:      caCertPool,
-				Certificates: []tls.Certificate{keyPair},
-			},
-		}
+func GetClientWithCertKey(certificate, key string) (*http.Client, error) {
+	// Use the default transport to pick up on things like http.ProxyFromEnvironment
+	transport := http.DefaultTransport.(*http.Transport).Clone()
+
+	tlsConfig, err := getTLSClientConfig(certificate, key)
+	if err != nil {
+		return &http.Client{Transport: transport}, err
 	}
-	return nil
+
+	transport.TLSClientConfig = tlsConfig
+	return &http.Client{Transport: transport}, nil
+}
+
+func getTLSClientConfig(certificate, key string) (*tls.Config, error) {
+	if certificate == "" || key == "" {
+		return nil, errors.New("certificate or key provided, but not both")
+	}
+
+	keyPair, err := tls.X509KeyPair([]byte(certificate), []byte(key))
+	if err != nil {
+		return nil, err
+	}
+
+	caCertPool, err := x509.SystemCertPool()
+	if err != nil {
+		return nil, err
+	}
+
+	return &tls.Config{
+		RootCAs:      caCertPool,
+		Certificates: []tls.Certificate{keyPair},
+	}, nil
 }
