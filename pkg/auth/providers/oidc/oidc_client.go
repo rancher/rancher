5f3d2dc8c68462cf35b611fed78021485decf1ab
Ethan Howell <ethan.c.howell@hotmail.com>
2023-10-26 21:08:56 -0400

    Revert some changes to handle cases with no certs.

--- pkg/auth/providers/oidc/oidc_client.go
+++ pkg/auth/providers/oidc/oidc_client.go
@@ -6,37 +6,36 @@ import (
 	"crypto/x509"
 	"net/http"
 
-	"github.com/pkg/errors"
-
 	"github.com/coreos/go-oidc/v3/oidc"
 )
 
 func AddCertKeyToContext(ctx context.Context, certificate, key string) (context.Context, error) {
-	certKeyClient, err := GetClientWithCertKey(certificate, key)
-	if err != nil {
-		return nil, err
+	if certificate != "" && key != "" {
+		certKeyClient := &http.Client{}
+		err := GetClientWithCertKey(certKeyClient, certificate, key)
+		if err != nil {
+			return nil, err
+		}
+		return oidc.ClientContext(ctx, certKeyClient), nil
 	}
-	return oidc.ClientContext(ctx, certKeyClient), nil
+
+	return ctx, nil
 }
 
-func GetClientWithCertKey(certificate, key string) (*http.Client, error) {
-	// Use the default transport to pick up on things like http.ProxyFromEnvironment
-	transport := http.DefaultTransport.(*http.Transport).Clone()
-
+func GetClientWithCertKey(httpClient *http.Client, certificate, key string) error {
 	tlsConfig, err := getTLSClientConfig(certificate, key)
 	if err != nil {
-		return &http.Client{Transport: transport}, err
+		return err
 	}
 
+	// Use the default transport to pick up on things like http.ProxyFromEnvironment
+	transport := http.DefaultTransport.(*http.Transport).Clone()
 	transport.TLSClientConfig = tlsConfig
-	return &http.Client{Transport: transport}, nil
+	httpClient.Transport = transport
+	return nil
 }
 
 func getTLSClientConfig(certificate, key string) (*tls.Config, error) {
-	if certificate == "" || key == "" {
-		return nil, errors.New("certificate or key provided, but not both")
-	}
-
 	keyPair, err := tls.X509KeyPair([]byte(certificate), []byte(key))
 	if err != nil {
 		return nil, err
