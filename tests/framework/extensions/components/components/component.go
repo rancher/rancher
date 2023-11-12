aedb75123b01db5442d162b3e888fad4815ab3c1
Sam Gartner <samuel.gartner@suse.com>
2023-11-11 18:37:28 -0600

    First working components

--- tests/framework/extensions/components/components/component.go
+++ tests/framework/extensions/components/components/component.go
@@ -2,11 +2,39 @@ package components
 
 import (
 	"github.com/rancher/rancher/tests/framework/clients/rancher"
-	log "github.com/sirupsen/logrus"
+	v1 "github.com/rancher/rancher/tests/framework/clients/rancher/v1"
 )
 
-func GenericCreate(client *rancher.Client, obj interface{}, objType string) (result interface{}, err error) {
-	log.Info("In Create")
-	client.Steve.SteveType(objType).Create(obj)
-	return obj, err
+type Component interface {
+	Apply(poll bool, poll_interval int, async bool) (err error)
+	Revert(poll bool, poll_interval int, async bool) (err error)
+}
+
+type GenericCreate struct {
+	ObjSpecs []interface{}
+	objs     []v1.SteveAPIObject
+	ObjType  string
+	Client   *rancher.Client
+}
+
+func (gc *GenericCreate) Apply(poll bool, poll_interval int, async bool) (err error) {
+	for _, objSpec := range gc.ObjSpecs {
+		resp, err := gc.Client.Steve.SteveType(gc.ObjType).Create(objSpec)
+		if err != nil {
+			return err
+		}
+		gc.objs = append(gc.objs, *resp)
+	}
+	return nil
+}
+
+func (gc *GenericCreate) Revert(poll bool, poll_interval int, async bool) (err error) {
+	for _, obj := range gc.objs {
+		err := gc.Client.Steve.SteveType(gc.ObjType).Delete(&obj)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
--- tests/v2/validation/provisioning/rke2/template_test.go
+++ tests/v2/validation/provisioning/rke2/template_test.go
@@ -1,18 +1,14 @@
-//go:build (validation || extended) && !infra.any && !infra.aks && !infra.eks && !infra.gke && !infra.rke2k3s && !cluster.any && !cluster.custom && !cluster.nodedriver && !sanity && !stress
-
 package rke2
 
 import (
 	"testing"
+	"time"
 
-	components "github.com/components/tests/framework/extensions/components"
 	"github.com/rancher/machine/libmachine/log"
 	v1 "github.com/rancher/rancher/pkg/apis/catalog.cattle.io/v1"
 	"github.com/rancher/rancher/tests/framework/clients/rancher"
 	management "github.com/rancher/rancher/tests/framework/clients/rancher/generated/management/v3"
-	"github.com/rancher/rancher/tests/framework/extensions/clusters"
-	"github.com/rancher/rancher/tests/framework/extensions/clusters/kubernetesversions"
-	"github.com/rancher/rancher/tests/framework/extensions/provisioninginput"
+	"github.com/rancher/rancher/tests/framework/extensions/components/components"
 	"github.com/rancher/rancher/tests/framework/extensions/users"
 	password "github.com/rancher/rancher/tests/framework/extensions/users/passwordgenerator"
 	"github.com/rancher/rancher/tests/framework/pkg/config"
@@ -20,6 +16,11 @@ import (
 	"github.com/rancher/rancher/tests/framework/pkg/session"
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+const (
+	repoType = "catalog.cattle.io.clusterrepo"
 )
 
 type ClusterTemplateTestSuite struct {
@@ -27,7 +28,7 @@ type ClusterTemplateTestSuite struct {
 	client             *rancher.Client
 	session            *session.Session
 	standardUserClient *rancher.Client
-	repoConfig         *RepoSpec
+	repoSpec           *v1.RepoSpec
 }
 
 func (r *ClusterTemplateTestSuite) TearDownSuite() {
@@ -37,16 +38,12 @@ func (r *ClusterTemplateTestSuite) TearDownSuite() {
 func (r *ClusterTemplateTestSuite) SetupSuite() {
 	testSession := session.NewSession()
 	r.session = testSession
-	r.provisioningConfig = new(provisioninginput.Config)
-	config.LoadConfig("repo", r.repoConfig)
+	r.repoSpec = new(v1.RepoSpec)
+	config.LoadConfig("repospec", r.repoSpec)
 
 	client, err := rancher.NewClient("", testSession)
 	require.NoError(r.T(), err)
 	r.client = client
-
-	r.provisioningConfig.RKE2KubernetesVersions, err = kubernetesversions.Default(
-		r.client, clusters.RKE2ClusterType.String(), r.provisioningConfig.RKE2KubernetesVersions)
-	require.NoError(r.T(), err)
 
 	enabled := true
 	var testuser = namegen.AppendRandomString("testuser-")
@@ -71,17 +68,28 @@ func (r *ClusterTemplateTestSuite) SetupSuite() {
 
 func (r *ClusterTemplateTestSuite) TestProvisionClusterTemplate() {
 	repo := v1.ClusterRepo{
-		Spec: v1.RepoSpec{
-			GitRepo:               "https://github.com/susesgartner/cluster-template-examples.git",
-			GitBranch:             "main",
-			InsecureSkipTLSverify: true,
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "my-repo",
 		},
+		Spec: *r.repoSpec,
 	}
-	created, err := components.GenericCreate(&rancher.Client{}, repo, repotype)
+	//created, err := components.GenericCreate(r.client, repo, repoType)
+	var interfaceSlice []interface{} = make([]interface{}, 1)
+	interfaceSlice[0] = repo
+	mycomponent := components.GenericCreate{
+		ObjSpecs: interfaceSlice,
+		ObjType:  repoType,
+		Client:   r.client,
+	}
+	err := mycomponent.Apply(true, 30, false)
 	if err != nil {
 		log.Info(err)
 	}
-	log.Info(created)
+	time.Sleep(5 * time.Second)
+	err = mycomponent.Revert(true, 30, false)
+	if err != nil {
+		log.Info(err)
+	}
 	log.Info("Entering main")
 }
 
