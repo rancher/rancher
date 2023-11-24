cf960f0553e66eddf9749f258fc68c83bc43b3a3
Ethan Howell <ethan.c.howell@hotmail.com>
2023-11-23 21:23:57 -0500

    Rework client build for easier testing. Add unit tests.

--- pkg/auth/providers/oidc/oidc_client.go
+++ pkg/auth/providers/oidc/oidc_client.go
@@ -4,50 +4,51 @@ import (
 	"context"
 	"crypto/tls"
 	"crypto/x509"
+	"fmt"
 	"net/http"
 
 	"github.com/coreos/go-oidc/v3/oidc"
 )
 
-func AddCertKeyToContext(ctx context.Context, certificate, key string) (context.Context, error) {
+func getClientCertificates(certificate, key string) ([]tls.Certificate, error) {
+	cert, err := tls.X509KeyPair([]byte(certificate), []byte(key))
+	if err != nil {
+		return nil, fmt.Errorf("could not parse cert/key pair: %w", err)
+	}
+
+	return []tls.Certificate{cert}, nil
+}
+
+func getHTTPClient(certificate, key string) (*http.Client, error) {
+	transport := http.DefaultTransport.(*http.Transport).Clone()
+	transport.TLSClientConfig = &tls.Config{
+		MinVersion: tls.VersionTLS12,
+	}
+
+	pool, err := x509.SystemCertPool()
+	if err != nil {
+		return nil, err
+	}
+
+	transport.TLSClientConfig.RootCAs = pool
 	if certificate != "" && key != "" {
-		certKeyClient := &http.Client{}
-		err := GetClientWithCertKey(certKeyClient, certificate, key)
+		certs, err := getClientCertificates(certificate, key)
 		if err != nil {
 			return nil, err
 		}
-		return oidc.ClientContext(ctx, certKeyClient), nil
+		transport.TLSClientConfig.Certificates = certs
 	}
 
-	return ctx, nil
-}
-
-func GetClientWithCertKey(httpClient *http.Client, certificate, key string) error {
-	tlsConfig, err := getTLSClientConfig(certificate, key)
-	if err != nil {
-		return err
-	}
-
-	// Use the default transport to pick up on things like http.ProxyFromEnvironment
-	transport := http.DefaultTransport.(*http.Transport).Clone()
-	transport.TLSClientConfig = tlsConfig
-	httpClient.Transport = transport
-	return nil
-}
-
-func getTLSClientConfig(certificate, key string) (*tls.Config, error) {
-	keyPair, err := tls.X509KeyPair([]byte(certificate), []byte(key))
-	if err != nil {
-		return nil, err
-	}
-
-	caCertPool, err := x509.SystemCertPool()
-	if err != nil {
-		return nil, err
-	}
-
-	return &tls.Config{
-		RootCAs:      caCertPool,
-		Certificates: []tls.Certificate{keyPair},
+	return &http.Client{
+		Transport: transport,
 	}, nil
+}
+
+func AddCertKeyToContext(ctx context.Context, certificate, key string) (context.Context, error) {
+	client, err := getHTTPClient(certificate, key)
+	if err != nil {
+		return nil, err
+	}
+
+	return oidc.ClientContext(ctx, client), nil
 }
