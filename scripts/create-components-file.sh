7b857cb218f55df186fec7b2747744ac686f6da5
Johnatas <johnatasr@hotmail.com>
2023-11-28 21:50:19 -0300

    adjust create components files dev-v2.7
    Signed-off-by: Johnatas <johnatasr@hotmail.com>

--- scripts/create-components-file.sh
+++ scripts/create-components-file.sh
@@ -10,19 +10,47 @@ mkdir -p bin
 
 COMPONENTSFILE=./bin/rancher-components.txt
 
-echo "# Images with -rc" > $COMPONENTSFILE
+FILES=(
+    "./Dockerfile.dapper"
+    "./go.mod"
+    "./package/Dockerfile"
+    "./pkg/apis/go.mod"
+    "./pkg/client/go.mod"
+    "./pkg/settings/setting.go"
+    "./scripts/package-env"
+)
 
-printf '%s\n' "$(grep -h "\-rc" ./bin/rancher-images.txt ./bin/rancher-windows-images.txt | awk -F: '{ print $1,$2 }')" | sort -u >> $COMPONENTSFILE
+IMAGE_FILES=(
+    "./bin/rancher-images.txt"
+    "./bin/rancher-windows-images.txt"
+)
 
-echo "# Components with -rc" >> $COMPONENTSFILE
+generate_section() {
+    local pattern="$1"
+    local label="$2"
+    local type="$3"
+    echo "" >> "$COMPONENTSFILE"
+    echo "# $label" >> "$COMPONENTSFILE"
 
-printf '%s\n' "$(grep "_VERSION" ./package/Dockerfile | grep ENV | egrep -v "http|\\$|MIN_VERSION" | grep CATTLE |sed 's/CATTLE_//g' | sed 's/=/ /g' |  awk '{ print $2,$3 }' | sort | grep "\-rc")" >> $COMPONENTSFILE
+    for file in "${FILES[@]}"; do
+        if [[ "$file" == "./scripts/package-env" ]]; then
+            grep -h -n -E "$pattern" "$file" | grep -i -E "$type" | sed -E "s|^([^:]*):(.*)|\* \2 (file $file, line \1)|"
+        else
+            grep -h -n -E "$pattern" "$file" | grep -i -E "$type" | grep -v "// indirect" | awk -F':' -v file="$file" -v label="$label" '{ sub(/^[ \t]+/, "", $2); sub(/[ \t]+/, " ", $2); print "* " $2 " (file " file ", line " $1 ")" }'
+        fi
+    done | sort -u >> "$COMPONENTSFILE"
+}
 
-printf '%s\n' "$(grep "rancher/" ./go.mod | egrep -v "\./"  | egrep -v "\/pkg\/apis|\/pkg\/client|^module" | grep -v "=>" | awk -F'/' '{ print $NF }' | awk '$1 = toupper($1)' | sort | grep "\-rc")" >> $COMPONENTSFILE
+echo "# Images with -rc" > "$COMPONENTSFILE"
+for file in "${IMAGE_FILES[@]}"; do
+    grep -h -n -E "rc[.]?[0-9]+" "$file" | awk -F':' -v file="$file" '{ sub(/^[ \t]+/, "", $2); sub(/[ \t]+/, " ", $2); print "* " $2 " (file " file ", line " $1 ")" }'
+done | sort -u >> "$COMPONENTSFILE"
 
+generate_section "rc[.]?[0-9]+" "Components with -rc"
+
+echo "" >> $COMPONENTSFILE
 echo "# Min version components with -rc" >> $COMPONENTSFILE
-
-printf '%s\n' "$(grep "_MIN_VERSION" ./package/Dockerfile | grep ENV | grep CATTLE |sed 's/CATTLE_//g' | sed 's/=/ /g' |  awk '{ print $2,$3 }' | sort | grep "\-rc")" >> $COMPONENTSFILE
+printf '%s\n' "$(grep -n -E "_MIN_VERSION" ./package/Dockerfile | grep ENV | grep CATTLE |sed 's/CATTLE_//g' | sed 's/=/ /g' |  awk -F':' '{ sub(/^[ \t]+/, "", $2); print "* " $2 " (file /package/Dockerfile, line " $1 ")" }' | sort | grep "\-rc")" >> $COMPONENTSFILE
 
 K8SVERSIONSFILE=./bin/rancher-rke-k8s-versions.txt
 
@@ -31,8 +59,7 @@ if [[ -f "$K8SVERSIONSFILE" ]]; then
     cat $K8SVERSIONSFILE >> $COMPONENTSFILE
 fi
 
-echo "# Chart/KDM sources" >> $COMPONENTSFILE
-
-bash ./scripts/check-chart-kdm-source-values >> $COMPONENTSFILE
+generate_section "dev-v2.[0-9]+" "KDM References with dev branch" "kdm"
+generate_section "dev-v2.[0-9]+" "Chart References with dev branch" "chart"
 
 echo "Done creating ./bin/rancher-components.txt"
