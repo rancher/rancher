aa24005535dda1e826036af037f861895d9bfa3d
Chris Kim <30601846+Oats87@users.noreply.github.com>
2023-12-05 15:15:53 -0800

    Don't render providerID field into machine provision arguments, and don't back-populate it until the inframachine is ready (#43512)
    Signed-off-by: Chris Kim <oats87g@gmail.com>

--- pkg/controllers/capr/machineprovision/args.go
+++ pkg/controllers/capr/machineprovision/args.go
@@ -255,6 +255,11 @@ func toArgs(driverName string, args map[string]interface{}, clusterID string) (c
 	}
 
 	for k, v := range args {
+		// Don't render providerID into the arguments, as this is only a cluster-api required field.
+		if k == "providerID" {
+			continue
+		}
+
 		dmField := "--" + driverName + "-" + strings.ToLower(regExHyphen.ReplaceAllString(k, "${1}-${2}"))
 		if v == nil {
 			continue
