8a11f8681ab3c190d381b5bc8d2a4f29e52ae889
Priyashetty17 <priyanka.shetty@suse.com>
2023-12-12 19:07:29 -0500

    Resolved merge conflict with defaults

--- tests/framework/extensions/kubeapi/rbac/rbac.go
+++ tests/framework/extensions/kubeapi/rbac/rbac.go
@@ -5,34 +5,64 @@ import (
 	"k8s.io/apimachinery/pkg/runtime/schema"
 )
 
-// RoleGroupVersionResource is the required Group Version Resource for accessing roles in a cluster,
-// using the dynamic client.
+const (
+	GroupName    = "management.cattle.io"
+	Version      = "v3"
+	localcluster = "local"
+)
+
+// RoleGroupVersionResource is the required Group Version Resource for accessing roles in a cluster, using the dynamic client.
 var RoleGroupVersionResource = schema.GroupVersionResource{
 	Group:    rbacv1.SchemeGroupVersion.Group,
 	Version:  rbacv1.SchemeGroupVersion.Version,
 	Resource: "roles",
 }
 
-// ClusterRoleGroupVersionResource is the required Group Version Resource for accessing clusterroles in a cluster,
-// using the dynamic client.
+// ClusterRoleGroupVersionResource is the required Group Version Resource for accessing clusterroles in a cluster, using the dynamic client.
 var ClusterRoleGroupVersionResource = schema.GroupVersionResource{
 	Group:    rbacv1.SchemeGroupVersion.Group,
 	Version:  rbacv1.SchemeGroupVersion.Version,
 	Resource: "clusterroles",
 }
 
-// RoleBindingGroupVersionResource is the required Group Version Resource for accessing rolebindings in a cluster,
-// using the dynamic client.
+// RoleBindingGroupVersionResource is the required Group Version Resource for accessing rolebindings in a cluster, using the dynamic client.
 var RoleBindingGroupVersionResource = schema.GroupVersionResource{
 	Group:    rbacv1.SchemeGroupVersion.Group,
 	Version:  rbacv1.SchemeGroupVersion.Version,
 	Resource: "rolebindings",
 }
 
-// ClusterRoleBindingGroupVersionResource is the required Group Version Resource for accessing clusterrolebindings in a cluster,
-// using the dynamic client.
+// ClusterRoleBindingGroupVersionResource is the required Group Version Resource for accessing clusterrolebindings in a cluster, using the dynamic client.
 var ClusterRoleBindingGroupVersionResource = schema.GroupVersionResource{
 	Group:    rbacv1.SchemeGroupVersion.Group,
 	Version:  rbacv1.SchemeGroupVersion.Version,
 	Resource: "clusterrolebindings",
+}
+
+// GlobalRoleGroupVersionResource is the required Group Version Resource for accessing global roles in a rancher server, using the dynamic client.
+var GlobalRoleGroupVersionResource = schema.GroupVersionResource{
+	Group:    GroupName,
+	Version:  Version,
+	Resource: "globalroles",
+}
+
+// GlobalRoleBindingGroupVersionResource is the required Group Version Resource for accessing clusterrolebindings in a cluster, using the dynamic client.
+var GlobalRoleBindingGroupVersionResource = schema.GroupVersionResource{
+	Group:    GroupName,
+	Version:  Version,
+	Resource: "globalrolebindings",
+}
+
+// ClusterRoleTemplateBindingGroupVersionResource is the required Group Version Resource for accessing clusterrolebindings in a cluster, using the dynamic client.
+var ClusterRoleTemplateBindingGroupVersionResource = schema.GroupVersionResource{
+	Group:    GroupName,
+	Version:  Version,
+	Resource: "clusterroletemplatebindings",
+}
+
+// RoleTemplateGroupVersionResource is the required Group Version Resource for accessing roletemplates in a cluster, using the dynamic client.
+var RoleTemplateGroupVersionResource = schema.GroupVersionResource{
+	Group:    GroupName,
+	Version:  Version,
+	Resource: "roletemplates",
 }
