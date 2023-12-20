2fffe420b5bc4b5a49f203d8f04b7c0955ece3e7
Yevhen Oliinyk <yevhene@realplusonline.com>
2023-12-20 21:43:55 +0200

    Add AmazonEBSCSIDriverPolicy to AWS NodeInstanceRole.

--- pkg/kontainer-engine/drivers/eks/templates.go
+++ pkg/kontainer-engine/drivers/eks/templates.go
@@ -541,6 +541,7 @@ Resources:
         - arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy
         - arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy
         - arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly
+        - arn:aws:iam::aws:policy/AmazonEBSCSIDriverPolicy
 
   NodeSecurityGroup:
     Type: AWS::EC2::SecurityGroup
