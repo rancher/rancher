027b68f5e019fce3f62b2196ad9d594754b24947
Richard Case <richard.case@suse.com>
2024-01-03 09:59:01 +0000

    feat: added capi label to kubeconfig secrets
    The newest version of CAPI requires that secrets that contain the kubeconfigs for  clusters
    must have the cluster name label. Starting with CAPI 1.5.0 if the secrets don't have this
    then the CAPI cluster reconciliation loop will not "find" the secret. This change adds the
    required label for new kubeconfig secrets created.
    
    Additionally a new migration has been added to add this label to kubeconfig secrets for
    RKE2 clusters that have already been created.
    
    Signed-off-by: Richard Case <richard.case@suse.com>
    
    wip: moved the migration
    
    Signed-off-by: Richard Case <richard.case@suse.com>

--- pkg/rancher/migrations.go
+++ pkg/rancher/migrations.go
@@ -71,6 +71,9 @@ func runMigrations(wranglerContext *wrangler.Context) error {
 		if err := migrateMachinePoolsDynamicSchemaLabel(wranglerContext); err != nil {
 			return err
 		}
+		if err := migrateCAPIKubeconfigs(wranglerContext); err != nil {
+			return fmt.Errorf("running capi kubeconfig migration: %w", err)
+		}
 	}
 
 	return nil
@@ -539,4 +542,70 @@ func insertOrUpdateCondition(d data.Object, desiredCondition summary.Condition) 
 	d.SetNested(conditions, "status", "conditions")
 
 	return true, nil
+}
+
+func migrateCAPIKubeconfigs(w *wrangler.Context) error {
+	logrus.Info("Running CAPI secret migration")
+
+	mgmtClusters, err := w.Mgmt.Cluster().List(metav1.ListOptions{})
+	if err != nil {
+		return err
+	}
+
+	for _, mgmtCluster := range mgmtClusters.Items {
+		logrus.Tracef("Checking if migration for cluster %s is needed", mgmtCluster.Name)
+
+		provClusters, err := w.Provisioning.Cluster().List(mgmtCluster.Spec.FleetWorkspaceName, metav1.ListOptions{})
+		if k8serror.IsNotFound(err) || len(provClusters.Items) == 0 {
+			continue
+		} else if err != nil {
+			return err
+		}
+		for _, provCluster := range provClusters.Items {
+			if provCluster.Spec.RKEConfig == nil {
+				continue
+			}
+			logrus.Tracef("Running migration for cluster %s", provCluster.Name)
+
+			if err = retry.RetryOnConflict(retry.DefaultRetry, func() error {
+				secretName := fmt.Sprintf("%s-kubeconfig", provCluster.Name)
+
+				secret, err := w.Core.Secret().Get(provCluster.Namespace, secretName, metav1.GetOptions{})
+				if err != nil {
+					if k8serror.IsNotFound(err) {
+						return nil
+					}
+
+					return fmt.Errorf("getting secret %s/%s: %w", provCluster.Namespace, secretName, err)
+				}
+
+				_, ok := secret.Labels[capi.ClusterNameLabel]
+				if ok {
+					logrus.Tracef("kubeconfig secret %s/%s already has the capi cluster label", provCluster.Namespace, secret.Name)
+					return nil
+				}
+
+				secretCopy := secret.DeepCopy()
+				if secretCopy.Labels == nil {
+					secretCopy.Labels = map[string]string{}
+				}
+				secretCopy.Labels[capi.ClusterNameLabel] = provCluster.Name
+
+				if _, updateErr := w.Core.Secret().Update(secretCopy); updateErr != nil {
+					return fmt.Errorf("updating secret %s/%s to add capi label: %w", provCluster.Namespace, secret.Name, err)
+				}
+
+				logrus.Debugf("Updated kubeconfig secret %s/%s with CAPI cluster label", provCluster.Namespace, secret.Name)
+
+				return nil
+
+			}); err != nil {
+				return err
+			}
+		}
+	}
+
+	logrus.Info("Finished migrating CAPI kubeconfig secrets")
+
+	return nil
 }
