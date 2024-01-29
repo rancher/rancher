15deca526fa6d02f6da50e4c7611a3ffaecf5b80
Chirayu Kapoor <chirayu.kapoor@suse.com>
2024-01-30 00:50:30 +0530

    go generate
    Signed-off-by: Chirayu Kapoor <chirayu.kapoor@suse.com>

--- pkg/client/generated/project/v3/zz_generated_job_spec.go
+++ pkg/client/generated/project/v3/zz_generated_job_spec.go
@@ -4,6 +4,7 @@ const (
 	JobSpecType                               = "jobSpec"
 	JobSpecFieldActiveDeadlineSeconds         = "activeDeadlineSeconds"
 	JobSpecFieldAutomountServiceAccountToken  = "automountServiceAccountToken"
+	JobSpecFieldBackoffLimitPerIndex          = "backoffLimitPerIndex"
 	JobSpecFieldCompletionMode                = "completionMode"
 	JobSpecFieldContainers                    = "containers"
 	JobSpecFieldDNSConfig                     = "dnsConfig"
@@ -21,11 +22,13 @@ const (
 	JobSpecFieldHostname                      = "hostname"
 	JobSpecFieldImagePullSecrets              = "imagePullSecrets"
 	JobSpecFieldJobConfig                     = "jobConfig"
+	JobSpecFieldMaxFailedIndexes              = "maxFailedIndexes"
 	JobSpecFieldNodeID                        = "nodeId"
 	JobSpecFieldOS                            = "os"
 	JobSpecFieldObjectMeta                    = "metadata"
 	JobSpecFieldOverhead                      = "overhead"
 	JobSpecFieldPodFailurePolicy              = "podFailurePolicy"
+	JobSpecFieldPodReplacementPolicy          = "podReplacementPolicy"
 	JobSpecFieldPreemptionPolicy              = "preemptionPolicy"
 	JobSpecFieldReadinessGates                = "readinessGates"
 	JobSpecFieldResourceClaims                = "resourceClaims"
@@ -54,6 +57,7 @@ const (
 type JobSpec struct {
 	ActiveDeadlineSeconds         *int64                         `json:"activeDeadlineSeconds,omitempty" yaml:"activeDeadlineSeconds,omitempty"`
 	AutomountServiceAccountToken  *bool                          `json:"automountServiceAccountToken,omitempty" yaml:"automountServiceAccountToken,omitempty"`
+	BackoffLimitPerIndex          *int64                         `json:"backoffLimitPerIndex,omitempty" yaml:"backoffLimitPerIndex,omitempty"`
 	CompletionMode                string                         `json:"completionMode,omitempty" yaml:"completionMode,omitempty"`
 	Containers                    []Container                    `json:"containers,omitempty" yaml:"containers,omitempty"`
 	DNSConfig                     *PodDNSConfig                  `json:"dnsConfig,omitempty" yaml:"dnsConfig,omitempty"`
@@ -71,11 +75,13 @@ type JobSpec struct {
 	Hostname                      string                         `json:"hostname,omitempty" yaml:"hostname,omitempty"`
 	ImagePullSecrets              []LocalObjectReference         `json:"imagePullSecrets,omitempty" yaml:"imagePullSecrets,omitempty"`
 	JobConfig                     *JobConfig                     `json:"jobConfig,omitempty" yaml:"jobConfig,omitempty"`
+	MaxFailedIndexes              *int64                         `json:"maxFailedIndexes,omitempty" yaml:"maxFailedIndexes,omitempty"`
 	NodeID                        string                         `json:"nodeId,omitempty" yaml:"nodeId,omitempty"`
 	OS                            *PodOS                         `json:"os,omitempty" yaml:"os,omitempty"`
 	ObjectMeta                    *ObjectMeta                    `json:"metadata,omitempty" yaml:"metadata,omitempty"`
 	Overhead                      map[string]string              `json:"overhead,omitempty" yaml:"overhead,omitempty"`
 	PodFailurePolicy              *PodFailurePolicy              `json:"podFailurePolicy,omitempty" yaml:"podFailurePolicy,omitempty"`
+	PodReplacementPolicy          string                         `json:"podReplacementPolicy,omitempty" yaml:"podReplacementPolicy,omitempty"`
 	PreemptionPolicy              string                         `json:"preemptionPolicy,omitempty" yaml:"preemptionPolicy,omitempty"`
 	ReadinessGates                []PodReadinessGate             `json:"readinessGates,omitempty" yaml:"readinessGates,omitempty"`
 	ResourceClaims                []PodResourceClaim             `json:"resourceClaims,omitempty" yaml:"resourceClaims,omitempty"`
