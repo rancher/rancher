d2531a35eabd2c9f61d928d2f8fb82c67b8b320c
Peter Matseykanets <peter.matseykanets@suse.com>
2024-02-06 10:51:30 -0500

    Regenerate files after updating RKE to 1.3.24

--- tests/framework/clients/rancher/generated/management/v3/zz_generated_cloud_provider.go
+++ tests/framework/clients/rancher/generated/management/v3/zz_generated_cloud_provider.go
@@ -1,22 +1,24 @@
 package client
 
 const (
-	CloudProviderType                        = "cloudProvider"
-	CloudProviderFieldAWSCloudProvider       = "awsCloudProvider"
-	CloudProviderFieldAzureCloudProvider     = "azureCloudProvider"
-	CloudProviderFieldCustomCloudProvider    = "customCloudProvider"
-	CloudProviderFieldHarvesterCloudProvider = "harvesterCloudProvider"
-	CloudProviderFieldName                   = "name"
-	CloudProviderFieldOpenstackCloudProvider = "openstackCloudProvider"
-	CloudProviderFieldVsphereCloudProvider   = "vsphereCloudProvider"
+	CloudProviderType                             = "cloudProvider"
+	CloudProviderFieldAWSCloudProvider            = "awsCloudProvider"
+	CloudProviderFieldAzureCloudProvider          = "azureCloudProvider"
+	CloudProviderFieldCustomCloudProvider         = "customCloudProvider"
+	CloudProviderFieldHarvesterCloudProvider      = "harvesterCloudProvider"
+	CloudProviderFieldName                        = "name"
+	CloudProviderFieldOpenstackCloudProvider      = "openstackCloudProvider"
+	CloudProviderFieldUseInstanceMetadataHostname = "useInstanceMetadataHostname"
+	CloudProviderFieldVsphereCloudProvider        = "vsphereCloudProvider"
 )
 
 type CloudProvider struct {
-	AWSCloudProvider       *AWSCloudProvider       `json:"awsCloudProvider,omitempty" yaml:"awsCloudProvider,omitempty"`
-	AzureCloudProvider     *AzureCloudProvider     `json:"azureCloudProvider,omitempty" yaml:"azureCloudProvider,omitempty"`
-	CustomCloudProvider    string                  `json:"customCloudProvider,omitempty" yaml:"customCloudProvider,omitempty"`
-	HarvesterCloudProvider *HarvesterCloudProvider `json:"harvesterCloudProvider,omitempty" yaml:"harvesterCloudProvider,omitempty"`
-	Name                   string                  `json:"name,omitempty" yaml:"name,omitempty"`
-	OpenstackCloudProvider *OpenstackCloudProvider `json:"openstackCloudProvider,omitempty" yaml:"openstackCloudProvider,omitempty"`
-	VsphereCloudProvider   *VsphereCloudProvider   `json:"vsphereCloudProvider,omitempty" yaml:"vsphereCloudProvider,omitempty"`
+	AWSCloudProvider            *AWSCloudProvider       `json:"awsCloudProvider,omitempty" yaml:"awsCloudProvider,omitempty"`
+	AzureCloudProvider          *AzureCloudProvider     `json:"azureCloudProvider,omitempty" yaml:"azureCloudProvider,omitempty"`
+	CustomCloudProvider         string                  `json:"customCloudProvider,omitempty" yaml:"customCloudProvider,omitempty"`
+	HarvesterCloudProvider      *HarvesterCloudProvider `json:"harvesterCloudProvider,omitempty" yaml:"harvesterCloudProvider,omitempty"`
+	Name                        string                  `json:"name,omitempty" yaml:"name,omitempty"`
+	OpenstackCloudProvider      *OpenstackCloudProvider `json:"openstackCloudProvider,omitempty" yaml:"openstackCloudProvider,omitempty"`
+	UseInstanceMetadataHostname *bool                   `json:"useInstanceMetadataHostname,omitempty" yaml:"useInstanceMetadataHostname,omitempty"`
+	VsphereCloudProvider        *VsphereCloudProvider   `json:"vsphereCloudProvider,omitempty" yaml:"vsphereCloudProvider,omitempty"`
 }
