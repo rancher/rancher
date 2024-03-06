35d6b4eedc3b0274c8c73b830d95bec30a1f2cbc
Tom Lebreux <tom.lebreux@suse.com>
2024-03-06 16:53:25 -0500

    Bump rancher-webhook to v0.4.3-rc1 (#44702)
    * Remove pinned version of webhook in test Dockerfile
    
    * Bump rancher-webhook to v0.4.3-rc1

--- pkg/buildconfig/constants.go
+++ pkg/buildconfig/constants.go
@@ -6,5 +6,5 @@ const (
 	CspAdapterMinVersion = "103.0.1+up3.0.1-rc2"
 	DefaultShellVersion  = "rancher/shell:v0.1.23-rc3"
 	FleetVersion         = "103.1.1+up0.9.1-rc.5"
-	WebhookVersion       = "103.0.1+up0.4.2"
+	WebhookVersion       = "103.0.2+up0.4.3-rc1"
 )
