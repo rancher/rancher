61d61b87537663dcd61a504474dd494c1d46fc16
Krunal Hingu <krunal.hingu222@gmail.com>
2024-03-15 05:27:49 +0530

    removed podsecuritypod code

--- pkg/client/generated/project/v3/zz_generated_load_balancer_ingress.go
+++ pkg/client/generated/project/v3/zz_generated_load_balancer_ingress.go
@@ -4,11 +4,13 @@ const (
 	LoadBalancerIngressType          = "loadBalancerIngress"
 	LoadBalancerIngressFieldHostname = "hostname"
 	LoadBalancerIngressFieldIP       = "ip"
+	LoadBalancerIngressFieldIPMode   = "ipMode"
 	LoadBalancerIngressFieldPorts    = "ports"
 )
 
 type LoadBalancerIngress struct {
 	Hostname string       `json:"hostname,omitempty" yaml:"hostname,omitempty"`
 	IP       string       `json:"ip,omitempty" yaml:"ip,omitempty"`
+	IPMode   string       `json:"ipMode,omitempty" yaml:"ipMode,omitempty"`
 	Ports    []PortStatus `json:"ports,omitempty" yaml:"ports,omitempty"`
 }
