d0d4b45317efa3e63ba24b57dada651811270f69
Peter Matseykanets <peter.matseykanets@suse.com>
2024-04-02 15:46:53 -0400

    Flatten conditionals

--- pkg/controllers/managementuser/clusterauthtoken/cluster_user_attribute.go
+++ pkg/controllers/managementuser/clusterauthtoken/cluster_user_attribute.go
@@ -26,24 +26,26 @@ func (h *clusterUserAttributeHandler) Sync(key string, clusterUserAttribute *clu
 	userAttribute, err := h.userAttributeLister.Get("", clusterUserAttribute.Name)
 	if err != nil {
 		// If the corresponding UserAttribute no longer exists, we need to clean up the ClusterUserAttribute.
-		if apierrors.IsNotFound(err) {
-			// There is a chance that it hasn't ended up in the cache yet so we try to get it afresh.
-			userAttribute, err = h.userAttribute.Get(clusterUserAttribute.Name, metav1.GetOptions{})
-			if err != nil {
-				if apierrors.IsNotFound(err) {
-					err = h.clusterUserAttribute.Delete(clusterUserAttribute.Name, &metav1.DeleteOptions{})
-					if err != nil && !apierrors.IsNotFound(err) && !apierrors.IsGone(err) {
-						return nil, fmt.Errorf("error deleting orphaned clusteruserattribute %s: %w", clusterUserAttribute.Name, err)
-					}
-					logrus.Infof("Deleted orphaned clusteruserattribute %s", clusterUserAttribute.Name)
-					return nil, nil
-				}
-				return nil, err
-			}
-			// The userAttribute exists, proceed.
-		} else {
+		if !apierrors.IsNotFound(err) {
 			return nil, err
 		}
+
+		// There is a chance that it hasn't ended up in the cache yet so we try to get it afresh.
+		userAttribute, err = h.userAttribute.Get(clusterUserAttribute.Name, metav1.GetOptions{})
+		if err != nil {
+			if !apierrors.IsNotFound(err) {
+				return nil, err
+			}
+
+			err = h.clusterUserAttribute.Delete(clusterUserAttribute.Name, &metav1.DeleteOptions{})
+			if err != nil && !apierrors.IsNotFound(err) && !apierrors.IsGone(err) {
+				return nil, fmt.Errorf("error deleting orphaned clusteruserattribute %s: %w", clusterUserAttribute.Name, err)
+			}
+
+			logrus.Infof("Deleted orphaned clusteruserattribute %s", clusterUserAttribute.Name)
+			return nil, nil
+		}
+		// The userAttribute exists, proceed.
 	}
 
 	if !clusterUserAttribute.NeedsRefresh {
