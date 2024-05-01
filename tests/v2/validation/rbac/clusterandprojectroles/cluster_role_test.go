MikeASnyder/demoMCM

Refacotring RBAC tests

tests/v2/validation/rbac/userclusterandprojectroles/cluster_role_test.go
@@ -1,0 +1,140 @@
+//go:build (validation || infra.any || cluster.any || extended) && !sanity && !stress
+
+package roles
+
+import (
+	"testing"
+
+	"github.com/rancher/shepherd/clients/rancher"
+	management "github.com/rancher/shepherd/clients/rancher/generated/management/v3"
+	"github.com/rancher/shepherd/extensions/clusters"
+	"github.com/rancher/shepherd/extensions/projects"
+	"github.com/rancher/shepherd/extensions/rbac"
+	"github.com/rancher/shepherd/extensions/users"
+	"github.com/rancher/shepherd/pkg/session"
+	log "github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+)
+
+type RBACAdditionalTestSuite struct {
+	suite.Suite
+	client  *rancher.Client
+	session *session.Session
+	cluster *management.Cluster
+}
+
+func (rb *RBACAdditionalTestSuite) TearDownSuite() {
+	rb.session.Cleanup()
+}
+
+func (rb *RBACAdditionalTestSuite) SetupSuite() {
+	testSession := session.NewSession()
+	rb.session = testSession
+
+	client, err := rancher.NewClient("", testSession)
+	require.NoError(rb.T(), err)
+
+	rb.client = client
+
+	log.Info("Getting cluster name from the config file and append cluster details in rb")
+	clusterName := client.RancherConfig.ClusterName
+	require.NotEmptyf(rb.T(), clusterName, "Cluster name to install should be set")
+	clusterID, err := clusters.GetClusterIDByName(rb.client, clusterName)
+	require.NoError(rb.T(), err, "Error getting cluster ID")
+	rb.cluster, err = rb.client.Management.Cluster.ByID(clusterID)
+	require.NoError(rb.T(), err)
+}
+
+func (rb *RBACAdditionalTestSuite) TestClusterOwnerAddsUserAsProjectOwner() {
+	subSession := rb.session.NewSession()
+	defer subSession.Cleanup()
+
+	clusterAdmin, standardUserClient, err := rbac.SetupUser(rb.client, rbac.StandardUser.String())
+	require.NoError(rb.T(), err)
+
+	additionalUser, additionalUserClient, err := rbac.SetupUser(rb.client, rbac.StandardUser.String())
+	require.NoError(rb.T(), err)
+
+	rb.T().Logf("Adding user as " + rbac.ClusterOwner.String() + " to the downstream cluster.")
+	err = users.AddClusterRoleToUser(rb.client, rb.cluster, clusterAdmin, rbac.ClusterOwner.String(), nil)
+	require.NoError(rb.T(), err)
+	standardUserClient, err = standardUserClient.ReLogin()
+	require.NoError(rb.T(), err)
+
+	clusterOwnerProject, err := standardUserClient.Management.Project.Create(projects.NewProjectConfig(rb.cluster.ID))
+	require.NoError(rb.T(), err)
+	err = users.AddProjectMember(standardUserClient, clusterOwnerProject, additionalUser, rbac.ProjectOwner.String(), nil)
+	require.NoError(rb.T(), err)
+
+	projectListClusterAdditionalUser, err := projects.ListProjectNames(additionalUserClient, rb.cluster.ID)
+	require.NoError(rb.T(), err)
+	assert.Equal(rb.T(), 1, len(projectListClusterAdditionalUser))
+	assert.Equal(rb.T(), clusterOwnerProject.Name, projectListClusterAdditionalUser[0])
+
+	err = users.RemoveProjectMember(standardUserClient, additionalUser)
+	require.NoError(rb.T(), err)
+	projectListClusterAdditionalUser, err = projects.ListProjectNames(additionalUserClient, rb.cluster.ID)
+	require.Empty(rb.T(), projectListClusterAdditionalUser)
+}
+
+func (rb *RBACAdditionalTestSuite) TestClusterOwnerAddsUserAsClusterOwner() {
+	subSession := rb.session.NewSession()
+	defer subSession.Cleanup()
+
+	_, standardUserClient, err := rbac.SetupUser(rb.client, rbac.StandardUser.String())
+	require.NoError(rb.T(), err)
+
+	additionalUser, additionalUserClient, err := rbac.SetupUser(rb.client, rbac.StandardUser.String())
+	require.NoError(rb.T(), err)
+
+	errUserRole := users.AddClusterRoleToUser(standardUserClient, rb.cluster, additionalUser, rbac.ClusterOwner.String(), nil)
+	require.NoError(rb.T(), errUserRole)
+	additionalUserClient, err = additionalUserClient.ReLogin()
+	require.NoError(rb.T(), err)
+
+	clusterList, err := additionalUserClient.Steve.SteveType(clusters.ProvisioningSteveResourceType).ListAll(nil)
+	require.NoError(rb.T(), err)
+	assert.Equal(rb.T(), 1, len(clusterList.Data))
+
+	err = users.RemoveClusterRoleFromUser(standardUserClient, additionalUser)
+	require.NoError(rb.T(), err)
+	clusterList, err = additionalUserClient.Steve.SteveType(clusters.ProvisioningSteveResourceType).ListAll(nil)
+	require.Empty(rb.T(), clusterList.Data)
+}
+
+func (rb *RBACAdditionalTestSuite) TestClusterOwnerAddsClusterMemberAsProjectOwner() {
+	subSession := rb.session.NewSession()
+	defer subSession.Cleanup()
+
+	_, standardUserClient, err := rbac.SetupUser(rb.client, rbac.StandardUser.String())
+	require.NoError(rb.T(), err)
+
+	additionalUser, additionalUserClient, err := rbac.SetupUser(rb.client, rbac.StandardUser.String())
+	require.NoError(rb.T(), err)
+
+	errUserRole := users.AddClusterRoleToUser(standardUserClient, rb.cluster, additionalUser, rbac.ClusterMember.String(), nil)
+	require.NoError(rb.T(), errUserRole)
+	additionalUserClient, err = additionalUserClient.ReLogin()
+	require.NoError(rb.T(), err)
+
+	clusterList, err := additionalUserClient.Steve.SteveType(clusters.ProvisioningSteveResourceType).ListAll(nil)
+	require.NoError(rb.T(), err)
+	assert.Equal(rb.T(), 1, len(clusterList.Data))
+
+	clusterOwnerProject, err := standardUserClient.Management.Project.Create(projects.NewProjectConfig(rb.cluster.ID))
+	require.NoError(rb.T(), err)
+
+	err = users.AddProjectMember(standardUserClient, clusterOwnerProject, additionalUser, rbac.ProjectOwner.String(), nil)
+	require.NoError(rb.T(), err)
+	projectListProjectOwner, err := projects.ListProjectNames(additionalUserClient, rb.cluster.ID)
+	require.NoError(rb.T(), err)
+
+	assert.Equal(rb.T(), 1, len(projectListProjectOwner))
+	assert.Equal(rb.T(), clusterOwnerProject.Name, projectListProjectOwner[0])
+}
+
+func TestRBACAdditionalTestSuite(t *testing.T) {
+	suite.Run(t, new(RBACAdditionalTestSuite))
+}

tests/v2/validation/rbac/userclusterandprojectroles/project_role_test.go
@@ -1,0 +1,121 @@
+//go:build (validation || infra.any || cluster.any || extended) && !sanity && !stress
+
+package roles
+
+import (
+	"testing"
+
+	"github.com/rancher/shepherd/clients/rancher"
+	management "github.com/rancher/shepherd/clients/rancher/generated/management/v3"
+	"github.com/rancher/shepherd/extensions/clusters"
+	"github.com/rancher/shepherd/extensions/projects"
+	rbac "github.com/rancher/shepherd/extensions/rbac"
+	"github.com/rancher/shepherd/extensions/users"
+	"github.com/rancher/shepherd/pkg/session"
+	log "github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+)
+
+type ProjectRolesTestSuite struct {
+	suite.Suite
+	client  *rancher.Client
+	session *session.Session
+	cluster *management.Cluster
+}
+
+func (pr *ProjectRolesTestSuite) TearDownSuite() {
+	pr.session.Cleanup()
+}
+
+func (pr *ProjectRolesTestSuite) SetupSuite() {
+	testSession := session.NewSession()
+	pr.session = testSession
+
+	client, err := rancher.NewClient("", testSession)
+	require.NoError(pr.T(), err)
+
+	pr.client = client
+
+	log.Info("Getting cluster name from the config file and append cluster details in pr")
+	clusterName := client.RancherConfig.ClusterName
+	require.NotEmptyf(pr.T(), clusterName, "Cluster name to install should be set")
+	clusterID, err := clusters.GetClusterIDByName(pr.client, clusterName)
+	require.NoError(pr.T(), err, "Error getting cluster ID")
+	pr.cluster, err = pr.client.Management.Cluster.ByID(clusterID)
+	require.NoError(pr.T(), err)
+}
+
+func (pr *ProjectRolesTestSuite) testSetupUserAndProject() (*rancher.Client, *management.Project) {
+	pr.T().Log("Set up User with cluster Role for additional rbac test cases " + rbac.ClusterOwner)
+	newUser, standardUserClient, err := rbac.SetupUser(pr.client, rbac.StandardUser.String())
+	require.NoError(pr.T(), err)
+
+	createProjectAsAdmin, err := standardUserClient.Management.Project.Create(projects.NewProjectConfig(pr.cluster.ID))
+	require.NoError(pr.T(), err)
+
+	log.Info("Adding a standard user as project Owner in the admin project")
+	errUserRole := users.AddProjectMember(pr.client, createProjectAsAdmin, newUser, rbac.ProjectOwner.String(), nil)
+	require.NoError(pr.T(), errUserRole)
+	standardUserClient, err = standardUserClient.ReLogin()
+	require.NoError(pr.T(), err)
+	return standardUserClient, createProjectAsAdmin
+}
+
+func (pr *ProjectRolesTestSuite) TestProjectOwnerAddsAndRemovesOtherProjectOwners() {
+	subSession := pr.session.NewSession()
+	defer subSession.Cleanup()
+
+	standardUserClient, adminProject := pr.testSetupUserAndProject()
+	additionalUser, additionalUserClient, err := rbac.SetupUser(pr.client, rbac.StandardUser.String())
+	require.NoError(pr.T(), err)
+
+	errUserRole := users.AddProjectMember(standardUserClient, adminProject, additionalUser, rbac.ProjectOwner.String(), nil)
+	require.NoError(pr.T(), errUserRole)
+	additionalUserClient, err = additionalUserClient.ReLogin()
+	require.NoError(pr.T(), err)
+
+	userGetProject, err := projects.ListProjectNames(additionalUserClient, pr.cluster.ID)
+	require.NoError(pr.T(), err)
+	assert.Equal(pr.T(), 1, len(userGetProject))
+	assert.Equal(pr.T(), adminProject.Name, userGetProject[0])
+
+	errRemoveMember := users.RemoveProjectMember(standardUserClient, additionalUser)
+	require.NoError(pr.T(), errRemoveMember)
+
+	userProjectEmptyAfterRemoval, err := projects.ListProjectNames(additionalUserClient, pr.cluster.ID)
+	require.Empty(pr.T(), userProjectEmptyAfterRemoval)
+}
+
+func (pr *ProjectRolesTestSuite) TestManageProjectUserRoleCannotAddProjectOwner() {
+	subSession := pr.session.NewSession()
+	defer subSession.Cleanup()
+
+	standardUserClient, adminProject := pr.testSetupUserAndProject()
+	additionalUser, additionalUserClient, err := rbac.SetupUser(pr.client, rbac.StandardUser.String())
+	require.NoError(pr.T(), err)
+
+	errUserRole := users.AddProjectMember(standardUserClient, adminProject, additionalUser, rbac.CustomManageProjectMember.String(), nil)
+	require.NoError(pr.T(), errUserRole)
+	additionalUserClient, err = additionalUserClient.ReLogin()
+	require.NoError(pr.T(), err)
+
+	addNewUserAsPO, addNewUserAsPOClient, err := rbac.SetupUser(pr.client, rbac.StandardUser.String())
+	require.NoError(pr.T(), err)
+
+	errUserRole2 := users.AddProjectMember(additionalUserClient, adminProject, addNewUserAsPO, rbac.ProjectOwner.String(), nil)
+	require.Error(pr.T(), errUserRole2)
+	assert.Contains(pr.T(), err.Error(), "422 Unprocessable Entity")
+
+	addNewUserAsPOClient, err = addNewUserAsPOClient.ReLogin()
+	require.NoError(pr.T(), err)
+
+	userGetProject, err := projects.ListProjectNames(addNewUserAsPOClient, pr.cluster.ID)
+	require.NoError(pr.T(), err)
+	assert.Equal(pr.T(), 0, len(userGetProject))
+}
+
+func TestProjectRolesTestSuite(t *testing.T) {
+	suite.Run(t, new(ProjectRolesTestSuite))
+}

rancher/rancher

tests/v2/validation/rbac/clusterandprojectroles/cluster_role_test.go
@@ -1,0 +1,149 @@
+//go:build (validation || infra.any || cluster.any || extended) && !sanity && !stress
+
+package clusterandprojectroles
+
+import (
+	"testing"
+
+	"github.com/rancher/shepherd/clients/rancher"
+	management "github.com/rancher/shepherd/clients/rancher/generated/management/v3"
+	"github.com/rancher/shepherd/extensions/clusters"
+	"github.com/rancher/shepherd/extensions/projects"
+	"github.com/rancher/shepherd/extensions/rbac"
+	"github.com/rancher/shepherd/extensions/users"
+	"github.com/rancher/shepherd/pkg/session"
+	log "github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+)
+
+type RBACAdditionalTestSuite struct {
+	suite.Suite
+	client  *rancher.Client
+	session *session.Session
+	cluster *management.Cluster
+}
+
+func (rb *RBACAdditionalTestSuite) TearDownSuite() {
+	rb.session.Cleanup()
+}
+
+func (rb *RBACAdditionalTestSuite) SetupSuite() {
+	rb.session = session.NewSession()
+
+	client, err := rancher.NewClient("", rb.session)
+	require.NoError(rb.T(), err)
+
+	rb.client = client
+
+	log.Info("Getting cluster name from the config file and append cluster details in rb")
+	clusterName := client.RancherConfig.ClusterName
+	require.NotEmptyf(rb.T(), clusterName, "Cluster name to install should be set")
+	clusterID, err := clusters.GetClusterIDByName(rb.client, clusterName)
+	require.NoError(rb.T(), err, "Error getting cluster ID")
+	rb.cluster, err = rb.client.Management.Cluster.ByID(clusterID)
+	require.NoError(rb.T(), err)
+}
+
+func (rb *RBACAdditionalTestSuite) TestClusterOwnerAddsUserAsProjectOwner() {
+	subSession := rb.session.NewSession()
+	defer subSession.Cleanup()
+
+	clusterAdmin, clusterAdminClient, err := rbac.SetupUser(rb.client, rbac.StandardUser.String())
+	require.NoError(rb.T(), err)
+
+	additionalUser, additionalUserClient, err := rbac.SetupUser(rb.client, rbac.StandardUser.String())
+	require.NoError(rb.T(), err)
+
+	rb.T().Logf("Adding user as " + rbac.ClusterOwner.String() + " to the downstream cluster.")
+	err = users.AddClusterRoleToUser(rb.client, rb.cluster, clusterAdmin, rbac.ClusterOwner.String(), nil)
+	require.NoError(rb.T(), err)
+	clusterAdminClient, err = clusterAdminClient.ReLogin()
+	require.NoError(rb.T(), err)
+
+	clusterOwnerProject, err := clusterAdminClient.Management.Project.Create(projects.NewProjectConfig(rb.cluster.ID))
+	require.NoError(rb.T(), err)
+	err = users.AddProjectMember(clusterAdminClient, clusterOwnerProject, additionalUser, rbac.ProjectOwner.String(), nil)
+	require.NoError(rb.T(), err)
+
+	projectListClusterAdditionalUser, err := projects.ListProjectNames(additionalUserClient, rb.cluster.ID)
+	require.NoError(rb.T(), err)
+	assert.Equal(rb.T(), 1, len(projectListClusterAdditionalUser))
+	assert.Equal(rb.T(), clusterOwnerProject.Name, projectListClusterAdditionalUser[0])
+
+	err = users.RemoveProjectMember(clusterAdminClient, additionalUser)
+	require.NoError(rb.T(), err)
+	projectListClusterAdditionalUser, err = projects.ListProjectNames(additionalUserClient, rb.cluster.ID)
+	require.Empty(rb.T(), projectListClusterAdditionalUser)
+}
+
+func (rb *RBACAdditionalTestSuite) TestClusterOwnerAddsUserAsClusterOwner() {
+	subSession := rb.session.NewSession()
+	defer subSession.Cleanup()
+
+	clusterAdmin, clusterAdminClient, err := rbac.SetupUser(rb.client, rbac.StandardUser.String())
+	require.NoError(rb.T(), err)
+
+	errUserRole := users.AddClusterRoleToUser(rb.client, rb.cluster, clusterAdmin, rbac.ClusterOwner.String(), nil)
+	require.NoError(rb.T(), errUserRole)
+	clusterAdminClient, err = clusterAdminClient.ReLogin()
+	require.NoError(rb.T(), err)
+
+	additionalUser, additionalUserClient, err := rbac.SetupUser(rb.client, rbac.StandardUser.String())
+	require.NoError(rb.T(), err)
+
+	errUserRole2 := users.AddClusterRoleToUser(clusterAdminClient, rb.cluster, additionalUser, rbac.ClusterOwner.String(), nil)
+	require.NoError(rb.T(), errUserRole2)
+	additionalUserClient, err = additionalUserClient.ReLogin()
+	require.NoError(rb.T(), err)
+
+	clusterList, err := additionalUserClient.Steve.SteveType(clusters.ProvisioningSteveResourceType).ListAll(nil)
+	require.NoError(rb.T(), err)
+	assert.Equal(rb.T(), 1, len(clusterList.Data))
+
+	err = users.RemoveClusterRoleFromUser(clusterAdminClient, additionalUser)
+	require.NoError(rb.T(), err)
+	clusterList, err = additionalUserClient.Steve.SteveType(clusters.ProvisioningSteveResourceType).ListAll(nil)
+	require.Empty(rb.T(), clusterList.Data)
+}
+
+func (rb *RBACAdditionalTestSuite) TestClusterOwnerAddsClusterMemberAsProjectOwner() {
+	subSession := rb.session.NewSession()
+	defer subSession.Cleanup()
+
+	clusterAdmin, clusterAdminClient, err := rbac.SetupUser(rb.client, rbac.StandardUser.String())
+	require.NoError(rb.T(), err)
+
+	errUserRole := users.AddClusterRoleToUser(rb.client, rb.cluster, clusterAdmin, rbac.ClusterOwner.String(), nil)
+	require.NoError(rb.T(), errUserRole)
+	clusterAdminClient, err = clusterAdminClient.ReLogin()
+	require.NoError(rb.T(), err)
+
+	additionalUser, additionalUserClient, err := rbac.SetupUser(rb.client, rbac.StandardUser.String())
+	require.NoError(rb.T(), err)
+
+	errUserRole2 := users.AddClusterRoleToUser(clusterAdminClient, rb.cluster, additionalUser, rbac.ClusterMember.String(), nil)
+	require.NoError(rb.T(), errUserRole2)
+	additionalUserClient, err = additionalUserClient.ReLogin()
+	require.NoError(rb.T(), err)
+
+	clusterList, err := additionalUserClient.Steve.SteveType(clusters.ProvisioningSteveResourceType).ListAll(nil)
+	require.NoError(rb.T(), err)
+	assert.Equal(rb.T(), 1, len(clusterList.Data))
+
+	clusterOwnerProject, err := clusterAdminClient.Management.Project.Create(projects.NewProjectConfig(rb.cluster.ID))
+	require.NoError(rb.T(), err)
+
+	err = users.AddProjectMember(clusterAdminClient, clusterOwnerProject, additionalUser, rbac.ProjectOwner.String(), nil)
+	require.NoError(rb.T(), err)
+	projectListProjectOwner, err := projects.ListProjectNames(additionalUserClient, rb.cluster.ID)
+	require.NoError(rb.T(), err)
+
+	assert.Equal(rb.T(), 1, len(projectListProjectOwner))
+	assert.Equal(rb.T(), clusterOwnerProject.Name, projectListProjectOwner[0])
+}
+
+func TestClusterRoleTestSuite(t *testing.T) {
+	suite.Run(t, new(RBACAdditionalTestSuite))
+}

tests/v2/validation/rbac/clusterandprojectroles/project_role_test.go
@@ -1,0 +1,119 @@
+//go:build (validation || infra.any || cluster.any || extended) && !sanity && !stress
+
+package clusterandprojectroles
+
+import (
+	"testing"
+
+	"github.com/rancher/shepherd/clients/rancher"
+	management "github.com/rancher/shepherd/clients/rancher/generated/management/v3"
+	"github.com/rancher/shepherd/extensions/clusters"
+	"github.com/rancher/shepherd/extensions/projects"
+	rbac "github.com/rancher/shepherd/extensions/rbac"
+	"github.com/rancher/shepherd/extensions/users"
+	"github.com/rancher/shepherd/pkg/session"
+	log "github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+)
+
+type ProjectRolesTestSuite struct {
+	suite.Suite
+	client  *rancher.Client
+	session *session.Session
+	cluster *management.Cluster
+}
+
+func (pr *ProjectRolesTestSuite) TearDownSuite() {
+	pr.session.Cleanup()
+}
+
+func (pr *ProjectRolesTestSuite) SetupSuite() {
+	pr.session = session.NewSession()
+	client, err := rancher.NewClient("", pr.session)
+	require.NoError(pr.T(), err)
+
+	pr.client = client
+
+	log.Info("Getting cluster name from the config file and append cluster details in pr")
+	clusterName := client.RancherConfig.ClusterName
+	require.NotEmptyf(pr.T(), clusterName, "Cluster name to install should be set")
+	clusterID, err := clusters.GetClusterIDByName(pr.client, clusterName)
+	require.NoError(pr.T(), err, "Error getting cluster ID")
+	pr.cluster, err = pr.client.Management.Cluster.ByID(clusterID)
+	require.NoError(pr.T(), err)
+}
+
+func (pr *ProjectRolesTestSuite) testSetupUserAndProject() (*rancher.Client, *management.Project) {
+	pr.T().Log("Set up User with cluster Role for additional rbac test cases " + rbac.ClusterOwner)
+	newUser, standardUserClient, err := rbac.SetupUser(pr.client, rbac.StandardUser.String())
+	require.NoError(pr.T(), err)
+
+	createProjectAsAdmin, err := pr.client.Management.Project.Create(projects.NewProjectConfig(pr.cluster.ID))
+	require.NoError(pr.T(), err)
+
+	log.Info("Adding a standard user as project Owner in the admin project")
+	errUserRole := users.AddProjectMember(pr.client, createProjectAsAdmin, newUser, rbac.ProjectOwner.String(), nil)
+	require.NoError(pr.T(), errUserRole)
+	standardUserClient, err = standardUserClient.ReLogin()
+	require.NoError(pr.T(), err)
+	return standardUserClient, createProjectAsAdmin
+}
+
+func (pr *ProjectRolesTestSuite) TestProjectOwnerAddsAndRemovesOtherProjectOwners() {
+	subSession := pr.session.NewSession()
+	defer subSession.Cleanup()
+
+	standardUserClient, adminProject := pr.testSetupUserAndProject()
+	additionalUser, additionalUserClient, err := rbac.SetupUser(pr.client, rbac.StandardUser.String())
+	require.NoError(pr.T(), err)
+
+	errUserRole := users.AddProjectMember(standardUserClient, adminProject, additionalUser, rbac.ProjectOwner.String(), nil)
+	require.NoError(pr.T(), errUserRole)
+	additionalUserClient, err = additionalUserClient.ReLogin()
+	require.NoError(pr.T(), err)
+
+	userGetProject, err := projects.ListProjectNames(additionalUserClient, pr.cluster.ID)
+	require.NoError(pr.T(), err)
+	assert.Equal(pr.T(), 1, len(userGetProject))
+	assert.Equal(pr.T(), adminProject.Name, userGetProject[0])
+
+	errRemoveMember := users.RemoveProjectMember(standardUserClient, additionalUser)
+	require.NoError(pr.T(), errRemoveMember)
+
+	userProjectEmptyAfterRemoval, err := projects.ListProjectNames(additionalUserClient, pr.cluster.ID)
+	require.Empty(pr.T(), userProjectEmptyAfterRemoval)
+}
+
+func (pr *ProjectRolesTestSuite) TestManageProjectUserRoleCannotAddProjectOwner() {
+	subSession := pr.session.NewSession()
+	defer subSession.Cleanup()
+
+	standardUserClient, adminProject := pr.testSetupUserAndProject()
+	additionalUser, additionalUserClient, err := rbac.SetupUser(pr.client, rbac.StandardUser.String())
+	require.NoError(pr.T(), err)
+
+	errUserRole := users.AddProjectMember(standardUserClient, adminProject, additionalUser, rbac.CustomManageProjectMember.String(), nil)
+	require.NoError(pr.T(), errUserRole)
+	additionalUserClient, err = additionalUserClient.ReLogin()
+	require.NoError(pr.T(), err)
+
+	addNewUserAsPO, addNewUserAsPOClient, err := rbac.SetupUser(pr.client, rbac.StandardUser.String())
+	require.NoError(pr.T(), err)
+
+	errUserRole2 := users.AddProjectMember(additionalUserClient, adminProject, addNewUserAsPO, rbac.ProjectOwner.String(), nil)
+	require.Error(pr.T(), errUserRole2)
+	assert.Contains(pr.T(), errUserRole2.Error(), "422 Unprocessable Entity")
+
+	addNewUserAsPOClient, err = addNewUserAsPOClient.ReLogin()
+	require.NoError(pr.T(), err)
+
+	userGetProject, err := projects.ListProjectNames(addNewUserAsPOClient, pr.cluster.ID)
+	require.NoError(pr.T(), err)
+	assert.Equal(pr.T(), 0, len(userGetProject))
+}
+
+func TestProjectRolesTestSuite(t *testing.T) {
+	suite.Run(t, new(ProjectRolesTestSuite))
+}