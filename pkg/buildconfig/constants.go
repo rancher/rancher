7d3e1dc8e8ab0536468d4af18803d81f77a77155
Dan Pock <self@danpock.me>
2024-05-03 11:43:41 -0400

    go generate

--- pkg/buildconfig/constants.go
+++ pkg/buildconfig/constants.go
@@ -4,7 +4,7 @@ package buildconfig
 
 const (
 	CspAdapterMinVersion = "103.0.1+up3.0.1"
-	DefaultShellVersion  = "rancher/shell:v0.1.24-rc1"
+	DefaultShellVersion  = "rancher/shell:v0.1.24"
 	FleetVersion         = "103.1.4+up0.9.4"
 	WebhookVersion       = "103.0.3+up0.4.4-rc1"
 )
