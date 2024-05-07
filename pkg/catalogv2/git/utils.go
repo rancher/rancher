a5c78169fd7aedbdbb16d5ef8e03ff998d9d2d96
Venkata Krishna Rohit Sakala <rohit.sakala@suse.com>
2024-05-06 17:40:14 -0700

    Remove unused functions

--- pkg/catalogv2/git/utils.go
+++ pkg/catalogv2/git/utils.go
@@ -11,8 +11,6 @@ import (
 	"path/filepath"
 	"regexp"
 	"strings"
-
-	"github.com/pkg/errors"
 )
 
 const (
@@ -116,27 +114,6 @@ func formatGitURL(endpoint, branch string) string {
 	}
 
 	return ""
-}
-
-func firstField(lines []string, errText string) (string, error) {
-	if len(lines) == 0 {
-		return "", errors.New(errText)
-	}
-
-	fields := strings.Fields(lines[0])
-	if len(fields) == 0 {
-		return "", errors.New(errText)
-	}
-
-	if len(fields[0]) == 0 {
-		return "", errors.New(errText)
-	}
-
-	return fields[0], nil
-}
-
-func formatRefForBranch(branch string) string {
-	return fmt.Sprintf("refs/heads/%s", branch)
 }
 
 type basicRoundTripper struct {
