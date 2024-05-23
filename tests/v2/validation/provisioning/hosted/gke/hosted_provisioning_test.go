ccd68a98323676288a70ba940e6761a8b9d26ad1
Parthvi <parthvi.vala@suse.com>
2024-05-23 14:38:57 +0530

    Deprecate hosted provisioning tests
    Signed-off-by: Parthvi <parthvi.vala@suse.com>

--- tests/v2/validation/provisioning/hosted/gke/hosted_provisioning_test.go
+++ tests/v2/validation/provisioning/hosted/gke/hosted_provisioning_test.go
@@ -78,5 +78,6 @@ func (h *HostedGKEClusterProvisioningTestSuite) TestProvisioningHostedGKE() {
 // In order for 'go test' to run this suite, we need to create
 // a normal test function and pass our suite to suite.Run
 func TestHostedGKEClusterProvisioningTestSuite(t *testing.T) {
+	t.Skip("This test has been deprecated; check https://github.com/rancher/hosted-providers-e2e for updated tests")
 	suite.Run(t, new(HostedGKEClusterProvisioningTestSuite))
 }
