db70fdd0a88d47f415d59c82fb562d9607f636ca
Chad Roberts <chad.roberts@suse.com>
2024-05-28 16:25:21 -0400

    Adding tests for SearchPrincipals and t.Parallel()

--- pkg/auth/providers/genericoidc/genericoidc_provider_test.go
+++ pkg/auth/providers/genericoidc/genericoidc_provider_test.go
@@ -104,6 +104,7 @@ func TestGenOIDCProvider_GetPrincipal(t *testing.T) {
 			},
 		}
 		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
 			got, err := g.GetPrincipal(tt.principalID, tt.token)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("GetPrincipal() error = %v, wantErr %v", err, tt.wantErr)
@@ -111,6 +112,99 @@ func TestGenOIDCProvider_GetPrincipal(t *testing.T) {
 			}
 			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("GetPrincipal() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestGenOIDCProvider_SearchPrincipals(t *testing.T) {
+	tests := []struct {
+		name          string
+		searchValue   string
+		principalType string
+		expected      []v3.Principal
+	}{
+		{
+			name:          "test search for user principal",
+			searchValue:   "user1",
+			principalType: UserType,
+			expected: []v3.Principal{
+				{
+					ObjectMeta:    metav1.ObjectMeta{Name: "genericoidc_user://user1"},
+					DisplayName:   "user1",
+					LoginName:     "user1",
+					PrincipalType: UserType,
+					Provider:      Name,
+				},
+			},
+		},
+		{
+			name:        "test search for user principal with empty principaltype",
+			searchValue: "user1",
+			expected: []v3.Principal{
+				{
+					ObjectMeta:    metav1.ObjectMeta{Name: "genericoidc_user://user1"},
+					DisplayName:   "user1",
+					LoginName:     "user1",
+					PrincipalType: UserType,
+					Provider:      Name,
+				},
+			},
+		},
+		{
+			name: "test search for user principal with empty principaltype and searchval",
+			expected: []v3.Principal{
+				{
+					ObjectMeta:    metav1.ObjectMeta{Name: "genericoidc_user://"},
+					PrincipalType: UserType,
+					Provider:      Name,
+				},
+			},
+		},
+		{
+			name:          "test search for group principal",
+			searchValue:   "group1",
+			principalType: GroupType,
+			expected: []v3.Principal{
+				{
+					ObjectMeta:    metav1.ObjectMeta{Name: "genericoidc_group://group1"},
+					DisplayName:   "group1",
+					LoginName:     "group1",
+					PrincipalType: GroupType,
+					Provider:      Name,
+				},
+			},
+		},
+		{
+			name:          "test search for group principal with empty searchval",
+			principalType: GroupType,
+			expected: []v3.Principal{
+				{
+					ObjectMeta:    metav1.ObjectMeta{Name: "genericoidc_group://"},
+					PrincipalType: GroupType,
+					Provider:      Name,
+				},
+			},
+		},
+	}
+
+	g := &GenOIDCProvider{
+		oidc.OpenIDCProvider{
+			Name: Name,
+			Type: client.GenericOIDCConfigType,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+			result, err := g.SearchPrincipals(test.searchValue, test.principalType, v3.Token{})
+			if err != nil {
+				t.Errorf("SearchPrincipals() returned an error: %v", err)
+			}
+
+			if !reflect.DeepEqual(result, test.expected) {
+				t.Errorf("SearchPrincipals() returned %+v, expected %+v", result, test.expected)
 			}
 		})
 	}
