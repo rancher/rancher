a37838ab00279e3d1a894680c0faf22b555187fc
Venkata Krishna Rohit Sakala <rohit.sakala@suse.com>
2024-06-03 17:37:18 -0700

    Change timeout of icons to 2 seconds

--- pkg/catalogv2/http/download.go
+++ pkg/catalogv2/http/download.go
@@ -9,6 +9,7 @@ import (
 	"net/url"
 	"path"
 	"strings"
+	"time"
 
 	"github.com/rancher/wrangler/v2/pkg/schemas/validation"
 
@@ -26,6 +27,7 @@ func Icon(secret *corev1.Secret, repoURL string, caBundle []byte, insecureSkipTL
 	}
 
 	client, err := HelmClient(secret, caBundle, insecureSkipTLSVerify, disableSameOriginCheck, repoURL)
+	client.Timeout = 2 * time.Second
 	if err != nil {
 		return nil, "", err
 	}
