517eac4fdf91c935a073d1988fee0d063b2c45c0
Jonathan Crowther <jonathan.crowther@suse.com>
2024-06-11 13:53:46 -0400

    Split reconcileCreatorRTB function into cluster and project versions

--- pkg/controllers/management/auth/project_handler.go
+++ pkg/controllers/management/auth/project_handler.go
@@ -1,6 +1,7 @@
 package auth
 
 import (
+	"encoding/json"
 	"errors"
 	"reflect"
 	"strings"
@@ -11,7 +12,9 @@ import (
 	"github.com/rancher/rancher/pkg/rbac"
 	"github.com/rancher/rancher/pkg/systemaccount"
 	"github.com/sirupsen/logrus"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	kerrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/api/meta"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -25,9 +28,11 @@ const (
 type projectLifecycle struct {
 	mgr                  *mgr
 	crtbClient           v3.ClusterRoleTemplateBindingInterface
-	systemAccountManager *systemaccount.Manager
+	crtbLister           v3.ClusterRoleTemplateBindingLister
+	prtbLister           v3.ProjectRoleTemplateBindingLister
 	rbLister             rbacv1.RoleBindingLister
 	roleBindings         rbacv1.RoleBindingInterface
+	systemAccountManager *systemaccount.Manager
 }
 
 func (l *projectLifecycle) sync(key string, orig *apisv3.Project) (runtime.Object, error) {
@@ -52,7 +57,7 @@ func (l *projectLifecycle) sync(key string, orig *apisv3.Project) (runtime.Objec
 		return nil, err
 	}
 
-	obj, err = l.mgr.reconcileCreatorRTB(obj)
+	obj, err = l.reconcileProjectCreatorRTB(obj)
 	if err != nil {
 		return nil, err
 	}
@@ -79,7 +84,7 @@ func (l *projectLifecycle) sync(key string, orig *apisv3.Project) (runtime.Objec
 func (l *projectLifecycle) enqueueCrtbs(project *apisv3.Project) error {
 	// get all crtbs in current project's cluster
 	clusterID := project.Namespace
-	crtbs, err := l.mgr.crtbLister.List(clusterID, labels.Everything())
+	crtbs, err := l.crtbLister.List(clusterID, labels.Everything())
 	if err != nil {
 		return err
 	}
@@ -113,4 +118,92 @@ func (l *projectLifecycle) Remove(obj *apisv3.Project) (runtime.Object, error) {
 	err = l.mgr.deleteNamespace(obj, projectRemoveController)
 	returnErr = errors.Join(returnErr, err)
 	return obj, returnErr
+}
+
+func (l *projectLifecycle) reconcileProjectCreatorRTB(obj runtime.Object) (runtime.Object, error) {
+	return apisv3.CreatorMadeOwner.DoUntilTrue(obj, func() (runtime.Object, error) {
+		metaAccessor, err := meta.Accessor(obj)
+		if err != nil {
+			return obj, err
+		}
+
+		typeAccessor, err := meta.TypeAccessor(obj)
+		if err != nil {
+			return obj, err
+		}
+
+		creatorID, ok := metaAccessor.GetAnnotations()[creatorIDAnn]
+		if !ok || creatorID == "" {
+			logrus.Warnf("%v %v has no creatorId annotation. Cannot add creator as owner", typeAccessor.GetKind(), metaAccessor.GetName())
+			return obj, nil
+		}
+		project := obj.(*apisv3.Project)
+
+		if apisv3.ProjectConditionInitialRolesPopulated.IsTrue(project) {
+			// The projectRoleBindings are already completed, no need to check
+			return obj, nil
+		}
+
+		// If the project does not have the annotation it indicates the
+		// project is from a previous rancher version so don't add the
+		// default bindings.
+		roleJSON, ok := project.Annotations[roleTemplatesRequired]
+		if !ok {
+			return project, nil
+		}
+
+		roleMap := make(map[string][]string)
+		err = json.Unmarshal([]byte(roleJSON), &roleMap)
+		if err != nil {
+			return obj, err
+		}
+
+		var createdRoles []string
+
+		for _, role := range roleMap["required"] {
+			rtbName := "creator-" + role
+
+			if rtb, _ := l.prtbLister.Get(metaAccessor.GetName(), rtbName); rtb != nil {
+				createdRoles = append(createdRoles, role)
+				// This projectRoleBinding exists, need to check all of them so keep going
+				continue
+			}
+
+			// The projectRoleBinding doesn't exist yet so create it
+			om := v1.ObjectMeta{
+				Name:      rtbName,
+				Namespace: metaAccessor.GetName(),
+			}
+
+			logrus.Infof("[%v] Creating creator projectRoleTemplateBinding for user %v for project %v", projectCreateController, creatorID, metaAccessor.GetName())
+			if _, err := l.mgr.mgmt.Management.ProjectRoleTemplateBindings(metaAccessor.GetName()).Create(&apisv3.ProjectRoleTemplateBinding{
+				ObjectMeta:       om,
+				ProjectName:      metaAccessor.GetNamespace() + ":" + metaAccessor.GetName(),
+				RoleTemplateName: role,
+				UserName:         creatorID,
+			}); err != nil && !apierrors.IsAlreadyExists(err) {
+				return obj, err
+			}
+			createdRoles = append(createdRoles, role)
+		}
+
+		project = project.DeepCopy()
+
+		roleMap["created"] = createdRoles
+		d, err := json.Marshal(roleMap)
+		if err != nil {
+			return obj, err
+		}
+
+		project.Annotations[roleTemplatesRequired] = string(d)
+
+		if reflect.DeepEqual(roleMap["required"], createdRoles) {
+			apisv3.ProjectConditionInitialRolesPopulated.True(project)
+			logrus.Infof("[%v] Setting InitialRolesPopulated condition on project %v", ctrbMGMTController, project.Name)
+		}
+		if _, err := l.mgr.mgmt.Management.Projects("").Update(project); err != nil {
+			return obj, err
+		}
+		return obj, nil
+	})
 }
