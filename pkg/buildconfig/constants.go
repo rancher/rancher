e1ab711ee582bcb25bfa92b320c80ff42deb1e78
Peter Matseykanets <peter.matseykanets@suse.com>
2024-06-12 11:23:15 -0400

    [v2.8.5] s4: Bump rancher-webhook to v0.4.6

--- pkg/buildconfig/constants.go
+++ pkg/buildconfig/constants.go
@@ -6,5 +6,5 @@ const (
 	CspAdapterMinVersion = "103.0.1+up3.0.1"
 	DefaultShellVersion  = "rancher/shell:v0.1.24"
 	FleetVersion         = "103.1.4+up0.9.4"
-	WebhookVersion       = "103.0.4+up0.4.5"
+	WebhookVersion       = "103.0.5+up0.4.6"
 )
