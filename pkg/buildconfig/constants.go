d73eb9f04a2db7a8fc954e77c5fe7a6294eef775
Michael Bolot <michael.bolot@suse.com>
2024-06-13 15:08:52 -0500

    Bump fleet version to v0.9.5

--- pkg/buildconfig/constants.go
+++ pkg/buildconfig/constants.go
@@ -5,6 +5,6 @@ package buildconfig
 const (
 	CspAdapterMinVersion = "103.0.1+up3.0.1"
 	DefaultShellVersion  = "rancher/shell:v0.1.24"
-	FleetVersion         = "103.1.4+up0.9.4"
+	FleetVersion         = "103.1.5+up0.9.5"
 	WebhookVersion       = "103.0.6+up0.4.7"
 )
