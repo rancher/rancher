0229d8fd96e61e3399ece8a2210c7efe46ecb9da
Dan <dan.pock@suse.com>
2024-07-02 14:30:43 -0400

    [v2.9] bump shell RC version to newest (#45967)
    * bump shell RC version to newest
    
    * go generate

--- pkg/buildconfig/constants.go
+++ pkg/buildconfig/constants.go
@@ -4,7 +4,7 @@ package buildconfig
 
 const (
 	CspAdapterMinVersion    = "104.0.0+up4.0.0-rc8"
-	DefaultShellVersion     = "rancher/shell:v0.2.1-rc.4"
+	DefaultShellVersion     = "rancher/shell:v0.2.1-rc.5"
 	FleetVersion            = "104.0.0+up0.10.0-rc.18"
 	ProvisioningCAPIVersion = "104.0.0+up0.3.0-rc.1"
 	WebhookVersion          = "104.0.0+up0.5.0-rc10"
