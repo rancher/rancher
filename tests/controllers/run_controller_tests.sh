c354dd5a3fa6d7bec27403088bd92532c91b9581
Jonathan Crowther <jonathan.crowther@suse.com>
2024-07-04 12:01:29 -0400

    Add controller integration tests to the CI

--- tests/controllers/run_controller_tests.sh
+++ tests/controllers/run_controller_tests.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-ENVTEST_K8S_VERSION=${ENVTEST_K8S_VERSION-1.28.3}
-
 if ! command -v setup-envtest &> /dev/null
 then
     go install sigs.k8s.io/controller-runtime/tools/setup-envtest@latest
@@ -9,8 +7,8 @@ fi
 
 if [ -z "$KUBEBUILDER_ASSETS" ];
 then 
-    KUBEBUILDER_ASSETS=$(setup-envtest use --use-env -p path $ENVTEST_K8S_VERSION)
+    KUBEBUILDER_ASSETS=$(setup-envtest use --use-env -p path)
     export KUBEBUILDER_ASSETS
 fi
 
-go test ./...
+go test -v $(dirname "$0")/...
