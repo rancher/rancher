13996985fde7b5950d9c8edbe83af0da9241fcf6
yarunachalam <yarunachalam@suse.com>
2024-07-12 10:38:20 -0500

    csp adapter 4.0.0

--- pkg/buildconfig/constants.go
+++ pkg/buildconfig/constants.go
@@ -3,7 +3,7 @@
 package buildconfig
 
 const (
-	CspAdapterMinVersion    = "104.0.0+up4.0.0-rc9"
+	CspAdapterMinVersion    = "104.0.0+up4.0.0"
 	DefaultShellVersion     = "rancher/shell:v0.2.1-rc.7"
 	FleetVersion            = "104.0.0+up0.10.0-rc.20"
 	ProvisioningCAPIVersion = "104.0.0+up0.3.0-rc.1"
