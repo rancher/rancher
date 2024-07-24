099aaebfca3ce8423296a0bef9b4c9bd6d9b9849
Chris Kim <oats87g@gmail.com>
2024-07-24 17:30:56 +0000

    go generate
    Signed-off-by: Chris Kim <oats87g@gmail.com>

--- pkg/buildconfig/constants.go
+++ pkg/buildconfig/constants.go
@@ -6,6 +6,6 @@ const (
 	CspAdapterMinVersion    = "104.0.0+up4.0.0"
 	DefaultShellVersion     = "rancher/shell:v0.2.1"
 	FleetVersion            = "104.0.0+up0.10.0"
-	ProvisioningCAPIVersion = "104.0.0+up0.3.0-rc.1"
+	ProvisioningCAPIVersion = "104.0.0+up0.3.0"
 	WebhookVersion          = "104.0.0+up0.5.0"
 )
