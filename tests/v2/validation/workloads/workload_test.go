2d80127414db71a3ecbbb7f5880ae49e9af7b553
Leonardo Scalabrini <leonardo.scalabrini@suse.com>
2024-07-26 17:52:54 -0300

    <2.9> Convert test_wl_sidekick test to go
    - Implemented TestWorkloadSideKick test

--- tests/v2/validation/workloads/workload_test.go
+++ tests/v2/validation/workloads/workload_test.go
@@ -5,13 +5,17 @@ import (
 
 	projectsapi "github.com/rancher/rancher/tests/v2/actions/projects"
 	deployment "github.com/rancher/rancher/tests/v2/actions/workloads/deployment"
+	"github.com/rancher/rancher/tests/v2/actions/workloads/pods"
 	"github.com/rancher/shepherd/clients/rancher"
 	management "github.com/rancher/shepherd/clients/rancher/generated/management/v3"
 	"github.com/rancher/shepherd/extensions/clusters"
+	"github.com/rancher/shepherd/extensions/workloads"
+	namegen "github.com/rancher/shepherd/pkg/namegenerator"
 	"github.com/rancher/shepherd/pkg/session"
 	log "github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
+	corev1 "k8s.io/api/core/v1"
 )
 
 type WorkloadTestSuite struct {
@@ -53,6 +57,41 @@ func (w *WorkloadTestSuite) TestWorkloadDeployment() {
 
 	_, err = deployment.CreateDeployment(w.client, w.cluster.ID, namespace.Name, 1, "", "", false, false)
 	require.NoError(w.T(), err)
+}
+
+func (w *WorkloadTestSuite) TestWorkloadSideKick() {
+	subSession := w.session.NewSession()
+	defer subSession.Cleanup()
+
+	_, namespace, err := projectsapi.CreateProjectAndNamespace(w.client, w.cluster.ID)
+	require.NoError(w.T(), err)
+
+	createdDeployment, err := deployment.CreateDeployment(w.client, w.cluster.ID, namespace.Name, 1, "", "", false, false)
+	require.NoError(w.T(), err)
+
+	countRunning, err := pods.CountPodContainerRunning(w.client, w.cluster.ID, namespace.Name)
+	require.NoError(w.T(), err)
+	require.Equal(w.T(), 1, countRunning)
+
+	containerName := namegen.AppendRandomString("updatetestcontainer")
+	newContainerTemplate := workloads.NewContainer(containerName,
+		"redis",
+		corev1.PullAlways,
+		[]corev1.VolumeMount{},
+		[]corev1.EnvFromSource{},
+		nil,
+		nil,
+		nil,
+	)
+
+	createdDeployment.Spec.Template.Spec.Containers = append(createdDeployment.Spec.Template.Spec.Containers, newContainerTemplate)
+
+	_, err = deployment.UpdateDeployment(w.client, w.cluster.ID, namespace.Name, createdDeployment)
+	require.NoError(w.T(), err)
+
+	countRunning, err = pods.CountPodContainerRunning(w.client, w.cluster.ID, namespace.Name)
+	require.NoError(w.T(), err)
+	require.Equal(w.T(), 2, countRunning)
 }
 
 func TestWorkloadTestSuite(t *testing.T) {
