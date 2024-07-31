8b0c1b00d39f329cf2c7826563916ebce5768d54
Leonardo Scalabrini <leonardo.scalabrini@suse.com>
2024-07-31 13:47:22 -0300

    <2.8> Convert test_wl_deployment test to go

---
+++ tests/v2/validation/workloads/workload_test.go
@@ -0,0 +1,60 @@
+package workloads
+
+import (
+	"testing"
+
+	projectsapi "github.com/rancher/rancher/tests/v2/actions/projects"
+	deployment "github.com/rancher/rancher/tests/v2/actions/workloads/deployment"
+	"github.com/rancher/shepherd/clients/rancher"
+	management "github.com/rancher/shepherd/clients/rancher/generated/management/v3"
+	"github.com/rancher/shepherd/extensions/clusters"
+	"github.com/rancher/shepherd/pkg/session"
+	log "github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+)
+
+type WorkloadTestSuite struct {
+	suite.Suite
+	client  *rancher.Client
+	session *session.Session
+	cluster *management.Cluster
+}
+
+func (w *WorkloadTestSuite) TearDownSuite() {
+	w.session.Cleanup()
+}
+
+func (w *WorkloadTestSuite) SetupSuite() {
+	w.session = session.NewSession()
+
+	client, err := rancher.NewClient("", w.session)
+	require.NoError(w.T(), err)
+
+	w.client = client
+
+	log.Info("Getting cluster name from the config file and append cluster details in connection")
+	clusterName := client.RancherConfig.ClusterName
+	require.NotEmptyf(w.T(), clusterName, "Cluster name to install should be set")
+
+	clusterID, err := clusters.GetClusterIDByName(w.client, clusterName)
+	require.NoError(w.T(), err, "Error getting cluster ID")
+
+	w.cluster, err = w.client.Management.Cluster.ByID(clusterID)
+	require.NoError(w.T(), err)
+}
+
+func (w *WorkloadTestSuite) TestWorkloadDeployment() {
+	subSession := w.session.NewSession()
+	defer subSession.Cleanup()
+
+	_, namespace, err := projectsapi.CreateProjectAndNamespace(w.client, w.cluster.ID)
+	require.NoError(w.T(), err)
+
+	_, err = deployment.CreateDeployment(w.client, w.cluster.ID, namespace.Name, 1, "", "", false, false)
+	require.NoError(w.T(), err)
+}
+
+func TestWorkloadTestSuite(t *testing.T) {
+	suite.Run(t, new(WorkloadTestSuite))
+}
