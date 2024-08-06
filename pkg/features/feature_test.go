4b1b556a5a0e813621d31514371c673e05e84c7d
Raul Cabello Martin <raul.cabello@suse.com>
2024-08-06 09:36:31 +0200

    Remove external-rules feature flag (#46314)
    Flag is present when upgrading from 2.8 to 2.9. This code removes the feature flag.

--- pkg/features/feature_test.go
+++ pkg/features/feature_test.go
@@ -3,7 +3,12 @@ package features
 import (
 	"testing"
 
+	"github.com/golang/mock/gomock"
+	v3 "github.com/rancher/rancher/pkg/apis/management.cattle.io/v3"
+	managementv3 "github.com/rancher/rancher/pkg/generated/controllers/management.cattle.io/v3"
+	"github.com/rancher/wrangler/v3/pkg/generic/fake"
 	"github.com/stretchr/testify/assert"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 var isDefFalse = newFeature("isfalse", "", false, false, true)
@@ -38,4 +43,28 @@ func TestInitializeNil(t *testing.T) {
 	assert.False(isDefFalse.Enabled())
 	InitializeFeatures(nil, "isfalse=true")
 	assert.True(isDefFalse.Enabled())
+}
+
+func TestInitializeFeatures(t *testing.T) {
+	tests := map[string]struct {
+		featureMock func() managementv3.FeatureClient
+		features    map[string]*Feature
+	}{
+		"delete external-rules feature is called": {
+			featureMock: func() managementv3.FeatureClient {
+				mock := fake.NewMockNonNamespacedControllerInterface[*v3.Feature, *v3.FeatureList](gomock.NewController(t))
+				mock.EXPECT().Delete("external-rules", &metav1.DeleteOptions{})
+
+				return mock
+			},
+			features: nil,
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			features = test.features // we can't run parallel tests if we modify this package var here. Code should be refactored to not use a package var if more tests cases are added.
+			InitializeFeatures(test.featureMock(), "")
+		})
+	}
 }
