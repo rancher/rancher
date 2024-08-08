26bc318dbf8651fc723eaafc7a2081737f40bb19
Peter Matseykanets <peter.matseykanets@suse.com>
2024-08-08 12:54:46 -0400

    [v2.9] Update kube-api-auth to v0.2.2

--- pkg/apis/management.cattle.io/v3/tools_system_images.go
+++ pkg/apis/management.cattle.io/v3/tools_system_images.go
@@ -5,7 +5,7 @@ var (
 		AuthSystemImages AuthSystemImages
 	}{
 		AuthSystemImages: AuthSystemImages{
-			KubeAPIAuth: "rancher/kube-api-auth:v0.2.1",
+			KubeAPIAuth: "rancher/kube-api-auth:v0.2.2",
 		},
 	}
 )
