775b37a67fd7013c9270d68853d9f0d6a896f307
Enrico Candino <enrico.candino@suse.com>
2024-08-08 10:39:50 +0200

    dropped fmt import, and cleaned up testing.T references.
    Signed-off-by: Enrico Candino <enrico.candino@suse.com>

--- tests/controllers/cleanup/delete_tokens_test.go
+++ tests/controllers/cleanup/delete_tokens_test.go
@@ -2,7 +2,6 @@ package feature_test
 
 import (
 	"context"
-	"fmt"
 	v3 "github.com/rancher/rancher/pkg/apis/management.cattle.io/v3"
 	client "github.com/rancher/rancher/pkg/client/generated/management/v3"
 	"github.com/rancher/rancher/pkg/controllers/management/auth"
@@ -34,17 +33,18 @@ func TestAuthConfigSuite(t *testing.T) {
 }
 
 func (s *AuthConfigSuite) SetupSuite() {
+	t := s.T()
 	s.ctx, s.cancel = context.WithCancel(context.Background())
 
 	// Load CRD from YAML for REST Client
 	s.testEnv = &envtest.Environment{}
 	restCfg, err := s.testEnv.Start()
-	assert.NoError(s.T(), err)
-	assert.NotNil(s.T(), restCfg)
+	assert.NoError(t, err)
+	assert.NotNil(t, restCfg)
 
 	// Create CRDs
 	factory, err := crd.NewFactoryFromClient(restCfg)
-	assert.NoError(s.T(), err)
+	assert.NoError(t, err)
 
 	err = factory.BatchCreateCRDs(s.ctx,
 		crd.CRD{
@@ -56,22 +56,22 @@ func (s *AuthConfigSuite) SetupSuite() {
 			NonNamespace: true,
 		},
 	).BatchWait()
-	assert.NoError(s.T(), err)
+	assert.NoError(t, err)
 
 	// Create the wrangler and management contexts
 	wranglerContext, err := wrangler.NewContext(s.ctx, nil, restCfg)
-	assert.NoError(s.T(), err)
+	assert.NoError(t, err)
 
 	scaledContext, clusterManager, _, err := multiclustermanager.BuildScaledContext(s.ctx, wranglerContext, &multiclustermanager.Options{})
-	assert.NoError(s.T(), err)
+	assert.NoError(t, err)
 	s.managementContext, err = scaledContext.NewManagementContext()
-	assert.NoError(s.T(), err)
+	assert.NoError(t, err)
 
 	// Register the auth controller
 	auth.RegisterEarly(s.ctx, s.managementContext, clusterManager)
 
 	// Start controllers
-	common.StartNormanControllers(s.ctx, s.T(), s.managementContext,
+	common.StartNormanControllers(s.ctx, t, s.managementContext,
 		schema.GroupVersionKind{
 			Group:   "management.cattle.io",
 			Version: "v3",
@@ -80,7 +80,7 @@ func (s *AuthConfigSuite) SetupSuite() {
 	)
 
 	// Start caches
-	common.StartWranglerCaches(s.ctx, s.T(), s.managementContext.Wrangler,
+	common.StartWranglerCaches(s.ctx, t, s.managementContext.Wrangler,
 		schema.GroupVersionKind{
 			Group:   "management.cattle.io",
 			Version: "v3",
@@ -90,7 +90,7 @@ func (s *AuthConfigSuite) SetupSuite() {
 }
 
 func (s *AuthConfigSuite) TestTokensCleanup() {
-	t := assert.New(s.T())
+	t := s.T()
 
 	const (
 		tick         = 1 * time.Second
@@ -107,7 +107,7 @@ func (s *AuthConfigSuite) TestTokensCleanup() {
 		Type:    client.OpenLdapConfigType,
 		Enabled: true,
 	})
-	t.NoError(err)
+	assert.NoError(t, err)
 
 	// create some tokens for the authProvider
 	for i := 0; i < numOfTokens; i++ {
@@ -115,7 +115,7 @@ func (s *AuthConfigSuite) TestTokensCleanup() {
 			ObjectMeta:   metav1.ObjectMeta{GenerateName: "t-"},
 			AuthProvider: authProvider,
 		})
-		t.NoError(err)
+		assert.NoError(t, err)
 	}
 
 	// and also some local tokens
@@ -124,22 +124,22 @@ func (s *AuthConfigSuite) TestTokensCleanup() {
 			ObjectMeta:   metav1.ObjectMeta{GenerateName: "t-"},
 			AuthProvider: "local",
 		})
-		t.NoError(err)
+		assert.NoError(t, err)
 	}
 
-	fmt.Printf("created %d tokens\n", numOfTokens)
+	t.Logf("created %d tokens\n", numOfTokens)
 
 	// disable the provider, setting the cleanup annotation (eventually because of retries)
 	authConfig, err := s.managementContext.Management.AuthConfigs("").Get(authProvider, metav1.GetOptions{})
-	t.NoError(err)
+	assert.NoError(t, err)
 
 	authConfig.Enabled = false
 	authConfig.Annotations = map[string]string{auth.CleanupAnnotation: auth.CleanupUnlocked}
 	authConfig, err = s.managementContext.Management.AuthConfigs("").Update(authConfig)
-	t.NoError(err)
+	assert.NoError(t, err)
 
 	// check that all the authProvider tokens are deleted
-	ok := assert.EventuallyWithT(s.T(), func(c *assert.CollectT) {
+	ok := assert.EventuallyWithT(t, func(c *assert.CollectT) {
 		tokens, err := s.managementContext.Management.Tokens("").List(metav1.ListOptions{})
 		assert.NoError(c, err)
 		assert.NotNil(c, tokens)
@@ -151,11 +151,11 @@ func (s *AuthConfigSuite) TestTokensCleanup() {
 			}
 		}
 
-		fmt.Printf("remaining %d tokens\n", remainingTokens)
+		t.Logf("remaining %d tokens\n", remainingTokens)
 		assert.Zero(c, remainingTokens)
 	}, duration, tick)
 
-	t.True(ok, "secrets were left over after disabling auth configuration")
+	assert.True(t, ok, "secrets were left over after disabling auth configuration")
 }
 
 func (s *AuthConfigSuite) TearDownSuite() {
