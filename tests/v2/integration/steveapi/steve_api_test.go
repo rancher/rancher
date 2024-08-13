5b6612e213387b5eae6d98f045e321d4a7bf5516
Jonathan Crowther <jonathan.crowther@suse.com>
2024-08-13 09:55:28 -0400

    re-enable steve test

--- tests/v2/integration/steveapi/steve_api_test.go
+++ tests/v2/integration/steveapi/steve_api_test.go
@@ -2688,7 +2688,5 @@ func TestSteveLocal(t *testing.T) {
 }
 
 func TestSteveDownstream(t *testing.T) {
-	// TODO: Re-enable the test when the bug is fixed
-	t.Skip()
 	suite.Run(t, new(DownstreamSteveAPITestSuite))
 }
