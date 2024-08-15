b5ff908c78e3e9e57e1aaa183fe4e44ba79ec3d1
Leonardo Scalabrini <leonardo.scalabrini@suse.com>
2024-08-15 16:29:38 -0300

    <v2.8> Convert test_wl_cronjob and test_wl_statefulset test to go
    Implemented TestWorkloadCronjob test
    Implemented TestWorkloadStatefulset test

--- tests/v2/actions/workloads/daemonset/daemonset.go
+++ tests/v2/actions/workloads/daemonset/daemonset.go
@@ -1,15 +1,24 @@
-package deamonset
+package daemonset
 
 import (
+	"context"
+
 	"github.com/rancher/rancher/tests/v2/actions/workloads/deployment"
 	"github.com/rancher/shepherd/clients/rancher"
-	"github.com/rancher/shepherd/extensions/charts"
+	"github.com/rancher/shepherd/extensions/unstructured"
 	"github.com/rancher/shepherd/extensions/workloads"
 	appv1 "k8s.io/api/apps/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 )
 
-// CreateDaemonset is a helper to create a deamonset
+var DaemonsetGroupVersionResource = schema.GroupVersionResource{
+	Group:    "apps",
+	Version:  "v1",
+	Resource: "daemonsets",
+}
+
+// CreateDaemonset is a helper to create a daemonset
 func CreateDaemonset(client *rancher.Client, clusterID, namespaceName string, replicaCount int, secretName, configMapName string, useEnvVars, useVolumes bool) (*appv1.DaemonSet, error) {
 	deploymentTemplate, err := deployment.CreateDeployment(client, clusterID, namespaceName, replicaCount, secretName, configMapName, useEnvVars, useVolumes)
 	if err != nil {
@@ -18,9 +27,17 @@ func CreateDaemonset(client *rancher.Client, clusterID, namespaceName string, re
 
 	createdDaemonset := workloads.NewDaemonSetTemplate(deploymentTemplate.Name, namespaceName, deploymentTemplate.Spec.Template, true, nil)
 
-	err = charts.WatchAndWaitDaemonSets(client, clusterID, namespaceName, metav1.ListOptions{
-		FieldSelector: "metadata.name=" + createdDaemonset.Name,
-	})
+	dynamicClient, err := client.GetDownStreamClusterClient(clusterID)
+	if err != nil {
+		return nil, err
+	}
 
-	return createdDaemonset, err
+	daemonsetResource := dynamicClient.Resource(DaemonsetGroupVersionResource).Namespace(namespaceName)
+
+	_, err = daemonsetResource.Create(context.TODO(), unstructured.MustToUnstructured(createdDaemonset), metav1.CreateOptions{})
+	if err != nil {
+		return nil, err
+	}
+
+	return createdDaemonset, nil
 }
