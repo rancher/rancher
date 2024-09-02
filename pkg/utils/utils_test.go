e36b8a2223bcb098efe6a09e35470c9ea1e541f3
Tim Hardeck <thardeck@suse.com>
2024-09-02 10:42:01 +0200

    Add additional IPv4 with port test to utils_test

--- pkg/utils/utils_test.go
+++ pkg/utils/utils_test.go
@@ -39,6 +39,7 @@ func TestIsPlainIPV6(t *testing.T) {
 		{"2001:cafe:43:1::3ec8", true},        // IPv6 address
 		{"::ffff:7f00:1", true},               // IPv6 address representing IPv4
 		{"192.168.1.1", false},                // IPv4 address
+		{"192.168.1.1:443", false},            // IPv4 address with port
 		{"[2001:cafe:43:1::3ec8]", false},     // Encapsulated IPv6 address
 		{"[2001:cafe:43:1::3ec8]:443", false}, // IPv6 address with port
 		{"hostname", false},                   // Hostname
