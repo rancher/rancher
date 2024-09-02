da8d51b9d18697b26a72029b5d7a87ce11812257
Andreas Kupries <andreas.kupries@suse.com>
2024-09-02 14:00:00 +0200

    chore: condition manipulation and checks

--- pkg/controllers/status/status.go
+++ pkg/controllers/status/status.go
@@ -1,5 +1,7 @@
 package status
 
+import v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
 // Status field summary stati
 const (
 	SummaryInProgress  = "InProgress"
@@ -7,3 +9,30 @@ const (
 	SummaryError       = "Error"
 	SummaryTerminating = "Terminating"
 )
+
+func RemoveConditions(conditions []v1.Condition, toRemove map[string]struct{}) []v1.Condition {
+	filtered := []v1.Condition{}
+	for _, c := range conditions {
+		// Skip over conditions found in toRemove.
+		if _, ok := toRemove[c.Reason]; ok {
+			continue
+		}
+		filtered = append(filtered, c)
+	}
+	return filtered
+}
+
+func HasAllOf(conditions []v1.Condition, toHave map[string]struct{}) bool {
+	// collect conditions for quick lookup
+	has := map[string]struct{}{}
+	for _, c := range conditions {
+		has[c.Reason] = struct{}{}
+	}
+	// check that all required conditions are present
+	for key, _ := range toHave {
+		if _, ok := has[key]; !ok {
+			return false
+		}
+	}
+	return true
+}
