6db6cbb5299064eb2d6ef06e8db3b4013b564728
Tom Lebreux <tom.lebreux@suse.com>
2024-09-10 12:30:58 -0400

    Bump webhook to 103.0.10+up0.4.11-rc.1

--- pkg/buildconfig/constants.go
+++ pkg/buildconfig/constants.go
@@ -6,5 +6,5 @@ const (
 	CspAdapterMinVersion = "103.0.1+up3.0.1"
 	DefaultShellVersion  = "rancher/shell:v0.1.26"
 	FleetVersion         = "103.1.8+up0.9.9-rc.2"
-	WebhookVersion       = "103.0.9+up0.4.10"
+	WebhookVersion       = "103.0.10+up0.4.11-rc.1"
 )
