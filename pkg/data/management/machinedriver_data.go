64bc32c255362d24cce347aa686edfc4fa54cf3f
Moritz Röhrich <moritz.rohrich@suse.com>
2024-10-09 13:51:24 +0200

    harvester machine driver: bump to v0.7.0
    Bump Harvester machine driver to version v0.7.0. This version contains
    a hook, giving Rancher the ability to update the cloud credentials, used
    by the Harvester machine driver, for existing downstream clusters.
    
    related-to: harvester/harvester#6645
    
    Signed-off-by: Moritz Röhrich <moritz.rohrich@suse.com>

--- pkg/data/management/machinedriver_data.go
+++ pkg/data/management/machinedriver_data.go
@@ -103,7 +103,7 @@ func addMachineDrivers(management *config.ManagementContext) error {
 	}
 	harvesterEnabled := features.GetFeatureByName(HarvesterDriver).Enabled()
 	// make sure the version number is consistent with the one at Line 40 of package/Dockerfile
-	if err := addMachineDriver(HarvesterDriver, "https://releases.rancher.com/harvester-node-driver/v0.6.7/docker-machine-driver-harvester-amd64.tar.gz", "", "a913f22a7cad5d0df18e0dfa8a8ae19703c36eb8ed4f6a34ae471a06af0890c3", []string{"releases.rancher.com"}, harvesterEnabled, harvesterEnabled, false, management); err != nil {
+	if err := addMachineDriver(HarvesterDriver, "https://releases.rancher.com/harvester-node-driver/v0.7.0/docker-machine-driver-harvester-amd64.tar.gz", "", "ee28240ca31f44f4ee003040da1d92e92a2487dbd96a387c8fe647c605b27d60", []string{"releases.rancher.com"}, harvesterEnabled, harvesterEnabled, false, management); err != nil {
 		return err
 	}
 	linodeBuiltin := true
