def5524905565b7c17f67fd333b8d2763399c183
Anupama <38144301+anupama2501@users.noreply.github.com>
2024-10-10 15:45:02 -0700

    Initial PR
    diff --git a/tests/v2/actions/charts/charts.go b/tests/v2/actions/charts/charts.go
    index b3150794c..e92634862 100644
    --- a/tests/v2/actions/charts/charts.go
    +++ b/tests/v2/actions/charts/charts.go
    @@ -21,6 +21,11 @@ type InstallOptions struct {
     	ProjectID string
     }
    
    +type ExtensionOptions struct {
    +	ChartName string
    +	Version string
    +	ReleaseName string
    +}
     // payloadOpts is a private struct that contains the options for the chart payloads.
     // It is used to avoid passing the same options to different functions while using the chart helpers.
     type payloadOpts struct {
    diff --git a/tests/v2/actions/charts/payloads.go b/tests/v2/actions/charts/payloads.go
    index 79dfdc9f2..13eef474c 100644
    --- a/tests/v2/actions/charts/payloads.go
    +++ b/tests/v2/actions/charts/payloads.go
    @@ -111,3 +111,27 @@ func newChartUpgrade(chartName, releaseName, version, clusterID, clusterName, ur
    
     	return &chartUpgrade
     }
    +
    +// newExtensionsInstallAction is a private constructor that creates a chart install with given chart values that can be used for chart install action.
    +func newExtensionsInstall(name, version string, chartValues map[string]interface{}) *types.ChartInstall {
    +	chartInstall := types.ChartInstall{
    +		ChartName:   name,
    +		ReleaseName: name,
    +		Version:     version,
    +		Values:      nil,
    +	}
    +
    +	for k, v := range chartValues {
    +		chartInstall.Values[k] = v
    +	}
    +
    +	return &chartInstall
    +}
    +
    +// newExtensionsInstallAction is a private constructor that creates a payload for extensions install action chartInstalls and extensions namespace.
    +func newExtensionsInstallAction(namespace string, chartInstalls []types.ChartInstall) *types.ChartInstallAction {
    +	return &types.ChartInstallAction{
    +		Namespace: namespace,
    +		Charts:    chartInstalls,
    +	}
    +}
    diff --git a/tests/v2/actions/charts/uiplugins.go b/tests/v2/actions/charts/uiplugins.go
    new file mode 100644
    index 000000000..dbd4ff87e
    --- /dev/null
    +++ b/tests/v2/actions/charts/uiplugins.go
    @@ -0,0 +1 @@
    +package charts
    diff --git a/tests/v2/validation/observability/stackstate.go b/tests/v2/validation/observability/stackstate.go
    new file mode 100644
    index 000000000..40a871b69
    --- /dev/null
    +++ b/tests/v2/validation/observability/stackstate.go
    @@ -0,0 +1,54 @@
    +package observability
    +
    +import (
    +	"fmt"
    +
    +	v1 "github.com/rancher/rancher/pkg/apis/catalog.cattle.io/v1"
    +	"github.com/rancher/shepherd/pkg/config"
    +	meta "k8s.io/apimachinery/pkg/apis/meta/v1"
    +	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
    +)
    +
    +const (
    +	rancherUIPlugins           = "rancher-ui-plugins"
    +	uiExtensionsRepo           = "https://github.com/rancher/ui-plugin-charts"
    +	uiGitBranch                = "main"
    +	StackStateCRDConfigFileKey = "stackstateCRDConfigs"
    +)
    +
    +var (
    +	clusterRepoObj = v1.ClusterRepo{
    +		ObjectMeta: meta.ObjectMeta{
    +			Name: rancherUIPlugins,
    +		},
    +		Spec: v1.RepoSpec{
    +			GitRepo:   uiExtensionsRepo,
    +			GitBranch: uiGitBranch,
    +		},
    +	}
    +)
    +
    +// Add comment
    +type StackstateCRD struct {
    +	ServiceToken string `json:"serviceToken" yaml:"serviceToken"`
    +	Url          string `json:"url" yaml:"url"`
    +}
    +
    +
    +// Add comment
    +func NewStackstateCRDConfig(namespace string) unstructured.Unstructured {
    +	var stackstateCRDConfigs StackstateCRD
    +
    +	fmt.Printf("Loading configuration from: %s\n", StackStateCRDConfigFileKey)
    +
    +	config.LoadConfig(StackStateCRDConfigFileKey, &stackstateCRDConfigs)
    +
    +	crdConfig := unstructured.Unstructured{}
    +	crdConfig.Object["url"] = 	stackstateCRDConfigs.Url
    +	crdConfig.Object["serviceToken"] = stackstateCRDConfigs.ServiceToken
    +	crdConfig.SetNamespace(namespace)
    +
    +
    +
    +	return crdConfig
    +}

--- tests/v2/actions/charts/payloads.go
+++ tests/v2/actions/charts/payloads.go
@@ -111,3 +111,27 @@ func newChartUpgrade(chartName, releaseName, version, clusterID, clusterName, ur
 
 	return &chartUpgrade
 }
+
+// newExtensionsInstallAction is a private constructor that creates a chart install with given chart values that can be used for chart install action.
+func newExtensionsInstall(name, version string, chartValues map[string]interface{}) *types.ChartInstall {
+	chartInstall := types.ChartInstall{
+		ChartName:   name,
+		ReleaseName: name,
+		Version:     version,
+		Values:      nil,
+	}
+
+	for k, v := range chartValues {
+		chartInstall.Values[k] = v
+	}
+
+	return &chartInstall
+}
+
+// newExtensionsInstallAction is a private constructor that creates a payload for extensions install action chartInstalls and extensions namespace.
+func newExtensionsInstallAction(namespace string, chartInstalls []types.ChartInstall) *types.ChartInstallAction {
+	return &types.ChartInstallAction{
+		Namespace: namespace,
+		Charts:    chartInstalls,
+	}
+}
