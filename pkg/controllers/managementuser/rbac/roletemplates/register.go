1f129f7d2d8e846566a0b9b8b84239fef7d77c27
Jonathan Crowther <jonathan.crowther@suse.com>
2024-10-16 16:08:08 -0400

    Add PRTB Downstream Handler
    ghstack-source-id: 89479601db08b77f3efaaa52f24e89a69063aeb6
    Pull Request resolved: https://github.com/rancher/rancher/pull/47572

--- pkg/controllers/managementuser/rbac/roletemplates/register.go
+++ pkg/controllers/managementuser/rbac/roletemplates/register.go
@@ -13,6 +13,10 @@ func Register(ctx context.Context, workload *config.UserContext) {
 	management.Wrangler.Mgmt.ClusterRoleTemplateBinding().OnChange(ctx, "cluster-crtb-change-handler", c.OnChange)
 	management.Wrangler.Mgmt.ClusterRoleTemplateBinding().OnRemove(ctx, "cluster-crtb-remove-handler", c.OnRemove)
 
+	p := newPRTBHandler(workload)
+	management.Wrangler.Mgmt.ProjectRoleTemplateBinding().OnChange(ctx, "cluster-prtb-change-handler", p.OnChange)
+	management.Wrangler.Mgmt.ProjectRoleTemplateBinding().OnRemove(ctx, "cluster-prtb-remove-handler", p.OnRemove)
+
 	rth := newRoleTemplateHandler(workload)
 	management.Wrangler.Mgmt.RoleTemplate().OnChange(ctx, "cluster-roletemplate-change-handler", rth.OnChange)
 	management.Wrangler.Mgmt.RoleTemplate().OnRemove(ctx, "cluster-roletemplate-remove-handler", rth.OnRemove)
