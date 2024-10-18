c7fb5efa64d78ada8ad07a74c3101e65e046d7a3
Anupama <38144301+anupama2501@users.noreply.github.com>
2024-10-18 13:51:10 -0700

    Stack state p0 cases automation

--- tests/v2/actions/charts/charts.go
+++ tests/v2/actions/charts/charts.go
@@ -8,9 +8,10 @@ const (
 	// defaultRegistrySettingID is a private constant string that contains the ID of system default registry setting.
 	defaultRegistrySettingID = "system-default-registry"
 	// serverURLSettingID is a private constant string that contains the ID of server URL setting.
-	serverURLSettingID = "server-url"
-	rancherChartsName  = "rancher-charts"
-	active             = "active"
+	serverURLSettingID   = "server-url"
+	rancherChartsName    = "rancher-charts"
+	rancherPartnerCharts = "rancher-partner-charts"
+	active               = "active"
 )
 
 // InstallOptions is a struct of the required options to install a chart.
@@ -21,10 +22,11 @@ type InstallOptions struct {
 }
 
 type ExtensionOptions struct {
-	ChartName string
-	Version string
+	ChartName   string
+	Version     string
 	ReleaseName string
 }
+
 // payloadOpts is a private struct that contains the options for the chart payloads.
 // It is used to avoid passing the same options to different functions while using the chart helpers.
 type payloadOpts struct {
--- tests/v2/validation/observability/stackstate.go
+++ tests/v2/validation/observability/stackstate.go
@@ -1,19 +1,38 @@
 package observability
 
 import (
+	"context"
 	"fmt"
+	"time"
 
 	v1 "github.com/rancher/rancher/pkg/apis/catalog.cattle.io/v1"
-	"github.com/rancher/shepherd/pkg/config"
+	"github.com/rancher/rancher/tests/v2/actions/observability"
+	"github.com/rancher/shepherd/clients/rancher"
+	management "github.com/rancher/shepherd/clients/rancher/generated/management/v3"
+	"github.com/rancher/shepherd/extensions/defaults"
+	"github.com/rancher/shepherd/pkg/wait"
+	log "github.com/sirupsen/logrus"
+	apiextv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	meta "k8s.io/apimachinery/pkg/apis/meta/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	kwait "k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/apimachinery/pkg/watch"
 )
 
 const (
-	rancherUIPlugins           = "rancher-ui-plugins"
-	uiExtensionsRepo           = "https://github.com/rancher/ui-plugin-charts"
-	uiGitBranch                = "main"
-	StackStateCRDConfigFileKey = "stackstateCRDConfigs"
+	rancherUIPlugins        = "rancher-ui-plugins"
+	uiExtensionsRepo        = "https://github.com/rancher/ui-plugin-charts"
+	uiGitBranch             = "main"
+	apiExtenisonsCRD        = "apiextensions.k8s.io.customresourcedefinition"
+	project                 = "management.cattle.io.project"
+	observabilitySteveType  = "configurations.observability.rancher.io"
+	rancherPartnerCharts    = "rancher-partner-charts"
+	systemProject           = "System"
+	localCluster            = "local"
+	stackStateConfigFileKey = "stackstateConfigs"
+	crdGroup                = "observability.rancher.io"
+	stackstateName          = "stackstate"
 )
 
 var (
@@ -28,27 +47,177 @@ var (
 	}
 )
 
-// Add comment
-type StackstateCRD struct {
-	ServiceToken string `json:"serviceToken" yaml:"serviceToken"`
-	Url          string `json:"url" yaml:"url"`
+func addExtensionsRepo(client *rancher.Client) error {
+	log.Info("Adding ui extensions repo to rancher chart repositories in the local cluster.")
+	repoObject, err := client.Catalog.ClusterRepos().Create(context.TODO(), &clusterRepoObj, meta.CreateOptions{})
+	if err != nil {
+		return err
+	}
+	client.Session.RegisterCleanupFunc(func() error {
+		err := client.Catalog.ClusterRepos().Delete(context.TODO(), repoObject.Name, meta.DeleteOptions{})
+		if err != nil {
+			return err
+		}
+
+		watchAppInterface, err := client.Catalog.ClusterRepos().Watch(context.TODO(), metav1.ListOptions{
+			FieldSelector:  "metadata.name=" + repoObject.Name,
+			TimeoutSeconds: &defaults.WatchTimeoutSeconds,
+		})
+		if err != nil {
+			return err
+		}
+
+		err = wait.WatchWait(watchAppInterface, func(event watch.Event) (ready bool, err error) {
+			if event.Type == watch.Error {
+				return false, fmt.Errorf("there was an error deleting the cluster repo")
+			} else if event.Type == watch.Deleted {
+				return true, nil
+			}
+			return false, nil
+		})
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
+
+	watchAppInterface, err := client.Catalog.ClusterRepos().Watch(context.TODO(), meta.ListOptions{
+		FieldSelector:  "metadata.name=" + clusterRepoObj.Name,
+		TimeoutSeconds: &defaults.WatchTimeoutSeconds,
+	})
+	if err != nil {
+		return err
+	}
+	err = wait.WatchWait(watchAppInterface, func(event watch.Event) (ready bool, err error) {
+		repo := event.Object.(*v1.ClusterRepo)
+
+		state := repo.Status.Conditions
+		for _, condition := range state {
+			if condition.Type == string(v1.RepoDownloaded) && condition.Status == "True" {
+				return true, nil
+			}
+		}
+		return false, nil
+	})
+
+	return err
 }
 
+func installStackstateCRD(client *rancher.Client) error {
 
-// Add comment
-func NewStackstateCRDConfig(namespace string) unstructured.Unstructured {
-	var stackstateCRDConfigs StackstateCRD
+	stackstateCRDConfig := newStackstateCRDConfig()
+	crd, err := client.Steve.SteveType(apiExtenisonsCRD).Create(stackstateCRDConfig)
+	if err != nil {
+		return err
+	}
 
-	fmt.Printf("Loading configuration from: %s\n", StackStateCRDConfigFileKey)
+	client.Session.RegisterCleanupFunc(func() error {
+		err := client.Steve.SteveType(apiExtenisonsCRD).Delete(crd)
+		if err != nil {
+			return err
+		}
 
-	config.LoadConfig(StackStateCRDConfigFileKey, &stackstateCRDConfigs)
+		err = kwait.PollUntilContextTimeout(context.TODO(), 500*time.Millisecond, defaults.ThirtyMinuteTimeout, true, func(ctx context.Context) (done bool, err error) {
+			_, err = client.Steve.SteveType(apiExtenisonsCRD).ByID(crd.ID)
+			if err != nil {
+				return false, nil
+			}
+			return done, nil
+		})
+		if err != nil {
+			return err
+		}
+		return nil
+	})
 
-	crdConfig := unstructured.Unstructured{}
-	crdConfig.Object["url"] = 	stackstateCRDConfigs.Url
-	crdConfig.Object["serviceToken"] = stackstateCRDConfigs.ServiceToken
-	crdConfig.SetNamespace(namespace)
+	err = kwait.PollUntilContextTimeout(context.TODO(), 500*time.Millisecond, defaults.ThirtyMinuteTimeout, true, func(ctx context.Context) (done bool, err error) {
+		resp, err := client.Steve.SteveType(apiExtenisonsCRD).ByID(observabilitySteveType)
+		if err != nil {
+			return false, err
+		}
 
+		if resp.ObjectMeta.State.Name == "active" {
+			return true, nil
+		}
+		return false, nil
+	})
 
+	return err
+
+}
+
+func newStackstateConfiguration(namespace string, stackstateCRDConfig observability.StackStateConfigs) *unstructured.Unstructured {
+
+	crdConfig := &unstructured.Unstructured{
+		Object: map[string]interface{}{
+			"metadata": map[string]interface{}{
+				"name":      stackstateName,
+				"namespace": namespace,
+			},
+			"spec": map[string]interface{}{
+				"url":          stackstateCRDConfig.Url,
+				"serviceToken": stackstateCRDConfig.ServiceToken,
+			},
+		},
+	}
 
 	return crdConfig
+}
+
+func newStackstateCRDConfig() apiextv1.CustomResourceDefinition {
+	return apiextv1.CustomResourceDefinition{
+		TypeMeta:   metav1.TypeMeta{Kind: "CustomResourceDefinition", APIVersion: "apiextensions.k8s.io/v1"},
+		ObjectMeta: metav1.ObjectMeta{Name: observabilitySteveType},
+		Spec: apiextv1.CustomResourceDefinitionSpec{
+			Group: crdGroup,
+			Versions: []apiextv1.CustomResourceDefinitionVersion{
+				0: {Name: "v1beta1",
+					Served:  true,
+					Storage: true,
+					Schema: &apiextv1.CustomResourceValidation{
+						OpenAPIV3Schema: &apiextv1.JSONSchemaProps{
+							Type: "object",
+						},
+					},
+				},
+			},
+			Names: apiextv1.CustomResourceDefinitionNames{
+				Plural:   "configurations",
+				Singular: "configuration",
+				Kind:     "Configuration",
+				ListKind: "ConfigurationList",
+			},
+			Scope: "Namespaced",
+		},
+	}
+
+}
+
+func installNodeDriver(client *rancher.Client, whitelistDomains []string) error {
+
+	nodedriver := &management.NodeDriver{
+		Name:             stackstateName,
+		Active:           true,
+		WhitelistDomains: whitelistDomains,
+	}
+
+	stackstateNodeDriver, err := client.Management.NodeDriver.Create(nodedriver)
+	if err != nil {
+		return err
+	}
+
+	err = kwait.PollUntilContextTimeout(context.TODO(), 500*time.Millisecond, defaults.ThirtyMinuteTimeout, true, func(ctx context.Context) (done bool, err error) {
+		resp, err := client.Management.NodeDriver.ByID(stackstateNodeDriver.ID)
+		if err != nil {
+			return false, err
+		}
+
+		if resp.State == "downloading" {
+			return true, nil
+		}
+		return false, nil
+	})
+
+	return err
 }
