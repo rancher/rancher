da81b4da6353974d7c68dcdc0933c95647d57a1e
Kinara Shah <kinara@rancher.com>
2024-10-18 13:29:07 -0700

    update cluster agent manifest

--- pkg/capr/common.go
+++ pkg/capr/common.go
@@ -715,3 +715,30 @@ func FormatWindowsEnvVar(envVar corev1.EnvVar, isPlanVariable bool) string {
 
 	return fmt.Sprintf("%s=%s", envVar.Name, envVar.Value)
 }
+
+// GetTaints returns a slice of taints for the machine in question
+func GetTaints(existingTaints, runtime string, isControlPlane, isEtcd, isWorker bool) (result []corev1.Taint, _ error) {
+	if existingTaints != "" {
+		if err := json.Unmarshal([]byte(existingTaints), &result); err != nil {
+			return result, err
+		}
+	}
+
+	if !isWorker {
+		// k3s charts do not have correct tolerations when the master node is both controlplane and etcd
+		if isEtcd && (runtime != RuntimeK3S || !isControlPlane) {
+			result = append(result, corev1.Taint{
+				Key:    "node-role.kubernetes.io/etcd",
+				Effect: corev1.TaintEffectNoExecute,
+			})
+		}
+		if isControlPlane {
+			result = append(result, corev1.Taint{
+				Key:    "node-role.kubernetes.io/control-plane",
+				Effect: corev1.TaintEffectNoSchedule,
+			})
+		}
+	}
+
+	return
+}
