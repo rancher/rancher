ebed7fd63f11782593f5cd2ccf03cc77dc0a7671
Anupama <38144301+anupama2501@users.noreply.github.com>
2024-10-24 14:37:02 -0700

    Additional updates based on the offline discussion
    diff --git a/tests/v2/actions/charts/charts.go b/tests/v2/actions/charts/charts.go
    index 6b158b604..b3150794c 100644
    --- a/tests/v2/actions/charts/charts.go
    +++ b/tests/v2/actions/charts/charts.go
    @@ -21,12 +21,6 @@ type InstallOptions struct {
     	ProjectID string
     }
    
    -type ExtensionOptions struct {
    -	ChartName   string
    -	Version     string
    -	ReleaseName string
    -}
    -
     // payloadOpts is a private struct that contains the options for the chart payloads.
     // It is used to avoid passing the same options to different functions while using the chart helpers.
     type payloadOpts struct {
    diff --git a/tests/v2/actions/charts/payloads.go b/tests/v2/actions/charts/payloads.go
    index eb23e6abe..79dfdc9f2 100644
    --- a/tests/v2/actions/charts/payloads.go
    +++ b/tests/v2/actions/charts/payloads.go
    @@ -111,19 +111,3 @@ func newChartUpgrade(chartName, releaseName, version, clusterID, clusterName, ur
    
     	return &chartUpgrade
     }
    -
    -// newExtensionsInstall is a private constructor that creates a chart install with given chart values that can be used for chart install action.
    -func newExtensionsInstall(name, version string, chartValues map[string]interface{}) *types.ChartInstall {
    -	chartInstall := types.ChartInstall{
    -		ChartName:   name,
    -		ReleaseName: name,
    -		Version:     version,
    -		Values:      nil,
    -	}
    -
    -	for k, v := range chartValues {
    -		chartInstall.Values[k] = v
    -	}
    -
    -	return &chartInstall
    -}

--- tests/v2/actions/charts/payloads.go
+++ tests/v2/actions/charts/payloads.go
@@ -111,19 +111,3 @@ func newChartUpgrade(chartName, releaseName, version, clusterID, clusterName, ur
 
 	return &chartUpgrade
 }
-
-// newExtensionsInstall is a private constructor that creates a chart install with given chart values that can be used for chart install action.
-func newExtensionsInstall(name, version string, chartValues map[string]interface{}) *types.ChartInstall {
-	chartInstall := types.ChartInstall{
-		ChartName:   name,
-		ReleaseName: name,
-		Version:     version,
-		Values:      nil,
-	}
-
-	for k, v := range chartValues {
-		chartInstall.Values[k] = v
-	}
-
-	return &chartInstall
-}
