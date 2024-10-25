b0bca7f11e04bdabb2e7cc20148b9cdf71000c15
Andreas Kupries <andreas.kupries@suse.com>
2024-10-25 10:54:19 +0200

    note: SECURITY - set token permissions for/on builtin role "user base"

--- pkg/data/management/role_data.go
+++ pkg/data/management/role_data.go
@@ -79,12 +79,20 @@ func addRoles(wrangler *wrangler.Context, management *config.ManagementContext) 
 
 	userRole := addUserRules(rb.addRole("User", "user"))
 	userRole.
+		// standard permissions for regular users, on their tokens
+		// BEWARE: The ext token store applies additional restrictions.
+		// In other words, each User can see and manipulate only their own tokens, and no other.
+		addRule().apiGroups("ext.cattle.io").resources("tokens").verbs("get", "list", "watch", "create", "delete", "update", "patch").
 		addRule().apiGroups("catalog.cattle.io").resources("clusterrepos").verbs("get", "list", "watch").
 		addRule().apiGroups("management.cattle.io").resources("podsecurityadmissionconfigurationtemplates").verbs("get", "list", "watch")
 
 	userRole.addNamespacedRule("cattle-global-data").addRule().apiGroups("").resources("secrets").verbs("create")
 
 	rb.addRole("User Base", "user-base").
+		// standard permissions for regular users, on their tokens
+		// BEWARE: The ext token store applies additional restrictions.
+		// In other words, each User can see and manipulate only their own tokens, and no other.
+		addRule().apiGroups("ext.cattle.io").resources("tokens").verbs("get", "list", "watch", "create", "delete", "update", "patch").
 		addRule().apiGroups("management.cattle.io").resources("preferences").verbs("*").
 		addRule().apiGroups("management.cattle.io").resources("settings").verbs("get", "list", "watch").
 		addRule().apiGroups("management.cattle.io").resources("features").verbs("get", "list", "watch").
