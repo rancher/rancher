74e9a3ed9600e560d32ce751aba39ac5153e92ac
raul <raul.cabello@suse.com>
2024-10-25 17:08:37 +0200

    fix imports

--- pkg/controllers/managementuser/rbac/globalrolebinding_handler_test.go
+++ pkg/controllers/managementuser/rbac/globalrolebinding_handler_test.go
@@ -1,11 +1,12 @@
 package rbac
 
 import (
+	"testing"
+	"time"
+
 	"github.com/rancher/rancher/pkg/controllers/status"
 	"github.com/rancher/wrangler/v3/pkg/generic/fake"
 	"go.uber.org/mock/gomock"
-	"testing"
-	"time"
 
 	"github.com/rancher/rancher/pkg/generated/norman/management.cattle.io/v3/fakes"
 	rbacFakes "github.com/rancher/rancher/pkg/generated/norman/rbac.authorization.k8s.io/v1/fakes"
