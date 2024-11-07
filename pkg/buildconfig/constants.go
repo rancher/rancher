4eb181a2447352a56fc7a785cdb5d89dd634317c
Gabriel Bueno <gabriel.bueno@suse.com>
2024-11-07 12:43:02 -0300

    csp-adapter v5.0.1-rc1

--- pkg/buildconfig/constants.go
+++ pkg/buildconfig/constants.go
@@ -3,7 +3,7 @@
 package buildconfig
 
 const (
-	CspAdapterMinVersion    = "105.0.0+up5.0.0"
+	CspAdapterMinVersion    = "105.0.0+up5.0.1-rc1"
 	DefaultShellVersion     = "rancher/shell:v0.3.0-rc.2"
 	FleetVersion            = "105.0.0+up0.11.0-rc.2"
 	ProvisioningCAPIVersion = "105.0.0+up0.4.0"
