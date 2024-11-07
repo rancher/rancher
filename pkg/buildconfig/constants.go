7c6dfd3073a8d60eec0d5bc0d97e5c478c433e51
Tom Lebreux <tom.lebreux@suse.com>
2024-11-07 12:28:13 -0500

    Bump rancher-webhook to v0.6.1-rc.12

--- pkg/buildconfig/constants.go
+++ pkg/buildconfig/constants.go
@@ -7,5 +7,5 @@ const (
 	DefaultShellVersion     = "rancher/shell:v0.3.0-rc.2"
 	FleetVersion            = "105.0.0+up0.11.0-rc.2"
 	ProvisioningCAPIVersion = "105.0.0+up0.4.0"
-	WebhookVersion          = "105.0.0+up0.6.1-rc.9"
+	WebhookVersion          = "105.0.0+up0.6.1-rc.12"
 )
