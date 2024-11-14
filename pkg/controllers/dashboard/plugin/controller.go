a801a8811f5670da0ad390ea1178c0adcde828b2
Diogo Souza <diogo.souza@suse.com>
2024-11-14 19:13:03 -0300

    adding exponential retry

--- pkg/controllers/dashboard/plugin/controller.go
+++ pkg/controllers/dashboard/plugin/controller.go
@@ -3,7 +3,12 @@ package plugin
 import (
 	"context"
 	"fmt"
+	"hash/maphash"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"math"
+	"math/rand"
 	"strconv"
+	"time"
 
 	"github.com/pkg/errors"
 	"github.com/rancher/rancher/pkg/namespace"
@@ -15,6 +20,14 @@ import (
 	plugincontroller "github.com/rancher/rancher/pkg/generated/controllers/catalog.cattle.io/v1"
 	"k8s.io/apimachinery/pkg/labels"
 )
+
+const (
+	minWait    = 10 * time.Second
+	maxWait    = 30 * time.Minute
+	maxRetries = 10
+)
+
+var timeNow = time.Now
 
 func Register(
 	ctx context.Context,
@@ -35,6 +48,9 @@ type handler struct {
 }
 
 func (h *handler) OnPluginChange(key string, plugin *v1.UIPlugin) (*v1.UIPlugin, error) {
+	if plugin != nil && plugin.Status.RetryAt.After(timeNow()) {
+		return nil, nil
+	}
 	cachedPlugins, err := h.pluginCache.List(h.systemNamespace, labels.Everything())
 	if err != nil {
 		return plugin, fmt.Errorf("failed to list plugins from cache: %w", err)
@@ -109,6 +125,10 @@ func (h *handler) OnPluginChange(key string, plugin *v1.UIPlugin) (*v1.UIPlugin,
 	if err != nil {
 		err = fmt.Errorf("failed to sync filesystem cache with controller cache: %w", err)
 		logrus.Errorf(err.Error())
+		backoff := calculateBackoff(plugin.Status.RetryNumber)
+		plugin.Status.RetryNumber++
+		plugin.Status.RetryAt = metav1.NewTime(time.Now().Add(backoff))
+		h.plugin.EnqueueAfter(plugin.Namespace, plugin.Name, backoff)
 		return plugin, err
 	}
 	if !plugin.Spec.Plugin.NoCache {
@@ -118,4 +138,24 @@ func (h *handler) OnPluginChange(key string, plugin *v1.UIPlugin) (*v1.UIPlugin,
 		plugin.Status.Error = ""
 	}
 	return plugin, nil
+}
+
+// calculateBackoff gets the amount of time to wait for the next call.
+// Reference: https://github.com/oras-project/oras-go/blob/main/registry/remote/retry/policy.go#L95
+func calculateBackoff(numberOfRetries int) time.Duration {
+	if numberOfRetries > maxRetries {
+		return maxWait
+	}
+	var h maphash.Hash
+	h.SetSeed(maphash.MakeSeed())
+	rand := rand.New(rand.NewSource(int64(h.Sum64())))
+	temp := float64(minWait) * math.Pow(2, float64(numberOfRetries))
+	backoff := time.Duration(temp*(1-0.2)) + time.Duration(rand.Int63n(int64(2*0.2*temp)))
+	if backoff < minWait {
+		return minWait
+	}
+	if backoff > maxWait {
+		return maxWait
+	}
+	return backoff
 }
