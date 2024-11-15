dda91c61b9017133df3ef4c8b72e1ac4eba46a6e
Anupama <38144301+anupama2501@users.noreply.github.com>
2024-11-15 13:11:12 -0800

    Mapping k8s versions in the config.yaml

--- tests/v2/validation/pipeline/scripts/rancher_k8s_version.sh
+++ tests/v2/validation/pipeline/scripts/rancher_k8s_version.sh
@@ -3,14 +3,22 @@
 latest_rancher_version=$(curl -s https://prime.ribs.rancher.io/index.html | grep  "release-v[0-9]*" | sort -V | tail -n 1 |  sed -E 's/.*release-(v[0-9]+\.[0-9]+\.[0-9]+).*/\1/')
 echo "Latest Rancher Version: $latest_rancher_version"
 
-k8s_versions=$(curl -s "https://prime.ribs.rancher.io/rancher/${latest_rancher_version}/rancher-images.txt" | grep "rancher/hardened-kubernetes:" | awk -F':' '{print $2}'  | awk -F '-build' '{print $1}' | sort -V | awk -F '.' '
+rke2_k8s_versions=$(curl -s "https://prime.ribs.rancher.io/rancher/${latest_rancher_version}/rancher-images.txt" | grep "rancher/hardened-kubernetes:" | awk -F':' '{print $2}'  | awk -F '-build' '{print $1}' | sort -V | awk -F '.' '
         {versions[$1"."$2] = $0} 
         END {
             for (v in versions) print versions[v]""
-        }' | sort -V | tail -n 2)
+        }' | sed 's/-rke2/+rke2/'| sort -V | tail -n 2)
 
+k3s_k8s_versions=$(curl -s "https://prime.ribs.rancher.io/rancher/${latest_rancher_version}/rancher-images.txt" | grep "rancher/k3s-upgrade:" | awk -F':' '{print $2}'  | awk -F '-build' '{print $1}' | sort -V | awk -F '.' '
+        {versions[$1"."$2] = $0} 
+        END {
+            for (v in versions) print versions[v]""
+        }' | sed 's/-k3s/+k3s/' | sort -V | tail -n 2)
+
+rke1_k8s_versions=$(curl -s "https://prime.ribs.rancher.io/rancher/${latest_rancher_version}/rancher-rke-k8s-versions.txt"  | sort -V | tail -n 2)
 
 # Output the results
 echo "Latest Kubernetes Versions: "
-echo "$k8s_versions" 
+echo -e "$rke2_k8s_versions\n$k3s_k8s_versions\n$rke1_k8s_versions"
+
 
