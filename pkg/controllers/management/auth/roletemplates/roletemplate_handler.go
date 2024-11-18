62819a87ef89ceb22f04e0dc1a2a4d79ba1a78df
Jonathan Crowther <jonathan.crowther@suse.com>
2024-11-18 15:19:00 -0500

    Finish up roletemplate handler

--- pkg/controllers/management/auth/roletemplates/roletemplate_handler.go
+++ pkg/controllers/management/auth/roletemplates/roletemplate_handler.go
@@ -9,48 +9,24 @@ import (
 	crbacv1 "github.com/rancher/wrangler/v3/pkg/generated/controllers/rbac/v1"
 	"github.com/rancher/wrangler/v3/pkg/slice"
 	v1 "k8s.io/api/rbac/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 var (
-	commonClusterAndProjectMgmtPlaneResources = map[string]bool{
-		"catalogtemplates":        true,
-		"catalogtemplateversions": true,
-	}
 	clusterManagementPlaneResources = map[string]string{
 		"clusterscans":                "management.cattle.io",
-		"catalogtemplates":            "management.cattle.io",
-		"catalogtemplateversions":     "management.cattle.io",
-		"clusteralertrules":           "management.cattle.io",
-		"clusteralertgroups":          "management.cattle.io",
-		"clustercatalogs":             "management.cattle.io",
-		"clusterloggings":             "management.cattle.io",
-		"clustermonitorgraphs":        "management.cattle.io",
 		"clusterregistrationtokens":   "management.cattle.io",
 		"clusterroletemplatebindings": "management.cattle.io",
 		"etcdbackups":                 "management.cattle.io",
 		"nodes":                       "management.cattle.io",
 		"nodepools":                   "management.cattle.io",
-		"notifiers":                   "management.cattle.io",
 		"projects":                    "management.cattle.io",
 		"etcdsnapshots":               "rke.cattle.io",
 	}
-	prtbClusterManagementPlaneResources = map[string]string{
-		"notifiers":               "management.cattle.io",
-		"clustercatalogs":         "management.cattle.io",
-		"catalogtemplates":        "management.cattle.io",
-		"catalogtemplateversions": "management.cattle.io",
-	}
 	projectManagementPlaneResources = map[string]string{
 		"apps":                        "project.cattle.io",
-		"apprevisions":                "project.cattle.io",
-		"catalogtemplates":            "management.cattle.io",
-		"catalogtemplateversions":     "management.cattle.io",
 		"sourcecodeproviderconfigs":   "project.cattle.io",
-		"projectloggings":             "management.cattle.io",
-		"projectalertrules":           "management.cattle.io",
-		"projectalertgroups":          "management.cattle.io",
-		"projectcatalogs":             "management.cattle.io",
-		"projectmonitorgraphs":        "management.cattle.io",
 		"projectroletemplatebindings": "management.cattle.io",
 		"secrets":                     "",
 	}
@@ -66,101 +42,92 @@ func newRoleTemplateHandler(w *wrangler.Context) *roleTemplateHandler {
 	}
 }
 
-// OnChange creates 4 objects. A cluster role to give access to project or cluster scoped objects and an accompanying aggregating cluster role
-// It also creates a cluster role for management plane privileges and an accompanying aggregating cluster role
+// OnChange creates all management plane cluster roles that will be needed. If there are no management plane rules in the role template, no cluster roles will be created.
 func (r *roleTemplateHandler) OnChange(key string, rt *v3.RoleTemplate) (*v3.RoleTemplate, error) {
 	if rt == nil || rt.DeletionTimestamp != nil {
 		return nil, nil
 	}
 
-	// TODO Need more investigation to make sure these rules are correct
-	// I might be able to skip some creation if there are no special rules and no inherited RTs
-	var managementPlaneRules, scopedPrivileges []v1.PolicyRule
+	rules, err := r.getAllRules(rt)
+	if err != nil {
+		return nil, err
+	}
+
+	var clusterScopedPrivileges, projectScopedPrivileges []v1.PolicyRule
 	if rt.Context == "project" {
-		managementPlaneRules = getManagementPlaneRules(rt, prtbClusterManagementPlaneResources)
-		scopedPrivileges = getMatchingRules(rt, projectManagementPlaneResources)
+		projectScopedPrivileges = getManagementPlaneRules(rules, projectManagementPlaneResources)
 	} else if rt.Context == "cluster" {
-		managementPlaneRules = getManagementPlaneRules(rt, clusterManagementPlaneResources)
-		scopedPrivileges = getMatchingRules(rt, projectManagementPlaneResources)
+		clusterScopedPrivileges = getManagementPlaneRules(rules, clusterManagementPlaneResources)
+		projectScopedPrivileges = getManagementPlaneRules(rules, projectManagementPlaneResources)
 	}
 
-	// TODO pick a better naming convention
-	cr := rbac.BuildClusterRole(rt.Name, rt.Name, managementPlaneRules)
-	if err := rbac.CreateOrUpdateResource(cr, r.crClient, rbac.AreClusterRolesSame); err != nil {
-		return nil, err
-	}
-	acr := rbac.BuildAggregatingClusterRole(rt, rbac.ClusterManagementPlaneClusterRoleNameFor)
-	if err := rbac.CreateOrUpdateResource(acr, r.crClient, rbac.AreClusterRolesSame); err != nil {
-		return nil, err
+	var clusterRoles []*v1.ClusterRole
+	rtName := rbac.ClusterRoleNameFor(rt.Name)
+
+	if clusterScopedPrivileges != nil {
+		clusterRoles = append(clusterRoles,
+			rbac.BuildClusterRole(rbac.ClusterManagementPlaneClusterRoleNameFor(rtName), rtName, clusterScopedPrivileges),
+			rbac.BuildAggregatingClusterRole(rt, rbac.ClusterManagementPlaneClusterRoleNameFor))
 	}
 
-	// TODO pick a better naming convention
-	cr = rbac.BuildClusterRole(rt.Name, rt.Name, scopedPrivileges)
-	if err := rbac.CreateOrUpdateResource(cr, r.crClient, rbac.AreClusterRolesSame); err != nil {
-		return nil, err
+	if projectScopedPrivileges != nil {
+		clusterRoles = append(clusterRoles,
+			rbac.BuildClusterRole(rbac.ProjectManagementPlaneClusterRoleNameFor(rtName), rtName, projectScopedPrivileges),
+			rbac.BuildAggregatingClusterRole(rt, rbac.ProjectManagementPlaneClusterRoleNameFor))
 	}
-	acr = rbac.BuildAggregatingClusterRole(rt, rbac.ClusterManagementPlaneClusterRoleNameFor)
-	if err := rbac.CreateOrUpdateResource(acr, r.crClient, rbac.AreClusterRolesSame); err != nil {
-		return nil, err
+
+	for _, cr := range clusterRoles {
+		if err := rbac.CreateOrUpdateResource(cr, r.crClient, rbac.AreClusterRolesSame); err != nil {
+			return nil, err
+		}
 	}
 
 	return rt, nil
 }
 
 func (r *roleTemplateHandler) OnRemove(key string, rt *v3.RoleTemplate) (*v3.RoleTemplate, error) {
-	// TODO pick better names
-	managementPlaneCRName := rt.Name
-	managementPlaneACRName := rbac.AggregatedClusterRoleNameFor(managementPlaneCRName)
-	scopedPrivilegesCRName := rt.Name
-	scopedPrivilegesACRName := rbac.AggregatedClusterRoleNameFor(scopedPrivilegesCRName)
+	rtName := rbac.ClusterRoleNameFor(rt.Name)
+	clusterScopedCRName := rbac.ClusterManagementPlaneClusterRoleNameFor(rtName)
+	clusterScopedACRName := rbac.AggregatedClusterRoleNameFor(clusterScopedCRName)
+	projectScopedCRName := rbac.ProjectManagementPlaneClusterRoleNameFor(rtName)
+	projectScopedACRName := rbac.AggregatedClusterRoleNameFor(projectScopedCRName)
 
 	return nil, errors.Join(
-		rbac.DeleteResource(managementPlaneCRName, r.crClient),
-		rbac.DeleteResource(managementPlaneACRName, r.crClient),
-		rbac.DeleteResource(scopedPrivilegesCRName, r.crClient),
-		rbac.DeleteResource(scopedPrivilegesACRName, r.crClient),
+		rbac.DeleteResource(clusterScopedCRName, r.crClient),
+		rbac.DeleteResource(clusterScopedACRName, r.crClient),
+		rbac.DeleteResource(projectScopedCRName, r.crClient),
+		rbac.DeleteResource(projectScopedACRName, r.crClient),
 	)
 }
 
-// TODO getMatchingRules and getManagementPlaneRules try to do the same thing. I need to figure this out more thoroughly
-func getMatchingRules(rt *v3.RoleTemplate, resources map[string]string) []v1.PolicyRule {
-	var matchingRules []v1.PolicyRule
-
-	for resource, apigroup := range resources {
-		// Adding this check, because we want cluster-owners to have access to catalogtemplates/versions of all projects, but no other cluster roles
-		// need to access catalogtemplates of projects they do not belong to
-		if !rt.Administrative && rt.Context == "cluster" && commonClusterAndProjectMgmtPlaneResources[resource] {
-			continue
-		}
-		for _, rule := range rt.Rules {
-			if checkResource(resource, rule) && checkGroup(apigroup, rule) {
-				matchingRules = append(matchingRules, rule)
+// getAllRules collects all policy rules that the RoleTemplate applies. Specifically it searches for external rules
+func (r *roleTemplateHandler) getAllRules(rt *v3.RoleTemplate) ([]v1.PolicyRule, error) {
+	var rules []v1.PolicyRule
+	if rt.External {
+		if rt.ExternalRules != nil {
+			rules = append(rules, rt.ExternalRules...)
+		} else {
+			externalRole, err := r.crClient.Get(rt.Name, metav1.GetOptions{})
+			if err != nil && !apierrors.IsNotFound(err) {
+				// dont error if it doesnt exist
+				return nil, err
+			}
+			if externalRole != nil {
+				rules = externalRole.Rules
 			}
 		}
+	} else {
+		rules = rt.Rules
 	}
-	return matchingRules
+	return rules, nil
 }
 
-func checkResource(resource string, rule v1.PolicyRule) bool {
-	return slice.ContainsString(rule.Resources, resource) || slice.ContainsString(rule.Resources, "*") && len(rule.ResourceNames) == 0
-}
-
-func checkGroup(apiGroup string, rule v1.PolicyRule) bool {
-	return slice.ContainsString(rule.APIGroups, apiGroup) || apiGroup == "*"
-}
-
-func getManagementPlaneRules(rt *v3.RoleTemplate, managementResources map[string]string) []v1.PolicyRule {
-	// TODO handle external rules
-	rules := rt.Rules
-
+// getManagementPlaneRules filters a set of rules based on the map passed in. Used to provide special resources that have cluster/project scope
+func getManagementPlaneRules(rules []v1.PolicyRule, managementResources map[string]string) []v1.PolicyRule {
 	var managementRules []v1.PolicyRule
 	for _, rule := range rules {
-		for _, resource := range rule.Resources {
-			apiGroup, ok := managementResources[resource]
-			if !ok {
-				continue
-			}
-			if slice.ContainsString(rule.APIGroups, apiGroup) {
+		for resource, apiGroup := range managementResources {
+			if ruleContainsManagementPlaneRule(resource, apiGroup, rule) {
 				managementRules = append(managementRules, v1.PolicyRule{
 					Resources: []string{resource},
 					APIGroups: []string{apiGroup},
@@ -170,4 +137,12 @@ func getManagementPlaneRules(rt *v3.RoleTemplate, managementResources map[string
 		}
 	}
 	return managementRules
+}
+
+// ruleContainsManagementPlaneRule takes a rule and checks if it has the resource and apigroup from the management plane rules.
+// If there are ResourceNames specified in the rule, it only applies to specific resources and doesn't count as a management plane rule.
+func ruleContainsManagementPlaneRule(resource, apiGroup string, rule v1.PolicyRule) bool {
+	return len(rule.ResourceNames) != 0 &&
+		slice.ContainsString(rule.Resources, resource) || slice.ContainsString(rule.Resources, v1.ResourceAll) &&
+		slice.ContainsString(rule.APIGroups, apiGroup) || slice.ContainsString(rule.APIGroups, v1.APIGroupAll)
 }
