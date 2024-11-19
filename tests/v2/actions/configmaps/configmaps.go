4ceb0738b3600609fa1190eaf13fcb94dde0d5d0
Leonardo Scalabrini <leonardo.scalabrini@suse.com>
2024-11-19 19:08:38 -0300

    Hack week 24

--- tests/v2/actions/configmaps/configmaps.go
+++ tests/v2/actions/configmaps/configmaps.go
@@ -3,17 +3,17 @@ package configmaps
 import (
 	"fmt"
 
-	"github.com/rancher/shepherd/clients/rancher"
 	"github.com/rancher/rancher/tests/v2/actions/kubeapi/configmaps"
+	"github.com/rancher/shepherd/clients/rancher"
 	namegen "github.com/rancher/shepherd/pkg/namegenerator"
-	coreV1 "k8s.io/api/core/v1"
 	"github.com/rancher/shepherd/pkg/wrangler"
+	coreV1 "k8s.io/api/core/v1"
 )
 
 // CreateConfigmap is a helper to create a configmap using public API.
 func CreateConfigmap(namespace string, client *rancher.Client, data map[string]string, clusterID string) (configMap *coreV1.ConfigMap, err error) {
 	var ctx *wrangler.Context
-	
+
 	if clusterID == "local" {
 		ctx = client.WranglerContext
 	} else {
@@ -22,7 +22,6 @@ func CreateConfigmap(namespace string, client *rancher.Client, data map[string]s
 			return nil, fmt.Errorf("failed to get downstream context: %w", err)
 		}
 	}
-	
 
 	newConfigmap := configmaps.NewConfigmapTemplate(namegen.AppendRandomString("testcm-"), namespace, nil, nil, data)
 	configMap, err = ctx.Core.ConfigMap().Create(&newConfigmap)
