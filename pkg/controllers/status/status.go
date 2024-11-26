c3081546d5763526426c34b0b6b7bf6c8b85d0e5
raul <raul.cabello@suse.com>
2024-11-26 10:20:34 +0100

    fix CRTB update endless loop

--- pkg/controllers/status/status.go
+++ pkg/controllers/status/status.go
@@ -56,10 +56,13 @@ func CompareConditions(s1 []metav1.Condition, s2 []metav1.Condition) bool {
 	for _, c1 := range s1 {
 		found := false
 		for _, c2 := range s2 {
-			found = c1.Type == c2.Type &&
+			if c1.Type == c2.Type &&
 				c1.Status == c2.Status &&
 				c1.Reason == c2.Reason &&
-				c1.Message == c2.Message
+				c1.Message == c2.Message {
+				found = true
+				continue
+			}
 		}
 		if !found {
 			return false
