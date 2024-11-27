455859c38144a7aab3a790f9f4896c9b68d188bb
Kevin McDermott <kevin.mcdermott@suse.com>
2024-11-27 13:08:47 +0000

    Cache mitigation (#47273) (#47982)
    Mitigate the impact of the client cache on detecting secrets.
    
    When we can't find a secret in EnsureSecretForServiceAccount, create a secret and annotate the ServiceAccount that it's for with the secret reference.
    
    If we get a conflict error doing this, then rollback the newly created secret.
    
    Co-authored-by: Alejandro Ruiz <alejandro.ruiz@suse.com>

--- pkg/utils/utils_test.go
+++ pkg/utils/utils_test.go
@@ -2,34 +2,6 @@ package utils
 
 import "testing"
 
-func TestFormatPrefix(t *testing.T) {
-	testStrings := []struct {
-		s    string
-		want string
-	}{
-		{
-			"example", "example-",
-		},
-		{
-			"Test", "test-",
-		},
-		{
-			"another-", "another-",
-		},
-		{
-			"", "",
-		},
-	}
-
-	for _, tt := range testStrings {
-		t.Run(tt.s, func(t *testing.T) {
-			if v := FormatPrefix(tt.s); v != tt.want {
-				t.Errorf("FormatPrefix() got %v, want %v", v, tt.want)
-			}
-		})
-	}
-}
-
 func TestIsPlainIPV6(t *testing.T) {
 	testCases := []struct {
 		ip       string
