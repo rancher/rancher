d17dafe80bcd72ec34010670bcc69a1dc31c5c34
Leonardo Scalabrini <leonardo.scalabrini@suse.com>
2024-12-02 18:26:54 -0300

    <main> Investigate recurring run failures in Workloads tests

--- tests/v2/validation/clustertemplates/rbac/clustertemplate_test.go
+++ tests/v2/validation/clustertemplates/rbac/clustertemplate_test.go
@@ -58,6 +58,7 @@ func (ct *ClusterTemplateRKE1RBACTestSuite) SetupSuite() {
 		ct.provisioningConfig.CNIs = []string{clustertemplates.CniCalico}
 	}
 
+	provisioning.DisableUpdateConfig(ct.client)
 }
 
 func (ct *ClusterTemplateRKE1RBACTestSuite) TestClusterTemplateEnforcementForStandardUser() {
