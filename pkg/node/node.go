0fd4befcfe1c4bef200ec71f76e411b8d88c3b97
vardhaman22 <vardhaman.surana@suse.com>
2024-12-07 09:15:13 +0530

    replace delete-local-data flag with delete-empty-dir

--- pkg/node/node.go
+++ pkg/node/node.go
@@ -278,7 +278,7 @@ func GetDrainFlags(node *v3.Node) []string {
 		ignoreDaemonSets = *input.IgnoreDaemonSets
 	}
 	return []string{
-		fmt.Sprintf("--delete-local-data=%v", input.DeleteLocalData),
+		fmt.Sprintf("--delete-emptydir-data=%v", input.DeleteLocalData),
 		fmt.Sprintf("--force=%v", input.Force),
 		fmt.Sprintf("--grace-period=%v", input.GracePeriod),
 		fmt.Sprintf("--ignore-daemonsets=%v", ignoreDaemonSets),
