da696099192bd66155b4cd26c54fec559c35988f
vivek-infracloud <vivek.shilimkar@infracloud.io>
2024-12-10 13:55:08 +0530

    update cis benchmark chart automation

--- tests/v2/validation/provisioning/resources/cisbenchmark/cisbenchmark.go
+++ tests/v2/validation/provisioning/resources/cisbenchmark/cisbenchmark.go
@@ -1,11 +1,15 @@
 package charts
 
 import (
+	"encoding/json"
+	"errors"
+	"strings"
 	"time"
 
 	cis "github.com/rancher/cis-operator/pkg/apis/cis.cattle.io/v1"
 	"github.com/rancher/rancher/tests/v2/actions/charts"
 	"github.com/rancher/shepherd/clients/rancher"
+	v1 "github.com/rancher/shepherd/clients/rancher/v1"
 	extensionscharts "github.com/rancher/shepherd/extensions/charts"
 	"github.com/rancher/shepherd/extensions/defaults"
 	namegen "github.com/rancher/shepherd/pkg/namegenerator"
@@ -19,9 +23,36 @@ const (
 	System = "System"
 	pass   = "pass"
 	scan   = "scan"
+	fail   = "fail"
 
 	cisBenchmarkSteveType = "cis.cattle.io.clusterscan"
+	clusterScanReportType = "cis.cattle.io.clusterscanreport"
 )
+
+// CisReport is the report structure stored as report json in cluster scan report spec.
+type CisReport struct {
+	Total         int
+	Pass          int
+	Fail          int
+	Skip          int
+	Warn          int
+	NotApplicable int
+	Results       []*Group `json:"results"`
+}
+
+// Group is the result structure stored as report json in Results of CisReport
+type Group struct {
+	ID     string      `yaml:"id" json:"id"`
+	Text   string      `json:"description"`
+	Checks []*CisCheck `json:"checks"`
+}
+
+// CisCheck is the ID, Description and State structure of individual test in cluster scan.
+type CisCheck struct {
+	Id          string
+	Description string
+	State       string
+}
 
 // SetupCISBenchmarkChart installs the CIS Benchmark chart and waits for all resources to be ready.
 func SetupCISBenchmarkChart(client *rancher.Client, projectClusterID string, chartInstallOptions *charts.InstallOptions, benchmarkNamespace string) error {
@@ -55,7 +86,7 @@ func SetupCISBenchmarkChart(client *rancher.Client, projectClusterID string, cha
 }
 
 // RunCISScan runs the CIS Benchmark scan with the specified profile name.
-func RunCISScan(client *rancher.Client, projectClusterID, scanProfileName string) error {
+func RunCISScan(client *rancher.Client, projectClusterID, scanProfileName string, printFailedChecks bool) error {
 	logrus.Infof("Running CIS Benchmark scan: %s", scanProfileName)
 
 	cisScan := cis.ClusterScan{
@@ -96,5 +127,59 @@ func RunCISScan(client *rancher.Client, projectClusterID, scanProfileName string
 
 	logrus.Infof("CIS Benchmark scan passed!")
 
+	if printFailedChecks {
+		logrus.Infof("Checking cluster scan report for failed checks")
+		err = checkScanReport(steveclient, scan.Name)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func checkScanReport(steveClient *v1.Client, scanName string) error {
+	scanReportList, err := steveClient.SteveType(clusterScanReportType).List(nil)
+	if err != nil {
+		return err
+	}
+
+	scanReportIdx := -1
+
+	for idx, scanReport := range scanReportList.Data {
+		if strings.Contains(scanReport.Name, scanName) {
+			scanReportIdx = idx
+			break
+		}
+	}
+
+	if scanReportIdx < 0 {
+		return errors.New("scan report not found")
+	}
+
+	scanReport := &cis.ClusterScanReport{}
+	scanReportSpec := scanReportList.Data[scanReportIdx]
+	err = v1.ConvertToK8sType(scanReportSpec, scanReport)
+	if err != nil {
+		return err
+	}
+
+	reportData := &CisReport{}
+	err = json.Unmarshal([]byte(scanReport.Spec.ReportJSON), &reportData)
+	if err != nil {
+		return err
+	}
+
+	logrus.Infof("Out of total number of %d scans, %d scans passed, %d scans were skipped and %d scans failed", reportData.Total, reportData.Pass, reportData.Skip, reportData.Fail)
+	for _, group := range reportData.Results {
+		for _, check := range group.Checks {
+			if check.State == fail {
+				logrus.Infof("check failed: id: %s state: %s description: %s ", check.Id, check.State, check.Description)
+			}
+		}
+	}
+	if reportData.Fail != 0 {
+		return errors.New("cluster scan failed")
+	}
 	return nil
 }
