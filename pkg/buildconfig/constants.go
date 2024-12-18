45a86efd808e077eee3a503e83ada0c804246a38
nflynt <nicholas.flynt@suse.com>
2024-12-18 14:15:56 -0500

    unRC webhook to 0.4.14 (#48489)

--- pkg/buildconfig/constants.go
+++ pkg/buildconfig/constants.go
@@ -6,5 +6,5 @@ const (
 	CspAdapterMinVersion = "103.0.1+up3.0.1"
 	DefaultShellVersion  = "rancher/shell:v0.1.26"
 	FleetVersion         = "103.1.11+up0.9.12"
-	WebhookVersion       = "103.0.13+up0.4.14-rc.1"
+	WebhookVersion       = "103.0.13+up0.4.14"
 )
