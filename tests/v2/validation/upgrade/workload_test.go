7c0ac9f63d3f2e5a80777aceface61c9bd182204
Anupama Upadhyayula <anupama.upadhyayula@suse.com>
2024-12-23 12:47:18 -0800

    Fixes the local upgrade failure (#48292) (#48462)
    * Fixes the local upgrade failure
    
    * Updating changes for rke1 upgrade
    
    * go mod changes to reflect rke1 state file
    
    * Resolvong go.mod conflicts

---
+++ tests/v2/validation/upgrade/workload_test.go
@@ -0,0 +1,65 @@
+//go:build validation
+
+package upgrade
+
+import (
+	"testing"
+
+	"github.com/rancher/rancher/tests/v2/actions/upgradeinput"
+	"github.com/rancher/shepherd/clients/rancher"
+	"github.com/rancher/shepherd/pkg/session"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+)
+
+var verifyIngress = true
+
+type UpgradeWorkloadTestSuite struct {
+	suite.Suite
+	session  *session.Session
+	client   *rancher.Client
+	clusters []upgradeinput.Cluster
+}
+
+func (u *UpgradeWorkloadTestSuite) TearDownSuite() {
+	u.session.Cleanup()
+}
+
+func (u *UpgradeWorkloadTestSuite) SetupSuite() {
+	testSession := session.NewSession()
+	u.session = testSession
+
+	client, err := rancher.NewClient("", testSession)
+	require.NoError(u.T(), err)
+
+	u.client = client
+
+	clusters, err := upgradeinput.LoadUpgradeKubernetesConfig(client)
+	require.NoError(u.T(), err)
+
+	u.clusters = clusters
+}
+
+func (u *UpgradeWorkloadTestSuite) TestWorkloadPreUpgrade() {
+	for _, cluster := range u.clusters {
+		cluster := cluster
+		u.Run(cluster.Name, func() {
+			cluster.FeaturesToTest.Ingress = &verifyIngress
+			createPreUpgradeWorkloads(u.T(), u.client, cluster.Name, cluster.FeaturesToTest, nil, containerImage)
+		})
+	}
+}
+
+func (u *UpgradeWorkloadTestSuite) TestWorkloadPostUpgrade() {
+	for _, cluster := range u.clusters {
+		cluster := cluster
+		u.Run(cluster.Name, func() {
+			cluster.FeaturesToTest.Ingress = &verifyIngress
+			createPostUpgradeWorkloads(u.T(), u.client, cluster.Name, cluster.FeaturesToTest)
+		})
+	}
+}
+
+func TestWorkloadUpgradeTestSuite(t *testing.T) {
+	suite.Run(t, new(UpgradeWorkloadTestSuite))
+}
