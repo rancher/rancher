81f888ff5499f323233c3165e3ea64349aef03c5
Daniel Newman <dan@danlovesprogramming.com>
2025-01-16 15:13:46 -0600

    Refactor configuration handling for observability tests
    Separate base and sizing configurations into distinct structs for improved clarity and reusability. Updated the chart installation logic to merge these configurations dynamically. This change enhances maintainability and better aligns with modular configuration practices.

--- tests/v2/actions/observability/config.go
+++ tests/v2/actions/observability/config.go
@@ -7,9 +7,8 @@ type StackStateConfig struct {
 	UpgradeVersion string `json:"upgradeVersion" yaml:"upgradeVersion"`
 }
 
-// StackStateServerValuesConfig represents the StateState Server configuration values
-// CombinedConfig represents the unified configuration values
-type StackStateServerValuesConfig struct {
+// BaseConfig represents the base configuration values
+type BaseConfig struct {
 	Global struct {
 		ImageRegistry string `yaml:"imageRegistry"`
 	} `yaml:"global"`
@@ -24,6 +23,105 @@ type StackStateServerValuesConfig struct {
 		License struct {
 			Key string `yaml:"key"`
 		} `yaml:"license"`
+	} `yaml:"stackstate"`
+}
+
+// SizingConfig represents the sizing configuration values
+type SizingConfig struct {
+	Clickhouse struct {
+		ReplicaCount int `yaml:"replicaCount"`
+		Persistence  struct {
+			Size string `yaml:"size"`
+		} `yaml:"persistence"`
+	} `yaml:"clickhouse"`
+	Elasticsearch struct {
+		PrometheusElasticsearchExporter struct {
+			Resources struct {
+				Limits struct {
+					CPU    string `yaml:"cpu"`
+					Memory string `yaml:"memory"`
+				} `yaml:"limits"`
+				Requests struct {
+					CPU    string `yaml:"cpu"`
+					Memory string `yaml:"memory"`
+				} `yaml:"requests"`
+			} `yaml:"resources"`
+		} `yaml:"prometheus-elasticsearch-exporter"`
+		MinimumMasterNodes int    `yaml:"minimumMasterNodes"`
+		Replicas           int    `yaml:"replicas"`
+		EsJavaOpts         string `yaml:"esJavaOpts"`
+		Resources          struct {
+			Requests struct {
+				CPU    string `yaml:"cpu"`
+				Memory string `yaml:"memory"`
+			} `yaml:"requests"`
+			Limits struct {
+				CPU    string `yaml:"cpu"`
+				Memory string `yaml:"memory"`
+			} `yaml:"limits"`
+		} `yaml:"resources"`
+		VolumeClaimTemplate struct {
+			Resources struct {
+				Requests struct {
+					Storage string `yaml:"storage"`
+				} `yaml:"requests"`
+			} `yaml:"resources"`
+		} `yaml:"volumeClaimTemplate"`
+	} `yaml:"elasticsearch"`
+	Hbase struct {
+		Version    string `yaml:"version"`
+		Deployment struct {
+			Mode string `yaml:"mode"`
+		} `yaml:"deployment"`
+		Stackgraph struct {
+			Persistence struct {
+				Size string `yaml:"size"`
+			} `yaml:"persistence"`
+			Resources struct {
+				Requests struct {
+					Memory string `yaml:"memory"`
+					CPU    string `yaml:"cpu"`
+				} `yaml:"requests"`
+				Limits struct {
+					CPU    string `yaml:"cpu"`
+					Memory string `yaml:"memory"`
+				} `yaml:"limits"`
+			} `yaml:"resources"`
+		} `yaml:"stackgraph"`
+		Tephra struct {
+			Resources struct {
+				Limits struct {
+					CPU    string `yaml:"cpu"`
+					Memory string `yaml:"memory"`
+				} `yaml:"limits"`
+				Requests struct {
+					Memory string `yaml:"memory"`
+					CPU    string `yaml:"cpu"`
+				} `yaml:"requests"`
+			} `yaml:"resources"`
+			ReplicaCount int `yaml:"replicaCount"`
+		} `yaml:"tephra"`
+	} `yaml:"hbase"`
+	Kafka struct {
+		DefaultReplicationFactor             int `yaml:"defaultReplicationFactor"`
+		OffsetsTopicReplicationFactor        int `yaml:"offsetsTopicReplicationFactor"`
+		ReplicaCount                         int `yaml:"replicaCount"`
+		TransactionStateLogReplicationFactor int `yaml:"transactionStateLogReplicationFactor"`
+		Resources                            struct {
+			Requests struct {
+				CPU    string `yaml:"cpu"`
+				Memory string `yaml:"memory"`
+			} `yaml:"requests"`
+			Limits struct {
+				Memory string `yaml:"memory"`
+				CPU    string `yaml:"cpu"`
+			} `yaml:"limits"`
+		} `yaml:"resources"`
+		Persistence struct {
+			Size string `yaml:"size"`
+		} `yaml:"persistence"`
+	} `yaml:"kafka"`
+	Stackstate struct {
 		Experimental struct {
 			Server struct {
 				Split bool `yaml:"split"`
@@ -158,99 +256,4 @@ type StackStateServerValuesConfig struct {
 	Zookeeper struct {
 		ReplicaCount int `yaml:"replicaCount"`
 	} `yaml:"zookeeper"`
-	Sizing struct {
-		Clickhouse struct {
-			ReplicaCount int `yaml:"replicaCount"`
-			Persistence  struct {
-				Size string `yaml:"size"`
-			} `yaml:"persistence"`
-		} `yaml:"clickhouse"`
-		Elasticsearch struct {
-			PrometheusElasticsearchExporter struct {
-				Resources struct {
-					Limits struct {
-						CPU    string `yaml:"cpu"`
-						Memory string `yaml:"memory"`
-					} `yaml:"limits"`
-					Requests struct {
-						CPU    string `yaml:"cpu"`
-						Memory string `yaml:"memory"`
-					} `yaml:"requests"`
-				} `yaml:"resources"`
-			} `yaml:"prometheus-elasticsearch-exporter"`
-			MinimumMasterNodes int    `yaml:"minimumMasterNodes"`
-			Replicas           int    `yaml:"replicas"`
-			EsJavaOpts         string `yaml:"esJavaOpts"`
-			Resources          struct {
-				Requests struct {
-					CPU    string `yaml:"cpu"`
-					Memory string `yaml:"memory"`
-				} `yaml:"requests"`
-				Limits struct {
-					CPU    string `yaml:"cpu"`
-					Memory string `yaml:"memory"`
-				} `yaml:"limits"`
-			} `yaml:"resources"`
-			VolumeClaimTemplate struct {
-				Resources struct {
-					Requests struct {
-						Storage string `yaml:"storage"`
-					} `yaml:"resources"`
-				} `yaml:"resources"`
-			} `yaml:"volumeClaimTemplate"`
-		} `yaml:"elasticsearch"`
-		Hbase struct {
-			Version    string `yaml:"version"`
-			Deployment struct {
-				Mode string `yaml:"mode"`
-			} `yaml:"deployment"`
-			Stackgraph struct {
-				Persistence struct {
-					Size string `yaml:"size"`
-				} `yaml:"persistence"`
-				Resources struct {
-					Requests struct {
-						Memory string `yaml:"memory"`
-						CPU    string `yaml:"cpu"`
-					} `yaml:"requests"`
-					Limits struct {
-						CPU    string `yaml:"cpu"`
-						Memory string `yaml:"memory"`
-					} `yaml:"limits"`
-				} `yaml:"resources"`
-			} `yaml:"stackgraph"`
-			Tephra struct {
-				Resources struct {
-					Limits struct {
-						CPU    string `yaml:"cpu"`
-						Memory string `yaml:"memory"`
-					} `yaml:"limits"`
-					Requests struct {
-						Memory string `yaml:"memory"`
-						CPU    string `yaml:"cpu"`
-					} `yaml:"requests"`
-				} `yaml:"resources"`
-				ReplicaCount int `yaml:"replicaCount"`
-			} `yaml:"tephra"`
-		} `yaml:"hbase"`
-		Kafka struct {
-			DefaultReplicationFactor             int `yaml:"defaultReplicationFactor"`
-			OffsetsTopicReplicationFactor        int `yaml:"offsetsTopicReplicationFactor"`
-			ReplicaCount                         int `yaml:"replicaCount"`
-			TransactionStateLogReplicationFactor int `yaml:"transactionStateLogReplicationFactor"`
-			Resources                            struct {
-				Requests struct {
-					CPU    string `yaml:"cpu"`
-					Memory string `yaml:"memory"`
-				} `yaml:"requests"`
-				Limits struct {
-					Memory string `yaml:"memory"`
-					CPU    string `yaml:"cpu"`
-				} `yaml:"limits"`
-			} `yaml:"resources"`
-			Persistence struct {
-				Size string `yaml:"size"`
-			} `yaml:"persistence"`
-		} `yaml:"kafka"`
-	} `yaml:"sizing"`
 }
