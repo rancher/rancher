a7a35b5614de4563537c5360191cf0ffbf68d5bf
Daniel Newman <dan@danlovesprogramming.com>
2025-01-16 15:53:13 -0600

    Add JSON tags alongside YAML tags for configuration structs
    This change introduces JSON tags in addition to existing YAML tags across all configuration structs. This ensures compatibility with JSON-based serialization while maintaining support for YAML-based configurations. It enhances flexibility for different use cases and tools that rely on either format.

--- tests/v2/actions/observability/config.go
+++ tests/v2/actions/observability/config.go
@@ -10,250 +10,250 @@ type StackStateConfig struct {
 // BaseConfig represents the base configuration values
 type BaseConfig struct {
 	Global struct {
-		ImageRegistry string `yaml:"imageRegistry"`
-	} `yaml:"global"`
+		ImageRegistry string `json:"imageRegistry" yaml:"imageRegistry"`
+	} `json:"global" yaml:"global"`
 	Stackstate struct {
-		BaseUrl        string `yaml:"baseUrl"`
+		BaseUrl        string `json:"baseUrl" yaml:"baseUrl"`
 		Authentication struct {
-			AdminPassword string `yaml:"adminPassword"`
-		} `yaml:"authentication"`
+			AdminPassword string `json:"adminPassword" yaml:"adminPassword"`
+		} `json:"authentication" yaml:"authentication"`
 		ApiKey struct {
-			Key string `yaml:"key"`
-		} `yaml:"apiKey"`
+			Key string `json:"key" yaml:"key"`
+		} `json:"apiKey" yaml:"apiKey"`
 		License struct {
-			Key string `yaml:"key"`
-		} `yaml:"license"`
-	} `yaml:"stackstate"`
+			Key string `json:"key" yaml:"key"`
+		} `json:"license" yaml:"license"`
+	} `json:"stackstate" yaml:"stackstate"`
 }
 
 // SizingConfig represents the sizing configuration values
 type SizingConfig struct {
 	Clickhouse struct {
-		ReplicaCount int `yaml:"replicaCount"`
+		ReplicaCount int `json:"replicaCount" yaml:"replicaCount"`
 		Persistence  struct {
-			Size string `yaml:"size"`
-		} `yaml:"persistence"`
-	} `yaml:"clickhouse"`
+			Size string `json:"size" yaml:"size"`
+		} `json:"persistence" yaml:"persistence"`
+	} `json:"clickhouse" yaml:"clickhouse"`
 	Elasticsearch struct {
 		PrometheusElasticsearchExporter struct {
 			Resources struct {
 				Limits struct {
-					CPU    string `yaml:"cpu"`
-					Memory string `yaml:"memory"`
-				} `yaml:"limits"`
+					CPU    string `json:"cpu" yaml:"cpu"`
+					Memory string `json:"memory" yaml:"memory"`
+				} `json:"limits" yaml:"limits"`
 				Requests struct {
-					CPU    string `yaml:"cpu"`
-					Memory string `yaml:"memory"`
-				} `yaml:"requests"`
-			} `yaml:"resources"`
-		} `yaml:"prometheus-elasticsearch-exporter"`
-		MinimumMasterNodes int    `yaml:"minimumMasterNodes"`
-		Replicas           int    `yaml:"replicas"`
-		EsJavaOpts         string `yaml:"esJavaOpts"`
+					CPU    string `json:"cpu" yaml:"cpu"`
+					Memory string `json:"memory" yaml:"memory"`
+				} `json:"requests" yaml:"requests"`
+			} `json:"resources" yaml:"resources"`
+		} `json:"prometheus-elasticsearch-exporter" yaml:"prometheus-elasticsearch-exporter"`
+		MinimumMasterNodes int    `json:"minimumMasterNodes" yaml:"minimumMasterNodes"`
+		Replicas           int    `json:"replicas" yaml:"replicas"`
+		EsJavaOpts         string `json:"esJavaOpts" yaml:"esJavaOpts"`
 		Resources          struct {
 			Requests struct {
-				CPU    string `yaml:"cpu"`
-				Memory string `yaml:"memory"`
-			} `yaml:"requests"`
+				CPU    string `json:"cpu" yaml:"cpu"`
+				Memory string `json:"memory" yaml:"memory"`
+			} `json:"requests" yaml:"requests"`
 			Limits struct {
-				CPU    string `yaml:"cpu"`
-				Memory string `yaml:"memory"`
-			} `yaml:"limits"`
-		} `yaml:"resources"`
+				CPU    string `json:"cpu" yaml:"cpu"`
+				Memory string `json:"memory" yaml:"memory"`
+			} `json:"limits" yaml:"limits"`
+		} `json:"resources" yaml:"resources"`
 		VolumeClaimTemplate struct {
 			Resources struct {
 				Requests struct {
-					Storage string `yaml:"storage"`
-				} `yaml:"requests"`
-			} `yaml:"resources"`
-		} `yaml:"volumeClaimTemplate"`
-	} `yaml:"elasticsearch"`
+					Storage string `json:"storage" yaml:"storage"`
+				} `json:"requests" yaml:"requests"`
+			} `json:"resources" yaml:"resources"`
+		} `json:"volumeClaimTemplate" yaml:"volumeClaimTemplate"`
+	} `json:"elasticsearch" yaml:"elasticsearch"`
 	Hbase struct {
-		Version    string `yaml:"version"`
+		Version    string `json:"version" yaml:"version"`
 		Deployment struct {
-			Mode string `yaml:"mode"`
-		} `yaml:"deployment"`
+			Mode string `json:"mode" yaml:"mode"`
+		} `json:"deployment" yaml:"deployment"`
 		Stackgraph struct {
 			Persistence struct {
-				Size string `yaml:"size"`
-			} `yaml:"persistence"`
+				Size string `json:"size" yaml:"size"`
+			} `json:"persistence" yaml:"persistence"`
 			Resources struct {
 				Requests struct {
-					Memory string `yaml:"memory"`
-					CPU    string `yaml:"cpu"`
-				} `yaml:"requests"`
+					Memory string `json:"memory" yaml:"memory"`
+					CPU    string `json:"cpu" yaml:"cpu"`
+				} `json:"requests" yaml:"requests"`
 				Limits struct {
-					CPU    string `yaml:"cpu"`
-					Memory string `yaml:"memory"`
-				} `yaml:"limits"`
-			} `yaml:"resources"`
-		} `yaml:"stackgraph"`
+					CPU    string `json:"cpu" yaml:"cpu"`
+					Memory string `json:"memory" yaml:"memory"`
+				} `json:"limits" yaml:"limits"`
+			} `json:"resources" yaml:"resources"`
+		} `json:"stackgraph" yaml:"stackgraph"`
 		Tephra struct {
 			Resources struct {
 				Limits struct {
-					CPU    string `yaml:"cpu"`
-					Memory string `yaml:"memory"`
-				} `yaml:"limits"`
+					CPU    string `json:"cpu" yaml:"cpu"`
+					Memory string `json:"memory" yaml:"memory"`
+				} `json:"limits" yaml:"limits"`
 				Requests struct {
-					Memory string `yaml:"memory"`
-					CPU    string `yaml:"cpu"`
-				} `yaml:"requests"`
-			} `yaml:"resources"`
-			ReplicaCount int `yaml:"replicaCount"`
-		} `yaml:"tephra"`
-	} `yaml:"hbase"`
+					Memory string `json:"memory" yaml:"memory"`
+					CPU    string `json:"cpu" yaml:"cpu"`
+				} `json:"requests" yaml:"requests"`
+			} `json:"resources" yaml:"resources"`
+			ReplicaCount int `json:"replicaCount" yaml:"replicaCount"`
+		} `json:"tephra" yaml:"tephra"`
+	} `json:"hbase" yaml:"hbase"`
 	Kafka struct {
-		DefaultReplicationFactor             int `yaml:"defaultReplicationFactor"`
-		OffsetsTopicReplicationFactor        int `yaml:"offsetsTopicReplicationFactor"`
-		ReplicaCount                         int `yaml:"replicaCount"`
-		TransactionStateLogReplicationFactor int `yaml:"transactionStateLogReplicationFactor"`
+		DefaultReplicationFactor             int `json:"defaultReplicationFactor" yaml:"defaultReplicationFactor"`
+		OffsetsTopicReplicationFactor        int `json:"offsetsTopicReplicationFactor" yaml:"offsetsTopicReplicationFactor"`
+		ReplicaCount                         int `json:"replicaCount" yaml:"replicaCount"`
+		TransactionStateLogReplicationFactor int `json:"transactionStateLogReplicationFactor" yaml:"transactionStateLogReplicationFactor"`
 		Resources                            struct {
 			Requests struct {
-				CPU    string `yaml:"cpu"`
-				Memory string `yaml:"memory"`
-			} `yaml:"requests"`
+				CPU    string `json:"cpu" yaml:"cpu"`
+				Memory string `json:"memory" yaml:"memory"`
+			} `json:"requests" yaml:"requests"`
 			Limits struct {
-				Memory string `yaml:"memory"`
-				CPU    string `yaml:"cpu"`
-			} `yaml:"limits"`
-		} `yaml:"resources"`
+				Memory string `json:"memory" yaml:"memory"`
+				CPU    string `json:"cpu" yaml:"cpu"`
+			} `json:"limits" yaml:"limits"`
+		} `json:"resources" yaml:"resources"`
 		Persistence struct {
-			Size string `yaml:"size"`
-		} `yaml:"persistence"`
-	} `yaml:"kafka"`
+			Size string `json:"size" yaml:"size"`
+		} `json:"persistence" yaml:"persistence"`
+	} `json:"kafka" yaml:"kafka"`
 	Stackstate struct {
 		Experimental struct {
 			Server struct {
-				Split bool `yaml:"split"`
-			} `yaml:"server"`
-		} `yaml:"experimental"`
+				Split bool `json:"split" yaml:"split"`
+			} `json:"server" yaml:"server"`
+		} `json:"experimental" yaml:"experimental"`
 		Components struct {
 			All struct {
 				ExtraEnv struct {
 					Open struct {
-						CONFIGFORCEStackstateTopologyQueryServiceMaxStackElementsPerQuery  string `yaml:"CONFIG_FORCE_stackstate_topologyQueryService_maxStackElementsPerQuery"`
-						CONFIGFORCEStackstateTopologyQueryServiceMaxLoadedElementsPerQuery string `yaml:"CONFIG_FORCE_stackstate_topologyQueryService_maxLoadedElementsPerQuery"`
-						CONFIGFORCEStackstateAgentsAgentLimit                              string `yaml:"CONFIG_FORCE_stackstate_agents_agentLimit"`
-					} `yaml:"open"`
-				} `yaml:"extraEnv"`
-			} `yaml:"all"`
+						CONFIGFORCEStackstateTopologyQueryServiceMaxStackElementsPerQuery  string `json:"CONFIG_FORCE_stackstate_topologyQueryService_maxStackElementsPerQuery" yaml:"CONFIG_FORCE_stackstate_topologyQueryService_maxStackElementsPerQuery"`
+						CONFIGFORCEStackstateTopologyQueryServiceMaxLoadedElementsPerQuery string `json:"CONFIG_FORCE_stackstate_topologyQueryService_maxLoadedElementsPerQuery" yaml:"CONFIG_FORCE_stackstate_topologyQueryService_maxLoadedElementsPerQuery"`
+						CONFIGFORCEStackstateAgentsAgentLimit                              string `json:"CONFIG_FORCE_stackstate_agents_agentLimit" yaml:"CONFIG_FORCE_stackstate_agents_agentLimit"`
+					} `json:"open" yaml:"open"`
+				} `json:"extraEnv" yaml:"extraEnv"`
+			} `json:"all" yaml:"all"`
 			Server struct {
 				ExtraEnv struct {
 					Open struct {
-						CONFIGFORCEStackstateSyncInitializationBatchParallelism     string `yaml:"CONFIG_FORCE_stackstate_sync_initializationBatchParallelism"`
-						CONFIGFORCEStackstateHealthSyncInitialLoadParallelism       string `yaml:"CONFIG_FORCE_stackstate_healthSync_initialLoadParallelism"`
-						CONFIGFORCEStackstateStateServiceInitializationParallelism  string `yaml:"CONFIG_FORCE_stackstate_stateService_initializationParallelism"`
-						CONFIGFORCEStackstateStateServiceInitialLoadTransactionSize string `yaml:"CONFIG_FORCE_stackstate_stateService_initialLoadTransactionSize"`
-					} `yaml:"open"`
-				} `yaml:"extraEnv"`
+						CONFIGFORCEStackstateSyncInitializationBatchParallelism     string `json:"CONFIG_FORCE_stackstate_sync_initializationBatchParallelism" yaml:"CONFIG_FORCE_stackstate_sync_initializationBatchParallelism"`
+						CONFIGFORCEStackstateHealthSyncInitialLoadParallelism       string `json:"CONFIG_FORCE_stackstate_healthSync_initialLoadParallelism" yaml:"CONFIG_FORCE_stackstate_healthSync_initialLoadParallelism"`
+						CONFIGFORCEStackstateStateServiceInitializationParallelism  string `json:"CONFIG_FORCE_stackstate_stateService_initializationParallelism" yaml:"CONFIG_FORCE_stackstate_stateService_initializationParallelism"`
+						CONFIGFORCEStackstateStateServiceInitialLoadTransactionSize string `json:"CONFIG_FORCE_stackstate_stateService_initialLoadTransactionSize" yaml:"CONFIG_FORCE_stackstate_stateService_initialLoadTransactionSize"`
+					} `json:"open" yaml:"open"`
+				} `json:"extraEnv" yaml:"extraEnv"`
 				Resources struct {
 					Limits struct {
-						EphemeralStorage string `yaml:"ephemeral-storage"`
-						CPU              string `yaml:"cpu"`
-						Memory           string `yaml:"memory"`
-					} `yaml:"limits"`
+						EphemeralStorage string `json:"ephemeral-storage" yaml:"ephemeral-storage"`
+						CPU              string `json:"cpu" yaml:"cpu"`
+						Memory           string `json:"memory" yaml:"memory"`
+					} `json:"limits" yaml:"limits"`
 					Requests struct {
-						CPU    string `yaml:"cpu"`
-						Memory string `yaml:"memory"`
-					} `yaml:"requests"`
-				} `yaml:"resources"`
-			} `yaml:"server"`
+						CPU    string `json:"cpu" yaml:"cpu"`
+						Memory string `json:"memory" yaml:"memory"`
+					} `json:"requests" yaml:"requests"`
+				} `json:"resources" yaml:"resources"`
+			} `json:"server" yaml:"server"`
 			E2Es struct {
 				Resources struct {
 					Requests struct {
-						Memory string `yaml:"memory"`
-						CPU    string `yaml:"cpu"`
-					} `yaml:"requests"`
+						Memory string `json:"memory" yaml:"memory"`
+						CPU    string `json:"cpu" yaml:"cpu"`
+					} `json:"requests" yaml:"requests"`
 					Limits struct {
-						Memory string `yaml:"memory"`
-					} `yaml:"limits"`
-				} `yaml:"resources"`
-			} `yaml:"e2es"`
+						Memory string `json:"memory" yaml:"memory"`
+					} `json:"limits" yaml:"limits"`
+				} `json:"resources" yaml:"resources"`
+			} `json:"e2es" yaml:"e2es"`
 			Correlate struct {
 				Resources struct {
 					Requests struct {
-						Memory string `yaml:"memory"`
-						CPU    string `yaml:"cpu"`
-					} `yaml:"requests"`
+						Memory string `json:"memory" yaml:"memory"`
+						CPU    string `json:"cpu" yaml:"cpu"`
+					} `json:"requests" yaml:"requests"`
 					Limits struct {
-						CPU    string `yaml:"cpu"`
-						Memory string `yaml:"memory"`
-					} `yaml:"limits"`
-				} `yaml:"resources"`
-			} `yaml:"correlate"`
+						CPU    string `json:"cpu" yaml:"cpu"`
+						Memory string `json:"memory" yaml:"memory"`
+					} `json:"limits" yaml:"limits"`
+				} `json:"resources" yaml:"resources"`
+			} `json:"correlate" yaml:"correlate"`
 			Receiver struct {
 				Split struct {
-					Enabled bool `yaml:"enabled"`
-				} `yaml:"split"`
+					Enabled bool `json:"enabled" yaml:"enabled"`
+				} `json:"split" yaml:"split"`
 				ExtraEnv struct {
 					Open struct {
-						CONFIGFORCEAkkaHTTPHostConnectionPoolMaxOpenRequests string `yaml:"CONFIG_FORCE_akka_http_host__connection__pool_max__open__requests"`
-					} `yaml:"open"`
-				} `yaml:"extraEnv"`
+						CONFIGFORCEAkkaHTTPHostConnectionPoolMaxOpenRequests string `json:"CONFIG_FORCE_akka_http_host__connection__pool_max__open__requests" yaml:"CONFIG_FORCE_akka_http_host__connection__pool_max__open__requests"`
+					} `json:"open" yaml:"open"`
+				} `json:"extraEnv" yaml:"extraEnv"`
 				Resources struct {
 					Requests struct {
-						Memory string `yaml:"memory"`
-						CPU    string `yaml:"cpu"`
-					} `yaml:"requests"`
+						Memory string `json:"memory" yaml:"memory"`
+						CPU    string `json:"cpu" yaml:"cpu"`
+					} `json:"requests" yaml:"requests"`
 					Limits struct {
-						Memory string `yaml:"memory"`
-						CPU    string `yaml:"cpu"`
-					} `yaml:"limits"`
-				} `yaml:"resources"`
-			} `yaml:"receiver"`
+						Memory string `json:"memory" yaml:"memory"`
+						CPU    string `json:"cpu" yaml:"cpu"`
+					} `json:"limits" yaml:"limits"`
+				} `json:"resources" yaml:"resources"`
+			} `json:"receiver" yaml:"receiver"`
 			Vmagent struct {
 				Resources struct {
 					Limits struct {
-						Memory string `yaml:"memory"`
-					} `yaml:"limits"`
+						Memory string `json:"memory" yaml:"memory"`
+					} `json:"limits" yaml:"limits"`
 					Requests struct {
-						Memory string `yaml:"memory"`
-					} `yaml:"requests"`
-				} `yaml:"resources"`
-			} `yaml:"vmagent"`
+						Memory string `json:"memory" yaml:"memory"`
+					} `json:"requests" yaml:"requests"`
+				} `json:"resources" yaml:"resources"`
+			} `json:"vmagent" yaml:"vmagent"`
 			UI struct {
-				ReplicaCount int `yaml:"replicaCount"`
-			} `yaml:"ui"`
-		} `yaml:"components"`
-	} `yaml:"stackstate"`
+				ReplicaCount int `json:"replicaCount" yaml:"replicaCount"`
+			} `json:"ui" yaml:"ui"`
+		} `json:"components" yaml:"components"`
+	} `json:"stackstate" yaml:"stackstate"`
 	VictoriaMetrics0 struct {
 		Server struct {
 			Resources struct {
 				Requests struct {
-					CPU    string `yaml:"cpu"`
-					Memory string `yaml:"memory"`
-				} `yaml:"requests"`
+					CPU    string `json:"cpu" yaml:"cpu"`
+					Memory string `json:"memory" yaml:"memory"`
+				} `json:"requests" yaml:"requests"`
 				Limits struct {
-					CPU    string `yaml:"cpu"`
-					Memory string `yaml:"memory"`
-				} `yaml:"limits"`
-			} `yaml:"resources"`
+					CPU    string `json:"cpu" yaml:"cpu"`
+					Memory string `json:"memory" yaml:"memory"`
+				} `json:"limits" yaml:"limits"`
+			} `json:"resources" yaml:"resources"`
 			PersistentVolume struct {
-				Size string `yaml:"size"`
-			} `yaml:"persistentVolume"`
-		} `yaml:"server"`
+				Size string `json:"size" yaml:"size"`
+			} `json:"persistentVolume" yaml:"persistentVolume"`
+		} `json:"server" yaml:"server"`
 		Backup struct {
 			Vmbackup struct {
 				Resources struct {
 					Requests struct {
-						Memory string `yaml:"memory"`
-					} `yaml:"requests"`
+						Memory string `json:"memory" yaml:"memory"`
+					} `json:"requests" yaml:"requests"`
 					Limits struct {
-						Memory string `yaml:"memory"`
-					} `yaml:"limits"`
-				} `yaml:"resources"`
-			} `yaml:"vmbackup"`
-		} `yaml:"backup"`
-	} `yaml:"victoria-metrics-0"`
+						Memory string `json:"memory" yaml:"memory"`
+					} `json:"limits" yaml:"limits"`
+				} `json:"resources" yaml:"resources"`
+			} `json:"vmbackup" yaml:"vmbackup"`
+		} `json:"backup" yaml:"backup"`
+	} `json:"victoria-metrics-0" yaml:"victoria-metrics-0"`
 	VictoriaMetrics1 struct {
-		Enabled bool `yaml:"enabled"`
+		Enabled bool `json:"enabled" yaml:"enabled"`
 		Server  struct {
 			PersistentVolume struct {
-				Size string `yaml:"size"`
-			} `yaml:"persistentVolume"`
-		} `yaml:"server"`
-	} `yaml:"victoria-metrics-1"`
+				Size string `json:"size" yaml:"size"`
+			} `json:"persistentVolume" yaml:"persistentVolume"`
+		} `json:"server" yaml:"server"`
+	} `json:"victoria-metrics-1" yaml:"victoria-metrics-1"`
 	Zookeeper struct {
-		ReplicaCount int `yaml:"replicaCount"`
-	} `yaml:"zookeeper"`
+		ReplicaCount int `json:"replicaCount" yaml:"replicaCount"`
+	} `json:"zookeeper" yaml:"zookeeper"`
 }
