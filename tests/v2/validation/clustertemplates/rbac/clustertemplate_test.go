36f5259a56ee7fb145c64faaf45456891a85a263
Anupama <38144301+anupama2501@users.noreply.github.com>
2025-01-22 13:54:51 -0800

    Updating tags for the cluster templates provisioning tests

--- tests/v2/validation/clustertemplates/rbac/clustertemplate_test.go
+++ tests/v2/validation/clustertemplates/rbac/clustertemplate_test.go
@@ -1,3 +1,5 @@
+//go:build (validation || infra.rke1 || cluster.nodedriver || stress) && !infra.any && !infra.aks && !infra.eks && !infra.gke && !infra.rke2k3s && !cluster.any && !sanity
+
 package rbac
 
 import (
