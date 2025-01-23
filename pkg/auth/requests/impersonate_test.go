eb97e90e2303d10d7be497559043d9bfecc8c41c
Peter Matseykanets <peter.matseykanets@suse.com>
2025-01-23 16:12:51 -0500

    Make sure impersonating user owns request token

--- pkg/auth/requests/impersonate_test.go
+++ pkg/auth/requests/impersonate_test.go
@@ -8,15 +8,22 @@ import (
 	"net/http/httptest"
 	"testing"
 
+	v3 "github.com/rancher/rancher/pkg/apis/management.cattle.io/v3"
 	"github.com/rancher/rancher/pkg/auth/requests/mocks"
 	"github.com/rancher/rancher/pkg/auth/requests/sar"
+	mgmtv3 "github.com/rancher/rancher/pkg/generated/controllers/management.cattle.io/v3"
+	"github.com/rancher/wrangler/v3/pkg/generic/fake"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"go.uber.org/mock/gomock"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apiserver/pkg/authentication/user"
 	"k8s.io/apiserver/pkg/endpoints/request"
 )
 
 func TestAuthenticateImpersonation(t *testing.T) {
+	t.Parallel()
+
 	ctrl := gomock.NewController(t)
 	userInfo := &user.DefaultInfo{
 		Name:   "user",
@@ -24,14 +31,29 @@ func TestAuthenticateImpersonation(t *testing.T) {
 		Groups: nil,
 		Extra:  nil,
 	}
-	tests := map[string]struct {
+
+	tests := []struct {
+		desc                  string
 		req                   func() *http.Request
-		sarMock               func(req *http.Request) sar.SubjectAccessReview
+		sar                   func(req *http.Request) sar.SubjectAccessReview
+		tokenCache            func() mgmtv3.TokenCache
 		wantUserInfo          *user.DefaultInfo
 		wantNextHandlerCalled bool
 		wantErr               string
+		status                int
 	}{
-		"no impersonation": {
+		{
+			desc: "unauthenticated",
+			req: func() *http.Request {
+				return &http.Request{}
+			},
+			sar: func(_ *http.Request) sar.SubjectAccessReview {
+				return mocks.NewMockSubjectAccessReview(ctrl)
+			},
+			status: http.StatusUnauthorized,
+		},
+		{
+			desc: "no impersonation",
 			req: func() *http.Request {
 				ctx := request.WithUser(context.Background(), userInfo)
 				req := &http.Request{}
@@ -39,12 +61,14 @@ func TestAuthenticateImpersonation(t *testing.T) {
 
 				return req
 			},
-			sarMock: func(_ *http.Request) sar.SubjectAccessReview {
+			sar: func(_ *http.Request) sar.SubjectAccessReview {
 				return mocks.NewMockSubjectAccessReview(ctrl)
 			},
 			wantNextHandlerCalled: true,
+			status:                http.StatusOK,
 		},
-		"impersonate user": {
+		{
+			desc: "impersonate user",
 			req: func() *http.Request {
 				ctx := request.WithUser(context.Background(), userInfo)
 				req := &http.Request{
@@ -56,7 +80,7 @@ func TestAuthenticateImpersonation(t *testing.T) {
 
 				return req
 			},
-			sarMock: func(req *http.Request) sar.SubjectAccessReview {
+			sar: func(req *http.Request) sar.SubjectAccessReview {
 				mock := mocks.NewMockSubjectAccessReview(ctrl)
 				mock.EXPECT().UserCanImpersonateUser(req, "user", "impUser").Return(true, nil)
 
@@ -69,8 +93,10 @@ func TestAuthenticateImpersonation(t *testing.T) {
 				Extra:  nil,
 			},
 			wantNextHandlerCalled: true,
+			status:                http.StatusOK,
 		},
-		"impersonate group": {
+		{
+			desc: "impersonate group",
 			req: func() *http.Request {
 				ctx := request.WithUser(context.Background(), userInfo)
 				req := &http.Request{
@@ -83,7 +109,7 @@ func TestAuthenticateImpersonation(t *testing.T) {
 
 				return req
 			},
-			sarMock: func(req *http.Request) sar.SubjectAccessReview {
+			sar: func(req *http.Request) sar.SubjectAccessReview {
 				mock := mocks.NewMockSubjectAccessReview(ctrl)
 				mock.EXPECT().UserCanImpersonateUser(req, "user", "impUser").Return(true, nil)
 				mock.EXPECT().UserCanImpersonateGroup(req, "user", "impGroup").Return(true, nil)
@@ -97,36 +123,57 @@ func TestAuthenticateImpersonation(t *testing.T) {
 				Extra:  nil,
 			},
 			wantNextHandlerCalled: true,
+			status:                http.StatusOK,
 		},
-		"impersonate extras": {
+		{
+			desc: "impersonate extras",
 			req: func() *http.Request {
 				ctx := request.WithUser(context.Background(), userInfo)
 				req := &http.Request{
 					Header: map[string][]string{
-						"Impersonate-User":      {"impUser"},
-						"Impersonate-Extra-foo": {"bar"},
+						"Impersonate-User":                 {"impUser"},
+						"Impersonate-Extra-foo":            {"bar"},
+						"Impersonate-Extra-requesttokenid": {"kubeconfig-u-user5zfww"},
 					},
 				}
 				req = req.WithContext(ctx)
 
 				return req
 			},
-			sarMock: func(req *http.Request) sar.SubjectAccessReview {
+			sar: func(req *http.Request) sar.SubjectAccessReview {
 				mock := mocks.NewMockSubjectAccessReview(ctrl)
 				mock.EXPECT().UserCanImpersonateUser(req, "user", "impUser").Return(true, nil)
-				mock.EXPECT().UserCanImpersonateExtras(req, "user", map[string][]string{"foo": {"bar"}}).Return(true, nil)
+				mock.EXPECT().UserCanImpersonateExtras(req, "user", map[string][]string{
+					"foo":            {"bar"},
+					"requesttokenid": {"kubeconfig-u-user5zfww"},
+				}).Return(true, nil)
 
 				return mock
+			},
+			tokenCache: func() mgmtv3.TokenCache {
+				cache := fake.NewMockNonNamespacedCacheInterface[*v3.Token](ctrl)
+				cache.EXPECT().Get("kubeconfig-u-user5zfww").Return(&v3.Token{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "kubeconfig-u-user5zfww",
+					},
+					UserID: "impUser",
+				}, nil)
+				return cache
 			},
 			wantUserInfo: &user.DefaultInfo{
 				Name:   "impUser",
 				UID:    "impUser",
 				Groups: []string{"system:authenticated"},
-				Extra:  map[string][]string{"foo": {"bar"}},
+				Extra: map[string][]string{
+					"foo":            {"bar"},
+					"requesttokenid": {"kubeconfig-u-user5zfww"},
+				},
 			},
 			wantNextHandlerCalled: true,
+			status:                http.StatusOK,
 		},
-		"impersonate serviceaccount": {
+		{
+			desc: "impersonate serviceaccount",
 			req: func() *http.Request {
 				ctx := request.WithUser(context.Background(), userInfo)
 				req := &http.Request{
@@ -138,7 +185,7 @@ func TestAuthenticateImpersonation(t *testing.T) {
 
 				return req
 			},
-			sarMock: func(req *http.Request) sar.SubjectAccessReview {
+			sar: func(req *http.Request) sar.SubjectAccessReview {
 				mock := mocks.NewMockSubjectAccessReview(ctrl)
 				mock.EXPECT().UserCanImpersonateServiceAccount(req, "user", "system:serviceaccount:default:test").Return(true, nil)
 
@@ -146,8 +193,10 @@ func TestAuthenticateImpersonation(t *testing.T) {
 			},
 			wantUserInfo:          userInfo,
 			wantNextHandlerCalled: true,
+			status:                http.StatusOK,
 		},
-		"impersonate user not allowed": {
+		{
+			desc: "impersonate user not allowed",
 			req: func() *http.Request {
 				ctx := request.WithUser(context.Background(), userInfo)
 				req := &http.Request{
@@ -159,15 +208,17 @@ func TestAuthenticateImpersonation(t *testing.T) {
 
 				return req
 			},
-			sarMock: func(req *http.Request) sar.SubjectAccessReview {
+			sar: func(req *http.Request) sar.SubjectAccessReview {
 				mock := mocks.NewMockSubjectAccessReview(ctrl)
 				mock.EXPECT().UserCanImpersonateUser(req, "user", "impUser").Return(false, nil)
 
 				return mock
 			},
 			wantErr: "not allowed to impersonate user",
+			status:  http.StatusForbidden,
 		},
-		"impersonate group not allowed": {
+		{
+			desc: "impersonate group not allowed",
 			req: func() *http.Request {
 				ctx := request.WithUser(context.Background(), userInfo)
 				req := &http.Request{
@@ -180,7 +231,7 @@ func TestAuthenticateImpersonation(t *testing.T) {
 
 				return req
 			},
-			sarMock: func(req *http.Request) sar.SubjectAccessReview {
+			sar: func(req *http.Request) sar.SubjectAccessReview {
 				mock := mocks.NewMockSubjectAccessReview(ctrl)
 				mock.EXPECT().UserCanImpersonateUser(req, "user", "impUser").Return(true, nil)
 				mock.EXPECT().UserCanImpersonateGroup(req, "user", "impGroup").Return(false, nil)
@@ -188,8 +239,10 @@ func TestAuthenticateImpersonation(t *testing.T) {
 				return mock
 			},
 			wantErr: "not allowed to impersonate group",
+			status:  http.StatusForbidden,
 		},
-		"impersonate extras not allowed": {
+		{
+			desc: "impersonate extras not allowed",
 			req: func() *http.Request {
 				ctx := request.WithUser(context.Background(), userInfo)
 				req := &http.Request{
@@ -202,7 +255,7 @@ func TestAuthenticateImpersonation(t *testing.T) {
 
 				return req
 			},
-			sarMock: func(req *http.Request) sar.SubjectAccessReview {
+			sar: func(req *http.Request) sar.SubjectAccessReview {
 				mock := mocks.NewMockSubjectAccessReview(ctrl)
 				mock.EXPECT().UserCanImpersonateUser(req, "user", "impUser").Return(true, nil)
 				mock.EXPECT().UserCanImpersonateExtras(req, "user", map[string][]string{"foo": {"bar"}}).Return(false, nil)
@@ -210,8 +263,46 @@ func TestAuthenticateImpersonation(t *testing.T) {
 				return mock
 			},
 			wantErr: "not allowed to impersonate extra",
+			status:  http.StatusForbidden,
 		},
-		"impersonate serviceaccount not allowed": {
+		{
+			desc: "user is not the owner of the request token",
+			req: func() *http.Request {
+				ctx := request.WithUser(context.Background(), userInfo)
+				req := &http.Request{
+					Header: map[string][]string{
+						"Impersonate-User":                 {"impUser"},
+						"Impersonate-Extra-requesttokenid": {"kubeconfig-u-user5zfww"},
+					},
+				}
+				req = req.WithContext(ctx)
+
+				return req
+			},
+			sar: func(req *http.Request) sar.SubjectAccessReview {
+				mock := mocks.NewMockSubjectAccessReview(ctrl)
+				mock.EXPECT().UserCanImpersonateUser(req, "user", "impUser").Return(true, nil)
+				mock.EXPECT().UserCanImpersonateExtras(req, "user", map[string][]string{
+					"requesttokenid": {"kubeconfig-u-user5zfww"},
+				}).Return(true, nil)
+
+				return mock
+			},
+			tokenCache: func() mgmtv3.TokenCache {
+				cache := fake.NewMockNonNamespacedCacheInterface[*v3.Token](ctrl)
+				cache.EXPECT().Get("kubeconfig-u-user5zfww").Return(&v3.Token{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "kubeconfig-u-user5zfww",
+					},
+					UserID: "someoneelse",
+				}, nil)
+				return cache
+			},
+			wantErr: "request token user does not match impersonation user",
+			status:  http.StatusForbidden,
+		},
+		{
+			desc: "impersonate serviceaccount not allowed",
 			req: func() *http.Request {
 				ctx := request.WithUser(context.Background(), userInfo)
 				req := &http.Request{
@@ -223,15 +314,17 @@ func TestAuthenticateImpersonation(t *testing.T) {
 
 				return req
 			},
-			sarMock: func(req *http.Request) sar.SubjectAccessReview {
+			sar: func(req *http.Request) sar.SubjectAccessReview {
 				mock := mocks.NewMockSubjectAccessReview(ctrl)
 				mock.EXPECT().UserCanImpersonateServiceAccount(req, "user", "system:serviceaccount:default:test").Return(false, nil)
 
 				return mock
 			},
 			wantErr: "not allowed to impersonate service account",
+			status:  http.StatusForbidden,
 		},
-		"impersonate user error": {
+		{
+			desc: "impersonate user error",
 			req: func() *http.Request {
 				ctx := request.WithUser(context.Background(), userInfo)
 				req := &http.Request{
@@ -243,15 +336,17 @@ func TestAuthenticateImpersonation(t *testing.T) {
 
 				return req
 			},
-			sarMock: func(req *http.Request) sar.SubjectAccessReview {
+			sar: func(req *http.Request) sar.SubjectAccessReview {
 				mock := mocks.NewMockSubjectAccessReview(ctrl)
 				mock.EXPECT().UserCanImpersonateUser(req, "user", "impUser").Return(false, errors.New("unexpected error"))
 
 				return mock
 			},
 			wantErr: "error checking if user can impersonate user: unexpected error",
+			status:  http.StatusForbidden,
 		},
-		"impersonate group error": {
+		{
+			desc: "impersonate group error",
 			req: func() *http.Request {
 				ctx := request.WithUser(context.Background(), userInfo)
 				req := &http.Request{
@@ -264,7 +359,7 @@ func TestAuthenticateImpersonation(t *testing.T) {
 
 				return req
 			},
-			sarMock: func(req *http.Request) sar.SubjectAccessReview {
+			sar: func(req *http.Request) sar.SubjectAccessReview {
 				mock := mocks.NewMockSubjectAccessReview(ctrl)
 				mock.EXPECT().UserCanImpersonateUser(req, "user", "impUser").Return(true, nil)
 				mock.EXPECT().UserCanImpersonateGroup(req, "user", "impGroup").Return(false, errors.New("unexpected error"))
@@ -272,8 +367,10 @@ func TestAuthenticateImpersonation(t *testing.T) {
 				return mock
 			},
 			wantErr: "error checking if user can impersonate group: unexpected error",
+			status:  http.StatusForbidden,
 		},
-		"impersonate extras error": {
+		{
+			desc: "impersonate extras error",
 			req: func() *http.Request {
 				ctx := request.WithUser(context.Background(), userInfo)
 				req := &http.Request{
@@ -286,7 +383,7 @@ func TestAuthenticateImpersonation(t *testing.T) {
 
 				return req
 			},
-			sarMock: func(req *http.Request) sar.SubjectAccessReview {
+			sar: func(req *http.Request) sar.SubjectAccessReview {
 				mock := mocks.NewMockSubjectAccessReview(ctrl)
 				mock.EXPECT().UserCanImpersonateUser(req, "user", "impUser").Return(true, nil)
 				mock.EXPECT().UserCanImpersonateExtras(req, "user", map[string][]string{"foo": {"bar"}}).Return(false, errors.New("unexpected error"))
@@ -294,8 +391,41 @@ func TestAuthenticateImpersonation(t *testing.T) {
 				return mock
 			},
 			wantErr: "error checking if user can impersonate extras: unexpected error",
+			status:  http.StatusForbidden,
 		},
-		"impersonate service account error": {
+		{
+			desc: "validating request token error",
+			req: func() *http.Request {
+				ctx := request.WithUser(context.Background(), userInfo)
+				req := &http.Request{
+					Header: map[string][]string{
+						"Impersonate-User":                 {"impUser"},
+						"Impersonate-Extra-requesttokenid": {"kubeconfig-u-user5zfww"},
+					},
+				}
+				req = req.WithContext(ctx)
+
+				return req
+			},
+			sar: func(req *http.Request) sar.SubjectAccessReview {
+				mock := mocks.NewMockSubjectAccessReview(ctrl)
+				mock.EXPECT().UserCanImpersonateUser(req, "user", "impUser").Return(true, nil)
+				mock.EXPECT().UserCanImpersonateExtras(req, "user", map[string][]string{
+					"requesttokenid": {"kubeconfig-u-user5zfww"},
+				}).Return(true, nil)
+
+				return mock
+			},
+			tokenCache: func() mgmtv3.TokenCache {
+				cache := fake.NewMockNonNamespacedCacheInterface[*v3.Token](ctrl)
+				cache.EXPECT().Get("kubeconfig-u-user5zfww").Return(nil, errors.New("unexpected error"))
+				return cache
+			},
+			wantErr: "error getting request token",
+			status:  http.StatusForbidden,
+		},
+		{
+			desc: "impersonate service account error",
 			req: func() *http.Request {
 				ctx := request.WithUser(context.Background(), userInfo)
 				req := &http.Request{
@@ -307,42 +437,48 @@ func TestAuthenticateImpersonation(t *testing.T) {
 
 				return req
 			},
-			sarMock: func(req *http.Request) sar.SubjectAccessReview {
+			sar: func(req *http.Request) sar.SubjectAccessReview {
 				mock := mocks.NewMockSubjectAccessReview(ctrl)
 				mock.EXPECT().UserCanImpersonateServiceAccount(req, "user", "system:serviceaccount:default:test").Return(false, errors.New("unexpected error"))
 
 				return mock
 			},
 			wantErr: "error checking if user can impersonate service account: unexpected error",
+			status:  http.StatusForbidden,
 		},
 	}
 
-	for name, test := range tests {
+	for _, test := range tests {
 		test := test
-		t.Run(name, func(t *testing.T) {
+		t.Run(test.desc, func(t *testing.T) {
 			t.Parallel()
 
 			req := test.req()
-			ia := NewImpersonatingAuth(test.sarMock(req))
+			ia := &ImpersonatingAuth{sar: test.sar(req)}
+			if test.tokenCache != nil {
+				ia.tokenCache = test.tokenCache()
+			}
+
 			mh := &mockHandler{}
-			rw := httptest.NewRecorder()
 			h := ia.ImpersonationMiddleware(mh)
 
+			rw := httptest.NewRecorder()
 			h.ServeHTTP(rw, req)
 
-			assert.Equal(t, test.wantNextHandlerCalled, mh.serveHTTPWasCalled)
+			require.Equal(t, test.status, rw.Code)
+
 			if test.wantErr != "" {
 				bodyBytes, err := io.ReadAll(rw.Body)
 				assert.NoError(t, err)
 				assert.Contains(t, string(bodyBytes), test.wantErr)
 			}
+
 			if test.wantUserInfo != nil {
 				info, _ := request.UserFrom(req.Context())
 				assert.Equal(t, test.wantUserInfo, info)
-			} else {
-				info, _ := request.UserFrom(req.Context())
-				assert.Equal(t, userInfo, info)
 			}
+
+			assert.Equal(t, test.wantNextHandlerCalled, mh.serveHTTPWasCalled)
 		})
 	}
 }
