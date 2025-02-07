b66746e880bf013a279d34a7e4c656371693acf8
vardhaman22 <vardhaman.surana@suse.com>
2025-02-07 14:18:02 +0530

    enabled machine drain rule crd

--- pkg/crds/provisioningv2/crds.go
+++ pkg/crds/provisioningv2/crds.go
@@ -22,6 +22,7 @@ var (
 		"MachineDeployment":  true,
 		"MachineSet":         true,
 		"Cluster":            true,
+		"MachineDrainRule":   true,
 	}
 
 	//go:embed capi-crds.yaml capi-webhooks.yaml
