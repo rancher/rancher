03797f96504596fa9207902cacce61e31436bfed
Andreas Kupries <andreas.kupries@suse.com>
2025-02-14 11:26:24 +0100

    comments - move common provider code into helper functions comments - keep interface and ability for customization of divergent providers.

--- pkg/auth/providers/activedirectory/activedirectory_provider.go
+++ pkg/auth/providers/activedirectory/activedirectory_provider.go
@@ -255,27 +255,11 @@ func (p *adProvider) getDNAndScopeFromPrincipalID(principalID string) (string, s
 }
 
 func (p *adProvider) GetUserExtraAttributes(userPrincipal v3.Principal) map[string][]string {
-	extras := make(map[string][]string)
-	if userPrincipal.Name != "" {
-		extras[common.UserAttributePrincipalID] = []string{userPrincipal.Name}
-	}
-	if userPrincipal.LoginName != "" {
-		extras[common.UserAttributeUserName] = []string{userPrincipal.LoginName}
-	}
-	return extras
+	return common.GetCommonUserExtraAttributes(userPrincipal)
 }
 
 func (p *adProvider) GetUserExtraAttributesFromToken(token accessor.TokenAccessor) map[string][]string {
-	principalID := token.GetUserPrincipalID()
-	userName := token.GetUserName()
-	extras := make(map[string][]string)
-	if principalID != "" {
-		extras[common.UserAttributePrincipalID] = []string{principalID}
-	}
-	if userName != "" {
-		extras[common.UserAttributeUserName] = []string{userName}
-	}
-	return extras
+	return common.GetCommonUserExtraAttributesFromToken(token)
 }
 
 type LoginDisabledError struct{}
--- pkg/auth/providers/github/github_provider.go
+++ pkg/auth/providers/github/github_provider.go
@@ -431,27 +431,11 @@ func (g *ghProvider) CanAccessWithGroupProviders(userPrincipalID string, groupPr
 }
 
 func (g *ghProvider) GetUserExtraAttributes(userPrincipal v3.Principal) map[string][]string {
-	extras := make(map[string][]string)
-	if userPrincipal.Name != "" {
-		extras[common.UserAttributePrincipalID] = []string{userPrincipal.Name}
-	}
-	if userPrincipal.LoginName != "" {
-		extras[common.UserAttributeUserName] = []string{userPrincipal.LoginName}
-	}
-	return extras
+	return common.GetCommonUserExtraAttributes(userPrincipal)
 }
 
 func (g *ghProvider) GetUserExtraAttributesFromToken(token accessor.TokenAccessor) map[string][]string {
-	principalID := token.GetUserPrincipalID()
-	userName := token.GetUserName()
-	extras := make(map[string][]string)
-	if principalID != "" {
-		extras[common.UserAttributePrincipalID] = []string{principalID}
-	}
-	if userName != "" {
-		extras[common.UserAttributeUserName] = []string{userName}
-	}
-	return extras
+	return common.GetCommonUserExtraAttributesFromToken(token)
 }
 
 // IsDisabledProvider checks if the GitHub auth provider is currently disabled in Rancher.
