5b952f3321fdb41b54e6b31873a9a7b149731ef0
Julia Bier <juliabiersuriano@gmail.com>
2025-02-18 12:37:49 -0400

    bump shell v0.1.28-rc.1

--- pkg/buildconfig/constants.go
+++ pkg/buildconfig/constants.go
@@ -4,7 +4,7 @@ package buildconfig
 
 const (
 	CspAdapterMinVersion = "103.0.1+up3.0.1"
-	DefaultShellVersion  = "rancher/shell:v0.1.26"
+	DefaultShellVersion  = "rancher/shell:v0.1.28-rc.1"
 	FleetVersion         = "103.1.13+up0.9.14-rc.1"
 	WebhookVersion       = "103.0.14+up0.4.15"
 )
