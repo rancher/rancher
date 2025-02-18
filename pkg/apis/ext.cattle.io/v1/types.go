f8a3acd64ab0dc467007e504a718c4b595ae166d
Alessio Greggi <alessio.greggi@suse.com>
2025-02-18 12:31:25 +0100

    style: add comments to the UserActivity resource
    Signed-off-by: Alessio Greggi <alessio.greggi@suse.com>

--- pkg/apis/ext.cattle.io/v1/types.go
+++ pkg/apis/ext.cattle.io/v1/types.go
@@ -20,10 +20,21 @@ type UserActivity struct {
 }
 
 type UserActivitySpec struct {
+	// TokenId is the token Id for which the UserActivity will update
+	// the LastIdleTimeout value on the Token resource.
+	// +optional
 	TokenId string `json:"tokenId"`
 }
 
 type UserActivityStatus struct {
+	// CurrentTimeout is the timestap of the idle timeout.
+	// The idle timeout is calculated by adding the
+	// auth-user-session-idle-ttl-minutes attribute to the time
+	// the request is made.
+	// +optional
 	CurrentTimeout string `json:"currentTimeout"`
-	LastActivity   string `json:"lastActivity"`
+	// LastActivity is the timestamp of the last user activity
+	// tracked by the UI.
+	// +optional
+	LastActivity string `json:"lastActivity,omitempty"`
 }
