c3076e93a8d829a1b49a4168ef8309fed53f4079
Kevin McDermott <bigkevmcd@gmail.com>
2025-02-20 10:50:56 +0000

    Add simple migration for preferences.

---
+++ pkg/migrations/preferences/migration.go
@@ -0,0 +1,145 @@
+package preferences
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/rancher/rancher/pkg/migrations"
+	"github.com/rancher/rancher/pkg/migrations/changes"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/types"
+)
+
+func init() {
+	// NOTE: This is not registered.
+	// migrations.Register(preferencesMigration{})
+}
+
+type preferencesMigration struct {
+}
+
+// Name implements the Migration interface.
+func (t preferencesMigration) Name() string {
+	return "preferences-migration"
+}
+
+// Changes implements the Migration interface.
+//
+// This would be a good example to use the MigrationOption.Continue and
+// per-user ChangeSets.
+func (t preferencesMigration) Changes(ctx context.Context, client changes.Interface, opts migrations.MigrationOptions) (*migrations.MigrationChanges, error) {
+	userPreferences, err := client.Resource(schema.GroupVersionResource{
+		Resource: "preferences",
+		Group:    "management.cattle.io",
+		Version:  "v3",
+	}).List(ctx, metav1.ListOptions{})
+	if err != nil {
+		return nil, fmt.Errorf("listing preferences to calculate migration: %s", err)
+	}
+
+	combined, err := combinePreferences(userPreferences.Items)
+	if err != nil {
+		return nil, fmt.Errorf("combining preferences to calculate migration: %s", err)
+	}
+
+	var resourceChanges []changes.ResourceChange
+	var migrationErr error
+	for k, v := range combined {
+		configMap := newConfigMap(k+"-preferences", k, v.data)
+		raw, err := runtime.DefaultUnstructuredConverter.ToUnstructured(configMap)
+		if err != nil {
+			// TODO: improve this error
+			migrationErr = errors.Join(migrationErr, err)
+			continue
+		}
+
+		resourceChanges = append(resourceChanges,
+			changes.ResourceChange{
+				Operation: changes.OperationCreate,
+				Create: &changes.CreateChange{
+					Resource: &unstructured.Unstructured{Object: raw}},
+			},
+		)
+
+		for _, preferenceName := range v.resources {
+			resourceChanges = append(resourceChanges,
+				changes.ResourceChange{
+					Operation: changes.OperationDelete,
+					Delete: &changes.DeleteChange{
+						ResourceRef: changes.ResourceReference{
+							ObjectRef: types.NamespacedName{
+								Name:      preferenceName.Name,
+								Namespace: preferenceName.Namespace,
+							},
+							Group:    "management.cattle.io",
+							Resource: "preferences",
+							Version:  "v3",
+						},
+					},
+				})
+		}
+	}
+
+	if migrationErr != nil {
+		return nil, migrationErr
+	}
+
+	return &migrations.MigrationChanges{Changes: resourceChanges}, nil
+}
+
+// Combine a set of preferences by user returning a map with the combined
+// preferences.
+
+type combinedPreferences struct {
+	data      map[string]string
+	resources []types.NamespacedName
+}
+
+func combinePreferences(preferences []unstructured.Unstructured) (map[string]*combinedPreferences, error) {
+	combined := map[string]*combinedPreferences{}
+
+	for _, preference := range preferences {
+		username := preference.GetNamespace()
+
+		current := combined[username]
+		if current == nil {
+			current = &combinedPreferences{data: map[string]string{}}
+		}
+		preferenceName := types.NamespacedName{Name: preference.GetName(), Namespace: preference.GetNamespace()}
+		// These are to be removed.
+		current.resources = append(current.resources, preferenceName)
+
+		key := preference.GetName()
+		value, found, err := unstructured.NestedString(preference.Object, "value")
+		if err != nil {
+			return nil, fmt.Errorf("invalid preference resource %s: %w", preferenceName, err)
+		}
+		if !found || value == "" {
+			// If there's no value - skip
+			continue
+		}
+		current.data[key] = value
+		combined[username] = current
+	}
+
+	return combined, nil
+}
+
+func newConfigMap(username, namespace string, data map[string]string) *corev1.ConfigMap {
+	return &corev1.ConfigMap{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "ConfigMap",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      username,
+			Namespace: namespace,
+		},
+		Data: data,
+	}
+}
---
+++ pkg/migrations/preferences/migration_test.go
@@ -0,0 +1,154 @@
+package preferences
+
+import (
+	"context"
+	"sort"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/dynamic/fake"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+
+	v3 "github.com/rancher/rancher/pkg/apis/management.cattle.io/v3"
+	"github.com/rancher/rancher/pkg/migrations"
+	"github.com/rancher/rancher/pkg/migrations/changes"
+	"github.com/rancher/rancher/pkg/migrations/test"
+)
+
+func TestMigrationChanges(t *testing.T) {
+	m := preferencesMigration{}
+	preference1 := newTestUserPreference("user-d7tv1", "locale", "en-gb")
+	preference2 := newTestUserPreference("user-xn8mf", "locale", "en-us")
+	preference3 := newTestUserPreference("user-xn8mf", "seen-whatsnew", "dev")
+
+	client := newFakeClient(t,
+		preference1, preference2, preference3,
+	)
+
+	result, err := m.Changes(context.TODO(), changes.ClientFrom(client), migrations.MigrationOptions{})
+	require.NoError(t, err)
+
+	want := &migrations.MigrationChanges{
+		Changes: []changes.ResourceChange{
+			{
+				Operation: changes.OperationCreate,
+				Create: &changes.CreateChange{
+					Resource: test.ToUnstructured(t,
+						&corev1.ConfigMap{
+							TypeMeta: metav1.TypeMeta{
+								Kind:       "ConfigMap",
+								APIVersion: "v1",
+							},
+							ObjectMeta: metav1.ObjectMeta{
+								Name:      "user-d7tv1-preferences",
+								Namespace: "user-d7tv1",
+							},
+							Data: map[string]string{
+								"locale": "en-gb",
+							},
+						}),
+				},
+			},
+			{
+				Operation: changes.OperationDelete,
+				Delete: &changes.DeleteChange{
+					ResourceRef: changes.ResourceReference{
+						ObjectRef: types.NamespacedName{
+							Name:      preference1.Name,
+							Namespace: preference1.Namespace,
+						},
+						Group:    "management.cattle.io",
+						Resource: "preferences",
+						Version:  "v3",
+					},
+				},
+			},
+			{
+				Operation: changes.OperationCreate,
+				Create: &changes.CreateChange{
+					Resource: test.ToUnstructured(t,
+						&corev1.ConfigMap{
+							TypeMeta: metav1.TypeMeta{
+								Kind:       "ConfigMap",
+								APIVersion: "v1",
+							},
+							ObjectMeta: metav1.ObjectMeta{
+								Name:      "user-xn8mf-preferences",
+								Namespace: "user-xn8mf",
+							},
+							Data: map[string]string{
+								"locale":        "en-us",
+								"seen-whatsnew": "dev",
+							},
+						}),
+				},
+			},
+			{
+				Operation: changes.OperationDelete,
+				Delete: &changes.DeleteChange{
+					ResourceRef: changes.ResourceReference{
+						ObjectRef: types.NamespacedName{
+							Name:      preference2.Name,
+							Namespace: preference2.Namespace,
+						},
+						Group:    "management.cattle.io",
+						Resource: "preferences",
+						Version:  "v3",
+					},
+				},
+			},
+			{
+				Operation: changes.OperationDelete,
+				Delete: &changes.DeleteChange{
+					ResourceRef: changes.ResourceReference{
+						ObjectRef: types.NamespacedName{
+							Name:      preference3.Name,
+							Namespace: preference3.Namespace,
+						},
+						Group:    "management.cattle.io",
+						Resource: "preferences",
+						Version:  "v3",
+					},
+				},
+			},
+		},
+	}
+
+	sort.Slice(result.Changes, func(i, j int) bool {
+		switch {
+		case result.Changes[i].Create != nil && result.Changes[j].Create != nil:
+			return result.Changes[i].Create.Resource.GetName() < result.Changes[j].Create.Resource.GetName()
+		case result.Changes[i].Delete != nil && result.Changes[j].Delete != nil:
+			return result.Changes[i].Delete.ResourceRef.ObjectRef.String() < result.Changes[j].Delete.ResourceRef.ObjectRef.String()
+		default:
+			return false
+		}
+	})
+
+	assert.Equal(t, want, result)
+}
+
+func newFakeClient(t *testing.T, objs ...runtime.Object) *fake.FakeDynamicClient {
+	testScheme := runtime.NewScheme()
+	require.NoError(t, clientgoscheme.AddToScheme(testScheme))
+	return fake.NewSimpleDynamicClient(testScheme, objs...)
+}
+
+func newTestUserPreference(username, name, value string) *v3.Preference {
+	return &v3.Preference{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Preference",
+			APIVersion: "management.cattle.io/v3",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: username,
+		},
+		Value: value,
+	}
+}
