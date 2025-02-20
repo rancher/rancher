889ac4e49919118c75fde5ab5ff2f505d0348658
Alessio Greggi <alessio.greggi@suse.com>
2025-02-21 00:17:36 +0100

    unify Create and change test accordingly
    Signed-off-by: Alessio Greggi <alessio.greggi@suse.com>

--- pkg/ext/stores/useractivity/store_test.go
+++ pkg/ext/stores/useractivity/store_test.go
@@ -2,7 +2,6 @@ package useractivity
 
 import (
 	"context"
-	"errors"
 	"fmt"
 	"reflect"
 	"testing"
@@ -14,18 +13,23 @@ import (
 	v3 "github.com/rancher/rancher/pkg/generated/norman/management.cattle.io/v3"
 	wranglerfake "github.com/rancher/wrangler/v3/pkg/generic/fake"
 	"go.uber.org/mock/gomock"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
 	k8suser "k8s.io/apiserver/pkg/authentication/user"
 	"k8s.io/apiserver/pkg/endpoints/request"
+	"k8s.io/apiserver/pkg/registry/rest"
 )
 
-func TestStore_create(t *testing.T) {
+func TestStoreCreate(t *testing.T) {
 	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
 	mockTokenControllerFake := wranglerfake.NewMockNonNamespacedControllerInterface[*v3Legacy.Token, *v3Legacy.TokenList](ctrl)
 	mockTokenCacheFake := wranglerfake.NewMockNonNamespacedCacheInterface[*v3Legacy.Token](ctrl)
 	mockUserCacheFake := wranglerfake.NewMockNonNamespacedCacheInterface[*v3.User](ctrl)
+
 	uas := &Store{
 		tokens:     mockTokenControllerFake,
 		tokenCache: mockTokenCacheFake,
@@ -33,13 +37,12 @@ func TestStore_create(t *testing.T) {
 	}
 
 	type args struct {
-		in0          context.Context
-		userActivity *ext.UserActivity
-		token        *v3Legacy.Token
-		lastActivity v1.Time
-		idleMins     int
-		dryRun       bool
+		ctx          context.Context
+		obj          *ext.UserActivity
+		validateFunc rest.ValidateObjectFunc
+		options      *metav1.CreateOptions
 	}
+
 	tests := []struct {
 		name      string
 		args      args
@@ -50,113 +53,82 @@ func TestStore_create(t *testing.T) {
 		{
 			name: "valid useractivity is created",
 			args: args{
-				in0: nil,
-				userActivity: &ext.UserActivity{
+				ctx: request.WithUser(context.Background(), &k8suser.DefaultInfo{
+					Name:   "admin",
+					Groups: []string{GroupCattleAuthenticated},
+					Extra: map[string][]string{
+						common.ExtraRequestTokenID: {"token-12345"},
+					},
+				}),
+				obj: &ext.UserActivity{
 					ObjectMeta: v1.ObjectMeta{
-						Name: "u-mo773yttt4",
+						Name: "token-12345",
 					},
 				},
-				token: &v3Legacy.Token{
-					ObjectMeta: v1.ObjectMeta{
-						Name: "u-mo773yttt4",
-					},
-					UserID: "u-mo773yttt4",
-				},
-				lastActivity: v1.Time{
-					Time: time.Date(2025, 1, 31, 16, 44, 0, 0, &time.Location{}),
-				},
-				idleMins: 10,
+				validateFunc: nil,
+				options:      nil,
 			},
 			mockSetup: func() {
-				// we don't care about the object returned by the Patch function,
-				// since we only check there are no errors.
-				mockTokenControllerFake.EXPECT().Patch(gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any()).DoAndReturn(
-					func(name string, pt types.PatchType, data []byte, subresources ...string) (*v3Legacy.Token, error) {
-						return &v3Legacy.Token{}, nil
-					},
-				).Times(1)
+				gomock.InOrder(
+					mockUserCacheFake.EXPECT().Get("admin").Return(&v3.User{
+						ObjectMeta: v1.ObjectMeta{
+							Name: "admin",
+						},
+					}, nil),
+
+					mockTokenCacheFake.EXPECT().Get("token-12345").Return(&v3Legacy.Token{
+						ObjectMeta: v1.ObjectMeta{
+							Name: "token-12345",
+						},
+						AuthProvider:  "oidc",
+						UserPrincipal: v3.Principal{},
+					}, nil),
+
+					mockTokenCacheFake.EXPECT().Get("token-12345").Return(&v3Legacy.Token{
+						ObjectMeta: v1.ObjectMeta{
+							Name: "token-12345",
+						},
+						AuthProvider:  "oidc",
+						UserPrincipal: v3.Principal{},
+					}, nil),
+
+					mockTokenControllerFake.EXPECT().Patch("token-12345", types.JSONPatchType, gomock.Any()).Return(&v3Legacy.Token{}, nil),
+				)
 			},
 			want: &ext.UserActivity{
 				ObjectMeta: v1.ObjectMeta{
-					Name: "u-mo773yttt4",
+					Name: "token-12345",
 				},
 				Status: ext.UserActivityStatus{
-					ExpiresAt: time.Date(2025, 1, 31, 16, 54, 0, 0, &time.Location{}).String(),
-				},
-			},
-			wantErr: false,
-		},
-		{
-			name: "error updating token value LastIdleTimeout",
-			args: args{
-				in0: nil,
-				userActivity: &ext.UserActivity{
-					ObjectMeta: v1.ObjectMeta{
-						Name: "u-mo773yttt4",
-					},
-				},
-				token: &v3Legacy.Token{
-					ObjectMeta: v1.ObjectMeta{},
-					UserID:     "u-mo773yttt4",
-				},
-				lastActivity: v1.Time{
-					Time: time.Date(2025, 1, 31, 16, 44, 0, 0, &time.Location{}).UTC(),
-				},
-				idleMins: 10,
-			},
-			mockSetup: func() {
-				mockTokenControllerFake.EXPECT().Patch(gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any()).DoAndReturn(
-					func(name string, pt types.PatchType, data []byte, subresources ...string) (*v3Legacy.Token, error) {
-						return nil, errors.New("some error happend")
-					},
-				).AnyTimes()
-			},
-			want:    nil,
-			wantErr: true,
-		},
-		{
-			name: "create useractivity with dry-run option",
-			args: args{
-				in0: nil,
-				userActivity: &ext.UserActivity{
-					ObjectMeta: v1.ObjectMeta{
-						Name: "u-mo773yttt4",
-					},
-				},
-				token: &v3Legacy.Token{
-					ObjectMeta: v1.ObjectMeta{
-						Name: "u-mo773yttt4",
-					},
-					UserID: "u-mo773yttt4",
-				},
-				lastActivity: v1.Time{
-					Time: time.Date(2025, 1, 31, 16, 44, 0, 0, &time.Location{}),
-				},
-				idleMins: 10,
-				dryRun:   true,
-			},
-			mockSetup: func() {},
-			want: &ext.UserActivity{
-				ObjectMeta: v1.ObjectMeta{
-					Name: "u-mo773yttt4",
-				},
-				Status: ext.UserActivityStatus{
-					ExpiresAt: time.Date(2025, 1, 31, 16, 54, 0, 0, &time.Location{}).String(),
+					ExpiresAt: metav1.NewTime(time.Date(2025, 2, 2, 0, 54, 0, 0, time.UTC)).String(),
 				},
 			},
 			wantErr: false,
 		},
 	}
+
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			// Mock the time function
+			mockNow := time.Date(2025, 2, 1, 8, 54, 0, 0, time.UTC)
+			origTimeNow := timeNow
+			timeNow = func() time.Time { return mockNow }
+			defer func() { timeNow = origTimeNow }() // Restore original function after test
+
+			// Setup mocks
 			tt.mockSetup()
-			got, err := uas.create(tt.args.in0, tt.args.userActivity, tt.args.token, tt.args.lastActivity, tt.args.idleMins, tt.args.dryRun)
+
+			// Execute function
+			got, err := uas.Create(tt.args.ctx, tt.args.obj, tt.args.validateFunc, tt.args.options)
+
+			// Validate results
 			if (err != nil) != tt.wantErr {
-				t.Errorf("Store.create() error = %v, wantErr %v", err, tt.wantErr)
+				t.Errorf("Store.Create() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
+
 			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("Store.create() = %v, want %v", got, tt.want)
+				t.Errorf("Store.Create() = %#v, want %#v", got, tt.want)
 			}
 		})
 	}
