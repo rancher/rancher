e2ce879473d535f1020be3d6881ebdf0c245135b
Andreas Kupries <andreas.kupries@suse.com>
2025-02-21 11:26:56 +0100

    address the lot of smaller comments

--- pkg/data/management/role_data.go
+++ pkg/data/management/role_data.go
@@ -81,8 +81,8 @@ func addRoles(wrangler *wrangler.Context, management *config.ManagementContext) 
 	userRole.
 		addRule().apiGroups("ext.cattle.io").resources("useractivities").verbs("get", "create").
 		// standard permissions for regular users, on their tokens
-		// BEWARE: The ext token store applies additional restrictions.
-		// In other words, each User can see and manipulate only their own tokens, and no other.
+		// Note: The ext token store applies additional restrictions.
+		// A user can see and manipulate only their own tokens.
 		addRule().apiGroups("ext.cattle.io").resources("tokens").verbs("get", "list", "watch", "create", "delete", "update", "patch").
 		addRule().apiGroups("catalog.cattle.io").resources("clusterrepos").verbs("get", "list", "watch").
 		addRule().apiGroups("management.cattle.io").resources("podsecurityadmissionconfigurationtemplates").verbs("get", "list", "watch")
@@ -92,8 +92,8 @@ func addRoles(wrangler *wrangler.Context, management *config.ManagementContext) 
 	rb.addRole("User Base", "user-base").
 		addRule().apiGroups("ext.cattle.io").resources("useractivities").verbs("get", "create").
 		// standard permissions for regular users, on their tokens
-		// BEWARE: The ext token store applies additional restrictions.
-		// In other words, each User can see and manipulate only their own tokens, and no other.
+		// Note: The ext token store applies additional restrictions.
+		// A user can see and manipulate only their own tokens.
 		addRule().apiGroups("ext.cattle.io").resources("tokens").verbs("get", "list", "watch", "create", "delete", "update", "patch").
 		addRule().apiGroups("management.cattle.io").resources("preferences").verbs("*").
 		addRule().apiGroups("management.cattle.io").resources("settings").verbs("get", "list", "watch").
