2b5b74c013ce50637c7e7875c0968c8bc96d8c9f
Jake Hyde <jakefhyde@gmail.com>
2025-02-21 11:44:02 -0500

    Remove etcd-snapshot list periodic instructions and replace with dummy etcd-snapshot delete to trigger refresh

--- pkg/capr/planner/instructions.go
+++ pkg/capr/planner/instructions.go
@@ -146,34 +146,19 @@ func (p *Planner) addInitNodePeriodicInstruction(nodePlan plan.NodePlan, control
 	return nodePlan, nil
 }
 
-func (p *Planner) addEtcdSnapshotListLocalPeriodicInstruction(nodePlan plan.NodePlan, controlPlane *rkev1.RKEControlPlane) (plan.NodePlan, error) {
+func (p *Planner) addEtcdSnapshotListRefreshPeriodicInstruction(nodePlan plan.NodePlan, controlPlane *rkev1.RKEControlPlane) (plan.NodePlan, error) {
 	nodePlan.PeriodicInstructions = append(nodePlan.PeriodicInstructions, plan.PeriodicInstruction{
-		Name:    "etcd-snapshot-list-local",
+		Name:    "etcd-snapshot-list-refresh",
 		Command: "sh",
 		Args: []string{
 			"-c",
-			// the grep here is to make the command fail if we don't get the output we expect, like empty string.
-			fmt.Sprintf("%s etcd-snapshot list --etcd-s3=false 2>/dev/null",
+			fmt.Sprintf("%s etcd-snapshot delete _SNAPSHOT_REFRESH_TRIGGER_ > /dev/null 2>&1",
 				capr.GetRuntime(controlPlane.Spec.KubernetesVersion)),
 		},
 		PeriodSeconds: 600,
 	})
 	return nodePlan, nil
-}
 
-func (p *Planner) addEtcdSnapshotListS3PeriodicInstruction(nodePlan plan.NodePlan, controlPlane *rkev1.RKEControlPlane) (plan.NodePlan, error) {
-	nodePlan.PeriodicInstructions = append(nodePlan.PeriodicInstructions, plan.PeriodicInstruction{
-		Name:    "etcd-snapshot-list-s3",
-		Command: "sh",
-		Args: []string{
-			"-c",
-			// the grep here is to make the command fail if we don't get the output we expect, like empty string.
-			fmt.Sprintf("%s etcd-snapshot list --etcd-s3 2>/dev/null",
-				capr.GetRuntime(controlPlane.Spec.KubernetesVersion)),
-		},
-		PeriodSeconds: 600,
-	})
-	return nodePlan, nil
 }
 
 // generateManifestRemovalInstruction generates a rm -rf command for the manifests of a server. This was created in response to https://github.com/rancher/rancher/issues/41174
