a8ce5d8ad168f3a58a5589aa4534e73147e6f01f
Felipe C. Gehrke <felipe.gehrke@suse.com>
2025-02-21 18:24:46 -0300

    fixed format

--- pkg/buildconfig/constants.go
+++ pkg/buildconfig/constants.go
@@ -3,10 +3,10 @@
 package buildconfig
 
 const (
-	CspAdapterMinVersion    = "105.0.0+up5.0.1-rc1"
-	DefaultShellVersion     = "rancher/shell:v0.4.0-rc.1"
-	FleetVersion            = "106.0.0+up0.12.0-alpha.14"
-	ProvisioningCAPIVersion = "106.0.0+up0.7.0"
+	CspAdapterMinVersion     = "105.0.0+up5.0.1-rc1"
+	DefaultShellVersion      = "rancher/shell:v0.4.0-rc.1"
+	FleetVersion             = "106.0.0+up0.12.0-alpha.14"
+	ProvisioningCAPIVersion  = "106.0.0+up0.7.0"
 	RemoteDialerProxyVersion = "106.0.0+up0.4.4-rc.2"
-	WebhookVersion          = "106.0.0+up0.7.0-rc.5"
+	WebhookVersion           = "106.0.0+up0.7.0-rc.5"
 )
