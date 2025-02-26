af92ecdd19625f7f02bc2cc9a2d307c1748b76b8
joshmeranda <joshua.meranda@gmail.com>
2025-02-26 13:02:05 -0500

    set imperative api feature default to true

--- pkg/features/feature.go
+++ pkg/features/feature.go
@@ -166,7 +166,7 @@ var (
 	ImperativeApiExtension = newFeature(
 		"imperative-api-extension",
 		"Enable imperative API extension as a k8s aggregation layer as proxy to the kube apiserver",
-		false,
+		true,
 		false,
 		true)
 )
