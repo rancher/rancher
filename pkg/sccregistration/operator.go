eb7268659c35d2dcfe5e5587b71091906a83ecae
Dan Pock <self@danpock.me>
2025-03-10 21:38:59 -0400

    WIP controller

--- pkg/sccregistration/operator.go
+++ pkg/sccregistration/operator.go
@@ -2,6 +2,7 @@ package sccregistration
 
 import (
 	"context"
+	sccv1 "github.com/rancher/rancher/pkg/apis/scc.cattle.io/v1"
 	"github.com/rancher/rancher/pkg/wrangler"
 )
 
@@ -13,11 +14,15 @@ func Register(
 		ctx,
 	}
 
-	wContext.Mgmt
+	wContext.SCC.RegistrationRequest().OnChange(ctx, "sccRegistrationRequests", h.OnChange)
 
 	return nil
 }
 
 type handler struct {
 	ctx context.Context
+}
+
+func (h *handler) OnChange(key string, registrationRequest *sccv1.RegistrationRequest) (*sccv1.RegistrationRequest, error) {
+	return registrationRequest, nil
 }
