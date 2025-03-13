4dac6ad0247daa4ded2f552995e35d060e06623e
Venkata Krishna Rohit Sakala <rohit.sakala@suse.com>
2025-03-13 11:57:26 -0700

    Fix the calculateBackoff to not panic

--- pkg/controllers/dashboard/helm/repo_oci.go
+++ pkg/controllers/dashboard/helm/repo_oci.go
@@ -347,8 +347,13 @@ func calculateBackoff(clusterRepo *catalog.ClusterRepo, policy retryPolicy) time
 	h.SetSeed(maphash.MakeSeed())
 	rand := rand.New(rand.NewSource(int64(h.Sum64())))
 	temp := float64(policy.MinWait) * math.Pow(2, float64(clusterRepo.Status.NumberOfRetries))
-	// rand.Int63n panics if jitter <= 0
-	jitter := int64(math.Max(1, 2*0.2*temp))
+	if temp > math.MaxInt64 {
+		temp = math.MaxInt64 / 3
+	}
+	jitter := int64(2 * 0.2 * temp)
+	if jitter <= 0 {
+		jitter = 1
+	}
 	backoff := time.Duration(temp*(1-0.2)) + time.Duration(rand.Int63n(jitter))
 	if backoff < policy.MinWait {
 		return policy.MinWait
