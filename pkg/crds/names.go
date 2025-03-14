2a2f0e9317f69d05c34038824912c5698db4a25f
joshmeranda <joshua.meranda@suse.com>
2025-03-14 01:17:31 -0400

    update crd spec and generation

--- pkg/crds/names.go
+++ pkg/crds/names.go
@@ -46,6 +46,7 @@ func BasicCRDs() []string {
 		"preferences.management.cattle.io",
 		"settings.management.cattle.io",
 		"navlinks.ui.cattle.io",
+		"auditlogpolicies.auditlog.cattle.io",
 	}
 }
 
@@ -256,4 +257,5 @@ var MigratedResources = map[string]bool{
 	"users.management.cattle.io":                                      false,
 	"uiplugins.catalog.cattle.io":                                     true,
 	"workloads.project.cattle.io":                                     false,
+	"auditlogpolicies.auditlog.cattle.io":                             true,
 }
