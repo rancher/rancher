b785e47311a88f63c58d38edc6a379ed5ab98794
joshmeranda <joshua.meranda@suse.com>
2025-03-21 14:40:48 -0400

    add response decompression tests

--- pkg/auth/audit/log.go
+++ pkg/auth/audit/log.go
@@ -2,6 +2,8 @@ package audit
 
 import (
 	"bytes"
+	"compress/gzip"
+	"compress/zlib"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -14,7 +16,11 @@ import (
 )
 
 const (
-	contentTypeJSON  = "application/json"
+	contentEncodingGZIP = "gzip"
+	contentEncodingZLib = "deflate"
+
+	contentTypeJSON = "application/json"
+
 	auditLogErrorKey = "auditLogError"
 )
 
@@ -24,6 +30,44 @@ var (
 		http.MethodPost: true,
 	}
 )
+
+func decompressGZIP(data []byte) ([]byte, error) {
+	gz, err := gzip.NewReader(bytes.NewReader(data))
+	if err != nil {
+		return nil, fmt.Errorf("failed to create gzip reader: %w", err)
+	}
+
+	return decompress(gz)
+}
+
+func decompressZLib(data []byte) ([]byte, error) {
+	zr, err := zlib.NewReader(bytes.NewReader(data))
+	if err != nil {
+		return nil, fmt.Errorf("failed to create zlib reader: %w", err)
+	}
+
+	return decompress(zr)
+}
+
+func decompress(readCloser io.ReadCloser) ([]byte, error) {
+	rawData, err := io.ReadAll(readCloser)
+	if err != nil {
+		retErr := fmt.Errorf("failed to read compressed response: %w", err)
+		closeErr := readCloser.Close()
+		if closeErr != nil {
+			// Using %v for close error because you can currently only wrap one error.
+			// The read error is more important to the caller in this instance.
+			retErr = fmt.Errorf("%w; failed to close readCloser: %v", retErr, closeErr)
+		}
+		return nil, retErr
+	}
+
+	if err = readCloser.Close(); err != nil {
+		return rawData, fmt.Errorf("failed to close reader: %w", err)
+	}
+
+	return rawData, nil
+}
 
 type log struct {
 	AuditID       k8stypes.UID `json:"auditID,omitempty"`
@@ -86,6 +130,31 @@ func newLog(userInfo *User, req *http.Request, rw *wrapWriter, reqTimestamp stri
 	return log, nil
 }
 
+func (l *log) decompressResponse() error {
+	var err error
+	var decompressed []byte
+
+	switch contentType := l.ResponseHeader.Get("Content-Encoding"); contentType {
+	case "", "none":
+		// not encoded do nothing
+		return nil
+	case contentEncodingGZIP:
+		decompressed, err = decompressGZIP(l.rawResponseBody)
+	case contentEncodingZLib:
+		decompressed, err = decompressZLib(l.rawResponseBody)
+	default:
+		err = fmt.Errorf("%w '%s' in resopnse header", ErrUnsupportedEncoding, contentType)
+	}
+
+	if err != nil {
+		return fmt.Errorf("failed to decode response body: %w", err)
+	}
+
+	l.rawResponseBody = decompressed
+
+	return nil
+}
+
 func (l *log) prepare(verbosity auditlogv1.LogVerbosity) {
 	if !verbosity.Request.Headers {
 		l.RequestHeader = nil
@@ -105,6 +174,12 @@ func (l *log) prepare(verbosity auditlogv1.LogVerbosity) {
 	l.rawRequestBody = nil
 
 	if verbosity.Response.Body && len(l.rawResponseBody) > 0 {
+		if err := l.decompressResponse(); err != nil {
+			l.RequestBody = map[string]any{
+				auditLogErrorKey: fmt.Sprintf("failed to decompressed reuqest body: %s", err.Error()),
+			}
+		}
+
 		if err := json.Unmarshal(l.rawResponseBody, &l.ResponseBody); err != nil {
 			l.ResponseBody = map[string]any{
 				auditLogErrorKey: fmt.Sprintf("failed to unmarshal response body: %s", err.Error()),
