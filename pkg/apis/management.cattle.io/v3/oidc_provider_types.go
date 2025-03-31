c6021f0ae0668f1aef5eaeeed4fd9ba9fdb9dbfa
raul <raul.cabello@suse.com>
2025-03-31 11:45:55 +0200

    improve OIDCClient crd comments
    Signed-off-by: raul <raul.cabello@suse.com>

--- pkg/apis/management.cattle.io/v3/oidc_provider_types.go
+++ pkg/apis/management.cattle.io/v3/oidc_provider_types.go
@@ -11,6 +11,9 @@ import (
 // +genclient
 // +genclient:nonNamespaced
 // +kubebuilder:subresource:status
+// +kubebuilder:printcolumn:name="Client ID",type="string",JSONPath=".status.clientID"
+// +kubebuilder:printcolumn:name="Token Expiration",type="integer",JSONPath=".spec.tokenExpirationSeconds"
+// +kubebuilder:printcolumn:name="RefreshToken Expiration",type="integer",JSONPath=".spec.refreshTokenExpirationSeconds"
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 // +kubebuilder:resource:scope=Cluster
 
@@ -38,11 +41,17 @@ type OIDCClientStatus struct {
 // OIDCClient is a description of the oidc client.
 type OIDCClientSpec struct {
 	// +optional
+	// Description provides additional context about the OIDC client.
+	// This field is optional.
 	Description string `json:"description,omitempty"`
+	// RedirectURIs defines the allowed redirect URIs for the OIDC client.
+	// These URIs must be registered and used during the authentication flow.
 	// +optional
 	RedirectURIs []string `json:"redirectURIs"`
-	// +optional
+	// TokenExpirationSeconds specifies the duration (in seconds) before
+	// an access token and ID token expire.
 	TokenExpirationSeconds time.Duration `json:"tokenExpirationSeconds"`
-	// +optional
+	// RefreshTokenExpirationSeconds defines how long (in seconds)
+	// a refresh token remains valid before expiration.
 	RefreshTokenExpirationSeconds time.Duration `json:"refreshTokenExpirationSeconds"`
 }
