562aeff953f7d17ba3beb81249a21d035a4dd144
Jacob Lindgren <jacob.lindgren@suse.com>
2025-04-02 21:00:34 -0500

    [2.10] Hostbusters removal of md5 usage (#49205) (#49677)
    * change rke certs check comparison to use sha256 rather than md5
    
    * change cloudca checksum to use sha256
    
    * change BaseDriver#getHasher(int) to warn on usage of md5 algorithm
    
    * update the systemtemplate to use sha256 + a different length suffix for cattel-credential
    
    * cleanup cattle-credentials on cluster-agent startup

--- pkg/systemtemplate/template.go
+++ pkg/systemtemplate/template.go
@@ -193,6 +193,8 @@ spec:
             value: "true"
           - name: CATTLE_CLUSTER_REGISTRY
             value: "{{.ClusterRegistry}}"
+          - name: CATTLE_CREDENTIAL_NAME
+            value: cattle-credentials-{{.TokenKey}}
           {{- if .IsPreBootstrap }}
           # since we're on the host network, talk to the apiserver over localhost
           {{- end }}
