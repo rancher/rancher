f4217b4a5749665145d8f9cf5c3b3f9cf0fd1844
Andreas Kupries <andreas.kupries@suse.com>
2025-04-11 11:06:05 +0200

    feat: bring cluster-scoping back to ext tokens note: re-created clustername field note: saving/restore of field note: reject creation for unknown and/or inaccessible clusters note: reject post-creation editing of field note: unit tests

--- pkg/controllers/management/auth/user_test.go
+++ pkg/controllers/management/auth/user_test.go
@@ -389,7 +389,7 @@ func TestUpdated(t *testing.T) {
 
 			timer := exttokens.NewMocktimeHandler(ctrl)
 
-			store := exttokens.NewSystem(nil, nil, secrets, users, nil, timer, nil, nil)
+			store := exttokens.NewSystem(nil, nil, secrets, users, nil, nil, timer, nil, nil)
 			ul.extTokenStore = store
 
 			tt.mockSetup(secrets, scache, timer)
