1158009b3d139574520f42145fb8e0d6eae045de
Jonathan Crowther <jonathan.crowther@suse.com>
2025-04-11 11:46:04 -0400

    Improve Norman store for handling new namespace

--- pkg/api/norman/store/scoped/store.go
+++ pkg/api/norman/store/scoped/store.go
@@ -25,7 +25,7 @@ func NewScopedStore(key string, store types.Store, pLister v3.ProjectLister) *St
 					return data, nil
 				}
 				v := convert.ToString(data[key])
-				if !strings.HasSuffix(v, ":"+convert.ToString(data[client.ProjectFieldNamespaceId])) && !strings.HasSuffix(v, "-"+convert.ToString(data[client.ProjectFieldNamespaceId])) {
+				if !strings.HasSuffix(v, ":"+convert.ToString(data[client.ProjectFieldNamespaceId])) && v != strings.Replace(convert.ToString(data[client.ProjectFieldNamespaceId]), "-", ":", 1) {
 					data[key] = data[client.ProjectFieldNamespaceId]
 				}
 
