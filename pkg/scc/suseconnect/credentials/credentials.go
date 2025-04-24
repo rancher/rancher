8116c104fe84c2f821ccc4e9726a7700ba3ab0f9
Dan Pock <self@danpock.me>
2025-04-24 13:24:42 -0400

    Add scc registration project files

---
+++ pkg/scc/suseconnect/credentials/credentials.go
@@ -0,0 +1,111 @@
+package credentials
+
+import (
+	"fmt"
+
+	"github.com/SUSE/connect-ng/pkg/connection"
+
+	"github.com/pkg/errors"
+)
+
+type CredentialType int
+
+const (
+	CredentialTypeUnconfigured CredentialType = iota
+	CredentialTypeToken
+	CredentialTypeLogin
+	CredentialTypeBoth
+)
+
+var credentialTypeName = map[CredentialType]string{
+	CredentialTypeUnconfigured: "unconfigured",
+	CredentialTypeToken:        "systemToken",
+	CredentialTypeLogin:        "systemLogin",
+	CredentialTypeBoth:         "systemToken and systemLogin",
+}
+
+func (ct CredentialType) String() string {
+	return credentialTypeName[ct]
+}
+
+type SccCredentials struct {
+	systemToken string
+	systemLogin string
+	password    string
+}
+
+// CredentialsType Returns the mode (or modes) that are configured for authentication
+func (c *SccCredentials) CredentialsType() CredentialType {
+	if c.systemToken != "" && c.systemLogin != "" && c.password != "" {
+		return CredentialTypeBoth
+	}
+
+	if c.systemToken != "" {
+		return CredentialTypeToken
+	}
+
+	if c.systemLogin != "" && c.password != "" {
+		return CredentialTypeLogin
+	}
+
+	return CredentialTypeUnconfigured
+}
+
+// HasAuthentication Returns true if we can authenticate at all, false otherwise.
+func (c *SccCredentials) HasAuthentication() bool {
+	configuredType := c.CredentialsType()
+	return configuredType == CredentialTypeBoth
+}
+
+// Token returns the current system used to detect duplicated systems. This
+// systemToken gets rotated on each non read operation.
+func (c *SccCredentials) Token() (string, error) {
+	return c.systemToken, nil
+}
+
+// UpdateToken is called when a systemToken has changed
+func (c *SccCredentials) UpdateToken(newToken string) error {
+	c.systemToken = newToken
+
+	return nil
+}
+
+// Login returns the username and password
+func (c *SccCredentials) Login() (string, string, error) {
+	configuredType := c.CredentialsType()
+	if configuredType != CredentialTypeLogin && configuredType != CredentialTypeBoth {
+		return "", "", errors.New("cannot use systemLogin credentials when they are not properly configured")
+	}
+
+	return c.systemLogin, c.password, nil
+}
+
+// SetLogin updates the saved username and password
+func (c *SccCredentials) SetLogin(newLogin string, newPassword string) error {
+	if newLogin == "" || newPassword == "" {
+		errorMessage := ""
+		if newLogin == "" {
+			errorMessage += "newLogin is empty"
+		}
+		if newPassword == "" {
+			if newLogin == "" {
+				errorMessage += " and "
+			}
+			errorMessage += "newPassword is empty"
+		}
+		return errors.New(fmt.Sprintf("cannot update systemLogin; %v", errorMessage))
+	}
+
+	c.systemLogin = newLogin
+	c.password = newPassword
+
+	return nil
+}
+
+func (c *SccCredentials) SccCredentials() connection.Credentials {
+	return c
+}
+
+func NewCredentials() *SccCredentials {
+	return &SccCredentials{}
+}
---
+++ pkg/scc/suseconnect/credentials/secrets_adapter.go
@@ -0,0 +1,144 @@
+package credentials
+
+import (
+	"github.com/SUSE/connect-ng/pkg/connection"
+	v1core "github.com/rancher/wrangler/v3/pkg/generated/controllers/core/v1"
+	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+const (
+	SecretName  = "rancher-scc-system-credentials"
+	Namespace   = "cattle-system"
+	UsernameKey = "systemLogin"
+	PasswordKey = "password"
+	TokenKey    = "systemToken"
+)
+
+type CredentialSecretsAdapter struct {
+	secrets     v1core.SecretController
+	credentials SccCredentials
+}
+
+func New(secrets v1core.SecretController) *CredentialSecretsAdapter {
+	newAdapterCreds := NewCredentials()
+
+	// Load initial creds from secret
+	sccCreds, err := secrets.Get(Namespace, SecretName, metav1.GetOptions{})
+	if err == nil && sccCreds != nil && len(sccCreds.Data) != 0 {
+		username, _ := sccCreds.Data[UsernameKey]
+		password, _ := sccCreds.Data[PasswordKey]
+		token, _ := sccCreds.Data[TokenKey]
+		_ = newAdapterCreds.SetLogin(string(username), string(password))
+		_ = newAdapterCreds.UpdateToken(string(token))
+	}
+
+	return &CredentialSecretsAdapter{
+		secrets:     secrets,
+		credentials: *newAdapterCreds,
+	}
+}
+
+func (c *CredentialSecretsAdapter) Refresh() error {
+	return c.loadCredentials()
+}
+
+func (c *CredentialSecretsAdapter) loadCredentials() error {
+	// TODO gather errors
+	sccCreds, err := c.secrets.Get(Namespace, SecretName, metav1.GetOptions{})
+	if err == nil && sccCreds != nil && len(sccCreds.Data) != 0 {
+		username, _ := sccCreds.Data[UsernameKey]
+		password, _ := sccCreds.Data[PasswordKey]
+		token, _ := sccCreds.Data[TokenKey]
+		_ = c.credentials.SetLogin(string(username), string(password))
+		_ = c.credentials.UpdateToken(string(token))
+	}
+
+	return nil
+}
+
+func (c *CredentialSecretsAdapter) saveCredentials() error {
+	create := false
+	// TODO gather errors
+	sccCreds, err := c.secrets.Get(Namespace, SecretName, metav1.GetOptions{})
+	if err != nil && !apierrors.IsNotFound(err) {
+		return err
+	}
+
+	if apierrors.IsNotFound(err) {
+		create = true
+		sccCreds = &corev1.Secret{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      SecretName,
+				Namespace: Namespace,
+			},
+			Data: map[string][]byte{},
+		}
+	}
+
+	if sccCreds.Data == nil {
+		sccCreds.Data = make(map[string][]byte)
+	}
+
+	login, pw, err := c.credentials.Login()
+	if err == nil {
+		sccCreds.Data[UsernameKey] = []byte(login)
+		sccCreds.Data[PasswordKey] = []byte(pw)
+	}
+
+	token, err := c.credentials.Token()
+	if err == nil {
+		sccCreds.Data[TokenKey] = []byte(token)
+	}
+
+	var createOrUpdateErr error
+	if create {
+		_, createOrUpdateErr = c.secrets.Create(sccCreds)
+	} else {
+		_, createOrUpdateErr = c.secrets.Update(sccCreds)
+	}
+
+	return createOrUpdateErr
+}
+
+func (c *CredentialSecretsAdapter) HasAuthentication() bool {
+	if err := c.loadCredentials(); err != nil {
+		return false
+	}
+	return c.credentials.HasAuthentication()
+}
+
+func (c *CredentialSecretsAdapter) Token() (string, error) {
+	if err := c.loadCredentials(); err != nil {
+		return "", err
+	}
+	return c.credentials.Token()
+}
+
+func (c *CredentialSecretsAdapter) UpdateToken(newToken string) error {
+	if newToken == "" && c.credentials.systemToken == "" {
+		return nil
+	}
+	updateErr := c.credentials.UpdateToken(newToken)
+	if updateErr != nil {
+		return updateErr
+	}
+	return c.saveCredentials()
+}
+
+func (c *CredentialSecretsAdapter) Login() (string, string, error) {
+	return c.credentials.Login()
+}
+
+func (c *CredentialSecretsAdapter) SetLogin(newUser string, newPass string) error {
+	updateErr := c.credentials.SetLogin(newUser, newPass)
+	if updateErr != nil {
+		return updateErr
+	}
+	return c.saveCredentials()
+}
+
+func (c *CredentialSecretsAdapter) SccCredentials() connection.Credentials {
+	return c
+}
