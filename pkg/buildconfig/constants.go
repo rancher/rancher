42656e08797e9120971ae4682cc50877a0ca1a4c
Dan P. <dan.pock@suse.com>
2025-05-02 15:47:48 -0400

    [main] Bump Shell to version with k8s support for 2.12 (#50000)
    * update shell version tag
    
    * go generate

--- pkg/buildconfig/constants.go
+++ pkg/buildconfig/constants.go
@@ -4,7 +4,7 @@ package buildconfig
 
 const (
 	CspAdapterMinVersion     = "106.0.0+up6.0.0-rc1"
-	DefaultShellVersion      = "rancher/shell:v0.4.0"
+	DefaultShellVersion      = "rancher/shell:v0.5.0-rc.2"
 	FleetVersion             = "107.0.0+up0.13.0-alpha.2"
 	ProvisioningCAPIVersion  = "106.0.0+up0.7.0"
 	RemoteDialerProxyVersion = "106.0.0+up0.4.4"
