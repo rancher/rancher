c9402a8f0cba61275109ebb93e41bdfb104dfca7
Pedro Tashima <tithe.coup0l@icloud.com>
2025-05-15 10:34:53 -0300

    backport reusable actions

--- scripts/artifacts-hashes.sh
+++ scripts/artifacts-hashes.sh
@@ -1,14 +1,49 @@
 #!/usr/bin/env bash
 
-cd $(dirname $0)/..
+set -ex
 
-# either use the first arg or bin
-dist_folder=${1:-"./dist"}
+cd "$(dirname "$0")/.." || return
 
-for filename in "$dist_folder"/*; do
-  sum_file=$(sha256sum "$filename")
+CHECKSUM_FILE=${CHECKSUM_FILE:-"sha256sum.txt"}
+
+if [[ -z "${ARTIFACTS_TYPE}" ]]; then
+  >&2 echo "missing ARTIFACTS_TYPE env var"
+  exit 1
+fi
+
+case $ARTIFACTS_TYPE in
+  components)
+    source scripts/artifacts-list.sh
+    ;;
+  digests)
+    export ARTIFACTS=(
+      "$(basename "$LINUX_AMD64_FILE")"
+      "$(basename "$LINUX_ARM64_FILE")"
+      "$(basename "$WINDOWS_2019_FILE")"
+      "$(basename "$WINDOWS_2022_FILE")"
+    )
+    ;;
+  *)
+    >&2 echo "invalid ARTIFACTS_TYPE"
+    exit 1
+esac
+
+
+if [[ -z "${ARTIFACTS_BASE_DIR}" ]]; then
+  >&2 echo "missing ARTIFACTS_BASE_DIR env var"
+  exit 1
+fi
+
+rm "$ARTIFACTS_BASE_DIR/$CHECKSUM_FILE" || true
+touch "$ARTIFACTS_BASE_DIR/$CHECKSUM_FILE"
+
+for artifact in "${ARTIFACTS[@]}"; do
+  if [[ -z "$artifact" ]]; then
+    >&2 echo "missing artifact"
+    exit 1
+  fi
+
+  sum_file=$(sha256sum "$ARTIFACTS_BASE_DIR/$artifact")
   sum=$(echo "$sum_file" | awk '{print $1}')
-  file_path=$(echo "$sum_file" | awk '{print $2}')
-  file=${file_path#"$dist_folder/"}
-  echo "$sum $file" >> "./$dist_folder/sha256sum.txt"
+  echo "$sum $artifact" >> "$ARTIFACTS_BASE_DIR/$CHECKSUM_FILE"
 done
