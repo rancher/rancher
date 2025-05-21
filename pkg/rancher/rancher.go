9c9ab396e77ae82b0c038dc05cdd7a1bdb6341be
joshmeranda <joshua.meranda@suse.com>
2025-05-20 20:06:28 -0400

    add flag for controlling APIService app label selector

--- pkg/rancher/rancher.go
+++ pkg/rancher/rancher.go
@@ -84,6 +84,7 @@ type Options struct {
 	AuditLevel        int
 	Features          string
 	ClusterRegistry   string
+	ExtensionOpts     ext.Options
 }
 
 type Rancher struct {
@@ -202,7 +203,7 @@ func New(ctx context.Context, clientConfg clientcmd.ClientConfig, opts *Options)
 		return nil, err
 	}
 
-	extensionAPIServer, err := ext.NewExtensionAPIServer(ctx, wranglerContext)
+	extensionAPIServer, err := ext.NewExtensionAPIServer(ctx, wranglerContext, opts.ExtensionOpts)
 	if err != nil {
 		return nil, fmt.Errorf("extension api server: %w", err)
 	}
