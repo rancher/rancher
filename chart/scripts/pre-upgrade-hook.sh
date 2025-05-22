4e07e9227fd91352e90a6ded39ef79c3101305a5
vardhaman22 <vardhaman.surana@suse.com>
2025-05-22 13:53:44 +0530

    use jsonpath instead of jq while checking the resource

--- chart/scripts/pre-upgrade-hook.sh
+++ chart/scripts/pre-upgrade-hook.sh
@@ -22,6 +22,7 @@ check_prerequisites() {
 print_resource_table() {
   local kind="$1"
   local items="$2"
+  local -a headers=("${@:3}")
 
   local count
   count=$(wc -l <<< "$items")
@@ -29,29 +30,40 @@ print_resource_table() {
   RESOURCES_FOUND=true
 
   echo "Found $count $kind resource(s):"
+  echo
 
   IFS=$'\n' read -r -d '' -a lines < <(printf '%s\0' "$items")
 
-  local max_ns_len=9
-  local max_name_len=4
-  local max_display_len=12
-
-  for line in "${lines[@]}"; do
-    IFS=$'\t' read -r ns name display <<< "$line"
-    (( ${#ns} > max_ns_len )) && max_ns_len=${#ns}
-    (( ${#name} > max_name_len )) && max_name_len=${#name}
-    (( ${#display} > max_display_len )) && max_display_len=${#display}
+  # Initialize max_lengths array with header lengths
+  local -a max_lengths
+  for i in "${!headers[@]}"; do
+    max_lengths[i]=${#headers[i]}
   done
 
-  printf "%-${max_ns_len}s  %-${max_name_len}s  %-${max_display_len}s\n" "NAMESPACE" "NAME" "DISPLAY NAME"
-  printf "%-${max_ns_len}s  %-${max_name_len}s  %-${max_display_len}s\n" \
-    "$(printf '%*s' "$max_ns_len" '' | tr ' ' '-')" \
-    "$(printf '%*s' "$max_name_len" '' | tr ' ' '-')" \
-    "$(printf '%*s' "$max_display_len" '' | tr ' ' '-')"
+  # Calculate max width for each column
+  for line in "${lines[@]}"; do
+    IFS=$'\t' read -r -a cols <<< "$line"
+    for i in "${!cols[@]}"; do
+      (( ${#cols[i]} > max_lengths[i] )) && max_lengths[i]=${#cols[i]}
+    done
+  done
+
+  for i in "${!headers[@]}"; do
+    printf "%-${max_lengths[i]}s  " "${headers[i]}"
+  done
+  printf "\n"
+
+  for i in "${!headers[@]}"; do
+    printf "%-${max_lengths[i]}s  " "$(printf '%*s' "${max_lengths[i]}" '' | tr ' ' '-')"
+  done
+  printf "\n"
 
   for line in "${lines[@]}"; do
-    IFS=$'\t' read -r ns name display <<< "$line"
-    printf "%-${max_ns_len}s  %-${max_name_len}s  %-${max_display_len}s\n" "$ns" "$name" "$display"
+    IFS=$'\t' read -r -a cols <<< "$line"
+    for i in "${!cols[@]}"; do
+      printf "%-${max_lengths[i]}s  " "${cols[i]}"
+    done
+    printf "\n"
   done
 
   echo
@@ -60,30 +72,28 @@ print_resource_table() {
 detect_resource() {
   local crd="$1"
   local kind="$2"
-  local filter="$3"
+  local jsonpath="$3"
+  local -a headers=("${@:4}")
 
   echo "Checking for $kind resources..."
 
-  local json
-  if ! json=$(kubectl get "$crd" --all-namespaces -o json 2>&1); then
-    if grep -q "the server doesn't have a resource type" <<< "$json"; then
+  local output
+  if ! output=$(kubectl get "$crd" --all-namespaces -o=jsonpath="$jsonpath" 2>&1); then
+    if grep -q "the server doesn't have a resource type" <<< "$output"; then
       echo "Resource type $crd not found. Skipping."
       echo
       return 0
     else
-      echo "Error retrieving $kind resources: $json"
+      echo "Error retrieving $kind resources: $output"
       exit 1
     fi
   fi
 
-  local items
-  items=$(jq -r "$filter" <<< "$json")
-
-  if [ -z "$items" ]; then
+  if [ -z "$output" ]; then
     echo "No $kind resources found."
     echo
   else
-    print_resource_table "$kind" "$items"
+    print_resource_table "$kind" "$output" "${headers[@]}"
   fi
 }
 
@@ -110,14 +120,16 @@ main() {
   check_prerequisites
 
   detect_resource "clusters.management.cattle.io" "RKE Management Cluster" \
-    '.items[] | select(.spec.rancherKubernetesEngineConfig != null) | 
-     [.metadata.namespace // "default", .metadata.name, .spec.displayName // ""] | @tsv'
+    '{range .items[?(@.spec.rancherKubernetesEngineConfig)]}{.metadata.name}{"\t"}{.spec.displayName}{"\n"}{end}' \
+    "NAME" "DISPLAY NAME"
 
   detect_resource "nodetemplates.management.cattle.io" "NodeTemplate" \
-    '.items[] | [.metadata.namespace // "default", .metadata.name, .spec.displayName // ""] | @tsv'
+    '{range .items[*]}{.metadata.namespace}{"\t"}{.metadata.name}{"\t"}{.spec.displayName}{"\n"}{end}' \
+    "NAMESPACE" "NAME" "DISPLAY NAME"
 
   detect_resource "clustertemplates.management.cattle.io" "ClusterTemplate" \
-    '.items[] | [.metadata.namespace // "default", .metadata.name, .spec.displayName // ""] | @tsv'
+    '{range .items[*]}{.metadata.namespace}{"\t"}{.metadata.name}{"\t"}{.spec.displayName}{"\n"}{end}' \
+    "NAMESPACE" "NAME" "DISPLAY NAME"
 
   print_summary
 }
