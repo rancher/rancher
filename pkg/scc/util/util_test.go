0f2f0cfc85f359a8513b26a55a0660ff5286f443
Dan Pock <self@danpock.me>
2025-05-30 13:39:47 -0400

    refactor Dev check for SCC and add tests

--- pkg/scc/util/util_test.go
+++ pkg/scc/util/util_test.go
@@ -1,1 +1,29 @@
 package util
+
+import (
+	coreVersion "github.com/rancher/rancher/pkg/version"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestVersionIsDevBuild(t *testing.T) {
+	coreVersion.Version = "dev"
+	assert.True(t, VersionIsDevBuild())
+
+	coreVersion.Version = "2.13.2"
+	assert.False(t, VersionIsDevBuild())
+	coreVersion.Version = "v2.13.2"
+	assert.False(t, VersionIsDevBuild())
+
+	coreVersion.Version = "v2.13.2+meta"
+	assert.False(t, VersionIsDevBuild())
+
+	coreVersion.Version = "v2.13.2-rc.42"
+	assert.True(t, VersionIsDevBuild())
+
+	coreVersion.Version = "v2.13.2+meta-with-hyphen"
+	assert.False(t, VersionIsDevBuild())
+
+	coreVersion.Version = "v2.13.2-rc.9999+meta-also"
+	assert.True(t, VersionIsDevBuild())
+}
