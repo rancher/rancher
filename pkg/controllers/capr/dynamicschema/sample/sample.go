9e29ea1f04a77640364f777347cc1c116e3f883c
Pedro Franco de Carvalho <pedro.carvalho@suse.com>
2025-06-09 11:38:13 -0300

    Use a sample CRD for documenting dynamic schemas
    Preivously, dynamic schemas imported some Rancher types at run-time to
    generate dynamic schemas. Because we don't have access to their comments at
    run-time, they could be used for the populating descriptions in the generated
    CRDs.
    
    This commit generates a sample CRD with that also imports these types with
    kubebuilder, which will read the comments as descriptions, then injects the
    corresponding documented properties into the dynamic schemas. The sample CRD
    is only read as a file embeded in the binary to obtain the descriptions, it
    doesn't get applied to any cluster.

---
+++ pkg/controllers/capr/dynamicschema/sample/sample.go
@@ -0,0 +1,38 @@
+// +groupName=rke-machine.cattle.io
+package sample
+
+import (
+	_ "embed"
+
+	rkev1 "github.com/rancher/rancher/pkg/apis/rke.cattle.io/v1"
+	"github.com/rancher/wrangler/v3/pkg/yaml"
+	apiextv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+//go:embed rke-machine.cattle.io_samples.yaml
+var sampleBytes []byte
+
+type Sample struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata"`
+
+	Spec SampleSpec `json:"spec"`
+
+	Status rkev1.RKEMachineStatus `json:"status"`
+}
+
+type SampleSpec struct {
+	Common rkev1.RKECommonNodeConfig `json:"common"`
+}
+
+func GetSampleProps() (*apiextv1.JSONSchemaProps, error) {
+	var sampleCRD apiextv1.CustomResourceDefinition
+
+	err := yaml.Unmarshal(sampleBytes, &sampleCRD)
+	if err != nil {
+		return nil, err
+	}
+
+	return sampleCRD.Spec.Versions[0].Schema.OpenAPIV3Schema, nil
+}
