18fa0914ef00af6f60a0832c23fdc7841ab34c2d
Josh Meranda <joshmeranda@gmail.com>
2025-06-10 16:13:16 -0400

    Revert "[main] block rancher until kube apiserver connects"

--- pkg/ext/extension_apiserver.go
+++ pkg/ext/extension_apiserver.go
@@ -7,7 +7,6 @@ import (
 	"net/http"
 	"os"
 	"strings"
-	"sync"
 	"time"
 
 	extstores "github.com/rancher/rancher/pkg/ext/stores"
@@ -36,9 +35,6 @@ const (
 type Options struct {
 	// AppSelector is the expected value for the "app" label on the rancher service.
 	AppSelector string
-
-	// KubeAggregatorReadyChan is the channel to close once the extension server receives a request from the kube API.
-	KubeAggregatorReadyChan chan struct{}
 }
 
 func DefaultOptions() Options {
@@ -207,8 +203,6 @@ func NewExtensionAPIServer(ctx context.Context, wranglerContext *wrangler.Contex
 
 	authenticator := steveext.NewUnionAuthenticator(authenticators...)
 
-	var once sync.Once
-
 	aslAuthorizer := steveext.NewAccessSetAuthorizer(wranglerContext.ASL)
 	codecs := serializer.NewCodecFactory(scheme)
 	extOpts := steveext.ExtensionAPIServerOptions{
@@ -226,12 +220,6 @@ func NewExtensionAPIServer(ctx context.Context, wranglerContext *wrangler.Contex
 		},
 		Authenticator: authenticator,
 		Authorizer: authorizer.AuthorizerFunc(func(ctx context.Context, a authorizer.Attributes) (authorizer.Decision, string, error) {
-			if a.GetUser().GetName() == "system:aggregator" || a.GetUser().GetName() == "system:kube-aggregator" {
-				once.Do(func() {
-					close(opts.KubeAggregatorReadyChan)
-				})
-			}
-
 			if a.IsResourceRequest() {
 				return aslAuthorizer.Authorize(ctx, a)
 			}
