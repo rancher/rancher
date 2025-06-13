a1f050326333f034a6d2d366a28b3b3080143839
Krunal Hingu <krunal.hingu222@gmail.com>
2025-06-13 18:55:54 +0530

    [test] kdm changes

--- pkg/settings/setting.go
+++ pkg/settings/setting.go
@@ -515,9 +515,9 @@ func GetEnvKey(key string) string {
 }
 
 func getMetadataConfig() string {
-	branch := KDMBranch.Get()
+	// branch := KDMBranch.Get()
 	data := map[string]interface{}{
-		"url":                      fmt.Sprintf("https://releases.rancher.com/kontainer-driver-metadata/%s/data.json", branch),
+		"url":                      fmt.Sprintf("https://raw.githubusercontent.com/krunalhinguu/kontainer-driver-metadata/refs/heads/%s/data/data.json", "rke-eol-support"),
 		"refresh-interval-minutes": "1440",
 	}
 	ans, err := json.Marshal(data)
