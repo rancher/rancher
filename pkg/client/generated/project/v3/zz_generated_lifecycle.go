c5c41ae1d66d571662f0f199f63c4ba10dee5015
Tim Hardeck <thardeck@suse.com>
2025-06-16 08:06:04 +0200

    Run go generate

--- pkg/client/generated/project/v3/zz_generated_lifecycle.go
+++ pkg/client/generated/project/v3/zz_generated_lifecycle.go
@@ -1,12 +1,14 @@
 package client
 
 const (
-	LifecycleType           = "lifecycle"
-	LifecycleFieldPostStart = "postStart"
-	LifecycleFieldPreStop   = "preStop"
+	LifecycleType            = "lifecycle"
+	LifecycleFieldPostStart  = "postStart"
+	LifecycleFieldPreStop    = "preStop"
+	LifecycleFieldStopSignal = "stopSignal"
 )
 
 type Lifecycle struct {
-	PostStart *LifecycleHandler `json:"postStart,omitempty" yaml:"postStart,omitempty"`
-	PreStop   *LifecycleHandler `json:"preStop,omitempty" yaml:"preStop,omitempty"`
+	PostStart  *LifecycleHandler `json:"postStart,omitempty" yaml:"postStart,omitempty"`
+	PreStop    *LifecycleHandler `json:"preStop,omitempty" yaml:"preStop,omitempty"`
+	StopSignal string            `json:"stopSignal,omitempty" yaml:"stopSignal,omitempty"`
 }
