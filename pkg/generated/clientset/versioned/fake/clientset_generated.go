8bf56b046af7879386aa5928f50eee678f84b057
Chirayu Kapoor <chirayu.kapoor@suse.com>
2025-06-24 11:51:03 +0530

    [main]  k8s v1.33 support (#50396)
    * Bumped dependencies for 1.33
    
    * updated script and dockerfile
    
    * capi changes
    
    * go generate
    
    * upgraded steve
    
    Signed-off-by: swastik959 <Sswastik959@gmail.com>
    
    * Upgraded helm and shell
    
    Signed-off-by: swastik959 <Sswastik959@gmail.com>
    
    * updated the go.mod
    
    Signed-off-by: swastik959 <Sswastik959@gmail.com>
    
    * Upgraded k3s
    
    Signed-off-by: swastik959 <Sswastik959@gmail.com>
    
    * Upgraded system-agent
    
    Signed-off-by: swastik959 <Sswastik959@gmail.com>
    
    * updated etcd,lasso,wrangler,updated k8s chart
    
    Signed-off-by: swastik959 <Sswastik959@gmail.com>
    
    ---------
    
    Signed-off-by: swastik959 <Sswastik959@gmail.com>
    Co-authored-by: swastik959 <Sswastik959@gmail.com>

--- pkg/generated/clientset/versioned/fake/clientset_generated.go
+++ pkg/generated/clientset/versioned/fake/clientset_generated.go
@@ -30,6 +30,7 @@ import (
 	fakesccv1 "github.com/rancher/rancher/pkg/generated/clientset/versioned/typed/scc.cattle.io/v1/fake"
 	upgradev1 "github.com/rancher/rancher/pkg/generated/clientset/versioned/typed/upgrade.cattle.io/v1"
 	fakeupgradev1 "github.com/rancher/rancher/pkg/generated/clientset/versioned/typed/upgrade.cattle.io/v1/fake"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/watch"
 	"k8s.io/client-go/discovery"
@@ -57,9 +58,13 @@ func NewSimpleClientset(objects ...runtime.Object) *Clientset {
 	cs.discovery = &fakediscovery.FakeDiscovery{Fake: &cs.Fake}
 	cs.AddReactor("*", "*", testing.ObjectReaction(o))
 	cs.AddWatchReactor("*", func(action testing.Action) (handled bool, ret watch.Interface, err error) {
+		var opts metav1.ListOptions
+		if watchActcion, ok := action.(testing.WatchActionImpl); ok {
+			opts = watchActcion.ListOptions
+		}
 		gvr := action.GetResource()
 		ns := action.GetNamespace()
-		watch, err := o.Watch(gvr, ns)
+		watch, err := o.Watch(gvr, ns, opts)
 		if err != nil {
 			return false, nil, err
 		}
--- pkg/settings/setting.go
+++ pkg/settings/setting.go
@@ -100,7 +100,7 @@ var (
 	WinsAgentVersion                    = NewSetting("wins-agent-version", "")
 	CSIProxyAgentVersion                = NewSetting("csi-proxy-agent-version", "")
 	CSIProxyAgentURL                    = NewSetting("csi-proxy-agent-url", "https://acs-mirror.azureedge.net/csi-proxy/%[1]s/binaries/csi-proxy-%[1]s.tar.gz")
-	SystemAgentInstallScript            = NewSetting("system-agent-install-script", "https://github.com/rancher/system-agent/releases/download/v0.3.13-rc.2/install.sh") // To ensure consistency between SystemAgentInstallScript default value and CATTLE_SYSTEM_AGENT_INSTALL_SCRIPT to utilize the local system-agent-install.sh script when both values are equal.
+	SystemAgentInstallScript            = NewSetting("system-agent-install-script", "https://github.com/rancher/system-agent/releases/download/v0.3.13-rc.3/install.sh") // To ensure consistency between SystemAgentInstallScript default value and CATTLE_SYSTEM_AGENT_INSTALL_SCRIPT to utilize the local system-agent-install.sh script when both values are equal.
 	WinsAgentInstallScript              = NewSetting("wins-agent-install-script", "https://raw.githubusercontent.com/rancher/wins/v0.5.1/install.ps1")
 	SystemAgentInstallerImage           = NewSetting("system-agent-installer-image", "") // Defined via environment variable
 	SystemAgentUpgradeImage             = NewSetting("system-agent-upgrade-image", "")   // Defined via environment variable
@@ -128,7 +128,7 @@ var (
 	EKSUpstreamRefresh                  = NewSetting("eks-refresh", "300")
 	GKEUpstreamRefresh                  = NewSetting("gke-refresh", "300")
 	HideLocalCluster                    = NewSetting("hide-local-cluster", "false")
-	MachineProvisionImage               = NewSetting("machine-provision-image", "rancher/machine:v0.15.0-rancher128")
+	MachineProvisionImage               = NewSetting("machine-provision-image", "rancher/machine:v0.15.0-rancher129")
 	SystemFeatureChartRefreshSeconds    = NewSetting("system-feature-chart-refresh-seconds", "21600")
 	ClusterAgentDefaultAffinity         = NewSetting("cluster-agent-default-affinity", ClusterAgentAffinity)
 	FleetAgentDefaultAffinity           = NewSetting("fleet-agent-default-affinity", FleetAgentAffinity)
