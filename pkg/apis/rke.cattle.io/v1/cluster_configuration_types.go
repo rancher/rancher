f7bf50f1d65b53c1e9da06ed929fc25a61548212
Jake Hyde <jakefhyde@gmail.com>
2025-06-27 16:36:25 -0400

    Removed AuthConfig and migrated registry to cluster_configuration_types.go

--- pkg/apis/rke.cattle.io/v1/cluster_configuration_types.go
+++ pkg/apis/rke.cattle.io/v1/cluster_configuration_types.go
@@ -100,6 +100,41 @@ type KeyToPath struct {
 	Hash        string `json:"hash,omitempty"`
 }
 
+// Registry is registry settings configured
+type Registry struct {
+	// Mirrors are namespace to mirror mapping for all namespaces.
+	Mirrors map[string]Mirror `json:"mirrors,omitempty"`
+	// Configs are configs for each registry.
+	// The key is the FDQN or IP of the registry.
+	Configs map[string]RegistryConfig `json:"configs,omitempty"`
+}
+
+// Mirror contains the config related to the registry mirror
+type Mirror struct {
+	// Endpoints are endpoints for a namespace. CRI plugin will try the endpoints
+	// one by one until a working one is found. The endpoint must be a valid url
+	// with host specified.
+	// The scheme, host and path from the endpoint URL will be used.
+	Endpoints []string `json:"endpoint,omitempty"`
+
+	// Rewrites are repository rewrite rules for a namespace. When fetching image resources
+	// from an endpoint and a key matches the repository via regular expression matching
+	// it will be replaced with the corresponding value from the map in the resource request.
+	Rewrites map[string]string `json:"rewrite,omitempty"`
+}
+
+// RegistryConfig contains configuration used to communicate with the registry.
+type RegistryConfig struct {
+	// Auth contains information to authenticate to the registry.
+	AuthConfigSecretName string `json:"authConfigSecretName,omitempty"`
+	// TLS is a pair of Cert/Key which then are used when creating the transport
+	// that communicates with the registry.
+	TLSSecretName string `json:"tlsSecretName,omitempty"`
+	CABundle      []byte `json:"caBundle,omitempty"`
+
+	InsecureSkipVerify bool `json:"insecureSkipVerify,omitempty"`
+}
+
 // Networking contains information regarding the desired and actual networking stack of the cluster.
 type Networking struct {
 	// Specifies which networking stack to prefer for external cluster communication. In practice, this is used by the
