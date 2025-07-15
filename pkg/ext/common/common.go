7b1ef7532dc41bb7d1e20692b4006816cd198f26
Andreas Kupries <andreas.kupries@suse.com>
2025-07-15 11:00:33 +0200

    simplify types and code for mapping managed fields.

--- pkg/ext/common/common.go
+++ pkg/ext/common/common.go
@@ -45,55 +45,9 @@ func EnsureNamespace(nsCache v1.NamespaceCache, nsClient v1.NamespaceClient, nam
 	})
 }
 
-// MapSpec holds the information for mapping a set of paths to different set of paths
-type MapSpec map[string]MapEntry
-
-// MapEntry holds the information to map a field path to a different field path.
-type MapEntry struct {
-	src fieldpath.Path
-	dst fieldpath.Path // Note: path is slice, can be nil
-}
-
-// NewMapSpec converts a set of mapping entries into a proper MapSpec
-func NewMapSpec(entries ...MapEntry) MapSpec {
-	result := make(MapSpec)
-	for _, entry := range entries {
-		result[entry.src.String()] = entry
-	}
-	return result
-}
-
-// NewMap creates a new mapping entry from origin and destination paths
-func NewMap(src fieldpath.Path, dst fieldpath.Path) MapEntry {
-	return MapEntry{
-		src: src,
-		dst: dst,
-	}
-}
-
-// MapPath maps a single path as per the set of mapping given. A `dst` of nil
-// means that `src` is not mapped at all, the caller has to recognize and deal
-// with that. A `src` for which there is no MapEntry is mapped to itself.
-func MapPath(mapper MapSpec, path fieldpath.Path) fieldpath.Path {
-	if entry, ok := mapper[path.String()]; ok {
-		return entry.dst
-	}
-	return path
-}
-
-// MapInvert converts a map A -> B into a map B -> A.
-func MapInvert(mapper MapSpec) MapSpec {
-	entries := make([]MapEntry, len(mapper))
-
-	for _, entry := range mapper {
-		if entry.dst == nil {
-			continue
-		}
-		entries = append(entries, NewMap(entry.dst, entry.src))
-	}
-
-	return NewMapSpec(entries...)
-}
+// MapSpec holds the information for mapping a set of field paths, specified by
+// their string rep, to different set of paths
+type MapSpec map[string]fieldpath.Path
 
 // MapManagedFields transform the originManagedFields under the control of the mapSpec.
 func MapManagedFields(mapSpec MapSpec, originManagedFields []metav1.ManagedFieldsEntry) ([]metav1.ManagedFieldsEntry, error) {
@@ -117,12 +71,16 @@ func MapManagedFields(mapSpec MapSpec, originManagedFields []metav1.ManagedField
 		}
 
 		destPaths := make([]fieldpath.Path, originPaths.Size())
+
 		originPaths.Iterate(func(originPath fieldpath.Path) {
-			destPath := MapPath(mapSpec, originPath)
-			if destPath == nil {
+			if destPath, ok := mapSpec[originPath.String()]; ok {
+				if destPath == nil {
+					return
+				}
+				destPaths = append(destPaths, destPath)
 				return
 			}
-			destPaths = append(destPaths, destPath)
+			destPaths = append(destPaths, originPath)
 		})
 
 		destJSON, err := fieldpath.NewSet(destPaths...).ToJSON()
