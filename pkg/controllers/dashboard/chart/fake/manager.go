a107de7cf3948609c0010e38b547761be1bb907c
Tom Lebreux <tom.lebreux@suse.com>
2025-07-23 11:24:26 -0400

    Add generation for dashboard/chart

--- pkg/controllers/dashboard/chart/fake/manager.go
+++ pkg/controllers/dashboard/chart/fake/manager.go
@@ -19,6 +19,7 @@ import (
 type MockManager struct {
 	ctrl     *gomock.Controller
 	recorder *MockManagerMockRecorder
+	isgomock struct{}
 }
 
 // MockManagerMockRecorder is the mock recorder for MockManager.
