b1c4bc227ea28db07391f2bed0234676c30f107b
Dan P. <dan.pock@suse.com>
2025-07-29 18:12:53 -0400

    [backport][release/v2.12] un-RC Shell image (#51256)

--- pkg/buildconfig/constants.go
+++ pkg/buildconfig/constants.go
@@ -4,7 +4,7 @@ package buildconfig
 
 const (
 	CspAdapterMinVersion     = "107.0.0+up7.0.0"
-	DefaultShellVersion      = "rancher/shell:v0.5.0-rc.4"
+	DefaultShellVersion      = "rancher/shell:v0.5.0"
 	FleetVersion             = "107.0.0+up0.13.0"
 	ProvisioningCAPIVersion  = "107.0.0+up0.8.0"
 	RemoteDialerProxyVersion = "106.0.1+up0.5.0"
