f27cc5b625623512cc35192d74deb6183889e3f4
Dan Pock <self@danpock.me>
2025-08-06 11:51:35 -0400

    Enable SCC-deployer controller to reconcile deployment with same mechanism as initial deploy

--- pkg/scc/controllers/deployer.go
+++ pkg/scc/controllers/deployer.go
@@ -2,10 +2,15 @@ package controllers
 
 import (
 	"context"
+	"fmt"
 
+	v3 "github.com/rancher/rancher/pkg/apis/management.cattle.io/v3"
+	"github.com/rancher/rancher/pkg/scc/deployer/params"
+	"github.com/rancher/rancher/pkg/settings"
 	deploymentControllers "github.com/rancher/wrangler/v3/pkg/generated/controllers/apps/v1"
 	"github.com/rancher/wrangler/v3/pkg/relatedresource"
 	appsv1 "k8s.io/api/apps/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 
 	mgmtv3 "github.com/rancher/rancher/pkg/generated/controllers/management.cattle.io/v3"
 	"github.com/rancher/rancher/pkg/scc/consts"
@@ -17,6 +22,7 @@ import (
 type deployersHandler struct {
 	ctx         context.Context
 	log         log.StructuredLogger
+	sccDeployer *deployer.SCCDeployer
 	deployments deploymentControllers.DeploymentController
 }
 
@@ -30,14 +36,16 @@ func RegisterDeployer(
 	controller := &deployersHandler{
 		ctx:         ctx,
 		log:         logger.WithField("controller", "scc-deployers"),
+		sccDeployer: sccDeployer,
 		deployments: deployments,
 	}
 
 	// Set up a handler for the SCCOperatorImage setting
 	relatedresource.Watch(ctx, "scc-operator-image-watcher",
-		sccDeployer.OnRelatedSettings,
+		controller.OnRelatedSettings,
 		controller.deployments,
-		settings)
+		settings,
+	)
 
 	generic.NamespaceScopedOnChange(
 		ctx,
@@ -56,14 +64,65 @@ func RegisterDeployer(
 	)
 }
 
+// OnRelatedSettings triggers changes to deployment if related settings change
+func (h *deployersHandler) OnRelatedSettings(_, name string, obj runtime.Object) ([]relatedresource.Key, error) {
+	// TODO: not sure if this is done correctly - didnt' seem to trigger an update on the deployment...
+	if _, ok := obj.(*v3.Setting); ok {
+		if name == settings.SCCOperatorImage.Name {
+			return []relatedresource.Key{{
+				Namespace: consts.DefaultSCCNamespace,
+				Name:      consts.DeploymentName,
+			}}, nil
+		}
+	}
+
+	return nil, nil
+}
+
 func (h *deployersHandler) OnChanged(name string, incoming *appsv1.Deployment) (*appsv1.Deployment, error) {
-	if name != consts.DeploymentName {
+	// This ensures that we only care about our deployment - any others will be ignored
+	if name != fmt.Sprintf("%s/%s", consts.DefaultSCCNamespace, consts.DeploymentName) {
 		return incoming, nil
 	}
 
-	h.log.Info(incoming)
+	h.log.Debugf("Received deployment update for %s", name)
+	h.log.Debug(incoming)
+
+	desiredSCCParams, paramsErr := params.ExtractSccOperatorParams()
+	if paramsErr != nil {
+		h.log.Errorf("Failed to extract SCC operator params: %v", paramsErr)
+		return nil, paramsErr
+	}
+
+	// TODO: this might need to be more conditional...
+	if err := h.ensureDependencies(desiredSCCParams); err != nil {
+		return nil, err
+	}
+
+	currentDeployment, err := h.sccDeployer.ReconcileDeployment(h.ctx, desiredSCCParams, incoming)
+	if err != nil {
+		return nil, err
+	}
+
+	h.log.Debugf("Finished reconciling deployment for %s", name)
+	h.log.Debug(desiredSCCParams)
+	h.log.Debug(currentDeployment)
 
 	return incoming, nil
+}
+
+func (h *deployersHandler) ensureDependencies(desiredSCCParams *params.SCCOperatorParams) error {
+	hasDeps, depsErr := h.sccDeployer.HasAllDependencies()
+	if depsErr != nil {
+		h.log.Errorf("error checking dependencies: %v", depsErr)
+	}
+	if !hasDeps {
+		if err := h.sccDeployer.EnsureDependenciesConfigured(h.ctx, desiredSCCParams); err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
 
 func (h *deployersHandler) OnRemoved(name string, incoming *appsv1.Deployment) (*appsv1.Deployment, error) {
