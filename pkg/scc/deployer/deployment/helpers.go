5622ae5a4d0bf00dd809c2faa13713bb33b7b437
Dan Pock <self@danpock.me>
2025-08-06 11:51:04 -0400

    Refactor deployment deployer per Sams feedback - makes it easier for re-use in controller

--- pkg/scc/deployer/deployment/helpers.go
+++ pkg/scc/deployer/deployment/helpers.go
@@ -1,25 +1,12 @@
 package deployment
 
 import (
-	appsControllers "github.com/rancher/wrangler/v3/pkg/generated/controllers/apps/v1"
-	appsv1 "k8s.io/api/apps/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-
 	"github.com/rancher/rancher/pkg/scc/consts"
+	appsv1 "k8s.io/api/apps/v1"
 )
 
-func fetchCurrentDeployment(deployments appsControllers.DeploymentController) (*appsv1.Deployment, error) {
-	currentDeployment, err := deployments.Get(consts.DefaultSCCNamespace, consts.DeploymentName, metav1.GetOptions{})
-	if err != nil {
-		return nil, err
-	}
-
-	return currentDeployment, nil
-}
-
-func fetchCurrentDeploymentHash(deployments appsControllers.DeploymentController) string {
-	deployment, err := fetchCurrentDeployment(deployments)
-	if err != nil {
+func fetchCurrentDeploymentHash(deployment *appsv1.Deployment) string {
+	if deployment == nil {
 		return ""
 	}
 
