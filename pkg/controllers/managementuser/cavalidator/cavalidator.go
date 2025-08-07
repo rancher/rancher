1ac4040e523ef89807dd91ceca19b15e9ef9147d
Tom Lebreux <tom.lebreux@suse.com>
2025-08-06 22:32:45 -0400

    Move CAValidator to single object wrangler controller

--- pkg/controllers/managementuser/cavalidator/cavalidator.go
+++ pkg/controllers/managementuser/cavalidator/cavalidator.go
@@ -2,14 +2,19 @@ package cavalidator
 
 import (
 	"context"
+	"fmt"
 
+	"github.com/rancher/lasso/pkg/cache"
+	"github.com/rancher/lasso/pkg/controller"
 	mgmtv3controllers "github.com/rancher/rancher/pkg/generated/controllers/management.cattle.io/v3"
 	"github.com/rancher/rancher/pkg/namespace"
 	"github.com/rancher/rancher/pkg/types/config"
-	"github.com/rancher/rancher/pkg/watcher"
-
 	"github.com/rancher/wrangler/v3/pkg/condition"
+	wcore "github.com/rancher/wrangler/v3/pkg/generated/controllers/core"
+	"github.com/rancher/wrangler/v3/pkg/generic"
 	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/client-go/util/retry"
 )
 
@@ -37,12 +42,42 @@ func Register(ctx context.Context, downstream *config.UserContext) {
 		clusters:     downstream.Management.Wrangler.Mgmt.Cluster(),
 	}
 
-	// Downstream Secret caches are restricted to the impersonation namespace only, see https://github.com/rancher/rancher/issues/46827
-	// Use a single Watch instead of relying on full-blown informers
-	watcher.OnChange(downstream.Corew.Secret(), ctx, "cavalidator-secret", c.onStvAggregationSecret,
-		watcher.WithNamespace(namespace.System),
-		watcher.WithFieldSelector(map[string]string{"metadata.name": stvAggregationSecretName}),
-	)
+	restConfig := downstream.RESTConfig
+
+	// TODO: Everything except the OnChange should move to where we define
+	// UserContext. UserContext should create and start a new controller factory
+	// with a `CAValidatorSecret wcorev1.SecretController` field imo.
+	opts := &controller.SharedControllerFactoryOptions{
+		CacheOptions: &cache.SharedCacheFactoryOptions{
+			KindTweakList: map[schema.GroupVersionKind]cache.TweakListOptionsFunc{
+				// Only keep a cache for this single Secret
+				corev1.SchemeGroupVersion.WithKind("Secret"): func(opts *metav1.ListOptions) {
+					opts.FieldSelector = fmt.Sprintf("metadata.namespace=%s,metadata.name=%s", namespace.System, stvAggregationSecretName)
+				},
+			},
+		},
+	}
+	controllerFactory, err := controller.NewSharedControllerFactoryFromConfigWithOptions(&downstream.RESTConfig, nil, opts)
+	if err != nil {
+		// TODO: handle error
+		return
+	}
+
+	genOpts := &generic.FactoryOptions{
+		SharedControllerFactory: controllerFactory,
+	}
+	coreCtrl, err := wcore.NewFactoryFromConfigWithOptions(&restConfig, genOpts)
+	if err != nil {
+		// TODO: handle error
+		return
+	}
+	controllerFactory.Start(ctx, 1)
+
+	coreCtrl.Core().V1().Secret().OnChange(ctx, "cavalidator-secret", c.onStvAggregationSecret)
+	if err != nil {
+		// TODO: handle error
+		return
+	}
 }
 
 func (c *CertificateAuthorityValidator) onStvAggregationSecret(_ string, obj *corev1.Secret) (*corev1.Secret, error) {
