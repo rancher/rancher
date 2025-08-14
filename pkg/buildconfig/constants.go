a2765c7cb4c1359b11c7abe8297f845a0b78f284
Tom Lebreux <tom.lebreux@suse.com>
2025-08-14 10:24:09 -0400

    Bump remotedialer-proxy charts to 106.0.1+up0.5.0 (#51470)

--- pkg/buildconfig/constants.go
+++ pkg/buildconfig/constants.go
@@ -8,6 +8,6 @@ const (
 	DefaultShellVersion      = "rancher/shell:v0.5.0"
 	FleetVersion             = "107.0.0+up0.13.0"
 	ProvisioningCAPIVersion  = "107.0.0+up0.8.0"
-	RemoteDialerProxyVersion = "106.0.1+up0.5.0-rc.1"
+	RemoteDialerProxyVersion = "106.0.1+up0.5.0"
 	WebhookVersion           = "108.0.0+up0.9.0-rc.4"
 )
