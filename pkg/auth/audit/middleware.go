ddbea1fcc4b36780c242d470c7044fff5951e9a8
Dan P. <dan.pock@suse.com>
2025-08-22 16:55:51 -0400

    [main] Refactor audit log midleware (#51610)
    * Refactor audit log midleware
    
    * Add back Body capture on Write calls
    
    * Track WriteHeader calls to ensure we call 200 OK if callee does not
    
    This mimics how the default Golang writers seem to work.
    Essentially because: we 100% know that we cannot send more headers if body is sent - then when body is called we should send 200 if none have been sent yet.
    
    * Also add default OK status to Flush to mimic stdlib default
    
    * Fix bug where AuditLog entry can mutate response
    
    * Correct WriteHeader to mimic stdlib
    
    * Add test for audit logs
    
    ---------
    
    Co-authored-by: Tom Lebreux <tom.lebreux@suse.com>

---
+++ pkg/auth/audit/middleware.go
@@ -0,0 +1,55 @@
+package audit
+
+import (
+	"context"
+	"net/http"
+	"time"
+
+	"github.com/sirupsen/logrus"
+)
+
+func GetAuditLoggerMiddleware(auditLog *LoggingHandler) func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+			if auditLog == nil || auditLog.writer == nil {
+				next.ServeHTTP(rw, req)
+				return
+			}
+
+			reqTimestamp := time.Now().Format(time.RFC3339)
+			user := getUserInfo(req)
+			context := context.WithValue(req.Context(), userKeyValue, user)
+			req = req.WithContext(context)
+			rawBody, userName := copyReqBody(req)
+
+			wrappedRw := &wrapWriter{
+				ResponseWriter: rw,
+				headerWrote:    false,
+				statusCode:     http.StatusTeapot, // Default status should never matter so it can be nonsense; controversial, but it serves as our canary in the coal mine. If we see teapots we KNOW we have bugs somewhere.
+			}
+
+			next.ServeHTTP(wrappedRw, req)
+			if wrappedRw.hijacked {
+				return
+			}
+
+			respTimestamp := time.Now().Format(time.RFC3339)
+
+			auditLogEntry := newLog(user, req, wrappedRw, reqTimestamp, respTimestamp, rawBody, userName)
+			if err := auditLog.writer.Write(auditLogEntry); err != nil {
+				// Locking after next is called to avoid performance hits on the request.
+				auditLog.errLock.Lock()
+				defer auditLog.errLock.Unlock()
+
+				// Only log duplicate error messages at most every errorDebounceTime.
+				// This is to prevent the rancher logs from being flooded with error messages
+				// when the log path is invalid or any other error that will always cause a write to fail.
+				if lastSeen, ok := auditLog.errMap[err.Error()]; !ok || time.Since(lastSeen) > errorDebounceTime {
+					logrus.Warnf("Failed to write audit log: %s", err)
+					auditLog.errMap[err.Error()] = time.Now()
+				}
+			}
+
+		})
+	}
+}
