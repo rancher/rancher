de1467e1381ece16c5cb717c998d6f0cfde3a7eb
Dan Pock <self@danpock.me>
2025-10-02 13:16:24 -0400

    refactor: split all ServerVersion helpers into new file

--- pkg/settings/setting.go
+++ pkg/settings/setting.go
@@ -433,11 +433,6 @@ func PrefixPrivateRegistry(image string) string {
 	return private + "/" + image
 }
 
-// IsRelease returns true if the running server is a released version of rancher.
-func IsRelease() bool {
-	return !strings.Contains(ServerVersion.Get(), "head") && releasePattern.MatchString(ServerVersion.Get())
-}
-
 func init() {
 	// setup auth setting
 	authsettings.AuthUserInfoResyncCron = AuthUserInfoResyncCron
@@ -657,26 +652,6 @@ func DefaultAgentSettingsAsEnvVars() []v1.EnvVar {
 	}
 
 	return envVars
-}
-
-func IsReleaseServerVersion(serverVersion string) bool {
-	if strings.HasPrefix(serverVersion, "dev") ||
-		strings.HasPrefix(serverVersion, "master") ||
-		serverVersion == "" ||
-		strings.HasSuffix(serverVersion, "-head") ||
-		strings.HasSuffix(serverVersion, "-main") {
-		return false
-	}
-	return true
-}
-
-// GetRancherVersion will return the stored server version without the 'v' prefix.
-func GetRancherVersion() string {
-	rancherVersion := ServerVersion.Get()
-	if !IsReleaseServerVersion(rancherVersion) {
-		return RancherVersionDev
-	}
-	return strings.TrimPrefix(rancherVersion, "v")
 }
 
 // IterateWhitelistedEnvVars iterates over the environment variables whitelisted
---
+++ pkg/settings/version_test.go
@@ -0,0 +1,48 @@
+package settings
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIsRelease(t *testing.T) {
+	inputs := map[string]bool{
+		"dev":         false,
+		"master-head": false,
+		"master":      false,
+		"v2.5.2":      true,
+		"v2":          true,
+		"v2.0":        true,
+		"v2.x":        true,
+		"v2.5-head":   false,
+		"2.5":         false,
+		"2.5-head":    false,
+	}
+	a := assert.New(t)
+	for key, value := range inputs {
+		if err := ServerVersion.Set(key); err != nil {
+			t.Errorf("Encountered error while setting temp version: %v\n", err)
+		}
+		result := IsRelease()
+		a.Equal(value, result, fmt.Sprintf("Expected value [%t] for key [%s]. Got value [%t]", value, key, result))
+	}
+}
+
+func TestGetRancherVersion(t *testing.T) {
+	inputs := map[string]string{
+		"dev-version":    RancherVersionDev,
+		"master-version": RancherVersionDev,
+		"version-head":   RancherVersionDev,
+		"v2.7.X":         "2.7.X",
+		"2.7.X":          "2.7.X",
+	}
+
+	for key, value := range inputs {
+		err := ServerVersion.Set(key)
+		assert.NoError(t, err)
+		result := GetRancherVersion()
+		assert.Equal(t, value, result)
+	}
+}
