931b31c17541ab635f89d0287f35936364ef9f85
Dan Pock <self@danpock.me>
2025-10-03 11:10:27 -0400

    Also rebuild version tests to use uniform test cases

--- pkg/settings/version_test.go
+++ pkg/settings/version_test.go
@@ -1,130 +1,183 @@
 package settings
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
+type testExpectations struct {
+	serverVersionHasReleasePrefixExcludesHead bool
+	getRancherVersion                         string
+	isVersionRelease                          bool
+}
+
+type genericRancherExampleCases struct {
+	name         string
+	version      string
+	expectations testExpectations
+}
+
+var exampleRancherVersions = []genericRancherExampleCases{
+	{
+		"Current Alpha",
+		"v2.12.3-alpha1",
+		testExpectations{
+			true,
+			RancherVersionDev,
+			false,
+		},
+	},
+	{
+		"New Style Alpha",
+		"v2.13.3-alpha.1",
+		testExpectations{
+			true,
+			RancherVersionDev,
+			false,
+		},
+	},
+	{
+		"RC Build (old)",
+		"v2.12.3-rc1",
+		testExpectations{
+			true,
+			RancherVersionDev,
+			false,
+		},
+	},
+	{
+		"RC Build (new)",
+		"v2.12.3-rc.1",
+		testExpectations{
+			true,
+			RancherVersionDev,
+			false,
+		},
+	},
+	{
+		"Stable Build",
+		"v2.12.3",
+		testExpectations{
+			true,
+			"2.12.3",
+			true,
+		},
+	},
+	{
+		"Hotfix Build",
+		"v2.12.0-hotfix-b112.1",
+		testExpectations{
+			true,
+			RancherVersionDev,
+			false,
+		},
+	},
+	{
+		"Dev IDE",
+		"dev",
+		testExpectations{
+			false,
+			RancherVersionDev,
+			false,
+		},
+	},
+	{
+		"Dev Build/Head Images",
+		"v2.12-207d1eaa2-head",
+		testExpectations{
+			false,
+			RancherVersionDev,
+			false,
+		},
+	},
+	{
+		"Manual Build Large Patch wo v prefix",
+		"2.13.9999",
+		testExpectations{
+			false,
+			"2.13.9999",
+			true,
+		},
+	},
+	{
+		"Patch with x",
+		"v2.7.x",
+		testExpectations{
+			true,
+			RancherVersionDev,
+			false,
+		},
+	},
+	{
+		"Branch release prefix",
+		"v2.x",
+		testExpectations{
+			true,
+			RancherVersionDev,
+			false,
+		},
+	},
+	{
+		"Branch release prefix w prerelease",
+		"v3.x-something",
+		testExpectations{
+			true,
+			RancherVersionDev,
+			false,
+		},
+	},
+	{
+		"Branch release head",
+		"v3.x-head",
+		testExpectations{
+			false,
+			RancherVersionDev,
+			false,
+		},
+	},
+}
+
 func Test_ServerVersionHasReleasePrefixExcludesHead(t *testing.T) {
-	inputs := map[string]bool{
-		"dev":         false,
-		"master-head": false,
-		"master":      false,
-		"v2.5.2":      true,
-		"v2":          true,
-		"v2.0":        true,
-		"v2.x":        true,
-		"v2.5-head":   false,
-		"2.5":         false,
-		"2.5-head":    false,
-	}
-	a := assert.New(t)
-	for key, value := range inputs {
-		if err := ServerVersion.Set(key); err != nil {
-			t.Errorf("Encountered error while setting temp version: %v\n", err)
-		}
-		result := ServerVersionHasReleasePrefixExcludesHead()
-		a.Equal(value, result, fmt.Sprintf("Expected value [%t] for key [%s]. Got value [%t]", value, key, result))
+	asserts := assert.New(t)
+	for _, tt := range exampleRancherVersions {
+		t.Run(tt.name, func(t *testing.T) {
+			err := ServerVersion.Set(tt.version)
+			asserts.NoError(err)
+			result := ServerVersionHasReleasePrefixExcludesHead()
+			asserts.Equal(
+				tt.expectations.serverVersionHasReleasePrefixExcludesHead,
+				result,
+				"Expected value [%t] for key [%s]. Got value [%t]",
+				tt.expectations.serverVersionHasReleasePrefixExcludesHead, tt.version, result,
+			)
+		})
 	}
 }
 
 func Test_GetRancherVersion(t *testing.T) {
-	inputs := map[string]string{
-		"dev-version":           RancherVersionDev,
-		"master-version":        RancherVersionDev,
-		"version-head":          RancherVersionDev,
-		"v2.12-dev-someGitHash": RancherVersionDev,
-		"v2.7.X":                RancherVersionDev,
-		"2.7.X":                 RancherVersionDev,
-		"v2.7.0":                "2.7.0",
-		"2.7.0":                 "2.7.0",
-	}
-
-	for key, value := range inputs {
-		err := ServerVersion.Set(key)
-		assert.NoError(t, err)
-		result := GetRancherVersion()
-		assert.Equal(t, value, result)
+	for _, tt := range exampleRancherVersions {
+		t.Run(tt.name, func(t *testing.T) {
+			err := ServerVersion.Set(tt.version)
+			assert.NoError(t, err)
+			result := GetRancherVersion()
+			assert.Equal(t, tt.expectations.getRancherVersion, result, "Expected value [%s] for key [%s]. Got value [%s]", tt.expectations.getRancherVersion, tt.version, result)
+		})
 	}
 }
 
 func Test_IsVersionRelease(t *testing.T) {
-	tests := []struct {
-		name          string
-		serverVersion string
-		want          bool
-	}{
-		{
-			"Normal SemVer",
-			"v2.13.99",
-			true,
-		},
-		{
-			"Normal SemVer wo v prefix",
-			"2.13.99",
-			true,
-		},
-		{
-			"Prerelease head wo patch",
-			"v2.12-head",
-			false,
-		},
-		{
-			"Prerelease head",
-			"v2.12.0-head",
-			false,
-		},
-		{
-			"Prerelease head wo v prefix",
-			"2.12.0-head",
-			false,
-		},
-		{
-			"Prerelease main",
-			"head-main",
-			false,
-		},
-		{
-			"Dev build",
-			"dev-someGitHash",
-			false,
-		},
-		{
-			"Empty version",
-			"",
-			false,
-		},
-		{
-			"Prerelease Dev Build",
-			"v2.12-dev-someGitHash",
-			false,
-		},
-	}
-	for _, tt := range tests {
+	for _, tt := range exampleRancherVersions {
 		t.Run(tt.name, func(t *testing.T) {
-			assert.Equalf(t, tt.want, IsVersionRelease(tt.serverVersion), "IsVersionRelease(%v)", tt.serverVersion)
+			assert.Equalf(t, tt.expectations.isVersionRelease, IsVersionRelease(tt.version), "IsVersionRelease(%v)", tt.version)
 		})
 	}
 }
 
 func Test_ServerVersionOrFallback(t *testing.T) {
-	tests := map[string]string{
-		"":                      RancherVersionDev,
-		"dev-version":           RancherVersionDev,
-		"master-version":        RancherVersionDev,
-		"version-head":          RancherVersionDev,
-		"v2.12-dev-someGitHash": RancherVersionDev,
-		"v2.7.X":                RancherVersionDev,
-		"2.7.X":                 RancherVersionDev,
-		"v2.7.0":                "2.7.0",
-		"2.7.0":                 "2.7.0",
-	}
-
-	for input, expected := range tests {
-		t.Run(fmt.Sprintf("%s => %s", input, expected), func(t *testing.T) {
-			assert.Equalf(t, expected, ServerVersionOrFallback(input), "ServerVersionOrFallback(%s)", input)
+	for _, tt := range exampleRancherVersions {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equalf(t, tt.expectations.getRancherVersion, ServerVersionOrFallback(tt.version), "ServerVersionOrFallback(%s)", tt.version)
 		})
 	}
 }
