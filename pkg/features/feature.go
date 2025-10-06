b2c8ecfb4905424293f41284055a78ada73b4e3f
Tom Lebreux <tom.lebreux@suse.com>
2025-10-06 14:48:04 -0400

    Add feature.Unset method

--- pkg/features/feature.go
+++ pkg/features/feature.go
@@ -266,7 +266,11 @@ func InitializeFeatures(featuresClient managementv3.FeatureClient, featureArgs s
 			if newVal == nil {
 				newVal = featureState.Spec.Value
 			}
-			f.Set(newVal)
+			if newVal == nil {
+				f.Unset()
+			} else {
+				f.Set(*newVal)
+			}
 		}
 	}
 }
@@ -368,8 +372,12 @@ func (f *Feature) Dynamic() bool {
 	return f.dynamic
 }
 
-func (f *Feature) Set(val *bool) {
-	f.val = val
+func (f *Feature) Set(val bool) {
+	f.val = &val
+}
+
+func (f *Feature) Unset() {
+	f.val = nil
 }
 
 func (f *Feature) Name() string {
