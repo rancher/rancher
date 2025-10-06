3644a600a66b34dc9445a1bfb312bdc3ff1382fd
Dan Pock <self@danpock.me>
2025-10-06 16:08:36 -0400

    Improve func docs to explain context

--- pkg/settings/version.go
+++ pkg/settings/version.go
@@ -7,7 +7,7 @@ import (
 )
 
 // ServerVersionHasReleasePrefixExcludesHead returns true if the running server has the release prefix `v` and doesn't include `head`.
-// This function is primarily used by the UI to know when to use local (to rancher image) assets or web assets
+// This function is primarily used by the UI to know when to use local (to rancher image) assets or web assets.
 func ServerVersionHasReleasePrefixExcludesHead() bool {
 	serverVersion := ServerVersion.Get()
 	version := semver.Version(serverVersion)
