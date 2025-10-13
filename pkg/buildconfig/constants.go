5e2cccc414224b0f08cc6f8cf5c6e928cd339e5d
Gabriel Silva Bueno <gabriel.bueno@192.168.0.34>
2025-10-13 14:18:15 -0300

    update csp-adapter version

--- pkg/buildconfig/constants.go
+++ pkg/buildconfig/constants.go
@@ -3,7 +3,7 @@
 package buildconfig
 
 const (
-	CspAdapterMinVersion     = "107.0.0+up7.0.0-rc.1"
+	CspAdapterMinVersion     = "108.0.0+up8.0.0-rc.1"
 	DefaultSccOperatorImage  = "rancher/scc-operator:head"
 	DefaultShellVersion      = "rancher/shell:v0.5.0"
 	FleetVersion             = "108.0.0+up0.14.0-alpha.4"
