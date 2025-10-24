20f92fac8e0ec29f010402e1914b1d73624fbd5c
Tom Lebreux <tom.lebreux@suse.com>
2025-10-24 13:45:40 -0400

    [release/v2.12] Don't restart Rancher on feature flag default change (#52492)
    This reverts commit 0d3171558be6011060444426b4b64a0b7ee70cc9.

--- pkg/features/feature_test.go
+++ pkg/features/feature_test.go
@@ -1,6 +1,7 @@
 package features
 
 import (
+	"fmt"
 	"testing"
 
 	v3 "github.com/rancher/rancher/pkg/apis/management.cattle.io/v3"
@@ -65,6 +66,124 @@ func TestInitializeFeatures(t *testing.T) {
 		t.Run(name, func(t *testing.T) {
 			features = test.features // we can't run parallel tests if we modify this package var here. Code should be refactored to not use a package var if more tests cases are added.
 			InitializeFeatures(test.featureMock(), "")
+		})
+	}
+}
+
+func TestRequireRestarts(t *testing.T) {
+	trueVal := true
+	falseVal := false
+
+	boolStr := func(b *bool) string {
+		if b == nil {
+			return "nil"
+		}
+		return fmt.Sprintf("%v", *b)
+	}
+
+	tests := []struct {
+		def             bool
+		val             *bool
+		specVal         *bool
+		lockedVal       *bool
+		expectedRestart bool
+	}{
+		// Default false, .spec.value
+		// Unset case
+		{def: false, val: nil},
+		{def: false, val: &trueVal, expectedRestart: true},
+		{def: false, val: &falseVal},
+		// Set to true case
+		{def: false, val: nil, specVal: &trueVal, expectedRestart: true},
+		{def: false, val: &trueVal, specVal: &trueVal},
+		{def: false, val: &falseVal, specVal: &trueVal, expectedRestart: true},
+		// Set to false case
+		{def: false, val: nil, specVal: &falseVal},
+		{def: false, val: &falseVal, specVal: &falseVal},
+		{def: false, val: &trueVal, specVal: &falseVal, expectedRestart: true},
+
+		// Default true, .spec.value
+		// Unset case
+		{def: true, val: nil},
+		{def: true, val: &trueVal},
+		{def: true, val: &falseVal, expectedRestart: true},
+		// Set to true case
+		{def: true, val: nil, specVal: &trueVal},
+		{def: true, val: &trueVal, specVal: &trueVal},
+		{def: true, val: &falseVal, specVal: &trueVal, expectedRestart: true},
+		// Set to false case
+		{def: true, val: nil, specVal: &falseVal, expectedRestart: true},
+		{def: true, val: &falseVal, specVal: &falseVal},
+		{def: true, val: &trueVal, specVal: &falseVal, expectedRestart: true},
+
+		// Default false, .status.lockedValue
+		// Unset case
+		{def: false, val: nil},
+		{def: false, val: &trueVal, expectedRestart: true},
+		{def: false, val: &falseVal},
+		// Set to true case
+		{def: false, val: nil, lockedVal: &trueVal, expectedRestart: true},
+		{def: false, val: &trueVal, lockedVal: &trueVal},
+		{def: false, val: &falseVal, lockedVal: &trueVal, expectedRestart: true},
+		// Set to false case
+		{def: false, val: nil, lockedVal: &falseVal},
+		{def: false, val: &falseVal, lockedVal: &falseVal},
+		{def: false, val: &trueVal, lockedVal: &falseVal, expectedRestart: true},
+
+		// Default true, .status.lockedValue
+		// Unset case
+		{def: true, val: nil},
+		{def: true, val: &trueVal},
+		{def: true, val: &falseVal, expectedRestart: true},
+		// Set to true case
+		{def: true, val: nil, lockedVal: &trueVal},
+		{def: true, val: &trueVal, lockedVal: &trueVal},
+		{def: true, val: &falseVal, lockedVal: &trueVal, expectedRestart: true},
+		// Set to false case
+		{def: true, val: nil, lockedVal: &falseVal, expectedRestart: true},
+		{def: true, val: &falseVal, lockedVal: &falseVal},
+		{def: true, val: &trueVal, lockedVal: &falseVal, expectedRestart: true},
+	}
+	for _, test := range tests {
+		name := fmt.Sprintf("non-dynamic,def=%v,val=%s,specVal=%s,lockedVal=%s", test.def, boolStr(test.val), boolStr(test.specVal), boolStr(test.lockedVal))
+		t.Run(name, func(t *testing.T) {
+			feat := &Feature{
+				dynamic: false,
+				def:     test.def,
+				val:     test.val,
+			}
+			featureObj := &v3.Feature{
+				ObjectMeta: metav1.ObjectMeta{},
+				Spec: v3.FeatureSpec{
+					Value: test.specVal,
+				},
+				Status: v3.FeatureStatus{
+					LockedValue: test.lockedVal,
+				},
+			}
+			needsRestart := RequireRestarts(feat, featureObj)
+			assert.Equal(t, test.expectedRestart, needsRestart)
+		})
+	}
+	for _, test := range tests {
+		name := fmt.Sprintf("dynamic,def=%v,val=%s,specVal=%s,lockedVal=%s", test.def, boolStr(test.val), boolStr(test.specVal), boolStr(test.lockedVal))
+		t.Run(name, func(t *testing.T) {
+			feat := &Feature{
+				dynamic: true,
+				def:     test.def,
+				val:     test.val,
+			}
+			featureObj := &v3.Feature{
+				ObjectMeta: metav1.ObjectMeta{},
+				Spec: v3.FeatureSpec{
+					Value: test.specVal,
+				},
+				Status: v3.FeatureStatus{
+					LockedValue: test.lockedVal,
+				},
+			}
+			needsRestart := RequireRestarts(feat, featureObj)
+			assert.False(t, needsRestart)
 		})
 	}
 }
