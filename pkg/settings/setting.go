1c876fb2d4f6820f4b612483b0e154b1259ceeca
vardhaman22 <vardhaman.surana@suse.com>
2025-11-07 13:30:55 +0530

    fix alibaba driver name to sync with kev2 operator name

--- pkg/settings/setting.go
+++ pkg/settings/setting.go
@@ -135,7 +135,7 @@ var (
 	EKSUpstreamRefreshCron              = NewSetting("eks-refresh-cron", "*/5 * * * *") // EKSUpstreamRefreshCron is deprecated and will be replaced by EKSUpstreamRefresh
 	EKSUpstreamRefresh                  = NewSetting("eks-refresh", "300")
 	GKEUpstreamRefresh                  = NewSetting("gke-refresh", "300")
-	AlibabaUpstreamRefresh              = NewSetting("alibabacloud-refresh", "300")
+	AlibabaUpstreamRefresh              = NewSetting("alibaba-refresh", "300")
 	HideLocalCluster                    = NewSetting("hide-local-cluster", "false")
 	MachineProvisionImage               = NewSetting("machine-provision-image", "rancher/machine:v0.15.0-rancher137")
 	SystemFeatureChartRefreshSeconds    = NewSetting("system-feature-chart-refresh-seconds", "21600")
