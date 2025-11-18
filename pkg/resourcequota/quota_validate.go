2933a81547d11fc7a372f14d74c9d5807fcce9a9
Andreas Kupries <andreas.kupries@suse.com>
2025-11-18 14:30:38 +0100

    fix: merge two conversion functions into a single
      - this fixes the missing handling of `extended` in the
        `ConvertLimitToResourceList`.
    
      - it also brings over a fix to the mapping from rancher field names to kube
        resource names. one of the improperly mapped fields was `limitsCpu`.  This
        is now properly mapped to `limits.cpu`.
    
        this fix exposed a bug in the integration tests, where the test was written
        to adhere to the original bogus behaviour. With `limitsCpu` written and
        `limits.cpu` read by the test the actual data to expect was not seen, and 0
        read. And expected for.
    
        the test in question is now corrected.
    
    note: tech debt against r/webhook

--- pkg/resourcequota/quota_validate.go
+++ pkg/resourcequota/quota_validate.go
@@ -1,6 +1,7 @@
 package resourcequota
 
 import (
+	"fmt"
 	"sync"
 	"time"
 
@@ -12,8 +13,22 @@ import (
 	quota "k8s.io/apiserver/pkg/quota/v1"
 )
 
+const ExtendedKey = "extended"
+
 var (
-	projectLockCache = cache.NewLRUExpireCache(1000)
+	projectLockCache        = cache.NewLRUExpireCache(1000)
+	resourceQuotaConversion = map[string]string{
+		"replicationControllers": "replicationcontrollers",
+		"configMaps":             "configmaps",
+		"persistentVolumeClaims": "persistentvolumeclaims",
+		"servicesNodePorts":      "services.nodeports",
+		"servicesLoadBalancers":  "services.loadbalancers",
+		"requestsCpu":            "requests.cpu",
+		"requestsMemory":         "requests.memory",
+		"requestsStorage":        "requests.storage",
+		"limitsCpu":              "limits.cpu",
+		"limitsMemory":           "limits.memory",
+	}
 )
 
 func GetProjectLock(projectID string) *sync.Mutex {
@@ -30,21 +45,21 @@ func IsQuotaFit(nsLimit *v32.ResourceQuotaLimit, nsLimits []*v32.ResourceQuotaLi
 	nssResourceList := api.ResourceList{}
 	nsResourceList, err := ConvertLimitToResourceList(nsLimit)
 	if err != nil {
-		return false, nil, err
+		return false, nil, fmt.Errorf("checking quota fit: %w", err)
 	}
 	nssResourceList = quota.Add(nssResourceList, nsResourceList)
 
 	for _, nsLimit := range nsLimits {
 		nsResourceList, err := ConvertLimitToResourceList(nsLimit)
 		if err != nil {
-			return false, nil, err
+			return false, nil, fmt.Errorf("checking namespace limits: %w", err)
 		}
 		nssResourceList = quota.Add(nssResourceList, nsResourceList)
 	}
 
 	projectResourceList, err := ConvertLimitToResourceList(projectLimit)
 	if err != nil {
-		return false, nil, err
+		return false, nil, fmt.Errorf("checking project limits: %w", err)
 	}
 
 	_, exceeded := quota.LessThanOrEqual(nssResourceList, projectResourceList)
@@ -58,17 +73,44 @@ func IsQuotaFit(nsLimit *v32.ResourceQuotaLimit, nsLimits []*v32.ResourceQuotaLi
 }
 
 func ConvertLimitToResourceList(limit *v32.ResourceQuotaLimit) (api.ResourceList, error) {
+	// TECH DEBT: Any change here has to be reflected in rancher/webhook
+	//   pkg/resources/management.cattle.io/v3/project/quota_validate.go
+	// until such time as both places are unified in a single function shared between r/r and r/w
+
 	toReturn := api.ResourceList{}
 	converted, err := convert.EncodeToMap(limit)
 	if err != nil {
 		return nil, err
 	}
-	for key, value := range converted {
-		q, err := resource.ParseQuantity(convert.ToString(value))
-		if err != nil {
-			return nil, err
+
+	// convert the extended set first, ...
+	if extended, ok := converted[ExtendedKey]; ok {
+		delete(converted, ExtendedKey)
+		for key, value := range extended.(map[string]any) {
+			resourceName := api.ResourceName(key)
+			resourceQuantity, err := resource.ParseQuantity(value.(string))
+			if err != nil {
+				return nil, fmt.Errorf("failed to parse value for key %q: %w", key, err)
+			}
+
+			toReturn[resourceName] = resourceQuantity
 		}
-		toReturn[api.ResourceName(key)] = q
+	}
+
+	// then place the fixed data. this order ensures that in case of
+	// conflicts between arbitrary and fixed data the fixed data wins.
+	for key, value := range converted {
+		var resourceName api.ResourceName
+		if val, ok := resourceQuotaConversion[key]; ok {
+			resourceName = api.ResourceName(val)
+		} else {
+			resourceName = api.ResourceName(key)
+		}
+		resourceQuantity, err := resource.ParseQuantity(convert.ToString(value))
+		if err != nil {
+			return nil, fmt.Errorf("parsing quantity %q: %w", key, err)
+		}
+		toReturn[resourceName] = resourceQuantity
 	}
 	return toReturn, nil
 }
