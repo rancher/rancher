7ac52a0feda685b8652810390f73f4549c7f95a6
Patrick Seidensal <pseidensal@suse.com>
2025-11-19 09:03:56 +0100

    Adapt code according to review comments
    - Adopt customization when creating v3 cluster from v1 cluster
    - Separate Rancher agent and Fleet agent scheduling customization
      annotations for applying defaults independently of each other

--- pkg/controllers/provisioningv2/cluster/scheduling_customization.go
+++ pkg/controllers/provisioningv2/cluster/scheduling_customization.go
@@ -11,12 +11,6 @@ import (
 	"github.com/rancher/rancher/pkg/settings"
 )
 
-// updateV1SchedulingCustomization looks for the provisioning.cattle.io/enable-scheduling-customization annotation on
-// the v1.Cluster and if found populates the spec.ClusterAgentDeploymentCustomization.SchedulingCustomization and
-// spec.FleetAgentDeploymentCustomization.SchedulingCustomization fields with the default values set in the global
-// settings. If the cluster-agent-scheduling-customization feature is disabled, the cluster will be returned unchanged.
-// The provisioning.cattle.io/enable-scheduling-customization annotation can be set to 'true' or 'false', which will add
-// or remove the scheduling customization field as needed.
 func (h *handler) updateV1SchedulingCustomization(_ string, cluster *v1.Cluster) (*v1.Cluster, error) {
 	if cluster == nil {
 		return nil, nil
@@ -26,6 +20,49 @@ func (h *handler) updateV1SchedulingCustomization(_ string, cluster *v1.Cluster)
 		return cluster, nil
 	}
 
+	cluster, err := h.updateV1AgentSchedulingCustomization(cluster)
+	if err != nil {
+		return nil, err
+	}
+
+	cluster, err = h.updateV1FleetAgentSchedulingCustomization(cluster)
+	if err != nil {
+		return nil, err
+	}
+
+	return cluster, nil
+}
+
+func (h *handler) updateV3SchedulingCustomization(_ string, cluster *v3.Cluster) (*v3.Cluster, error) {
+	if cluster == nil {
+		return nil, nil
+	}
+
+	if !features.ClusterAgentSchedulingCustomization.Enabled() {
+		return cluster, nil
+	}
+
+	cluster, err := h.updateV3AgentSchedulingCustomization(cluster)
+	if err != nil {
+		return nil, err
+	}
+
+	cluster, err = h.updateV3FleetAgentSchedulingCustomization(cluster)
+	if err != nil {
+		return nil, err
+	}
+
+	return cluster, nil
+}
+
+// updateV1AgentSchedulingCustomization looks for the
+// provisioning.cattle.io/enable-scheduling-customization annotation on the v1.Cluster and if found
+// populates the spec.ClusterAgentDeploymentCustomization.SchedulingCustomization fields with the
+// default values set in the global settings. If the cluster-agent-scheduling-customization feature
+// is disabled, the cluster will be returned unchanged. The
+// provisioning.cattle.io/enable-scheduling-customization annotation can be set to 'true' or
+// 'false', which will add or remove the scheduling customization field as needed.
+func (h *handler) updateV1AgentSchedulingCustomization(cluster *v1.Cluster) (*v1.Cluster, error) {
 	value, ok := cluster.Annotations[manageSchedulingDefaultsAnn]
 	if !ok {
 		return cluster, nil
@@ -44,16 +81,11 @@ func (h *handler) updateV1SchedulingCustomization(_ string, cluster *v1.Cluster)
 			cluster.Spec.ClusterAgentDeploymentCustomization.SchedulingCustomization = nil
 		}
 
-		if cluster.Spec.FleetAgentDeploymentCustomization != nil {
-			cluster.Spec.FleetAgentDeploymentCustomization.SchedulingCustomization = nil
-		}
-
 		return h.clusters.Update(cluster)
 	}
 
 	// annotation was added to a cluster that already has the fields set, we should not override the existing values.
-	if cluster.Spec.ClusterAgentDeploymentCustomization != nil && cluster.Spec.ClusterAgentDeploymentCustomization.SchedulingCustomization != nil ||
-		cluster.Spec.FleetAgentDeploymentCustomization != nil && cluster.Spec.FleetAgentDeploymentCustomization.SchedulingCustomization != nil {
+	if cluster.Spec.ClusterAgentDeploymentCustomization != nil && cluster.Spec.ClusterAgentDeploymentCustomization.SchedulingCustomization != nil {
 		delete(cluster.Annotations, manageSchedulingDefaultsAnn)
 		return h.clusters.Update(cluster)
 	}
@@ -72,7 +104,46 @@ func (h *handler) updateV1SchedulingCustomization(_ string, cluster *v1.Cluster)
 		}
 	}
 
-	defaultPC, defaultPDB, err = getDefaultFleetSchedulingCustomization[v1.PriorityClassSpec, v1.PodDisruptionBudgetSpec]()
+	delete(cluster.Annotations, manageSchedulingDefaultsAnn)
+	return h.clusters.Update(cluster)
+}
+
+// updateV1FleetAgentSchedulingCustomization looks for the
+// provisioning.cattle.io/enable-scheduling-customization annotation on the v1.Cluster and if found
+// populates the spec.FleetAgentDeploymentCustomization.SchedulingCustomization fields with the
+// default values set in the global settings. If the cluster-agent-scheduling-customization feature
+// is disabled, the cluster will be returned unchanged. The
+// provisioning.cattle.io/enable-fleet-scheduling-customization annotation can be set to 'true' or
+// 'false', which will add or remove the scheduling customization field as needed.
+func (h *handler) updateV1FleetAgentSchedulingCustomization(cluster *v1.Cluster) (*v1.Cluster, error) {
+	value, ok := cluster.Annotations[manageFleetSchedulingDefaultsAnn]
+	if !ok {
+		return cluster, nil
+	}
+
+	lowerVal := strings.ToLower(value)
+	if lowerVal != "true" && lowerVal != "false" {
+		return cluster, nil
+	}
+
+	cluster = cluster.DeepCopy()
+	if lowerVal == "false" {
+		delete(cluster.Annotations, manageFleetSchedulingDefaultsAnn)
+
+		if cluster.Spec.FleetAgentDeploymentCustomization != nil {
+			cluster.Spec.FleetAgentDeploymentCustomization.SchedulingCustomization = nil
+		}
+
+		return h.clusters.Update(cluster)
+	}
+
+	// annotation was added to a cluster that already has the fields set, we should not override the existing values.
+	if cluster.Spec.FleetAgentDeploymentCustomization != nil && cluster.Spec.FleetAgentDeploymentCustomization.SchedulingCustomization != nil {
+		delete(cluster.Annotations, manageFleetSchedulingDefaultsAnn)
+		return h.clusters.Update(cluster)
+	}
+
+	defaultPC, defaultPDB, err := getDefaultFleetSchedulingCustomization[v1.PriorityClassSpec, v1.PodDisruptionBudgetSpec]()
 	if err != nil {
 		return cluster, fmt.Errorf("failed to get default scheduling customization: %w", err)
 	}
@@ -86,28 +157,20 @@ func (h *handler) updateV1SchedulingCustomization(_ string, cluster *v1.Cluster)
 		}
 	}
 
-	delete(cluster.Annotations, manageSchedulingDefaultsAnn)
+	delete(cluster.Annotations, manageFleetSchedulingDefaultsAnn)
 	return h.clusters.Update(cluster)
 }
 
-// updateV3SchedulingCustomization looks for the provisioning.cattle.io/enable-scheduling-customization annotation on
+// updateV3AgentSchedulingCustomization looks for the provisioning.cattle.io/enable-scheduling-customization annotation on
 // the v3.Cluster and if found populates the spec.ClusterAgentDeploymentCustomization.SchedulingCustomization and
 // spec.FleetAgentDeploymentCustomization.SchedulingCustomization fields with the default values set in the global
 // settings. If the cluster-agent-scheduling-customization feature is disabled, the cluster will be returned unchanged.
 // The provisioning.cattle.io/enable-scheduling-customization annotation can be set to 'true' or 'false', which will add
-// or remove the scheduling customization field as needed. updateV3SchedulingCustomization is intended to handle KEv2
+// or remove the scheduling customization field as needed. updateV3AgentSchedulingCustomization is intended to handle KEv2
 // and legacy clusters specifically.
-func (h *handler) updateV3SchedulingCustomization(_ string, cluster *v3.Cluster) (*v3.Cluster, error) {
-	if cluster == nil {
-		return nil, nil
-	}
-
+func (h *handler) updateV3AgentSchedulingCustomization(cluster *v3.Cluster) (*v3.Cluster, error) {
 	if !h.isLegacyCluster(cluster) {
 		return nil, nil
-	}
-
-	if !features.ClusterAgentSchedulingCustomization.Enabled() {
-		return cluster, nil
 	}
 
 	value, ok := cluster.Annotations[manageSchedulingDefaultsAnn]
@@ -127,16 +190,12 @@ func (h *handler) updateV3SchedulingCustomization(_ string, cluster *v3.Cluster)
 		if cluster.Spec.ClusterAgentDeploymentCustomization != nil {
 			cluster.Spec.ClusterAgentDeploymentCustomization.SchedulingCustomization = nil
 		}
-		if cluster.Spec.FleetAgentDeploymentCustomization != nil {
-			cluster.Spec.FleetAgentDeploymentCustomization.SchedulingCustomization = nil
-		}
 
 		return h.mgmtClusters.Update(cluster)
 	}
 
 	// annotation was added to a cluster that already has the fields set, we should not override the existing values.
-	if cluster.Spec.ClusterAgentDeploymentCustomization != nil && cluster.Spec.ClusterAgentDeploymentCustomization.SchedulingCustomization != nil ||
-		cluster.Spec.FleetAgentDeploymentCustomization != nil && cluster.Spec.FleetAgentDeploymentCustomization.SchedulingCustomization != nil {
+	if cluster.Spec.ClusterAgentDeploymentCustomization != nil && cluster.Spec.ClusterAgentDeploymentCustomization.SchedulingCustomization != nil {
 		delete(cluster.Annotations, manageSchedulingDefaultsAnn)
 		return h.mgmtClusters.Update(cluster)
 	}
@@ -155,7 +214,51 @@ func (h *handler) updateV3SchedulingCustomization(_ string, cluster *v3.Cluster)
 		}
 	}
 
-	defaultPC, defaultPDB, err = getDefaultFleetSchedulingCustomization[v3.PriorityClassSpec, v3.PodDisruptionBudgetSpec]()
+	delete(cluster.ObjectMeta.Annotations, manageSchedulingDefaultsAnn)
+	return h.mgmtClusters.Update(cluster)
+}
+
+// updateV3FleetAgentSchedulingCustomization looks for the
+// provisioning.cattle.io/enable-scheduling-customization annotation on the v3.Cluster and if found
+// populates the spec.FleetAgentDeploymentCustomization.SchedulingCustomization fields with the
+// default values set in the global settings. If the cluster-agent-scheduling-customization feature
+// is disabled, the cluster will be returned unchanged. The
+// provisioning.cattle.io/enable-scheduling-customization annotation can be set to 'true' or
+// 'false', which will add or remove the scheduling customization field as needed.
+// updateV3FleetAgentSchedulingCustomization is intended to handle KEv2 and legacy clusters specifically.
+func (h *handler) updateV3FleetAgentSchedulingCustomization(cluster *v3.Cluster) (*v3.Cluster, error) {
+	if !h.isLegacyCluster(cluster) {
+		return nil, nil
+	}
+
+	value, ok := cluster.Annotations[manageFleetSchedulingDefaultsAnn]
+	if !ok {
+		return cluster, nil
+	}
+
+	lowerVal := strings.ToLower(value)
+	if lowerVal != "true" && lowerVal != "false" {
+		return cluster, nil
+	}
+
+	cluster = cluster.DeepCopy()
+	if lowerVal == "false" {
+		delete(cluster.Annotations, manageFleetSchedulingDefaultsAnn)
+
+		if cluster.Spec.FleetAgentDeploymentCustomization != nil {
+			cluster.Spec.FleetAgentDeploymentCustomization.SchedulingCustomization = nil
+		}
+
+		return h.mgmtClusters.Update(cluster)
+	}
+
+	// annotation was added to a cluster that already has the fields set, we should not override the existing values.
+	if cluster.Spec.FleetAgentDeploymentCustomization != nil && cluster.Spec.FleetAgentDeploymentCustomization.SchedulingCustomization != nil {
+		delete(cluster.Annotations, manageFleetSchedulingDefaultsAnn)
+		return h.mgmtClusters.Update(cluster)
+	}
+
+	defaultPC, defaultPDB, err := getDefaultFleetSchedulingCustomization[v3.PriorityClassSpec, v3.PodDisruptionBudgetSpec]()
 	if err != nil {
 		return cluster, fmt.Errorf("failed to get default scheduling customization: %w", err)
 	}
@@ -169,7 +272,7 @@ func (h *handler) updateV3SchedulingCustomization(_ string, cluster *v3.Cluster)
 		}
 	}
 
-	delete(cluster.ObjectMeta.Annotations, manageSchedulingDefaultsAnn)
+	delete(cluster.ObjectMeta.Annotations, manageFleetSchedulingDefaultsAnn)
 	return h.mgmtClusters.Update(cluster)
 }
 
