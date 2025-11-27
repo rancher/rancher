b8d2782e2c88163a70547d14cadd47024c646311
Tom Lebreux <tom.lebreux@suse.com>
2025-11-27 12:18:03 -0500

    UnRC remotedialer-proxy (#52874)

--- pkg/buildconfig/constants.go
+++ pkg/buildconfig/constants.go
@@ -9,7 +9,7 @@ const (
 	DefaultShellVersion           = "rancher/shell:v0.6.0-rc.1"
 	FleetVersion                  = "108.0.0+up0.14.0-rc.1"
 	ProvisioningCAPIVersion       = "108.0.0+up0.9.0"
-	RemoteDialerProxyVersion      = "106.0.2+up0.6.0-rc.4"
+	RemoteDialerProxyVersion      = "106.0.2+up0.6.0"
 	TurtlesVersion                = "108.0.0+up0.25.0-rc.4"
 	WebhookVersion                = "108.0.0+up0.9.0-rc.14"
 )
