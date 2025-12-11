9e9374cada120f838c4c205613baa367d4abfd6b
Dan Pock <self@danpock.me>
2025-12-11 09:54:31 -0500

    Bump Shell for 2.13.1

--- pkg/buildconfig/constants.go
+++ pkg/buildconfig/constants.go
@@ -6,7 +6,7 @@ const (
 	ClusterAutoscalerChartVersion = "9.50.1"
 	CspAdapterMinVersion          = "108.0.0+up8.0.0"
 	DefaultSccOperatorImage       = "rancher/scc-operator:v0.3.1"
-	DefaultShellVersion           = "rancher/shell:v0.6.0"
+	DefaultShellVersion           = "rancher/shell:v0.6.1-rc.1"
 	FleetVersion                  = "108.0.1+up0.14.1-rc.1"
 	ProvisioningCAPIVersion       = "108.0.0+up0.9.0"
 	RemoteDialerProxyVersion      = "106.0.2+up0.6.0"
