4ea48bd1087a7f4cb223e604c3a274c9a285c407
joshmeranda <joshua.meranda@suse.com>
2026-01-08 10:06:21 -0500

    implement wrapper core interface

---
+++ pkg/namespace/interfaces.go
@@ -0,0 +1,53 @@
+package namespace
+
+import (
+	wranglercore "github.com/rancher/wrangler/v3/pkg/generated/controllers/core"
+	wranglercorev1 "github.com/rancher/wrangler/v3/pkg/generated/controllers/core/v1"
+	_ "k8s.io/api/core/v1"
+	corev1 "k8s.io/api/core/v1"
+)
+
+func NewFactory(f *wranglercore.Factory) *Factory {
+	return &Factory{
+		f,
+	}
+}
+
+type Factory struct {
+	*wranglercore.Factory
+}
+
+func (f *Factory) Core() wranglercore.Interface {
+	return &core{
+		f.Factory.Core(),
+	}
+}
+
+type core struct {
+	wranglercore.Interface
+}
+
+func (c *core) V1() wranglercorev1.Interface {
+	return &v1{
+		c.Interface.V1(),
+	}
+}
+
+type v1 struct {
+	wranglercorev1.Interface
+}
+
+func (v *v1) Namespace() wranglercorev1.NamespaceController {
+	return &namespace{
+		v.Interface.Namespace(),
+	}
+}
+
+type namespace struct {
+	wranglercorev1.NamespaceController
+}
+
+func (n *namespace) Create(ns *corev1.Namespace) (*corev1.Namespace, error) {
+	ApplyLabelsAndAnnotations(ns)
+	return n.NamespaceController.Create(ns)
+}
