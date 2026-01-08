744aaae3cb85f544fa4466a765b2d3dbe908dc74
Dan Pock <self@danpock.me>
2026-01-08 17:21:17 -0500

    Set up volumes for read only root

--- pkg/api/steve/clusters/shell.go
+++ pkg/api/steve/clusters/shell.go
@@ -178,6 +178,41 @@ func (s *shell) createPod(imageOverride string) *v1.Pod {
 					ImagePullPolicy: v1.PullIfNotPresent,
 					SecurityContext: &v1.SecurityContext{
 						AllowPrivilegeEscalation: &f,
+						ReadOnlyRootFilesystem:   &t,
+					},
+					VolumeMounts: []v1.VolumeMount{
+						{
+							Name:      "tmp",
+							MountPath: "/tmp",
+						},
+						{
+							Name:      "run",
+							MountPath: "/run",
+						},
+						{
+							Name:      "helm-run",
+							MountPath: "/home/shell/helm-run",
+						},
+					},
+				},
+			},
+			Volumes: []v1.Volume{
+				{
+					Name: "tmp",
+					VolumeSource: v1.VolumeSource{
+						EmptyDir: &v1.EmptyDirVolumeSource{},
+					},
+				},
+				{
+					Name: "run",
+					VolumeSource: v1.VolumeSource{
+						EmptyDir: &v1.EmptyDirVolumeSource{},
+					},
+				},
+				{
+					Name: "helm-run",
+					VolumeSource: v1.VolumeSource{
+						EmptyDir: &v1.EmptyDirVolumeSource{},
 					},
 				},
 			},
