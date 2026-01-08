99ed1615f7711405c0168117b30c98613178c0aa
Dan Pock <self@danpock.me>
2026-01-08 17:09:30 -0500

    Launch the Shell pod as non-root

--- pkg/api/steve/clusters/shell.go
+++ pkg/api/steve/clusters/shell.go
@@ -114,12 +114,19 @@ func (s *shell) createPod(imageOverride string) *v1.Pod {
 	if imageName == "" {
 		imageName = settings.FullShellImage()
 	}
+	runAsUser := int64(1000)
 	return &v1.Pod{
 		ObjectMeta: metav1.ObjectMeta{
 			GenerateName: "dashboard-shell-",
 			Namespace:    s.namespace,
 		},
 		Spec: v1.PodSpec{
+			SecurityContext: &v1.PodSecurityContext{
+				RunAsNonRoot: boolPointer(true),
+				RunAsUser:    &runAsUser,
+				RunAsGroup:   &runAsUser,
+				FSGroup:      &runAsUser,
+			},
 			TerminationGracePeriodSeconds: new(int64),
 			RestartPolicy:                 v1.RestartPolicyNever,
 			NodeSelector: map[string]string{
@@ -165,8 +172,15 @@ func (s *shell) createPod(imageOverride string) *v1.Pod {
 					},
 					Image:           imageName,
 					ImagePullPolicy: v1.PullIfNotPresent,
+					SecurityContext: &v1.SecurityContext{
+						AllowPrivilegeEscalation: boolPointer(false),
+					},
 				},
 			},
 		},
 	}
+}
+
+func boolPointer(b bool) *bool {
+	return &b
 }
