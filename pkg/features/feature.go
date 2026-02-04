06b426ac5a3abbc5914b1ecc25302b8142bc4a22
Dan Pock <self@danpock.me>
2026-02-04 13:39:21 -0500

    Add cluster shell feature flag that can be disabled

--- pkg/features/feature.go
+++ pkg/features/feature.go
@@ -204,6 +204,13 @@ var (
 		false,
 		true,
 	)
+	ClusterShell = newFeature(
+		"cluster-shell",
+		"Enable the Rancher cluster shell feature",
+		true,
+		false,
+		true,
+	)
 )
 
 func ListEnabled() []string {
