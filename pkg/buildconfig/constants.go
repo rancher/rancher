c855628c11af90ff8a4df3e8e169687a344bb2f1
Dan Pock <self@danpock.me>
2026-02-13 15:00:26 -0500

    Bump Shell to 2.14 branch

--- pkg/buildconfig/constants.go
+++ pkg/buildconfig/constants.go
@@ -6,7 +6,7 @@ const (
 	ClusterAutoscalerChartVersion = "9.50.1"
 	CspAdapterMinVersion          = "109.0.0+up9.0.0-rc.3"
 	DefaultSccOperatorImage       = "rancher/scc-operator:v0.3.1-rc.2"
-	DefaultShellVersion           = "rancher/shell:v0.6.2-rc.1"
+	DefaultShellVersion           = "rancher/shell:v0.7.0-rc.2"
 	FleetVersion                  = "109.0.0+up0.15.0-alpha.10"
 	RemoteDialerProxyVersion      = "109.0.0+up0.7.0-rc.5"
 	TurtlesVersion                = "109.0.0+up0.26.0-rc.2"
