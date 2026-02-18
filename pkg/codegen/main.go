4b2ac57d00321cf72332f530f25e132d1eb4bb01
vardhaman22 <vardhaman.surana@suse.com>
2026-02-18 12:30:59 +0530

    updated openapi model package name

--- pkg/codegen/main.go
+++ pkg/codegen/main.go
@@ -133,6 +133,7 @@ func main() {
 					"k8s.io/apimachinery/pkg/runtime",
 					"k8s.io/apimachinery/pkg/version",
 				},
+				OpenAPIModelPackageName: "ext.cattle.io.v1",
 			},
 			"telemetry.cattle.io": {
 				PackageName: "telemetry.cattle.io",
