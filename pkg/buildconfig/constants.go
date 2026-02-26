64e0ed9e3b4910df616335aececdf870a1a4fe2b
Dan Pock <self@danpock.me>
2026-02-26 13:58:18 -0500

    bump SCC Operator tag

--- pkg/buildconfig/constants.go
+++ pkg/buildconfig/constants.go
@@ -4,7 +4,7 @@ package buildconfig
 
 const (
 	CspAdapterMinVersion     = "107.0.0+up7.0.0"
-	DefaultSccOperatorImage  = "rancher/scc-operator:v0.3.1"
+	DefaultSccOperatorImage  = "rancher/scc-operator:v0.4.0-rc.0"
 	DefaultShellVersion      = "rancher/shell:v0.5.1"
 	FleetVersion             = "107.0.8+up0.13.8"
 	ProvisioningCAPIVersion  = "107.0.0+up0.8.0"
