41d022385cf0a75429f7be8ad64b9402b3f2998b
Tim Hardeck <thardeck@suse.com>
2026-03-06 15:03:58 +0100

    Allocate AgentSchedulingCustomization only when data is present (#54027)
    * Allocate AgentSchedulingCustomization only when data is present
    
    The struct was always allocated as a non-nil empty pointer, causing
    agentSchedulingCustomization: {} to be written to the Fleet cluster even
    when no scheduling was configured. OpenAPI-aware clients (Rancher UI)
    then rendered the field uncommented, as if it were required.
    
    Declare the variable as a nil pointer and only allocate it when at least
    one sub-field is populated. When SchedulingCustomization is non-nil but
    both PodDisruptionBudget and PriorityClass are nil, a temporary variable
    is used and only assigned when at least one sub-field is set, preventing
    an empty object from being emitted.
    
    Add test coverage for the nil, empty, single-sub-field, and fully
    populated cases.
    
    * Remove tmp variable in schedulingCustomization allocation
    
    Populate schedulingCustomization directly and reset it to nil at the
    end if neither sub-field was set, instead of using a separate tmp
    variable.

--- pkg/controllers/provisioningv2/fleetcluster/fleetcluster.go
+++ pkg/controllers/provisioningv2/fleetcluster/fleetcluster.go
@@ -250,9 +250,10 @@ func (h *handler) createCluster(cluster *provv1.Cluster, status provv1.ClusterSt
 	// sort tolerations for consistent ordering to avoid unnecessary updates
 	sortTolerations(tolerations)
 
-	schedulingCustomization := &fleet.AgentSchedulingCustomization{}
+	var schedulingCustomization *fleet.AgentSchedulingCustomization
 	if cluster.Spec.FleetAgentDeploymentCustomization != nil && cluster.Spec.FleetAgentDeploymentCustomization.SchedulingCustomization != nil {
 		sc := cluster.Spec.FleetAgentDeploymentCustomization.SchedulingCustomization
+		schedulingCustomization = &fleet.AgentSchedulingCustomization{}
 		if sc.PodDisruptionBudget != nil {
 			schedulingCustomization.PodDisruptionBudget = &fleet.PodDisruptionBudgetSpec{}
 			schedulingCustomization.PodDisruptionBudget.MaxUnavailable = sc.PodDisruptionBudget.MaxUnavailable
@@ -262,6 +263,9 @@ func (h *handler) createCluster(cluster *provv1.Cluster, status provv1.ClusterSt
 			schedulingCustomization.PriorityClass = &fleet.PriorityClassSpec{}
 			schedulingCustomization.PriorityClass.PreemptionPolicy = sc.PriorityClass.PreemptionPolicy
 			schedulingCustomization.PriorityClass.Value = sc.PriorityClass.Value
+		}
+		if schedulingCustomization.PodDisruptionBudget == nil && schedulingCustomization.PriorityClass == nil {
+			schedulingCustomization = nil
 		}
 	}
 
