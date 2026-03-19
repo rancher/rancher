5bad95280dbfd076201cac9c0dfa25c46167109c
Samuel Vasconcelos <samuel.vasconcelos@suse.com>
2026-03-19 14:50:59 -0300

    planner: add wildcard support to clusterObjectAuthorized

--- pkg/capr/planner/config.go
+++ pkg/capr/planner/config.go
@@ -609,9 +609,24 @@ func clusterObjectAuthorized(obj runtime.Object, annotation, clusterName string)
 			annotationValueFound = true
 		}
 		for _, authorizedCluster := range authorizedClusters {
+			authorizedCluster = strings.TrimSpace(authorizedCluster)
 			if clusterName == authorizedCluster {
 				return true, annotationValueFound
 			}
+
+			// Global wildcard match ("*")
+			if authorizedCluster == "*" {
+				return true, annotationValueFound
+			}
+
+			// Prefix wildcard match (e.g., "dev-*")
+			if strings.HasSuffix(authorizedCluster, "*") {
+				prefix := strings.TrimSuffix(authorizedCluster, "*")
+				if strings.HasPrefix(clusterName, prefix) {
+					return true, annotationValueFound
+				}
+			}
+
 		}
 	}
 	return false, annotationValueFound
