b64b042c1a3fa2bbdfa171eb76070e1b3605736b
Jonathan Crowther <jonathan.crowther@suse.com>
2026-04-08 10:28:26 -0400

    Migrate impersonation passthrough test

--- tests/v2/integration/rtbs/rtbs_test.go
+++ tests/v2/integration/rtbs/rtbs_test.go
@@ -15,6 +15,7 @@ import (
 	"github.com/rancher/shepherd/clients/rancher"
 	management "github.com/rancher/shepherd/clients/rancher/generated/management/v3"
 	extauthz "github.com/rancher/shepherd/extensions/kubeapi/authorization"
+	extunstructured "github.com/rancher/shepherd/extensions/unstructured"
 	"github.com/rancher/shepherd/extensions/users"
 	password "github.com/rancher/shepherd/extensions/users/passwordgenerator"
 	"github.com/rancher/shepherd/pkg/api/scheme"
@@ -24,6 +25,7 @@ import (
 	"github.com/stretchr/testify/suite"
 	authzv1 "k8s.io/api/authorization/v1"
 	corev1 "k8s.io/api/core/v1"
+	rbacv1 "k8s.io/api/rbac/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	k8stypes "k8s.io/apimachinery/pkg/types"
@@ -811,6 +813,153 @@ func (p *RTBTestSuite) TestUserRolePermissions() {
 	user2RTs, err := user2Client.Management.RoleTemplate.List(nil)
 	require.NoError(p.T(), err)
 	require.Empty(p.T(), user2RTs.Data, "user2 does not have permission to view roleTemplates")
+}
+
+// checkAccessAllowed performs a single SelfSubjectAccessReview and returns whether access is allowed.
+func checkAccessAllowed(client *rancher.Client, clusterID string, attr *authzv1.ResourceAttributes) (bool, error) {
+	dynamicClient, err := client.GetDownStreamClusterClient(clusterID)
+	if err != nil {
+		return false, err
+	}
+
+	ssar := &authzv1.SelfSubjectAccessReview{
+		Spec: authzv1.SelfSubjectAccessReviewSpec{
+			ResourceAttributes: attr,
+		},
+	}
+
+	ssarGVR := authzv1.SchemeGroupVersion.WithResource("selfsubjectaccessreviews")
+	resp, err := dynamicClient.Resource(ssarGVR).Create(context.TODO(), extunstructured.MustToUnstructured(ssar), metav1.CreateOptions{})
+	if err != nil {
+		return false, err
+	}
+
+	result := &authzv1.SelfSubjectAccessReview{}
+	if err := scheme.Scheme.Convert(resp, result, resp.GroupVersionKind()); err != nil {
+		return false, err
+	}
+
+	return result.Status.Allowed, nil
+}
+
+func (p *RTBTestSuite) TestImpersonationPassthrough() {
+	subSession := p.session.NewSession()
+	defer subSession.Cleanup()
+
+	client, err := p.client.WithSession(subSession)
+	require.NoError(p.T(), err)
+
+	enabled := true
+
+	// Create user1 with standard "user" role.
+	user1Pass := password.GenerateUserPassword("testpass-")
+	user1, err := users.CreateUserWithRole(client, &management.User{
+		Username: namegen.AppendRandomString("imp-user1-"),
+		Password: user1Pass,
+		Name:     "imp-user1",
+		Enabled:  &enabled,
+	}, "user")
+	require.NoError(p.T(), err)
+	user1.Password = user1Pass
+
+	// Create user2 with standard "user" role.
+	user2Pass := password.GenerateUserPassword("testpass-")
+	user2, err := users.CreateUserWithRole(client, &management.User{
+		Username: namegen.AppendRandomString("imp-user2-"),
+		Password: user2Pass,
+		Name:     "imp-user2",
+		Enabled:  &enabled,
+	}, "user")
+	require.NoError(p.T(), err)
+	user2.Password = user2Pass
+
+	localCluster, err := client.Management.Cluster.ByID(p.downstreamClusterID)
+	require.NoError(p.T(), err)
+
+	// Give user1 cluster-member and user2 cluster-owner.
+	err = users.AddClusterRoleToUser(client, localCluster, user1, "cluster-member", nil)
+	require.NoError(p.T(), err)
+
+	err = users.AddClusterRoleToUser(client, localCluster, user2, "cluster-owner", nil)
+	require.NoError(p.T(), err)
+
+	user1Client, err := client.AsUser(user1)
+	require.NoError(p.T(), err)
+
+	user2Client, err := client.AsUser(user2)
+	require.NoError(p.T(), err)
+
+	impersonateAttr := &authzv1.ResourceAttributes{
+		Verb:     "impersonate",
+		Resource: "users",
+		Group:    "",
+	}
+
+	// Admin can always impersonate.
+	err = extauthz.WaitForAllowed(client, p.downstreamClusterID, []*authzv1.ResourceAttributes{impersonateAttr})
+	require.NoError(p.T(), err)
+
+	// User1 is a cluster-member which does not grant impersonate.
+	allowed, err := checkAccessAllowed(user1Client, p.downstreamClusterID, impersonateAttr)
+	require.NoError(p.T(), err)
+	require.False(p.T(), allowed, "cluster-member should not be able to impersonate")
+
+	// User2 is a cluster-owner which allows impersonation.
+	err = extauthz.WaitForAllowed(user2Client, p.downstreamClusterID, []*authzv1.ResourceAttributes{impersonateAttr})
+	require.NoError(p.T(), err)
+
+	// Create a ClusterRole allowing limited impersonation of user2 only.
+	dynamicClient, err := client.GetDownStreamClusterClient(p.downstreamClusterID)
+	require.NoError(p.T(), err)
+
+	impRoleName := namegen.AppendRandomString("limited-impersonator-")
+	impRole := &rbacv1.ClusterRole{
+		ObjectMeta: metav1.ObjectMeta{Name: impRoleName},
+		Rules: []rbacv1.PolicyRule{
+			{
+				APIGroups:     []string{""},
+				Resources:     []string{"users"},
+				Verbs:         []string{"impersonate"},
+				ResourceNames: []string{user2.ID},
+			},
+		},
+	}
+
+	crResource := dynamicClient.Resource(extrbac.ClusterRoleGroupVersionResource)
+	_, err = crResource.Create(context.TODO(), extunstructured.MustToUnstructured(impRole), metav1.CreateOptions{})
+	require.NoError(p.T(), err)
+
+	// Create a ClusterRoleBinding binding user1 to the impersonation role.
+	impBindingName := namegen.AppendRandomString("limited-impersonator-binding-")
+	impBinding := &rbacv1.ClusterRoleBinding{
+		ObjectMeta: metav1.ObjectMeta{Name: impBindingName},
+		Subjects: []rbacv1.Subject{
+			{
+				Kind: "User",
+				Name: user1.ID,
+			},
+		},
+		RoleRef: rbacv1.RoleRef{
+			APIGroup: rbacv1.SchemeGroupVersion.Group,
+			Kind:     "ClusterRole",
+			Name:     impRoleName,
+		},
+	}
+
+	crbResource := dynamicClient.Resource(extrbac.ClusterRoleBindingGroupVersionResource)
+	_, err = crbResource.Create(context.TODO(), extunstructured.MustToUnstructured(impBinding), metav1.CreateOptions{})
+	require.NoError(p.T(), err)
+
+	// User1 should now be able to impersonate user2 specifically.
+	err = extauthz.WaitForAllowed(user1Client, p.downstreamClusterID, []*authzv1.ResourceAttributes{
+		{
+			Verb:     "impersonate",
+			Resource: "users",
+			Group:    "",
+			Name:     user2.ID,
+		},
+	})
+	require.NoError(p.T(), err)
 }
 
 func TestRTBTestSuite(t *testing.T) {
