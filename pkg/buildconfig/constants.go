7fe78ebe90fcf84ac139655fa1dd5a3405c3bc97
Felipe Gehrke <felipe.gehrke@suse.com>
2026-04-10 17:48:43 -0300

    Bump webhook, remotedialer-proxy charts

--- pkg/buildconfig/constants.go
+++ pkg/buildconfig/constants.go
@@ -8,7 +8,7 @@ const (
 	DefaultSccOperatorImage       = "rancher/scc-operator:v0.4.0"
 	DefaultShellVersion           = "rancher/shell:v0.7.0"
 	FleetVersion                  = "109.0.1+up0.15.1-beta.2"
-	RemoteDialerProxyVersion      = "109.0.1+up0.7.1"
+	RemoteDialerProxyVersion      = "109.0.1+up0.7.2-rc.1"
 	TurtlesVersion                = "109.0.1+up0.26.1-rc.0"
-	WebhookVersion                = "109.0.1+up0.10.1-rc.1"
+	WebhookVersion                = "109.0.1+up0.10.1-rc.2"
 )
