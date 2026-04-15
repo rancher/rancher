0c54d41634c1e09415aca7fbf6585e96ef4a5bc4
joshmeranda <joshua.meranda@gmail.com>
2026-04-15 11:11:56 -0400

    update steve setting formatter

--- pkg/api/steve/settings/settings.go
+++ pkg/api/steve/settings/settings.go
@@ -1,10 +1,18 @@
 package settings
 
 import (
+	"fmt"
+	"slices"
+
 	"github.com/rancher/apiserver/pkg/types"
+	v3 "github.com/rancher/rancher/pkg/generated/norman/management.cattle.io/v3"
 	schema2 "github.com/rancher/steve/pkg/schema"
 	steve "github.com/rancher/steve/pkg/server"
 )
+
+var ReadOnlySettings = []string{
+	"cacerts",
+}
 
 func Register(server *steve.Server) {
 	server.SchemaFactory.AddTemplate(schema2.Template{
@@ -14,6 +22,14 @@ func Register(server *steve.Server) {
 			data := resource.APIObject.Data()
 			if data.String("value") == "" {
 				data.Set("value", data.String("default"))
+			}
+
+			if data.String("source") == "env" || slices.Contains(ReadOnlySettings, resource.ID) {
+				delete(resource.Links, "update")
+			} else {
+				if err := request.AccessControl.CanDo(request, fmt.Sprintf("%s/%s", v3.SettingGroupVersionResource.Group, v3.SettingResource.Name), "update", resource.APIObject.Namespace(), resource.APIObject.Name()); err != nil {
+					delete(resource.Links, "update")
+				}
 			}
 		},
 	})
