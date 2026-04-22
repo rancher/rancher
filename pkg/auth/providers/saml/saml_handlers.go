28494511d9dc69b0b2150dc7262e64414c1f3731
Kevin McDermott <bigkevmcd@gmail.com>
2026-04-22 13:34:31 +0100

    Replace the current saml provider
    This drops our use of github.com/crewjam/saml (and the fork we made) in
    favour of https://github.com/russellhaering/gosaml2

--- pkg/auth/providers/saml/saml_handlers.go
+++ pkg/auth/providers/saml/saml_handlers.go
@@ -1,14 +1,13 @@
 package saml
 
 import (
+	"crypto/rand"
 	"crypto/x509"
 	"encoding/base64"
 	"encoding/xml"
 	"fmt"
 	"net/http"
-	"strings"
 
-	"github.com/crewjam/saml"
 	"github.com/golang-jwt/jwt/v5"
 	log "github.com/sirupsen/logrus"
 )
@@ -23,8 +22,14 @@ func (s *Provider) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	r.ParseForm()
 
-	if r.URL.Path == serviceProvider.MetadataURL.Path {
-		buf, _ := xml.MarshalIndent(serviceProvider.Metadata(), "", "  ")
+	if r.URL.Path == s.metadataURL.Path {
+		metadata, err := serviceProvider.MetadataWithSLO(0)
+		if err != nil {
+			log.Errorf("SAML [ServeHTTP]: error generating metadata: %v", err)
+			http.Error(w, "internal server error", http.StatusInternalServerError)
+			return
+		}
+		buf, _ := xml.MarshalIndent(metadata, "", "  ")
 		w.Header().Set("Content-Type", "application/samlmetadata+xml")
 		w.Write(buf)
 
@@ -32,22 +37,14 @@ func (s *Provider) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if r.URL.Path == serviceProvider.AcsURL.Path {
+	if r.URL.Path == s.acsURL.Path {
 		log.Debugf("SAML [ServeHTTP]: assertion processing started")
 
 		r.ParseForm()
-		assertion, err := serviceProvider.ParseResponse(r, s.getPossibleRequestIDs(r))
+		assertionInfo, err := serviceProvider.RetrieveAssertionInfo(r.FormValue("SAMLResponse"))
 
 		if err != nil {
 			log.Debugf("SAML [ServeHTTP]: assertion validation failed: %q", err)
-
-			if parseErr, ok := err.(*saml.InvalidResponseError); ok {
-				// Note: If access to the response itself is needed (debugging)
-				// just add `parseErr.Response` to the log statement.
-
-				log.Debugf("SAML NOW: %s\nSAML ERROR: %s",
-					parseErr.Now, parseErr.PrivateErr)
-			}
 
 			redirectURL := r.URL.Host + "/login?errorCode=403"
 			http.Redirect(w, r, redirectURL, http.StatusFound)
@@ -56,13 +53,13 @@ func (s *Provider) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 		log.Debugf("SAML [ServeHTTP]: assertions validated ok")
 
-		s.HandleSamlAssertion(w, r, assertion)
+		s.HandleSamlAssertion(w, r, assertionInfo)
 
 		log.Debugf("SAML [ServeHTTP]: assertion processing completed")
 		return
 	}
 
-	if r.URL.Path == serviceProvider.SloURL.Path {
+	if r.URL.Path == s.sloURL.Path {
 		log.Debugf("SAML [ServeHTTP]: logout response processing started")
 
 		s.FinalizeSamlLogout(w, r)
@@ -76,42 +73,11 @@ func (s *Provider) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	http.NotFoundHandler().ServeHTTP(w, r)
 }
 
-func (s *Provider) getPossibleRequestIDs(r *http.Request) []string {
-	rv := []string{}
-	serviceProvider := s.serviceProvider
-
-	for name, value := range s.clientState.GetStates(r) {
-		if strings.HasPrefix(name, "Rancher_") {
-			continue
-		}
-		jwtParser := newJWTParser()
-		token, err := jwtParser.Parse(value, func(t *jwt.Token) (any, error) {
-			secretBlock := x509.MarshalPKCS1PrivateKey(serviceProvider.Key)
-			return secretBlock, nil
-		})
-		if err != nil || !token.Valid {
-			log.Debugf("... invalid token %s", err)
-			continue
-		}
-		claims := token.Claims.(jwt.MapClaims)
-		rv = append(rv, claims["id"].(string))
-	}
-	return rv
-}
-
 // HandleSamlLogin is the endpoint for /saml/login endpoint
 func (s *Provider) HandleSamlLogin(w http.ResponseWriter, r *http.Request, userID string) (string, error) {
 	serviceProvider := s.serviceProvider
-	if r.URL.Path == serviceProvider.AcsURL.Path {
+	if r.URL.Path == s.acsURL.Path {
 		return "", fmt.Errorf("don't wrap Middleware with RequireAccount")
-	}
-
-	binding := saml.HTTPRedirectBinding
-	bindingLocation := serviceProvider.GetSSOBindingLocation(binding)
-
-	req, err := serviceProvider.MakeAuthenticationRequest(bindingLocation, binding, saml.HTTPPostBinding)
-	if err != nil {
-		return "", err
 	}
 
 	// relayState is limited to 80 bytes but also must be integrity protected.
@@ -119,10 +85,9 @@ func (s *Provider) HandleSamlLogin(w http.ResponseWriter, r *http.Request, userI
 	// we set a cookie that corresponds to the state
 	relayState := base64.URLEncoding.EncodeToString(randomBytes(42))
 
-	secretBlock := x509.MarshalPKCS1PrivateKey(serviceProvider.Key)
+	secretBlock := x509.MarshalPKCS1PrivateKey(s.spKey)
 	state := jwt.New(jwt.SigningMethodHS256)
 	claims := state.Claims.(jwt.MapClaims)
-	claims["id"] = req.ID
 	claims["uri"] = r.URL.String()
 	if userID != "" {
 		claims[rancherUserID] = userID
@@ -136,38 +101,34 @@ func (s *Provider) HandleSamlLogin(w http.ResponseWriter, r *http.Request, userI
 
 	s.clientState.SetState(w, r, relayState, signedState)
 
-	redirectURL, err := req.Redirect(relayState, serviceProvider)
+	// Use redirect binding: signature goes in the URL query string.
+	doc, err := serviceProvider.BuildAuthRequestDocumentNoSig()
+	if err != nil {
+		return "", err
+	}
+	redirectURL, err := serviceProvider.BuildAuthURLRedirect(relayState, doc)
 	if err != nil {
 		return "", err
 	}
 
-	return redirectURL.String(), nil
+	return redirectURL, nil
 }
 
 // HandleSamlLogout is the final handler for the logoutAll action
 func (s *Provider) HandleSamlLogout(name string, w http.ResponseWriter, r *http.Request) (string, error) {
 	serviceProvider := s.serviceProvider
-	if r.URL.Path == serviceProvider.AcsURL.Path {
+	if r.URL.Path == s.acsURL.Path {
 		return "", fmt.Errorf("don't wrap Middleware with RequireAccount")
 	}
 
-	binding := saml.HTTPRedirectBinding
-	bindingLocation := serviceProvider.GetSLOBindingLocation(binding)
-
-	req, err := serviceProvider.MakeLogoutRequest(bindingLocation, name)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return "", err
-	}
 	// relayState is limited to 80 bytes but also must be integrity protected.
 	// this means that we cannot use a JWT because it is way too long. Instead
 	// we set a cookie that corresponds to the state
 	relayState := base64.URLEncoding.EncodeToString(randomBytes(42))
 
-	secretBlock := x509.MarshalPKCS1PrivateKey(serviceProvider.Key)
+	secretBlock := x509.MarshalPKCS1PrivateKey(s.spKey)
 	state := jwt.New(jwt.SigningMethodHS256)
 	claims := state.Claims.(jwt.MapClaims)
-	claims["id"] = req.ID
 	claims["uri"] = r.URL.String()
 
 	signedState, err := state.SignedString(secretBlock)
@@ -178,17 +139,24 @@ func (s *Provider) HandleSamlLogout(name string, w http.ResponseWriter, r *http.
 
 	s.clientState.SetState(w, r, relayState, signedState)
 
-	redirectURL, err := req.Redirect(relayState, serviceProvider)
+	// name is the NameID of the user at the IdP. Session index is not tracked
+	// so an empty string is passed.
+	doc, err := serviceProvider.BuildLogoutRequestDocumentNoSig(name, "")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return "", err
+	}
+	redirectURL, err := serviceProvider.BuildLogoutURLRedirect(relayState, doc)
 	if err != nil {
 		return "", err
 	}
 
-	return redirectURL.String(), nil
+	return redirectURL, nil
 }
 
 func randomBytes(n int) []byte {
 	rv := make([]byte, n)
-	if _, err := saml.RandReader.Read(rv); err != nil {
+	if _, err := rand.Read(rv); err != nil {
 		panic(err)
 	}
 	return rv
