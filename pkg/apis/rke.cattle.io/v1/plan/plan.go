e6554259940c0aa1719101c891c561709cdf655d
Furkat Gofurov <furkat.gofurov@suse.com>
2026-04-30 01:49:38 +0300

    pkg/plan: add `Dynamic`, `Minor`, `DrainHashFunc` fields to `File` and alias CAPR `File` type
    Signed-off-by: Furkat Gofurov <furkat.gofurov@suse.com>

--- pkg/apis/rke.cattle.io/v1/plan/plan.go
+++ pkg/apis/rke.cattle.io/v1/plan/plan.go
@@ -1,8 +1,6 @@
 package plan
 
 import (
-	"hash"
-
 	planapi "github.com/rancher/rancher/pkg/plan"
 	capi "sigs.k8s.io/cluster-api/api/core/v1beta2"
 )
@@ -11,6 +9,7 @@ type OneTimeInstruction = planapi.OneTimeInstruction
 type PeriodicInstruction = planapi.PeriodicInstruction
 type PeriodicInstructionOutput = planapi.PeriodicInstructionOutput
 type ProbeStatus = planapi.ProbeStatus
+type File = planapi.File
 
 type Plan struct {
 	Nodes    map[string]*Node         `json:"nodes,omitempty"`
@@ -40,24 +39,6 @@ type Node struct {
 type Secret struct {
 	ServerToken string `json:"serverToken,omitempty"`
 	AgentToken  string `json:"agentToken,omitempty"`
-}
-
-type File struct {
-	Content     string `json:"content,omitempty"`
-	Path        string `json:"path,omitempty"`
-	Permissions string `json:"permissions,omitempty"`
-	Dynamic     bool   `json:"dynamic,omitempty"`
-	// Minor signifies that the file can be changed on a node without having to cause a full-blown drain/cordon operation
-	Minor bool `json:"minor,omitempty"`
-
-	// DrainHashFunc specifies a custom hash function to be used by the planner when this file is processed while
-	// updating the plans DRAIN_HASH value, as opposed to the default behavior of directly hashing the contents of the
-	// File.
-	// This allows the planner to better determine when a given File operation should drain or restart a node (or both).
-	// This can be useful when changing certain properties of the rke2/k3s ConfigYamlFileName file, such as the "server"
-	// arg, which should restart the distribution but not trigger a drain (so long as that was the only change within
-	// the File).
-	DrainHashFunc func(hash.Hash) error `json:"-"`
 }
 
 // NodePlan is the struct used to deliver instructions/files/probes to the system-agent, and retrieve feedback
