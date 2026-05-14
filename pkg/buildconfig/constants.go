4dce3906c0b7b09e0c3d540e6b67ca8d2eb91bde
Chad Roberts <chad.roberts@suse.com>
2026-05-14 13:51:35 -0400

    Bump remotedialer-proxy to 110.0.0+up0.8.0-rc.5
    Signed-off-by: Chad Roberts <chad.roberts@suse.com>

--- pkg/buildconfig/constants.go
+++ pkg/buildconfig/constants.go
@@ -7,7 +7,7 @@ const (
 	DefaultSccOperatorImage  = "rancher/scc-operator:v0.4.0-rc.1"
 	DefaultShellVersion      = "rancher/shell:v0.8.0-rc.2"
 	FleetVersion             = "110.0.0+up0.16.0-alpha.5"
-	RemoteDialerProxyVersion = "109.0.0+up0.7.0"
+	RemoteDialerProxyVersion = "110.0.0+up0.8.0-rc.5"
 	TurtlesVersion           = "109.0.1+up0.26.1"
 	WebhookVersion           = "110.0.0+up0.11.0-rc.2"
 )
