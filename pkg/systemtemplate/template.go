33b194497ffe497e180c4b6b30671f271f368ab6
Kinara Shah <kinara.shah@suse.com>
2026-05-19 15:45:38 -0700

    validate auth image (#55155)

--- pkg/systemtemplate/template.go
+++ pkg/systemtemplate/template.go
@@ -201,7 +201,7 @@ spec:
       {{- if .AgentEnvVars}}
 {{ .AgentEnvVars | indent 10 }}
       {{- end }}
-          image: {{.AgentImage}}
+          image: "{{.AgentImage}}"
           volumeMounts:
           - name: cattle-credentials
             mountPath: /cattle-credentials
@@ -477,7 +477,7 @@ spec:
       - operator: Exists
       containers:
       - name: kube-api-auth
-        image: {{.AuthImage}}
+        image: "{{.AuthImage}}"
         imagePullPolicy: IfNotPresent
         volumeMounts:
         - name: k8s-ssl
