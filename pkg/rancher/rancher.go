db02761bc83143043fd84e6c944d25bed9585522
Kevin McDermott <kevin.mcdermott@suse.com>
2026-05-19 13:52:27 +0100

    Ensure local-user-passwords are stored without MCM (#55136)
    This changes the logic for local-user-passwords to only create them if explicitly configured to do so, and the main rancher binary enables this.

--- pkg/rancher/rancher.go
+++ pkg/rancher/rancher.go
@@ -110,6 +110,10 @@ type Options struct {
 	ClusterRegistry                string
 	AggregationRegistrationTimeout time.Duration
 	RancherNamespaceOptions        string
+
+	// LocalUserPasswordsNamespace should be set to true if the namespace for
+	// storing user passwords should be created.
+	LocalUserPasswordsNamespace bool
 }
 
 type Rancher struct {
@@ -443,7 +447,7 @@ func getSQLCacheGCValues(wranglerContext *wrangler.Context) (time.Duration, int)
 }
 
 func (r *Rancher) Start(ctx context.Context) error {
-	if features.MCM.Enabled() {
+	if r.opts.LocalUserPasswordsNamespace {
 		// ensure namespace for storing local users password is created
 		if _, err := r.Wrangler.Core.Namespace().Create(&v1.Namespace{
 			ObjectMeta: metav1.ObjectMeta{Name: pbkdf2.LocalUserPasswordsNamespace},
