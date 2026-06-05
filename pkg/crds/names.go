fdac1849a1ce2819679106eeda36aafa4e38a35d
Jake Hyde <jakefhyde@gmail.com>
2026-06-05 18:31:45 -0400

    Add etcdsnapshotrestores to pkg/crds/names.go

--- pkg/crds/names.go
+++ pkg/crds/names.go
@@ -246,6 +246,7 @@ var MigratedResources = map[string]bool{
 	"dynamicschemas.management.cattle.io":                             true,
 	"etcdsnapshots.rke.cattle.io":                                     true,
 	"etcdsnapshotsaves.operation.cattle.io":                           true,
+	"etcdsnapshotrestores.operation.cattle.io":                        true,
 	"extensionconfigs.runtime.cluster.x-k8s.io":                       false,
 	"features.management.cattle.io":                                   false,
 	"fleetworkspaces.management.cattle.io":                            false,
