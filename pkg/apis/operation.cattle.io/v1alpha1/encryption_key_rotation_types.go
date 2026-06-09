de88808be93245b7b7e46edc1be096e8a597e9ac
jferrazbr <joao.ferraz@suse.com>
2026-06-09 10:43:11 -0300

    add controller flow

--- pkg/apis/operation.cattle.io/v1alpha1/encryption_key_rotation_types.go
+++ pkg/apis/operation.cattle.io/v1alpha1/encryption_key_rotation_types.go
@@ -15,7 +15,7 @@ type EncryptionKeyRotationStep string
 
 const (
 	// EncryptionKeyRotationStepRotate indicates the step is to rotate the encryption keys
-	// by running the secrets-encrypt rotate-keys command on all control plane nodes.
+	// by running the secrets-encrypt rotate-keys command on the elected control-plane leader.
 	EncryptionKeyRotationStepRotate EncryptionKeyRotationStep = "Rotate"
 
 	// EncryptionKeyRotationStepRestart indicates the step is to restart the distro server
@@ -48,7 +48,7 @@ type EncryptionKeyRotationStatus struct {
 // +kubebuilder:printcolumn:name="Age",type=date,JSONPath=".metadata.creationTimestamp"
 
 // EncryptionKeyRotation is the mechanism for initiating an encryption key rotation
-// operation for imported RKE2 or K3s clusters.
+// operation for provisioned or imported RKE2/K3s clusters.
 type EncryptionKeyRotation struct {
 	metav1.TypeMeta `json:",inline"`
 	// metadata is the standard object's metadata.
