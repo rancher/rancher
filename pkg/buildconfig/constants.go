3e5c93e41389e0f7f92987a6899a27fb414f71f8
furkatgofurov7 <40443040+furkatgofurov7@users.noreply.github.com>
2026-06-10 09:36:07 +0000

    Bump turtles to 108.0.5+up0.25.5-rc.0 (chart version patch bump)

--- pkg/buildconfig/constants.go
+++ pkg/buildconfig/constants.go
@@ -10,6 +10,6 @@ const (
 	FleetVersion                  = "108.0.6+up0.14.6"
 	ProvisioningCAPIVersion       = "108.0.0+up0.9.0"
 	RemoteDialerProxyVersion      = "108.0.2+up0.6.2-rc.2"
-	TurtlesVersion                = "108.0.4+up0.25.4"
+	TurtlesVersion                = "108.0.5+up0.25.5-rc.0"
 	WebhookVersion                = "108.0.6+up0.9.6-rc.3"
 )
