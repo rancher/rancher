3c40de02184ba3c74e65cc94b9c2904261be3ae8
release-automation <release-automation@users.noreply.github.com>
2026-06-12 09:58:02 +0000

    Bump github.com/rancher/remotedialer-proxy to v0.7.4-rc.3

--- pkg/buildconfig/constants.go
+++ pkg/buildconfig/constants.go
@@ -7,7 +7,7 @@ const (
 	DefaultSccOperatorImage  = "rancher/scc-operator:v0.4.1"
 	DefaultShellVersion      = "rancher/shell:v0.7.0"
 	FleetVersion             = "109.0.3+up0.15.3-rc.1"
-	RemoteDialerProxyVersion = "109.0.4+up0.7.4-rc.2"
+	RemoteDialerProxyVersion = "109.0.4+up0.7.4-rc.3"
 	TurtlesVersion           = "109.0.3+up0.26.3-rc.0"
 	WebhookVersion           = "109.0.3+up0.10.7-rc.5"
 )
