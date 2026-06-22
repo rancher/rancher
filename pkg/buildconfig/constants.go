9616d6d3263a5a34992de9dfb9953ffbdb04c044
Dan Pock <self@danpock.me>
2026-06-22 16:29:21 -0400

    bump ORBS artifacts

--- pkg/buildconfig/constants.go
+++ pkg/buildconfig/constants.go
@@ -5,8 +5,8 @@ package buildconfig
 const (
 	ClusterAutoscalerChartVersion = "9.50.1"
 	CspAdapterMinVersion          = "108.0.0+up8.0.0"
-	DefaultSccOperatorImage       = "rancher/scc-operator:v0.4.2-rc.1"
-	DefaultShellVersion           = "rancher/shell:v0.6.4-rc.1"
+	DefaultSccOperatorImage       = "rancher/scc-operator:v0.4.2"
+	DefaultShellVersion           = "rancher/shell:v0.6.4"
 	FleetVersion                  = "108.0.7+up0.14.7-rc.2"
 	ProvisioningCAPIVersion       = "108.0.0+up0.9.0"
 	RemoteDialerProxyVersion      = "108.0.2+up0.6.2-rc.3"
