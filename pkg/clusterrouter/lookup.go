3dfe278d40665d8b1a81fe9bd8f55e7114040e1c
Kevin McDermott <bigkevmcd@gmail.com>
2026-06-23 16:44:08 +0100

    Restrict cluster-impersonation.
    This drops support for impersonation headers when making requests to the
    cluster.
    
    Verification of permissions is now only done via the management cluster.

--- pkg/clusterrouter/lookup.go
+++ pkg/clusterrouter/lookup.go
@@ -4,13 +4,14 @@ import (
 	"net/http"
 	"strings"
 
-	v3 "github.com/rancher/rancher/pkg/generated/norman/management.cattle.io/v3"
+	v3 "github.com/rancher/rancher/pkg/apis/management.cattle.io/v3"
 )
 
 type ClusterLookup interface {
 	Lookup(req *http.Request) (*v3.Cluster, error)
 }
 
+// GetClusterID extracts the cluster ID from the request URL path.
 func GetClusterID(req *http.Request) string {
 	parts := strings.Split(req.URL.Path, "/")
 	if len(parts) > 3 &&
