76282f0679968a5a55737337e54652e70b101f99
Andreas Kupries <andreas.kupries@suse.com>
2026-07-02 13:56:42 +0200

    switch import yaml action over to an ext token

--- pkg/api/norman/customization/cluster/actionhandler.go
+++ pkg/api/norman/customization/cluster/actionhandler.go
@@ -6,7 +6,9 @@ import (
 
 	"github.com/rancher/norman/httperror"
 	"github.com/rancher/norman/types"
+	ext "github.com/rancher/rancher/pkg/apis/ext.cattle.io/v1"
 	v32 "github.com/rancher/rancher/pkg/apis/management.cattle.io/v3"
+	"github.com/rancher/rancher/pkg/auth/providers/common"
 	"github.com/rancher/rancher/pkg/auth/requests"
 	"github.com/rancher/rancher/pkg/auth/tokens"
 	mgmtclient "github.com/rancher/rancher/pkg/client/generated/management/v3"
@@ -14,7 +16,9 @@ import (
 	exttokenstore "github.com/rancher/rancher/pkg/ext/stores/tokens"
 	v3 "github.com/rancher/rancher/pkg/generated/norman/management.cattle.io/v3"
 	"github.com/rancher/rancher/pkg/user"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apiserver/pkg/endpoints/request"
 	clientcmdapi "k8s.io/client-go/tools/clientcmd/api"
 )
 
@@ -106,10 +110,71 @@ func (a ActionHandler) createTokenInput(apiContext *types.APIContext) (user.Toke
 }
 
 func (a ActionHandler) generateKubeConfig(apiContext *types.APIContext, cluster *mgmtclient.Cluster) (*clientcmdapi.Config, error) {
-	token, err := a.ensureToken(apiContext)
+	authToken, err := a.AuthToken.TokenFromRequest(apiContext.Request)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get session token: %w", err)
+	}
+
+	defaultTokenTTL, err := tokens.GetKubeconfigDefaultTokenTTLInMilliSeconds()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get default token TTL: %w", err)
+	}
+
+	userID := authToken.GetUserID()
+	tokenName := authToken.GetName()
+
+	// Create a proper ext token, commit it to kubernetes, and pass the
+	// resulting bearer token on.
+	//
+	// The ability to set a custom generateName prefix (kubeconfig-<user>-...)
+	// is lost for ext tokens. The ext token store internally forces the
+	// prefix `token-`.
+
+	kcToken := &ext.Token{
+		Spec: ext.TokenSpec{
+			UserID:      userID,
+			Kind:        "kubeconfig",
+			Description: "Kubeconfig token",
+			TTL:         *defaultTokenTTL,
+		},
+	}
+
+	// Note that `userinfo == nil` is ok. The argument is only used when the
+	// token refers to a cluster by name, requiring a permission check. This
+	// is not the case here, we are asking for an unscoped token.
+	kcNewToken, err := a.ExtTokenStore.Create(
+		// Make the auth token available to `Create`, via reference by
+		// name, through a local [user.Info] interface implementation.
+		// This enables `Create` to retrieve it and the principal
+		// information it needs.
+		request.WithUser(apiContext.Request.Context(), &actionUserInfo{
+			authTokenName: tokenName,
+		}),
+		exttokenstore.GVR.GroupResource(),
+		kcToken,
+		&metav1.CreateOptions{},
+		nil)
 	if err != nil {
 		return nil, err
 	}
 
-	return a.ClusterManager.KubeConfig(cluster.ID, token), nil
+	return a.ClusterManager.KubeConfig(cluster.ID, kcNewToken.Status.BearerToken), nil
+}
+
+// actionUserInfo implements [user.Info] to pass the session token by name into
+// the ext token system store.
+type actionUserInfo struct {
+	authTokenName string
+}
+
+// implements [user.Info]
+func (a *actionUserInfo) GetName() string     { return "" }
+func (a *actionUserInfo) GetUID() string      { return "" }
+func (a *actionUserInfo) GetGroups() []string { return []string{} }
+func (a *actionUserInfo) GetExtra() map[string][]string {
+	return map[string][]string{
+		common.ExtraRequestTokenID: []string{
+			a.authTokenName,
+		},
+	}
 }
