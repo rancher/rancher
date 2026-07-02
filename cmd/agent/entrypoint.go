c1be53184566571330eb8753cd5097a02408b945
Alejandro Ruiz <alejandro.ruiz@suse.com>
2026-07-02 16:21:25 +0200

    Workaround special modes of operation

--- cmd/agent/entrypoint.go
+++ cmd/agent/entrypoint.go
@@ -41,10 +41,6 @@ const (
 // These actions were previously executed as part of the agent image's entrypoint script, before actually executing the agent binary
 // The logic has been migrated from that shell script to native Go
 func preStart(ctx context.Context) error {
-	if os.Getenv("CATTLE_ENTRYPOINT_BYPASS") == "true" {
-		return nil
-	}
-
 	cattleServer := os.Getenv("CATTLE_SERVER")
 	if err := pingCattleServer(ctx, cattleServer); err != nil {
 		return fmt.Errorf("error pinging %s: %w", cattleServer, err)
