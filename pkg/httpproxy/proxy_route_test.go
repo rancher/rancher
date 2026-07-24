a5b81634940924b5efe8ca7b9d8bc72da5ba286a
Eric Promislow <epromislow@suse.com>
2026-07-24 15:14:38 -0700

    WIP - change #2

--- pkg/httpproxy/proxy_route_test.go
+++ pkg/httpproxy/proxy_route_test.go
@@ -297,7 +297,7 @@ func TestPerRouteTLSTransport_SelfSignedRoute_InsecureSkipTLSVerifyTrue_Succeeds
 
 	p := &proxy{
 		proxyEndpointCache: cache,
-		insecureTransport:  &http.Transport{TLSClientConfig: &tls.Config{InsecureSkipVerify: true}},
+		insecureTransport: &http.Transport{TLSClientConfig: &tls.Config{InsecureSkipVerify: true}},
 	}
 	transport := &perRouteTLSTransport{proxy: p}
 
