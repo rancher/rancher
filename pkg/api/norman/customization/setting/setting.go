6a88ee09b497a2aaeff4d8175e95bdb82c3e3ef3
Kinara Shah <kinara.shah@suse.com>
2026-08-05 09:04:12 -0700

     Refactor CRT TTL logic

--- pkg/api/norman/customization/setting/setting.go
+++ pkg/api/norman/customization/setting/setting.go
@@ -2,7 +2,6 @@ package setting
 
 import (
 	"fmt"
-	"strconv"
 
 	"github.com/rancher/norman/api/access"
 	"github.com/rancher/norman/httperror"
@@ -12,12 +11,6 @@ import (
 	"github.com/rancher/rancher/pkg/auth/providerrefresh"
 	v3client "github.com/rancher/rancher/pkg/client/generated/management/v3"
 	v3 "github.com/rancher/rancher/pkg/generated/norman/management.cattle.io/v3"
-	"github.com/rancher/rancher/pkg/settings"
-)
-
-const (
-	minTTLMinutes         = 30 // 30 minutes
-	minGracePeriodMinutes = 10 // 10 minutes
 )
 
 var ReadOnlySettings = []string{
@@ -74,48 +67,10 @@ func Validator(request *types.APIContext, schema *types.Schema, data map[string]
 		_, err = providerrefresh.ParseMaxAge(newValueString)
 	case "auth-user-info-resync-cron":
 		_, err = providerrefresh.ParseCron(newValueString)
-	case "crt-default-ttl-minutes":
-		err = validateCRTTTL(request, newValueString)
-	case "crt-default-grace-period-minutes":
-		err = validateCRTGracePeriod(request, newValueString)
 	}
 
 	if err != nil {
 		return httperror.NewAPIError(httperror.InvalidBodyContent, fmt.Sprintf("%v", err))
-	}
-
-	return nil
-}
-
-func validateCRTTTL(request *types.APIContext, value string) error {
-	ttl, err := strconv.Atoi(value)
-	if err != nil {
-		return fmt.Errorf("crt-default-ttl-minutes must be a valid integer: %v", err)
-	}
-	if ttl < minTTLMinutes {
-		return fmt.Errorf("crt-default-ttl-minutes must be >= %d", minTTLMinutes)
-	}
-	// validate TTL > grace period
-	gp := settings.CRTDefaultGracePeriod.GetInt()
-	if ttl <= gp {
-		return fmt.Errorf("crt-default-ttl-minutes (%d) must be greater than crt-default-grace-period-minutes (%d)", ttl, gp)
-	}
-
-	return nil
-}
-
-func validateCRTGracePeriod(request *types.APIContext, value string) error {
-	gp, err := strconv.Atoi(value)
-	if err != nil {
-		return fmt.Errorf("crt-default-grace-period-minutes must be a valid integer: %v", err)
-	}
-	if gp < minGracePeriodMinutes {
-		return fmt.Errorf("crt-default-grace-period-minutes must be >= %d", minGracePeriodMinutes)
-	}
-	// validate TTL > grace period
-	ttl := settings.CRTDefaultTTL.GetInt()
-	if ttl <= gp {
-		return fmt.Errorf("crt-default-grace-period-minutes (%d) must be less than crt-default-ttl-minutes (%d)", gp, ttl)
 	}
 
 	return nil
