289646cc18588696ef92d27d6fccd9c07c2b51a4
Rohit <rohit.sakala@suse.com>
2026-08-06 10:53:56 -0700

    Remove the ui-sql-cache feature flag and treat the SQL cache as always enabled.

--- pkg/systemtemplate/import.go
+++ pkg/systemtemplate/import.go
@@ -341,7 +341,6 @@ func GetDesiredFeatures(cluster *apimgmtv3.Cluster) map[string]bool {
 		features.RKE2.Name():                           false,
 		features.ProvisioningV2.Name():                 false,
 		features.Turtles.Name():                        false,
-		features.UISQLCache.Name():                     features.UISQLCache.Enabled(),
 		features.ProvisioningPreBootstrap.Name():       capr.PreBootstrap(cluster),
 		features.ManagedSystemUpgradeController.Name(): enableMSUC,
 	}
