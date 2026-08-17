26dcdc7c0c5ec74d765373b2c67046d4eb6a3bde
Jonathan Crowther <jonathan.crowther@suse.com>
2026-08-17 09:19:58 -0400

    Add the APIGroup to the CRB RoleRef

--- pkg/controllers/management/auth/globalroles/globalrolebinding_handler.go
+++ pkg/controllers/management/auth/globalroles/globalrolebinding_handler.go
@@ -406,8 +406,9 @@ func (l *globalRoleBindingLifecycle) reconcileGlobalRoleBinding(globalRoleBindin
 		},
 		Subjects: []rbacv1.Subject{rbac.GetGRBSubject(globalRoleBinding)},
 		RoleRef: rbacv1.RoleRef{
-			Name: getCRName(globalRoleBinding.GlobalRoleName),
-			Kind: clusterRoleKind,
+			APIGroup: rbacv1.GroupName,
+			Name:     getCRName(globalRoleBinding.GlobalRoleName),
+			Kind:     clusterRoleKind,
 		},
 	}
 
