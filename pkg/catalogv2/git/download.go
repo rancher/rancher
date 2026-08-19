7db5a7af9e0e4b3b1a824f78c1b86915bfffa8d9
Dan P. <dan.pock@suse.com>
2026-08-19 14:32:02 -0400

    ClusterRepos fix for bundled charts (#56782)
    * Add new bundled git tests and update existing ones
    
    * Ensure HEAD is used for bundling during upgrades

--- pkg/catalogv2/git/download.go
+++ pkg/catalogv2/git/download.go
@@ -21,6 +21,10 @@ func Ensure(secret *corev1.Secret, namespace, name, gitURL, commit string, insec
 		if err := git.reset("HEAD"); err != nil {
 			return fmt.Errorf("ensure failure: %w", err)
 		}
+		// Always use HEAD in bundled mode. During upgrades, ClusterRepo.Status.Commit may reference
+		// an old commit. Without this return, attempting to reset to that old commit could revert
+		// bundled charts to an older version, removing new charts needed by the upgraded Rancher.
+		return nil
 	}
 
 	if err := git.clone(""); err != nil {
