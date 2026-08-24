82d3392f593c0b81177e6aa399588ec62cc85952
salasberryfin <10220061+salasberryfin@users.noreply.github.com>
2026-08-24 11:24:29 +0000

    Bump turtles to 110.0.1+up0.27.1 (no chart version bump)

--- pkg/buildconfig/constants.go
+++ pkg/buildconfig/constants.go
@@ -9,6 +9,6 @@ const (
 	DefaultShellVersion      = "rancher/shell:v0.8.1"
 	FleetVersion             = "110.0.1+up0.16.1"
 	RemoteDialerProxyVersion = "110.0.1+up0.8.1-rc.2"
-	TurtlesVersion           = "110.0.1+up0.27.1-rc.1"
+	TurtlesVersion           = "110.0.1+up0.27.1"
 	WebhookVersion           = "110.0.2+up0.11.1-rc.5"
 )
