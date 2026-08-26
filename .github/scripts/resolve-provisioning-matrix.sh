0161d53facc9d060fb2c0008f07d7e803decb2f1
Harrison Affel <harrisonaffel@gmail.com>
2026-08-26 14:50:53 -0400

    support test parallelism in provisioning test scopes file

--- .github/scripts/resolve-provisioning-matrix.sh
+++ .github/scripts/resolve-provisioning-matrix.sh
@@ -204,6 +204,7 @@ yq -o=json '.' "$CONFIG" | jq -c --arg selected "$selected" '
       | { V2PROV_TEST_DIST: .dist, V2PROV_TEST_RUN_REGEX: .regex }
         + (if .features then { CATTLE_FEATURES: .features } else {} end)
         + (if .cpus then { V2PROV_TEST_CPUS: .cpus } else { V2PROV_TEST_CPUS: 16 } end)
+        + (if .parallelism then { V2PROV_TEST_PARALLELISM: .parallelism } else { V2PROV_TEST_PARALLELISM: 1 } end)
     ]
   }
 '
