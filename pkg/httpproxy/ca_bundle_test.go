791815a121ba0efa1fce6af81e41fc3192c838d8
Eric Promislow <epromislow@suse.com>
2026-09-01 15:13:24 -0700

    Fix test expected error messages

--- pkg/httpproxy/ca_bundle_test.go
+++ pkg/httpproxy/ca_bundle_test.go
@@ -14,14 +14,14 @@ This is not valid base64 content at all!!!
 func TestParseCACertificates_WithInvalidPEM_ReturnsError(t *testing.T) {
 	pool, err := parseCACertificates(testInvalidPEM)
 	require.Error(t, err)
-	assert.Contains(t, err.Error(), "failed to append CA certificates")
+	assert.Contains(t, err.Error(), "failed to parse CA bundle PEM data")
 	assert.Nil(t, pool)
 }
 
 func TestParseCACertificates_WithEmptyString_ReturnsError(t *testing.T) {
 	pool, err := parseCACertificates("")
 	require.Error(t, err)
-	assert.Contains(t, err.Error(), "failed to append CA certificates")
+	assert.Contains(t, err.Error(), "CA bundle must contain at least one CERTIFICATE PEM block")
 	assert.Nil(t, pool)
 }
 
