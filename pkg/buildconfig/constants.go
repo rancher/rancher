fda3a7fc85edf7404249bc8c137f00f6b8bb72af
release-automation <release-automation@users.noreply.github.com>
2026-09-04 15:01:29 +0000

    Bump github.com/rancher/webhook to v0.11.2-rc.1

--- pkg/buildconfig/constants.go
+++ pkg/buildconfig/constants.go
@@ -10,5 +10,5 @@ const (
 	FleetVersion             = "110.0.1+up0.16.1"
 	RemoteDialerProxyVersion = "110.0.1+up0.8.1"
 	TurtlesVersion           = "110.0.2+up0.27.2-rc.0"
-	WebhookVersion           = "110.0.2+up0.11.1"
+	WebhookVersion           = "110.0.3+up0.11.2-rc.1"
 )
