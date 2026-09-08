555bdcd0b913df56e3ff9a919a27208b0c0eed7d
Peter Matseykanets <peter.matseykanets@suse.com>
2026-09-08 11:46:28 -0400

    [v2.13] Bump kube-api-auth to v0.13.2 (#57101)

--- pkg/apis/management.cattle.io/v3/tools_system_images.go
+++ pkg/apis/management.cattle.io/v3/tools_system_images.go
@@ -5,7 +5,7 @@ var (
 		AuthSystemImages AuthSystemImages
 	}{
 		AuthSystemImages: AuthSystemImages{
-			KubeAPIAuth: "rancher/kube-api-auth:v0.13.1",
+			KubeAPIAuth: "rancher/kube-api-auth:v0.13.2",
 		},
 	}
 )
