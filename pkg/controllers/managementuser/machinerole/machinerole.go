f29c47b56358cc85f511c417cb1108c1d082bc5c
Samuel Vasconcelos <samuel.vasconcelos@suse.com>
2026-09-15 15:05:30 -0300

    implement automatic node taint and worker label management

--- pkg/controllers/managementuser/machinerole/machinerole.go
+++ pkg/controllers/managementuser/machinerole/machinerole.go
@@ -24,7 +24,6 @@ import (
 type handler struct {
 	clusterName      string
 	nodes            corecontrollers.NodeClient
-	secretCache      corecontrollers.SecretCache
 	clusterCache     mgmtcontrollers.ClusterCache
 	mgmtNodeCache    mgmtcontrollers.NodeCache
 	capiClusterCache capicontrollers.ClusterCache
@@ -35,13 +34,11 @@ func Register(ctx context.Context, userCtx *config.UserContext, capiCtx *wrangle
 	h := handler{
 		clusterName:      userCtx.ClusterName,
 		nodes:            userCtx.Corew.Node(),
-		secretCache:      userCtx.Management.Wrangler.Core.Secret().Cache(),
 		clusterCache:     userCtx.Management.Wrangler.Mgmt.Cluster().Cache(),
 		mgmtNodeCache:    userCtx.Management.Wrangler.Mgmt.Node().Cache(),
 		capiClusterCache: capiCtx.CAPI.Cluster().Cache(),
 		capiMachineCache: capiCtx.CAPI.Machine().Cache(),
 	}
-	userCtx.Corew.Node().OnChange(ctx, "machine-worker-label", h.WorkerLabelSync)
 	userCtx.Corew.Node().OnChange(ctx, "machine-lifecycle-label", h.ImportedLabelSync)
 }
 
@@ -147,32 +144,4 @@ func (h *handler) lifecycleObjectsForNode(mgmtCluster *apimgmtv3.Cluster, node *
 		return nil, nil, nil
 	}
 	return mgmtCluster, mgmtNodes[0], nil
-}
-
-func (h *handler) WorkerLabelSync(_ string, node *corev1.Node) (*corev1.Node, error) {
-	if node == nil || node.DeletionTimestamp != nil || node.Labels == nil || node.Annotations == nil {
-		return node, nil
-	}
-
-	if _, ok := node.Labels["node-role.kubernetes.io/worker"]; ok {
-		return node, nil
-	}
-	machineName := node.Annotations[capi.MachineAnnotation]
-	if machineName == "" {
-		return node, nil
-	}
-	machineNS := node.Annotations[capi.ClusterNamespaceAnnotation]
-	if machineNS == "" {
-		return node, nil
-	}
-
-	secrets, err := h.secretCache.List(machineNS, labels.SelectorFromSet(labels.Set{capr.MachineNameLabel: machineName, capr.WorkerRoleLabel: "true"}))
-	if err != nil || len(secrets) == 0 {
-		return node, err
-	}
-
-	node = node.DeepCopy()
-	node.Labels["node-role.kubernetes.io/worker"] = "true"
-
-	return h.nodes.Update(node)
 }
