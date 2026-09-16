1cf1ef3bf6932729150276c64c7c6136c82158b7
Alejandro Moreno <alejandro.moreno@suse.com>
2026-09-16 17:01:56 +0200

    bump shell to v0.6.5-rc.2

--- pkg/buildconfig/constants.go
+++ pkg/buildconfig/constants.go
@@ -7,7 +7,7 @@ const (
 	ClusterAutoscalerChartVersion = "9.50.1"
 	CspAdapterMinVersion          = "108.0.0+up8.0.0"
 	DefaultSccOperatorImage       = "rancher/scc-operator:v0.5.2-rc.1"
-	DefaultShellVersion           = "rancher/shell:v0.6.4"
+	DefaultShellVersion           = "rancher/shell:v0.6.5-rc.2"
 	FleetVersion                  = "108.0.11+up0.14.11-rc.2"
 	ProvisioningCAPIVersion       = "108.0.0+up0.9.0"
 	RemoteDialerProxyVersion      = "108.0.4+up0.6.4-rc.2"
