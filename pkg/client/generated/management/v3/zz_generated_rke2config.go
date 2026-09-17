f47c90fd0a20444dcb2b502073cddf294fee209e
Jake Hyde <jakefhyde@gmail.com>
2026-09-17 14:53:37 -0400

    go generate

--- pkg/client/generated/management/v3/zz_generated_rke2config.go
+++ pkg/client/generated/management/v3/zz_generated_rke2config.go
@@ -3,10 +3,12 @@ package client
 const (
 	Rke2ConfigType                        = "rke2Config"
 	Rke2ConfigFieldClusterUpgradeStrategy = "rke2upgradeStrategy"
+	Rke2ConfigFieldETCD                   = "etcd"
 	Rke2ConfigFieldVersion                = "kubernetesVersion"
 )
 
 type Rke2Config struct {
 	ClusterUpgradeStrategy *ClusterUpgradeStrategy `json:"rke2upgradeStrategy,omitempty" yaml:"rke2upgradeStrategy,omitempty"`
+	ETCD                   *ETCD                   `json:"etcd,omitempty" yaml:"etcd,omitempty"`
 	Version                string                  `json:"kubernetesVersion,omitempty" yaml:"kubernetesVersion,omitempty"`
 }
