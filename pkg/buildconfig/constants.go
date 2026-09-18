3f0d52b2fa44593a7731ca7c952a1e2b3adcaafa
release-automation <release-automation@users.noreply.github.com>
2026-09-18 00:36:20 +0000

    Bump github.com/rancher/remotedialer-proxy to v0.4.6

--- pkg/buildconfig/constants.go
+++ pkg/buildconfig/constants.go
@@ -7,6 +7,6 @@ const (
 	DefaultShellVersion      = "rancher/shell:v0.4.5-rc.1"
 	FleetVersion             = "106.1.18+up0.12.20-rc.1"
 	ProvisioningCAPIVersion  = "106.0.0+up0.7.0"
-	RemoteDialerProxyVersion = "106.0.2+up0.4.6-rc.3"
+	RemoteDialerProxyVersion = "106.0.2+up0.4.6"
 	WebhookVersion           = "106.0.11+up0.7.11-rc.3"
 )
