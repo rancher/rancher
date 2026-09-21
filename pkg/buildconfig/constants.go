9a53e73ece7b113bb99de35797cd87dbdedf95d8
Rodolfo Mói de Oliveira <rodolfo.oliveira@suse.com>
2026-09-21 16:59:51 -0300

    Bump rancher/shell to v0.5.4

--- pkg/buildconfig/constants.go
+++ pkg/buildconfig/constants.go
@@ -6,7 +6,7 @@ const (
 	ChartAuditLogImage       = "rancher/mirrored-bci-micro:15.6.24.2"
 	CspAdapterMinVersion     = "107.0.0+up7.0.0"
 	DefaultSccOperatorImage  = "rancher/scc-operator:v0.5.2-rc.3"
-	DefaultShellVersion      = "rancher/shell:v0.5.4-rc.2"
+	DefaultShellVersion      = "rancher/shell:v0.5.4"
 	FleetVersion             = "107.0.16+up0.13.16"
 	ProvisioningCAPIVersion  = "107.0.0+up0.8.0"
 	RemoteDialerProxyVersion = "106.0.3+up0.5.2"
