d332f002631c3567808f23926ff0aedba7c4ee8e
Swastik Gour <swastik.gour@suse.com>
2026-09-25 10:18:38 +0530

    migrated eks handler from v1 to v2 (#57541)
    Signed-off-by: swastik959 <Sswastik959@gmail.com>

--- pkg/controllers/management/eks/eks_cluster_handler.go
+++ pkg/controllers/management/eks/eks_cluster_handler.go
@@ -14,9 +14,8 @@ import (
 	"github.com/aws/aws-sdk-go-v2/aws"
 	awsconfig "github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/credentials"
+	ekstypes "github.com/aws/aws-sdk-go-v2/service/eks/types"
 	"github.com/aws/aws-sdk-go-v2/service/sts"
-	"github.com/aws/aws-sdk-go/aws/awserr"
-	"github.com/aws/aws-sdk-go/service/eks"
 	eksv1 "github.com/rancher/eks-operator/pkg/apis/eks.cattle.io/v1"
 	"github.com/rancher/eks-operator/utils"
 	apimgmtv3 "github.com/rancher/rancher/pkg/apis/management.cattle.io/v3"
@@ -661,9 +660,10 @@ func (e *eksOperatorController) getRestConfig(ctx context.Context, cluster *mgmt
 	}, nil
 }
 
+// notFound reports whether the error is an EKS ResourceNotFoundException. The v2 SDK models
+// service errors as concrete types instead of the v1 awserr.Error error codes, so the check is
+// done with errors.As against the modeled exception.
 func notFound(err error) bool {
-	if awsErr, ok := err.(awserr.Error); ok {
-		return awsErr.Code() == eks.ErrCodeResourceNotFoundException
-	}
-	return false
+	var notFoundErr *ekstypes.ResourceNotFoundException
+	return errors.As(err, &notFoundErr)
 }
